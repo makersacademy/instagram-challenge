947212630882f65f3abc05d45b676033e75acbd2
Danielle Demkiw <ddemkiw@gmail.com>
2015-02-20 20:39:16 +0000

    can add pictures but not able to display them

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,6 +1,23 @@
 class PicturesController < ApplicationController
   def index
-    @pictures = Pictures.all  
+    @pictures = Picture.all  
   end  
+
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    @picture = Picture.create(post_params)
+    redirect_to '/pictures'
+  end 
+
+  def show
+    @picture = Restaurant.find(post_params)
+  end
+
+  def post_params
+    params.require(:picture).permit(:content, :image)
+  end 
 
 end
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,8 +1,6 @@
 class Picture < ActiveRecord::Base
 
 has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }
-validates_attachment :image, :presence => true,
-  :content_type => { :content_type => "image/jpg" },
-  :size => { :in => 0..100.kilobytes }
+validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
 end
