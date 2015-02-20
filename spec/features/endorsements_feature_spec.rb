1f0d3e1ec20fc1b010dcdf7f10b03aea9a7577a4
stepholdcorn <stephieoldcorn@gmail.com>
2015-02-20 20:32:43 +0000

    Allows a user to leave a comment on a post

---
+++ spec/features/endorsements_feature_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+feature 'endorsements' do
+  before { Post.create description: 'Awesome latte' }
+
+  scenario 'allows user to leave a comment' do
+    visit '/posts'
+    click_link 'Write a comment'
+    fill_in 'Thoughts', with: 'Great picture'
+    click_button 'Comment'
+    expect(page).to have_content 'Great picture'
+    expect(current_path).to eq '/posts'
+  end
+
+end
