4f4dfbf2afc3aecaa2f737e58996bdb18ecfdedc
Josh Bebbington <josh@focalwebsites.com>
2015-02-20 15:19:13 +0000

    Can view a post on its own page

--- spec/features/post_feature_spec.rb
+++ spec/features/post_feature_spec.rb
@@ -1,24 +1,33 @@
 feature 'Posts' do
+  
   context 'Viewing posts' do
-    
     scenario 'No pictures have been added by default' do
       visit '/'
       expect(page).to have_content 'No posts'
       expect(page).to have_link 'Add an image'
     end
 
+    scenario 'Viewing a single post' do
+      create_post
+      click_link('Capybara')
+      expect(current_path).to include '/posts/'
+      expect(page).to have_css('img')
+    end
   end
 
   context 'Creating a post' do
-    
     scenario 'From the homepage' do
-      visit '/'
-      click_link 'Add an image'
-      attach_file 'Image', 'spec/support/capybara.jpg'
-      click_button 'Post Image'
+      create_post
       expect(page).to have_css("img")
       expect(page).not_to have_content 'No posts'
     end
-
   end
+
+end
+
+def create_post
+  visit '/'
+  click_link 'Add an image'
+  attach_file 'Image', 'spec/support/capybara.jpg'
+  click_button 'Post Image'
 end
