127c2051fe4f37fbcb92eb037fe9112ac940cd63
Josh Bebbington <josh@focalwebsites.com>
2015-02-20 14:44:27 +0000

    Images can be uploaded to the public directory

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'haml'
+gem 'paperclip'
 
 group :development, :test do
   gem 'byebug'
--- spec/features/post_feature_spec.rb
+++ spec/features/post_feature_spec.rb
@@ -1,10 +1,23 @@
 feature 'Posts' do
   context 'Viewing posts' do
     
-    scenario 'No pictures have been added' do
+    scenario 'No pictures have been added by default' do
       visit '/'
       expect(page).to have_content 'No posts'
       expect(page).to have_link 'Add an image'
+    end
+
+  end
+
+  context 'Creating a post' do
+    
+    scenario 'From the homepage' do
+      visit '/'
+      click_link 'Add an image'
+      attach_file 'Image', 'spec/support/capybara.jpg'
+      click_button 'Post Image'
+      expect(page).to have_css("img")
+      expect(page).not_to have_content 'No posts'
     end
 
   end
