8ee9521190edf5ea66621030b226d726aee8b538
matteomanzo <imatteo.manzo@gmail.com>
2015-02-21 17:59:11 +0000

    creating post with title and image, test passes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,20 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save ? (redirect_to '/posts') : (render 'new')
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :image)
+  end
 end
--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 feature 'posts' do
   context 'no posts have been added' do 
+
     scenario 'should display a prompt to create a new post' do
       visit '/posts'
       expect(page).to have_content 'No posts...'
@@ -12,14 +13,28 @@ feature 'posts' do
   context 'posts have been added' do
 
     before do
-      Post.create(name: 'Holidays')
+      Post.create(title: 'My bass guitar')
     end
 
     scenario 'display posts' do
       visit '/posts'
-      expect(page).to have_content('Holidays')
+      expect(page).to have_content('My bass guitar')
       expect(page).not_to have_content ('No posts..')
     end
-
   end
+
+  context 'creating posts' do
+
+    scenario 'user creates a post' do
+      visit '/posts'
+      click_link 'Create a post!'
+      fill_in 'Title', with: 'My bass guitar'
+      attach_file 'Image', 'spec/test-images/bass.jpeg'
+      click_button 'Post!'
+      expect(page).to have_content 'My bass guitar'
+      expect(page).to have_css 'img'
+      expect(current_path).to eq '/posts'
+    end
+  end
+
 end
