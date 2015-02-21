2811c3e4b319d7225d44365ecd117b8c1b8f3a2b
stepholdcorn <stephieoldcorn@gmail.com>
2015-02-21 15:40:29 +0000

    Adds user accounts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -17,6 +18,8 @@ Rails.application.routes.draw do
     resources :comments
     resources :likes
   end
+
+  root to: 'posts#index'
 
   # Example resource route with options:
   #   resources :products do
---
+++ spec/features/users_feature_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+context 'user not signed in and on the homepage' do
+  it 'displays sign in and sign up links' do
+    visit '/'
+    expect(page).to have_link 'Sign in'
+    expect(page).to have_link 'Sign up'
+  end
+
+  it 'does not display sign out link' do
+    visit '/'
+    expect(page).not_to have_content 'Sign out'
+  end
+end
+
+context 'user signed in on the homepage' do
+  before do
+    visit '/'
+    click_link 'Sign up'
+    fill_in 'Email', with: 'test@example.com'
+    fill_in 'Password', with: 'testtest'
+    fill_in 'Password confirmation', with: 'testtest'
+    click_button 'Sign up'
+  end
+
+  it 'displays sign out link' do
+    visit '/'
+    expect(page).to have_link 'Sign out'
+  end
+
+  it 'does not display sign in and sign up links' do
+    visit '/'
+    expect(page).not_to have_link 'Sign in'
+    expect(page).not_to have_link 'Sign up'
+  end
+
+end
