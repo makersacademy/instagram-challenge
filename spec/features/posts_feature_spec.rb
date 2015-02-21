003d6d774ed7f1605b26dbb2762e94e842e1304c
bibiana <bcristofol@gmail.com>
2015-02-21 21:03:12 +0000

    Add edit picture

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -32,4 +32,18 @@ feature 'posts' do
     end
   end
 
+  context 'editing posts' do
+    scenario 'let a user edit a restaurant' do
+      create_post
+      visit '/posts'
+      click_link 'Picture'
+      click_link 'Edit'
+      attach_file 'post_picture', 'spec/images/test_pic.jpeg'
+      fill_in 'post_title', with: 'Really cute kitten'
+      click_button 'Update Post'
+      expect(page).to have_content 'Really cute kitten'
+      expect(current_path). to eq '/posts'
+    end
+  end
+
 end
