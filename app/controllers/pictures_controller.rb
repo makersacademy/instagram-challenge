99c793815c7b5328f2134e3a9ea29b8b9f6310fe
ciawalsh <cia.walsh@gmail.com>
2015-02-21 00:39:18 +0000

    Users can add 'Picture' and is connected to their UserID

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -6,4 +6,19 @@ before_action :authenticate_user!
     
   end
 
+  def new
+
+  end
+
+  def create
+    picture = Picture.create(picture_params)
+    picture.user = current_user
+    picture.save
+    redirect_to '/'
+  end
+
+  def picture_params
+    params.require(:picture).permit(:name)
+  end
+
 end
--- spec/features/pictures_feature_spec.rb
+++ spec/features/pictures_feature_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
 feature 'pictures' do
-  
-  context 'no pictures have been added' do
 
   before do
     visit '/'
@@ -12,13 +10,29 @@ feature 'pictures' do
     fill_in('Password confirmation', with: 'testtest')
     click_button 'Sign up'
   end
+  
+  context 'no pictures have been added' do
 
     scenario 'should display a prompt to add a picture' do
       visit '/'
-      expect(page).to have_content 'No pictures'
+      expect(page).to have_content 'No pictures yet'
       expect(page).to have_link 'Add a picture'
     end
 
   end
+
+  context 'creating pictures' do
+    
+    scenario 'prompts user to fill out a form, then displays the new picture' do
+      visit '/'
+      click_link 'Add a picture'
+      fill_in 'Name', with: 'Sunny'
+      click_button 'Create Picture'
+      expect(page).to have_content 'Sunny'
+      expect(current_path).to eq '/'
+    end
+
+end
+
 
 end
