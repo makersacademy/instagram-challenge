4c1761348fc2116312ec400b07be3f97e9c85f2c
matteomanzo <imatteo.manzo@gmail.com>
2015-02-22 22:24:57 +0000

    user must be logged in in order to leave comments on posts

--- spec/features/comments_feature_spec.rb
+++ spec/features/comments_feature_spec.rb
@@ -6,13 +6,29 @@ feature 'comments' do
     Post.create(title: 'Holidays')
   end
 
-  scenario 'allows user to leave a comment on a post' do
+  scenario 'does not allow not logged in user to leave a comment on a post' do
     visit '/posts'
+    click_link 'Comment it'
+    expect(page).to have_content 'You need to sign in or sign up before continuing.'
+    expect(current_path).to eq '/users/sign_in'
+  end
+
+  scenario 'allows logged in user to leave a comment on a post' do
+    visit '/posts'
+    sign_up
     click_link 'Comment it'
     fill_in 'Comment', with: 'Wonderful place'
     click_button 'Comment it!'
     expect(current_path).to eq '/posts'
     expect(page).to have_content 'Wonderful place'
+  end
+
+  def sign_up
+    click_link 'Sign up'
+    fill_in 'Email', with: 'my@email.com'
+    fill_in 'Password', with: 'testtest'
+    fill_in 'Password confirmation', with: 'testtest'
+    click_button 'Sign up'
   end
 
 end
