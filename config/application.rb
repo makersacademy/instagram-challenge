b48e52596f680c567a4e4cf29578a9d7d07d245d
Josh Bebbington <josh@focalwebsites.com>
2015-02-22 22:36:00 +0000

    heroku fix

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,7 @@ module InstagramChallenge
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.serve_static_assets = true
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
