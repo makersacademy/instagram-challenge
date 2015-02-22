389cb7534f8886e8f66817afc8862bb45234a020
matteomanzo <imatteo.manzo@gmail.com>
2015-02-22 17:20:41 +0000

    validate title length

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ActiveRecord::Base
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   has_many :comments, dependent: :destroy
+  validates :title, length: {minimum: 2}
 end
