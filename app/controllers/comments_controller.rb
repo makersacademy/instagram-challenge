3318445f935720776450a1c940803f6946998177
stepholdcorn <stephieoldcorn@gmail.com>
2015-02-22 10:38:11 +0000

    Prevents a user leaving a comment unless they are signing in

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
 
   def new
     @post = Post.find(params[:post_id])
--- spec/features/comments_feature_spec.rb
+++ spec/features/comments_feature_spec.rb
@@ -4,7 +4,7 @@ feature 'comments' do
   before { Post.create description: 'Awesome latte' }
 
   scenario 'allows user to leave a comment' do
-    visit '/posts'
+    sign_up('test@test.com', 'testtest', 'testtest')
     click_link 'Write a comment'
     fill_in 'Thoughts', with: 'Great picture'
     click_button 'Comment'
@@ -12,8 +12,14 @@ feature 'comments' do
     expect(current_path).to eq '/posts'
   end
 
+  scenario 'prevents a user leaving a comment if they are not signed in' do
+    visit '/'
+    click_link 'Write a comment'
+    expect(page).to have_content 'You need to sign in or sign up before continuing.'
+  end
+
   scenario 'an invalid comment' do
-    visit '/posts'
+    sign_up('test@test.com', 'testtest', 'testtest')
     click_link 'Write a comment'
     fill_in 'Thoughts', with: 'XY'
     click_button 'Comment'
@@ -21,4 +27,12 @@ feature 'comments' do
     expect(page).to have_content 'error'
   end
 
+end
+
+def sign_up(email, password, password_confirmation)
+  visit '/users/sign_up'
+  fill_in 'Email', with: email
+  fill_in 'Password', with: password
+  fill_in 'Password confirmation', with: password_confirmation
+  click_button 'Sign up'
 end
