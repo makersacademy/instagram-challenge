0cc7cab883258ab559671113cfbb77fa7c82bd4c
Josh Bebbington <josh@focalwebsites.com>
2015-02-22 15:14:44 +0000

    Users can sign up

--- spec/features/user_feature_spec.rb
+++ spec/features/user_feature_spec.rb
@@ -1,12 +1,24 @@
 feature 'Users' do
-  context 'As a user on the homepage' do
+  context 'As a new user on the homepage' do
+
+    before(:each) { visit '/' }
 
     scenario 'Logged out by default' do
-      visit '/'
-
       expect(page).to have_link 'Sign up'
       expect(page).to have_link 'Sign in'
     end
 
+    scenario 'Can sign up' do
+      click_link 'Sign up'
+      fill_in 'Email', with: 'test@test.com'
+      fill_in 'Password', with: 'password1'
+      fill_in 'Password confirmation', with: 'password1'
+      click_button 'Sign up'
+
+      expect(page).to have_content 'Sign out'
+      expect(page).to have_content 'Welcome! You have signed up successfully'
+    end
+
   end
+
 end
