cf4434107d97f7df656284bde99ea4e56ef5ff9d
Danielle Demkiw <ddemkiw@gmail.com>
2015-02-22 23:46:44 +0000

    more styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "10x10>" }, 
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100X100>" }, 
   :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
