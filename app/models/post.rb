12cd81089e7f1e4ebc0234c5d1b049e2b5c16237
stepholdcorn <stephieoldcorn@gmail.com>
2015-02-22 17:11:09 +0000

    Aligns elements on page

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ActiveRecord::Base
   belongs_to :user
   validates :description, length: { minimum: 2 }
 
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :styles => { :medium => "400x400>", :thumb => "200x200>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   def created_by?(user)
