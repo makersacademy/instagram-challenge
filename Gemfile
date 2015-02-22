be1971ba86bac4626c13be7c89ab04175ecad52e
Sanda Golcea <sanda.golcea@gmail.com>
2015-02-22 23:55:05 +0000

    Allows post creation

--- Gemfile
+++ Gemfile
@@ -10,7 +10,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'spring',        group: :development
-
+gem 'paperclip'
 
 group :test do
   gem 'rspec-rails'
--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -9,3 +9,26 @@ feature 'posts' do
     end
   end
 end
+
+context 'posts have been added' do
+  before do
+    Post.create(name: 'Celebrating Chinese New Year')
+  end
+
+  scenario 'display posts' do
+    visit '/posts'
+    expect(page).to have_content('Celebrating Chinese New Year')
+    expect(page).not_to have_content('There are no posts')
+  end
+end
+
+context 'creating a new post' do
+  scenario 'asks for new post, then displays it' do
+    visit '/posts'
+    click_link 'Create a post'
+    fill_in 'Name', with: 'Chinese New Year'
+    click_button 'Create Post'
+    expect(page).to have_content 'Chinese New Year'
+    expect(current_path).to eq '/posts'
+  end
+end
