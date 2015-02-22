94ee1b09e377bfd61fcf0c9c63e0eb23faadee7c
Andy <andygnewman@gmail.com>
2015-02-22 12:59:28 +0000

    user sign up and sign out tests completed

--- spec/features/user_feature_spec.rb
+++ spec/features/user_feature_spec.rb
@@ -24,19 +24,36 @@ feature 'user' do
     end
 
     it 'should show the user their signed in status' do
-      expect(page).to have_content('You are signed in as: test@test.com')
+      expect(page).to have_content('You are signed in as: test@example.com')
     end
 
-    scenario 'should see sign out link' do
+    it 'should display a sign out link' do
       visit('/')
       expect(page).to have_link('Sign out')
     end
 
-    it 'should not see a sign in link or a sign up link' do
+    it 'should not display a sign in link or a sign up link' do
       visit('/')
       expect(page).not_to have_link('Sign in')
       expect(page).not_to have_link('Sign up')
     end
+
+  end
+
+  context 'user sign out' do
+
+    before do
+      sign_up('test')
+    end
+
+    it 'should sign the user out of their session' do
+      click_link('Sign out')
+      expect(page).to have_content 'Signed out successfully'
+      expect(page).to have_link 'Sign in'
+      expect(page).to have_link 'Sign up'
+      expect(page).not_to have_link 'Sign out'
+    end
+
 
   end
 
