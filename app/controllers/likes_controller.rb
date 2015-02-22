75b8c66e425db61efff111d2d97be74f2c416a01
stepholdcorn <stephieoldcorn@gmail.com>
2015-02-22 10:48:40 +0000

    Prevents a user liking a post unless they are signed in

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,5 @@
 class LikesController < ApplicationController
+  before_action :authenticate_user!
 
   def create
     @post = Post.find(params[:post_id])
--- spec/features/likes_feature_spec.rb
+++ spec/features/likes_feature_spec.rb
@@ -4,8 +4,23 @@ feature 'liking' do
   before { Post.create description: 'Awesome latte' }
 
   scenario 'allows a user to like a post' do
-    visit '/posts'
+    sign_up('test@test.com', 'testtest', 'testtest')
     click_link 'Like'
     expect(page).to have_content('1 like')
   end
+
+  scenario 'prevents a user liking a post unless they are signed in' do
+    visit '/posts'
+    click_link 'Like'
+    expect(page).to have_content 'You need to sign in or sign up before continuing.'
+  end
+
+end
+
+def sign_up(email, password, password_confirmation)
+  visit '/users/sign_up'
+  fill_in 'Email', with: email
+  fill_in 'Password', with: password
+  fill_in 'Password confirmation', with: password_confirmation
+  click_button 'Sign up'
 end
