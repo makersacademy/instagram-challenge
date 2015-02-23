6dd826ce4c2c231a6ea9795f12685c594ed8e3c4
Sanda Golcea <sanda.golcea@gmail.com>
2015-02-23 02:18:31 +0000

    Comments added on side page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  root to: "posts#index"
   get 'posts' => 'posts#index'
-  resources :posts
+  
+  resources :posts do
+    resources :comments
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150223000946) do
+ActiveRecord::Schema.define(version: 20150223020843) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: true do |t|
+    t.text     "thoughts"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "post_id"
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
 
   create_table "posts", force: true do |t|
     t.string   "name"
