22c5a61c97154f7a56a78f8fa9b26c6b3f0a4258
wardymate <chriswarddev@gmail.com>
2015-04-03 19:02:11 +0100

    added picture class to database and migrated. picture class has title and likes

--- instagram_clone/spec/features/pictures_feature_spec.rb
+++ instagram_clone/spec/features/pictures_feature_spec.rb
@@ -8,4 +8,16 @@ feature 'pictures' do
       expect(page).to have_link "Add a picture"
     end
   end
+
+  context 'pictures have been added' do
+    before do
+      Picture.create(name: "First Pic")
+    end
+
+    scenario 'display pictures' do
+      visit '/pictures'
+      expect(page).to have_content "First Pic"
+      expect(page).not_to have_content 'No pictures yet'
+    end
+  end
 end
