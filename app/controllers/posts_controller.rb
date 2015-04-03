e2997b98de3dc5ec23fa7887825d2965974deaeb
Phil Brockwell <nineteen40eight@gmail.com>
2015-04-03 12:56:09 +0100

    more testing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,11 @@
 class PostsController < ApplicationController
 
   def index
-    
+    @posts = Post.all
+  end
+
+  def new
+
   end
 
 end
--- spec/features/post_feature_spec.rb
+++ spec/features/post_feature_spec.rb
@@ -10,4 +10,30 @@ feature 'posts' do
       expect(page).to have_link 'Post to Instantgram'
     end
   end
+
+  context 'a post has been added' do
+
+    before do 
+      Post.create(description: 'First Post')
+    end
+
+    scenario 'display posts' do
+      visit '/posts'
+      expect(page).to have_content('First Post')
+      expect(page).not_to have_content('No posts yet')
+    end
+  end
+
+  context 'posting' do
+
+    scenario 'prompts user to fill out a form then displays the restuarant' do
+      visit '/posts'
+      click_link 'Post to Instantgram'
+      fill_in 'Description', with: 'First Post'
+      click_button 'Post'
+      expect(page).to have_content('First Post')
+      expect(current_path).to eq('/posts')
+    end
+
+  end
 end
