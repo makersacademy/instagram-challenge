f43f66d7b26209127d6cf0917f256be9dbc9d0aa
Phil Brockwell <nineteen40eight@gmail.com>
2015-04-04 10:55:46 +0100

    added likes resource

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,15 +2,4 @@ require 'spec_helper'
 
 describe Post, type: :model do
 
-  it 'initialises with 0 likes' do
-    post = Post.new
-    expect(post.likes).to be(0)
-  end
-
-  it 'can add a like' do
-    post = Post.new
-    post.add_like
-    expect(post.likes).to be(1)
-  end
-
 end
