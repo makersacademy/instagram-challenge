cd5ee32d6c634da59523d84a9c7cdc0cd71018f9
Kevin Lanzon <kevinlanzon@gmail.com>
2015-04-04 17:54:19 +0100

    second test

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -10,4 +10,16 @@ feature 'posts' do
       expect(page).to have_link 'Post to Instagram'
     end
   end
+
+  context 'posts have been added' do
+    before do
+      Post.create(description: 'This is my first post')
+    end
+
+    scenario 'display posts' do
+      visit '/posts'
+      expect(page).to have_content 'This is my first post'
+      expect(page).not_to have_content 'No posts yet'
+    end
+  end
 end
