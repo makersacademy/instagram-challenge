8340130cd1e7f69c317584ebf558ff38442af23c
Phil Brockwell <nineteen40eight@gmail.com>
2015-04-05 13:53:19 +0100

    users and likes association added

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -8,9 +8,9 @@ class LikesController < ApplicationController
   end
 
   def create
-    @post = Post.find(params[:post_id])
-    @post.likes.create
-    flash[:notice] = 'Liked!'
+    post = Post.find(params[:post_id])
+    new_like = post.likes.create(:user_id => current_user)
+    flash[:notice] = 'Liked!' if new_like.save
     redirect_to posts_path
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   
   has_many :posts, dependent: :destroy
-  has_many :comments
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :liked_posts, through: :likes, source: :posts
   has_many :commented_posts, through: :comments, source: :post
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
