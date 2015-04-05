a25d1d955c61b8e3031416b8de5b72272f3df1cb
Phil Brockwell <nineteen40eight@gmail.com>
2015-04-06 00:41:28 +0100

    added user page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
+  resources :users, :only => [:show]
   resources :posts, { shallow: true } do
     resources :likes
     resources :comments
