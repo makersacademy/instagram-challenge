4b0bcd5e4b454a020b4f7bae6692efcb7f2a6e9f
wardymate <chriswarddev@gmail.com>
2015-04-06 19:09:44 +0100

    can add a picture

--- instagram_clone/app/controllers/pictures_controller.rb
+++ instagram_clone/app/controllers/pictures_controller.rb
@@ -4,4 +4,17 @@ class PicturesController < ApplicationController
     @pictures = Picture.all
   end
 
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    Picture.create(picture_params)
+    redirect_to '/pictures'
+  end
+
+  def picture_params
+    params.require(:picture).permit(:title)
+  end
+
 end
--- instagram_clone/spec/features/pictures_feature_spec.rb
+++ instagram_clone/spec/features/pictures_feature_spec.rb
@@ -20,4 +20,16 @@ feature 'pictures' do
       expect(page).not_to have_content 'No pictures yet'
     end
   end
+
+  context 'creating pictures' do
+    scenario 'prompts user to fill out form' do
+      visit '/pictures'
+      click_link 'Add a picture'
+      fill_in 'Title', with: 'Test picture'
+      click_button 'Create Picture'
+      expect(page).to have_content 'Test picture'
+      expect(current_path).to eq '/pictures'
+    end
+  end
+
 end
