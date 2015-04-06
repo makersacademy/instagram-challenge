bd34667d5c2329d512b137f1855187241bb87eec
wardymate <chriswarddev@gmail.com>
2015-04-06 20:37:09 +0100

    users can sign up and sign in with facebook

--- instagram_clone/config/routes.rb
+++ instagram_clone/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   get 'pictures' => 'pictures#index'
   resources :pictures
+
+  root to: "pictures#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
