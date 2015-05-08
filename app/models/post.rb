3073151127f1a03539143871e080dbe606bd1df5
armi1189 <armi1189@gmail.com>
2015-05-08 14:07:50 +0100

    Post title cannot be under 3 char length

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
+  validates :title, length: {minimum: 3}
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -6,6 +6,6 @@ describe Post, type: :model do
   it 'is not valid with a title less than three characters' do
     post = Post.new(title: 'Aw')
     expect(post).to have(1).error_on(:title)
-    expect(post).not_to be_vaild
+    expect(post).not_to be_valid
   end
 end
