cc8b3950e12f10d81fc335306d345a9c84a49791
Richard Lynch <rjlynch86@googlemail.com>
2015-05-08 11:11:52 +0100

    adds feature test for no posts on start/link to add post

---
+++ instagram_clone/spec/features/post_feature_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+feature 'Post' do
+  context 'There are no posts until a first one has been created' do
+    scenario 'There is a prompt to add a post' do
+      visit '/posts'
+      expect(page).to have_content('No posts yet :-(')
+      expect(page).to have_link('add a post!')
+    end
+  end
+end
