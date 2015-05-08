f2b056b9cf9041377a15cb82f4849b74baf7e558
paulwallis42 <paulwallis42@gmail.com>
2015-05-08 14:48:45 +0100

    likes not quite working

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150508111308) do
+ActiveRecord::Schema.define(version: 20150508125710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 20150508111308) do
   end
 
   add_index "comments", ["picture_id"], name: "index_comments_on_picture_id", using: :btree
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "picture_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "likes", ["picture_id"], name: "index_likes_on_picture_id", using: :btree
 
   create_table "pictures", force: :cascade do |t|
     t.string   "title"
@@ -58,4 +66,5 @@ ActiveRecord::Schema.define(version: 20150508111308) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "comments", "pictures"
+  add_foreign_key "likes", "pictures"
 end
