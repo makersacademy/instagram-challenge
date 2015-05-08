cdd6fe3938c94788b8b00198a8034d8f4f9495ac
Richard Lynch <rjlynch86@googlemail.com>
2015-05-08 12:51:03 +0100

    adds and passes test for posts that are in the database are displayed

--- instagram_clone/spec/features/post_feature_spec.rb
+++ instagram_clone/spec/features/post_feature_spec.rb
@@ -8,4 +8,15 @@ feature 'Post' do
       expect(page).to have_link('add a post!')
     end
   end
+
+  context 'Posts that have been created are viewable at the /posts link' do
+    # create a post in the database
+    before { Post.create(name: 'its a post', time:(Time.now.strftime("%y:%b:%d:%H:%M:%S")))}
+    
+    scenario 'Posts are displayed' do
+      visit '/posts'
+      expect(page).to have_content('its a post')
+      expect(page).to have_content(Time.now.strftime("%y:%b:%d:%H:%M:%S"))
+    end
+  end
 end
