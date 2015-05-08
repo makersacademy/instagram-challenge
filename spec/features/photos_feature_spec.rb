9b6bf2ab4440effc19f9d261bbd056719e8ba365
stacca <caronstace@gmail.com>
2015-05-08 14:32:54 +0100

    adding additional pages

--- spec/features/photos_feature_spec.rb
+++ spec/features/photos_feature_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 feature 'photos' do 
   context 'no photos have been added' do
-    scenario 'should display a prompt to add a restaurant' do
+    scenario 'should display a prompt to add a photo' do
       visit '/photos'
       expect(page).to have_content 'No photos yet'
       expect(page).to have_link 'add a photo'
