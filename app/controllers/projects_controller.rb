f409cde19b916329223a96a78869ef904865b3fa
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2015-05-09 23:17:32 +0100

    Displays Projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,5 @@
 class ProjectsController < ApplicationController
   def index
+    @projects = Project.all
   end
 end
--- spec/features/projects_feature_spec.rb
+++ spec/features/projects_feature_spec.rb
@@ -8,4 +8,17 @@ feature 'projects' do
       expect(page).to have_link 'Add a project'
     end
   end
+
+  context 'projects added' do
+    before do
+      Project.create(name:'Pipeline Pigging', company:'RB Geomatics')
+    end
+
+    scenario 'displays projects' do
+      visit '/projects'
+      expect(page).to have_content('Project: Pipeline Pigging')
+      expect(page).to have_content('Company: RB Geomatics')
+      expect(page).not_to have_content('No projects added yet')
+    end
+  end
 end
