8ac33ce499bded23e133fbc43a587e391345304f
sanjsanj <sanjsanj@hotmail.com>
2015-05-09 13:22:13 +0100

    can enter image description

--- spec/features/website_feature_spec.rb
+++ spec/features/website_feature_spec.rb
@@ -10,4 +10,30 @@ feature 'Wesbite' do
       expect(page).to have_link 'Upload Image'
     end
   end
+
+  context 'when image has been added in the database' do
+
+    before do
+      Image.create(description: 'My face')
+    end
+
+    scenario 'will display image description' do
+      visit '/images'
+      expect(page).to have_content('My face')
+      expect(page).not_to have_content('No Images Yet')
+    end
+  end
+
+  context 'uploading images' do
+
+    scenario 'prompts user to enter a description, then displays it' do
+      visit '/images'
+      click_link 'Upload Image'
+      fill_in 'Description', with: 'My face'
+      click_button 'Upload'
+      expect(page).to have_content 'My face'
+      expect(current_path).to eq '/images'
+    end
+  end
+
 end
