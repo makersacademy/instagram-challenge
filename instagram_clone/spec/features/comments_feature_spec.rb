a52696c2f3c06a4c541e15b558737416ed51db8d
Richard Lynch <rjlynch86@googlemail.com>
2015-05-09 11:21:40 +0100

    when parent post is deleted child comments are destroyed

--- instagram_clone/spec/features/comments_feature_spec.rb
+++ instagram_clone/spec/features/comments_feature_spec.rb
@@ -12,4 +12,16 @@ feature 'Leaving a comment' do
     expect(page).to have_content "Yeah buddy!!!"
   end
 
+  scenario 'when parent post is destroyed, child comments are destroyed as well' do
+    post = Post.create(content: 'its a post')
+    visit '/posts'
+    click_link "post ##{post.id}"
+    click_link "comment on post"
+    fill_in :Thoughts, with: "Yeah buddy!!!"
+    click_button 'leave comment'
+    click_link "post ##{post.id}"
+    click_link "delete post"
+    expect(page).not_to have_content("Yeah buddy!!!")
+  end
+
 end
