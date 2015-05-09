e355d4b22f3f1b362d376fe5732cdccaf93bdbe4
stacca <caronstace@gmail.com>
2015-05-09 11:45:20 +0100

    Css update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,8 +9,9 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
 //= require turbolinks
 //= require_tree .
