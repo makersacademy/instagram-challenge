97da555492c9ea8fda1b64f1706f03ed7c484f7a
Richard Lynch <rjlynch86@googlemail.com>
2015-05-09 17:03:16 +0100

    adds only signed in users can comment

--- instagram_clone/app/controllers/comments_controller.rb
+++ instagram_clone/app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!, :except => [:index, :show]
   def new
     @post = Post.find(params[:post_id])
     @comment = Comment.new
--- instagram_clone/spec/features/comments_feature_spec.rb
+++ instagram_clone/spec/features/comments_feature_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 feature 'Leaving a comment' do
   scenario 'allows users to leave a comment' do
     post = Post.create(content: 'its a post')
+    sign_up
     visit '/posts'
     click_link "post ##{post.id}"
     click_link "comment on post"
@@ -12,8 +13,18 @@ feature 'Leaving a comment' do
     expect(page).to have_content "Yeah buddy!!!"
   end
 
+  scenario 'doesnt allow visitors to comment' do
+    post = Post.create(content: 'its a post')
+    visit '/posts'
+    click_link "post ##{post.id}"
+    click_link "comment on post"
+    expect(page).to have_content 'You need to sign in or sign up before continuing.'
+
+  end
+
   scenario 'when parent post is destroyed, child comments are destroyed as well' do
     post = Post.create(content: 'its a post')
+    sign_up
     visit '/posts'
     click_link "post ##{post.id}"
     click_link "comment on post"
@@ -26,6 +37,7 @@ feature 'Leaving a comment' do
 
   scenario 'A comment can be deleted' do
     post = Post.create(content: 'its a post')
+    sign_up
     visit '/posts'
     click_link "post ##{post.id}"
     click_link "comment on post"
