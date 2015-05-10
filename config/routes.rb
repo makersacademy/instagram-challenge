c9cb8e06b0ed5f8350957ab0cfd0b699eed3f0b1
Joseph Knowles <joejknowles@gmail.com>
2015-05-10 17:48:36 +0100

    changed setup for facebook login

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,7 @@ Rails.application.routes.draw do
   resources :photos
   root to: "photos#index"
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  devise_scope :user do
-    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-  end
+
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
