846ec2c92a2d0aed05bc1c5f39e3da0b286dcb74
sanjsanj <sanjsanj@hotmail.com>
2015-05-10 21:56:49 +0100

    run heroku assets:precompile and add remote

--- config/application.rb
+++ config/application.rb
@@ -31,8 +31,9 @@ module InstagramChallenge
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    
+
     #For HEROKU maybe ???
     config.assets.initialize_on_precompile = false
+    config.serve_static_assets = true
   end
 end
