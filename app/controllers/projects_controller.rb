061bb50fbfcf02578fce86f5e491cfc4257093bb
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2015-05-10 11:27:13 +0100

    A new project can be added

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,17 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.all
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    Project.create(project_params)
+    redirect_to '/projects'
+  end
+
+  def project_params
+    params.require(:project).permit(:name, :company)
+  end
 end
