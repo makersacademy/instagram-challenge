0971c8ff747c82ec7d84b8a458ab46aa6198b3db
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2015-05-10 21:24:33 +0100

    Passed test for comments db structure

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150510200838) do
+ActiveRecord::Schema.define(version: 20150510202043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,10 @@ ActiveRecord::Schema.define(version: 20150510200838) do
     t.text     "feedback"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "post_id"
   end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
 
   create_table "posts", force: :cascade do |t|
     t.text     "description"
@@ -60,5 +63,6 @@ ActiveRecord::Schema.define(version: 20150510200838) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "comments", "posts"
   add_foreign_key "posts", "projects"
 end
