59357c23e0e8fa4dc6a9f1314371f8f290a004e4
sanjsanj <sanjsanj@hotmail.com>
2015-05-10 13:46:58 +0100

    method created to upload image for rspec tests

--- spec/helpers/images.rb
+++ spec/helpers/images.rb
@@ -4,6 +4,7 @@ module ImageHelpers
     visit '/images'
     click_link 'Upload Image'
     fill_in 'Description', with: 'My face'
+    attach_file "Image", "spec/fixtures/files/myface.jpg"
     click_button 'Upload'
   end
 
