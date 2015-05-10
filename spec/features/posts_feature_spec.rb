cfadb7eea61471cb104c6bfc1788f1e81882847e
GJMcGowan <gemcgow@gmail.com>
2015-05-10 21:28:41 +0100

    v basic mvp

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -9,6 +9,15 @@ feature 'posts' do
     click_button 'Create Post'
   end
 
+  def sign_up
+    visit '/posts'
+    click_link 'Sign up'
+    fill_in 'Email', with: 'g@g.com'
+    fill_in 'Password', with: '12345678'
+    fill_in 'Password confirmation', with: '12345678'
+    click_button 'Sign up'
+  end
+
   ###
 
   context 'No posts have been made' do
@@ -33,11 +42,20 @@ feature 'posts' do
 
   context 'Adding a post' do
     scenario 'can add a post' do
+      sign_up
       add_post
       expect(page).to have_content 'Beach'
     end
 
+    scenario 'only signed in users can add a post' do
+      Post.create(title: 'Beach')
+      visit('/posts')
+      click_link 'Delete Beach'
+      expect(page).to have_content('Log in')
+    end
+
     scenario 'can view a post in more detail' do
+      sign_up
       add_post
       click_link('Beach')
       expect(page).to have_content 'Title: Beach'
@@ -46,15 +64,20 @@ feature 'posts' do
 
   context 'Posts can be deleted' do
     scenario 'can delete a post' do
+      sign_up
       add_post
       click_link 'Delete Beach'
       expect(page).not_to have_content 'Beach'
       expect(page).to have_content 'Post deleted'
     end
+
+    xscenario 'only user who created post can delete it' do
+    end
   end
 
   context 'Posts can have images attached' do
     scenario 'can add an image' do
+      sign_up
       visit '/posts'
       click_link 'Add a post'
       fill_in 'Title', with: 'Beach'
