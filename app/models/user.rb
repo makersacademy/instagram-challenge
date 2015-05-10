05bc705d91eaa922b57a6c3d2659bed11bf59603
Joseph Knowles <joejknowles@gmail.com>
2015-05-10 18:18:41 +0100

    facebook login

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,7 @@ class User < ActiveRecord::Base
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-      user.name = auth.info.name   # assuming the user model has a name
-      user.image = auth.info.image # assuming the user model has an image
+      user.username = auth.info.name   # assuming the user model has a name
     end
   end
 
