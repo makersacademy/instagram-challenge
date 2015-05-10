6d0bedba19184ccfb9949fdd3ae440311480ccb8
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2015-05-10 11:49:20 +0100

    Can view a particular project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,10 @@
 class ProjectsController < ApplicationController
+
+  def create
+    Project.create(project_params)
+    redirect_to '/projects'
+  end
+
   def index
     @projects = Project.all
   end
@@ -7,12 +13,12 @@ class ProjectsController < ApplicationController
     @project = Project.new
   end
 
-  def create
-    Project.create(project_params)
-    redirect_to '/projects'
+  def show
+    @project = Project.find(params[:id])
   end
 
   def project_params
     params.require(:project).permit(:name, :company)
   end
+
 end
