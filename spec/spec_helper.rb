4c402ec7448e50983c9d07ca13868fbf7e225ab5
armi1189 <armi1189@gmail.com>
2015-05-10 19:28:36 +0100

    coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,11 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+require 'coveralls'
+Coveralls.wear!
+
+# Your code here.
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
