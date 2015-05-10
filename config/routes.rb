d8f4a5d45926a3f354551d1a10213704aa3c584c
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2015-05-10 19:14:59 +0100

    Added tests and passed for devise user sign in

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ end
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root to: 'projects#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
