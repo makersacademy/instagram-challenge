8356eae8c5b0e220cbec4dd2f92d1ba3f79457d0
Richard Lynch <rjlynch86@googlemail.com>
2015-05-10 20:03:17 +0100

    fixed migration issue?

--- db/migrate/20150508124753_rename_name_column_typo_wtf_again_f_me.rb
+++ db/migrate/20150508124753_rename_name_column_typo_wtf_again_f_me.rb
@@ -1,9 +1,9 @@
 class RenameNameColumnTypoWtfAgainFMe < ActiveRecord::Migration
- def self.up
-    rename_table :post, :posts
-  end
+ # def self.up
+ #    rename_table :post, :posts
+ #  end
 
- def self.down
-    rename_table :posts, :post
- end
+ # def self.down
+ #    rename_table :posts, :post
+ # end
 end
