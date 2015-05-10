0c385a8363753603a497278334273cd8d9f3ff5e
Richard Lynch <rjlynch86@googlemail.com>
2015-05-10 20:27:07 +0100

    fixed migration issue?

--- db/migrate/20150509091616_add_post_id_to_comments.rb
+++ db/migrate/20150509091616_add_post_id_to_comments.rb
@@ -1,5 +1,5 @@
 class AddPostIdToComments < ActiveRecord::Migration
   def change
-    add_reference :comments, :posts, index: true, foreign_key: true
+    add_reference :comments, :post, index: true, foreign_key: true
   end
 end
