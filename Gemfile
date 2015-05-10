83b780fab6011143f5564ac554acf101faae0bed
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2015-05-10 18:10:49 +0100

    Added AWS Storage

--- Gemfile
+++ Gemfile
@@ -25,6 +25,10 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'paperclip'
 
+gem 'aws-sdk', '< 2.0'
+
+gem 'devise'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,10 @@
 class Post < ActiveRecord::Base
   belongs_to :project
-  has_attached_file :image, :styles => { :medium => "600x600", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image,
+                    :styles => { :medium => "600x600", :thumb => "100x100>" },
+                    :storage => :s3,
+                    :s3_credentials => {:bucket => ENV['S3_BUCKET_NAME'],
+                                        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+                                        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']}
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
