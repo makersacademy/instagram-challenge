aab08ebf92e29cc51bfd2592dfe9df9e5f469890
stacca <caronstace@gmail.com>
2015-05-11 09:45:20 +0100

    tidy files

--- spec/features/users_feature_spec.rb
+++ spec/features/users_feature_spec.rb
@@ -9,30 +9,29 @@ feature 'users' do
     end
 
     it "should not see 'sign out' link" do
-    visit('/')
-    expect(page).not_to have_link('Sign out')
-  end
+      visit('/')
+      expect(page).not_to have_link('Sign out')
+    end
   end
   context "user signed in on the homepage" do
-  before do
-    visit('/')
-    click_link('Sign Up')
-    fill_in('Email', with: 'test@example.com')
-    fill_in('Password', with: 'testtest')
-    fill_in('Password confirmation', with: 'testtest')
-    click_button('Sign up')
-  end
+    before do
+     visit('/')
+      click_link('Sign Up')
+      fill_in('Email', with: 'test@example.com')
+      fill_in('Password', with: 'testtest')
+      fill_in('Password confirmation', with: 'testtest')
+      click_button('Sign up')
+    end
 
-  it 'should see sign out link' do
-    visit('/')
-    expect(page).to have_link('Sign out')
-  end
+    it 'should see sign out link' do
+      visit('/')
+      expect(page).to have_link('Sign out')
+    end
 
-  it "should not see a 'sign in' link and a 'sign up' link" do
-    visit('/')
-    expect(page).not_to have_link('Log in')
-    expect(page).not_to have_link('Sign Up')
-  end
-end
+    it "should not see a 'sign in' link and a 'sign up' link" do
+      visit('/')
+      expect(page).not_to have_link('Log in')
+      expect(page).not_to have_link('Sign Up')
+    end
   end
 end
