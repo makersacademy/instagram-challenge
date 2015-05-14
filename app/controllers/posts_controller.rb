76571462e3f9e68ea05c4b03732f8581c6da58a9
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2015-05-14 20:28:47 +0100

    Registered users can post on the page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,13 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def create
+    Post.create(post_params)
+    redirect_to '/'
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 feature 'posts' do
-  context 'unregistered user' do
+  context 'user not signed in' do
     before do
       Post.create(title: 'Funny', body: 'The most funny post in history')
     end
@@ -10,6 +10,23 @@ feature 'posts' do
       visit '/posts'
       expect(page).to have_content 'Funny'
       expect(page).to have_content 'The most funny post in history'
+    end
+  end
+
+  context 'signed in user' do
+    before do
+      Post.create(title: 'Funny', body: 'The most funny post in history')
+      visit '/'
+      sign_up
+    end
+
+    scenario 'is able to add new posts' do
+      visit '/posts'
+      fill_in 'Title', with: 'Look at this!'
+      fill_in 'Body', with: 'This is a really cool story'
+      click_button 'Post'
+      expect(page).to have_content 'Look at this!'
+      expect(page).to have_content 'This is a really cool story'
     end
   end
 end
