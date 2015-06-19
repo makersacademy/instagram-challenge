1e5e874771653b87bc011d1694188cd4513e57be
Alex Handy <handy437@gmail.com>
2015-06-19 14:54:31 +0100

    Completed Picture File upload use case

--- instafamous/spec/features/pictures_features_spec.rb
+++ instafamous/spec/features/pictures_features_spec.rb
@@ -8,12 +8,24 @@ feature 'Pictures' do
     expect(page).to have_button('Add Picture')
   end
 
-  xscenario 'show all the pictures on homepage' do
+  scenario 'add a Picture' do
+    visit('/')
+    click_button('Add Picture')
+    attach_file 'Image', '/spec/features/uploads/nuggets.jpeg'
+    fill_in 'Caption', with: 'My first photo'
+    click_button('Post')
+    expect(current_path).to eq ('/')
+    expect(page).to have_content('My first photo')
+    expect(page).to have_css('.img-post')
+  end
+
+  context 'pictures added' do
     before do
       Picture.create(caption: 'Photo 1')
     end
-    visit ('/')
-    expect(page).to have_content('Photo 1')
+    scenario 'show all the pictures on homepage' do
+      visit ('/')
+      expect(page).to have_content('Photo 1')
+    end
   end
-
 end
