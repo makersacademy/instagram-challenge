dd38c72395076b3f862835e29b2bb5dc37cf7e3b
andygout <andygout@hotmail.co.uk>
2015-06-20 22:38:37 +0100

    Adds: OmniAuth installed and functional

--- Gemfile
+++ Gemfile
@@ -46,6 +46,7 @@ group :development, :test do
   gem 'shoulda'
   gem 'rspec-collection_matchers'
   gem 'devise'
+  gem 'omniauth-facebook'
 
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150620202253) do
+ActiveRecord::Schema.define(version: 20150620205554) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 20150620202253) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
