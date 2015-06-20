097f89336177ee312a444d09be27c9466b860019
Anna <anna.klimas@gmail.com>
2015-06-20 16:59:56 +0100

    Added: user can sign up, sign in, and sign out. Tested

---
+++ instagram-challenge/spec/features/helpers/session_helpers.rb
@@ -0,0 +1,19 @@
+module SessionHelpers
+
+  def sign_up(email, password, password_confirmation)
+    visit '/pictures'
+    click_link 'Sign up'
+    fill_in 'Email', with: email
+    fill_in 'Password', with: password
+    fill_in 'Password confirmation', with: password_confirmation
+    click_button 'Sign up'
+  end
+
+  def sign_in(email, password)
+    visit '/pictures'
+    click_link 'Sign in'
+    fill_in 'Email', with: email
+    fill_in 'Password', with: password
+    click_button 'Log in'
+  end
+end
