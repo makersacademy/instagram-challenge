c1a2cf3b5292a45f313a6ff1bfea4de542e4c39f
Anna <anna.klimas@gmail.com>
2015-06-20 14:19:56 +0100

    Added: picture with description. Tested

--- instagram-challenge/app/controllers/pictures_controller.rb
+++ instagram-challenge/app/controllers/pictures_controller.rb
@@ -1,6 +1,23 @@
 class PicturesController < ApplicationController
 
   def index
+    @pictures = Picture.all
+  end
 
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    @picture = Picture.new(picture_params)
+    if @picture.save
+      redirect_to pictures_path
+    else
+      render 'new'
+    end
+  end
+
+  def picture_params
+    params.require(:picture).permit(:description, :image)
   end
 end
--- instagram-challenge/spec/features/pictures_feature_spec.rb
+++ instagram-challenge/spec/features/pictures_feature_spec.rb
@@ -2,10 +2,22 @@ require 'rails_helper'
 
 feature 'Pictures' do
   context 'no pictures have been added' do
-    scenario 'should ask user to log in to add a picture' do
+    scenario 'should ask user to add a picture' do
       visit '/pictures'
       expect(page).to have_content "No pictures added yet"
       expect(page).to have_content "Add a picture"
+    end
+  end
+
+  context 'picture have been added' do
+    scenario 'and displayed on the homepage' do
+      visit '/pictures'
+      click_link 'Add a picture'
+      attach_file 'Image', 'spec/features/Iceland.jpg'
+      fill_in('Description', with: 'My lovely Iceland')
+      click_button 'Create Picture'
+      expect(page).to have_content 'My lovely Iceland'
+      expect(page).to have_css('img[alt="Iceland"]')
     end
   end
 end
