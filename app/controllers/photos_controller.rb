48c489e1d43e6318626c001708ff0cbd7d404a8f
andygout <andygout@hotmail.co.uk>
2015-06-20 16:40:44 +0100

    Adds: RSpec tests for adding photos (w/descr) and link to photo profile page

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,4 +4,21 @@ class PhotosController < ApplicationController
     @photos = Photo.all
   end
 
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.create(photo_params)
+    redirect_to '/photos'
+  end
+
+  def show
+    @photo = Photo.find(params[:id])
+  end
+
+  def photo_params
+    params.require(:photo).permit(:descr)
+  end
+
 end
--- spec/features/photo_feature_spec.rb
+++ spec/features/photo_feature_spec.rb
@@ -1,23 +1,48 @@
 require 'rails_helper'
 
-feature 'photos' do
-  context 'no photos have been added' do
-    scenario 'should display a prompt to add a restaurant' do
+feature 'Photos' do
+  context 'No photos have been added' do
+    scenario 'Should display a prompt to add a restaurant' do
       visit '/photos'
       expect(page).to have_content 'No photos yet'
       expect(page).to have_link 'Add a photo'
     end
   end
 
-  context 'photos have been added' do
+  context 'Photos have been added' do
     before do
       Photo.create(descr: 'Mount Fuji')
     end
 
-    scenario 'display photos' do
+    scenario 'Display photos' do
       visit '/photos'
       expect(page).to have_content('Mount Fuji')
       expect(page).not_to have_content('No photos yet')
     end
   end
+
+  context 'Adding photos' do
+    scenario 'prompts user to add photo, then displays the description' do
+      visit '/photos'
+      click_link 'Add a photo'
+      fill_in 'Description', with: 'Mount Fuji'
+      click_button 'Add photo'
+      expect(page).to have_content 'Mount Fuji'
+      expect(current_path).to eq '/photos'
+    end
+  end
+
+  context 'viewing photos' do
+
+  let!(:mount_fuji){Photo.create(descr:'Mount Fuji')}
+
+  scenario 'lets a user view profile page of a photo' do
+   visit '/photos'
+   click_link 'Mount Fuji'
+   expect(page).to have_content 'Mount Fuji'
+   expect(current_path).to eq "/photos/#{mount_fuji.id}"
+  end
+
+end
+
 end
