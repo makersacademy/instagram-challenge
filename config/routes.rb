0b9da18e3aa7aca0bc6053d03584d36226238c87
Dan Bolger <bowedcymbal@gmail.com>
2015-06-21 23:57:08 +0100

    adds devise and sign-in/out functions

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
 
 
-get 'photos' => 'photos#index'
+  devise_for :users
+  get 'photos' => 'photos#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root to: 'photos#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
