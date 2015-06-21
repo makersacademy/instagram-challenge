e9925fe56f5100aefbdf019f25a91152a07e4c7a
Anna <anna.klimas@gmail.com>
2015-06-21 12:18:44 +0100

    Added: user can sign in with facebook

--- instagram-challenge/config/initializers/devise.rb
+++ instagram-challenge/config/initializers/devise.rb
@@ -236,6 +236,7 @@ Devise.setup do |config|
   config.sign_out_via = :delete
 
   # ==> OmniAuth
+  config.omniauth :facebook, Rails.application.secrets.facebook_app_id, Rails.application.secrets.facebook_app_secret
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
--- instagram-challenge/config/routes.rb
+++ instagram-challenge/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => 'users/omniauth_callbacks' }
   get 'pictures' => 'pictures#index'
   root to: 'pictures#index'
 
