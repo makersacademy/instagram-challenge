3fd86e5056843f5b6a8cb884941ae3df3a2ee0ec
Dan Bolger <bowedcymbal@gmail.com>
2015-06-23 01:34:14 +0100

    passes test for hiding upload button from those not logged in

--- spec/features/photos_feature_spec.rb
+++ spec/features/photos_feature_spec.rb
@@ -3,9 +3,14 @@ require 'rails_helper'
 feature 'photos' do
   context 'no photos have been added' do
     scenario 'should show an upload button' do
-      visit '/photos'
+      visit('/')
+      click_link('Sign up')
+      fill_in('Email', with: 'test@test.es')
+      fill_in('Password', with: 'pAssw0rd')
+      fill_in('Password confirmation', with: 'pAssw0rd')
+      click_button('Sign up')
       expect(page).to have_content 'no photos!'
-      expect(page).to have_button 'Upload your photo'
+      expect(page).to have_button 'Upload a photo'
     end
   end
 
@@ -24,13 +29,23 @@ feature 'photos' do
   context 'creating photos' do
     scenario 'gives user a form to add photo, displays caption' do
       visit '/photos'
-      click_button 'Upload your photo'
+      click_link('Sign up')
+      fill_in('Email', with: 'test@test.es')
+      fill_in('Password', with: 'pAssw0rd')
+      fill_in('Password confirmation', with: 'pAssw0rd')
+      click_button('Sign up')
+      click_button 'Upload a photo'
       fill_in 'Caption', with: 'a lion'
       attach_file 'Image', 'spec/features/lion.jpg'
       click_button 'Create photo'
       expect(current_path).to eq '/photos'
       expect(page).to have_content 'a lion'
       expect(page).to have_selector 'img'
+    end
+
+    scenario 'does not allow posting of photos unless logged in' do
+      visit '/photos'
+      expect(page).not_to have_button 'Upload a photo'
     end
   end
 end
