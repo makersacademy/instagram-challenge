ec791dfa075b26b5f296209b184f463628d84525
Triffanys101 <l.hargrave@hotmail.co.uk>
2015-07-24 12:41:25 +0100

    about to start incorporating photos

--- instagram_clone/spec/features/users_feature_spec.rb
+++ instagram_clone/spec/features/users_feature_spec.rb
@@ -1,1 +1,38 @@
 require 'rails_helper'
+
+feature "User can sign in and out" do
+  context "user not signed in and on the homepage" do
+    it "should see a 'log in' link and a 'sign up' link" do
+      visit '/'
+      expect(page).to have_link('Log in')
+      expect(page).to have_link('Sign Up')
+    end
+
+    it "should not see 'log out' link" do
+      visit '/'
+      expect(page).not_to have_link('Log out')
+    end
+  end
+
+  context "user logged in on the homepage" do
+    before do
+      visit '/'
+      click_link('Sign Up')
+      fill_in('Email', with: 'lucy@mail.com')
+      fill_in('Password', with: 'starwars')
+      fill_in('Password confirmation', with: 'starwars')
+      click_button('Sign up')
+    end
+
+    it "should see 'log out' link" do
+      visit '/'
+      expect(page).to have_link('Log out')
+    end
+
+    it "should not see a 'log in' link and a 'sign up' link" do
+      visit '/'
+      expect(page).not_to have_link('Log in')
+      expect(page).not_to have_link('Sign Up')
+    end
+  end
+end
