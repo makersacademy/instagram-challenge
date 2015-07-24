5b8464acde5dcd7fb8bda4b32a7be3caf7f9550f
MassimilianoMura <massimilianomura1983@gmail.com>
2015-07-24 14:23:54 +0100

    adding first test to check if there is a link to add photos

---
+++ spec/features/photos_feature_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+feature 'photos' do
+
+  context 'no photo have beeb added' do
+    scenario 'should display a prompt to add a photo' do
+      visit '/photos'
+      expect(page).to have_content 'No photos yet'
+      expect(page).to have_link 'Add a photo'
+    end
+  end
+
+end
