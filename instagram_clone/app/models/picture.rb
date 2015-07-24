caac865fe1dfba1c7c731a7e149a7140caba403a
Triffanys101 <l.hargrave@hotmail.co.uk>
2015-07-24 16:20:31 +0100

    images are on the homepage

--- instagram_clone/app/models/picture.rb
+++ instagram_clone/app/models/picture.rb
@@ -2,12 +2,11 @@ class Picture < ActiveRecord::Base
 
   has_attached_file :image,
                     :styles => { :medium => "300x300", :thumb => "100X100" },
-                    :default_url => "/images/:style/missing.png",
+                    :default_url => "",
+                    # :default_url => "/images/:style/missing.png",
                     :path => "#{Rails.root}/public/system/:class/:id_partition/:style.:extension",
                     :storage => :filesystem
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-
-  validates_presence_of :descr
 
 end
