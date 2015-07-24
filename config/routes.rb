dc83556bc327764639cb9e75b78769771f2d0a78
Simon Legg <leggsimon@gmail.com>
2015-07-24 12:16:38 +0100

    pictures controller and view added, prompts to add picture when no pictures exist

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-
+  resources :pictures
   # Example resource route with options:
   #   resources :products do
   #     member do
---
+++ spec/features/pictures_feature_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+feature 'pictures' do
+
+  context 'no pictures have been added' do
+    scenario 'should display a prompt to upload a picture' do
+      visit '/pictures'
+      expect(page).to have_content 'No pictures yet'
+      expect(page).to have_content 'Upload a picture'
+    end
+  end
+end
