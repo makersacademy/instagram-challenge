c62378193e4d2dbd233fede21e2328c378adfea7
Zeina Dajani <dajani.zeina@gmail.com>
2015-07-24 18:03:06 +0100

    upload pictures

---
+++ instaz/db/migrate/20150724145843_create_photos.rb
@@ -0,0 +1,10 @@
+class CreatePhotos < ActiveRecord::Migration
+  def change
+    create_table :photos do |t|
+      t.string :name
+      t.text :description
+
+      t.timestamps null: false
+    end
+  end
+end
