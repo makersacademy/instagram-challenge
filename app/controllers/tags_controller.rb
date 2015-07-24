b3acb99d60811e4f3cdd712948dd72c03d12767c
Nicola Hughes <nicolarhughes@gmail.com>
2015-07-24 14:46:24 +0100

    Creating devise users and tags controller

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,6 @@
+class TagsController < ApplicationController
+
+  def index
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  devise_for :users
+  resources :tags
+
+  root to: "tags#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
