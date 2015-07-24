2f51fee7155362e4a65cc40b4064e298b5963054
TheoLeanse <theoleanse@gmail.com>
2015-07-24 18:47:08 +0100

    adds routes for photos

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  root 'photos#index'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
@@ -13,6 +15,8 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+
+  resources :photos
 
   # Example resource route with options:
   #   resources :products do
