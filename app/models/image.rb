f35aa11dc75bc0f86ff98b2efc5961ac9fb5118e
Nicola Hughes <nicolarhughes@gmail.com>
2015-07-24 16:32:56 +0100

    Adding image attachment to image model

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,4 @@
 class Image < ActiveRecord::Base
+  has_attached_file :picture
+  validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150724152420) do
+ActiveRecord::Schema.define(version: 20150724153023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "images", force: :cascade do |t|
     t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+    t.string   "picture_file_name"
+    t.string   "picture_content_type"
+    t.integer  "picture_file_size"
+    t.datetime "picture_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
