fdf1d41efb33d51ba568f86030a60b059976031b
Triffanys101 <l.hargrave@hotmail.co.uk>
2015-07-24 15:08:22 +0100

    trying to get images to appear on homepage

--- instagram_clone/app/models/picture.rb
+++ instagram_clone/app/models/picture.rb
@@ -8,4 +8,6 @@ class Picture < ActiveRecord::Base
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
+  validates_presence_of :descr
+
 end
--- instagram_clone/spec/features/pictures_feature_spec.rb
+++ instagram_clone/spec/features/pictures_feature_spec.rb
@@ -5,7 +5,7 @@ feature 'pictures' do
     scenario 'should display a prompt to add pictures' do
       visit '/pictures'
       expect(page).to have_content 'No pictures yet!'
-      expect(page).to have_link 'Upload one'
+      expect(page).to have_link 'Upload a picture'
     end
   end
 
@@ -13,8 +13,10 @@ feature 'pictures' do
     scenario 'adds a single picture to the homepage' do
 
       visit '/'
-      click_link 'Upload one'
+      click_link 'Upload a picture'
+      attach_file 'Image', 'spec/features/McAvoy.jpg'
       click_button 'Upload your picture'
+      expect(page).to have_content 'McAvoy'
       expect(current_path).to eq '/pictures'
     end
   end
