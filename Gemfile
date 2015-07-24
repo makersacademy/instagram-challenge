cab8eddccb38d18e732fd3a7c8357626d1c3df29
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-07-24 22:43:08 +0100

    Set up paperclip, test passes for uploading image with caption on front end.

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ gem 'devise'
 gem 'omniauth-facebook'
 
 gem 'haml'
+
+gem 'paperclip'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- spec/features/pictures_feature_spec.rb
+++ spec/features/pictures_feature_spec.rb
@@ -9,5 +9,13 @@ feature 'pictures' do
     end
   end
 
-  
+    scenario 'adding a picture' do
+      visit '/pictures'
+      click_link 'Add a picture'
+      fill_in('Caption', with: 'Test caption')
+      attach_file 'Image', '/users/kirstenjones/projects/instagram-challenge/spec/capybara.jpeg'
+      click_button 'Create Picture'
+      expect(page).to have_content 'Test caption'
+      expect(page).not_to have_content 'No pictures yet'
+    end
 end
