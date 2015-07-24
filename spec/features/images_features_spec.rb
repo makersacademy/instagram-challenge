d92752778ef669fe904b0ab2f1f7f4058cce1663
Nicola Hughes <nicolarhughes@gmail.com>
2015-07-24 18:24:09 +0100

    Ensuring new images are viewable on the home page

--- spec/features/images_features_spec.rb
+++ spec/features/images_features_spec.rb
@@ -1,16 +1,23 @@
 require 'rails_helper'
 
 feature 'Images' do
-  
-  scenario "allows creation" do
-    visit '/images/new'
-    expect(page).to have_selector 'form'
-  end
 
   context 'none have been uploaded' do
     scenario 'should let the user know there are no images' do
       visit '/images'
       expect(page).to have_content 'No images yet'
+    end
+  end
+
+  context 'creating images' do
+
+    scenario "allows creation" do
+      visit '/images'
+      click_link 'Add Image'
+      fill_in 'Description', with: 'Anything'
+      attach_file 'Picture', 'app/assets/images/fatty.jpg'
+      click_button 'Create Image'
+      expect(page).to have_selector 'img[src*="fatty.jpg"]'
     end
   end
 
