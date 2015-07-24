926e19b1035915c60c7ed6a1042f9949be41dc47
Nicola Hughes <nicolarhughes@gmail.com>
2015-07-24 20:03:26 +0100

    Refactoring to have current user create image

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,7 +1,6 @@
 class ImagesController < ApplicationController
 
   before_action :authenticate_user!, except: [:index, :show]
-  # before_action :set_current_user, only: :destroy
 
   def index
     @images = Image.all
@@ -12,12 +11,13 @@ class ImagesController < ApplicationController
   end
 
   def create
-    Image.create(image_params)
+    current_user.images.create(image_params)
     redirect_to images_path
   end
 
   def image_params
     params.require(:image).permit(:description, :picture)
   end
+
 
 end
