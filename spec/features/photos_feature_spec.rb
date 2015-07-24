7d7ab768627e6ed6ab6ff69cf68b58a932348960
MassimilianoMura <massimilianomura1983@gmail.com>
2015-07-24 15:41:09 +0100

    adding photo in the database and a test to check if a photo has been added

--- spec/features/photos_feature_spec.rb
+++ spec/features/photos_feature_spec.rb
@@ -2,11 +2,19 @@ require 'rails_helper'
 
 feature 'photos' do
 
-  context 'no photo have beeb added' do
+  context 'no photo have been added' do
     scenario 'should display a prompt to add a photo' do
       visit '/photos'
       expect(page).to have_content 'No photos yet'
       expect(page).to have_link 'Add a photo'
+    end
+  end
+
+  context 'photo have been added' do
+    before {Photo.create title: 'Awesome see'}
+    scenario 'should display a photo' do
+      visit 'photos'
+      expect(page).to have_content 'Awesome see'
     end
   end
 
