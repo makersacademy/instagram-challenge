af53c7c561e0d26960688dcd3895b0d64e0d9450
Jack Hall-Tipping <jack.halltipping@gmail.com>
2015-07-24 14:29:06 +0100

    trying to write test for displaying pictures

--- spec/features/pictures_feature_spec.rb
+++ spec/features/pictures_feature_spec.rb
@@ -1,11 +1,24 @@
 require 'rails_helper'
 
 feature 'pictures' do
+
   context 'no pictures have been added' do
-    scenario 'should display a prompt to add a pictures' do
+    scenario 'should display a prompt to add a picture' do
       visit '/pictures'
       expect(page).to have_content 'No pictures yet'
       expect(page).to have_link 'Add a picture'
+    end
+  end
+
+  context 'pictures have been added' do
+    before do
+      # upload a picture??
+    end
+
+    scenario 'display pictures' do
+      visit '/pictures'
+      # expect(page).to have_content(THE PICTURE)
+      expect(page).not_to have_content('No pictures yet')
     end
   end
 end
