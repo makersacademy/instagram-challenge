05ba3ce91f25add7ec5787272cd53790ecb37e5d
TheoLeanse <theoleanse@gmail.com>
2015-07-25 11:59:48 +0100

    adds association and validation for uploaded image to Photo model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,4 @@
 class Photo < ActiveRecord::Base
+  has_attached_file :gram, styles: { large: "600x600>", med: "300x300>", thumb: "150x150#" }
+  validates_attachment_content_type :gram, content_type: /\Aimage\/.*\Z/
 end
