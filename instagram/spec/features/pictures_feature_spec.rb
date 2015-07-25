a18550be5fcfdfcd7cdf14b3774ebd7026c4e40a
Elin <elinnet@gmail.com>
2015-07-25 15:44:19 +0100

    creation of failing test of upload of an image to show on home page. next is to change relevant pages

--- instagram/spec/features/pictures_feature_spec.rb
+++ instagram/spec/features/pictures_feature_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 feature 'pictures' do
+
   context 'no pictures have been added' do
     scenario 'should display a prompt to add a picture' do
       visit '/pictures'
@@ -11,7 +12,7 @@ feature 'pictures' do
 
   context 'pictures have been added' do
     before do
-      Picture.create(description: 'raspberries')
+      Picture.create(name: 'garden', description: 'raspberries')
     end
 
     scenario 'display pictures' do
@@ -20,15 +21,20 @@ feature 'pictures' do
       expect(page).not_to have_content('No photos yet')
     end
   end
-end
 
-# context 'photo can be uploaded to the page' do
-#   scenario 'display picture' do
-#     visit '/'
-#     click_link 'Add a picture'
-#     attach_file 'image','spec/features/rasp.jpg'
-#     click_link 'Upload picture'
-#     expect(page).to have_css("img[src='rasp.jpg']")
-#     expect(current_path).to eq '/pictures'
-#   end
-# end
+
+  context 'creating new pictures' do
+    scenario 'user prompted to fill out form, upload picture, which displays new picture' do
+      visit '/pictures'
+      click_link 'Add a picture'
+      attach_file 'Image','spec/features/rasp.jpg'
+      fill_in 'Name', with: 'garden'
+      fill_in 'Description', with: 'raspberries'
+      click_link 'Upload picture'
+      expect(page).to have_css('img[src="rasp.jpg"]')
+      expect(page).to have_content('garden')
+      expect(current_path).to eq '/pictures'
+    end
+  end
+
+end
