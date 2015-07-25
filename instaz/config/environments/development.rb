c100a8ee2a7d6c478656bdb5d551cf7703702582
Zeina Dajani <dajani.zeina@gmail.com>
2015-07-25 23:57:06 +0100

    added users

--- instaz/config/environments/development.rb
+++ instaz/config/environments/development.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   Paperclip.options[:command_path] = "/usr/local/bin/"
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
