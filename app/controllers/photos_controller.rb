4aed9fcc2d75d98310582e75589d002211340246
Natalie Akam <natalie.akam@gmail.com>
2015-07-25 22:08:55 +0100

    Allowed images to be uploaded

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,19 @@
 class PhotosController < ApplicationController
 
   def index
+    @photos = Photo.all
+  end
 
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    Photo.create(photo_params)
+    redirect_to photos_path
+  end
+
+  def photo_params
+    params.require(:photo).permit(:description, :image)
   end
 end
--- spec/features/photos_feature_spec.rb
+++ spec/features/photos_feature_spec.rb
@@ -9,4 +9,16 @@ feature 'photos' do
       expect(page).to have_link 'Upload a photo'
     end
   end
+
+  context 'adding a new photo' do
+  	scenario 'allows user to name a photo' do
+      visit '/'
+      click_link 'Upload a photo'
+      fill_in 'Description', with: 'Instrumental'
+      attach_file 'Image', 'spec/fixtures/files/gramophone.png'
+      click_button 'Create Photo'
+      expect(page).to have_content 'Instrumental'
+      expect(page).not_to have_content 'No photos yet'
+    end
+  end
 end
