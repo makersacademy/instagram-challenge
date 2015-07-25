6cf1d6126536f89d3f98e759be6c10f106e2ab8a
Lucy Oliphant <lucy_oliphant@yahoo.co.uk>
2015-07-25 15:29:30 +0100

    alter image processing

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -4,21 +4,32 @@ feature 'posts' do
   context 'no posts have been added' do
     scenario 'should display a prompt to add a post' do
       visit '/posts'
-      expect(page).to have_content 'No cute animal pictures yet'
+      expect(page).to have_content 'No posts yet'
       expect(page).to have_link 'Add a post'
     end
   end
-end
 
-feature 'adds new post' do
-  scenario 'successfully adds a new picture' do
-    # sign_in
-    visit '/posts'
-    click_link "Add a post"
-    expect(page).to have_content 'Add post'
-    fill_in "Title", with: "Monk"
-    # attach_file "Large cover", "spec/support/uploads/monk_large.jpg"
-    click_button "Add Post"
-    expect(page).to have_selector("img[src='/my-flix/monk_large.jpg']")
-  end
+  context 'post has been added successfully' do
+     before {Post.create name: 'Istanbul'}
+     scenario 'Display photos' do
+       visit '/posts'
+       expect(page).to have_content 'Istanbul'
+       expect(page).not_to have_content 'No posts yet'
+     end
+   end
+
+  #  context 'add post' do
+  #    scenario 'successfully adds a new picture' do
+  #      # sign_in
+  #      visit '/posts'
+  #      click_link "Add a post"
+  #      expect(page).to have_content 'Add post'
+  #      fill_in "Name", with: "Istanbul"
+  #      attach_file 'Image', 'spec/features/support/istanbul.jpg'
+  #      click_button "Add post"
+  #      expect(page).to have_selector("img[src*='istanbul.jpg']")
+  #    end
+  #  end
+
+
 end
