1b37167cbf832f1beac285353f9315972aae8388
Triffanys101 <l.hargrave@hotmail.co.uk>
2015-07-25 17:45:55 +0100

    users can delete other users comments

--- instagram_clone/spec/features/users_feature_spec.rb
+++ instagram_clone/spec/features/users_feature_spec.rb
@@ -85,6 +85,23 @@ feature "When users are logged in" do
     expect(page).to have_content 'You have succesfully deleted your comment'
   end
 
+  it "they cannot delete other peoples comments for pictures" do
+    sign_up
+    click_link 'Upload a picture'
+    attach_file 'Image', 'spec/features/McAvoy.jpg'
+    fill_in 'Description', with: 'Bae'
+    click_button 'Upload your picture'
+    expect(current_path).to eq '/pictures'
+    click_link 'Comment on Bae'
+    fill_in 'Comment', with: 'My bae too'
+    click_button 'Leave your comment'
+    click_link 'Log out'
+    sign_up_2
+    click_link 'Delete comment for Bae'
+    expect(current_path).to eq '/'
+    expect(page).to have_content 'You do not have permission to delete this comment'
+  end
+
   def sign_up
     visit '/'
     click_link 'Sign Up'
