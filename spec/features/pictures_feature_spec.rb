1ef54512f3c29fc421d466eab422691f1326f9e8
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-07-25 10:41:18 +0100

    Users must be signed in to upload images

--- spec/features/pictures_feature_spec.rb
+++ spec/features/pictures_feature_spec.rb
@@ -18,11 +18,19 @@ feature 'pictures' do
     scenario 'a user adds a picture when signed in' do
       visit '/pictures'
       click_link 'Add a picture'
+      expect(current_path).to eq '/pictures/new'
       fill_in('Caption', with: 'Test caption')
       attach_file 'Image', '/users/kirstenjones/projects/instagram-challenge/spec/capybara.jpeg'
       click_button 'Create Picture'
       expect(page).to have_content 'Test caption'
       expect(page).not_to have_content 'No pictures yet'
+    end
+
+    scenario 'a user who is not signed in should not be able to add a picture' do
+      visit '/pictures'
+      click_link 'Sign out'
+      click_link 'Add a picture'
+      expect(current_path).not_to eq '/pictures/new'
     end
   end
 end
