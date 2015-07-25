5cc0ec072193fbd9d30517236c68bb67dc0fb8cf
MassimilianoMura <massimilianomura1983@gmail.com>
2015-07-25 11:35:32 +0100

    adding new and create methods in comments controller and the input form in new comment erb file

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,15 @@
 class CommentsController < ApplicationController
-end
+
+  def new
+    @photos = Photo.find(params[:photo_id])
+    @comments = Comment.new
+  end
+
+  def create
+    @photo = Photo.find(params[:photo_id])
+    @photo.comments.create(comment_params)
+  end
+
+  def comment_params
+    params.require(:comment).permit(:thoughts)
+  end
