9660f499d1f8e2c54e77af46c4680f5d36f4a22c
TheoLeanse <theoleanse@gmail.com>
2015-07-25 12:08:32 +0100

    updates db schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150724222059) do
+ActiveRecord::Schema.define(version: 20150725110639) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "photos", force: :cascade do |t|
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+    t.string   "gram_file_name"
+    t.string   "gram_content_type"
+    t.integer  "gram_file_size"
+    t.datetime "gram_updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
