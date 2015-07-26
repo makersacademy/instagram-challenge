e89a04b88b388fe9c8f419fc9d54ad6455640c58
Elin <elinnet@gmail.com>
2015-07-27 00:40:22 +0100

    edited view files relating to 'likes' - rspec frozen

--- instagram/app/controllers/likes_controller.rb
+++ instagram/app/controllers/likes_controller.rb
@@ -2,8 +2,8 @@ class LikesController < ApplicationController
 
   def create
     @picture = Picture.find(params[:picture_id])
-    @like = Like.new
-    redirect_to pictures_path
+    @picture.likes.create
+    render json: {new_like_count: @picture.likes.count}
   end
 
 
