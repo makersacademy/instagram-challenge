55565c321e71b46c9139fc38c076191c3634ebe0
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-07-26 10:45:39 +0100

    Modify likes controller so create method serves JSON of new like count whose value is computed when the method is called

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -8,6 +8,6 @@ class LikesController < ApplicationController
   def create
     @picture = Picture.find(params[:picture_id])
     @picture.likes.create
-    redirect_to pictures_path
+    render json: {new_like_count: @picture.likes.count}
   end
 end
