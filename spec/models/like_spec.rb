0ebbcd9f4f72c9a07e836cf28728db2068f40c15
MassimilianoMura <massimilianomura1983@gmail.com>
2015-07-26 17:53:47 +0100

    adding test to check if user can like a photo if is signed in and can't if is not

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
-RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+feature 'Liking photos' do
+
+  before {Photo.create title:'Awesome sea'}
+
+  context 'Signed in' do
+    before {sign_up}
+    scenario "User can like a photo, which updates the photo 'like' count" do
+      click_link 'Like'
+      expect(page).to have_content '1 like'
+      expect(page).not_to have_content '0 likes'
+    end
+  end
+
+  context 'Not signed in' do
+    scenario "Does not allow users to like a photo" do
+      visit '/photos'
+      click_link 'Like'
+      expect(page).to have_content '0 likes'
+      expect(page).not_to have_content '1 like'
+    end
+  end
+
+  def sign_up
+    visit('/')
+    click_link('Sign up')
+    fill_in('Email', with: 'test@example.com')
+    fill_in('Password', with: 'testtest')
+    fill_in('Password confirmation', with: 'testtest')
+    click_button('Sign up')
+  end
+
 end
