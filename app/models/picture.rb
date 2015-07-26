bac594c111ef65e1f0e3b2ab4c5cc2fba7dbe946
Simon Legg <leggsimon@gmail.com>
2015-07-26 15:05:12 +0100

    Comment are deleted when picture is deleted

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,6 +3,6 @@ class Picture < ActiveRecord::Base
   has_attached_file :picture, styles: {thumb: "300x300>"}
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
 
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
 end
