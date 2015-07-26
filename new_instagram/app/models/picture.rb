8aa83c371788a18be7aa2ef23375ec78c8fb61b8
Gerard Morera <grdmorera@gmail.com>
2015-07-26 16:23:56 +0100

    trying to add pictures

---
+++ new_instagram/app/models/picture.rb
@@ -0,0 +1,3 @@
+class Picture < ActiveRecord::Base
+  has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }
+end
--- new_instagram/spec/features/pictures_features_spec.rb
+++ new_instagram/spec/features/pictures_features_spec.rb
@@ -5,7 +5,17 @@ feature 'pictures' do
     scenario 'should display a promp to add a picture' do
       visit '/pictures'
       expect(page).to have_content 'No pictures yet'
-      expect(page).to have_link 'Add a picture'
+      expect(page).to have_link 'Add new picture'
+    end
+  end
+  context "Some pictures added" do
+     before do
+    Picture.create(title: me,)
+    end
+    scenario "display pictures" do
+      visit '/pictures'
+      expect(page).to have_content 'me'
+      expect(page).not_to have_content 'No pictures yet'
     end
   end
 end
