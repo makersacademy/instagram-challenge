6c1a536752eec815fc7355fc119ead934d811aa1
TheoLeanse <theoleanse@gmail.com>
2015-07-26 18:17:13 +0100

    adds destroy method to photos controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -10,6 +10,17 @@ class PhotosController < ApplicationController
     @photo = Photo.create( photo_params )
   end
 
+  def destroy
+    @photo = Photo.first(params[:id])
+    if @photo.user == current_user
+      @photo.destroy
+      flash[:notice] = 'Photo successfully deleted'
+    else
+      flash[:notice] = 'Unsupported action'
+    end
+    redirect_to photos_path
+  end
+
   private
 
   def photo_params
