68ac2e541ffe009521c545da951434b566a61e81
Faisal Choura <faisalchoura@Faisals-MacBook-Pro.local>
2015-07-26 12:54:48 +0100

    title of photos now visible on index page

--- spec/features/photo_feature_spec.rb
+++ spec/features/photo_feature_spec.rb
@@ -8,4 +8,15 @@ feature 'restaurants' do
       expect(page).to have_link 'Upload a photo'
     end
   end
+
+  context 'photos have been added' do
+    before do
+      Photo.create(title: 'Best Burger')
+    end
+    scenario 'display title of photos' do
+      visit '/photos'
+      expect(page).to have_content 'Best Burger'
+      expect(page).not_to have_content 'No photos yet'
+    end
+  end
 end
