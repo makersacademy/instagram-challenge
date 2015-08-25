fbe5ddc61fab8df58452c00d8bda740da70c3b24
zdajani <dajani.zeina@gmail.com>
2015-08-25 18:17:38 +0100

    tests refactored with factory girl

--- spec/features/comments_feature_spec.rb
+++ spec/features/comments_feature_spec.rb
@@ -1,17 +1,16 @@
 require 'rails_helper'
 
 feature 'commenting' do
-  before do 
-    @user = User.create username: 'fake', email: 'fake@gmail.com', password: '12345678', password_confirmation: '12345678'
+  before do
+    @user = create(:user)
     login_as @user
-    add_photo  
-    @user2 = User.create username: 'fake2', email: 'fake2@gmail.com', password: '12345678', password_confirmation: '12345678'
+    add_photo 
+    @user2 = create(:user, username: 'fake2', email: 'fake2@fakemail.com')
   end
 
   scenario 'allow users to leave comments using a form' do
      visit '/'
      login_as @user
-     click_link 'Comment'
      fill_in 'Leave a comment', with: 'GRUMPY CAT!!'
      click_button 'Submit'
      expect(current_path).to eq '/'
@@ -21,7 +20,7 @@ feature 'commenting' do
   scenario 'does not allow non-users to leave comments' do
      click_link 'Sign out'
      visit '/'
-     click_link 'Comment'
+     click_button 'Submit'
      expect(current_path).to eq '/users/sign_in'
      expect(page).to have_content('You need to sign in or sign up before continuing')
   end
@@ -41,23 +40,23 @@ feature 'commenting' do
     expect(page).not_to have_content 'GRUMPY CAT!!'
   end
   
-  xcontext 'deleting comments' do
-    before do 
-      login_as @user2
-      add_comment
-    end
-    
-    scenario 'removes comment when it is deleted only by the user added it' do
-      visit '/'
-      click_link 'Grumpy cat'
-      click_link 'Delete comment'
-      expect(page).to have_content 'comment successfully deleted'
-    end
-    
-    scenario 'deos not delete a photo if it is not created by that user' do
-      visit '/'
-      click_link 'Grumpy cat'
-      expect(page).not_to have_content 'Delete comment'
-    end
-  end
+  # xcontext 'deleting comments' do
+  #   before do 
+  #     login_as @user2
+  #     add_comment
+  #   end
+  #   
+  #   scenario 'removes comment when it is deleted only by the user added it' do
+  #     visit '/'
+  #     click_link 'Grumpy cat'
+  #     click_link 'Delete comment'
+  #     expect(page).to have_content 'comment successfully deleted'
+  #   end
+  #   
+  #   scenario 'deos not delete a photo if it is not created by that user' do
+  #     visit '/'
+  #     click_link 'Grumpy cat'
+  #     expect(page).not_to have_content 'Delete comment'
+  #   end
+  # end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -39,6 +39,8 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   include Warden::Test::Helpers
   Warden.test_mode!
+  
+  config.include FactoryGirl::Syntax::Methods
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
