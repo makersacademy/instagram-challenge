e9911c5d41b434fc0e9e7be9d614512bf54d96d2
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-08-29 22:15:34 +0100

    Adjusts top padding and large image sizes. Corrects images path in gitignore.

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,5 @@
 class Picture < ActiveRecord::Base
-  has_attached_file :image, :styles => { :large => "500x500", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/missing.png"
+  has_attached_file :image, :styles => { :large => "500x500>", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   belongs_to :user
