b7d90815cb78c11aa33f88207bfc5b77535010a1
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-08-30 20:45:21 +0100

    Configures app controller, user model, devise.rb config file for using usernames. Generates and customises Devise views for usernames'

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  attr_accessor :login
 
   has_many :pictures,
       -> { extending WithUserAssociationExtension }, dependent: :destroy
@@ -19,6 +20,15 @@ class User < ActiveRecord::Base
       # user.image = auth.info.image
       end
     end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions.to_hash).first
+    end
+  end
 
   def self.new_with_session(params, session)
     super.tap do |user|
