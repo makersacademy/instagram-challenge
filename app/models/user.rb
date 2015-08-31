d221d502a4a690292dacc1c44871f8dd19fbffd8
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-08-31 21:38:56 +0100

    Removes user model validation of username so facebook login is possible

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   has_many :likes
   has_many :liked_pictures, through: :likes, source: :picture
 
-  validates :username, :presence => true, :uniqueness => { :case_sensitive => false }
+  # validates :username, :presence => true, :uniqueness => { :case_sensitive => false }
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
