0e7d9df53ad47b29e5eb7386a80675af5edfac2d
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-09-03 13:58:41 +0100

    Passing tests on picture and comment models

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,9 +1,40 @@
 require 'rails_helper'
 
-describe Comment do
+describe Comment, type: :model do
+
+  before do
+    user = create(:user)
+    user.comments.create(my_comment: 'who is this?')
+  end
 
   it { is_expected.to belong_to :picture }
-
   it { is_expected.to belong_to :user }
 
+  it 'can be deleted by the user who posted it' do
+    comment = Comment.find_by(my_comment: 'who is this?')
+    current_user = User.find_by(username: 'katsuraku')
+    comment.destroy_as current_user
+    expect(Comment.find_by(my_comment: 'who is this?')).to be nil
+  end
+
+  it 'cannot be deleted by a user other then the one who posted it' do
+    comment = Comment.find_by(my_comment: 'who is this?')
+    user2 = User.create email: 'katsuraku@gmail.com', password: 'kjkjkjkj', password_confirmation: 'kjkjkjkj', username: 'kjones'
+    expect(comment.destroy_as user2).to be false
+  end
+
+  describe 'build_with_user' do
+    let(:user) { User.create email: 'k@jones.com', username: 'katsuraku' }
+    let(:pic) { Picture.create caption: 'Yo', image_file_name: "monkeys.jpg" }
+    let(:comment_params) { {my_comment: 'who is this?'} }
+    subject(:comment) { pic.comments.build_with_user(comment_params, user) }
+
+    it 'builds a comment' do
+      expect(comment).to be_a Comment
+    end
+
+    it 'builds a comment associated with the specified user' do
+      expect(comment.user).to eq user
+    end
+  end
 end
--- spec/models/picture_spec.rb
+++ spec/models/picture_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
-describe Picture do
+describe Picture, type: :model do
+
+  before do
+    user = create(:user)
+    user.pictures.create(caption: 'monkey', image_file_name: "monkeys.jpg")
+  end
 
   it { is_expected.to belong_to :user }
 
@@ -8,4 +13,16 @@ describe Picture do
 
   it { is_expected.to have_many :likes }
 
+  it 'can be deleted by the user who posted it' do
+    pic = Picture.find_by(caption: 'monkey')
+    current_user = User.find_by(username: 'katsuraku')
+    pic.destroy_as current_user
+    expect(Picture.find_by(caption: 'monkey')).to be nil
+  end
+
+  it 'cannot be deleted by a user other then the one who posted it' do
+    pic = Picture.find_by(caption: 'monkey')
+    user2 = User.create email: 'katsuraku@gmail.com', password: 'kjkjkjkj', password_confirmation: 'kjkjkjkj', username: 'kjones'
+    expect(pic.destroy_as user2).to be false
+  end
 end
