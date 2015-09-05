a67e3d2e3d4485e19a63b5441cc5dc8746c2e56e
Ben <ben.c.hawker@gmail.com>
2015-09-05 14:05:07 +0100

    Comment belongs to Photo - added dependent destory & test associated with this

--- instagram/spec/models/comment_spec.rb
+++ instagram/spec/models/comment_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it{ is_expected.to belong_to :photo }
+
+  it "when a photo is deleted, it's comments are deleted too" do
+	  photo = Photo.create(description: "Fujisan")
+	  comment = photo.comments.create(comment: "Epic!")
+	  expect { photo.destroy }.to change { Comment.count }.by(-1)
+	end
+
 end
