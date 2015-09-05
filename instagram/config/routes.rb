3596ef87fb9380ee703afbeebdc8e21801acc78b
Ben <ben.c.hawker@gmail.com>
2015-09-05 15:13:05 +0100

    Devise added - user feature tests all passing

--- instagram/config/routes.rb
+++ instagram/config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   resources :photos do
     resources :comments
   end
+
+  root to: "photos#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
