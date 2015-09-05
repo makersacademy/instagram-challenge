d12479d67e0f204914e5ad1c8f7b7c092d37258b
Christopher Alcock <christopher.alcock@gmail.com>
2015-09-05 21:13:04 +0100

    forbidden attributes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,15 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
+
+  def new
+  end
+
+  def create
+    Post.create(params[:post])
+    redirect_to '/posts'
+  end
+
+
 end
--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -20,4 +20,16 @@ feature 'posts' do
       expect(page).not_to have_content('No posts yet')
     end
   end
+
+
+  context 'making posts' do
+    scenario 'prompts user to fill out a form, then displays the new post' do
+      visit '/posts'
+      click_link 'Make a post'
+      fill_in 'Text', with: 'Check out the hot new service CHRINSTAGRAM!!!'
+      click_button 'Create Post'
+      expect(page).to have_content 'Check out the hot new service CHRINSTAGRAM!!!'
+      expect(current_path).to eq '/posts'
+    end
+  end
 end
