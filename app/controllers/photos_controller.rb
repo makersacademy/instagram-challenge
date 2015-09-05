7cf56d43110295d0b4e66850137756c43aa7ca62
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2015-09-05 19:51:33 +0100

    edit function working, 5 tests passing

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -20,4 +20,14 @@ class PhotosController < ApplicationController
   def show
     @photo = Photo.find(params[:id])
   end
+
+  def edit
+    @photo = Photo.find(params[:id])
+  end
+
+  def update
+    @photo = Photo.find(params[:id])
+    @photo.update(photo_params)
+    redirect_to '/photos'
+  end
 end
