3ff681d81280941ec494908238c4d4913acaa538
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2015-09-05 16:29:32 +0100

    3 tests passing, able to add photos page completed

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,4 +3,17 @@ class PhotosController < ApplicationController
   def index
     @photo = Photo.all
   end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.create(photo_params)
+    redirect_to '/photos'
+  end
+
+  def photo_params
+    params.require(:photo).permit(:title)
+  end
 end
