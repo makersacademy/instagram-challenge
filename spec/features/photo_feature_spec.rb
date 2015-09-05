6cad28e8456eea74f96eda80c2bc100fb1072f79
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2015-09-05 15:45:58 +0100

    2 tests passing

--- spec/features/photo_feature_spec.rb
+++ spec/features/photo_feature_spec.rb
@@ -8,4 +8,16 @@ feature 'photos' do
       expect(page). to have_link 'Add a photo'
     end
   end
+
+  context 'photos have been added' do
+    before do
+      Photo.create(title: 'sunset.jpg')
+  end
+
+    scenario 'display photos' do
+      visit '/photos'
+      expect(page).to have_content('sunset.jpg')
+      expect(page).not_to have_content('No photos added')
+    end
+  end
 end
