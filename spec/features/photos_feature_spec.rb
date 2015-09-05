229769b813fb834916132589e88efac4ce815b92
giuseppe de santis <giusdesan@gmail.com>
2015-09-05 20:43:47 +0100

    generated photo model with caption

--- spec/features/photos_feature_spec.rb
+++ spec/features/photos_feature_spec.rb
@@ -3,8 +3,22 @@ require 'rails_helper'
 feature 'photos' do
   context 'no photos have been added' do
     scenario 'should display message "no photos on your feed"' do
-      visit '/photos'
+      visit photos_path
       expect(page).to have_content 'no photos on your feed'
+      expect(page).to have_link 'Add a photo'
     end
   end
+
+  context 'photos have been added' do
+    before do
+      Photo.create(caption: 'Cat')
+    end
+
+    scenario 'display photos' do
+      visit photos_path
+      expect(page).to have_content 'Cat'
+      expect(page).not_to have_content 'no photos on your feed'
+    end
+  end
+
 end
