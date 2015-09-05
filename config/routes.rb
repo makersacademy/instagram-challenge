e180273e5484f999a884e35f0b815fc4f13aca47
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-09-05 19:50:04 +0100

    links to sign in/out sign-up on homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   root 'pictures#index'
   resources :pictures
   # The priority is based upon order of creation: first created -> highest priority.
