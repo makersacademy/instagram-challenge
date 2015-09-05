08cc1839666860c9a2f2c3687de48afc4c74884f
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2015-09-05 15:10:48 +0100

    haml gem added, first test passing

--- spec/features/photo_feature_spec.rb
+++ spec/features/photo_feature_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 feature 'photos' do
   context 'no photos have been added' do
     scenario 'should display a propmt to add a photo' do
-      visit '/'
-      expect(page).to have_content 'No photos added'
+      visit '/photos'
+      expect(page).to have_content 'No photos added!'
       expect(page). to have_link 'Add a photo'
     end
   end
