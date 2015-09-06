037b5f4521aae40f465f555c5a76b817cba2cb4a
Christopher Alcock <christopher.alcock@gmail.com>
2015-09-06 14:19:39 +0100

    test written to stop people deleting others 's posts

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -22,14 +22,28 @@ feature 'posts' do
   end
 
 
-  context 'making posts' do
+  context 'making posts when signed in' do
     scenario 'prompts user to fill out a form, then displays the new post' do
       visit posts_path
+      click_link "Sign up"
+      fill_in 'Email', with: 'test@test.com'
+      fill_in 'Password', with: '12345678'
+      fill_in 'Password confirmation', with: '12345678'
+      click_button 'Sign up'
       click_link 'Make a post'
       fill_in 'Text', with: 'Check out the hot new service CHRINSTAGRAM!!!'
       click_button 'Create Post'
       expect(page).to have_content 'Check out the hot new service CHRINSTAGRAM!!!'
       expect(current_path).to eq posts_path
+    end
+  end
+
+  context 'trying to make posts when signed out' do
+    scenario 'does not allow users to post signed out' do
+      visit posts_path
+      click_link 'Make a post'
+      expect(page).to have_content 'You need to sign in or sign up before continuing'
+      expect(current_path).to eq new_user_session_path
     end
   end
 
@@ -48,17 +62,52 @@ feature 'posts' do
 
   context 'deleting posts' do
 
-    before {Post.create text: 'Check out the hot new service CHRINSTAGRAM!!!'}
-
-    scenario 'removes a post when a user clicks a delete link' do
+    scenario 'removes a post when a user clicks a delete their own link' do
       visit posts_path
+      click_link "Sign up"
+      fill_in 'Email', with: 'test@test.com'
+      fill_in 'Password', with: '12345678'
+      fill_in 'Password confirmation', with: '12345678'
+      click_button 'Sign up'
+      click_link 'Make a post'
+      fill_in 'Text', with: 'Check out the hot new service CHRINSTAGRAM!!!'
+      click_button 'Create Post'
       click_link 'Delete post'
       expect(page).not_to have_content 'Check out the hot new service CHRINSTAGRAM!!!'
       expect(page).to have_content 'Post deleted successfully'
     end
 
+    scenario "errors when a user tries to delete someone else's post" do
+      visit posts_path
+      click_link "Sign up"
+      fill_in 'Email', with: 'test@test.com'
+      fill_in 'Password', with: '12345678'
+      fill_in 'Password confirmation', with: '12345678'
+      click_button 'Sign up'
+      click_link 'Make a post'
+      fill_in 'Text', with: 'Check out the hot new service CHRINSTAGRAM!!!'
+      click_button 'Create Post'
+      click_link 'Sign out'
+      click_link "Sign up"
+      fill_in 'Email', with: 'test2@test.com'
+      fill_in 'Password', with: '12345678'
+      fill_in 'Password confirmation', with: '12345678'
+      click_button 'Sign up'
+      click_link 'Delete post'
+      expect(page).to have_content 'Check out the hot new service CHRINSTAGRAM!!!'
+      expect(page).to have_content 'Youre not allowed to do that'
+    end
+
     scenario 'removes a post even when it has comments' do
       visit posts_path
+      click_link "Sign up"
+      fill_in 'Email', with: 'test@test.com'
+      fill_in 'Password', with: '12345678'
+      fill_in 'Password confirmation', with: '12345678'
+      click_button 'Sign up'
+      click_link 'Make a post'
+      fill_in 'Text', with: 'Check out the hot new service CHRINSTAGRAM!!!'
+      click_button 'Create Post'
       click_link "Reply to post: Check out the hot new service CHRINSTAGRAM!!!"
       fill_in "Text", with: "It's hot!"
       click_button 'Submit reply'
