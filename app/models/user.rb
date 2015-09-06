b4d9723fd9d97d27161bf5b35e98f6e46ef2f6fa
Rebecca Appleyard <rklappleyard@gmail.com>
2015-09-06 13:58:40 +0100

    Altered login to not require username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 
-  attr_accessor :login
+  # attr_accessor :login
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -8,13 +8,13 @@ class User < ActiveRecord::Base
   validates :username, :presence => true,
             :uniqueness => {:case_sensitive => false}
 
-  def self.find_for_database_authentication(warden_conditions)
-    conditions = warden_conditions.dup
-    if login = conditions.delete(:login)
-      where(conditions.to_h).where(["lower(username) = :value OR low(email) = :value", { :value => login.downcase }]).first
-    else
-      where(conditions.to_hash).first
-    end
-  end
+  # def self.find_for_database_authentication(warden_conditions)
+  #   conditions = warden_conditions.dup
+  #   if login = conditions.delete(:login)
+  #     where(conditions.to_h).where(["lower(username) = :value OR low(email) = :value", { :value => login.downcase }]).first
+  #   else
+  #     where(conditions.to_hash).first
+  #   end
+  # end
 
 end
