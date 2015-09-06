c326f073d4ba244400193831d5c17d710ab7d462
RichardCharman <richardcharman@live.com>
2015-09-06 20:34:11 +0000

    Users can now sign up, sign in and sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+
+  root to: "pictures#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  devise_for :users
 get 'pictures' => 'pictures#index'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
