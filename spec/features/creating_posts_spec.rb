f556efddeea9b06f28a6defe1cca8168c2451241
Zaid Al Lahham <zlahham@gmail.com>
2015-09-06 19:06:46 +0100

    Can upload an image with a caption

--- spec/features/creating_posts_spec.rb
+++ spec/features/creating_posts_spec.rb
@@ -4,10 +4,10 @@ feature 'Creating posts' do
   scenario 'can create a post' do
     visit '/'
     click_link 'New Post'
-    attach_file('Image', "spec/files/images/coffee.jpg")
-    fill_in 'Caption', with: 'nom nom nom #coffeetime'
+    attach_file('Image', "spec/files/images/monkey.jpg")
+    fill_in 'Caption', with: 'What a cute little #monkey'
     click_button 'Create Post'
-    expect(page).to have_content('#coffeetime')
-    expect(page).to have_css("img[src*='coffee.jpg']")
+    expect(page).to have_content('#monkey')
+    expect(page).to have_css("img[src*='monkey.jpg']")
   end
 end
