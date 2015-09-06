8f66fc56503c583430803d2a5d3e7db0a4c733dc
Christopher Alcock <christopher.alcock@gmail.com>
2015-09-06 19:53:55 +0100

    added basics of image uploads

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150906182439) do
+ActiveRecord::Schema.define(version: 20150906185257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,9 +29,13 @@ ActiveRecord::Schema.define(version: 20150906182439) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
