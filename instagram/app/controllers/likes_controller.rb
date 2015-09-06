246865ae2b22cbc69e1f49660518f6a4ebffb53e
Ben <ben.c.hawker@gmail.com>
2015-09-06 10:31:12 +0100

    Added likes but views are a mess - AJAX needed

--- instagram/app/controllers/likes_controller.rb
+++ instagram/app/controllers/likes_controller.rb
@@ -1,8 +1,28 @@
 class LikesController < ApplicationController
 
 def new
-	@photo = Photo.find(params[:restaurant_id])
+	@photo = Photo.find(params[:photo_id])
 	@like = Like.new
 end
+
+# def create
+# 	@photo = Photo.find(params[:photo_id])
+# 	@photo.likes.create(like_params)
+# end
+
+def create
+  @photo = Photo.find(params[:photo_id])
+  @like = @photo.likes.create(like_params)
+  if @like.save
+    redirect_to photos_path
+  else
+  	flash[:notice] = 'Houston we have a problem'
+    render 'new'
+  end
+end
+
+def like_params
+	params.require(:like).permit(:like)
+end	
 
 end
