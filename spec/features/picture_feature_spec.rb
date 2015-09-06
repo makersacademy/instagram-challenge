aa490ca593fb5f50b9ab8634700645ff91338068
Ajitsy <sanghera.p@gmail.com>
2015-09-06 15:49:33 +0100

    displays picture caption

--- spec/features/picture_feature_spec.rb
+++ spec/features/picture_feature_spec.rb
@@ -8,4 +8,27 @@ feature 'pictures' do
       expect(page).to have_link 'Add a picture'
     end
   end
+
+  context 'picture caption have been added' do
+    before do
+      Picture.create(caption: 'holiday snap')
+    end
+
+    scenario 'display captions' do
+      visit '/pictures'
+      expect(page).to have_content('holiday snap')
+      expect(page).not_to have_content('No pictures yet')
+    end
+  end
+
+  context 'creating picture captions' do
+    scenario 'prompts user to fill out a form, then displays the new picture caption' do
+      visit '/pictures'
+      click_link 'Add a picture'
+      fill_in 'Caption', with: 'holiday snap'
+      click_button 'Create Picture'
+      expect(page).to have_content 'holiday snap'
+      expect(current_path).to eq '/pictures'
+    end
+  end
 end
