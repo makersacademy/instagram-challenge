45e37129daa92e059bff0538a06cb7e77999e963
Winifred Au <winifredau1990@gmail.com>
2015-09-06 19:55:59 -0400

    adding photos controller and photos html.erb in views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  get 'photos' => 'photos#index'
+  resources :photos
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
