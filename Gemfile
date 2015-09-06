542e72cf566bfbf448ea57f3cef2f152080af7cb
Diego Regules <diegoregules@gmail.com>
2015-09-06 13:50:13 +0100

    Omniauth implemented

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem "paperclip", git: "git://github.com/thoughtbot/paperclip.git"
 
 gem 'devise'
 
+gem 'omniauth-facebook'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root to: "photos#index"
   resources :photos
   # The priority is based upon order of creation: first created -> highest priority.
