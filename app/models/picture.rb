ed723508d359df5c3eccc86ca95ed41f150c8c43
Rebecca Appleyard <rklappleyard@gmail.com>
2015-09-06 20:42:43 +0100

    Fixing hound violations

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -5,14 +5,14 @@ class Picture < ActiveRecord::Base
 
   belongs_to :user
 
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.jpg"
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.jpg"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
   def timestamp
     time_passed = (Time.now - self.created_at).to_i
-    if (time_passed < 60)
+    if time_passed < 60
       return "a moment ago"
-    elsif (time_passed < 3600)
+    elsif time_passed < 3600
       return "#{time_passed / 60}m"
     else
       return "#{time_passed / 3600}h"
