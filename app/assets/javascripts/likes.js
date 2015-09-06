ef3026bb0331840edcdbd17bef4d54ce6c62923b
Rebecca Appleyard <rklappleyard@gmail.com>
2015-09-06 20:04:37 +0100

    Fixed javascript issue

--- app/assets/javascripts/likes.js
+++ app/assets/javascripts/likes.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $('.likes-link').on('click', function(event) {
     event.preventDefault();
 
-    var likesCount = $(this).siblings('.likes_count');
+    var likesCount = $('.likes_count');
 
     $.post(this.href, function(response) {
       likesCount.text(response.new_like_count);
