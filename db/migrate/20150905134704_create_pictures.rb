c2593b975e889e2ae4843050f5a0426264959387
Rebecca Appleyard <rklappleyard@gmail.com>
2015-09-06 20:29:20 +0100

    Fixing hound violations

--- db/migrate/20150905134704_create_pictures.rb
+++ db/migrate/20150905134704_create_pictures.rb
@@ -2,7 +2,6 @@ class CreatePictures < ActiveRecord::Migration
   def change
     create_table :pictures do |t|
       t.string :caption
-
       t.timestamps null: false
     end
   end
--- db/migrate/20150905150932_create_comments.rb
+++ db/migrate/20150905150932_create_comments.rb
@@ -2,7 +2,6 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.text :thoughts
-
       t.timestamps null: false
     end
   end
