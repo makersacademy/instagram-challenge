ada445c1aa51ae45ef86c131c8cae14d64bd90e0
Sivan Patel <sivan.patel@googlemail.com>
2015-09-06 16:38:42 +0100

    Updated tests

--- spec/features/pictures_feature_spec.rb
+++ spec/features/pictures_feature_spec.rb
@@ -22,4 +22,29 @@ feature 'pictures' do
     end
   end
 
+  context 'deleting pictures' do
+    scenario 'pictures can be deleted' do
+      visit '/pictures'
+      click_link('Add a picture')
+      page.attach_file 'picture[image]', 'spec/assets/images/respect.jpg'
+      fill_in 'Name', with: 'respect!'
+      click_button 'Create Picture'
+      click_link('Delete image')
+      expect(page).not_to have_content('respect!')
+      expect(page).not_to have_selector('img')
+    end
+  end
+
+  scenario 'comments are deleted alongside a Picture' do
+    visit '/pictures'
+    click_link('Add a picture')
+    page.attach_file 'picture[image]', 'spec/assets/images/respect.jpg'
+    fill_in 'Name', with: 'respect!'
+    click_button 'Create Picture'
+    click_link('Comment')
+    fill_in('Thoughts', with:'cool pic')
+    click_button('Leave Comment')
+    click_link('Delete image')
+    expect(page).not_to have_content('cool pic')
+  end
 end
