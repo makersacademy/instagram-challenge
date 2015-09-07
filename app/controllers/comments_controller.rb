caf05c31f81995b7513b41ad1e03bc4f48468832
leon-wee <leonwee27@gmail.com>
2015-09-07 19:46:19 +0700

    Only users can delete their own comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,6 +10,16 @@ class CommentsController < ApplicationController
     redirect_to posts_path
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    if @comment.destroy_as_user(current_user)
+      redirect_to posts_path
+    else
+      flash[:notice] = 'Only the user who commented can delete this comment'
+      redirect_to posts_path
+    end
+  end
+
   def comment_params
     params.require(:comment).permit(:thoughts)
   end
--- spec/features/comments_feature_spec.rb
+++ spec/features/comments_feature_spec.rb
@@ -10,4 +10,35 @@ feature 'Comments' do
     expect(current_path).to eq('/posts')
     expect(page).to have_content('I agree')
   end
+
+  context 'deleting comments' do
+    before do
+      user = build(:user)
+      sign_up_as(user)
+
+      visit '/posts'
+      click_link 'Add a post'
+      fill_in 'Caption', with: 'Nah nah nah'
+      attach_file 'post[image]', 'spec/asset_spec/images/testing.png'
+      click_button 'Upload'
+      click_link 'Add Comment'
+      fill_in 'Thoughts', with: "That's nice"
+      click_button 'Leave Comment'
+    end
+
+    scenario 'only creators can delete comments' do
+      visit '/posts'
+      click_link 'Delete Comment'
+      expect(page).not_to have_content('#life')
+    end
+
+    scenario 'non-creators cannot delete comments' do
+      click_link 'Sign out'
+      user2 = build(:user, email: 'testing@testing.com')
+      sign_up_as(user2)
+      visit '/posts'
+      click_link 'Delete Comment'
+      expect(page).to have_content('Only the user who commented can delete this comment')
+    end
+  end
 end
