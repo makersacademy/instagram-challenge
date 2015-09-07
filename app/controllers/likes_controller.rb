18ba02342f37b4e67b751b61b41f8ecbb056adfc
leon-wee <leonwee27@gmail.com>
2015-09-08 00:18:37 +0700

    Commented out JSON

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,10 @@
 class LikesController < ApplicationController
+
   def create
     @post = Post.find(params[:post_id])
     @post.likes.create
+    # render json: {new_like_count: @post.likes.count}
     redirect_to posts_path
   end
+
 end
