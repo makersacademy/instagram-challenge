db43352b1e955e7d7614301f6ebbc28b002a982c
leon-wee <leonwee27@gmail.com>
2015-09-07 16:32:01 +0700

    Implemented Facebook sign in

--- Gemfile
+++ Gemfile
@@ -27,6 +27,8 @@ gem 'paperclip'
 
 gem 'devise'
 
+gem 'omniauth-facebook'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150907072917) do
+ActiveRecord::Schema.define(version: 20150907091908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,10 +52,14 @@ ActiveRecord::Schema.define(version: 20150907072917) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["provider"], name: "index_users_on_provider", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["uid"], name: "index_users_on_uid", using: :btree
 
   add_foreign_key "comments", "posts"
 end
