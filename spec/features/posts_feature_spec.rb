d41e1b5418148b4bd69c5ff35fe043ef62f1dc50
leon-wee <leonwee27@gmail.com>
2015-09-07 11:46:24 +0700

    Passed 2nd test to add post

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -8,4 +8,16 @@ feature 'Posts' do
       expect(page).to have_link('Add a post')
     end
   end
+
+  context 'when posts have been created' do
+    before do
+      Post.create(caption: '#life')
+    end
+
+    scenario 'displays posts' do
+      visit '/posts'
+      expect(page).to have_content('#life')
+      expect(page).not_to have_content('No posts have been added')
+    end
+  end
 end
