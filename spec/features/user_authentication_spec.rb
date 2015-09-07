1aadbce1a0b8696f03481a7b395d786367dd1fd8
Zaid Al Lahham <zlahham@gmail.com>
2015-09-07 07:36:10 +0100

    Passes Test - More User authentication restrictions

--- spec/features/user_authentication_spec.rb
+++ spec/features/user_authentication_spec.rb
@@ -24,4 +24,14 @@ feature 'User authentication' do
     click_link 'Sign Out'
     expect(page).to have_content('You need to sign in or sign up before continuing.')
   end
+
+  scenario 'cannot view index posts without logging in' do
+    visit '/'
+    expect(page).to have_content('You need to sign in or sign up before continuing.')
+  end
+
+  scenario 'cannot create a new post without logging in' do
+    visit new_post_path
+    expect(page).to have_content('You need to sign in or sign up before continuing.')
+  end
 end
