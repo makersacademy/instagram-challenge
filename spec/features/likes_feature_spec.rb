aeb618f6ad25027a4016e1b321fbffc8e2f19a66
Josh <Tattsy@hotmail.co.uk>
2015-09-07 22:06:08 +0100

    likes are displayed proporly

--- spec/features/likes_feature_spec.rb
+++ spec/features/likes_feature_spec.rb
@@ -15,6 +15,21 @@ feature "likes" do
       expect(page).to have_link "Like"
       expect { click_link "Like" }.to change(Like, :count).by(1) 
       expect(page).to have_content "You and 0 people like this photo"
+      click_link "Sign out"
+      sign_up(email: "test2@email.co.uk", username: "Testing")
+      visit "/"
+      click_link "#{ @photo.id }"
+      expect(page).to have_content "1 like"
+      click_link "Like"
+      expect(page).to have_content "You and 1 person like this photo"
+    end
+    scenario "cannot like a photo if not signed in" do
+      click_link "#{ @photo.id }"
+      click_link "Like"
+      click_link "Sign out"
+      click_link "#{ @photo.id }"
+      expect(page).to have_content "1 like"
+      expect(page).not_to have_link "Like"
     end
   end
 end
