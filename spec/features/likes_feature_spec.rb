f8e3c32900a9a6eff09d65c9bd9cae3c30b2f9c4
Sarah Middleton <middleton.sarahlouise@gmail.com>
2015-09-08 15:10:24 +0100

    js driver changed to poltergeist and working

--- spec/features/likes_feature_spec.rb
+++ spec/features/likes_feature_spec.rb
@@ -1,24 +1,7 @@
 require 'rails_helper'
 
-feature 'liking posts' do
-  # before(:each) do
-  #   visit '/'
-  #   click_link 'Sign up'
-  #   fill_in "Email", with: "rob@test.com"
-  #   fill_in "Password", with: "12345678"
-  #   fill_in "Password confirmation", with: "12345678"
-  #   click_button "Sign up"
-  #   click_link "Make a post"
-  #   fill_in "Title", with: "I love cheese"
-  #   attach_file("post[image]", "spec/assets/images/KFC.jpg")
-  #   click_button "Post"
-  # end
-
-  # before(:each) do
-  #   post = Post.create(title: "blah")
-  # end
-
-  scenario 'users can like a post, which updates the like count', js: true do
+feature 'liking posts', js: true do
+  before(:each) do
     user = build(:user)
     sign_up user
     click_link "Make a post"
@@ -26,43 +9,30 @@ feature 'liking posts' do
     attach_file("post[image]", "/Users/Sarah/Documents/Projects/programs/Makers/Challenges/instagram-challenge/instagram_clone/spec/assets/images/KFC.jpg")
     click_button "Post"
     expect(page).to have_content "Like"
+  end
+
+  scenario 'users can like a post, which updates the like count' do
+
+    expect(page).to have_content "Like"
     click_link "Like"
     expect(page).to have_content "1 Like"
   end
 
-  scenario 'user can only like post once', js: true do
-    user = build(:user)
-    sign_up user
-    click_link "Make a post"
-    fill_in "Title", with: "I love cheese"
-    attach_file("post[image]", "/Users/Sarah/Documents/Projects/programs/Makers/Challenges/instagram-challenge/instagram_clone/spec/assets/images/KFC.jpg")
-    click_button "Post"
+  scenario 'user can only like post once' do
     click_link "Like"
     click_link "Like"
     expect(page).to have_content "1 Like"
   end
 
   context "on post page" do
-    scenario 'users can like a post, which updates the like count', js: true do
-      user = build(:user)
-      sign_up user
-      click_link "Make a post"
-      fill_in "Title", with: "I love cheese"
-      attach_file("post[image]", "/Users/Sarah/Documents/Projects/programs/Makers/Challenges/instagram-challenge/instagram_clone/spec/assets/images/KFC.jpg")
-      click_button "Post"
+    scenario 'users can like a post, which updates the like count' do
       # find("img[alt='Kfc']").click
       visit "/posts/1"
       click_link "Like"
       expect(page).to have_content "1 Like"
     end
 
-    scenario 'user can only like post once', js: true do
-      user = build(:user)
-      sign_up user
-      click_link "Make a post"
-      fill_in "Title", with: "I love cheese"
-      attach_file("post[image]", "/Users/Sarah/Documents/Projects/programs/Makers/Challenges/instagram-challenge/instagram_clone/spec/assets/images/KFC.jpg")
-      click_button "Post"
+    scenario 'user can only like post once' do
       # find("img[alt='Kfc']").click
       visit "/posts/1"
       expect(page).to have_content "0 Likes"
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,28 +10,10 @@ require 'capybara/poltergeist'
 require 'capybara-webkit'
 require 'factory_girl_rails'
 # Capybara.default_driver = :selenium
-Capybara.javascript_driver = :webkit
+Capybara.javascript_driver = :poltergeist
 require 'support/database_cleaner'
 require_relative './support/helpers/user_helpers'
-# Add additional requires below this line. Rails is not loaded until this point!
 
-# Requires supporting ruby files with custom matchers and macros, etc, in
-# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
-# run as spec files by default. This means that files in spec/support that end
-# in _spec.rb will both be required and run as specs, causing the specs to be
-# run twice. It is recommended that you do not name files matching this glob to
-# end with _spec.rb. You can configure this pattern with the --pattern
-# option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
-#
-# The following line is provided for convenience purposes. It has the downside
-# of increasing the boot-up time by auto-requiring all files in the support
-# directory. Alternatively, in the individual `*_spec.rb` files, manually
-# require only the support files necessary.
-#
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
-
-# Checks for pending migrations before tests are run.
-# If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
@@ -45,19 +27,7 @@ RSpec.configure do |config|
 
   config.include FactoryGirl::Syntax::Methods
 
-  # RSpec Rails can automatically mix in different behaviours to your tests
-  # based on their file location, for example enabling you to call `get` and
-  # `post` in specs under `spec/controllers`.
-  #
-  # You can disable this behaviour by removing the line below, and instead
-  # explicitly tag your specs with their type, e.g.:
-  #
-  #     RSpec.describe UsersController, :type => :controller do
-  #       # ...
-  #     end
-  #
-  # The different available types are documented in the features, such as in
-  # https://relishapp.com/rspec/rspec-rails/docs
+
   config.infer_spec_type_from_file_location!
 
 end
