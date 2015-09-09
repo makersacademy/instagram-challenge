b5812a466cccf6e349d55aa76ab142ecac8fb6d2
leon-wee <leonwee27@gmail.com>
2015-09-09 21:53:50 +0700

    Added feature tests for liking posts

--- spec/features/likes_feature_spec.rb
+++ spec/features/likes_feature_spec.rb
@@ -19,18 +19,24 @@ feature 'Liking images' do
     expect(page).to have_content('1 like')
   end
 
+  context 'Users are only allowed to like a post once', js: true do
+    scenario '1 user trying to like twice would only increase like once' do
+      visit '/posts'
+      click_link 'Like'
+      click_link 'Like'
+      expect(page).to have_content('1 like')
+    end
 
+    scenario '2 users liking a post' do
+      visit '/posts'
+      click_link 'Like'
+      click_link 'Sign out'
+      user2 = create(:user, email: 'another@person.com', username: 'anotherperson')
+      sign_in_as(user2)
+      click_link 'Like'
+      visit '/posts'
+      expect(page).to have_content('2 likes')
+    end
+  end
 
-  # scenario 'a user can like images of post' do
-  #   visit '/posts'
-  #   click_link 'Like'
-  #   expect(page).to have_content('1 like')
-  # end
-  #
-  # scenario 'the likes are pluralize properly' do
-  #   visit '/posts'
-  #   click_link 'Like'
-  #   click_link 'Like'
-  #   expect(page).to have_content('2 likes')
-  # end
 end
