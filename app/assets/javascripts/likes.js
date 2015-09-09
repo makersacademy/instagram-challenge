29e45dada22f3bceed16b516e940aefaed5b218b
leon-wee <leonwee27@gmail.com>
2015-09-10 00:07:18 +0700

    Added javascript for unlike

--- app/assets/javascripts/likes.js
+++ app/assets/javascripts/likes.js
@@ -8,5 +8,20 @@ $(document).ready(function() {
       $.post(this.href, function(response){
         likeCount.text(response.new_like_count);
     })
-  })
+  });
+
+  $('.unlikes-link').on('click', function(event) {
+    event.preventDefault();
+
+    var likeCount = $(this).siblings('.likes_count');
+
+    $.ajax({
+      url: this.href,
+      type: 'delete',
+      success: function(response){
+        likeCount.text(response.destroy_like_count);
+      }
+    })
+  });
+
 })
