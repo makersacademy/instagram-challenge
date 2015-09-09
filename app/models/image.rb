c41a3c74efa10c85c6f555cc3f4c98e25de6ce23
Toby Clarke <teeohbee@gmail.com>
2015-09-09 09:20:26 +0100

    Gentle refactoring

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,6 +2,6 @@ class Image < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   belongs_to :user
 
-  has_attached_file :upload, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :upload, styles: { large: "640x640", medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :upload, content_type: /\Aimage\/.*\Z/
 end
