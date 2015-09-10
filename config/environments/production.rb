cd9ef59d2aa050992d043a215731c92e4dc1b386
Antonio Voce <antonio.v0c3@gmail.com>
2015-09-10 17:55:49 +0100

    edit secrets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
+      :bucket => 'instafraudnew',
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
