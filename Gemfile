58929218b6403ca5e7385360b38ff6b231d28b02
Zaid Al Lahham <zlahham@gmail.com>
2015-09-10 01:43:25 +0100

    Voting fully works

--- Gemfile
+++ Gemfile
@@ -23,7 +23,7 @@ end
 
 group :development do
   gem 'web-console', '~> 2.0'
-
+  gem 'pry-rails'
   gem 'spring'
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ActiveRecord::Base
 
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  acts_as_voter
 end
