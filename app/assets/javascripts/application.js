6e8c39b2a5f2810337a56b590c3c126558ce5fae
lewis <ljones140@gmail.com>
2015-09-13 11:37:03 +0100

    fixed turbolinks issue properly with jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,5 +16,14 @@
 //= require turbolinks
 //= require_tree .
 
-$(function(){ $(document).foundation(); });
+var ready;
+ready = function() {
+  $(document).foundation();
+};
+$(document).ready(ready);
+$(document).on('page:load', ready);
+
+
+
+
 
