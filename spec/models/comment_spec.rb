5b7d17cf0f876a4780f689ea0131a186c369710f
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-09-14 23:24:59 +0100

    a few tweaks to get Hound off my back

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  it {is_expected.to belong_to :picture}
+  it { is_expected.to belong_to :picture }
 
-  it {is_expected.to belong_to :user}
+  it { is_expected.to belong_to :user }
 
-  it {is_expected.to validate_presence_of :user}
+  it { is_expected.to validate_presence_of :user }
 
-  it {is_expected.to validate_presence_of :picture}
+  it { is_expected.to validate_presence_of :picture }
 
   it 'can be deleted' do
     user =    create :user
