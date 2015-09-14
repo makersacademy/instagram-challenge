30d71060163d69ca32e5f8404aa4da609a31d04a
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-09-14 23:21:56 +0100

    a few tweaks to get Hound off my back

--- spec/models/picture_spec.rb
+++ spec/models/picture_spec.rb
@@ -3,22 +3,22 @@ require 'rails_helper'
 RSpec.describe Picture, type: :model do
   let(:user) { User.create email: 'testuser@email.com', password: "password", password_confirmation: "password" }
 
-  it { should belong_to :user }
+  it {is_expected.to belong_to :user}
 
-  it { should validate_presence_of :user }
+  it {is_expected.to validate_presence_of :user}
 
-  it { should validate_presence_of :title }
+  it {is_expected.to validate_presence_of :title}
 
-  it { should have_attached_file :image }
+  it {is_expected.to have_attached_file :image}
 
-  it { should validate_attachment_presence :image }
+  it {is_expected.to validate_attachment_presence :image}
 
-  it { should validate_attachment_size(:image).
-                less_than(1.megabyte) }
+  it {is_expected.to validate_attachment_size(:image).
+                less_than(1.megabyte)}
 
-  it { should validate_attachment_content_type(:image).
+  it {is_expected.to validate_attachment_content_type(:image).
                 allowing('image/png', 'image/gif').
-                rejecting('text/plain', 'text/xml') }
+                rejecting('text/plain', 'text/xml')}
 
-  it { is_expected.to have_many :comments }
+  it {is_expected.to have_many :comments}
 end
