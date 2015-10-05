fa0d35c699e237e21e742e7a13958cde3d9f7626
Antonio Voce <antonio.v0c3@gmail.com>
2015-10-06 00:38:23 +0100

    add change likes controller to get json, update like view, delete useless helpers, add temp folder to gitignore. All tests green

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,6 +2,6 @@ class LikesController < ApplicationController
   def create
     @photo = Photo.find(params[:photo_id])
     @photo.likes.create
-    redirect_to photos_path
+    render json: {new_like_count: @photo.likes.count}
   end
 end
