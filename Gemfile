74037b821299ed81b498490311b5002b09833d7d
Antonio Voce <antonio.v0c3@gmail.com>
2015-10-07 19:05:58 +0100

    add foundation, and top bar, poltergeist tests now broken

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'aws-sdk', '< 2.0'
 gem 'paperclip'
 gem 'devise'
+gem 'foundation-rails'
 
 
 group :development, :test do
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,6 +12,7 @@ Rails.application.configure do
 
 
 
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
