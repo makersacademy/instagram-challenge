aaa501fe7d78c2f3a1a4c047cabf6f0431e88531
kyle macpherson <kyle1@kyles-MacBook-Pro.local>
2015-10-14 17:44:57 +0100

    Can create and view posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,24 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    redirect_to '/posts'
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
+end
---
+++ spec/features/posts_feature_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+feature 'posts' do
+  context 'no posts have been added' do
+    scenario 'should display a prompt to add a post' do
+      visit '/posts'
+      expect(page).to have_content 'No posts yet'
+      expect(page).to have_link 'Add a post'
+    end
+  end
+
+  context 'restaurants have been added' do
+
+  before do
+    Post.create(content: 'Hello world')
+  end
+
+    scenario 'display posts' do
+      visit '/posts'
+      expect(page).to have_content('Hello world')
+      expect(page).not_to have_content('No posts yet')
+    end
+  end
+
+  context 'creating posts' do
+  scenario 'prompts user to fill out a form, then displays the new post' do
+    visit '/posts'
+    click_link 'Add a post'
+    fill_in 'Content', with: 'Hello World'
+    click_button 'Create Post'
+    expect(page).to have_content 'Hello World'
+    expect(current_path).to eq '/posts'
+  end
+end
+
+  context 'viewing posts' do
+
+  scenario 'lets a user view a post' do
+     post = Post.create(content: 'hey')
+     visit '/posts'
+     click_link 'hey'
+     expect(page).to have_content 'hey'
+     expect(current_path).to eq "/posts/#{post.id}"
+  end
+
+end
+
+end
