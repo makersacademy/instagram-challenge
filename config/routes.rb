6110d8f72afe34670672b363df3f8fe59a6e2f08
Harry <harryandrew.dix@gmail.com>
2015-10-16 21:28:00 +0100

    user controller/user model/devise user/db:migrations - all updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  # devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users #, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   root to: "posts#index"
 
