015fb2147eb49ed71fbbf42c257d4c859f529eee
Harry <harryandrew.dix@gmail.com>
2015-10-16 20:45:00 +0100

    first spec red - about to create posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
-  root to: "application#index"
+  root to: "posts#index"
+
+  resources :users do
+    resources :posts
+  end
   
 end
---
+++ spec/features/user_feature_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+feature 'users' do
+
+  context 'when visiting the home page' do
+
+    scenario 'can see a sign up link' do
+      visit root_path
+      expect(page).to have_button "Sign up"
+    end
+
+  end
+  
+end
