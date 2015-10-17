40ac5bde4cc15099d92b4dd41e4324a61282c1ab
chweeks <chrisharryweeks@gmail.com>
2015-10-17 16:37:45 +0100

    can edit descriptions

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -3,4 +3,31 @@ class PicturesController < ApplicationController
   def index
     @pictures = Picture.all
   end
+
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+  @picture = Picture.new(picture_params)
+    if @picture.save
+      redirect_to pictures_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @picture = Picture.find(params[:id])
+  end
+
+  def update
+    @picture = Picture.find(params[:id])
+    @picture.update(picture_params)
+    redirect_to '/pictures'
+  end
+
+  def picture_params
+    params.require(:picture).permit(:description)
+  end
 end
--- spec/features/picture_feature_spec.rb
+++ spec/features/picture_feature_spec.rb
@@ -9,16 +9,36 @@ feature 'pictures' do
     end
   end
 
+  context 'creating pictures' do
+    scenario 'prompts user to fill out a form, then displays the new pictures' do
+      visit '/pictures'
+      click_link 'Add a picture'
+      fill_in 'Description', with: 'tower bridge'
+      click_button 'Create Picture'
+      expect(page).to have_content 'tower bridge'
+      expect(current_path).to eq '/pictures'
+    end
+  end
+
   context 'pictures have been added' do
 
-  before do
-    Picture.create(description: 'london bridge')
-  end
+    before do
+      Picture.create(description: 'tower bridge')
+    end
 
     scenario 'display pictures' do
       visit '/pictures'
-      expect(page).to have_content('london bridge')
+      expect(page).to have_content('tower bridge')
       expect(page).not_to have_content('No pictures yet')
+    end
+
+    scenario 'let a user edit a picture' do
+      visit '/pictures'
+      click_link 'Edit'
+      fill_in 'Description', with: 'london bridge'
+      click_button 'Update Picture'
+      expect(page).to have_content 'london bridge'
+      expect(current_path).to eq '/pictures'
     end
   end
 end
