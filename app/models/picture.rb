4c903943a28cc5339bf6cc6415e03fdfb1e880c3
Proskurina <yproskurina@yahoo.com>
2015-10-17 22:01:31 +0100

    Added belongs_to, dependent destroy and respective tests

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,6 @@
 class Picture < ActiveRecord::Base
 
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_attached_file :image, styles: { medium: "300x300>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
