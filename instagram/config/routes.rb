87802cf0eb8fed63e911fe19288256601a4ebfaa
Adrian Booth <Adrian.booth17@gmail.com>
2015-10-17 22:05:22 +0100

    Form added to upload images. Image not being saved in db

--- instagram/config/routes.rb
+++ instagram/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+root "pictures#index"
 get 'pictures' => 'pictures#index'
 resources :pictures
 
--- instagram/spec/features/pictures_feature_spec.rb
+++ instagram/spec/features/pictures_feature_spec.rb
@@ -23,4 +23,18 @@ feature 'pictures' do
     end
   end
 
+  context 'uploading images' do
+    scenario 'prompts user to upload an image and write a caption' do
+      visit '/pictures'
+      upload_picture
+      expect(page).to have_content "Picture uploaded"
+    end
+  end
+
+  def upload_picture
+    fill_in "caption", with: "lunch"
+    attach_file "Image", 'spec/spec_assets/lunch.png'
+    click_button "Upload"
+  end
+
 end
