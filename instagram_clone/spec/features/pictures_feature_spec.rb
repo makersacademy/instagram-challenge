b64fc84b459e96830b795d2ad62d6f89803c4322
Harriet Craven <harrietc52@hotmail.co.uk>
2015-10-17 13:03:17 +0100

    added picture model

--- instagram_clone/spec/features/pictures_feature_spec.rb
+++ instagram_clone/spec/features/pictures_feature_spec.rb
@@ -8,4 +8,17 @@ feature 'pictures' do
       expect(page).to have_link 'Add a picture'
     end
   end
+
+  context 'pictures have been added' do
+    before do
+      Picture.create(title: 'Holiday')
+    end
+
+    scenario 'display picture' do
+      visit '/pictures'
+      expect(page).to have_content('Holiday')
+      expect(page).not_to have_content('No pictures yet')
+    end
+  end
+
 end
