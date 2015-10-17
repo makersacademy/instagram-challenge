763a2574821f9e5160c0ac65160784670e05830a
Adrian Booth <Adrian.booth17@gmail.com>
2015-10-17 16:58:56 +0100

    Display picture test passing

--- instagram/spec/features/pictures_feature_spec.rb
+++ instagram/spec/features/pictures_feature_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 
 feature 'pictures' do
+
   context 'no pictures should be added' do
     scenario 'should display a prompt to add a picture' do
       visit '/pictures'
@@ -9,4 +10,17 @@ feature 'pictures' do
       expect(page).to have_link "Upload a picture"
     end
   end
+
+  context 'pictures have been uploaded' do
+    before do
+      Picture.create(caption: "Food")
+    end
+
+    scenario 'display pictures' do
+      visit '/pictures'
+      expect(page).to have_content("Food")
+      expect(page).not_to have_content("No pictures yet")
+    end
+  end
+
 end
