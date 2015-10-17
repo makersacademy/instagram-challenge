5f20f5c3863acce2f68c17f9110169bf4d9b7ffc
Jongmin Kim <kjm141215@gmail.com>
2015-10-18 00:27:04 +0100

    Edit create action in likes_controller

--- spec/features/likes_feature_spec.rb
+++ spec/features/likes_feature_spec.rb
@@ -8,6 +8,7 @@ feature 'liking pictures' do
   scenario 'user can like a picture, which updates the like count' do
     visit '/pictures'
     click_link 'Like'
+
     expect(page).to have_content '1 like'
   end
 end
