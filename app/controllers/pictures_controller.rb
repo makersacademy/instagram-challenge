c4ae152943c6c1d5684793503553569229ecf0d6
chn.challenger <chn.challenger@gmail.com>
2015-10-18 00:52:37 +0100

    can now delete a picture which also removes the uploaded picture from public directory

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -13,8 +13,14 @@ class PicturesController < ApplicationController
     redirect_to pictures_path
   end
 
+  def destroy
+    Picture.find(params[:id]).destroy
+    flash[:notice] = 'Picture deleted successfully'
+    redirect_to pictures_path
+  end
+
   def picture_params
-    params.require(:picture).permit(:caption, :image)
+    params.require(:picture).permit(:caption, :name, :image)
   end
 
 end
--- spec/features/pictures_feature_spec.rb
+++ spec/features/pictures_feature_spec.rb
@@ -15,26 +15,56 @@ feature 'pictures' do
   context 'pictures has been uploaded' do
 
     before do
-      Picture.create(caption: 'coding at home')
+      Picture.create(caption: 'coding at home', name: 'Nightlife')
     end
 
     scenario 'display pictures' do
       visit '/pictures'
-      expect(page).to have_content('coding at home')
-      expect(page).not_to have_content('No one has uploaded any pictures yet!')
+      expect(page).to have_content 'Nightlife'
+      expect(page).to have_content 'coding at home'
+      expect(page).not_to have_content 'No one has uploaded any pictures yet!'
+    end
+
+  end
+
+  context 'deleting restaurant' do
+
+    before do
+      Picture.create(caption: 'coding at home', name: 'Nightlife')
+    end
+
+    scenario 'display pictures' do
+      visit '/pictures'
+      expect(page).to have_content 'coding at home'
+      expect(page).to have_link 'Delete Nightlife'
+    end
+
+    scenario 'can delete a picture' do
+      visit '/pictures'
+      click_link 'Delete Nightlife'
+      expect(current_path).to eq '/pictures'
+      expect(page).to have_content "Picture deleted successfully"
+      expect(page).to have_content "No one has uploaded any pictures yet!"
     end
 
   end
 
   context 'uploading new pictures' do
 
+    after do
+      click_link 'Delete Had Nandos!'
+      #does not help need to think of a way to stub the saving to public folder
+    end
+
     scenario 'give a user a form for add a new picture then display it' do
       visit '/pictures'
       click_link 'Upload a picture'
+      fill_in 'Name', with: 'Had Nandos!'
       fill_in 'Caption', with: 'what a day!'
       page.attach_file("picture_image", File.absolute_path('./spec/imgs/nandos.png'))
+      #does not help need to think of a way to stub the saving to public folder
       click_button 'Create Picture'
-      expect(page).to have_content('what a day!')
+      expect(page).to have_content 'what a day!'
       expect(page).to have_css('img', text: "")
       expect{page.find(:xpath, "//img[@alt='Nandos']")}.not_to raise_error
     end
