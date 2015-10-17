ed889d15a2a66da783e665b9bc482fa5e74ee53b
Harriet Craven <harrietc52@hotmail.co.uk>
2015-10-17 12:36:32 +0100

    added pictures controller

--- instagram_clone/config/routes.rb
+++ instagram_clone/config/routes.rb
@@ -7,12 +7,13 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  # get 'restaurants' => 'restaurants#index'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+    resources :pictures
 
   # Example resource route with options:
   #   resources :products do
