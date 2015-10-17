66c659cc4d1e399c8fc0c8a3ff6cb2bf7c750158
gavin.w.cheng <gavin.w.cheng@gmail.com>
2015-10-17 18:53:42 +0100

    Can add photo with title

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,18 @@
+class PhotosController < ApplicationController
+  def index
+    @photos = Photo.all
+  end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    Photo.create(photo_params)
+    redirect_to photos_path
+  end
+
+  def photo_params
+    params.require(:photo).permit(:title, :image)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  # get 'photos' => 'photos#index'
+  resources :photos
 end
