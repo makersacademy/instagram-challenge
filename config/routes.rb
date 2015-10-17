6e7658cb252824dcf478d80caec5d6bb5336c088
chweeks <chrisharryweeks@gmail.com>
2015-10-17 23:30:08 +0100

    added devise and sign in/out/up links

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  root to: "pictures#index"
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   resources :pictures do
