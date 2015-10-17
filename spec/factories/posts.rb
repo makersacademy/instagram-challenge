ca11c95a10074b9826c983ac1c32e76de5a04801
Harry <harryandrew.dix@gmail.com>
2015-10-17 17:49:33 +0100

    styling updates

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,7 +1,7 @@
-FactoryGirl.define do
-  factory :post do
-    caption "MyString"
-likes 1
-  end
+# FactoryGirl.define do
+#   factory :post do
+#     caption "MyString"
+#     likes 1
+#   end
 
-end
+# end
