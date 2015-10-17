156646e80f317f298b55cb80ef44ffeaf7e38b08
Jongmin Kim <kjm141215@gmail.com>
2015-10-17 11:54:14 +0100

    Added devise and user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   resources :pictures do
     resources :comments
   end
+  root 'pictures#index'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
