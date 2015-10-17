7b3c1f763980a7ddf22a13796fbec60997adc68e
chweeks <chrisharryweeks@gmail.com>
2015-10-17 20:09:15 +0100

    comments deleted when picture deleted

--- spec/features/comment_feature_spec.rb
+++ spec/features/comment_feature_spec.rb
@@ -13,4 +13,15 @@ feature 'comments' do
      expect(page).to have_content('great')
   end
 
+  scenario 'comments deleted when picture deleted' do
+
+    visit '/pictures'
+    click_link 'Comment'
+    fill_in "Comment", with: "great"
+    click_button 'Leave Comment'
+    click_link 'Delete'
+    expect(current_path).to eq '/pictures'
+    expect(page).to_not have_content('great')
+  end
+
 end
