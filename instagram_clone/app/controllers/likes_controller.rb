22ec9520da3ddcd4032f1c137effcc8f52a5cb84
Harriet Craven <harrietc52@hotmail.co.uk>
2015-10-18 00:23:10 +0100

    like endorsement working

--- instagram_clone/app/controllers/likes_controller.rb
+++ instagram_clone/app/controllers/likes_controller.rb
@@ -2,6 +2,6 @@ class LikesController < ApplicationController
   def create
     @picture = Picture.find(params[:picture_id])
     @picture.likes.create
-    redirect_to pictures_path
+    render json: {new_like_count: @picture.likes.count}
   end
 end
--- instagram_clone/spec/features/likes_feature_spec.rb
+++ instagram_clone/spec/features/likes_feature_spec.rb
@@ -3,12 +3,13 @@ require 'rails_helper'
 feature 'liking pictures' do
 
   scenario 'a user can like a picture, which updates the likes count', js: true do
-    visit '/pictures'
+    visit '/'
     user = build :user
     sign_up(user)
     click_link 'Add a picture'
     fill_in 'Title', with: 'Holiday'
     attach_file "picture_image", "spec/fixtures/image3.png"
+    # attach_file(id="picture_image", Rails.root + 'public/images/thumb/image2.png')
     click_button 'Create Picture'
     click_link 'Like'
     expect(page).to have_content('1 Like')
