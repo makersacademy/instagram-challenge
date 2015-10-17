62f7e3e8423b627c514a752b542be05887022905
Jongmin Kim <kjm141215@gmail.com>
2015-10-18 00:43:27 +0100

    Amend create action in likes_controller so that user can like a picture only when signed in

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,11 @@
 class LikesController < ApplicationController
   def create
     @picture = Picture.find(params[:picture_id])
-    @like = @picture.likes.create
-    redirect_to pictures_path
+    if user_signed_in?
+      @like = @picture.likes.create
+      redirect_to pictures_path
+    else
+      redirect_to pictures_path
+    end
   end
 end
--- spec/features/likes_feature_spec.rb
+++ spec/features/likes_feature_spec.rb
@@ -4,11 +4,28 @@ feature 'liking pictures' do
   before do
     @user = create :user
     @user.pictures.create(picture_file_name: 'associations')
+    sign_in(@user)
   end
   scenario 'user can like a picture, which updates the like count' do
     visit '/pictures'
     click_link 'Like'
-
     expect(page).to have_content '1 like'
   end
+
+  scenario 'clicking like link twice results in "2 likes"' do
+    visit '/pictures'
+    click_link 'Like'
+    click_link 'Like'
+    expect(page).to have_content '2 likes'
+  end
+
+  context 'user not signed in' do
+    scenario 'user cannot like a picture' do
+      visit '/pictures'
+      click_link 'Sign out'
+      click_link 'Like'
+      expect(page).to have_content '0 likes'
+    end
+  end
+
 end
