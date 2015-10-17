a92a01f5922ab98b3d0983abed449764095c745e
Proskurina <yproskurina@yahoo.com>
2015-10-17 16:31:35 +0100

    Second test passes, picture can be created, can have image

---
+++ app/models/picture.rb
@@ -0,0 +1,6 @@
+class Picture < ActiveRecord::Base
+
+  has_attached_file :image, styles: { medium: "300x300>" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+end
--- spec/features/pictures_feature_spec.rb
+++ spec/features/pictures_feature_spec.rb
@@ -8,4 +8,17 @@ feature 'pictures' do
       expect(page).to have_link 'Add a picture'
     end
   end
+
+  context 'pictures have been added' do
+    before do
+      Picture.create(image_file_name: 'spec/assets/images/dism.jpg')
+    end
+
+    scenario 'display pictures' do
+      visit '/pictures'
+      expect(page).to have_xpath("//img[@alt='Dism']")
+      expect(page).not_to have_content('No pictures yet')
+    end
+  end
+
 end
