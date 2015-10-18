2da907d72740baffb9342a90c987310d90603bb7
MoeSadoon <msadoon590@gmail.com>
2015-10-18 13:29:40 +0100

    added post model

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -3,9 +3,21 @@ require 'rails_helper'
 feature 'posts' do
   context 'no posts have been added' do
     scenario 'should display a prompt to add a post' do
-    visit '/posts'
-    expect(page).to have_content 'No posts yet'
-    expect(page).to have_link 'Add post'
+      visit '/posts'
+      expect(page).to have_content 'No posts yet'
+      expect(page).to have_link 'Add post'
     end
+  end
+
+  context 'posts have been added' do
+    before do
+      Post.create(description: 'My first instagram post')
+    end
+
+    scenario 'display posts' do
+      visit '/posts'
+      expect(page).to have_content 'My first instagram post'
+      expect(page).not_to have_content 'No posts yet'
+      end
   end
 end
