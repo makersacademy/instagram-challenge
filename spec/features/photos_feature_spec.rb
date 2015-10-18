ee32fe824c5f190487fb9a6e205d9724917a2066
adellanno <anthony@dellanno.co.uk>
2015-10-18 12:22:26 +0100

    user must be logged in to post pictures now

--- spec/features/photos_feature_spec.rb
+++ spec/features/photos_feature_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+include UsersHelper
 
 feature 'photos' do
 
@@ -17,15 +18,24 @@ feature 'photos' do
 
   context 'creating photos' do
 
+    before do
+      user = build :user
+      sign_up user
+    end
+
+    scenario 'user must be logged in to post photos' do
+      click_link 'Sign out'
+      click_link 'Add a photo'
+      expect(page).not_to have_button "Create Photo"
+      expect(current_path).to eq '/users/sign_in'
+    end
+
     scenario 'should take you to a page where you can upload a photo' do
-      visit '/'
       click_link 'Add a photo'
       expect(current_path).to eq '/photos/new'
     end
-    end
 
     scenario 'should let you upload a photo' do
-      visit '/'
       click_link 'Add a photo'
       fill_in 'photo_title', with: 'test title'
       attach_file 'photo_image', "spec/features/files/test.jpg"
@@ -35,7 +45,6 @@ feature 'photos' do
     end
 
     scenario 'should have a description of the photo' do
-      visit '/'
       click_link 'Add a photo'
       fill_in 'photo_title', with: 'test title'
       fill_in 'photo_description', with: 'test description'
@@ -44,5 +53,7 @@ feature 'photos' do
       expect(current_path).to eq '/photos'
       expect(page).to have_content 'test description'
     end
+  end
+
 
 end
