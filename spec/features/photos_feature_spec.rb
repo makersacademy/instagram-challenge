a0dec2dfe0fc42edbc688b906eea58125c4e1b62
philip <philip.clarke@redhood.co.uk>
2015-10-18 09:30:09 +0100

    moved add photo link to nav bar and removed from index footer to ensure add photo tests pass without a capybara ambigious match error

--- spec/features/photos_feature_spec.rb
+++ spec/features/photos_feature_spec.rb
@@ -4,13 +4,13 @@ require 'rails_helper'
 
   context 'photos have been added' do
     before do
-      Photo.create(title: 'Test Photo')
       visit('/')
       click_link('Sign up')
       fill_in('Email', with: 'test@example.com')
       fill_in('Password', with: 'testtest')
       fill_in('Password confirmation', with: 'testtest')
       click_button('Sign up')
+      Photo.create(title: 'Test Photo')
     end
 
     context 'no photos have been added' do
