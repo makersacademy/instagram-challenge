e28aa4e8248793f3f08de3993e8dadb16da81dd3
chweeks <chrisharryweeks@gmail.com>
2015-10-18 20:45:01 +0100

    added link.js file

--- spec/features/likes_feature_spec.rb
+++ spec/features/likes_feature_spec.rb
@@ -28,4 +28,10 @@ feature 'liking pictures' do
     click_link('Like')
     expect(page).to_not have_content('1 like')
   end
+
+  it 'a user can like a picture, which increments the like count', js: true do
+    visit '/pictures'
+    click_link 'Like'
+    expect(page).to have_content("1 like")
+  end
 end
