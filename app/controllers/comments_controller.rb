8086a81d60ccf00dceb9a245cbb5e424d868942b
Dom Batten <dominic.batten@googlemail.com>
2015-10-18 13:46:13 +0100

    users can only delete their own comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,12 +7,21 @@ class CommentsController < ApplicationController
 
   def create
     @photo = Photo.find(params[:photo_id])
-    @photo.comments.create(comment_params)
+    @comment = @photo.comments.new(comment_params)
+    @comment.user_id = current_user.id
+    @comment.save
     redirect_to photos_path
   end
 
   def comment_params
     params.require(:comment).permit(:comment)
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    flash[:notice] = 'Comment deleted successfully'
+    redirect_to '/photos'
   end
 
 end
--- spec/features/comments_feature_spec.rb
+++ spec/features/comments_feature_spec.rb
@@ -11,15 +11,35 @@ feature 'commenting' do
     click_button('Sign up')
   end
 
+  def leave_comment(comment)
+    click_link 'Leave a comment'
+    fill_in 'Comment', with: comment
+    click_button 'Leave Comment'
+  end
+
   before {Photo.create caption: 'sunrise'}
 
-  scenario 'allows users to leave a comment using a form' do
-     sign_up_with('test@example.com')
-     click_link 'Leave a comment'
-     fill_in "Comment", with: "nice photo"
-     click_button 'Leave Comment'
+  before(:each) do
+    sign_up_with('test@example.com')
+  end
 
+  scenario 'allows users to leave a comment using a form' do
+     leave_comment('nice photo')
      expect(current_path).to eq '/photos'
      expect(page).to have_content('nice photo')
+  end
+
+  scenario 'user can delete a comment' do
+    leave_comment('nice photo')
+    click_link 'Delete Comment'
+    expect(page).not_to have_content('nice photo')
+    expect(page).to have_content('Comment deleted successfully')
+  end
+
+  scenario 'user can delete only their own comments' do
+    leave_comment("nice photo")
+    click_link 'Sign out'
+    sign_up_with('new@example.com')
+    expect(page).not_to have_content('Delete Comment')
   end
 end
