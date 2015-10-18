a98fe86aa3df757ffd39778d53a079aab10f2129
Aaron <aarongkendall@gmail.com>
2015-10-18 20:28:39 +0100

    Comment validation, must fill in comment form to leave one

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,6 @@ class CommentsController < ApplicationController
   def create
     @picture = Picture.find(params[:picture_id])
     @picture.comments.create(comment_params)
-    redirect_to picture_path(@picture)
   end
 
   def comment_params
