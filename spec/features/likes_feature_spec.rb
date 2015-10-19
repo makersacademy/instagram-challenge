e9086c61aae0845959806bf3c914a2ae66e5796c
Harry <harryandrew.dix@gmail.com>
2015-10-19 01:44:00 +0100

    more styling updates

---
+++ spec/features/likes_feature_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+feature 'liking user posts' do
+  
+  context 'whilst on the main page' do
+    
+    scenario "registered users can 'like' posts" do
+      
+    end
+
+    scenario "unregisted users cannot 'like' posts" do
+      
+    end
+
+    scenario "user can only 'like' a post once" do
+      
+    end
+
+  end
+
+end
