c58b614e0ba1c8d732c1a1f7e79ec98aac15c0bf
Harry Venables <venablesh@yahoo.co.uk>
2015-10-19 01:12:06 +0100

    Added individual styling for each image

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,11 @@
 class LikesController < ApplicationController
   def create
-    @image = Image.find(params[:image_id])
-    @image.likes.create
-    redirect_to images_path
+    if current_user
+      @image = Image.find(params[:image_id])
+      @image.likes.create
+      redirect_to images_path
+    else
+      redirect_to new_user_session_path
+    end
   end
 end
