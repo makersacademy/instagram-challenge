a698a95d5ed6857484dfc4d0bb76b7351c19e71b
Jongmin Kim <kjm141215@gmail.com>
2015-10-19 06:20:01 +0100

    Some clean up: delete test folder & fix some hound errors

--- app/assets/javascripts/pictures.js
+++ app/assets/javascripts/pictures.js
@@ -4,13 +4,4 @@ $(function() {
     e.preventDefault();
     $(this).parent().children('.edit_form').toggle();
   })
-
-  // $('.post_button').on('click', function(e) {
-  //   e.preventDefault();
-  //   $.ajax({url: $(this).parent().attr('action'), type: 'PUT', data: {picture: {description: 'test'}}, success: function() {
-  //     alert('success');}, error: function() {alert('error')
-  //
-  //   }
-  //   })
-  // })
 })
