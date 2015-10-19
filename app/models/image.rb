5ef881765c65b842a4321a103e43eda9271f8608
Harry Venables <venablesh@yahoo.co.uk>
2015-10-19 21:48:39 +0100

    Added styling and started getting s3 set up

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,9 +1,9 @@
 class Image < ActiveRecord::Base
   has_many :comments, dependent: :destroy
-  has_many :likes
+  has_many :likes, dependent: :destroy
   belongs_to :user
 
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :styles => { :medium => "600x600>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
 end
