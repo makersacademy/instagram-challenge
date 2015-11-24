0bc18535ad543bcd9ea24801ee441851e74bdcb2
Usman Jamil <usman.jamil@live.co.uk>
2015-11-24 11:55:45 +0000

    added coveralls gem

--- Gemfile
+++ Gemfile
@@ -27,6 +27,8 @@ gem 'paperclip'
 gem 'devise'
 gem 'aws-sdk', '< 2.0'
 
+gem 'coveralls', require: false
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,6 +12,9 @@ require 'capybara/poltergeist'
 Capybara.javascript_driver = :poltergeist
 require 'database_cleaner'
 
+require 'coveralls'
+Coveralls.wear!
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
