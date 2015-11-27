b1252ddce55495b0e9a6f482ea02768fde579a95
blancopado <pablo.dominguezblanco@gmail.com>
2015-11-27 20:51:37 +0100

    routing created: resouces pictures

--- instagram-challenge/config/routes.rb
+++ instagram-challenge/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :pictures
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -7,7 +8,7 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
+  get 'pictures' => 'pictures#index'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
