579f1a5702500043dd8f48352daa4fd480302131
thisdotrob <thisdotrob@gmail.com>
2015-11-28 20:12:24 +0000

    Change app name

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 feature 'feed' do
   scenario 'display the welcome message' do
     visit '/'
-    expect(page).to have_content 'Welcome to Filterspam!'
+    expect(page).to have_content('Welcome to Filterspam!')
   end
 
   context 'no filterspams have been posted' do
     scenario 'display prompt to add filterspam' do
       visit '/'
-      expect(page).to have_content 'No filterspams :('
-      expect(page).to have_link 'Add filterspam'
+      expect(page).to have_content('No filterspams :(')
+      expect(page).to have_link('Add filterspam')
     end
   end
 
