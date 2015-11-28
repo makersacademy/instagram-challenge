f77fc1011aabcb222d528e9765b42abfd08a5509
blancopado <pablo.dominguezblanco@gmail.com>
2015-11-28 19:08:12 +0100

    devise and adding links to log in and out

--- instagram-challenge/config/routes.rb
+++ instagram-challenge/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :pictures do
     resources :comments
   end
@@ -10,7 +11,7 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-  get 'pictures' => 'pictures#index'
+  root to: "pictures#index"
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
