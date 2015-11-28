08951bf4072d213907ad029e8e55e5c0139e4304
Ezzy Elliott <ezzy.elliott@gmail.com>
2015-11-28 19:14:34 +0000

    Added picture posts

--- spec/feature/post_feature_spec.rb
+++ spec/feature/post_feature_spec.rb
@@ -8,4 +8,16 @@ feature 'post pictures' do
       expect(page).to have_link 'Add a post picture'
     end
   end
+  context 'picture posts have been added' do
+  before do
+    Post.create(caption: 'big ass cakes')
+  end
+
+    scenario 'display picture posts' do
+      visit '/posts'
+      expect(page).to have_content('big ass cakes')
+      expect(page).not_to have_content('No post pictures yet')
+    end
+  end
+
 end
