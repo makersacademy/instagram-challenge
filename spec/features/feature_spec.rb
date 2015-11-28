dd402fa4077fd3f70a9dc1f955af5fc0d2c576c6
thisdotrob <thisdotrob@gmail.com>
2015-11-28 19:32:02 +0000

    Add link to post new filterspam

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -1,10 +1,17 @@
 require 'rails_helper'
 
 feature 'feed' do
+  scenario 'display the welcome message' do
+    visit '/'
+    expect(page).to have_content 'Welcome to Filterspam!'
+  end
+
   context 'no filterspams have been posted' do
-    scenario 'should display the welcome message' do
+    scenario 'display prompt to add filterspam' do
       visit '/'
       expect(page).to have_content 'No filterspams :('
+      expect(page).to have_link 'Add filterspam'
     end
   end
+
 end
