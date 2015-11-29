8e9c1ef2fe317cc542f91ca2b51923520b6d1f86
Yev Dyko <evgeny.dyko@gmail.com>
2015-11-29 21:09:07 +0300

    Added the index view

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -6,5 +6,10 @@ feature "posts" do
       visit("/posts")
       expect(page).to have_link("Create a post")
     end
+
+    scenario "display a message that there are no posts" do
+      visit("/posts")
+      expect(page).to have_content("There are no posts")
+    end
   end
 end
