72253c2ad107e1ff401eb35d4ee22bb622bdf22d
Chuka Ebi <chuka.ebi@outlook.com>
2015-11-29 15:58:31 +0000

    deletes images after tests are run

--- instagram_clone/spec/spec_helper.rb
+++ instagram_clone/spec/spec_helper.rb
@@ -20,10 +20,8 @@ RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
-  config.after(:suite) do # or :each or :all
-    Dir["#{Rails.root}/public/system/images/000/000/**/*"].each do |file| 
-          File.delete(file) 
-        end
+  config.after(:all) do
+        FileUtils.rm_rf(Dir["#{Rails.root}/public/system/[^.]*"])
   end
 
   config.expect_with :rspec do |expectations|
