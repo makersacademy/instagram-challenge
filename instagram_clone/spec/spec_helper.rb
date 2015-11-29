4c484ebd4b009636974b2efdb7ee292008986e5d
Chuka Ebi <chuka.ebi@outlook.com>
2015-11-29 15:52:40 +0000

    xx

--- instagram_clone/spec/spec_helper.rb
+++ instagram_clone/spec/spec_helper.rb
@@ -20,6 +20,12 @@ RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
+  config.after(:suite) do # or :each or :all
+    Dir["#{Rails.root}/public/system/images/000/000/**/*"].each do |file| 
+          File.delete(file) 
+        end
+  end
+
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
     # and `failure_message` of custom matchers include text for helper methods
