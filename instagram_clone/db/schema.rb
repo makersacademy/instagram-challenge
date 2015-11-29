228af47a3563fa6b763ae97cd214e6dc7085e621
alaanzr <alaanzr@gmail.com>
2015-11-29 18:26:15 +0000

    Comments model and associations created

--- instagram_clone/db/schema.rb
+++ instagram_clone/db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151129152706) do
+ActiveRecord::Schema.define(version: 20151129182552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.integer  "picture_id"
+  end
+
+  add_index "comments", ["picture_id"], name: "index_comments_on_picture_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "pictures", force: :cascade do |t|
     t.string   "tag"
@@ -47,5 +57,7 @@ ActiveRecord::Schema.define(version: 20151129152706) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "comments", "pictures"
+  add_foreign_key "comments", "users"
   add_foreign_key "pictures", "users"
 end
