7db978cbcd285f6ef5632e593a3082641ce66f14
Robert Youngman <rob_youngman@yahoo.com>
2015-11-30 09:27:09 +0000

    uncommented a few commented out lines of code

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,10 +7,9 @@ Rails.application.configure do
     :s3_credentials => {
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
-    # },
-    # :url =>':s3_domain_url',
-    # :path => '/:class/:attachment/:id_partition/:style/:filename',
+    },
+    :url =>':s3_domain_url',
+    :path => '/:class/:attachment/:id_partition/:style/:filename',
   }
 
   # config.paperclip_defaults = {
