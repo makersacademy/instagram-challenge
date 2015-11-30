4c4340c095cf8c7bf7b2bf28c71ed969c0aaf8bb
Tom Barnes <ruralmyth@gmail.com>
2015-11-30 13:02:59 +0000

    fix Hound error

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -8,7 +8,7 @@ class Picture < ActiveRecord::Base
 
   has_many :comments
 
-  has_attached_file :image, styles: { large: "750x", medium: "600x", thumb: "100x" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, styles: { large: "750x", medium: "600x", thumb: "100x" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   validates_attachment_presence :image
   crop_attached_file :image
