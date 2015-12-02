90d79f8b3f65f2beefc57e56170df006206c12e5
Yev Dyko <evgeny.dyko@gmail.com>
2015-12-02 03:00:08 +0300

    Updated model tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-describe Comment, :type => :model do
-  it { is_expected.to belong_to :user }
-  it { is_expected.to belong_to :post }
+describe Comment, type: :model do
+  it { is_expected.to belong_to(:user) }
+  it { is_expected.to belong_to(:post) }
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
 describe Post, type: :model do
-  it { is_expected.to belong_to :user }
-  it { is_expected.to have_many :comments }
+  it { is_expected.to belong_to(:user) }
+  it { is_expected.to have_many(:comments) }
 end
