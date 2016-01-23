1dce650f15d6886c316b465c26f01ad70424cfbc
Sara Tateno <sara.tateno@gmail.com>
2016-01-23 17:44:03 +0000

    Sets a valid range of times

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,6 @@
 class Listing < ActiveRecord::Base
   include ConvertToFormattedTime
   attr_accessor :hour, :mins
-
+  
+  validates :start_time, numericality: { only_integer: true }, inclusion: (1..1440)
 end
--- spec/models/listings_spec.rb
+++ spec/models/listings_spec.rb
@@ -1,11 +1,10 @@
 require "rails_helper"
 
-# describe Listing, type: :model do
-#   context "creating a new listing" do
-#     it "requires a time in mins since midnight" do
-#       listing = Listing.new(day: "Monday", start_time: (60*25))
-#       expect(listing).to have(1).error_on(:start_time)
-#     end
-#
-#   end
-# end
+describe Listing, type: :model do
+  context "creating a new listing" do
+    it "takes a max time in mins since midnight" do
+      listing = Listing.new(day: "Monday", start_time: (60*25))
+      expect(listing).to have(1).error_on(:start_time)
+    end
+  end
+end
