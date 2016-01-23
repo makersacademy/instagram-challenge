2ffe388e47d439f4fa6f41f152a02a037ae75572
Chris Wynne <chris.wynndow@gmail.com>
2016-01-23 14:04:21 +0000

    Adds controller for pictures and view for index

--- spec/features/pictures_features_spec.rb
+++ spec/features/pictures_features_spec.rb
@@ -3,7 +3,7 @@ feature 'pictures' do
     scenario 'should display a prompt to add a picture' do
       visit '/pictures'
       expect(page).to have_content 'No pictures yet'
-      expect(page).to have_link 'Add a restaurant'
+      expect(page).to have_link 'Add a picture'
     end
   end
 end
