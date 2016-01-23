cb403fa083dfb257dd2eb41f328f990fcdb52ad0
Katie Smith <klssmith@users.noreply.github.com>
2016-01-23 13:35:13 +0000

    Adds pictures model, view and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'pictures#index'
+  resources :pictures
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
