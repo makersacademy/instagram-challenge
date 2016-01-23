762950d690be35323e0b179f9c8a09f6c843e7e2
Chris Wynne <chris.wynndow@gmail.com>
2016-01-23 14:34:23 +0000

    Adds picture model

--- spec/features/pictures_features_spec.rb
+++ spec/features/pictures_features_spec.rb
@@ -6,4 +6,17 @@ feature 'pictures' do
       expect(page).to have_link 'Add a picture'
     end
   end
+
+  context 'pictures have been added' do
+
+    before do
+      Picture.create(title: 'My can is awesome')
+    end
+
+    scenario 'display picture titles' do
+      visit '/pictures'
+      expect(page).to have_content('My cat is awesome')
+      expect(page).not_to have_content('No pictures yet')
+    end
+  end
 end
