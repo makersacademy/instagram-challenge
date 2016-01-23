4298e888f5f2bb1eb6297f3bde9f08b3a30f6e6a
hibreez <4ngus.whitehead@gmail.com>
2016-01-23 17:23:25 +0000

    Hound voilations and revert to latest spring version.

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -1,8 +1,10 @@
 include ActionDispatch::TestProcess
 FactoryGirl.define do
   factory :test_photo, class: Photo do
-    image { fixture_file_upload(
-      Rails.root.join('spec', 'images', 'test.png'), 'image/png'
-    )}
+    image do 
+      fixture_file_upload(
+        Rails.root.join('spec', 'images', 'test.png'), 'image/png'
+      )
+    end
   end
-end
+end 
