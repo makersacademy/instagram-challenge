d2ba7823c7ae1628d70f4f89fa6e527c543a563b
vegim qehaja <med.batt.uk@gmail.com>
2016-01-24 15:42:08 +0000

    add basic skeleton for user login and logout with devise

--- pictagram/config/routes.rb
+++ pictagram/config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
 
-root 'posts#index'
+root  to 'posts#index'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   resources :posts do
