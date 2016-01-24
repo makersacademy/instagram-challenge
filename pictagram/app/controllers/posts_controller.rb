f76db5c0d4c54ee8f5c2d26681b8cbc5dff86221
vegim qehaja <med.batt.uk@gmail.com>
2016-01-24 13:13:47 +0000

    adds ability to create posts with form linked to database

--- pictagram/app/controllers/posts_controller.rb
+++ pictagram/app/controllers/posts_controller.rb
@@ -2,4 +2,16 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+  end
+
+  def create
+    Post.create(post_params)
+    redirect_to '/posts'
+  end
+
+  def post_params
+    params.require(:post).permit(:comment)
+  end
 end
--- pictagram/spec/feature/post_feature_spec.rb
+++ pictagram/spec/feature/post_feature_spec.rb
@@ -20,4 +20,15 @@ feature 'posts' do
       expect(page).not_to have_content('No posts yet')
     end
   end
+
+  context 'creating posts' do
+    scenario 'prompts user to fill out a form, then displays the new post' do
+      visit '/posts'
+      click_link 'Add a post'
+      fill_in 'post[comment]', with: 'My Fave Breakfast!'
+      click_button 'Create Post'
+      expect(page).to have_content 'My Fave Breakfast!'
+      expect(current_path).to eq '/posts'
+    end
+  end
 end
