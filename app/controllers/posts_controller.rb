19ea33d70e53e0efbe869934a51eefb78f5ca2e1
Jelgar <jonathanelgardiner@gmail.com>
2016-01-24 16:01:42 +0000

    create new post + show stream implemented and tested

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,26 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+  end
+  def new
+    @post = Post.new
+  end
+  def create
+    @post = Post.create(post_params)
+    if @post.save
+      flash[:success] = "Your post has been created."
+      redirect_to @post
+    else
+      flash[:alert] = "Stop, in the name of love! You need to upload an image"
+      render :new
+    end
+  end
+  def show
+    @post = Post.find(params[:id])
+  end
+
+private
+  def post_params
+    params.require(:post).permit(:caption, :image)
   end
 end
--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -3,9 +3,42 @@ require 'rails_helper'
 feature 'posts' do
   context 'no posts have been added' do
     scenario 'should display a prompt to add a post' do
-      visit '/posts'
+      visit '/'
       expect(page).to have_content 'No posts yet!'
-      expect(page).to have_link 'Add a post'
+      expect(page).to have_link 'New Post'
     end
+  end
+end
+
+feature 'Creating posts' do
+  scenario 'can create a job' do
+    visit '/'
+    click_link 'New Post'
+    attach_file('Image', "spec/files/images/maccabees.jpg")
+    fill_in 'Caption', with: 'great gig last night'
+    click_button 'Create Post'
+    expect(page).to have_content('great gig last night')
+    expect(page).to have_css("img[src*='maccabees.jpg']")
+  end
+  it 'needs an image to create a post' do
+    visit '/'
+    click_link 'New Post'
+    fill_in 'Caption', with: "The pen is mightier than the sword"
+    click_button 'Create Post'
+    expect(page).to have_content('Stop, in the name of love! You need to upload an image')
+  end
+end
+
+feature 'Index displays a list of posts' do
+  before do
+    job_one = create(:post, caption: "This is post one")
+    job_two = create(:post, caption: "This is the second post")
+  end
+  scenario 'the index displays correct created job information' do
+
+    visit '/'
+    expect(page).to have_content("This is post one")
+    expect(page).to have_content("This is the second post")
+    expect(page).to have_css("img[src*='maccabees']")
   end
 end
