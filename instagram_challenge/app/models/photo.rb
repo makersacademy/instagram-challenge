a883268eeefe71aa6af4c93b55bfc4f57b3aa137
Andrew <andy_htun@hotmail.com>
2016-01-24 15:02:45 +0000

    css updated

--- instagram_challenge/app/models/photo.rb
+++ instagram_challenge/app/models/photo.rb
@@ -1,5 +1,5 @@
 class Photo < ActiveRecord::Base
   has_many :comments, dependent: :destroy
-  has_attached_file :image, :styles => { :medium => "500x500>", :thumb => "300x300>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :styles => { :medium => "500x500>", :thumb => "400x400>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
