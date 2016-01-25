77db5d9ef53e38d6d83bab31c3e4f9d031574be6
edwardkerry <edwardkerry@gmail.com>
2016-01-25 07:28:54 +0000

    local database for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,13 +41,13 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_host_name => "s3-eu-west-1.amazonaws.com",
-  :s3_credentials => {
-    :bucket => ENV['S3_BUCKET_NAME'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
-  }
+  # config.paperclip_defaults = {
+  # :storage => :s3,
+  # :s3_host_name => "s3-eu-west-1.amazonaws.com",
+  # :s3_credentials => {
+  #   :bucket => ENV['S3_BUCKET_NAME'],
+  #   :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+  #   :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  #   }
+  # }
 end
