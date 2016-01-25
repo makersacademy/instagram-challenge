d8278b3a6cb5f2b060db4f88f6d5637e57efac49
giamir <giamir.buoncristiani@gmail.com>
2016-01-25 00:11:46 +0000

    fix assets pipeline heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.serve_static_files = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -76,7 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  
+
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
