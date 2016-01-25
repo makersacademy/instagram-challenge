811642b69e0d4e00f02a95c9358533c0f7aa4028
edwardkerry <edwardkerry@gmail.com>
2016-01-25 13:50:14 +0000

    Remove require dotenv for heroku

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,8 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
-require "dotenv"
-Dotenv.load(".env")
+# require "dotenv"
+# Dotenv.load(".env")
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
