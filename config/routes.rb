4ad8fd24bd55adbd81b0aa48ac8e9bf540bcf01e
Thomas Bradley <bradley.tom.r@gmail.com>
2016-01-25 01:43:33 +0000

    Adds user login via facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :posts do
     resources :comments
   end
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160125005624) do
+ActiveRecord::Schema.define(version: 20160125012338) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "text"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 20160125005624) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
