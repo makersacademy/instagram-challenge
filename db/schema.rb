e5114b45d93088b34a227fe3f07bd117782f73a1
Thomas Bradley <bradley.tom.r@gmail.com>
2016-01-25 03:12:56 +0000

    Adds image upload and minor navigation features

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160125015914) do
+ActiveRecord::Schema.define(version: 20160125023205) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "text"
@@ -32,8 +32,12 @@ ActiveRecord::Schema.define(version: 20160125015914) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "caption"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
