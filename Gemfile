1b7a6bd016e32f728bed913188efa8ebe6e78b6f
Iryna Howarth <irynahowarth@gmail.com>
2016-03-04 22:31:13 +0000

    Add Post model with image (using paperclip gem) and caption/feature test fails

--- Gemfile
+++ Gemfile
@@ -26,6 +26,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
+gem "paperclip", "~> 4.3"
+
 # Use Unicorn as the app server
 # gem 'unicorn'
 
---
+++ spec/features/posts_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+feature 'Posts' do
+  context 'creating posts' do
+    scenario 'user fills in a form, then displays the new post' do
+      visit('/')
+      click_link 'New post'
+      attach_file('Image', "spec/files/images/first.jpg")
+      fill_in 'Caption', with: 'Lorem ipsum dolor sit amet.'
+      click_button 'Create Post'
+      expect(page).to have_content('Lorem ipsum dolor sit amet.')
+      expect(page).to have_css("img[src*='first.jpg']")
+    end
+  end
+end
