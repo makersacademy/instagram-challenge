40c026b275fe914b83578a6e5f4cc95c64ca0bc9
Matt <mattpgough@gmail.com>
2016-03-05 11:38:19 +0000

    Created Post model, created post migrations with message as text.

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160305113327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.text     "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -8,4 +8,16 @@ feature 'posts' do
       expect(page).to have_link 'Add a post'
     end
   end
+
+  context 'restaurants have been added' do
+    before do
+      Post.create(message: 'This is my first post!')
+    end
+
+    scenario 'display posts' do
+      visit '/posts'
+      expect(page).to have_content('This is my first post!')
+      expect(page).not_to have_content('No posts yet')
+    end
+  end
 end
