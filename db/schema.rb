55049454394367c8664bae891b0dd423647e711e
Matt <mattpgough@gmail.com>
2016-03-05 13:57:55 +0000

    Added a comments controller and model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160305113327) do
+ActiveRecord::Schema.define(version: 20160305135332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.text     "message"
---
+++ spec/features/comments_feature_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+feature 'reviewing' do
+  before {Post.create message: 'Hello World'}
+
+  scenario 'allows users to leave a review using a form' do
+     visit '/posts'
+     click_link 'Add comment'
+     fill_in 'Message', with: 'I agree'
+     click_button 'Leave Comment'
+     expect(current_path).to eq '/posts'
+     expect(page).to have_content('I agree')
+  end
+
+end
