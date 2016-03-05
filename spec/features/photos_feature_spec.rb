1c8ec0d025be3d66307d424a42d7793b6e6be163
Henry Gordon <hwgordon247@gmail.com>
2016-03-05 14:02:16 +0000

    can display photos description

--- spec/features/photos_feature_spec.rb
+++ spec/features/photos_feature_spec.rb
@@ -1,11 +1,23 @@
 require 'rails_helper'
 
 feature 'photos' do
+
   context 'no photos have been added' do
     scenario 'should display a prompt to add a restaurant' do
       visit '/photos'
       expect(page).to have_content 'No photos uploaded yet'
       expect(page).to have_link 'Add a photo'
+    end
+  end
+
+  context 'add photos' do
+    before do
+      Photo.create(description: 'The Beach')
+    end
+
+    scenario 'should display the photo description' do
+      visit '/photos'
+      expect(page).to have_content 'The Beach'
     end
   end
 end
