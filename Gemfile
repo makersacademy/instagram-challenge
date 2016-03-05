f22ca773e6921a858e7ddcc20673358c07a6b291
Matt <mattpgough@gmail.com>
2016-03-05 15:02:47 +0000

    Added a User model using devise and added sign in, out and up links in the appropirate places.

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'shoulda'
 gem 'paperclip'
+gem 'devise'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5.1'
 # Use postgresql as the database for Active Record
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   get 'posts' => 'posts#index'
   resources :posts do
     resources :comments
   end
+
+  root to: "posts#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
