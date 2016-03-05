1e9fe81d71e3c08f372d8e09ab015e304bb91987
Matt <mattpgough@gmail.com>
2016-03-05 11:55:31 +0000

    Added ability to create a post on the front end, with a new view with a form for filling in and posting a message.

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -9,7 +9,7 @@ feature 'posts' do
     end
   end
 
-  context 'restaurants have been added' do
+  context 'posts have been added' do
     before do
       Post.create(message: 'This is my first post!')
     end
@@ -18,6 +18,17 @@ feature 'posts' do
       visit '/posts'
       expect(page).to have_content('This is my first post!')
       expect(page).not_to have_content('No posts yet')
+    end
+  end
+
+  context 'creating posts' do
+    scenario 'prompts user to fill out a form, then displays the new post' do
+      visit '/posts'
+      click_link 'Add a post'
+      fill_in 'Message', with: 'This is my first real message'
+      click_button 'Create Post'
+      expect(page).to have_content 'This is my first real message'
+      expect(current_path).to eq '/posts'
     end
   end
 end
