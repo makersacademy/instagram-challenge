43dfd76feb368d2049d82d2c1d6e06ce0dc6fd40
Mic <mic.cassano@gmail.com>
2016-03-05 21:52:16 +0000

    Implement post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,19 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+
+  end
+
+  def create
+    Post.create(post_params)
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:description)
+  end
 end
--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 feature 'posts' do
+  scenario 'display a link to post a photo' do
+    visit '/posts'
+    expect(page).to have_link 'Post a photo'
+  end
+
   context 'if no photos have been posted' do
     scenario 'display a message that there are no posts' do
       visit '/posts'
       expect(page).to have_content 'No posts yet...'
-    end
-
-    scenario 'display a link to post a photo' do
-      visit '/posts'
-      expect(page).to have_link 'Post a photo'
     end
   end
 
@@ -22,6 +22,16 @@ feature 'posts' do
       visit '/posts'
       expect(page).to have_content 'Sample post'
       expect(page).not_to have_content 'No posts yet...'
+    end
+  end
+
+  context 'posting a photo' do
+    scenario 'display a form to fill out, then display the post' do
+      visit '/posts'
+      click_link 'Post a photo'
+      fill_in 'Description', with: 'Sample post'
+      click_button 'Post'
+      expect(page).to have_content 'Sample post'
     end
   end
 end
