d07fd887e5396f443e16e66744fed425af3139b0
Mic <mic.cassano@gmail.com>
2016-03-05 21:24:42 +0000

    Generate post model and migration

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -1,15 +1,27 @@
 require 'rails_helper'
 
 feature 'posts' do
-  context 'when no posts have been added' do
+  context 'when no photos have been posted' do
     scenario 'displays a message that there are no posts' do
       visit '/posts'
-      expect(page).to have_content 'Looks like there are no posts yet...'
+      expect(page).to have_content 'No posts yet...'
     end
 
-    scenario 'displays a link to create a post' do
+    scenario 'displays a link to post a photo' do
       visit '/posts'
       expect(page).to have_link 'Post a photo'
+    end
+  end
+
+  context 'when photos have been posted' do
+    before do
+      Post.create(description: 'Sample post', image_file_name: 'sample.jpg')
+    end
+
+    scenario 'displays posts' do
+      visit '/posts'
+      expect(page).to have_content 'Sample post'
+      expect(page).not_to have_content 'No posts yet...'
     end
   end
 end
