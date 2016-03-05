69815dc92ab267b1928e9eb46ed8ba6273a043ab
Matt <mattpgough@gmail.com>
2016-03-05 15:30:17 +0000

    Added ability to sign in with facebook using the omniauth-facebook gem.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
   get 'posts' => 'posts#index'
   resources :posts do
     resources :comments
   end
 
   root to: "posts#index"
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
