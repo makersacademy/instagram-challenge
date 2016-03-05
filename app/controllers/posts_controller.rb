10ee0b2791ad8f1d96d28c9febdbaaf857676dae
seanhawkridge <sean@hawkridge.co>
2016-03-05 09:26:54 +0000

    third green test; adding posts on the front end

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,21 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(restaurant_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render 'new'
+    end
+  end
+
+  def restaurant_params
+    params.require(:post).permit(:description, :image)
+  end
+
 end
--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -11,14 +11,23 @@ feature 'posts' do
   end
 
   context 'posts have been added' do
-
     scenario 'display posts' do
       post = FactoryGirl.create(:post)
       visit '/posts'
       expect(page).to have_content(post.description)
       expect(page).not_to have_content('No posts yet')
     end
-    
-end
+  end
+
+  context 'adding posts' do
+    scenario 'prompts user to fill out a form, then displays the new post' do
+      visit '/posts'
+      click_link 'Add a post'
+      fill_in 'Description', with: 'This is the description'
+      click_button 'Create Post'
+      expect(page).to have_content 'This is the description'
+      expect(current_path).to eq '/posts'
+    end
+  end
 
 end
