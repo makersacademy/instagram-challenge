1d86d6413106871f11bb370ad2658095960587f7
arnoldmanzano <arnold.manzano@gmail.com>
2016-03-05 17:37:22 +0000

    Implemenented facebook sign in integration

--- Gemfile
+++ Gemfile
@@ -1,6 +1,5 @@
 source 'https://rubygems.org'
 
-
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5.1'
 # Use postgresql as the database for Active Record
@@ -32,8 +31,10 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+gem 'dotenv-rails', require: 'dotenv/rails-now'
 gem 'devise'
-gem "haml-rails", "~> 0.9"
+gem 'omniauth-facebook'
+gem 'haml-rails', '~> 0.9'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- spec/features/user_feature_spec.rb
+++ spec/features/user_feature_spec.rb
@@ -1,20 +1,26 @@
 require 'rails_helper'
 
-feature "User can sign in and out" do
-  context "user not signed in and on the homepage" do
-    it "should see a 'sign in' link and a 'sign up' link" do
+feature 'User can sign in and out' do
+  context 'user not signed in and on the homepage' do
+    scenario 'user should see a "Sign in" and a "Sign up" link' do
       visit('/')
       expect(page).to have_link('Sign in')
       expect(page).to have_link('Sign up')
     end
 
-    it "should not see 'sign out' link" do
+    scenario 'user should not see "Sign out" link' do
       visit('/')
       expect(page).not_to have_link('Sign out')
     end
+
+    scenario 'user can "Sign in" using facebook' do
+      visit('/')
+      click_link('Sign in')
+      expect(page).to have_link('Sign in with Facebook')
+    end
   end
 
-  context "user signed in on the homepage" do
+  context 'user signed in on the homepage' do
     before do
       visit('/')
       click_link('Sign up')
@@ -24,12 +30,12 @@ feature "User can sign in and out" do
       click_button('Sign up')
     end
 
-    it "should see 'sign out' link" do
+    scenario 'user should see "Sign out" link' do
       visit('/')
       expect(page).to have_link('Sign out')
     end
 
-    it "should not see a 'sign in' link and a 'sign up' link" do
+    scenario 'user should not see a "Sign in" and a "Sign up" link' do
       visit('/')
       expect(page).not_to have_link('Sign in')
       expect(page).not_to have_link('Sign up')
