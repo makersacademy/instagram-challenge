2c923b62b4a0c22abe4912c5af6fdf7fd6254162
Matt <mattpgough@gmail.com>
2016-03-05 11:25:55 +0000

    Added a Posts controller and index erb.

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -4,7 +4,7 @@ feature 'posts' do
   context 'no posts have been added' do
     scenario 'should display a prompt to add a post' do
       visit '/posts'
-      expect(page).to have_content 'No post yet'
+      expect(page).to have_content 'No posts yet'
       expect(page).to have_link 'Add a post'
     end
   end
