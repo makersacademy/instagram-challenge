2504050d632974deba246290e7dbdaf2325046bb
Iryna Howarth <irynahowarth@gmail.com>
2016-03-05 22:28:30 +0000

    add functionality to view, edit, delete posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,13 @@
 class PostsController < ApplicationController
+
   def index
+    @posts = Post.all
   end
+
   def new
     @post = Post.new
   end
+
   def create
     @post = Post.new(post_params)
     if @post.save
@@ -12,8 +16,29 @@ class PostsController < ApplicationController
       render 'new'
     end
   end
+
   def show
     @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:notice] = 'Post deleted successfully'
+    redirect_to '/'
   end
 
   private
--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 feature 'Posts' do
+
   context 'creating posts' do
+
     scenario 'user fills in a form, then displays the new post' do
       visit('/')
       click_link 'New post'
@@ -11,6 +13,7 @@ feature 'Posts' do
       expect(page).to have_content('Lorem ipsum dolor sit amet.')
       expect(page).to have_css("img[src*='first.jpg']")
     end
+
     scenario 'not saving new post without image' do
       visit('/')
       click_link 'New post'
@@ -19,5 +22,80 @@ feature 'Posts' do
       expect(page).not_to have_content('Lorem ipsum dolor sit amet.')
       expect(page).to have_content 'error'
     end
+
   end
+
+  context 'viewing posts' do
+
+    let!(:post1){Post.create(
+                        image: File.open("#{Rails.root}/spec/files/images/first.jpg"),
+                        caption: 'First post')}
+
+    let!(:post2){Post.create(
+                        image: File.open("#{Rails.root}/spec/files/images/second.jpg"),
+                        caption: 'Second post')}
+
+    scenario 'let a user view posts' do
+      visit '/'
+      expect(page).to have_content 'First post'
+      expect(page).to have_css("img[src*='first.jpg']")
+      expect(page).to have_content 'Second post'
+      expect(page).to have_css("img[src*='second.jpg']")
+    end
+
+    scenario 'let a user view individual post' do
+      visit '/'
+      click_link 'View First post'
+      expect(page).to have_content 'First post'
+      expect(page.current_path).to eq(post_path(post1))
+    end
+
+  end
+
+  context 'editing posts' do
+
+    let!(:post1){Post.create(
+                        image: File.open("#{Rails.root}/spec/files/images/first.jpg"),
+                        caption: 'First post')}
+
+
+    scenario 'let a user edit a post' do
+      visit '/'
+      click_link 'View First post'
+      click_link 'Edit'
+      fill_in 'Caption', with: 'First POST'
+      click_button 'Update Post'
+      expect(page).to have_content 'First POST'
+      expect(current_path).to eq(post_path(post1))
+    end
+
+    scenario 'not saving updates for post without image' do
+      visit('/')
+      click_link 'View First post'
+      click_link 'Edit'
+      fill_in 'Caption', with: 'Something new.'
+      attach_file('Image',"spec/files/images/first.txt")
+      click_button 'Update Post'
+      expect(page).not_to have_content('Something new.')
+      expect(page).to have_content 'error'
+    end
+  end
+
+  context 'deleting posts' do
+    let!(:post1){Post.create(
+                        image: File.open("#{Rails.root}/spec/files/images/first.jpg"),
+                        caption: 'First post')}
+
+    scenario 'delete a post when user press Delete button' do
+      visit '/'
+      click_link 'View First post'
+      click_link 'Edit'
+      click_link 'Delete'
+      expect(current_path).to eq('/')
+      expect(page).to have_content 'Post deleted successfully'
+      expect(page).not_to have_content 'First post'
+    end
+
+  end
+
 end
