f5a2949c4b6ce4dd145b37f553b077fdf522b582
seanhawkridge <sean@hawkridge.co>
2016-03-05 11:05:13 +0000

    adding user model and controller with Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
+  devise_for :users
+  root to: "posts#index"
+
   resources :posts
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
