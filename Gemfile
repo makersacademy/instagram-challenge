95b15c9d034d001de47d229a0880633dbd5deb66
Eilw <eirikwiig@gmail.com>
2016-03-05 12:05:18 +0000

    connected AWS service

--- Gemfile
+++ Gemfile
@@ -26,6 +26,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # gem 'rails_12factor'
 gem 'haml'
 gem 'paperclip'
+gem 'aws-sdk', '< 2.0'
+
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,15 @@ Rails.application.configure do
   # when problems arise.
   config.log_level = :debug
 
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
+
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 
