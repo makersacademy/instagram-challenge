697cf607251e0c3b69c00da97c65771265b118bc
arnoldmanzano <arnold.manzano@gmail.com>
2016-03-05 23:09:21 +0000

    passed feature test for uploading post with image

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,22 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render 'new'
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:image)
   end
 end
---
+++ spec/features/post_feature_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+feature 'Uploading and Viewing posts' do
+  scenario 'no posts yet' do
+    visit '/'
+    expect(page).to have_content 'No posts, upload one!'
+  end
+
+  context 'uploading posts' do
+    scenario 'user can upload posts' do
+      visit '/'
+      click_link 'Post a photo'
+      attach_file 'post[image]',
+                  Rails.root.join('spec', 'fixtures', 'testpattern.jpg')
+      # fill_in 'Caption', with: 'Test image'
+      click_button 'Post'
+      # expect(page).to have_content 'Test image'
+      expect(page).to have_selector("img[src*='testpattern.jpg']")
+      expect(current_path).to eq '/posts'
+    end
+  end
+end
