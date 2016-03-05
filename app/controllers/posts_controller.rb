637a1c091d5ad28ec6b29f121891ef5a6660f0fa
Mic <mic.cassano@gmail.com>
2016-03-05 21:33:04 +0000

    Render post descriptions at /posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
   def index
-
+    @posts = Post.all
   end
 end
--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -1,24 +1,24 @@
 require 'rails_helper'
 
 feature 'posts' do
-  context 'when no photos have been posted' do
-    scenario 'displays a message that there are no posts' do
+  context 'if no photos have been posted' do
+    scenario 'display a message that there are no posts' do
       visit '/posts'
       expect(page).to have_content 'No posts yet...'
     end
 
-    scenario 'displays a link to post a photo' do
+    scenario 'display a link to post a photo' do
       visit '/posts'
       expect(page).to have_link 'Post a photo'
     end
   end
 
-  context 'when photos have been posted' do
+  context 'if photos have been posted' do
     before do
       Post.create(description: 'Sample post', image_file_name: 'sample.jpg')
     end
 
-    scenario 'displays posts' do
+    scenario 'display posts' do
       visit '/posts'
       expect(page).to have_content 'Sample post'
       expect(page).not_to have_content 'No posts yet...'
