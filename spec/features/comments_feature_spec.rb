8f1fd2fd9e9342e7e213c3737d9f56034ebcdaff
Matt <mattpgough@gmail.com>
2016-03-05 15:34:22 +0000

    Refactored code to align properly.

--- spec/features/comments_feature_spec.rb
+++ spec/features/comments_feature_spec.rb
@@ -1,21 +1,21 @@
-require 'rails_helper'
+  require 'rails_helper'
 
-feature 'reviewing' do
-  before {Post.create message: 'Hello World'}
+  feature 'reviewing' do
+    before {Post.create message: 'Hello World'}
 
-  scenario 'allows users to leave a review using a form' do
-     visit '/posts'
-     click_link 'Add comment'
-     fill_in 'Message', with: 'I agree'
-     click_button 'Leave Comment'
-     expect(current_path).to eq '/posts'
-     expect(page).to have_content('I agree')
+    scenario 'allows users to leave a review using a form' do
+       visit '/posts'
+       click_link 'Add comment'
+       fill_in 'Message', with: 'I agree'
+       click_button 'Leave Comment'
+       expect(current_path).to eq '/posts'
+       expect(page).to have_content('I agree')
+    end
+
+    scenario 'deleting a post deletes the comments as well' do
+      visit '/posts'
+      click_link 'Delete post'
+      expect(page).not_to have_content('I agree')
+    end
+
   end
-
-  scenario 'deleting a post deletes the comments as well' do
-    visit '/posts'
-    click_link 'Delete post'
-    expect(page).not_to have_content('I agree')
-  end
-
-end
--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -1,80 +1,80 @@
-require 'rails_helper'
+  require 'rails_helper'
 
-feature 'posts' do
-  context 'no posts have been added' do
-    scenario 'should display a prompt to add a post' do
-      visit '/posts'
-      expect(page).to have_content 'No posts yet'
-      expect(page).to have_link 'Add a post'
-    end
-  end
-
-  context 'posts have been added' do
-    before do
-      Post.create(message: 'This is my first post!')
-    end
-
-    scenario 'display posts' do
-      visit '/posts'
-      expect(page).to have_content('This is my first post!')
-      expect(page).not_to have_content('No posts yet')
-    end
-  end
-
-  context 'creating posts' do
-    scenario 'prompts user to fill out a form, then displays the new post' do
-      visit '/posts'
-      click_link 'Add a post'
-      fill_in 'Message', with: 'This is my first real message'
-      click_button 'Create Post'
-      expect(page).to have_content 'This is my first real message'
-      expect(current_path).to eq '/posts'
-    end
-    scenario 'shows date of post' do
-      visit '/posts'
-      click_link 'Add a post'
-      fill_in 'Message', with: 'This is my first real message'
-      click_button 'Create Post'
-      expect(page).to have_content DateTime.now.strftime('%m/%d/%Y')
-      expect(page).to have_content DateTime.now.strftime('%I:%M%p')
-    end
-  end
-
-      context 'viewing posts' do
-
-      let!(:post1){Post.create(message: 'Hello world!')}
-
-      scenario 'lets a user view a post' do
-       visit '/posts'
-       click_link 'Hello world!'
-       expect(page).to have_content 'Hello world!'
-       expect(page).to have_content "Posted at #{DateTime.now.strftime('%I:%M%p, %m/%d/%Y')}"
-       expect(current_path).to eq "/posts/#{post1.id}"
+  feature 'posts' do
+    context 'no posts have been added' do
+      scenario 'should display a prompt to add a post' do
+        visit '/posts'
+        expect(page).to have_content 'No posts yet'
+        expect(page).to have_link 'Add a post'
       end
     end
 
-    context 'editing posts' do
-      before { Post.create message: 'Hello Universe!' }
+    context 'posts have been added' do
+      before do
+        Post.create(message: 'This is my first post!')
+      end
 
-    scenario 'let a user edit a post' do
-     visit '/posts'
-     click_link 'Edit post'
-     fill_in 'Message', with: 'Goodbye World'
-     click_button 'Update Post'
-     expect(page).to have_content 'Goodbye World'
-     expect(current_path).to eq '/posts'
+      scenario 'display posts' do
+        visit '/posts'
+        expect(page).to have_content('This is my first post!')
+        expect(page).not_to have_content('No posts yet')
+      end
     end
+
+    context 'creating posts' do
+      scenario 'prompts user to fill out a form, then displays the new post' do
+        visit '/posts'
+        click_link 'Add a post'
+        fill_in 'Message', with: 'This is my first real message'
+        click_button 'Create Post'
+        expect(page).to have_content 'This is my first real message'
+        expect(current_path).to eq '/posts'
+      end
+      scenario 'shows date of post' do
+        visit '/posts'
+        click_link 'Add a post'
+        fill_in 'Message', with: 'This is my first real message'
+        click_button 'Create Post'
+        expect(page).to have_content DateTime.now.strftime('%m/%d/%Y')
+        expect(page).to have_content DateTime.now.strftime('%I:%M%p')
+      end
+    end
+
+        context 'viewing posts' do
+
+        let!(:post1){Post.create(message: 'Hello world!')}
+
+        scenario 'lets a user view a post' do
+         visit '/posts'
+         click_link 'Hello world!'
+         expect(page).to have_content 'Hello world!'
+         expect(page).to have_content "Posted at #{DateTime.now.strftime('%I:%M%p, %m/%d/%Y')}"
+         expect(current_path).to eq "/posts/#{post1.id}"
+        end
+      end
+
+      context 'editing posts' do
+        before { Post.create message: 'Hello Universe!' }
+
+      scenario 'let a user edit a post' do
+       visit '/posts'
+       click_link 'Edit post'
+       fill_in 'Message', with: 'Goodbye World'
+       click_button 'Update Post'
+       expect(page).to have_content 'Goodbye World'
+       expect(current_path).to eq '/posts'
+      end
+    end
+
+    context 'deleting posts' do
+
+    before {Post.create message: 'Look at my cat'}
+
+    scenario 'removes a post when a user clicks a delete post' do
+      visit '/posts'
+      click_link 'Delete post'
+      expect(page).not_to have_content 'Look at my cat'
+      expect(page).to have_content 'Post deleted successfully'
+    end
+   end
   end
-
-  context 'deleting posts' do
-
-  before {Post.create message: 'Look at my cat'}
-
-  scenario 'removes a post when a user clicks a delete post' do
-    visit '/posts'
-    click_link 'Delete post'
-    expect(page).not_to have_content 'Look at my cat'
-    expect(page).to have_content 'Post deleted successfully'
-  end
- end
-end
