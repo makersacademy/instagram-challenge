53d67db505f5a4fd365dd470fc2543b5547f6acd
hedudelgado <hedu.delgado@gmail.com>
2016-03-05 22:48:58 +0000

    added pictures name

--- instragram/db/schema.rb
+++ instragram/db/schema.rb
@@ -11,10 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160305205412) do
+ActiveRecord::Schema.define(version: 20160305213415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "picture", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "pictures", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
--- instragram/spec/features/pictures_features_spec.rb
+++ instragram/spec/features/pictures_features_spec.rb
@@ -9,15 +9,25 @@ feature 'pictures' do
 		end
 	end
 
-	# context 'picture has been added' do 
-	# 	before do 
-	# 		Picture.create(name: 'Sunshine')
-	# 	end
+	context 'picture has been added' do 
+		before do 
+			Picture.create(name: 'Sunshine')
+		end
 
-	# 	scenario 'display picture' do 
-	# 		visit '/pictures'
-	# 		expect(page).to have_content('Sunshine')
-	# 		expect(page).not_to have_content('No pictures yet')
-	# 	end
-	# end
+		scenario 'display picture' do 
+			visit '/pictures'
+			expect(page).to have_content('Sunshine')
+			expect(page).not_to have_content('No pictures yet')
+		end
+	end
+	context 'add pictures' do
+  scenario 'prompts user to fill the picture name, then displays it' do
+    visit '/pictures'
+    click_link 'Add a picture'
+    fill_in 'Name', with: 'Sunshine'
+    click_button 'Create Picture'
+    expect(page).to have_content 'Sunshine'
+    expect(current_path).to eq '/pictures'
+  	end
+	end
 end
