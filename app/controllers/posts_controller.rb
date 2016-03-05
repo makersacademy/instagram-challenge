14854852010ad27cac1b6690cd9cc96c573a4b85
Marcus <marcusbullock@hotmail.co.uk>
2016-03-05 17:06:42 +0000

    Photos can now be uploaded

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,29 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, :except => [:index, :show]
 
   def index
-    'Yo'
+    @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    if @post.save
+      redirect_to '/posts'
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:description, :image)
+  end
+
+
 end
