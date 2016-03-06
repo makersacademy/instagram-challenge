3c202e8d55ec4d151605e00b91bed306a8cb61fd
russellvaughan <russellvaughan@gmail.com>
2016-03-06 00:21:09 +0000

    user story 1 complete. as a user so i can show people know what I had for dinner
    I would like to be able to add pictures

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,7 +2,6 @@ class Photo < ActiveRecord::Base
 	has_attached_file :image,
 	:styles => { :medium => "300x300>", 
 		:thumb => "100x100>" }
-
 		validates_attachment_content_type :image, { content_type: ["image/jpeg", 
 			"image/gif", "image/png"] }
 
