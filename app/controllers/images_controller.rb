24f3465774adb319ced495ca37cfc566088360a2
Rufus Raghunath <rufusraghunath@gmail.com>
2016-03-06 11:54:36 +0000

    add show pages for images

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,9 +1,13 @@
 class ImagesController < ApplicationController
 
-  before_action :authenticate_user!, :except => [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @images = Image.all
+  end
+
+  def show
+    @image = Image.find(params[:id])
   end
 
   def new
