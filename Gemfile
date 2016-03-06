149b3b7a6062cc05fe7e3df821f130e796d80d25
Heather <campbell.hf@gmail.com>
2016-03-06 18:08:21 +0000

    Fixed failing sign up tests by adding photo upload requirements to photo model.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-gem 'paperclip'
+gem 'paperclip', "~> 4.3"
 
 gem 'devise'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,4 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-
 end
