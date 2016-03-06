a90360c1996034c5648205477bcb9691974570ff
Heather <campbell.hf@gmail.com>
2016-03-06 23:06:55 +0000

    Comments now appear on page.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,5 @@
 class Photo < ActiveRecord::Base
+
   has_many :comments, dependent: :destroy
 
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
