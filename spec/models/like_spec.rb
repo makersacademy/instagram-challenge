6cfcf2f741a9f234d8fe32a3647c81dc611870f0
seanhawkridge <sean@hawkridge.co>
2016-03-06 20:42:19 +0000

    minor style fixes

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { is_expected.to belong_to :post }
+  it { is_expected.to belong_to :user }
+
 end
