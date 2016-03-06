86a1d4acc8a544216500db73960184b59993e5b3
Yan-Yi Li <liyanyi@gmail.com>
2016-03-06 20:05:54 +0000

    added session helper for sign_up and post_picture

--- spec/features/pictures_feature_spec.rb
+++ spec/features/pictures_feature_spec.rb
@@ -1,32 +1,6 @@
 require 'rails_helper'
 
 feature 'pictures' do
-  def sign_up
-    visit '/'
-    click_link 'Sign up'
-    fill_in 'Email', with: 'test@example.com'
-    fill_in 'Password', with: 'testtest'
-    fill_in 'Password confirmation', with: 'testtest'
-    click_button 'Sign up'
-  end
-
-  def sign_up_another_user
-    visit '/'
-    click_link 'Sign up'
-    fill_in 'Email', with: 'anotheruser@example.com'
-    fill_in 'Password', with: 'testtest'
-    fill_in 'Password confirmation', with: 'testtest'
-    click_button 'Sign up'
-  end
-
-  def post_picture
-    visit '/pictures'
-    click_link 'Post a picture'
-    attach_file 'picture[image]', Rails.root.join('spec','images','img.jpg')
-    fill_in 'Caption', with: 'a nice picture'
-    click_button 'Post picture'
-  end
-
   context 'no pictures have been added' do
     scenario 'should display a prompt to add a picture' do
       sign_up
