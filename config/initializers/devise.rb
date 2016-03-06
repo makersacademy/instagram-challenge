67388ea16eeb78fa0daf9611b3cd5ee367c2237a
Godfrey Chiu <ggwc82@gmail.com>
2016-03-06 23:49:16 +0000

    WIP Implemented devise and omniauth logins

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,6 +1,8 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+
+  config.omniauth :facebook, Rails.application.secrets.facebook_app_id, Rails.application.secrets.facebook_app_secret
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :posts
 
   root to: 'posts#index'
