bf38570e432236ad4c1390b62e9c12c7541b0076
Alex <avlonitis@msn.com>
2016-03-06 21:49:57 +0000

    added s3 support

--- Gemfile
+++ Gemfile
@@ -38,7 +38,7 @@ gem 'haml-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'dotenv'
+  gem 'dotenv-rails'
   gem 'byebug'
 end
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,10 +2,13 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.paperclip_defaults = {
     storage: :s3,
+    url: ':s3_domain_url',
+    path: '/:class/:attachment/:id_partition/:style/:filename',
     s3_credentials: {
       bucket: ENV['S3_BUCKET_NAME'],
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      endpoint: 'http://s3-eu-west-1.amazonaws.com'
     }
   }
 
