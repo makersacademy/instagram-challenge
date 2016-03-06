dc8c65c6be258d924c4ed296ee8bd13e9929fa28
russellvaughan <russellvaughan@gmail.com>
2016-03-06 15:34:57 +0000

    wip adding comment feature

--- spec/comments_feature_spec.rb
+++ spec/comments_feature_spec.rb
@@ -2,6 +2,10 @@ require 'Rails_helper'
 
 feature 'A user can comment on an image' do
   scenario 'comment on an image' do
-    
+    sign_up_and_in("Russell", "Vaughan", "russellvaughan", "russell@example.com")
+    create_photo
+    fill_in('Comment', with: "you have a cute cat" )
+    click_link("Add Comment")
+    expec
   end
 end
