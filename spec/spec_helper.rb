f6e85b7bbc44d653f3c56ef8f379b1b6af6701ef
Jeremy Barrass <jeremybarrass@Jeremys-MacBook-Pro.local>
2016-03-07 00:49:32 +0000

    FileUtils config set up in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,10 @@ require 'spec_web_helpers.rb'
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  config.after(:suite) do # or :each or :all
+   FileUtils.rm_rf(Dir["#{Rails.root}/public/system/photos/images"])
+   FileUtils.rm_rf(Dir["#{Rails.root}/public/system/users/profile_pics"])
+ end
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
