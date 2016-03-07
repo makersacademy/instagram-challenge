974f6e1ec82ad26a8d2974469847d56c098d9369
Yan-Yi Li <liyanyi@gmail.com>
2016-03-07 06:30:59 +0000

    setup bootstrap and added navbar

--- Gemfile
+++ Gemfile
@@ -38,6 +38,8 @@ gem 'paperclip'
 # Use Devise for authentication
 gem 'devise'
 
+gem 'bootstrap-sass'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,5 @@
 class Picture < ActiveRecord::Base
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :styles => {:medium => "640x640>"}, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   belongs_to :user
