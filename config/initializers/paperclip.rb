124b982823fcdd41f8d8e4653f3163e5d88217c1
emmabeynon <emma.beynon@gmail.com>
2016-03-07 21:36:23 +0000

    Added paperclip initializer

---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,3 @@
+Paperclip::Attachment.default_options[:use_timestamp] = false
+Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
