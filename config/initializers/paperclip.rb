2df83acfd909eb1b87ee67d0ae103aadea39c3a8
emmabeynon <emma.beynon@gmail.com>
2016-03-07 21:56:02 +0000

    Changed AWS S3 bucket location to US

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,3 @@
-Paperclip::Attachment.default_options[:use_timestamp] = false
-Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+# Paperclip::Attachment.default_options[:use_timestamp] = false
+# Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+# Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
