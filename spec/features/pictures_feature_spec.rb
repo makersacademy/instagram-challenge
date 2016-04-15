de51fbb8997a24238cd58a65d7faa129ee5e3983
Gabrielle <gabivk@gmail.com>
2016-04-16 00:05:09 +0100

    added picture model

--- spec/features/pictures_feature_spec.rb
+++ spec/features/pictures_feature_spec.rb
@@ -6,4 +6,16 @@ feature 'pictures' do
     expect(page).to have_content 'No pictures yet!'
     expect(page).to have_link 'Add pictures'
   end
+
+  context 'pictures have been added' do
+    before do
+      Picture.create(description: '#sun #spring')
+    end
+    
+    scenario 'displays pictures' do
+      visit '/pictures'
+      expect(page).to have_content '#sun #spring'
+      expect(page).not_to have_content 'No pictures yet!'
+    end
+  end
 end
