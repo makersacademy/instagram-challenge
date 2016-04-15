ff5a2033845042fe36dcdb5f8c8092e9868a0f3a
Misa Ogura <misa.ogura01@gmail.com>
2016-04-15 12:51:35 +0100

    Require cpaybara/rails in rails_helper to enable Capybara in end user acceptance tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+require 'capybara/rails'
 
 ActiveRecord::Migration.maintain_test_schema!
 
