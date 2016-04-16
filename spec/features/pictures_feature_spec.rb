0e0b575a2d24e95c1540f56a8dd82749989ca52c
Gabrielle <gabivk@gmail.com>
2016-04-16 14:26:23 +0100

    tests for image display passing

--- spec/features/pictures_feature_spec.rb
+++ spec/features/pictures_feature_spec.rb
@@ -11,13 +11,10 @@ feature 'pictures' do
     scenario 'adds new picture, displays description' do
       visit '/pictures'
       click_link 'Add picture'
-      # p current_path
-      # click_button 'Choose File'
-      # attach_file 'image' , 'app/assets/images/DOGE.jpeg'
-      # pix = Picture.create(:image 'app/assets/images/DOGE.jpeg', description:'#sun #spring')
+      attach_file 'picture[image]', 'app/assets/images/DOGE.jpeg'
       fill_in 'Description', with: '#sun #spring'
       click_button 'Add'
-      # expect(page).to have_css("img[src*='Doge']")
+      expect(page).to have_css("img[src*='DOGE']")
       expect(page).to have_content '#sun #spring'
       expect(page).not_to have_content 'No pictures yet!'
     end
