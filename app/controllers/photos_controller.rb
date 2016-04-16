24ca62cf588d54b30a9f40b5363ef115a3fbe58c
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2016-04-16 15:32:43 +0100

    pushing to remove .env from git

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -12,6 +12,6 @@ class PhotosController < ApplicationController
   end
 
   def photo_params
-    params.require(:photo).permit(:caption)
+    params.require(:photo).permit(:caption, :image)
   end
 end
