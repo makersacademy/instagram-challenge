209cb9720821676e588a3fb6708603be252afaa2
Misa Ogura <misa.ogura01@gmail.com>
2016-04-16 13:29:29 +0100

    User can post a picture with a title using link & form on the page

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -4,9 +4,31 @@ class PicturesController < ApplicationController
     all_pictures
   end
 
+  def new
+    new_picture
+  end
+
+  def create
+    create_picture
+    redirect_to pictures_path
+  end
+
   private
+
+  def picture_params
+    params.require(:picture).permit(:title)
+  end
 
   def all_pictures
     @pictures = Picture.all
+  end
+
+  def new_picture
+    @picture = Picture.new
+  end
+
+  def create_picture
+    @picture = Picture.new(picture_params)
+    @picture.save
   end
 end
--- spec/features/pictures_feature_spec.rb
+++ spec/features/pictures_feature_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 feature 'Picture:' do
-  context 'No pictures added' do
+  context 'When no pictures added' do
     scenario 'prompts users to post a picture' do
       visit pictures_path
       expect(page).to have_content 'No picture has been added'
@@ -9,11 +9,22 @@ feature 'Picture:' do
     end
   end
 
-  context 'A picture has been added' do
+  context 'When a picture has been added' do
     let!(:picture) { Picture.create(title: 'My Picture') }
 
     scenario 'displays a title of a posted picture' do
       visit pictures_path
+      expect(page).to have_link 'My Picture'
+    end
+  end
+
+  context "When a user visits a pictures page" do
+    scenario 'user can post a picture with title' do
+      visit pictures_path
+      click_link 'Post a picture'
+      fill_in 'Title', with: 'My Picture'
+      click_button 'Post'
+      expect(current_path).to eq pictures_path
       expect(page).to have_link 'My Picture'
     end
   end
