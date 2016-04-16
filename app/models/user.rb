cc1d11113ebce7605a5e59a1d78ae4c3c74be986
Caspar Fischer-Zernin <casparfz@gmail.com>
2016-04-16 17:21:31 +0100

    edited devise sign up form to include username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  #has_many :images
+  validates :name, :username, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
