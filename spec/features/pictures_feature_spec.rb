13a42dba2a5d1b7d47055e2ccbbc85660c6f7b83
Gabrielle <gabivk@gmail.com>
2016-04-16 14:36:24 +0100

    Can add gifs!

--- spec/features/pictures_feature_spec.rb
+++ spec/features/pictures_feature_spec.rb
@@ -12,10 +12,10 @@ feature 'pictures' do
       visit '/pictures'
       click_link 'Add picture'
       attach_file 'picture[image]', 'app/assets/images/DOGE.jpeg'
-      fill_in 'Description', with: '#sun #spring'
+      fill_in 'Description', with: 'much doge'
       click_button 'Add'
       expect(page).to have_css("img[src*='DOGE']")
-      expect(page).to have_content '#sun #spring'
+      expect(page).to have_content 'much doge'
       expect(page).not_to have_content 'No pictures yet!'
     end
   end
