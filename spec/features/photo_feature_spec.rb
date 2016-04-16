dae27eb1154f2b66312f8400ead313dec8bfbc4d
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2016-04-16 17:27:13 +0100

    uploading photos works

--- spec/features/photo_feature_spec.rb
+++ spec/features/photo_feature_spec.rb
@@ -10,24 +10,22 @@ feature 'photos' do
   end
 
   context 'have been added' do
-    before do 
-      Photo.create(caption: 'caption1') 
-    end
+    before do
+    visit '/photos'
+    click_link 'Add a photo'
+    fill_in 'photo[caption]', with: 'caption1'
+    attach_file 'photo[image]', 'spec/assets/images/test.jpg'
+    click_button 'Create Photo'
+  end
 
-    scenario 'should display photos captions' do
+    scenario 'should display photos and captions' do
       visit '/photos'
       expect(page).to have_content('caption1')
       expect(page).not_to have_content('No photos yet')
-    end
-
-    scenario 'can add a caption for a photo' do
-      visit '/photos'
-      click_link 'Add a photo'
-      fill_in 'photo[caption]', with: 'caption2'
-      click_button 'Create Photo'
-      expect(page).to have_content('caption2') 
+      expect(page).to have_css("img[src*='test']")
     end
   end
 end
+
 
 
