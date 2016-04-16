e2d69b8879001ee40e5ea13d8e8d737135f79f56
Misa Ogura <misa.ogura01@gmail.com>
2016-04-16 13:12:57 +0100

    User can see a link with the title of a posted picture

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,12 @@
 class PicturesController < ApplicationController
 
   def index
+    all_pictures
+  end
+
+  private
+
+  def all_pictures
+    @pictures = Picture.all
   end
 end
--- spec/features/pictures_feature_spec.rb
+++ spec/features/pictures_feature_spec.rb
@@ -4,16 +4,17 @@ feature 'Picture:' do
   context 'No pictures added' do
     scenario 'prompts users to post a picture' do
       visit pictures_path
-      expect(page).to have_content 'Start creating your kaleidoboard'
+      expect(page).to have_content 'No picture has been added'
       expect(page).to have_link 'Post a picture'
     end
   end
 
-  # context 'A picture has been added' do
-  #   let(:picture) { Picture.create
-  #
-  #   scenario 'displays a posted picture' do
-  #     visit pictures_path
-  #   end
-  # end
+  context 'A picture has been added' do
+    let!(:picture) { Picture.create(title: 'My Picture') }
+
+    scenario 'displays a title of a posted picture' do
+      visit pictures_path
+      expect(page).to have_link 'My Picture'
+    end
+  end
 end
