e347ed3a38c1ed76833ec95c84a75d137cef6e63
Junyuan Xue <junyuanxue.is@gmail.com>
2016-04-16 12:01:17 +0100

    aws bucket working

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,12 @@
 Rails.application.configure do
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
