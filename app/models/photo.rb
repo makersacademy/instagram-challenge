a9924a17781847774261e17227fc7e03524aee9a
Junyuan Xue <junyuanxue.is@gmail.com>
2016-04-16 20:57:40 +0100

    set up devise

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,8 +2,8 @@ class Photo < ActiveRecord::Base
   has_many :comments, dependent: :destroy
 
   has_attached_file :image,
-                    :styles => { :large => "500x500>",
-                                 :medium => "400x400>" },
-                    :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+                    styles: { large: "500x500>",
+                              medium: "400x400>" },
+                    default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
