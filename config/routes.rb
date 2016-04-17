7dc732ab56d5f1adbe5b6156a497b2642bfc4848
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2016-04-17 13:30:11 +0100

    users can like pictures

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  resources :photos
+  resources :photos do
+    resources :likes
+  end
 
 
   root to: 'photos#index'
--- spec/features/likes_feature_spec.rb
+++ spec/features/likes_feature_spec.rb
@@ -11,6 +11,11 @@ feature 'likes' do
     scenario 'should display prompt to like photo' do
       expect(page).to have_link('Like photo')
     end
+
+    scenario 'should display number of likes' do
+      click_link 'Like photo'
+      expect(page).to have_content('1 like')
+    end
   end
 end
 
