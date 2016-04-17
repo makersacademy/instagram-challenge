bd8f8b9a2e5e0d84744399266fe2e5a368ea50ee
Misa Ogura <misa.ogura01@gmail.com>
2016-04-17 11:11:54 +0100

    Hide label for picture title and add placeholder

--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -1,7 +1,7 @@
 def post_picture_with_image
   visit pictures_path
   click_link 'Post a picture'
-  fill_in 'Title', with: 'Bird'
+  fill_in 'picture_title', with: 'Bird'
   attach_file 'picture[image]', 'spec/assets/images/bird.png'
   click_button 'Post'
 end
@@ -14,7 +14,7 @@ end
 def edit_title
   visit picture_path(pic)
   click_link 'Edit'
-  fill_in 'Title', with: 'Better title'
+  fill_in 'picture_title', with: 'Better title'
   click_button 'Update'
 end
 
