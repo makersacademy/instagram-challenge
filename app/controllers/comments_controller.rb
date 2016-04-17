00c6b15a02f0d934124b0de4793fdc55e0d0b462
rhiannonruth <rhiannonruth@hotmail.co.uk>
2016-04-17 16:59:10 +0100

    Can delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,25 @@
 class CommentsController < ApplicationController
 
+  before_action :find_post
+
   def create
-    @post = Post.find(params[:post_id])
     @post.comments.create(comment_params)
-    redirect_to posts_path
+    redirect_to request.referer
+  end
+
+  def destroy
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    flash[:notice] = 'Comment deleted successfully'
+    redirect_to request.referer
   end
 
   private
     def comment_params
       params.require(:comment).permit(:content)
+    end
+
+    def find_post
+      @post = Post.find(params[:post_id])
     end
 end
--- spec/features/comments_feature_spec.rb
+++ spec/features/comments_feature_spec.rb
@@ -3,21 +3,49 @@ require 'rails_helper'
 feature 'comments' do
 
   let!(:post) { create(:post) }
-  let(:comment) { build(:comment) }
+  let(:comment1) { build(:comment) }
+  let(:comment2) { build(:comment) }
 
-  scenario 'allows users to leave comment on post from index page' do
-     visit '/posts'
-     fill_in "comment[content]", with: comment.content
-     click_button 'Leave comment'
-     expect(page).to have_content comment.content
+  context 'creating and reading comments' do
+    scenario 'allows users to leave comment on post from index page' do
+       visit '/posts'
+       fill_in "comment[content]", with: comment2.content
+       click_button 'Leave comment'
+       expect(page).to have_content comment2.content
+       expect(current_path).to eq "/posts"
+    end
+
+    scenario 'allows users to leave comment on post from post page' do
+       visit '/posts'
+       click_link "post#{post.id}"
+       fill_in "comment[content]", with: comment2.content
+       click_button 'Leave comment'
+       expect(page).to have_content comment2.content
+       expect(current_path).to eq "/posts/#{post.id}"
+    end
   end
 
-  scenario 'allows users to leave comment on post', focus: true do
-     visit '/posts'
-     click_link "post#{post.id}"
-     fill_in "comment[content]", with: comment.content
-     click_button 'Leave comment'
-     expect(page).to have_content comment.content
-  end
+  context 'deleting comments' do
 
+    before do
+      comment1.post_id=post.id
+      comment1.save
+    end
+
+    scenario 'deleting comment from index page' do
+      visit '/posts'
+      click_link 'Delete comment'
+      expect(page).not_to have_content comment1.content
+      expect(page).to have_content 'Comment deleted successfully'
+      expect(current_path).to eq "/posts"
+    end
+    scenario 'deleting comment from post page', focus: true do
+      visit '/posts'
+      click_link "post#{post.id}"
+      click_link 'Delete comment'
+      expect(page).not_to have_content comment1.content
+      expect(page).to have_content 'Comment deleted successfully'
+      expect(current_path).to eq "/posts/#{post.id}"
+    end
+  end
 end
