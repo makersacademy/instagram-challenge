f69bc7e34d3f7595bcdd8b51f364c8437ab43282
Misa Ogura <misa.ogura01@gmail.com>
2016-04-17 11:43:05 +0100

    Install devise, create user model using devise. Drop/create/migrate database as there was conflict with migration from previous attampt to implement user on a separate branch

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   resources :pictures, shallow: true do
     resources :comments
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160417094809) do
+ActiveRecord::Schema.define(version: 20160417104014) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "boards", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "comments", force: :cascade do |t|
     t.text     "thoughts"
