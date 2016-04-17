991407f6bef75e01749190f4d21f17b82316fceb
rhiannonruth <rhiannonruth@hotmail.co.uk>
2016-04-17 18:33:41 +0100

    Sign in/out links in layout

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,9 +1,18 @@
 FactoryGirl.define do
+
   factory :post do |f|
     f.sequence(:caption) { |n| "cheeky monkey#{n}" }
     f.image Rack::Test::UploadedFile.new("#{Rails.root}/spec/assets/images/monkey.jpg", "image/jpg")
   end
+
   factory :comment do |f|
     f.sequence(:content) { |n| "Great photo#{n}" }
   end
+
+  factory :user do |u|
+    u.sequence(:email) { |n| "user#{n}@gmail.com" }
+    u.password 'abcd1234'
+    u.password_confirmation 'abcd1234'
+  end
+
 end
