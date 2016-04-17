367701fbc1c465782f7a887601074e6584fd5676
Misa Ogura <misa.ogura01@gmail.com>
2016-04-17 23:20:14 +0100

    Like/unlike feature implemented

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,16 +3,28 @@ class LikesController < ApplicationController
   before_action :authenticate_user!, :except => [:index, :show]
 
   def new
+    like_current_picture
+  end
+
+  def destroy
+    unlike_current_picture
+  end
+
+  private
+
+  def like_current_picture
     @picture = Picture.find(params[:picture_id])
     unless @picture.has_been_liked_by?(current_user)
-      @like = @picture.likes.create(userid: current_user.id)
+      @picture.likes.create(userid: current_user.id)
     else
       flash[:notice] = 'You cannot like the same picture twice'
     end
     redirect_to picture_path(@picture)
   end
 
-  def destroy
-    p params
+  def unlike_current_picture
+    @picture = Picture.find(params[:id])
+    @picture.likes.each {|like| like.destroy if like.userid == current_user.id}
+    redirect_to picture_path(@picture)
   end
 end
