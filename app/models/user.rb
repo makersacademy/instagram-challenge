a9494ef912fc2a823a0064457d57d1e24f90bd27
Misa Ogura <misa.ogura01@gmail.com>
2016-04-17 12:25:41 +0100

    Add username column to User model. Enable sign_up and sign_in with username. Update relevant views

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  attr_accessor :login
+
+  validates :username,
+  :presence => true,
+  :uniqueness => {
+    :case_sensitive => false
+  }
+
+  validate :validate_username
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
 end
