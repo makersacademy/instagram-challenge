c9449d7fe6cb59ae297f472e7ac5555ce6bdec09
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2016-04-17 11:16:30 +0100

    all tests passing, skinnier controller, no flash messages

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,6 @@
 class PhotosController < ApplicationController
   before_action :authenticate_user!, :except => [:index, :show]
-
+  
   def index
     @photos = Photo.all
   end
@@ -9,18 +9,12 @@ class PhotosController < ApplicationController
   end
 
   def create
-    build_photo(photo_params, current_user.id)
+    Photo.create_photo_with_user(photo_params, current_user.id)
     redirect_to photos_path
   end
 
   def destroy
-    @photo = Photo.find(params[:id])
-    unless current_user.has_created?(@photo)
-      flash[:notice] = 'You cannot delete this photo'
-    else
-      @photo.destroy
-      flash[:notice] = 'Photo deleted successfully'
-    end
+    Photo.find_and_delete_photo(params[:id], current_user)
     redirect_to photos_path
   end
 
@@ -29,4 +23,13 @@ class PhotosController < ApplicationController
   end
 
   private
+  def flash_notice_message(photo)
+    if photo.authorized_to_delete(current_user)
+      'Photo has been deleted'
+    else
+      'You cannot delete this photo'
+    end
+  end
+    
 end
+
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,10 +4,22 @@ class Photo < ActiveRecord::Base
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
-  def build_photo(photo_params, current_user_id)
+  def self.create_photo_with_user(photo_params, current_user_id)
     photo = Photo.create(photo_params)
     photo.user_id = current_user_id
     photo.save
   end
 
+  def self.find_and_delete_photo(photo_id, current_user)
+    photo = Photo.find(photo_id)
+    photo.delete_if_authorized(current_user)
+  end
+
+  def delete_if_authorized(current_user)  
+    destroy if authorized_to_delete(current_user)
+  end
+
+  def authorized_to_delete(current_user)
+    user_id == current_user.id
+  end
 end
