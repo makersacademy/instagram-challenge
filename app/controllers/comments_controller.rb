082cb9ae66350b90c8f7c7771c84ad97476e4e21
Misa Ogura <misa.ogura01@gmail.com>
2016-04-17 11:34:41 +0100

    Refactor comment controller with private methods

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,34 @@
 class CommentsController < ApplicationController
 
   def new
-    @picture = Picture.find(params[:picture_id])
-    @comment = Comment.new
+    current_picture
+    new_comment
   end
 
   def create
-    @picture = Picture.find(params[:picture_id])
-    @picture.comments.create(comment_params)
-    redirect_to picture_path(params[:picture_id])
+    comment_on_current_picture
+    redirect_to current_picture_path
   end
+
+  private
 
   def comment_params
     params.require(:comment).permit(:thoughts)
   end
 
+  def current_picture
+    @picture = Picture.find(params[:picture_id])
+  end
+
+  def new_comment
+    @comment = Comment.new
+  end
+
+  def comment_on_current_picture
+    current_picture.comments.create(comment_params)
+  end
+
+  def current_picture_path
+    picture_path(params[:picture_id])
+  end
 end
