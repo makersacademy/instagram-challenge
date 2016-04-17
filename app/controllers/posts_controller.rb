10b60e2d6fa283ebd931e48f19e51afea1965cc8
rhiannonruth <rhiannonruth@hotmail.co.uk>
2016-04-17 13:14:33 +0100

    CRUD methods complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,12 +8,36 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
-  def post_params
-    params.require(:post).permit(:caption, :image)
-  end
-
   def create
     Post.create(post_params)
     redirect_to '/posts'
   end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to '/posts'
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:notice] = 'Post deleted successfully'
+    redirect_to '/posts'
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:caption, :image)
+    end
+
 end
--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -14,21 +14,58 @@ feature 'posts' do
     let!(:post1) { create(:post) }
     let(:post2) { build(:post) }
 
-    scenario 'display restaurants' do
+    scenario 'display posts' do
       visit '/posts'
       expect(page).to have_content post1.caption
       expect(page).to have_xpath("//img[contains(@src,'monkey.jpg')]")
     end
 
-    scenario 'prompts user to fill out a form, then displays post' do
-      visit '/posts'
-      click_link 'Post an image'
-      attach_file 'post[image]', "#{Rails.root}/spec/images/monkey.jpg"
-      fill_in 'Caption', with: post2.caption
-      click_button 'Post image'
-      expect(page).to have_content post2.caption
-      expect(current_path).to eq '/posts'
-      expect(page).to have_xpath("//img[contains(@src,'monkey.jpg')]")
+    context 'creating posts' do
+      scenario 'user can post image with caption' do
+        visit '/posts'
+        click_link 'Post an image'
+        attach_file 'post[image]', "#{Rails.root}/spec/images/monkey.jpg"
+        fill_in 'Caption', with: post2.caption
+        click_button 'Post image'
+        expect(page).to have_content post2.caption
+        expect(current_path).to eq '/posts'
+        expect(page).to have_xpath("//img[contains(@src,'monkey.jpg')]")
+      end
+    end
+
+    context 'showing posts' do
+      scenario 'user can view post' do
+       visit '/posts'
+       click_link "post#{post1.id}"
+       expect(page).to have_xpath("//img[contains(@src,'monkey.jpg')]")
+       expect(page).to have_content post1.caption
+       expect(current_path).to eq "/posts/#{post1.id}"
+       expect(page).not_to have_link 'Post an image'
+      end
+    end
+
+    context 'updating posts' do
+      scenario 'user can edit caption' do
+       visit '/posts'
+       click_link "post#{post1.id}"
+       click_link 'Edit caption'
+       fill_in 'Caption', with: post2.caption
+       click_button 'Update caption'
+       expect(page).to have_content post2.caption
+       expect(current_path).to eq '/posts'
+      end
+    end
+
+    context 'deleting restaurants', focus: true do
+      scenario 'removes a restaurant when a user clicks a delete link' do
+        visit '/posts'
+        click_link "post#{post1.id}"
+        click_link 'Delete post'
+        expect(page).not_to have_xpath("//img[contains(@src,'monkey.jpg')]")
+        expect(page).not_to have_content post1.caption
+        expect(current_path).to eq '/posts'
+        expect(page).to have_content 'Post deleted successfully'
+      end
     end
   end
 end
