87f5247f96ceb08ab59a156f2095d0091cd7af0a
Robota <lorenzo.turrino@gmail.com>
2016-04-17 11:35:00 +0100

    manually switch db to psql

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   root to: 'images#index'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
