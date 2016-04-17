897c522fa048e4bc6c68280c58932a45aeb99df9
rhiannonruth <rhiannonruth@hotmail.co.uk>
2016-04-17 16:17:20 +0100

    Can leave comments on posts page

--- spec/features/comments_feature_spec.rb
+++ spec/features/comments_feature_spec.rb
@@ -5,8 +5,16 @@ feature 'comments' do
   let!(:post) { create(:post) }
   let(:comment) { build(:comment) }
 
+  scenario 'allows users to leave comment on post from index page' do
+     visit '/posts'
+     fill_in "comment[content]", with: comment.content
+     click_button 'Leave comment'
+     expect(page).to have_content comment.content
+  end
+
   scenario 'allows users to leave comment on post', focus: true do
      visit '/posts'
+     click_link "post#{post.id}"
      fill_in "comment[content]", with: comment.content
      click_button 'Leave comment'
      expect(page).to have_content comment.content
