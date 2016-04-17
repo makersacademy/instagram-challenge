2679343942d09a311b9e2015464be441e1cff571
Sachin Karia <sachin.karia01@gmail.com>
2016-04-17 17:02:37 +0100

    new posts view and updated index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,22 @@
 class PostsController < ApplicationController
+
   def index
-  @posts = Post.all
-end
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    Post.create(restaurant_params)
+    redirect_to '/posts'
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:post).permit(:description)
+  end
+
 end
--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -9,14 +9,25 @@ feature 'posts' do
   end
 
   context 'restaurants have been added' do
-  before do
-    Post.create(description: "First post")
+    before do
+      Post.create(description: "First post")
+    end
+
+    scenario 'display posts' do
+      visit '/posts'
+      expect(page).to have_content('First post')
+      expect(page).not_to have_content('No posts yet')
+    end
   end
 
-  scenario 'display posts' do
-    visit '/posts'
-    expect(page).to have_content('First post')
-    expect(page).not_to have_content('No posts yet')
+  context 'creating restaurants' do
+    scenario 'prompts user to fill out a form, then displays the new post' do
+      visit '/posts'
+      click_link 'New post'
+      fill_in 'Description', with: 'First post'
+      click_button 'Create Post'
+      expect(page).to have_content 'First post'
+      expect(current_path).to eq '/posts'
+    end
   end
-end
 end
