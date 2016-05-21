1d742c65309952ccf4a5526d00b9aea9ac848780
Lexi Tran <hazukitran@gmail.com>
2016-05-22 00:55:06 +0700

    Add assocation between users and photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,6 @@
 class PhotosController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
     @photos = Photo.all
   end
@@ -8,7 +10,7 @@ class PhotosController < ApplicationController
   end
 
   def create
-    Photo.create(photo_params)
+    Photo.user_create_photo(photo_params, current_user.id)
     redirect_to '/photos'
   end
 
