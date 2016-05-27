cf14ed7a9cfdbba516ca4fe1bab47d285edf2eeb
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-05-27 20:30:33 +0100

    WIP temp

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'omniauth-facebook'
 # gem 'paperclip'
 gem 'aws-sdk'
 gem 'paperclip', :git=> 'https://github.com/thoughtbot/paperclip', :ref => '523bd46c768226893f23889079a7aa9c73b57d68'
+gem 'simple_form'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -8,4 +8,27 @@ feature 'posts' do
       expect(page).to have_link 'Add a post'
     end
   end
+
+  context 'posts have been added' do
+    before do
+      Post.create(caption: 'perrito')
+    end
+
+    scenario 'display posts' do
+      visit '/posts'
+      expect(page).to have_content('perrito')
+      expect(page).not_to have_content('No posts yet')
+    end
+  end
+
+  context 'creating posts' do
+  scenario 'prompts user to fill out a form, then displays the new post' do
+    visit '/posts'
+    click_link 'Add a post'
+    fill_in 'Caption', with: 'perrito'
+    click_button 'Create Post'
+    expect(page).to have_content 'perrito'
+    expect(current_path).to eq '/posts'
+  end
+end
 end
