092be0767f2b0527829a98f94fbb9482c011acf7
Nick <nm.richards@yahoo.com>
2016-05-27 09:02:31 -0700

    Add images to Jackagram

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  validates :image, presence:true
+  has_attached_file :image, styls: { :medium => "640x"}
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
