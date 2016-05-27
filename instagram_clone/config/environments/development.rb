3853c9a19eef2679ced9399039f6e12a5fc8174c
ollieh-m <ollie.hm@cantab.net>
2016-05-27 18:50:39 +0100

    Link to new S3 bucket

--- instagram_clone/config/environments/development.rb
+++ instagram_clone/config/environments/development.rb
@@ -38,4 +38,16 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_region => ENV['S3_REGION'],
+    :s3_host_name => "s3-#{ENV['S3_REGION']}.amazonaws.com",
+    :s3_credentials => {
+      :bucket => ENV['S3_INSTAGRAM_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
+
 end
