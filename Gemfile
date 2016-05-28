5573d36f42f49f51723bd50baf3978bf71bc88be
Amy Nicholson <missamynicholson@gmail.com>
2016-05-28 16:07:37 +0100

    Styling

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '4.2.6'
 gem 'pg', '~> 0.15'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
+gem 'bootstrap-sass', '~> 3.3.6'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,7 +13,7 @@ class PostsController < ApplicationController
       flash[:success] = "Woohoo! Here's your new post!"
       redirect_to post_path(@post)
     else
-      flash.now[:error] = "Uh oh! You need to select an image!"
+      flash.now[:alert] = "Uh oh! You need to select an image!"
       render "new"
     end
   end
