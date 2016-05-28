3ec6d30e0dc4f19e52aae7108a501808b1ec5de2
Max Provin <maxprovin@outlook.com>
2016-05-28 19:23:32 +0100

    Post model created with title and caption fields

--- picture_face/spec/features/posts_feature_spec.rb
+++ picture_face/spec/features/posts_feature_spec.rb
@@ -1,10 +1,24 @@
 feature 'Posts' do
 
-  context 'no posts have been added' do
+  context 'have not been added' do
     scenario 'should display a prompt to make a post' do
       visit '/posts'
       expect(page).to have_content 'No posts yet'
       expect(page).to have_link 'Make a Post'
     end
   end
+
+  context 'have been added' do
+    before do
+      Post.create(title: 'Picture', caption: 'a picture')
+    end
+
+    scenario 'display posts' do
+      visit '/posts'
+      expect(page).to have_content('Picture')
+      expect(page).to have_content('a picture')
+      expect(page).not_to have_content('No posts yet')
+    end
+  end
+
 end
