6b6cab35afd99a4481d9ce556f652309720f55ca
sitypop <sityemilyshah@gmail.com>
2016-05-28 13:56:52 +0100

    User can add an image and description

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,19 @@
 class PicturesController < ApplicationController
   def index
-  end  
+    @pictures = Picture.all
+  end
+
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    Picture.create(picture_params)
+    redirect_to '/pictures'
+  end
+
+  def picture_params
+    params.require(:picture).permit(:description, :image)
+  end
+
 end
--- spec/features/pictures_feature_spec.rb
+++ spec/features/pictures_feature_spec.rb
@@ -2,13 +2,42 @@ require 'rails_helper'
 
 feature 'Pictures' do
 
-  describe 'no pictures have been added' do
+  context 'no pictures have been added' do
     it 'should display a prompt to add a picture' do
       visit '/pictures'
       expect(page).to have_content 'No pictures here 😳'
       expect(page).to have_link 'Add a picture'
     end
   end
+
+  context 'pictures have been added' do
+     before do
+       Picture.create(description: '#sunday #foodie')
+     end
+
+     it 'displays pictures' do
+       visit '/pictures'
+       expect(page).to have_content '#sunday #foodie'
+       expect(page).not_to have_content 'No pictures here 😳'
+     end
+   end
+
+
+
+  describe 'adding pictures' do
+    it 'prompts user to upload a picture, then displays the new picture' do
+    visit '/pictures'
+    click_link 'Add a picture'
+    attach_file :picture_image, "./spec/images/succulents.jpg"
+    fill_in 'Description', with: '#sweetsucculents'
+    click_button 'Post'
+    expect(page).to have_content '#sweetsucculents'
+    expect(page).to have_css "img[src*=succulents]"
+    expect(current_path).to eq '/pictures'
+    end
+  end
+
+
 
 
 end
