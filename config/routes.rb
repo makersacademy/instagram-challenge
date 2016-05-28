8a87f35ec230e3eb1baad1ea538358ac87947482
letianw <tmac4ever0824@gmail.com>
2016-05-28 16:12:31 +1000

    models and routes for posts set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  root 'posts#index'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-
+  resources :posts
   # Example resource route with options:
   #   resources :products do
   #     member do
