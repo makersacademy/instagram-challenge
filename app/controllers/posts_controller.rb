fba87f19e1b5fecb89e58fb5d21e0171fb5ce12a
letianw <tmac4ever0824@gmail.com>
2016-05-28 21:22:09 +1000

    individual posts and editing implemented

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,7 +13,22 @@ class PostsController < ApplicationController
     redirect_to posts_path
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to(post_path(@post))
+  end
+
   def post_params
     params.require(:post).permit(:image, :caption)
   end
+
 end
