af7b4a27cdc0170c2a05cad89f9d3e601896551e
sitypop <sityemilyshah@gmail.com>
2016-05-28 14:51:34 +0100

    User can edit a picture and description

--- spec/features/pictures_feature_spec.rb
+++ spec/features/pictures_feature_spec.rb
@@ -4,7 +4,7 @@ feature 'Pictures' do
 
   describe 'no pictures have been added' do
     it 'should display a prompt to add a picture' do
-      visit '/pictures'
+      visit pictures_path
       expect(page).to have_content 'No pictures here 😳'
       expect(page).to have_link 'Add a picture'
     end
@@ -12,14 +12,40 @@ feature 'Pictures' do
 
   describe 'adding pictures' do
     it 'prompts user to upload a picture, then displays the new picture' do
-    visit '/pictures'
+    visit pictures_path
     click_link 'Add a picture'
     attach_file :picture_image, "./spec/images/succulents.jpg"
     fill_in 'Description', with: '#sweetsucculents'
     click_button 'Post'
     expect(page).to have_content '#sweetsucculents'
     expect(page).to have_css "img[src*=succulents]"
-    expect(current_path).to eq '/pictures'
+    expect(current_path).to eq pictures_path
+    end
+  end
+
+  # context 'viewing pictures' do
+  #
+  #   let!(:succulent){ Picture.create(description:'#sweetsucculents', image_file_name: "./spec/images/succulents.jpg") }
+  #
+  #   scenario 'lets a user view a picture' do
+  #    visit '/pictures'
+  #    find(".picture").click
+  #    expect(current_path).to eq "/pictures/#{succulent.id}"
+  #   end
+  #
+  # end
+
+  describe 'editing pictures' do
+
+  let!(:succulent){ Picture.create(description:'#sweetsucculents', image_file_name: "./spec/images/succulents.jpg") }
+
+    scenario 'let a user edit a post' do
+     visit pictures_path
+     click_link 'Edit'
+     fill_in 'Description', with: '#sexysucculents'
+     click_button 'Update'
+     expect(page).to have_content '#sexysucculents'
+     expect(current_path).to eq pictures_path
     end
   end
 
