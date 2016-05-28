fcd56fd8acaf1b7d9001d2f7a09531ab64577efa
sitypop <sityemilyshah@gmail.com>
2016-05-28 12:19:30 +0100

    No pictures added test

---
+++ spec/features/pictures_feature_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+feature 'Pictures' do
+
+  describe 'no pictures have been added' do
+    it 'should display a prompt to add a picture' do
+      visit '/'
+      expect(page).to have_content 'No pictures here 😳'
+      expect(page).to have_link 'Add a picture'
+    end
+  end
+
+
+end
