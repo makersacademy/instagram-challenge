fae2f4dbe026badbceec38e14d36a878c717f968
letianw <tmac4ever0824@gmail.com>
2016-05-28 16:07:56 +1000

    Revert "model and controller created"
    This reverts commit b782054faec0adc41c8ecf4d2d0f647e1bee0079.

--- db/migrate/20160528060310_create_images.rb
+++
@@ -1,9 +0,0 @@
-class CreateImages < ActiveRecord::Migration
-  def change
-    create_table :images do |t|
-      t.string :caption
-
-      t.timestamps null: false
-    end
-  end
-end
--- spec/features/images_feature_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-feature 'images' do
-
-end
