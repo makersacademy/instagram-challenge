578e806aefdebb836ceced09719080a787b4e80c
Elia Bardashevich <ebardashevich@gmail.com>
2016-05-28 18:24:41 +0100

    user sigh-up/in/out

--- instagram-challenge/Gemfile
+++ instagram-challenge/Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-
+gem 'devise'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.6'
 # Use postgresql as the database for Active Record
--- instagram-challenge/config/routes.rb
+++ instagram-challenge/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   resources :pictures
+
+  root "pictures#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
