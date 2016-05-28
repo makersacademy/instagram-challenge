fdec021a179ee3a02faf4fed1903025116e6ed56
sitypop <sityemilyshah@gmail.com>
2016-05-28 14:57:10 +0100

    User must upload an image to post

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,5 @@
 class Picture < ActiveRecord::Base
   has_attached_file :image, :styles => { :medium => "500x500>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  validates :image, :presence => true
 end
