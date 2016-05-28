7be3b5a891f70b2160f247c8d3ebccfb878f9549
Amy Nicholson <missamynicholson@gmail.com>
2016-05-28 13:43:08 +0100

    Test - cannot create a post with no image

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,13 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.create(post_params)
-    redirect_to post_path(@post)
+    if @post.save
+      flash[:success] = "Woohoo! Here's your new post!"
+      redirect_to post_path(@post)
+    else
+      flash.now[:error] = "Uh oh! You need to select an image!"
+      render "new"
+    end
   end
 
   def show
--- spec/features/creating_post_spec.rb
+++ spec/features/creating_post_spec.rb
@@ -1,11 +1,22 @@
 feature "creating post" do
-  scenario "can create a post" do
-    visit "/"
-    click_link "New"
-    fill_in "Caption", with: "hipster coffee"
-    attach_file("Image", "spec/files/images/hipster.jpg")
-    click_button "Submit"
-    expect(page).to have_content "hipster coffee"
-    expect(page).to have_css("img[src*='hipster.jpg']")
+  context "when image is selected" do
+    scenario "can create a post" do
+      visit "/"
+      click_link "New"
+      fill_in "Caption", with: "hipster coffee"
+      attach_file("Image", "spec/files/images/hipster.jpg")
+      click_button "Submit"
+      expect(page).to have_content "hipster coffee"
+      expect(page).to have_css("img[src*='hipster.jpg']")
+    end
+  end
+  context "when image is not selected" do
+    scenario "can not create a post" do
+      visit "/"
+      click_link "New"
+      fill_in "Caption", with: "hipster coffee"
+      click_button "Submit"
+      expect(page).to have_content "Uh oh! You need to select an image!"
+    end
   end
 end
