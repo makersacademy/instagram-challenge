2c83704e86f38f3c735ce97ccb73d0d094ee3c45
letianw <tmac4ever0824@gmail.com>
2016-05-28 21:38:08 +1000

    added preview of the pic in edit mode

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,6 +27,12 @@ class PostsController < ApplicationController
     redirect_to(post_path(@post))
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
   def post_params
     params.require(:post).permit(:image, :caption)
   end
