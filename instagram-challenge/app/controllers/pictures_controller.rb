01709835f834c5f21cb8ef2634a7b6c10d32e0d0
Elia Bardashevich <ebardashevich@gmail.com>
2016-05-28 20:55:46 +0100

    adds picture deplay

--- instagram-challenge/app/controllers/pictures_controller.rb
+++ instagram-challenge/app/controllers/pictures_controller.rb
@@ -1,5 +1,25 @@
 class PicturesController < ApplicationController
   def index
+    @pictures = Picture.all
+  end
+  def new
+    @picture = Picture.new
+  end
+  def create
+    @picture = Picture.create(picture_params)
+    # @picture.user = current_user
+    if @picture.save
+      redirect_to pictures_path
+    else
+      render 'new'
+    end
+  end
+  def show
+    @picture = Picture.find(params[:id])
   end
 
+  private
+  def picture_params
+    params.require(:picture).permit(:image, :description)
+  end
 end
--- instagram-challenge/spec/features/pictures_feature_spec.rb
+++ instagram-challenge/spec/features/pictures_feature_spec.rb
@@ -7,5 +7,18 @@ feature 'pictures' do
       expect(page).to have_content 'No pictures yet'
       expect(page).to have_link 'Add a picture'
     end
+
+  context 'adding a picture' do
+    scenario 'prompt user to add a description for the picture' do
+      visit '/pictures'
+      click_link 'Add a picture'
+      attach_file 'Image', 'app/assets/images/anime1.jpeg'
+      fill_in 'Description', with: 'New picture'
+      click_button 'Create Picture'
+      expect(page).to have_content 'New picture'
+      expect(current_path).to eq '/pictures'
+      expect(page).not_to have_content 'No pictures yet'
+    end
   end
+end
 end
