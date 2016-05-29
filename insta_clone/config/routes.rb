d28d3f9e68743ff5c612132e1fb7764503f70cb5
Robin Heathcote <robin.heathcote@icloud.com>
2016-05-29 15:30:15 +0100

    Adds users and Facebook login

--- insta_clone/config/routes.rb
+++ insta_clone/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'posts#index'
 
+  devise_for :users,
+      :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
   resources :posts do
     resources :comments
   end
