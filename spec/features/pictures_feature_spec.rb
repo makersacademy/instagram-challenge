c71c115039b6b6c4d24cb8b559156dee7922cf7c
sitypop <sityemilyshah@gmail.com>
2016-05-29 10:47:07 +0100

    User must log in to add, delete, edit pictures

--- spec/features/pictures_feature_spec.rb
+++ spec/features/pictures_feature_spec.rb
@@ -10,27 +10,66 @@ feature 'Pictures' do
     end
   end
 
-  describe 'adding pictures' do
-    it 'prompts user to upload a picture, then displays the new picture' do
-    visit pictures_path
-    click_link 'Add a picture'
-    attach_file :picture_image, "./spec/images/succulents.jpg"
-    fill_in 'Description', with: '#sweetsucculents'
-    click_button 'Post'
-    expect(page).to have_content '#sweetsucculents'
-    expect(page).to have_css "img[src*=succulents]"
-    expect(current_path).to eq pictures_path
+  context 'User is signed in' do
+
+    before do
+      visit pictures_path
+      click_link 'Sign up'
+      fill_in('Email', with: 'test@example.com')
+      fill_in('Password', with: 'testtest')
+      fill_in('Password confirmation', with: 'testtest')
+      click_button 'Sign up'
     end
 
-    it 'does not let you post without a picture' do
+    describe 'adding pictures' do
+      it 'prompts user to upload a picture, then displays the new picture' do
       visit pictures_path
       click_link 'Add a picture'
+      attach_file :picture_image, "./spec/images/succulents.jpg"
       fill_in 'Description', with: '#sweetsucculents'
       click_button 'Post'
-      expect(page).to have_content 'error'
+      expect(page).to have_content '#sweetsucculents'
+      expect(page).to have_css "img[src*=succulents]"
+      expect(current_path).to eq pictures_path
+      end
+
+      it 'does not let you post without a picture' do
+        visit pictures_path
+        click_link 'Add a picture'
+        fill_in 'Description', with: '#sweetsucculents'
+        click_button 'Post'
+        expect(page).to have_content 'error'
+      end
+    end
+
+    describe 'editing pictures' do
+
+    let!(:succulent){ Picture.create(description:'#sweetsucculents', image_file_name: "./spec/images/succulents.jpg") }
+
+      scenario 'let a user edit a post' do
+       visit pictures_path
+       click_link 'Edit'
+       fill_in 'Description', with: '#sexysucculents'
+       click_button 'Update'
+       expect(page).to have_content '#sexysucculents'
+       expect(current_path).to eq pictures_path
+      end
+    end
+
+    describe 'deleting pictures' do
+
+    let!(:succulent){ Picture.create(description:'#sweetsucculents', image_file_name: "./spec/images/succulents.jpg") }
+
+      it 'deletes a picture and it\'s description' do
+        visit pictures_path
+        click_link 'Delete'
+        expect(page).not_to have_content '#sweetsucculents'
+        expect(page).to have_content 'Picture deleted 😔'
+      end
     end
 
   end
+
 
   # context 'viewing pictures' do
   #
@@ -43,34 +82,6 @@ feature 'Pictures' do
   #   end
   #
   # end
-
-  describe 'editing pictures' do
-
-  let!(:succulent){ Picture.create(description:'#sweetsucculents', image_file_name: "./spec/images/succulents.jpg") }
-
-    scenario 'let a user edit a post' do
-     visit pictures_path
-     click_link 'Edit'
-     fill_in 'Description', with: '#sexysucculents'
-     click_button 'Update'
-     expect(page).to have_content '#sexysucculents'
-     expect(current_path).to eq pictures_path
-    end
-  end
-
-  describe 'deleting restaurants' do
-
-  let!(:succulent){ Picture.create(description:'#sweetsucculents', image_file_name: "./spec/images/succulents.jpg") }
-
-    it 'deletes an image and it\'s description' do
-      visit pictures_path
-      click_link 'Delete'
-      expect(page).not_to have_content '#sweetsucculents'
-      expect(page).to have_content 'Picture deleted 😔'
-    end
-  end
-
-
 
 
 end
