7333751a4ce4aaf6384e94d189b66171dac19670
Max Provin <maxprovin@outlook.com>
2016-05-29 15:50:30 +0100

    Image upload implemented but not tested

--- picture_face/app/models/post.rb
+++ picture_face/app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ActiveRecord::Base
+
+  # validates :image, presence: true
+
+  has_attached_file :image, styles: { :medium => "640x", :thumb => "200x200" }
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
 end
--- picture_face/db/schema.rb
+++ picture_face/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160528181119) do
+ActiveRecord::Schema.define(version: 20160529144247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,8 +19,12 @@ ActiveRecord::Schema.define(version: 20160528181119) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.string   "caption"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
