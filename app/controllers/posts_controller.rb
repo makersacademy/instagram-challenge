aee5e40110048b157e3b985d4da83be6aba97adb
Ben Stein <bumstain@gmail.com>
2016-05-29 19:04:42 +0100

    Refactored post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
   
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
@@ -20,15 +22,12 @@ class PostsController < ApplicationController
   end
 
   def show 
-    @post = Post.find(params[:id])
   end
 
   def edit
-    @post = Post.find(params[:id]) 
   end
 
   def update
-    @post = Post.find(params[:id])
     if @post.save 
       @post.update(post_params)
       redirect_to(post_path(@post))
@@ -40,7 +39,6 @@ class PostsController < ApplicationController
   end
 
   def destroy  
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path
     flash[:success] = "Post deleted!"
@@ -50,6 +48,10 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:caption, :image)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 
 end
