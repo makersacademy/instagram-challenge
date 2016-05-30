d5f681a47bc74fe812de0f41bc1787578d942c51
RichardWatkins1 <richardwatkins1@gmail.com>
2016-05-30 23:43:06 +0100

    devise views added and converted to haml

--- photogram/app/controllers/posts_controller.rb
+++ photogram/app/controllers/posts_controller.rb
@@ -1,49 +1,59 @@
 class PostsController < ApplicationController
 
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
-   @posts = Post.all
- end
-
- def new
-  @post = Post.new
-end
-
-def create
-  @post = Post.create(post_params)
-  if @post.save
-    flash[:success] = "Your post has been created."
-    redirect_to @post
-  else
-    flash[:alert] = "Halt, you fiend! You need an image to post here!"
-    render :new
+    @posts = Post.all
   end
-end
 
-def show
-  @post = Post.find(params[:id])
-end
+  def new
+    @post = Post.new
+  end
 
-def edit
-   @post = Post.find(params[:id])
-end
+  def create
+    @post = Post.create(post_params)
+    if @post.save
+      flash[:success] = "Your post has been created."
+      redirect_to @post
+    else
+      flash[:alert] = "Halt, you fiend! You need an image to post here!"
+      render :new
+    end
+  end
 
-def update
-  @post = Post.find(params[:id])
-  @post.update(post_params)
-  redirect_to(post_path(@post))
-end
+  def show
+    @post = Post.find(params[:id])
+  end
 
-def destroy
-  @post = Post.find(params[:id])
-  @post.destroy
-  redirect_to posts_path
-end
+  def edit
+    @post = Post.find(params[:id])
+  end
 
-private
+  def update
+    if @post.update(post_params)
+      flash[:success] = "Post updated."
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "Update failed.  Please check the form."
+      render :edit
+    end
+  end
 
-def post_params
-  params.require(:post).permit(:caption, :image)
-end
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:caption, :image)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
 
 end
