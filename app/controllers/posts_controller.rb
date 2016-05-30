d7989f4a1779a1c51b09bc4173101ff9243b4a3e
rahulrama <rahulrama@gmail.com>
2016-05-30 19:26:09 +0100

    Posts tidied up

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index]
   before_action :owned_post, only: [:edit, :update, :destroy]
 
   def index
