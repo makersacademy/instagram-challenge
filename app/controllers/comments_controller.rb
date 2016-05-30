c8d05d371297a6305521a5cd867768fe05898fb3
chriselevate <coates.c@hotmail.com>
2016-05-30 21:55:30 +0100

    added ajax for deletion and adding of comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,21 +7,28 @@ class CommentsController < ApplicationController
     @comment.user_id = current_user.id
 
     if @comment.save
-      flash[:success] = "Comment successful"
-      redirect_to :back
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js
+      end
     else
       flash[:alert] = "Comment not successful"
       render root_path
     end
   end
 
-  def destroy
+def destroy
     @comment = @post.comments.find(params[:id])
 
-    @comment.destroy
-    flash[:success] = "Comment deleted"
-    redirect_to root_path
+    if @comment.user_id == current_user.id
+      @comment.delete
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js
+      end
+    end
   end
+
 
   private
 
