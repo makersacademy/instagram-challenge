4aeca87565694290f97dfe24b1d98602b31f00a0
Robin Heathcote <robin.heathcote@icloud.com>
2016-05-30 17:42:11 +0100

    Solves some hound issues

--- insta_clone/db/schema.rb
+++ insta_clone/db/schema.rb
@@ -1,19 +1,8 @@
-# encoding: UTF-8
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended that you check this file into your version control system.
+
 
 ActiveRecord::Schema.define(version: 20160529183551) do
 
-  # These are extensions that must be enabled in order to support this database
+
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
@@ -23,7 +12,8 @@ ActiveRecord::Schema.define(version: 20160529183551) do
     t.integer  "post_id"
   end
 
-  add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
+  add_index "comments", ["post_id"],
+    name: "index_comments_on_post_id", using: :btree
 
   create_table "posts", force: :cascade do |t|
     t.string   "caption"
@@ -56,9 +46,13 @@ ActiveRecord::Schema.define(version: 20160529183551) do
     t.string   "user_name"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  add_index "users", ["user_name"], name: "index_users_on_user_name", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true,
+   using: :btree
+  add_index "users", ["reset_password_token"],
+    name: "index_users_on_reset_password_token",
+    unique: true, using: :btree
+  add_index "users", ["user_name"], name: "index_users_on_user_name",
+  unique: true, using: :btree
 
   add_foreign_key "comments", "posts"
   add_foreign_key "posts", "users"
