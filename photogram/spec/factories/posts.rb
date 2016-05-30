98be723d68d0c19bfb9aec5b1d58cbe7f26f7ad2
RichardWatkins1 <richardwatkins1@gmail.com>
2016-05-30 22:23:23 +0100

    CSS added

--- photogram/spec/factories/posts.rb
+++ photogram/spec/factories/posts.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :post do
-    caption "MyString"
+    caption "nofilter"
+    image Rack::Test::UploadedFile.new(Rails.root + 'spec/files/images/coffee.jpg', 'image/jpg')
   end
 end
