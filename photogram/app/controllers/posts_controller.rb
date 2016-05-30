261ac43b3decd6c3915427d5d65419ede3bb24b4
RichardWatkins1 <richardwatkins1@gmail.com>
2016-05-30 22:41:46 +0100

    edit working flash messages not working so 2 tests failing

--- photogram/app/controllers/posts_controller.rb
+++ photogram/app/controllers/posts_controller.rb
@@ -1,33 +1,43 @@
 class PostsController < ApplicationController
-#module ApplicationHelper
+
   def index
-     @posts = Post.all
-  end
+   @posts = Post.all
+ end
 
-  def new
-    @post = Post.new
-  end
+ def new
+  @post = Post.new
+end
 
-  def create
-    @post = Post.create(post_params)
-    if @post.save
-      flash[:success] = "Your post has been created."
-      redirect_to @post
-    else
-      flash[:alert] = "Halt, you fiend! You need an image to post here!"
-      render :new
-    end
+def create
+  @post = Post.create(post_params)
+  if @post.save
+    flash[:success] = "Your post has been created."
+    redirect_to @post
+  else
+    flash[:alert] = "Halt, you fiend! You need an image to post here!"
+    render :new
   end
+end
 
-  def show
-    @post = Post.find(params[:id])
-  end
+def show
+  @post = Post.find(params[:id])
+end
 
-  private
+def edit
+   @post = Post.find(params[:id])
+end
 
-  def post_params
-    params.require(:post).permit(:caption, :image)
-  end
+def update
+  @post = Post.find(params[:id])
+  @post.update(post_params)
+  redirect_to(post_path(@post))
+end
+
+private
+
+def post_params
+  params.require(:post).permit(:caption, :image)
+end
 
 
 end
