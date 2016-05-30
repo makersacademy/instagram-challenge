723968153beeba54fea6b0210218c4906f48e650
Lucy Clarke <l.l.c.clarke@gmail.com>
2016-05-30 12:01:56 +0100

    starting crud

--- instagram_clone/app/controllers/posts_controller.rb
+++ instagram_clone/app/controllers/posts_controller.rb
@@ -1,2 +1,11 @@
 class PostsController < ApplicationController
+
+def index
+  @posts = Post.all
+end
+
+def new
+
+end
+
 end
--- instagram_clone/spec/features/posts_feature_spec.rb
+++ instagram_clone/spec/features/posts_feature_spec.rb
@@ -8,4 +8,28 @@ feature 'posts' do
       expect(page).to have_link 'Add a photo'
     end
   end
+
+  context 'photo has been uploaded' do
+    before do
+      Post.create(title: 'Vacation')
+    end
+
+    scenario 'display photos' do
+      visit '/posts'
+      expect(page).to have_content 'Vacation'
+      expect(page).not_to have_content 'No photos yet'
+    end
+  end
+
+  context 'uploading a photo' do
+    scenario 'prompts a user to fill out a for and then displays the photo' do
+      visit '/posts'
+      click_link 'Add a photo'
+      fill_in 'Title', with: 'Vacation'
+      click_button 'Upload photo'
+      expect(page).to have_content 'Vacation'
+      expect(current_path).to eq '/posts'
+    end
+  end
+
 end
