09ee97a49c2a60f1a653db98d1e1511cdbb249ee
RichardWatkins1 <richardwatkins1@gmail.com>
2016-05-30 21:08:45 +0100

    first tests passing

---
+++ photogram/app/models/post.rb
@@ -0,0 +1,7 @@
+class Post < ActiveRecord::Base
+
+  has_attached_file :image, styles: { :medium => "640x" }
+
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+end
---
+++ photogram/spec/factories/posts.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :post do
+    caption "MyString"
+  end
+end
