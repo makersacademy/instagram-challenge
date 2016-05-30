660ab737c095eed4bdb93ab6967cb7aa4a404a33
chriselevate <coates.c@hotmail.com>
2016-05-30 22:02:42 +0100

    added maximum post lengths and time since stamp

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
   validates :image, presence: true
+  validates :caption, length: { in: 3..300 }
   validates :user_id, presence: true
   has_attached_file :image, styles: { :medium => "640x" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
