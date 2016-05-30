ef4da2b0744124e57b2cab6e127ddd1cc6e15edc
chriselevate <coates.c@hotmail.com>
2016-05-30 20:22:04 +0100

    can delete a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update]
-
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -28,7 +27,6 @@ class PostsController < ApplicationController
   end
 
   def update
-
     if @post.update(post_params)
       flash[:success] = "Post updated"
       redirect_to posts_path
@@ -36,6 +34,12 @@ class PostsController < ApplicationController
       flash.now[:alert] = "Update failed. Please check the form."
       render :edit
     end
+  end
+
+  def destroy
+    @post.destroy
+    flash[:success] = "Post deleted"
+    redirect_to posts_path
   end
 
   private
