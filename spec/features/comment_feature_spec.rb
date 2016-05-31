ffcfa952eceb1777090003693d1d753f46da0ef0
naridas <gregsparta@gmail.com>
2016-05-31 09:19:06 +0100

    fixed comment submit

--- spec/features/comment_feature_spec.rb
+++ spec/features/comment_feature_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 feature 'Creating Comments' do
-  scenario 'can comment on an existing post', :js => true do
+  scenario 'can comment on an existing post' do
     user = create :user
     post = create(:post, user_id: user.id)
     sign_in_with user
 
     visit '/'
-    fill_in "comment_content_#{post.id}", with: ";P"
-    find("#comment_content_#{post.id}").native.send_keys(:return)
+    fill_in "comment_content_#{post.id}", with: "meow"
+    click_button 'Submit'
 
-    expect(page).to have_content(';P')
+    expect(page).to have_content('meow')
   end
 end
