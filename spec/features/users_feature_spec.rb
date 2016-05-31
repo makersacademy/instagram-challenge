2e4cec4a2af5853b2acad87f77d8f3b895574411
naridas <gregsparta@gmail.com>
2016-05-31 08:11:07 +0100

    fixed tests, need to add more tests

--- spec/features/users_feature_spec.rb
+++ spec/features/users_feature_spec.rb
@@ -2,38 +2,38 @@ require 'rails_helper'
 
 feature "User can sign in and out" do
   context "user not signed in and on the homepage" do
-    it "should see a 'sign in' link and a 'sign up' link" do
+    it "should see a 'Login' link and a 'Register' link" do
       visit('/')
-      expect(page).to have_link('Sign in')
-      expect(page).to have_link('Sign up')
+      expect(page).to have_link('Login')
+      expect(page).to have_link('Register')
     end
 
-    it "should not see 'sign out' link" do
+    it "should not see 'Logout' link" do
       visit('/')
-      expect(page).not_to have_link('Sign out')
+      expect(page).not_to have_link('Logout')
     end
   end
 
-  context "user signed in on the homepage" do
+  context "user 'Register' on the homepage" do
     before do
       visit('/')
-      click_link('Sign up')
+      click_link('Register')
       fill_in('Email', with: 'test@example.com')
       fill_in('User name', with: 'test')
-      fill_in('Password', with: 'testtest')
+      fill_in('Password', with: 'testtest', match: :first)
       fill_in('Password confirmation', with: 'testtest')
       click_button('Sign up')
     end
 
-    it "should see 'sign out' link" do
+    it "should see 'Logout' link" do
       visit('/')
-      expect(page).to have_link('Sign out')
+      expect(page).to have_link('Logout')
     end
 
-    it "should not see a 'sign in' link and a 'sign up' link" do
+    it "should not see a 'Login' link and a 'Register' link" do
       visit('/')
-      expect(page).not_to have_link('Sign in')
-      expect(page).not_to have_link('Sign up')
+      expect(page).not_to have_link('Login')
+      expect(page).not_to have_link('Register')
     end
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -28,8 +28,19 @@ require 'web_helper'
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
+module AuthHelpers
+  def sign_in_with(user)
+    visit '/'
+    click_link 'Login'
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Log in'
+  end
+end
+
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
+  config.include AuthHelpers, type: :feature
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
