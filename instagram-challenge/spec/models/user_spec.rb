d864acffa2f0f42c0161ce099736e8577ab1d66f
Elia Bardashevich <ebardashevich@gmail.com>
2016-05-31 11:32:34 +0100

    adds user model test

--- instagram-challenge/spec/models/user_spec.rb
+++ instagram-challenge/spec/models/user_spec.rb
@@ -1,11 +1,7 @@
-# require 'rails_helper'
-#
-# describe User, type: :model do
-#
-#   it { is_expected.to have_many :images }
-#
-#   it { is_expected.to have_many :likes }
-#
-#   it { is_expected.to have_many :liked_images }
-#
-# end
+require 'rails_helper'
+
+describe User, type: :model do
+
+  it { is_expected.to have_many :liked_pictures}
+
+end
