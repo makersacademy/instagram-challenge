3b03971e222daa12d639099bdb21fb30811c9f21
letianw <tmac4ever0824@gmail.com>
2016-05-31 16:43:41 +1000

    liking implemented

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,24 @@
 module PostsHelper
+  def likers_of(post)
+    votes = post.votes_for.up.by_type(User)
+    user_names = []
+    unless votes.blank?
+      votes.voters.each do |voter|
+        user_names.push(voter.user_name)
+      end
+      user_names.to_sentence.html_safe + like_plural(votes)
+    end
+  end
+
+  def liked_post(post)
+    return 'glyphicon-heart' if current_user.voted_for? post
+    'glyphicon-heart-empty'
+  end
+
+  private
+
+  def like_plural(votes)
+    return ' like this' if votes.count > 1
+    ' likes this'
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  acts_as_voter
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :user_name, presence: true, length: { minimum: 4, maximum: 16}
