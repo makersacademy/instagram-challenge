b188c6ba2250bc3d3d6b3eebc4dab172cd586236
Emmett Walsh <emmett.finarr.walsh@gmail.com>
2016-05-31 10:29:13 +0100

    Failing test - and i've broken the migrations somehow. Sugar.

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,10 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-  
-  resources :photos
+
+  resources :photos do
+    resources :comments
+  end
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
