558f474135b27e49f3d0fe8a69d83303d15f4f8b
Marco Bagnasco <admin@hecticmojo.com>
2016-06-04 21:37:46 +0100

    WIP CSS tyling

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -237,7 +237,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :facebook, ENV['FB_API'], ENV['FB_SEC'], callback_url: 'https://picta-challenge.herokuapp.com/users/auth/facebook/callback'
+  config.omniauth :facebook, ENV['FB_API'], ENV['FB_SEC'], callback_url: 'https://localhost:3000/users/auth/facebook/callback'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
