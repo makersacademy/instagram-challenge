24e31f7792071022acc94e417ef4e1e450037b02
Luke <lukeaveil@yahoo.com>
2016-07-16 17:02:40 +0100

    haml uninstalled

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
 
   has_many :comments
 
-  has_attached_file :image, styles: { :medium => "640x" }
+  has_attached_file :image, styles: { :medium => "300x" }
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
