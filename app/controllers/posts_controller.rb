09711fb4c3a65f1755445e97e603f5c9d3c6f5c8
Luke <lukeaveil@yahoo.com>
2016-07-16 14:31:49 +0100

    edit caption functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,16 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to '/posts'
+  end
+
   private
 
   def post_params
--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 feature 'Create posts' do
-    scenario 'should display a prompt to add a photo' do
+  context 'new post' do
+    scenario 'should display a prompt to add a photo and caption' do
       visit '/'
       click_link 'New Post'
       attach_file 'Image', "spec/files/images/canada.jpg"
@@ -11,4 +12,20 @@ feature 'Create posts' do
       expect(page).to have_css "img[src*='canada.jpg']"
       expect(current_path).to eq '/posts'
     end
+  end
+
+  context 'editing posts' do
+    scenario 'user can edit image caption' do
+      visit '/'
+      click_link 'New Post'
+      attach_file 'Image', "spec/files/images/canada.jpg"
+      fill_in 'Caption', with: 'Snowboarding in Canada #winter'
+      click_button 'Create Post'
+      click_link 'Edit Post'
+      fill_in 'Caption', with: 'Snowboarding in Canada #winter #mountains'
+      click_button 'Update Post'
+      expect(page).to have_content 'Snowboarding in Canada #winter #mountains'
+      expect(current_path).to eq '/posts'
+    end
+  end
 end
