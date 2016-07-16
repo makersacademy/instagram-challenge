cf69c74521d00441396ccebd8fe57eb509a6b69b
toby676 <tjcmui@gmail.com>
2016-07-16 17:31:41 +0100

    images working

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+
+  has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
 end
