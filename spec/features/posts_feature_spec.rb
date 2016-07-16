237430adde1e4b540102f11aaba8b4b23db167ed
toby676 <tjcmui@gmail.com>
2016-07-16 17:18:39 +0100

    add ability to dd posts

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -14,11 +14,23 @@ feature 'posts' do
     Post.create(title: 'Pug', description: 'cute')
   end
 
-  scenario 'display restaurants' do
+  scenario 'display posts' do
     visit '/posts'
     expect(page).to have_content('cute')
     expect(page).not_to have_content('No posts yet')
   end
 end
+
+  context 'creating posts' do
+    scenario 'prompts user to fill out a form, then displays the new post' do
+      visit '/posts'
+      click_link 'Create a Post'
+      fill_in 'Title', with: 'Pug'
+      fill_in 'Description', with: 'Cute'
+      click_button 'Create Post'
+      expect(page).to have_content 'Pug'
+      expect(current_path).to eq '/posts'
+    end
+  end
 
 end
