a6b2312e5ad820071cf2620d7f6ef8cab230fdc9
Stephen Dawes <sdawes@outlook.com>
2016-07-17 14:27:50 +0100

    add flash confirmation status and errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,26 +10,39 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.create(post_params)
-    redirect_to posts_path
+    if @post = Post.create(post_params)
+      flash[:success] = "Your post has been created!"
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "Your new post couldn't be created! Please check the form."
+      render :new
+    end
   end
 
   def show
-
   end
 
   def edit
-
   end
 
   def update
-    @post.update(post_params)
-    redirect_to posts_path
+    if @post.update(post_params)
+      flash[:success] = "Post updated"
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "Update failed. Please check the form."
+      render :edit
+    end
   end
 
   def destroy
-    @post.destroy
-    redirect_to posts_path
+    if @post.destroy
+      flash[:success] = "Your post has been deleted."
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "Deletion failed. Please try again."
+      render :destroy
+    end
   end
 
 
