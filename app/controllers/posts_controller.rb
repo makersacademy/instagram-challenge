c46c462b02fbfe22a3fc9318b49b7e6b6e0911ea
Stephen Dawes <sdawes@outlook.com>
2016-07-17 13:54:41 +0100

    add _form partial for rendering form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,23 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to posts_path
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end 
+
+
   private
 
   def post_params
