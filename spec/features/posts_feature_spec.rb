bdc5556908a921f20e278d5e5c82188d815dbc49
LukaszGr <lukasz.grychowski@gmail.com>
2016-07-17 17:26:48 +0100

    test setup

---
+++ spec/features/posts_feature_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+feature 'posts' do
+  context 'no posts have been added' do
+    scenario 'should display a prompt to add a post' do
+      visit '/posts'
+      expect(page).to have_content 'No posts yet'
+      expect(page).to have_link 'Add a post'
+    end
+  end
+end
