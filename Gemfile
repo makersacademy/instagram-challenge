328ab1a5a46fc3c8ced929d31c2b90555d9532c4
Luke <lukeaveil@yahoo.com>
2016-07-17 12:14:28 +0100

    user sign in and out

--- Gemfile
+++ Gemfile
@@ -18,6 +18,8 @@ gem 'coffee-rails', '~> 4.2'
 
 gem 'paperclip', '~> 4.2'
 
+gem 'devise'
+
 gem 'simple_form'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
 
   has_many :comments, dependent: :destroy
 
-  has_attached_file :image, styles: { :medium => "300x" }
+  has_attached_file :image, styles: { :medium => "450x" }
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
