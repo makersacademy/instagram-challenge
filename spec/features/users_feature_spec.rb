cb96176c2e2072500e18dd2c33f9ddb0d40adde3
Jonathan <j_passe@live.fr>
2016-07-17 21:24:39 +0100

    add sign in and sign out links

---
+++ spec/features/users_feature_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+feature 'User can sign in and out' do
+  context 'User not signed in and on the homepage' do
+    it 'should see a \'sign in\' link and a \'sign up\' link' do
+      visit '/'
+      expect(page).to have_link('Sign in')
+      expect(page).to have_link('Sign up')
+    end
+
+    it 'should not see \'sign out\' link' do
+      visit '/'
+      expect(page).not_to have_link('Sign out')
+    end
+  end
+
+  context 'user signed in on the homepage' do
+    before do
+      visit '/'
+      click_link('Sign up')
+      fill_in('Email', with:'a@mail.com')
+      fill_in('Password', with: 'aaaaaa')
+      fill_in('Password confirmation', with: 'aaaaaa')
+      click_button('Sign up')
+    end
+
+    it 'should see \'sign out \' link' do
+      visit '/'
+      expect(page).not_to have_link('Sign in')
+      expect(page).not_to have_link('Sign up')
+    end
+  end
+end
