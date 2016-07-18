680918fcd03a0307dc91ad732f967d6e6ce08300
mtaner <mervetaner88@gmail.com>
2016-07-18 18:59:56 +0100

    Adds application settings for Heroku

--- config/application.rb
+++ config/application.rb
@@ -18,8 +18,6 @@ Bundler.require(*Rails.groups)
 
 module InstagramChallenge
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.assets.initialize_on_precompile = false
   end
 end
