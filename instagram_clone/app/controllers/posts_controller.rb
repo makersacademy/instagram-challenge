7898ec939d921a0bbcd80be5157688e896a17b94
Anna <anna.hollandsmith3@gmail.com>
2016-07-18 22:33:37 +0100

    drying code

--- instagram_clone/app/controllers/posts_controller.rb
+++ instagram_clone/app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -21,15 +22,12 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-      @post = Post.find params[:id]
    if @post.update(post_params)
      flash[:success] = "Post updated."
      redirect_to root_path
@@ -40,7 +38,6 @@ class PostsController < ApplicationController
  end
 
  def destroy
-  @post = Post.find(params[:id])
   @post.destroy
   redirect_to posts_path
 end
@@ -49,5 +46,9 @@ end
 
   def post_params
     params.require(:post).permit(:caption, :image)
+  end
+
+  def set_post
+     @post = Post.find(params[:id])
   end
 end
