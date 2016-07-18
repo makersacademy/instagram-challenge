975408aabf9dc95022b819b4feda0eee8334a9d4
Marlon Date-Chong <mdatechong@gmail.com>
2016-07-18 10:04:18 +0100

    picture sizing

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :destroy
   validates :image, presence: true
-  has_attached_file :image, styles: { :medium => "400x400" }
+  has_attached_file :image, styles: { :medium => "640x800" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
---
+++ spec/features/endorsing_comments_feature_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+feature 'endorsing comments' do
+
+end
