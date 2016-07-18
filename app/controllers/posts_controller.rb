81c03c7f8c90b1317ca8bcf05d1c75fe8aae278c
Kenneth Barrett <barrett.kenneth@gmail.com>
2016-07-18 09:42:23 +0100

    post controller fixed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,7 +21,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.create(post_params)
+    @post = current_user.posts.create(post_params)
     redirect_to posts_path
   end
 
