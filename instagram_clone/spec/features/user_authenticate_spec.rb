de05c3eadb7331bf42b7a0981754b4529ec0be32
Anna <anna.hollandsmith3@gmail.com>
2016-07-18 23:28:54 +0100

    additional tests written for greater coverage

--- instagram_clone/spec/features/user_authenticate_spec.rb
+++ instagram_clone/spec/features/user_authenticate_spec.rb
@@ -23,4 +23,9 @@ feature 'User authentication' do
     expect(page).to have_content('Logout')
   end
 
+  scenario 'can log out once logged in' do
+    sign_up
+    click_link 'Logout'
+    expect(page).to have_content('You need to sign in or sign up before continuing.')
+  end
 end
