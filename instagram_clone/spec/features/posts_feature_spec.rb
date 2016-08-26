868c222bf7007dd2885c1323dc1362e404782eae
lukew244 <luke.webster86@gmail.com>
2016-08-26 18:10:40 +0100

    Adds new post form

--- instagram_clone/spec/features/posts_feature_spec.rb
+++ instagram_clone/spec/features/posts_feature_spec.rb
@@ -5,8 +5,19 @@ feature 'posts' do
     scenario 'displays a prompt to add a post' do
       visit '/posts'
       expect(page).to have_content 'No posts yet'
-      expect(page).to have_link 'Add a post'
+      expect(page).to have_link 'New post'
     end
   end
+
+  context 'creating posts' do
+    scenario 'prompts user to fill out a form, then displays the new post' do
+      visit '/posts'
+      click_link 'New post'
+      fill_in 'Title', with: 'First post'
+      click_button 'Create Post'
+      expect(page).to have_content 'First post'
+      expect(current_path).to eq '/posts'
+    end
+end
 
 end
