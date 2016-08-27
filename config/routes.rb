6a164ebbd73c890ef79ed3af07248d06de2a8b5c
Nomi Vos <nvoswork@gmail.com>
2016-08-27 07:12:47 -0500

    sets up initial images controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'images#index'
+
+  get 'images' => 'images#index'
+
+  resources :images
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
---
+++ spec/features/images_feature_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+feature 'images' do
+  context 'no images have been added' do
+    scenario 'should display a prompt to add an image' do
+      visit '/images'
+      expect(page).to have_content 'No images yet'
+      expect(page).to have_link 'Add an image'
+    end
+  end
+end
