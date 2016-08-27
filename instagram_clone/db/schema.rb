e4b3707c3e8b1635369f19e41e75d15c5ca14ee1
Jonny Moore <jonnymoore121212@gmail.com>
2016-08-27 23:28:07 +0100

    Adds Omniauth facebook sign up

--- instagram_clone/db/schema.rb
+++ instagram_clone/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160827191423) do
+ActiveRecord::Schema.define(version: 20160827220634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,8 @@ ActiveRecord::Schema.define(version: 20160827191423) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
