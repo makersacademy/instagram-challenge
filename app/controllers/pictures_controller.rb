e06dbeb9374ae6854057300c0bdc424214d454b9
acsauk <acsauk@gmail.com>
2016-08-27 17:26:44 +0100

    adds form to pictures#new to allow users to upload image and description

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -4,4 +4,19 @@ class PicturesController < ApplicationController
     @pictures = Picture.all
   end
 
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    Picture.create(pictures_params)
+    redirect_to pictures_path
+  end
+
+  private
+
+  def pictures_params
+    params.require(:picture).permit(:description, :image)
+  end
+
 end
--- spec/features/pictures_feature_spec.rb
+++ spec/features/pictures_feature_spec.rb
@@ -1,25 +1,38 @@
 require 'rails_helper'
 
 feature 'pictures' do
+
   context 'no pictures have been added' do
     scenario 'should display a prompt to add a picture' do
       visit '/pictures'
       expect(page).to have_content 'No pictures added yet'
-      expect(page).to have_content 'Add a picture'
+      expect(page).to have_content 'Upload a picture'
     end
   end
 
   context 'pictures have been added' do
     before do
-      Picture.create(name:'cat_in_hat.jpg',
-                     description:'My cat wearing my hat',
+      Picture.create(description:'My cat wearing my hat',
                      image:'storage/imgs/cat_in_hat.jpg')
     end
 
     scenario 'display picture' do
       visit '/pictures'
-      expect(page).to have_content('cat_in_hat.jpg')
+      expect(page).to have_content('My cat wearing my hat')
       expect(page).not_to have_content('No pictures added yet')
     end
+
   end
+
+  context 'uploading pictures' do
+    scenario 'prompts user to fill out a form, then displays the new picture' do
+      visit '/pictures'
+      click_link 'Upload a picture'
+      fill_in 'Description', with: 'My cat in a hat'
+      click_button 'Upload Picture'
+      expect(page).to have_content 'My cat in a hat'
+      expect(current_path).to eq '/pictures'
+    end
+  end
+
 end
