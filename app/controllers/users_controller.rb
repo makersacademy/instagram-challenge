38d1f0f1b65f0f0fcc6f4a90810f0c3e9ff10eb0
liskowsky <karollebiedzinski@gmail.com>
2016-08-27 22:21:08 +0100

    Sign out tests passing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   def index
+    
   end
-  
+
 end
---
+++ spec/features/sign_up_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature "Sign up" do
+  context "after signing up" do
+    before do
+      visit "/"
+      click_link "Sign up"
+      fill_in "Email", with: "test@test"
+      fill_in "Password", with: "qwerty"
+      fill_in "Password confirmation", with: "qwerty"
+      click_button "Sign up"
+    end
+    it "should display 'sign out' link" do
+      expect(page).to have_link("Sign out")
+    end
+  end
+end
