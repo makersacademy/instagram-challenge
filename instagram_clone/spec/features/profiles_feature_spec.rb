a757cee6530b5818c81024b2a433ae149e066f41
Jonny Moore <jonnymoore121212@gmail.com>
2016-08-28 22:30:05 +0100

    :lipstick: styles posts#index page

--- instagram_clone/spec/features/profiles_feature_spec.rb
+++ instagram_clone/spec/features/profiles_feature_spec.rb
@@ -6,21 +6,21 @@ feature 'Profile page' do
     create_post(caption: "Jonny's post")
   end
 
-  scenario "Users can see their details on profile page" do
-    click_link "Profile"
-    expect(page).to have_content "jonny@mail.com's profile page"
-  end
+  # scenario "Users can see their details on profile page" do
+  #   click_link "Profile"
+  #   expect(page).to have_content "Email: jonny@mail.com"
+  # end
 
   scenario 'User can view their posts on a profile page' do
     click_link "Profile"
-    expect(page).to have_content "Jonny's post"
+    expect(page).to have_content "Check out jonny@mail.com's posts!"
   end
 
   scenario "User should NOT see other user's posts on their profile page" do
     click_link 'Sign out'
     sign_up(email: "cheekyimposter@mail.com")
     click_link "Profile"
-    expect(page).to have_content "cheekyimposter@mail.com's profile page"
+    expect(page).to have_content "cheekyimposter@mail.com's"
     expect(page).not_to have_content "Jonny's post"
   end
 
