9b907fd81c9c08857c4588ec9b42db3ffb048fdd
Mali Michael <malimccalla@gmail.com>
2016-08-28 15:10:55 +0100

    Adds photos to view in desc order

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,11 +1,22 @@
 class PhotosController < ApplicationController
 
   def index
-    @photos = Photo.all
+    @photos = Photo.all.order('created_at DESC')
   end
 
   def new
     @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.create(photo_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:caption, :image)
   end
 
 end
