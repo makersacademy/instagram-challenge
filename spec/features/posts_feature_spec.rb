ce4827705f0b9bf9d6b0ecee07a9db8f7a6b17a7
Jose <jcopoviking@outlook.com>
2016-08-28 19:25:38 +0100

    Adds Post model to db and redenrs all posts to index view

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -5,4 +5,15 @@ feature 'posts' do
       expect(page).to have_link 'Add a post'
     end
   end
+
+  context 'posts have been added' do
+    before do
+      Post.create(title: 'dog')
+    end
+
+    scenario 'display posts' do
+      visit '/posts'
+      expect(page).to have_content('dog')
+    end    
+  end
 end
