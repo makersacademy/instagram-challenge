2905bbeb5078eef72ddfb1225638e482a24ace6a
JulieLev <jkgee1@yahoo.co.uk>
2016-08-28 02:46:00 +0100

    username added to sign-up

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :username, :presence => true, :uniqueness => { :case_sensitive => false }
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+  
 end
