748cd3a1a105c27de91c1da81b462058c5779077
dan-cl <djclissold@yahoo.co.uk>
2016-08-28 17:04:06 +0100

    Adds user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :posts do
     resources :comments
   end
+  root "posts#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
