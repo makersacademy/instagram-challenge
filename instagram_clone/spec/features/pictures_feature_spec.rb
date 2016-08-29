e1605c95347caaec9ad59182a7a836372fa8e485
Adam <adamjbrowner@gmail.com>
2016-08-29 17:13:26 +0100

    checkpoint before attempting paperclip

--- instagram_clone/spec/features/pictures_feature_spec.rb
+++ instagram_clone/spec/features/pictures_feature_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 feature "Pictures" do
   context "There aren't any pictures yet" do
     scenario "Should prompt the user to upload a picture" do
-      visit '/'
+      visit '/pictures'
       expect(page).to have_content "No one has added any pictures yet!"
       expect(page).to have_link "Add a picture!"
     end
