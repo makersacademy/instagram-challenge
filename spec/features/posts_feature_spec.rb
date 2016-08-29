9acf8a0d21becca8f836f8d8ee7e7fd4ef50f113
Aga Kowalczuk <kowalczuk.a@outlook.com>
2016-08-29 13:43:48 +0100

    creating and showing posts

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -20,5 +20,29 @@ feature 'posts' do
       expect(page).not_to have_content('No posts yet')
     end
   end
-  
+
+  context 'creating posts' do
+    scenario 'prompts user to fill out a form, then displays the new post' do
+      visit '/posts'
+      click_link 'Add a post'
+      fill_in 'Title', with: 'dog'
+      fill_in 'Description', with: 'adorable'
+      click_button 'Create Post'
+      expect(page).to have_content 'dog'
+      expect(current_path).to eq '/posts'
+    end
+  end
+
+  context 'viewing posts' do
+
+    let!(:dog){ Post.create(title:'dog', description:'adorable') }
+
+    scenario 'lets a user view a post' do
+     visit '/posts'
+     click_link 'dog'
+     expect(page).to have_content 'adorable'
+     expect(current_path).to eq "/posts/#{dog.id}"
+    end
+
+  end
 end
