0753b5a4855a348c99e29977173d8b03dceb7f2b
Aga Kowalczuk <kowalczuk.a@outlook.com>
2016-08-29 15:51:30 +0100

    facebook login

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ gem 'jbuilder', '~> 2.5'
 # gem 'bcrypt', '~> 3.1.7'
 gem 'devise'
 
+gem 'omniauth-facebook'
+
 gem 'haml'
 
 gem 'launchy'
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,6 +12,8 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
+  config.omniauth :facebook, Rails.application.secrets.facebook_app_id, Rails.application.secrets.facebook_app_secret
+
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
 
   # Configure the class responsible to send e-mails.
