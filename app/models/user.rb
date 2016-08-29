8b38808f43a9615375de5157150df33cdbcd5082
Nomi Vos <nvoswork@gmail.com>
2016-08-29 06:08:46 -0500

    removes code set up for carrier wave

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and
-  mount_uploader :avatar, AvatarUploader
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get 'images' => 'images#index'
-  resources :images do
-    resource :comments
-  end
-
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root 'images#index'
 end
