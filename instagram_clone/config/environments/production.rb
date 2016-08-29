0d2bb602dc4ee1d9f48aee06c86ada074dc85479
Jonny Moore <jonnymoore121212@gmail.com>
2016-08-29 21:07:54 +0100

    Change to production environment in config

--- instagram_clone/config/environments/production.rb
+++ instagram_clone/config/environments/production.rb
@@ -7,7 +7,9 @@ Rails.application.configure do
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
+    },
+    :url =>':s3_domain_url',
+    :path => '/:class/:attachment/:id_partition/:style/:filename'
   }
   # Code is not reloaded between requests.
   config.cache_classes = true
