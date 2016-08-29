6256434aa270dfba8d6825dfe06871fe0b0ae67f
Adam <adamjbrowner@gmail.com>
2016-08-29 19:08:01 +0100

    Push to heroku

--- instagram_clone/app/controllers/pictures_controller.rb
+++ instagram_clone/app/controllers/pictures_controller.rb
@@ -1,7 +1,22 @@
 class PicturesController < ApplicationController
 
   def index
+    @pictures = Picture.all
+  end
 
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    @picture = Picture.new(picture_params)
+    redirect_to '/pictures'
+  end
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:image)
   end
 
 end
--- instagram_clone/spec/features/pictures_feature_spec.rb
+++ instagram_clone/spec/features/pictures_feature_spec.rb
@@ -7,5 +7,11 @@ feature "Pictures" do
       expect(page).to have_content "No one has added any pictures yet!"
       expect(page).to have_link "Add a picture!"
     end
+    scenario "A user wants to upload an image" do
+      visit '/pictures'
+      click_link "Add a picture!"
+      page.attach_file('picture_image', Rails.root + 'spec/capybara.jpg')
+      click_button "Upload Image"
+    end
   end
 end
