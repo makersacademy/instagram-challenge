7558886592dea9d7d88d8738f29390c2b608d719
noby <n.fujioka@abacusmaths.info>
2016-08-29 08:46:20 +0100

    issues found. duplicating photos after posting before refresh. also, error message for empty post not working.

--- picturegram/app/controllers/posts_controller.rb
+++ picturegram/app/controllers/posts_controller.rb
@@ -11,8 +11,10 @@ class PostsController < ApplicationController
 
   def create
     if @post = Post.create(post_params)
+      flash[:success] = "Your post has been created!"
       redirect_to posts_path
     else
+      flash.now[:alert]="Your new post couldn't be created! Please check the form."
       render :new
     end
   end
@@ -25,8 +27,10 @@ class PostsController < ApplicationController
 
   def update
     if @post.update(post_params)
-      redirect_to(post_path(@post))
+      flash[:success]="Post updated."
+      redirect_to posts_path
     else
+      flash.now[:alert]="Update failed. Please check the form."
       render :edit
     end
   end
