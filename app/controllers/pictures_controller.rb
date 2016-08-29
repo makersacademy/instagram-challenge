33488aa734edb7f34a105df1ddf7b9a63bf89e0a
Robert Summers <rsummers02@gmail.com>
2016-08-29 10:57:52 +0100

    user can upload photos

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -4,7 +4,7 @@ class PicturesController < ApplicationController
   end
 
   def create
-    @picture = Picture.create(picture_params)
+    @picture = Picture.create_with_user(picture_params, current_user)
     redirect_to '/profiles#index'
   end
 
--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,6 +2,7 @@ class ProfilesController < ApplicationController
 
   def index
     @user = current_user
+    @pictures = Picture.all.where(user_id: current_user.id)
   end
 
 end
