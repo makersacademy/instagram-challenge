bd4cbfc748e83b6a312e25812aa15de4ba224c81
Terry <terry@terrytilley.com>
2016-08-29 14:40:27 +0100

    Bootstrap styling and display photo test

--- spec/features/photos_feature_spec.rb
+++ spec/features/photos_feature_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 feature 'photos' do
+
   context 'no photos have been added' do
     scenario 'should display a prompt to add a photo' do
       visit '/photos'
@@ -8,4 +9,17 @@ feature 'photos' do
       expect(page).to have_link 'Add a photo'
     end
   end
+
+  context 'photos have been added' do
+    before do
+      Photo.create(description: 'Selfie')
+    end
+
+    scenario 'display photos' do
+      visit '/photos'
+      expect(page).to have_content('Selfie')
+      expect(page).not_to have_content('No photos yet')
+    end
+  end
+
 end
