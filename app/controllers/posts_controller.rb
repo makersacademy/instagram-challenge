e77811be952d583890965a45c3bef54fa803e376
prashantmathias <mathiasaudio@gmail.com>
2016-08-29 19:25:59 +0100

    adds bootstrap template, posts MVC and sign up functionality using devise

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,10 @@
+class PostsController < ApplicationController
+  def new
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+
+  root 'posts#index'
+  resources :posts
+  devise_for :users
+
+
+end
