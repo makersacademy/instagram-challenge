2f6d0e1a6e1506b2adbd610cf8d5fd45ab903c7b
Sophie Gill <sophie-gill@hotmail.com>
2016-08-29 20:46:17 +0100

    adds authentication to ensure users are logged in before they can add or edit a photo

--- spec/features/photos_feature_spec.rb
+++ spec/features/photos_feature_spec.rb
@@ -6,6 +6,11 @@ feature 'Photos' do
 
     it 'should display a prompt to add a photo' do
       visit '/photos'
+      click_link 'Sign up'
+      fill_in 'Email', with: "test@test.com"
+      fill_in 'Password', with: "123456"
+      fill_in 'Password confirmation', with: "123456"
+      click_button 'Sign up'
       expect(page).to(have_content("No photos yet"))
       expect(page).to(have_link("Add a photo"))
     end
@@ -28,8 +33,18 @@ feature 'Photos' do
 
   context 'adding photos' do
 
+    scenario 'a logged out user cannot add photos' do
+      visit '/photos'
+      expect(page).not_to(have_link('Add a photo'))
+    end
+
     it 'prompts a user to add a caption' do
       visit '/photos'
+      click_link 'Sign up'
+      fill_in 'Email', with: "test@test.com"
+      fill_in 'Password', with: "123456"
+      fill_in 'Password confirmation', with: "123456"
+      click_button 'Sign up'
       click_link 'Add a photo'
       fill_in 'Caption', with: 'This is my first photo'
       click_button 'Create Photo'
@@ -59,6 +74,11 @@ feature 'Photos' do
 
     it 'lets the user edit a photo' do
       visit '/photos'
+      click_link 'Sign up'
+      fill_in 'Email', with: "test@test.com"
+      fill_in 'Password', with: "123456"
+      fill_in 'Password confirmation', with: "123456"
+      click_button 'Sign up'
       click_link 'Edit photo'
       fill_in 'Caption', with: "It's still a photo"
       click_button 'Update Photo'
@@ -74,6 +94,11 @@ feature 'Photos' do
 
     it 'lets the user delete a photo' do
       visit '/photos'
+      click_link 'Sign up'
+      fill_in 'Email', with: "test@test.com"
+      fill_in 'Password', with: "123456"
+      fill_in 'Password confirmation', with: "123456"
+      click_button 'Sign up'
       click_link 'Delete photo'
       expect(page).not_to(have_content("this is a photo"))
       expect(page).to(have_content("Photo deleted successfully"))
