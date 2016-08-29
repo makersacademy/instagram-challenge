788766b01f27a23a711211de4c18e1150431be88
Arunas Skirius <arukomp@gmail.com>
2016-08-29 19:44:16 +0100

    Use Croppie for cropping images before uploading

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,6 +22,8 @@ $(function() {
     var fileInput = $(elem);
     var form      = $(fileInput.parents('form:first'));
     var submitButton = form.find('input[type="submit"]');
+    var photoView = form.find('.photo-view');
+    var cropButton = form.find('#crop-it');
     var progressBar  = $("<div class='progress-bar'></div>");
     var barContainer = $("<div class='progress'></div>").append(progressBar);
     fileInput.after(barContainer);
@@ -29,12 +31,32 @@ $(function() {
       fileInput:       fileInput,
       url:             form.data('url'),
       type:            'POST',
-      autoUpload:      true,
+      autoUpload:      false,
       formData:        form.data('form-data'),
       paramName:       'file', // S3 does not like nested name fields, i.e. name="user[avatar_url]"
       dataType:        'XML',  // S3 returns XML if success_action_status is set to 201
       replaceFileInput: false,
+      add: function(e, data) {
+        submitButton.prop('disabled', true);
+        var fileUrl = window.URL.createObjectURL(data.files[0]);
+        crop = photoView.croppie({
+          url: fileUrl,
+          viewport: { width: 250, height: 250, type: 'square' }
+        });
+        var $this = $(this);
+        cropButton.css('display', 'initial');
+        cropButton.on('click', function(e) {
+          e.preventDefault();
+          crop.croppie('result').then(function(resp){
+            blobData = dataURItoBlob(resp);
+            imageFile = new File([blobData], data.files[0].name, {type: "image/png", lastModified: Date.now()});
+            data.files[0] = imageFile;
+            data.submit();
+          });
+        });
+      },
       progressall: function(e, data) {
+        console.log(data);
         var progress = parseInt(data.loaded / data.total * 100, 10);
         progressBar.css('width', progress + '%')
       },
@@ -70,4 +92,24 @@ $(function() {
       }
     });
   });
+
+  function dataURItoBlob(dataURI, callback) {
+    // convert base64 to raw binary data held in a string
+    // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+    var byteString = atob(dataURI.split(',')[1]);
+
+    // separate out the mime component
+    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+
+    // write the bytes of the string to an ArrayBuffer
+    var ab = new ArrayBuffer(byteString.length);
+    var ia = new Uint8Array(ab);
+    for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+    }
+
+    // write the ArrayBuffer to a blob, and you're done
+    var bb = new Blob([ab]);
+    return bb;
+  }
 });
