f99d28d71f1c141eb29cfb02ec642ed79fc43c46
Terry <terry@terrytilley.com>
2016-08-29 15:15:52 +0100

    Photo controller new photo

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,22 @@
 class PhotosController < ApplicationController
 
   def index
-    
+    @photos = Photo.all
+  end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.create(photo_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:description)
   end
 
 end
--- spec/features/photos_feature_spec.rb
+++ spec/features/photos_feature_spec.rb
@@ -6,7 +6,7 @@ feature 'photos' do
     scenario 'should display a prompt to add a photo' do
       visit '/photos'
       expect(page).to have_content 'No photos yet'
-      expect(page).to have_link 'Add a photo'
+      expect(page).to have_link 'Add Photo'
     end
   end
 
@@ -19,6 +19,17 @@ feature 'photos' do
       visit '/photos'
       expect(page).to have_content('Selfie')
       expect(page).not_to have_content('No photos yet')
+    end
+  end
+
+  context 'creating photos' do
+    scenario 'prompts user to fill out a form, then displays the new photo' do
+      visit '/photos'
+      click_link 'Add Photo'
+      fill_in 'Description', with: 'Selfie'
+      click_button 'Create Photo'
+      expect(page).to have_content 'Selfie'
+      expect(current_path).to eq '/'
     end
   end
 
