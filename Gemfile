c3dc646dc63624059d3584d9d505193e99c2f3d8
hannako <jessjones1@gmail.com>
2016-08-29 23:36:55 +0100

    installs bootstrap and initial attempt at styling! Tests now failing

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'haml'
 gem 'paperclip'
 gem 'devise'
+gem 'bootstrap-sass', '~> 3.3.6'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ActiveRecord::Base
 
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-  has_attached_file :image, :styles => { :thumb => "60x60>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :styles => { :thumb => "400x400>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   validates_attachment_presence :image
 
