653b9ccbf6df5c4745ceef6bf2623fbe696033e3
Jonny Moore <jonnymoore121212@gmail.com>
2016-08-29 16:21:37 +0100

    Replaces likes with hearts. Empty heart for no likes

--- instagram_clone/app/helpers/likes_helper.rb
+++ instagram_clone/app/helpers/likes_helper.rb
@@ -1,2 +1,9 @@
 module LikesHelper
+  def hearts(likes)
+    if likes == 0
+      '0 ♡'
+    else
+      likes.to_s + ' ♥'
+    end
+  end
 end
--- instagram_clone/spec/features/likes_feature_spec.rb
+++ instagram_clone/spec/features/likes_feature_spec.rb
@@ -8,8 +8,14 @@ feature 'Likes' do
 
   scenario 'A user can like a post which increases the number of likes by 1' do
     visit '/posts'
-    click_button 'Like'
-    expect(page).to have_content '1 like'
+    click_button 'Love this post'
+    expect(page).to have_content '1 ♥'
+  end
+
+  scenario 'When there are no likes for a post, we see a "hollow" heart' do
+    visit '/posts'
+    expect(page).not_to have_content '1 ♥'
+    expect(page).to have_content '0 ♡'
   end
 
 end
