1404b28345fe31ba5e3f7380ed5b49d98d166026
6eff <6eff@mail.ru>
2016-08-30 10:56:03 +0300

    partial solution

---
+++ instagram/app/assets/javascripts/application.js
@@ -0,0 +1,75 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+// = require jquery
+// = require jquery_ujs
+// = require turbolinks
+// = require_tree .
+// = require z.jquery.fileupload
+// = require jquery.ui.widget
+
+$(function() {
+  $('.directUpload').find("input:file").each(function(i, elem) {
+    var fileInput    = $(elem);
+    var form         = $(fileInput.parents('form:first'));
+    var submitButton = form.find('input[type="submit"]');
+    var progressBar  = $("<div class='bar'></div>");
+    var barContainer = $("<div class='progress'></div>").append(progressBar);
+    fileInput.after(barContainer);
+    fileInput.fileupload({
+      fileInput:       fileInput,
+      url:             form.data('url'),
+      type:            'POST',
+      autoUpload:       true,
+      formData:         form.data('form-data'),
+      paramName:        'file', // S3 does not like nested name fields i.e. name="user[avatar_url]"
+      dataType:         'XML',  // S3 returns XML if success_action_status is set to 201
+      replaceFileInput: false,
+      progressall: function (e, data) {
+        var progress = parseInt(data.loaded / data.total * 100, 10);
+        progressBar.css('width', progress + '%');
+        console.log("done progressall");
+      },
+      start: function (e) {
+        submitButton.prop('disabled', true);
+        console.log("done start");
+
+        progressBar.
+          css('background', 'green').
+          css('display', 'block').
+          css('width', '0%').
+          text("Loading...");
+      },
+      done: function(e, data) {
+        submitButton.prop('disabled', false);
+        progressBar.text("Uploading done");
+
+        // extract key and generate URL from response
+        var key   = $(data.jqXHR.responseXML).find("Key").text();
+       var url   = '//' + form.data('host') + '/' + key;
+console.log("done done");
+       // create hidden field
+       var input = $("<input />", { type:'hidden', name: fileInput.attr('name'), value: url });
+       console.log("done");
+       form.append(input);
+            console.log("done form apend");
+     },
+     fail: function(e, data) {
+       submitButton.prop('disabled', false);
+     console.log("done fail");
+       progressBar.
+         css("background", "red").
+         text("Failed");
+     }
+   });
+ });
+});
