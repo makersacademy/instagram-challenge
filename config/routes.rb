198734fb4635582a092e56e5dbb934b4de09a2e1
6eff <6eff@mail.ru>
2016-08-30 12:28:49 +0300

    routing to users#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'users'
+  root 'users#index'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
