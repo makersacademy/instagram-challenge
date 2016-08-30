da6703cc0992cadc73d6e6ad1571e4594f1f87e4
Titus Iudean <iudeant@gmail.com>
2016-08-30 12:01:16 +0300

    Adds image posts using PaperClip

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -10,4 +10,15 @@ feature 'posts' do
     end
   end
 
+  context 'Creating posts' do
+    scenario 'prompts user with a form and on fill in it displays the new image post' do
+      visit '/'
+      click_link 'New Post'
+      attach_file('Image', "spec/files/images/ironing-boards.jpg")
+      fill_in 'Caption', with: 'sad story of #ironing-boards'
+      click_button 'Create Post'
+      expect(page).to have_content('#ironing-boards')
+    end
+  end
+
 end
