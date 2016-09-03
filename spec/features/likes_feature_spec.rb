675bb1491053ef49a1dbdbee571a03bc37cc6a65
Richard Bates <richo225@gmail.com>
2016-09-03 18:20:16 +0100

    Fix likes feature test

--- spec/features/likes_feature_spec.rb
+++ spec/features/likes_feature_spec.rb
@@ -9,17 +9,17 @@ feature "likes" do
   end
 
   scenario "image starts out with no likes" do
-    expect(page).to have_content "Likes: 0"
+    expect(page).to have_content "Like: 0"
     click_link "Selfie"
-    expect(page).to have_content "Likes: 0"
+    expect(page).to have_content "Like: 0"
   end
 
   scenario "liking an image increases counter by 1" do
     click_link "Selfie"
     click_link "Like"
-    expect(page).to have_content "Likes: 1"
+    expect(page).to have_content "Like: 1"
     visit "/"
-    expect(page).to have_content "Likes: 1"
+    expect(page).to have_content "Like: 1"
 
   end
 end
