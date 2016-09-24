f2db42052cfc3a6fcc5e41977c7473839c9d1fe3
prashantmathias <mathiasaudio@gmail.com>
2016-09-24 11:08:18 +0100

    refactors code and adds db cleaner

--- spec/features/posts_features_spec.rb
+++ spec/features/posts_features_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 
 feature 'creating posts with images' do
-  scenario 'can upload an image and add a description to it' do
+  scenario 'can upload an image and add a description' do
     sign_up
     click_link 'New Post'
     fill_in 'Description', with: 'Jonny Bravo'
     attach_file('post_image', "#{Dir.pwd}/spec/features/test_images/jonny.png")
     click_button 'Create Post'
-    expect(page).to have_content 'Jonny Bravo'
+
     expect(page).to have_content 'Success'
+    expect(page).to have_css('img')
+    expect(current_path).to eq '/posts/1'
   end
 end
