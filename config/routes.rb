cbdd6b62a29b101797fbc539870176e9bd3daf9c
Peter Miklos <pt.miklos@gmail.com>
2016-10-07 21:29:01 +0100

    routes, posts controller, start page added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+  root "application#index"
+
+  resources :posts do
+    resources :likes
+    resources :comments
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
