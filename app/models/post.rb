5c455c6f47c6603e84cda24f823b7151ce7dbd64
Abigail <abigail.mcphillips@gmail.com>
2016-10-08 21:32:05 +0100

    adds styling to index

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, styles: { medium: "700x", thumb: "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   validates :image, presence: true
 end
