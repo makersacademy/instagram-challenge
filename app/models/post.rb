803a7290f6be106209cdfc98787e060b21a64410
Peter Miklos <pt.miklos@gmail.com>
2016-10-08 14:16:52 +0100

    layout changes added

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   validates :description, presence: true
   validates :image, presence: true
 
-  has_attached_file :image, :styles => { medium: "300x300>", thumb: "400x400>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :styles => { medium: "autoxauto>", thumb: "300x300>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
 end
