7130f0cf8a1d3808e19a037f96178738958da544
Abigail <abigail.mcphillips@gmail.com>
2016-10-09 17:40:20 +0100

    facebook callback fix

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -249,9 +249,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :facebook, Rails.application.secrets.facebook_app_id,
-                             Rails.application.secrets.facebook_app_secret,
-                             callback_url: "http://localhost:3000/users/auth/facebook/callback"
-
+                             Rails.application.secrets.facebook_app_secret
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
