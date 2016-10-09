8c12a78c69218c2723f07a6ddc58d85b5bab4d29
Peter Miklos <pt.miklos@gmail.com>
2016-10-09 23:16:33 +0100

    db related files updated

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
 
-  has_one :post
+  belongs_to :post
   belongs_to :user
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161009145223) do
+ActiveRecord::Schema.define(version: 20161009220947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,8 +19,8 @@ ActiveRecord::Schema.define(version: 20161009145223) do
     t.text     "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "post_id"
     t.integer  "user_id"
+    t.integer  "post_id"
     t.index ["post_id"], name: "index_comments_on_post_id", using: :btree
     t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
   end
