0e86be74765dd82168de1a57887fff56b35a3cc6
Abigail <abigail.mcphillips@gmail.com>
2016-10-09 15:16:09 +0100

    tags saved without # but displayed with #

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,11 +12,15 @@ class Post < ApplicationRecord
 
   def all_tags=(names)
     self.tags = names.split(",").map do |name|
-      Tag.where(name: name.strip).first_or_create!
+      Tag.where(name: name.strip.delete('#')).first_or_create!
     end
   end
 
   def all_tags
     self.tags.map(&:name)
+  end
+
+  def self.tagged_with(name)
+    Tag.find_by_name!(name).posts
   end
 end
--- spec/features/tag_feature_spec.rb
+++ spec/features/tag_feature_spec.rb
@@ -8,12 +8,23 @@ feature 'tags' do
     sign_up
   end
 
-  scenario 'users can comment on posts' do
+  scenario 'users can tag posts' do
     visit '/posts'
     click_link 'Add a post'
     fill_in 'post_caption', with: 'this is a cat'
     page.attach_file('post_image', Rails.root + "spec/images/cat.jpg")
     fill_in 'post_all_tags', with: '#cat, #myfriend'
+    click_button 'Add'
+    expect(page).to have_link '#cat'
+    expect(page).to have_link '#myfriend'
+  end
+
+  scenario 'tags should begin with a #' do
+    visit '/posts'
+    click_link 'Add a post'
+    fill_in 'post_caption', with: 'this is a cat'
+    page.attach_file('post_image', Rails.root + "spec/images/cat.jpg")
+    fill_in 'post_all_tags', with: 'cat, myfriend'
     click_button 'Add'
     expect(page).to have_link '#cat'
     expect(page).to have_link '#myfriend'
