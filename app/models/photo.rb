cbdbb40bc47d41325c61693c4457f53b41ce751a
ercekal <ercekalabalikoglu@gmail.com>
2016-10-15 21:36:06 +0100

    adds bootstrap, removes pics. working on FB omniauth

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,6 @@
 class Photo < ApplicationRecord
   has_many :comments, dependent: :destroy
-  has_many :likes, dependent: :destroy
+  has_many :likes
   belongs_to :user
   validates :image, presence: true
   has_attached_file :image, styles: {medium: "300x300>"}, defaul_url: "/images/image_missing.png"
