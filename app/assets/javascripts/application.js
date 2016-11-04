0b72fd5f59ecf2462733ed5d4f26aa8811c5cbb9
6eff <6eff@mail.ru>
2016-11-04 21:06:26 +0300

    assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,66 +11,8 @@
 // about supported directives.
 //
 // = require jquery
+// = require aws_upload_client_side
 // = require jquery_ujs
 // = require turbolinks
-// = require_self
-// = require_tree .
-// = require z.jquery.fileupload
 // = require jquery.ui.widget
-
-$(function() {
-  $('.directUpload').find("input:file").each(function(i, elem) {
-    var fileInput    = $(elem);
-    var form         = $(fileInput.parents('form:first'));
-    var submitButton = form.find('input[type="submit"]');
-    var progressBar  = $("<div class='bar'></div>");
-    var barContainer = $("<div class='progress'></div>").append(progressBar);
-    fileInput.after(barContainer);
-    fileInput.fileupload({
-      fileInput:       fileInput,
-      url:             form.data('url'),
-      type:            'POST',
-      autoUpload:       true,
-      formData:         form.data('form-data'),
-      paramName:        'file', // S3 does not like nested name fields i.e. name="user[avatar_url]"
-      dataType:         'XML',  // S3 returns XML if success_action_status is set to 201
-      replaceFileInput: false,
-      progressall: function (e, data) {
-        var progress = parseInt(data.loaded / data.total * 100, 10);
-        progressBar.css('width', progress + '%');
-        console.log("done progressall");
-      },
-      start: function (e) {
-        submitButton.prop('disabled', true);
-        console.log("done start");
-
-        progressBar.
-          css('background', 'green').
-          css('display', 'block').
-          css('width', '0%').
-          text("Loading...");
-      },
-      done: function(e, data) {
-        submitButton.prop('disabled', false);
-        progressBar.text("Uploading done");
-
-        // extract key and generate URL from response
-        var key   = $(data.jqXHR.responseXML).find("Key").text();
-       var url   = '//' + form.data('host') + '/' + key;
-console.log("done done");
-       // create hidden field
-       var input = $("<input />", { type:'hidden', name: fileInput.attr('name'), value: url });
-       console.log("done");
-       form.append(input);
-            console.log("done form apend");
-     },
-     fail: function(e, data) {
-       submitButton.prop('disabled', false);
-     console.log("done fail");
-       progressBar.
-         css("background", "red").
-         text("Failed");
-     }
-   });
- });
-});
+// = require z.jquery.fileupload
