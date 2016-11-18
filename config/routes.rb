b7910ac18527050cab7174ce8bf1d89a8209961a
Liam Taylor <liam.taylor888@googlemail.com>
2016-11-18 22:16:55 +0000

    1) Added comments and reactions controllers 2) Added underlying routes for the aforementioned controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
 
-  resources :photos
+  resources :photos, shallow: true do
+    resources :comments do
+      resources :replies do
+      end
+    end
+    resources :reactions do
+    end
+  end
+
+
+   root to: "home#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
