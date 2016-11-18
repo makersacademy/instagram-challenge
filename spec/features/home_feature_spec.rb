b3c60efcd0905c773a2bc7f886510f9af3badb57
Tomy8s <tomy8s@hotmail.com>
2016-11-18 19:21:23 +0000

    test for link to posts#new

--- spec/features/home_feature_spec.rb
+++ spec/features/home_feature_spec.rb
@@ -6,12 +6,17 @@ describe 'Home' do
       visit '/'
       expect(page).to have_content 'Instakilo - like Instagram, just a thousand times better'
     end
+    scenario 'link takes visitor to posts#new' do
+      visit '/'
+      click_link 'Post photo'
+      expect(current_path).to eq '/posts/new'
+    end
+  end
 
-    context 'no posts to display' do
-      scenario 'visitor seed message' do
-        visit '/'
-        expect(page).to have_content 'No posts yet :\'('
-      end
+  context 'no posts to display' do
+    scenario 'visitor sees message' do
+      visit '/'
+      expect(page).to have_content 'No posts yet :\'('
     end
   end
 end
