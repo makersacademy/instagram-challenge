e438beb180de8e16ec89a2e24486ba7e5b26c8c6
Tomy8s <tomy8s@hotmail.com>
2016-11-18 19:18:13 +0000

    added text to posts#index if no posts

--- spec/features/home_feature_spec.rb
+++ spec/features/home_feature_spec.rb
@@ -6,5 +6,12 @@ describe 'Home' do
       visit '/'
       expect(page).to have_content 'Instakilo - like Instagram, just a thousand times better'
     end
+
+    context 'no posts to display' do
+      scenario 'visitor seed message' do
+        visit '/'
+        expect(page).to have_content 'No posts yet :\'('
+      end
+    end
   end
 end
