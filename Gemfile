3fd879c3dc947ae0b3a8ad1e6d31f99ecab86280
crispinandrews <crispin.andrews@gmail.com>
2016-11-19 16:49:15 +0000

    User features added. User feature tests all passing

--- Gemfile
+++ Gemfile
@@ -1,7 +1,8 @@
 source 'https://rubygems.org'
 
 gem 'devise'
-gem "paperclip"
+gem 'paperclip'
+gem 'shoulda'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
--- spec/features/pictures_feature_spec.rb
+++ spec/features/pictures_feature_spec.rb
@@ -8,4 +8,14 @@ feature 'pictures' do
       expect(page).to have_link 'Post a picture'
     end
   end
+
+  # context 'pictures have been added' do
+  #   before do
+  #     Picture.new(image: '/Users/crispinandrews/Pictures/brighton_2472753b-1.jpg')
+  #   end
+  #   scenario 'display pictures' do
+  #     vist '/pictures'
+  #     expect(page).to have_css("img[src*='/Users/crispinandrews/Pictures/brighton_2472753b-1.jpg']")
+  #   end
+  # end
 end
