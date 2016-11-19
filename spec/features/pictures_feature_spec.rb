840666fb91064f9e1aa0f883859987f3e1130935
Tom Page <tom.w.page@gmail.com>
2016-11-19 12:53:08 +0000

    pre controller creation

--- spec/features/pictures_feature_spec.rb
+++ spec/features/pictures_feature_spec.rb
@@ -2,12 +2,24 @@ require 'rails_helper'
 
 feature 'pictures' do
   context 'on the first visit ' do
-    scenario 'there should be content....' do
+    scenario 'there should be basic content....' do
     visit('/pictures')
     expect(page).to have_content('No pictures on here yet')
     expect(page).to have_link('Add a photo!')
     expect(page).to have_link('Sign in')
     expect(page).to have_link('Sign up')
+    end
+  end
+
+
+  context 'posting basics to the home page' do
+    before do
+      Post.create(post_text: 'Seasons greetings')
+    end
+    scenario 'we should be able to see the posts on the indexpage' do
+      visit('/pictures')
+      expect(page).to have_content('Seasons greetings')
+      expect(page).not_to have_content('No pictures on here yet')
     end
   end
 end
