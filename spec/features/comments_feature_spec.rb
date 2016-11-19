c28df02713d7b8abe68dd560a44ec8d5d0e7d664
Liam Taylor <liam.taylor888@googlemail.com>
2016-11-19 15:39:42 +0000

    1) Added passing test for photo upload functionality

--- spec/features/comments_feature_spec.rb
+++ spec/features/comments_feature_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-feature 'photo' do
-  context 'no photos have been added' do
-    scenario 'should display a prompt to add a photo' do
+feature 'comments' do
+  context 'posting a comment' do
+    scenario 'posting a comment' do
       visit '/photos'
       expect(page).to have_content 'Be the first to post a photo'
       expect(page).to have_link 'Post a photo'
