35c82b04dc9d3a1b9aefc330b1b05359da70101d
Katherine Pobee-Norris <kathwath@hotmail.com>
2016-11-19 13:32:48 +0000

    added ability to log in via Facebook and appended database to include name

--- instagram/db/schema.rb
+++ instagram/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161119125046) do
+ActiveRecord::Schema.define(version: 20161119133023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,9 @@ ActiveRecord::Schema.define(version: 20161119125046) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
