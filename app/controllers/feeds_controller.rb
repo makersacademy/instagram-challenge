9ce7b275b8979bf98ff4a7b7cd8ee55c3fdeb5fc
Anna Beddow <anna.beddow@gmail.com>
2016-11-19 18:43:49 +0000

    added feed controller and view

---
+++ app/controllers/feeds_controller.rb
@@ -0,0 +1,13 @@
+class FeedsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :feeds
+
   get 'welcome/index'
 
   root 'welcome#index'
