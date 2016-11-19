73dabc00dd567b7774afd57e4cd3e97a9b38df35
Kyaw Tun Sein <kyawtunsein.gla@gmail.com>
2016-11-19 13:00:03 +0000

    Add feature to upload photos

---
+++ instagram_clone/app/controllers/photos_controller.rb
@@ -0,0 +1,20 @@
+class PhotosController < ApplicationController
+  def index
+    @photos = Photo.all
+  end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    Photo.create(photo_params)
+    redirect_to '/photos'
+  end
+end
+
+private
+
+def photo_params
+  params.require(:photo).permit(:image, :caption)
+end
---
+++ instagram_clone/spec/features/photo_feature_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+feature 'photos' do
+  context 'no photos have been added' do
+    scenario 'should display a prompt to add a photo' do
+      visit '/photos'
+      expect(page).to have_link 'Add a photo'
+    end
+  end
+
+  context 'photos have been added' do
+    before do
+      Photo.create(caption: 'dog')
+    end
+
+    scenario 'display photos' do
+      visit '/photos'
+      expect(page).to have_content('dog')
+      expect(page).not_to have_content('No photos yet')
+    end
+  end
+
+end
