31a34ebff220981bcf2ac7abebadcc965d3d30ea
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2016-11-19 15:29:14 +0000

    Update, Destroy added to V, C

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -5,6 +5,40 @@ class PicturesController < ApplicationController
   end
 
   def new
+    @picture = Picture.new
   end
+
+  def create
+    @picture = Picture.create(picture_params)
+    p @picture.persisted?
+    redirect_to '/pictures'
+  end
+
+  def show
+    @picture = Picture.find(params[:id])
+  end
+
+  def edit
+    @picture = Picture.find(params[:id])
+  end
+
+  def update
+    picture = Picture.find(params[:id])
+    picture.update(picture_params)
+    redirect_to '/pictures'
+  end
+
+  def destroy
+    picture = Picture.find(params[:id])
+    picture.destroy
+    flash[:notice] = 'Picture deleted successfully'
+    redirect_to '/pictures'
+  end
+
+private
+
+def picture_params
+params.require(:picture).permit(:name, :description)
+end
 
 end
--- spec/feature/pictures_feature_spec.rb
+++ spec/feature/pictures_feature_spec.rb
@@ -9,9 +9,18 @@ feature 'pictures' do
     end
   end
 
+  context 'adding pictures' do
+    scenario 'prompts the user with a form to add pictures' do
+      visit '/pictures'
+      add_a_picture
+      expect(page).to have_content 'My picture'
+      expect(page).not_to have_content 'No pictures yet'
+    end
+  end
+
   context 'pictures have been added' do
     before do
-      Pictures.create(name: 'My picture')
+      Picture.create(name: 'My picture', description: 'This is a fantastic pic')
     end
     scenario 'display pictures' do
       visit '/pictures'
@@ -19,4 +28,55 @@ feature 'pictures' do
       expect(page).not_to have_content 'No pictures yet'
     end
   end
+
+  context 'viewing pictures' do
+
+    let!(:my_picture){ Picture.create(name:'My picture', description: 'This is a fantastic pic') }
+
+    scenario 'lets a user view a picture' do
+      visit '/pictures'
+      click_link 'My picture'
+      expect(page).to have_content 'My picture'
+      expect(current_path).to eq "/pictures/#{my_picture.id}"
+    end
+
+  end
+
+  context 'editing pictures' do
+
+    before { Picture.create name: 'My picture', description: 'This is a fantastic pic' }
+
+    scenario 'let a user edit a picture' do
+      visit '/pictures'
+      click_link 'Edit My picture'
+      fill_in 'Name', with: 'My picture'
+      fill_in 'Description', with: 'Even better description'
+      click_button 'Update Picture'
+      expect(page).to have_content 'My picture'
+      expect(page).to have_content 'Even better description'
+      expect(current_path).to eq '/pictures'
+    end
+
+  end
+
+  context 'deleting pictures' do
+
+    before { Picture.create name: 'My picture', description: 'This is a fantastic pic' }
+
+    scenario 'removes a picture when a user clicks a delete link' do
+      visit '/pictures'
+      click_link 'Delete My picture'
+      expect(page).not_to have_content 'My picture'
+      expect(page).to have_content 'Picture deleted successfully'
+    end
+
+  end
+
+end
+
+def add_a_picture
+  click_link 'Add a picture'
+  fill_in 'Name', with: 'My picture'
+  fill_in 'Description', with: 'This is a fantastic pic'
+  click_button 'Create Picture'
 end
