2cf0e66412cf8c12e1ca40c891eee6174150dd40
Darrenf <darrenf54321@gmail.com>
2016-11-19 09:01:14 +0000

    routes file updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :photos do
+  resources :comments
+  end
+
+  root "photos#index"
+
 end
