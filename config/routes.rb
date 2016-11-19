ad494bd3032527d5cafeed56d1d109452335a9eb
glynester <glynester@hotmail.com>
2016-11-19 22:41:24 +0000

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root 'galleries#index'
-  
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get 'gallery' => 'galleries#index'
