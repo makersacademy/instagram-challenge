1a987ab7f086d1c1175b9d9e28dcbcc47e3a3e88
Laurent Bouhnik <misterbl@hotmail.com>
2016-11-19 11:27:30 +0000

    added ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,8 +6,10 @@ class CommentsController < ApplicationController
     @comment.user_id = current_user.id
 
     if @comment.save
-      flash[:success] = "Your comment is now available for all eyes to see!"
-      redirect_to :back
+      respond_to do |format|
+        format.html {redirect_to root_path}
+        format.js
+      end
     else
       flash[:alert] = "Check the comment form, something went wrong."
       render root_path
@@ -16,9 +18,13 @@ class CommentsController < ApplicationController
 
   def destroy
   @comment = @post.comments.find(params[:id])
-  @comment.destroy
-  flash[:success] = "Comment deleted"
-  redirect_to root_path
+  if @comment.user_id == current_user.id
+      @comment.delete
+      respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js
+    end
+  end
   end
 
   private
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   validates :user_id, presence: true
   validates :image, presence: true
+  validates_length_of :caption, :minimum => 2, :maximum => 300, :allow_blank => true
 
   has_attached_file :image, styles: { :medium => "640x" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
