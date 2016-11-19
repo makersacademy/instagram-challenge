84aa05db507e5590e76368af5c36c3a99ae6dd28
Tomy8s <tomy8s@hotmail.com>
2016-11-19 22:02:30 +0000

    Added comment box to Posts#show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,7 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    @comment = @post.comments.build
   end
 
   def new
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
   has_attached_file :image, default_url: '/images/missing.png'
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  has_many :comments
 end
