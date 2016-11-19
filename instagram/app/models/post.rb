1c45036b1eea93474b6654e0b5a186a1d130e111
Katherine Pobee-Norris <kathwath@hotmail.com>
2016-11-19 12:47:54 +0000

    added validation so that caption cannot be repeated

--- instagram/app/models/post.rb
+++ instagram/app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
 
   has_many :comments, dependent: :destroy
-  validates :caption, length: { minimum: 3 }
+  validates :caption, length: { minimum: 3 }, uniqueness: true
+
 end
--- instagram/spec/models/post_spec.rb
+++ instagram/spec/models/post_spec.rb
@@ -6,4 +6,10 @@ describe Post, type: :model do
     expect(post).to have(1).error_on(:caption)
     expect(post).not_to be_valid
   end
+
+  it "is not valid unless it has a unique name" do
+    Post.create(caption: "Catford")
+    post = Post.new(caption: "Catford")
+    expect(post).to have(1).error_on(:caption)
+  end
 end
