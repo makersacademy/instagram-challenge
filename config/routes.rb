cd2d5196b17f7c9e1d4444fd4ae0e6bdd9153117
Laurent Bouhnik <misterbl@hotmail.com>
2016-11-19 19:07:53 +0000

    fixing heroku

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,5 @@ end
 get ':user_name', to: 'profiles#show', as: :profile
 get ':user_name/edit', to: 'profiles#edit', as: :edit_profile
 patch ':user_name/edit', to: 'profiles#update', as: :update_profile
-delete 'logout' => 'sessions#destroy'
+get 'logout' => :destroy
 end
