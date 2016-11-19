660f32f7fa1fdc9055fffbe73cba601e5981e8f6
Katherine Pobee-Norris <kathwath@hotmail.com>
2016-11-19 14:22:45 +0000

    added user id to comments so this now passes failing test

--- instagram/db/schema.rb
+++ instagram/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161119133023) do
+ActiveRecord::Schema.define(version: 20161119142200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,9 +21,11 @@ ActiveRecord::Schema.define(version: 20161119133023) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "post_id"
+    t.integer  "user_id"
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "posts", force: :cascade do |t|
     t.string   "caption"
@@ -54,4 +56,5 @@ ActiveRecord::Schema.define(version: 20161119133023) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
 end
