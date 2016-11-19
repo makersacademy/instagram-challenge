6cf5812988394294d3be3397706b456322c8910d
James <bakerje@mac.com>
2016-11-19 20:36:11 +0000

    Started to add pictures

--- spec/features/pictures_feature_spec.rb
+++ spec/features/pictures_feature_spec.rb
@@ -8,3 +8,15 @@ feature 'pictures' do
       expect(page).to have_link 'Add a picture'
     end
 end
+
+context 'restaurants have been added' do
+  before do
+    Pictures.create(name: 'pic1')
+  end
+
+  scenario 'display pictures' do
+    visit '/pictures'
+    expect(page).to have_css("img[src*='file_name_here']")
+    expect(page).not_to have_content('No pictures yet')
+  end
+end
