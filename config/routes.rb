27c1cf4a5746203eba4482dffc23d2b1ee34c7fc
Laurent Bouhnik <misterbl@hotmail.com>
2016-11-19 18:48:15 +0000

    fixing heroku

--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,10 @@ resources :posts do
     member do
       get 'like'
     end
-end 
+end
 
 get ':user_name', to: 'profiles#show', as: :profile
 get ':user_name/edit', to: 'profiles#edit', as: :edit_profile
 patch ':user_name/edit', to: 'profiles#update', as: :update_profile
+get 'logout' => 'sessions#destroy'
 end
