fd375dc5288995dc163a5aad28e1930ffc8f6001
Laurent Bouhnik <misterbl@hotmail.com>
2016-11-19 00:54:54 +0000

    only owner can edit a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,19 @@
 class PostsController < ApplicationController
-before_action :authenticate_user!  
+before_action :authenticate_user!
 before_action :set_post, only: [:show, :edit, :update, :destroy]
-
+before_action :owned_post, only: [:edit, :update, :destroy]
   def index
     @posts = Post.all
   end
 
 
   def new
-    @post = Post.new
+     @post = current_user.posts.build
   end
 
   def create
-    if @post = Post.create(post_params)
+    @post = current_user.posts.build(post_params)
+    if @post.save
         flash[:success] = "Your post has been created!"
         redirect_to posts_path
     else
@@ -52,5 +53,12 @@ before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def post_params
     params.require(:post).permit(:image, :caption)
+  end
+
+  def owned_post
+  unless current_user == @post.user
+    flash[:alert] = "That post doesn't belong to you!"
+    redirect_to root_path
+  end
   end
 end
