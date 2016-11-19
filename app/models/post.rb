3602f242daa7a087e39fd634b3ec17812d98ec2d
Tomy8s <tomy8s@hotmail.com>
2016-11-19 12:13:47 +0000

    link to missing image

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
-  has_attached_file :image, default_url: '/images/:style/missing.png'
+  has_attached_file :image, default_url: '/assets/images/missing.png'
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
