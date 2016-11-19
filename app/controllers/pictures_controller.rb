6d6b54e9fbce2134ba2ebbd9f2fa23d713284909
B-raw <michaelbarry1989@hotmail.com>
2016-11-19 21:37:19 +0000

    Able to upload image

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,7 +1,20 @@
 class PicturesController < ApplicationController
 
-  def index
+  def picture_params
+    params.require(:picture).permit(:description, :image)
+  end
 
+  def index
+    @pictures = Picture.all
+  end
+
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    @picture = Picture.create(picture_params)
+    redirect_to '/pictures'
   end
 
 end
--- spec/features/pictures_feature_spec.rb
+++ spec/features/pictures_feature_spec.rb
@@ -8,4 +8,30 @@ feature "Pictures" do
       expect(page).to have_link 'Add a picture'
     end
   end
+
+  context 'pictures have been added' do
+    before do
+      @rick = User.create(name: "rick", email: "asda@example.com")
+      @rick.pictures.create(description: 'Loch Lomond')
+      puts @rick.id
+    end
+
+    scenario 'display pictures' do
+      visit '/pictures'
+      expect(page).to have_content('Loch Lomond')
+      expect(page).not_to have_content('No pictures yet')
+    end
+  end
+
+  context 'create pictures' do
+    scenario 'can add new pictures' do
+      # skip "Need to figure out how to test this!"
+      visit '/pictures'
+      click_link 'Add a picture'
+      expect(current_path).to eq '/pictures/new'
+      fill_in "Description", with: "Loch Hope"
+      click_button "Upload Picture"
+      expect(page).to have_content "Loch Hope"
+    end
+  end
 end
