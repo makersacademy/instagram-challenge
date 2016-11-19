ace3eacd6b4913cc9b059dd6a54c1fabd90a18e3
Laurent Bouhnik <misterbl@hotmail.com>
2016-11-19 22:32:50 +0000

    delete chnged to destroy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
   config.serve_static_assets = true
   config.assets.digest = true
 
