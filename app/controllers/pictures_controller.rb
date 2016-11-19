14bf0faa039573b0db9c0b5e1ed53a728e33612d
Frances Maxwell <fmaxwell@gmail.com>
2016-11-19 02:55:26 +0000

    User can upload pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,25 @@
 class PicturesController < ApplicationController
   def index
+    @pictures = Picture.all
+  end
 
-  end  
+  def show
+    @picture = Picture.find(params[:id])
+  end
+
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    @picture = Picture.create(picture_params)
+    redirect_to '/pictures'
+  end
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:image, :description)
+  end
+
 end
