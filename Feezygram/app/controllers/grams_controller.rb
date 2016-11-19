c87b6478488908a2def9cace44467d1b60a25dd5
Asif Hafeez <asif.hafeez@hotmail.co.uk>
2016-11-19 14:44:34 +0000

    moved travis

--- Feezygram/app/controllers/grams_controller.rb
+++ Feezygram/app/controllers/grams_controller.rb
@@ -1,6 +1,7 @@
 class GramsController < ApplicationController
 
   def index
+    @grams = Gram.all
   end
 
   def new
@@ -18,6 +19,26 @@ class GramsController < ApplicationController
       end
     else
       flash[:alert] = "You must be signed in"
+      redirect_to "#index"
+    end
+  end
+
+  def edit
+    @gram = Gram.find(params[:id])
+  end
+
+  def update
+    @gram = Gram.find(params[:id])
+    @gram.update_attribute(:image, params[:gram][:image])
+    if current_user && current_user == @gram.user
+      if @gram.save
+      flash[:notice] = "You updated this gram!"
+      redirect_to @gram
+      else
+        render 'edit'
+      end
+    else
+      flash[:alert] = "That doesn't belong to you!"
       redirect_to "#index"
     end
   end
