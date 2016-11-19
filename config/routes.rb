de2610fe90e89f0f6982b8e62c0418458bc63e0b
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2016-11-19 13:19:23 +0000

    pictures routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+
+  get 'pictures' => 'pictures#index'
+  get 'pictures/new' => 'pictures#new'
+
+  resources :pictures
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
