b6a0996c2890ed2b5ae126685f17cd130236eea0
Katherine Pobee-Norris <kathwath@hotmail.com>
2016-11-19 12:50:31 +0000

    installed devise and completed on-screen instructions

--- instagram/config/routes.rb
+++ instagram/config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root to: "post#index"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
