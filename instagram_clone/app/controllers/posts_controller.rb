09b4200205b2d46467f6e267ecb88777e8fb013e
ManuCiao <mn.sabatino@gmail.com>
2016-11-20 20:08:42 +0000

    Show single posts

--- instagram_clone/app/controllers/posts_controller.rb
+++ instagram_clone/app/controllers/posts_controller.rb
@@ -10,7 +10,11 @@ class PostsController < ApplicationController
     def create
         @post = Post.create(post_params)
         redirect_to '/posts'
-     end
+    end
+
+    def show
+        @post = Post.find(params[:id])
+   end
 
     private
 
