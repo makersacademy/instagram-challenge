c292b9c9deba4e4ce2612e1748e6d679bdbf5be1
Dionysis <Dionysis.k22@gmail.com>
2016-11-20 16:16:34 +0000

    validation for requiring presence of image, rspec success

--- Picgram/app/controllers/posts_controller.rb
+++ Picgram/app/controllers/posts_controller.rb
@@ -9,7 +9,13 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.create(post_params)
-    redirect_to @post
+    if @post.save
+      flash[:success] = "You have successfully posted an image "
+      redirect_to @post
+    else
+      flash[:alert] = "You have't uploaded an image"
+      render :new
+    end
   end
 
   def show
--- Picgram/spec/features/creating_posts_spec.rb
+++ Picgram/spec/features/creating_posts_spec.rb
@@ -10,4 +10,14 @@ feature 'Creating posts' do
     expect(page).to have_content('#astronomy')
     expect(page).to have_css("img[src*='messier.jpg']")
   end
+
+  it 'need an image to create a post' do
+    visit '/'
+    click_link 'New Post'
+    fill_in 'Caption', with: 'not selected an image'
+    click_button 'Post'
+    expect(page).to have_content("You have't uploaded an image")
+  end
+
+
 end
