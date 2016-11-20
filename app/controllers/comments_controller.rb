179eeba9db2cf0793f2531b5b2f2464573597547
Anna Beddow <anna.beddow@gmail.com>
2016-11-20 19:54:38 +0000

    added comments with user_id

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,14 +8,18 @@ class CommentsController < ApplicationController
     if !user_signed_in?
       redirect_to '/feeds'
     end
-    @comment = Comment.new
-
+  p  @comment = Comment.new
+  p "at new"
   end
 
   def create
+    p "at create"
     @comment = Comment.new(comment_params)
+    p @comment
   p  @comment.photo_id = params[:photo_id]
+    p @comment.user_id = @current_user.id
     @comment.save
+    p @comment
     flash[:notice] = "comment successfully posted"
     redirect_to '/feeds'
   end
