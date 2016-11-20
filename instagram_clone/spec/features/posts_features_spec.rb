e94020c98d6d680b5fc095377c6b8d31c0d8d027
ManuCiao <mn.sabatino@gmail.com>
2016-11-20 18:45:59 +0000

    first test

---
+++ instagram_clone/spec/features/posts_features_spec.rb
@@ -0,0 +1,13 @@
+ require 'rails_helper'
+
+  feature 'posts' do
+
+    context 'no posts should have been added' do
+      scenario 'should display a prompt to add a post' do
+        visit '/posts'
+        expect(page).to have_content 'No posts yet'
+        expect(page).to have_link 'Add a post'
+      end
+    end
+
+  end
