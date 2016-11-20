92e60464c00e6e00efb44ca943f1b1bf78d955b2
alexanderwjrussell <awjrussell@live.co.uk>
2016-11-20 14:13:22 +0000

    Creating new posts 1) Modifed the posts controller to create new posts to the database 2) Modified to the index to correctly redirect to the new post page 3) New post test passing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,13 @@ class PostsController < ApplicationController
   end
 
   def create
-    
+    @post = Post.create(post_params)
+    redirect_to '/posts'
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:description)
   end
 end
--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -20,4 +20,15 @@ feature 'Posts' do
       expect(page).not_to have_content('No posts yet')
     end
   end
+
+  context 'creating posts' do
+    scenario 'prompts user to fill out a form, then displays the new post' do
+      visit '/posts'
+      click_link 'Add a post'
+      fill_in 'post_description', with: 'Test post'
+      click_button 'Create Post'
+      expect(page).to have_content 'Test post'
+      expect(current_path).to eq '/posts'
+    end
+  end
 end
