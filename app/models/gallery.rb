902419b59c231467e396eece29b9e606ed193ba8
glynester <glynester@hotmail.com>
2016-11-20 11:45:49 +0000

    Added 'belongs to gallery' to picture. Created failing test for deleting a picture.

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,4 +1,5 @@
 class Gallery < ApplicationRecord
-  validates :name, length: { minimum: 1, maximum: 30 }, uniqueness: true
   has_many :pictures, dependent: :destroy
+  # belongs_to :user
+  validates :name, length: { minimum: 1, maximum: 30 }, uniqueness: true
 end
--- spec/features/pictures_feature_spec.rb
+++ spec/features/pictures_feature_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
 feature 'adding pictures' do
-  before { Picture.create title: 'Another cute pic' }
-
+    # before { Picture.create title: 'Another cute pic' }
   scenario 'allows users to add a new picture using a form' do
     sign_up_and_sign_in
     create_a_gallery
@@ -11,6 +10,20 @@ feature 'adding pictures' do
     fill_in "Title", with: "Oh my god!"
     click_button 'Create Picture'
     expect(page).to have_content('Oh my god!')
+  end
+end
+
+feature 'deleting pictures' do
+  scenario 'allows users to delete a picture using a form' do
+    sign_up_and_sign_in
+    create_a_gallery
+    visit_a_gallery
+    click_link 'Add Picture'
+    fill_in "Title", with: "Oh my god!"
+    click_button 'Create Picture'
+    click_button 'Delete Picture'
+    expect(page).not_to have_content('Another cute pic')
+    expect(page).to have_content("Picture \"Another cute pic\" was successfully deleted")
   end
 
 end
