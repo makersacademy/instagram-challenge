0964494228659daa92b40386478b5e1aafaedc94
Malinna Leach <malinnab@yahoo.com>
2016-11-20 16:17:12 +0000

    user can login with username

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,15 @@ class User < ActiveRecord::Base
 
   has_many :pictures, dependent: :destroy
 
+  attr_accessor :login
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
+
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,6 +14,10 @@ Devise.setup do |config|
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
 
+  config.authentication_keys = [ :login ]
+
+  config.scoped_views = true
+  
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
