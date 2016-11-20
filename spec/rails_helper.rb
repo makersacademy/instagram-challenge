a20ac8a5a993c42b4a1868880f8a9a4482e34905
James Gardiner <jamesgardiner075@gmail.com>
2016-11-20 17:56:00 +0000

    Add model helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,8 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require 'database_cleaner'
+
+require_relative './helpers/model_helpers'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
