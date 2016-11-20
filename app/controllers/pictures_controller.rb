b48d5203304a0049a4b98ba9c32d45b0159d3232
glynester <glynester@hotmail.com>
2016-11-20 10:34:48 +0000

    Pic create method on pics controller. Gallery has_many pics. Tests still failing as no method 'pictures' - association is needed.

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -3,4 +3,15 @@ class PicturesController < ApplicationController
     @gallery = Gallery.find(params[:gallery_id])
     @picture = Picture.new
   end
+
+  def create
+    @gallery = Gallery.find(params[:gallery_id])
+    p params[:gallery_id]
+    @gallery.pictures.create(picture_params)
+  end
+
+  private
+  def picture_params
+    params.require(:picture).permit(:title)
+  end
 end
--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,3 +1,4 @@
 class Gallery < ApplicationRecord
   validates :name, length: { minimum: 1, maximum: 30 }, uniqueness: true
+  has_many :pictures, dependent: :destroy
 end
