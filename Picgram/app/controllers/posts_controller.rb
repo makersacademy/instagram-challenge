d35a809adfa985748cc6e6f4ed80019e6e3e6943
Dionysis <Dionysis.k22@gmail.com>
2016-11-20 19:48:52 +0000

    can edit post

--- Picgram/app/controllers/posts_controller.rb
+++ Picgram/app/controllers/posts_controller.rb
@@ -30,6 +30,7 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
     @post.update(post_params)
+    flash[:alert] = "Post updated"
     redirect_to(post_path(@post))
   end
 
