7ecabca866830bed170f09fef48aa1d609cca6da
Nicole <nicoleshasha@hotmail.co.uk>
2016-11-20 23:41:32 +0000

    Two tests passing

--- spec/features/images_feature_spec.rb
+++ spec/features/images_feature_spec.rb
@@ -15,8 +15,21 @@ context 'adding images' do
     visit '/images'
     click_link 'Add an image'
     fill_in 'Url', with: 'https://s-media-cache-ak0.pinimg.com/564x/0a/c5/bd/0ac5bd36c10d334d2b985893285caf14.jpg', visible: false
-    click_button 'Create image'
+    click_button 'Create Image'
     expect(page).to have_content 'https://s-media-cache-ak0.pinimg.com/564x/0a/c5/bd/0ac5bd36c10d334d2b985893285caf14.jpg'
     expect(current_path).to eq '/images'
   end
+end
+
+context 'viewing images' do
+
+  let!(:hello){ Image.create(url:'hello') }
+
+  scenario 'lets a user view an image' do
+   visit '/images'
+   click_link 'hello'
+   expect(page).to have_content 'hello'
+   expect(current_path).to eq "/images/#{hello.id}"
+  end
+
 end
