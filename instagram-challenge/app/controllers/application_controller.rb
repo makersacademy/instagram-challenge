d2201fde63b70f31e5a1b5e01f699f188142f381
James <James.Steel@me.com>
2016-11-20 19:52:38 +0000

    user can add small about me on sign up and edit profile

--- instagram-challenge/app/controllers/application_controller.rb
+++ instagram-challenge/app/controllers/application_controller.rb
@@ -6,9 +6,9 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
+    added_attrs = [:username, :email, :about_me, :password, :password_confirmation, :remember_me]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
-  
+
 end
