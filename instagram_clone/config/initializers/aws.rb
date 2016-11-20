c5c6f8dce91187b36a8122cb39b20237503593b7
ManuCiao <mn.sabatino@gmail.com>
2016-11-20 21:19:38 +0000

    set up AWS and the private env

---
+++ instagram_clone/config/initializers/aws.rb
@@ -0,0 +1,4 @@
+Aws.config.update(region: 'eu-east-1',
+                  credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']))
+
+S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['S3_BUCKET'])
