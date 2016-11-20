ea966fb3f4068471743a6dc79ada336c3c2624a2
James <James.Steel@me.com>
2016-11-20 19:03:36 +0000

    user can only like image once, HREF gets disabled after first click

--- instagram-challenge/app/assets/javascripts/application.js
+++ instagram-challenge/app/assets/javascripts/application.js
@@ -19,10 +19,10 @@ $(document).ready(function() {
   $('.endorsements-link').on('click', function(event){
       event.preventDefault();
 
-      var endorsementCount = $(this).siblings('.endorsements_count');
-
-      $.post(this.href, function(response){
+       var endorsementCount = $(this).siblings('.endorsements_count');
+       $.post(this.href, function(response){
         endorsementCount.text(response.new_endorsement_count);
+        $('.endorsements-link').removeAttr('href');
     });
   });
 });
