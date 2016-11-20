68125c9df83d3015d8b3054bb6a86b663124ad94
Anna Lucking <annavlucking@hotmail.co.uk>
2016-11-20 12:58:33 +0000

    add functionality for adding photos with captions

--- happysnaps/app/controllers/posts_controller.rb
+++ happysnaps/app/controllers/posts_controller.rb
@@ -1,7 +1,26 @@
 class PostsController < ApplicationController
 
   def index
-
+    @posts = Post.all
   end
-  
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+      # if @post.save
+        redirect_to posts_path
+      # else
+      #   render 'new'
+      # end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:image, :caption)
+  end
+
 end
