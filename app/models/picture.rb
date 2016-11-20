c3ccf9bbc7327527eb04d6059d464805aeb5f0b6
James Gardiner <jamesgardiner075@gmail.com>
2016-11-20 13:05:58 +0000

    Add validation for image

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,3 +1,4 @@
 class Picture < ApplicationRecord
+  validates :image, presence: true
   mount_uploader :image, PictureUploader
 end
--- spec/models/picture_spec.rb
+++ spec/models/picture_spec.rb
@@ -4,12 +4,17 @@ RSpec.describe Picture, type: :model do
 
   subject(:picture) {described_class.create(comment: "Love this image!")}
 
-  it "adds an image" do
+  it "adds an image to the picture" do
     File.open('./app/assets/images/duck.jpg') do |f|
       picture.image = f
     end
     picture.save
     expect(picture.image_identifier).to eq("duck.jpg")
+  end
+
+  it "requires an image" do
+    picture2 = Picture.new(comment: "Love this image!")
+    expect{picture2.save}.not_to change{Picture.count}
   end
 
   it "adds an optional comment" do
