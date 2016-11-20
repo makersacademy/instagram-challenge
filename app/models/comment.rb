341b128e433fd985c96b9217ad9b90e4c1b11697
Tomy8s <tomy8s@hotmail.com>
2016-11-20 04:13:15 +0000

    added comments_counter cache to posts

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,3 @@
 class Comment < ApplicationRecord
-  belongs_to :post
+  belongs_to :post, counter_cache: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161119134234) do
+ActiveRecord::Schema.define(version: 20161120035733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20161119134234) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.integer  "comments_count"
   end
 
   add_foreign_key "comments", "posts"
