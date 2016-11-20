4e9234a1b0f4f51d55dcc8ab9b81d5af080c4b68
James Gardiner <jamesgardiner075@gmail.com>
2016-11-20 14:56:35 +0000

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'pictures/index'
 
-  get 'pictures/new'
+  resources :pictures
+
+  root 'pictures#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
