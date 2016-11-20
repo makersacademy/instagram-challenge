0df4a50e72cfbea4a57d49c639e422f90c692ec7
Tadas Majeris <tadas.majeris@gmail.com>
2016-11-20 12:40:13 +0000

    tests for editing and deleting pics

--- spec/features/pics_feature_spec.rb
+++ spec/features/pics_feature_spec.rb
@@ -16,10 +16,27 @@ feature 'pics' do
       page.attach_file('pic_url', Rails.root + 'public/test.png')
       click_button 'Create Pic'
     end
-    
+
     scenario 'should display the pic and description' do
       expect(page).to have_css('img')
       expect(page).to have_content('awesome picture')
+    end
+
+    scenario 'picture can be edited' do
+      visit '/pics'
+      click_link 'Edit'
+      expect(page).to have_css('form.edit_pic')
+      fill_in :Description, with: 'a very cool picture'
+      click_button 'Update Pic'
+      expect(page).not_to have_content('awesome picture')
+      expect(page).to have_content('a very cool picture')
+    end
+
+    scenario 'picture can be deleted' do
+      visit '/pics'
+      click_link 'Delete'
+      expect(page).not_to have_content('awesome picture')
+      expect(page).not_to have_css('img')
     end
   end
 
