c74eecd3bc8c680c58c2b51f4a440c07e81d054c
Elizabeth Wicks <elizabeth.wicks@hotmail.com>
2016-11-20 20:32:52 +0000

    must be signed in to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,27 @@
 class PostsController < ApplicationController
 
   def new
-    @post = Post.new
+    if !current_user
+      flash[:error] = "You must be signed in to post"
+      @post = Post.new
+    else
+      @post = Post.new
+    end
   end
 
   def create
-    @post = Post.create(image_params)
-    redirect_to posts_path
+    if current_user
+      @post = current_user.post.new(image_params)
+        if @post.save
+          flash[:success] = "Post added"
+          redirect_to posts_path
+        else
+          render 'new'
+        end
+    else
+      flash[:error] = "You mmust be signed in to post"
+      redirect_to new_user_session_path
+    end
   end
 
   def index
