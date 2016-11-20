615c82450e48f982ee63313c16e57dc9acf224a9
Tomy8s <tomy8s@hotmail.com>
2016-11-20 04:16:57 +0000

    comment_counter defaults to 0

--- db/schema.rb
+++ db/schema.rb
@@ -25,13 +25,13 @@ ActiveRecord::Schema.define(version: 20161120035733) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "caption"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
-    t.integer  "comments_count"
+    t.integer  "comments_count",     default: 0
   end
 
   add_foreign_key "comments", "posts"
