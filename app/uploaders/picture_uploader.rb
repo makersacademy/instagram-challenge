733648aabe54f24327d2b28ff9bcb6e4f0d48dbf
James Gardiner <jamesgardiner075@gmail.com>
2016-11-20 19:42:52 +0000

    Add s3 storage for images and resizing

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -2,11 +2,10 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -30,9 +29,21 @@ class PictureUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+  version :large do
+    process resize_to_limit: [800, 800]
+  end
+
+  version :medium, :from_version => :large do
+    process resize_to_limit: [500, 500]
+  end
+
+  version :thumb, :from_version => :medium do
+    process resize_to_fit: [100, 100]
+  end
+
+  version :square do
+    process :resize_to_fill => [500, 500]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
