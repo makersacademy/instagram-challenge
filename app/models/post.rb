cbdd990f3e0da98192a13b8f414e18423ca18f9f
Katherine Pobee-Norris <kathwath@hotmail.com>
2016-11-20 13:55:01 +0000

    additional css added

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,7 @@ class Post < ActiveRecord::Base
 
   validates :caption, length: { minimum: 3 }, uniqueness: true
 
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "300x300>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :styles => { :medium => "600x600>", :thumb => "400x400>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
 end
