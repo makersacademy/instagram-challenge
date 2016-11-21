0ea143ceb5a037c09995c0532fbf4235e4dae500
Tomy8s <tomy8s@hotmail.com>
2016-11-21 06:22:17 +0000

    destroy  likes on dislike

--- app/controllers/dislikes_controller.rb
+++ app/controllers/dislikes_controller.rb
@@ -5,6 +5,7 @@ class DislikesController < ApplicationController
     @post = Post.find(params[:post_id])
     if @post.dislikes.where(user: current_user).empty?
       @post.dislikes.create(user: current_user)
+      @post.likes.where(user: current_user).destroy_all
       @post.update_rating
     end
     redirect_to post_path(@post.id)
