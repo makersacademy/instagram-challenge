ec77c64cadc00238fa2b1e5e86855fd79e87015e
Katherine Pobee-Norris <kathwath@hotmail.com>
2016-11-21 11:44:23 +0000

    changed files to reflect aws

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,11 +5,12 @@ Rails.application.configure do
   config.cache_classes = true
 
   config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
     }
   }
   # Eager load code on boot. This eager loads most of Rails and
