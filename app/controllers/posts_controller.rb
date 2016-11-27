5e6add4472b3ec91ad3106e9b54786f0a5c3658b
ManuCiao <mn.sabatino@gmail.com>
2016-11-27 02:01:11 +0000

    user cannot edit a post that does not belong to him/her

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, :except => [:index, :show]
+  before_action :owned_post, only: [:edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -47,4 +48,12 @@ class PostsController < ApplicationController
   def post_params
     params[:post].permit(:name, :image)
   end
+
+  def owned_post
+     @post = Post.find(params[:id])
+     unless current_user == @post.user
+       flash[:alert] = "Sorry, you cannot change a post that doesn't belong to you!"
+       redirect_to posts_path
+     end
+   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-has_many :posts
+has_many :posts, dependent: destroy
 
 end
