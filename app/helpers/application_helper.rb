09ca66f5db4fa9fffb7f581b61564b6a693a648c
crispinandrews <crispin.andrews@gmail.com>
2017-01-09 14:10:49 +0000

    Changes to edit post and sign up and in forms styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def form_image_select(post)
-    return image_tag picture.image.url,
+    return image_tag @picture.image.url,
                      id: 'image-preview',
                      class: 'img-responsive' if @picture.image.exists?
     image_tag 'placeholder.jpg', id: 'image-preview', class: 'img-responsive'
