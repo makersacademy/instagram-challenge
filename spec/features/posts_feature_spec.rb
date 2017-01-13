1e553d39432b0f5342e19e96e846e47a5a6d6195
Tom Fuller <tomf95@hotmail.co.uk>
2017-01-13 14:04:54 +0000

    can click on picture to look at it bigger

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -18,8 +18,19 @@ feature 'posts' do
     scenario 'display posts' do
       visit '/posts'
       expect(page).to have_content 'Beautiful day'
+      expect(page).to have_css("img[src*='snow_and_sun.jpg']")
       expect(current_path).to eq '/posts'
       expect(page).not_to have_content 'No posts yet'
+    end
+  end
+
+  context 'viewing posts' do
+
+    scenario 'lets a user view the posts' do
+      add_post
+      click_link 'image'
+      expect(page).to have_content 'Beautiful day'
+      expect(page).to have_css("img[src*='snow_and_sun.jpg']")
     end
   end
 end
--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -2,5 +2,6 @@ def add_post
   visit '/posts'
   click_link 'Add a post'
   fill_in 'Caption', with: 'Beautiful day'
+  page.attach_file('post[image]', Rails.root + 'spec/fixtures/snow_and_sun.jpg')
   click_button 'Post'
 end
