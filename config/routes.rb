43739a5a6f318fd1c8fb3e4720c73c241c2e707a
TudorTacal <tudortacal@gmail.com>
2017-01-14 16:37:57 +0000

    Added username to the User model. Updated devise to accept username as params when sign_up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+       sessions: 'users/sessions',
+       registrations: 'users/registrations'
+     }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root to: "users#index"
+  # root to: "user#index"
+
 end
