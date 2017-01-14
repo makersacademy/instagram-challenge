eeffe686ec5fce372774d081796a728a6e2350b3
George Seeger <georgeseeger@hotmail.com>
2017-01-14 21:59:25 +0000

    Picture upload teset

---
+++ spec/features/picture_spec.rb
@@ -0,0 +1,16 @@
+require 'rails-helper'
+
+feature "Pictures" do
+
+  context "user should be able to upload pictures" do
+
+    scenario do
+      visit '/pictures/'
+      expect(page).to have_content "Add a new pik"
+      click_link "Add a new pik"
+      page.attach_file('image_upload', Rails.root + '/spec/features/test.png')
+      click_button 'Upload'
+      expect(current_path).to eq '/pictures'
+    end
+  end
+end
