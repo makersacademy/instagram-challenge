95fc2adcd9a7443988f7b7014ca82e9667823d90
Oscar Barlow <oscar.barlow@gmail.com>
2017-01-14 17:40:05 +0000

    if a user deletes their account, their photos all go too

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook]
 
-  has_many :photos
+  has_many :photos, dependent: :destroy
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
