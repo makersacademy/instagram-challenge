b49fcebc7f9741db1dd381f83337bfeb92ac3f2a
Courtney Osborn <Courtney.lee.osborn@gmail.com>
2017-01-14 12:32:30 +0000

    choosing a picture

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,7 +1,17 @@
 class PicturesController < ApplicationController
 
   def index
-
+    @pictures = Picture.all
   end
-  
+
+  def new
+    @picture = Picture.new
+  end
+
+  private
+
+  def params_image
+    params.require(:picture).permit(:image)
+  end
+
 end
--- spec/features/pictures_feature_spec.rb
+++ spec/features/pictures_feature_spec.rb
@@ -1,12 +1,32 @@
 require 'rails_helper'
 
   feature "pictures" do
-    context "viewing pictures" do
+    context "viewing no pictures" do
       scenario "no pictures have been add and therefore no pictures are currently being displayed" do
 
       visit("/pictures")
       expect(page).to have_content("No pictures to display")
       expect(page).to have_link("Add a picture")
-    end 
+    end
   end
+
+  # context "viewing a picture" do
+  #   Picture.create(image: )
+  #   scenario "I want to display picture" do
+  #     visit("/pictures")
+  # end
+
+  context "adding pictures" do
+  
+    scenario "I to to be able to add a picture" do
+      visit("/pictures")
+      click_link("Add a picture")
+
+
+      expect(page).to have_content("Image")
+      expect(current_path).to eq("/pictures/new")
+    end
+  end
+
+
 end
