ac1c5de6b2857311ede185e573b268eb1547be65
Pea M. Tyczynska <CrystalPea@github.com>
2017-01-14 19:51:51 +0000

    Pictures can be displayed now

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170114161050) do
+ActiveRecord::Schema.define(version: 20170114194456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pictures", force: :cascade do |t|
+    t.string   "filepath"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
--- spec/features/pictures_feature_spec.rb
+++ spec/features/pictures_feature_spec.rb
@@ -8,4 +8,15 @@ feature "pictures" do
         expect(page).to have_link("Add a picture")
     end
   end
+
+  context "pictures have been added" do
+    before do
+      Picture.create(filepath: "/app/assets/images/zagatka.jpg", description: "Friendship is magic")
+    end
+    scenario "display pictures" do
+      visit '/pictures'
+      expect(page).to have_content("Friendship is magic")
+      expect(page).not_to have_content("No pictures yet")
+    end
+  end
 end
