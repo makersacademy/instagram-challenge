230155c3dc9202050e15415359ad975ffcadbce4
George Eaton <george_eaton@hotmail.co.uk>
2017-01-14 17:18:31 +0000

    users can post pictures

--- instagram/app/controllers/photos_controller.rb
+++ instagram/app/controllers/photos_controller.rb
@@ -2,4 +2,19 @@ class PhotosController < ApplicationController
   def index
     @photos = Photo.all
   end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.create(photo_params)
+    redirect_to '/photos'
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:image, :caption)
+  end
 end
--- instagram/spec/features/photo_feature_spec.rb
+++ instagram/spec/features/photo_feature_spec.rb
@@ -15,8 +15,21 @@ feature 'photos' do
 
     scenario 'display pictures' do
       visit '/photos'
-      expect(page).to have_content('My Lunch')
+      expect(page).to have_content 'My Lunch'
       expect(page).not_to have_content 'No one has posted any photos yet...'
+    end
+  end
+
+  context 'posting photos' do
+    scenario 'prompts user to fill in caption, add a photo, then displays the photo' do
+      visit '/photos'
+      click_link 'Post a photo'
+      attach_file("photo[image]", "spec/pictures/my_lunch.jpg")
+      fill_in 'Caption', with: 'My Lunch'
+      click_button 'Post'
+      expect(page).to have_css("img[src*='my_lunch']")
+      expect(page).to have_content 'My Lunch'
+      expect(current_path).to eq photos_path
     end
   end
 end
