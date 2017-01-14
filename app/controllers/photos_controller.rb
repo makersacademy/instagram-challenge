c207448bc787b0ddc63ec05b6b75314155ea8320
Oscar Barlow <oscar.barlow@gmail.com>
2017-01-14 12:39:03 +0000

    give the user a form to create a photo in the db

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,4 +4,25 @@ class PhotosController < ApplicationController
     @photos = Photo.all
   end
 
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+
+    if @photo.save
+      redirect_to photos_path
+    else
+      render :new
+    end
+
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:name)
+  end
+
 end
--- spec/features/photos_feature_spec.rb
+++ spec/features/photos_feature_spec.rb
@@ -9,11 +9,9 @@ feature 'photos' do
       expect(page).to have_content 'No photos yet'
       expect(page).to have_content 'Add a photo'
     end
-
   end
 
   context 'photos have been added' do
-
     before do
       Photo.create(name: 'Posh Coffee')
     end
@@ -23,7 +21,18 @@ feature 'photos' do
       expect(page).to have_content 'Posh Coffee'
       expect(page).to_not have_content 'No photos yet'
     end
+  end
 
+  context 'creating photos' do
+
+    it 'should show a form that allows a user to enter a photo' do
+      visit '/photos'
+      click_link('Add a photo')
+      fill_in 'Name', with: 'My adorable cat'
+      click_button('Create Photo')
+      expect(page).to have_content 'My adorable cat'
+      expect(current_path).to eq '/photos'
+    end
   end
 
 end
