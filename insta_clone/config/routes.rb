c4ba2f2571b5c2a228f0d72ca4655f9a9e533915
Chris Cooper <cjcooper252@gmail.com>
2017-01-14 09:03:05 +0000

    sets up rails project

---
+++ insta_clone/config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "pictures#index"
+
+  resources :pictures
+
+
+
+end
