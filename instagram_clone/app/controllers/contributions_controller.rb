b1a6c846d3f430f2f0ac0a67a9bba7a6d587aee9
Asuka Ochi <asukaochi@gmail.com>
2017-01-14 20:37:35 +0000

    display contributions with only comment

--- instagram_clone/app/controllers/contributions_controller.rb
+++ instagram_clone/app/controllers/contributions_controller.rb
@@ -1,4 +1,9 @@
 class ContributionsController < ApplicationController
   def index
+    @all_contributions = Contribution.all
   end
+
+  def new
+  end
+  
 end
--- instagram_clone/spec/features/contributions_feature_spec.rb
+++ instagram_clone/spec/features/contributions_feature_spec.rb
@@ -8,4 +8,17 @@ feature 'contributions' do
       expect(page).to have_link 'Add a contribution'
     end
   end
+
+  context 'contributions have been added' do
+    before do
+      Contribution.create(comment: 'A black cat')
+      # image: '/Users/asukaochi/Downloads/IMG_20161220_101030.jpg',
+    end
+
+    scenario 'display contributions' do
+      visit '/contributions'
+      expect(page).to have_content 'A black cat'
+      expect(page).not_to have_content('No contributions yet')
+    end
+  end
 end
