1d160b6f8665ebb2516c2a467dad5035a3d3bedf
Dan Pelensky <dan@pelensky.com>
2017-01-14 13:56:30 +0000

    create method for photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,4 +2,19 @@ class PhotosController < ApplicationController
   def index
     @photos = Photo.all
   end
+
+  def new
+  end
+
+  def create
+    Photo.create(photos_params)
+    redirect_to '/photos'
+  end
+
+  private
+
+  def photos_params
+    params.require(:photo).permit(:description)
+  end
+
 end
--- spec/features/photos_feature_spec.rb
+++ spec/features/photos_feature_spec.rb
@@ -10,14 +10,25 @@ feature 'photos' do
   end
 
   context 'photos have been added' do
-  before do
-    Photo.create(description: 'A fun trip to the beach!')
+    before do
+      Photo.create(description: 'A fun trip to the beach!')
+    end
+
+    scenario 'display photos' do
+      visit '/photos'
+      expect(page).to have_content('A fun trip to the beach!')
+      expect(page).not_to have_content('No photos yet')
+    end
   end
 
-  scenario 'display restaurants' do
-    visit '/photos'
-    expect(page).to have_content('A fun trip to the beach!')
-    expect(page).not_to have_content('No photos yet')
+  context 'uploading a photo' do
+    scenario 'prompts user to fill in a description (for now), then desplays it' do
+      visit '/photos'
+      click_link 'Add a photo!'
+      fill_in 'Description', with: "A fun trip to the beach!"
+      click_button 'Upload photo'
+      expect(page).to have_content 'A fun trip to the beach!'
+      expect(current_path).to eq '/photos'
+    end
   end
-end
 end
