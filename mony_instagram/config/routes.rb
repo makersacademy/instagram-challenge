24a1b4c5cb8b53d638fc4fd142a0272e24e6f407
keomony khun <keomony@gmail.com>
2017-01-14 19:26:40 +0000

    adds user login and sign up with Devise

--- mony_instagram/config/routes.rb
+++ mony_instagram/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "home#index"
+  root "photos#index"
+  resources :photos
 end
