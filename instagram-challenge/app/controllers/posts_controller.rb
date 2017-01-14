65c9971f1d5e46ae77e828545c50a1ed43cad008
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2017-01-14 15:22:29 +0000

    Feature to add a post with description

--- instagram-challenge/app/controllers/posts_controller.rb
+++ instagram-challenge/app/controllers/posts_controller.rb
@@ -4,4 +4,19 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def new
+
+  end
+
+  def create
+    Post.create(permitted_params)
+    redirect_to posts_path
+  end
+
+  private
+
+  def permitted_params
+    params.require(:post).permit(:description)
+  end
+
 end
--- instagram-challenge/spec/features/posts_feature_spec.rb
+++ instagram-challenge/spec/features/posts_feature_spec.rb
@@ -7,11 +7,20 @@ feature 'Posts' do
     expect(page).to have_content "Post a new photo"
   end
 
-  scenario 'Create a post' do
+  scenario 'View posts that have been created' do
     post = Post.create(description: "Loving life")
     visit '/posts'
     expect(page).to have_content "Loving life"
     expect(page).not_to have_content "No Posts Yet"
   end
-  
+
+  scenario 'Post a new photo' do
+    visit '/posts'
+    click_link "Post a new photo"
+    fill_in "Description", with: "Loving life"
+    click_button "Create Post"
+    expect(current_path).to eq '/posts'
+    expect(page).to have_content "Loving life"
+  end
+
 end
