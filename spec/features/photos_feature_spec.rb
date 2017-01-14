2f86ae22b63e9966cffa90ab36f63db41fb5b358
Dan Pelensky <dan@pelensky.com>
2017-01-14 13:40:58 +0000

    photo model created - only allows description for now

--- spec/features/photos_feature_spec.rb
+++ spec/features/photos_feature_spec.rb
@@ -5,7 +5,19 @@ feature 'photos' do
     scenario 'should display a prompt to add a photo' do
       visit '/photos'
       expect(page).to have_content 'No photos yet'
-      expect(page).to have_link 'Add your first photo!'
+      expect(page).to have_link 'Add a photo!'
     end
   end
+
+  context 'photos have been added' do
+  before do
+    Photo.create(description: 'A fun trip to the beach!')
+  end
+
+  scenario 'display restaurants' do
+    visit '/photos'
+    expect(page).to have_content('A fun trip to the beach!')
+    expect(page).not_to have_content('No photos yet')
+  end
+end
 end
