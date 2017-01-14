99cd573075ed13c9a411d15935073bd68d11d331
Pea M. Tyczynska <CrystalPea@github.com>
2017-01-14 17:52:51 +0000

    Basic page with no pictures and prompt to ad done developed

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
 
-
-
   devise_scope :user do
     root to: "devise/sessions#new"
   end
+
+  # get 'pictures' => 'picture#index'
+  resources :pictures
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
