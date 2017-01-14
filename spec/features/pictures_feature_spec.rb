7265aaec594ae839ec7586c8bd8a40f5001f6df0
ajbater <amanda22@hotmail.co.uk>
2017-01-14 14:22:08 +0000

    validates presence of image

--- spec/features/pictures_feature_spec.rb
+++ spec/features/pictures_feature_spec.rb
@@ -11,7 +11,11 @@ feature 'restaurants' do
 
   context 'pictures have been added' do
     before do
-      Picture.create(caption: 'This is my cat')
+      visit '/pictures'
+      click_link 'Add a picture'
+      attach_file('picture_image', File.absolute_path('./public/cat.png'))
+      fill_in 'picture_caption', with: 'This is my cat'
+      click_button 'Create Picture'
     end
 
     scenario 'display pictures' do
