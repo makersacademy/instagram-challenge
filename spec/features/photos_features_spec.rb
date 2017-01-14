01759190cacc87ff56b522e07915f6e55d02bd8c
Mike Hurl <mikehurl@gmail.com>
2017-01-14 20:20:36 +0000

    second test passes: site displays titles for photos

--- spec/features/photos_features_spec.rb
+++ spec/features/photos_features_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 feature 'photos' do
+
   context 'no photos have been added' do
     scenario 'should display a prompt to add a photos' do
       visit '/photos'
@@ -8,4 +9,17 @@ feature 'photos' do
       expect(page).to have_link 'Add a photo'
     end
   end
+
+  context 'photos have been added' do
+    before do
+      Photo.create(title: 'Test photo')
+    end
+
+    scenario 'display photo titles' do
+      visit '/photos'
+      expect(page).to have_content 'Test photo'
+      expect(page).not_to have_content 'No photos yet'
+    end
+  end
+
 end
