6d6e79d3a446b68905c2bb67b3be74f13bc974ab
Mike Hurl <mikehurl@gmail.com>
2017-01-14 20:46:10 +0000

    third test passes: users can create new posts with the title of their photo

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,4 +4,19 @@ class PhotosController < ApplicationController
     @photos = Photo.all
   end
 
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.create(photo_params)
+    redirect_to '/photos'
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:title)
+  end
+
 end
--- spec/features/photos_features_spec.rb
+++ spec/features/photos_features_spec.rb
@@ -14,11 +14,21 @@ feature 'photos' do
     before do
       Photo.create(title: 'Test photo')
     end
-
     scenario 'display photo titles' do
       visit '/photos'
       expect(page).to have_content 'Test photo'
       expect(page).not_to have_content 'No photos yet'
+    end
+  end
+
+  context 'creating photo posts' do
+    scenario "prompts user to fill in a form, then displays photo's title" do
+      visit '/photos'
+      click_link 'Add a photo'
+      fill_in 'Title', with: 'Test photo'
+      click_button 'Create Photo'
+      expect(page).to have_content 'Test photo'
+      expect(current_path).to eq '/photos'
     end
   end
 
