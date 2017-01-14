979b918526ecc02d5f7fdad92ef13eb4598f85cc
Oscar Barlow <oscar.barlow@gmail.com>
2017-01-14 16:49:15 +0000

    user can login from facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root to: "photos#index"
   resources :photos
 end
