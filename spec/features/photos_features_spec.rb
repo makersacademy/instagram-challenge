34e89ec56a454f2b69ae69a0db9aeecb3722dd5f
Mike Hurl <mikehurl@gmail.com>
2017-01-15 17:31:17 +0000

    users must sign if they want to do more than view photo posts

--- spec/features/photos_features_spec.rb
+++ spec/features/photos_features_spec.rb
@@ -8,20 +8,36 @@ feature 'photos' do
       expect(page).to have_content 'No photos yet'
       expect(page).to have_link 'Add a photo'
     end
+
+    scenario 'user cannot add a photo post if not signed in' do
+      visit '/photos/new'
+      expect(current_path).to eq '/users/sign_in'
+    end
+
   end
 
   context 'photos have been added' do
     before do
       Photo.create(title: 'Test photo')
     end
+
     scenario 'display photo titles' do
       visit '/photos'
       expect(page).to have_content 'Test photo'
       expect(page).not_to have_content 'No photos yet'
     end
+
   end
 
   context 'creating photo posts' do
+    before do
+      visit '/users/sign_up'
+      fill_in 'Email', with: 'test@example.com'
+      fill_in 'Password', with: 'testtest'
+      fill_in 'Password confirmation', with: 'testtest'
+      click_button 'Sign up'
+    end
+
     scenario "prompts user to fill in a form, then displays photo's title" do
       visit '/photos'
       click_link 'Add a photo'
@@ -30,7 +46,9 @@ feature 'photos' do
       expect(page).to have_content 'Test photo'
       expect(current_path).to eq '/photos'
     end
+
     context 'an invalid photo post' do
+
       scenario 'does not let you submit a title that is too short' do
         visit '/photos'
         click_link 'Add a photo'
@@ -39,21 +57,35 @@ feature 'photos' do
         expect(page).not_to have_css 'h3', text: 'Te'
         expect(page).to have_content 'error'
       end
+
     end
+
   end
 
   context 'viewing individual photo posts' do
+
     let!(:test_photo){ Photo.create(title: 'Test photo') }
+
     scenario 'lets a user view a photo post' do
       visit '/photos'
       click_link 'Test photo'
       expect(page).to have_content 'Test photo'
       expect(current_path).to eq "/photos/#{test_photo.id}"
     end
+
   end
 
   context 'editing photo posts' do
-    before { Photo.create title: 'Test photo', description: 'This is a test', id: 1 }
+
+    before do
+      Photo.create(title: 'Test photo', description: 'This is a test', id: 1)
+      visit '/users/sign_up'
+      fill_in 'Email', with: 'test@example.com'
+      fill_in 'Password', with: 'testtest'
+      fill_in 'Password confirmation', with: 'testtest'
+      click_button 'Sign up'
+    end
+
     scenario 'let a user edit a photo post' do
       visit '/photos'
       click_link 'Edit Test photo'
@@ -65,16 +97,27 @@ feature 'photos' do
       expect(page).to have_content 'This is not a test'
       expect(current_path).to eq '/photos/1'
     end
+
   end
 
   context 'deleting photos' do
-    before { Photo.create title: 'Test photo', description: 'This is a test' }
+
+    before do
+      Photo.create(title: 'Test photo', description: 'This is a test')
+      visit '/users/sign_up'
+      fill_in 'Email', with: 'test@example.com'
+      fill_in 'Password', with: 'testtest'
+      fill_in 'Password confirmation', with: 'testtest'
+      click_button 'Sign up'
+    end
+
     scenario 'removes a photo when a user clicks a delete link' do
       visit '/photos'
       click_link 'Delete Test photo'
       expect(page).not_to have_content 'Test photo'
       expect(page).to have_content 'Photo deleted successfully'
     end
+
   end
 
 end
