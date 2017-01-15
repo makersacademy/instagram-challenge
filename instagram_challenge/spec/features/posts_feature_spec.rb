fba8d3c3e7d85acdbabd3a05e2dd9cd8de5ead27
Chris <chrixs99@hotmail.com>
2017-01-15 14:59:22 +0000

    initialization completed and first rspec test passing

---
+++ instagram_challenge/spec/features/posts_feature_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+feature 'posts' do
+
+  context 'no posts have been made' do
+    scenario 'should display a prompt to make a post' do
+      visit '/posts'
+      expect(page).to have_content 'No posts yet'
+      expect(page).to have_link 'Add a post'
+    end
+  end
+end
