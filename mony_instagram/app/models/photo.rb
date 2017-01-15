8441b05d73bfbe5867a5d2acca9e17f811ad4ece
keomony khun <keomony@gmail.com>
2017-01-15 23:15:12 +0000

    migrates add user ref. to photos

--- mony_instagram/app/models/photo.rb
+++ mony_instagram/app/models/photo.rb
@@ -1,3 +1,5 @@
 class Photo < ApplicationRecord
    mount_uploader :insta_photo, InstaPhotoUploader
+
+   belongs_to :user
 end
--- mony_instagram/db/schema.rb
+++ mony_instagram/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170115202537) do
+ActiveRecord::Schema.define(version: 20170115231200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20170115202537) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "insta_photo"
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_photos_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,4 +41,5 @@ ActiveRecord::Schema.define(version: 20170115202537) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "photos", "users"
 end
