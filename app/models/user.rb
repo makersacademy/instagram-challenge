fddcc30a26e1fc43882d53f0b160f692c44f1a52
ajbater <amanda22@hotmail.co.uk>
2017-01-15 11:28:16 +0000

    created associations - user has many comments, user has many pictures

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+
+  has_many :pictures
+  has_many :comments
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
