3d81e20f6707e1b0b216682a1b6dbef54351e998
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2017-01-15 19:36:58 +0000

    More messing about with aws config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
       s3_region: ENV.fetch('AWS_REGION'),
-      s3_host_name: "s3-website-us-east-1.amazonaws.com",
+      s3_host_name: "s3.amazonaws.com",
       url: ':s3_domain_url'
     }
   }
