57d2609e541d8870ef460f36f4701b9b683b3a35
Simon <simon.conway@condaal.com>
2017-01-15 08:58:44 +0000

    Added a restaurant model and migration

--- spec/features/image_feature_spec.rb
+++ spec/features/image_feature_spec.rb
@@ -8,4 +8,17 @@ feature 'photos' do
       expect(page).to have_link 'Add a photo'
     end
   end
+
+  context 'After a photo is added the user should see the photo' do
+
+    before do
+      Photo.create(name: 'Beach holiday')
+    end
+
+    scenario 'Add a photo and the title should appear on the page' do
+      visit '/photos'
+      expect(page).to have_content('Beach holiday')
+      expect(page).not_to have_content('No photos uploaded yet')
+    end
+  end
 end
