b74a60f2d9434112667b137ba03ead6fbdae6038
DarkChocLatte <darkchoclatte@gmail.com>
2017-01-15 14:41:53 +0000

    created empty CRUD methods in Photos controller

--- instagram_clone/app/controllers/photos_controller.rb
+++ instagram_clone/app/controllers/photos_controller.rb
@@ -1,2 +1,22 @@
 class PhotosController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
