0e4ed12644da1b3ffde00739692df14513e0f304
TudorTacal <tudortacal@gmail.com>
2017-01-15 15:08:31 +0000

    Initialized AWS. Pictures are being stored on S3

---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,3 @@
+# Paperclip::Attachment.default_options[:url] = 'instagramtudor.s3.amazonaws.com'
+# Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+# Paperclip::Attachment.default_options[:s3_host_name] = 's3-eu-west-2.amazonaws.com'
