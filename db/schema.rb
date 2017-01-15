da1e41423ab9fd7053901d5e49e09ff7ebfbae46
Dan Pelensky <dan@pelensky.com>
2017-01-15 15:11:18 +0000

    comment controller created

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170115132156) do
+ActiveRecord::Schema.define(version: 20170115151043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "remarks"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "photos", force: :cascade do |t|
     t.string   "description"
@@ -31,6 +37,12 @@ ActiveRecord::Schema.define(version: 20170115132156) do
     t.datetime "updated_at", null: false
     t.integer  "photo_id"
     t.index ["photo_id"], name: "index_remarks_on_photo_id", using: :btree
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text     "remarks"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
