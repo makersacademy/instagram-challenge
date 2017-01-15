7142f4648b6edcc3ba44ed179bcb0b27789946b4
Dan Pelensky <dan@pelensky.com>
2017-01-15 15:22:07 +0000

    comments display

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,4 +3,14 @@ class CommentsController < ApplicationController
     @photo = Photo.find(params[:photo_id])
     @comment = Comment.new
   end
+
+  def create
+    @photo = Photo.find(params[:photo_id])
+    @photo.comments.create(comment_params)
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:remarks)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170115151043) do
+ActiveRecord::Schema.define(version: 20170115151655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20170115151043) do
     t.text     "remarks"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "photo_id"
+    t.index ["photo_id"], name: "index_comments_on_photo_id", using: :btree
   end
 
   create_table "photos", force: :cascade do |t|
@@ -64,5 +66,6 @@ ActiveRecord::Schema.define(version: 20170115151043) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "comments", "photos"
   add_foreign_key "remarks", "photos"
 end
