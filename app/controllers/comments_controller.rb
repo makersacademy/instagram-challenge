962417c1bd2d6fa8d64ab0fa5015c59a914a6d6e
Dan Pelensky <dan@pelensky.com>
2017-01-15 15:25:58 +0000

    comment tests pass

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,7 @@ class CommentsController < ApplicationController
   def create
     @photo = Photo.find(params[:photo_id])
     @photo.comments.create(comment_params)
+    redirect_to photo_path(@photo)
   end
 
   private
