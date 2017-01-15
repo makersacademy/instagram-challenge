6a09fc0cdb00832f98bbf55a1f2955e5a3bdae3e
Asuka Ochi <asukaochi@gmail.com>
2017-01-15 22:54:40 +0000

    change format

--- instagram_clone/app/models/contribution.rb
+++ instagram_clone/app/models/contribution.rb
@@ -4,6 +4,6 @@ class Contribution < ApplicationRecord
   has_many :likes, dependent: :destroy
   belongs_to :user
 
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :styles => { :medium => "300>x300", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
