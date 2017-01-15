60b435a84d947bce8c3e9af912f94d1e5b56de29
Chris <chrixs99@hotmail.com>
2017-01-15 15:11:19 +0000

    users can add a post

--- instagram_challenge/spec/features/posts_feature_spec.rb
+++ instagram_challenge/spec/features/posts_feature_spec.rb
@@ -9,4 +9,16 @@ feature 'posts' do
       expect(page).to have_link 'Add a post'
     end
   end
+
+  context 'posts have been added' do
+    before do
+      Post.create(caption: 'The snow looks great')
+    end
+
+    scenario 'display restaurants' do
+      visit '/posts'
+      expect(page).to have_content('The snow looks great')
+      expect(page).not_to have_content('No posts yet')
+    end
+  end
 end
