48b7ca7390346c7e78e6893d1b7fb338cbba096d
Asuka Ochi <asukaochi@gmail.com>
2017-01-15 01:42:37 +0000

    add Users

--- instagram_clone/config/routes.rb
+++ instagram_clone/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  devise_for :users
+  root to: "contributions#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'contributions' => 'contributions#index'
 
