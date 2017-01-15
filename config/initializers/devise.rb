c484f33d8ae028a0b63e0ac5fd3bdd7e7488d85d
Louisa <louisaspicer@gmail.com>
2017-01-15 17:36:53 +0000

    testing heroku facebook login

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,8 +14,7 @@ Devise.setup do |config|
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
 
-  config.omniauth :facebook, ENV["FB_APP_ID"], ENV["FB_APP_SECRET"],
-    { :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
+  config.omniauth :facebook, ENV["FB_APP_ID"], ENV["FB_APP_SECRET"]
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
