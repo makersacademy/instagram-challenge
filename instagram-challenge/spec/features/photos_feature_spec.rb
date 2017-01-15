1548b77a5133dfde1d86166ea63280a158c74cf3
ejatkin <ejatkin@gmail.com>
2017-01-15 12:12:57 +0000

    Second test to add and display photos, all passing

--- instagram-challenge/spec/features/photos_feature_spec.rb
+++ instagram-challenge/spec/features/photos_feature_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 feature 'photos' do
+
   context 'no photos have been added' do
     scenario 'should prompt user to add a photo' do
       visit '/photos'
@@ -8,4 +9,17 @@ feature 'photos' do
       expect(page).to have_content 'Add a photo'
     end
   end
+
+  context 'photos have been added' do
+    before do
+      Photo.create(caption: 'My cat is fluffy')
+    end
+
+    scenario 'display photos' do
+      visit '/photos'
+      expect(page).to have_content('My cat is fluffy')
+      expect(page).not_to have_content('No photos yet')
+    end
+  end
+
 end
