9b17e9ec8c8e2b65451cf944407b828b16aabb1f
Leke Abolade <leke_abolade92@hotmail.co.uk>
2017-01-15 21:12:25 +0000

    adding greeting and edit profile to navigation

--- spec/features/users_feature_spec.rb
+++ spec/features/users_feature_spec.rb
@@ -11,6 +11,7 @@ feature 'User can sign in and out' do
     it 'should not see a \'sign out\' link' do
       visit('/')
       expect(page).not_to have_link('Sign out')
+      expect(page).not_to have_content('Welcome')
     end
   end
 
@@ -27,12 +28,40 @@ feature 'User can sign in and out' do
     it 'should see \'sign out\' link' do
       visit('/')
       expect(page).to have_link('Sign out')
+      expect(page).to have_content('Welcome test@example.com')
     end
 
     it 'should not see a \'sign in \' link and a \'sign up\' link' do
       visit('/')
       expect(page).not_to have_link('Sign in')
       expect(page).not_to have_link('Sign up')
+    end
+  end
+end
+
+feature 'User can edit their profile' do
+  context 'User not signed in and on the home page' do
+
+    it 'should not see a \'Edit profile\' link' do
+      visit('/')
+      expect(page).not_to have_link('Edit profile')
+    end
+
+  end
+
+  context 'User signed in and on the home page' do
+    before do
+      visit('/')
+      click_link('Sign up')
+      fill_in('Email', with: 'test@example.com')
+      fill_in('Password', with: 'testtest')
+      fill_in('Password confirmation', with: 'testtest')
+      click_button('Sign up')
+    end
+
+    it 'should see \'Edit profile\' link' do
+      visit('/')
+      expect(page).to have_link('Edit profile')
     end
   end
 end
