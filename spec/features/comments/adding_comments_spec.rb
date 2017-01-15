2f31fde47f2788bc6f11dd7d7a9c3c1361abee25
Russell Yeo <rusyeo@gmail.com>
2017-01-15 21:00:21 +0000

    must be logged in to comment on posts

--- spec/features/comments/adding_comments_spec.rb
+++ spec/features/comments/adding_comments_spec.rb
@@ -3,17 +3,33 @@ require 'rails_helper.rb'
 feature 'Adding comments' do
 
   let(:user) { create(:user) }
-  
+
+
   before do
     login_as(user, :scope => :user)
     new_post = create(:post, caption: "I'm gonna eat this burrito!", user_id: user.id)
   end
 
-  scenario 'adding a comment to a post' do
-    visit '/posts/1'
-    fill_in 'comment_text', with: "Holy shit, that looks delicious!"
-    click_button 'save'
-    expect(page).to have_content "Holy shit, that looks delicious!"
+  context 'logged in' do
+    scenario 'adding a comment to a post' do
+      visit '/posts/1'
+      fill_in 'comment_text', with: "Holy shit, that looks delicious!"
+      click_button 'save'
+      expect(page).to have_content "Holy shit, that looks delicious!"
+    end
+  end
+
+  context 'not logged in' do
+    before do
+      logout(:user)
+    end
+    scenario 'adding a comment to a post' do
+      visit '/posts/1'
+      fill_in 'comment_text', with: "Holy shit, that looks delicious!"
+      click_button 'save'
+      expect(page).to have_current_path '/users/sign_in'
+      expect(page).to have_content "You need to sign in or sign up before continuing."
+    end
   end
 
 end
