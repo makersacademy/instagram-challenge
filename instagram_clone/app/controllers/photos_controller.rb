5509ace1f1f2792a3cc403ddf4c801d521c93bde
DarkChocLatte <darkchoclatte@gmail.com>
2017-01-15 18:18:31 +0000

    created new form, updated photo controllers create action

--- instagram_clone/app/controllers/photos_controller.rb
+++ instagram_clone/app/controllers/photos_controller.rb
@@ -3,9 +3,18 @@ class PhotosController < ApplicationController
   end
 
   def new
+    @photo = Photo.new
   end
 
   def create
+    @photo = Photo.new(photo_params)
+
+    if @photo.save
+      redirect_to @photo, notice: "Photo submitted"
+    else
+      render 'new'
+    end
+
   end
 
   def show
@@ -18,5 +27,11 @@ class PhotosController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:title, :description)
   end
 end
---
+++ instagram_clone/spec/features/_form_feature_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+feature '_form' do
+
+  context 'When a user naviagtes to the new page' do
+    scenario 'Page should have Title field' do
+      visit('/pins/new')
+      expect(page).to have_field('Title')
+    end
+
+    scenario 'Page should have Description field' do
+      visit('/pins/new')
+      expect(page).to have_field('Description')
+    end
+
+    scenario 'Page should have Create Photo button' do
+      visit('/pins/new')
+      expect(page).to have_button('Create Photo')
+    end
+  end
+
+  context 'When a user inputs a correct title and description' do
+    scenario 'Should redirect page once user submits form' do
+      create_a_pin
+      expect(current_path).not_to eq('/photos/new')
+    end
+  end
+end
