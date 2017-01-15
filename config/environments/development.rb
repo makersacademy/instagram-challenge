ce998619db983288c98ee2dee62093970de77ef4
Simon <simon.conway@condaal.com>
2017-01-15 15:53:20 +0000

    Added paperclip and added an image to the photo model

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,8 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  Paperclip.options[:command_path] = "/usr/local/bin/"
+
   # Show full error reports.
   config.consider_all_requests_local = true
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170115145738) do
+ActiveRecord::Schema.define(version: 20170115155056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "photos", force: :cascade do |t|
     t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
