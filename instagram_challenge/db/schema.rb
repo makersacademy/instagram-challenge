824e112ce8297b05afa703b2105b5231cf473c4e
Chris <chrixs99@hotmail.com>
2017-01-15 16:10:06 +0000

    can leave comments on a post

--- instagram_challenge/db/schema.rb
+++ instagram_challenge/db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170115150524) do
+ActiveRecord::Schema.define(version: 20170115160207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "post_id"
+    t.index ["post_id"], name: "index_comments_on_post_id", using: :btree
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "caption"
@@ -22,4 +30,5 @@ ActiveRecord::Schema.define(version: 20170115150524) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
 end
