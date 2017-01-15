1a36411aaa66a72628cdc50026f5b89f43b3bcb8
DarkChocLatte <darkchoclatte@gmail.com>
2017-01-15 14:39:11 +0000

    photo model generated

---
+++ instagram_clone/db/migrate/20170115143838_create_photos.rb
@@ -0,0 +1,10 @@
+class CreatePhotos < ActiveRecord::Migration[5.0]
+  def change
+    create_table :photos do |t|
+      t.string :title
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
