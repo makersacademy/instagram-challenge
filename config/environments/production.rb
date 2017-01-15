257d366d996ef5af81562741a6f4e08dce266b70
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2017-01-15 18:56:09 +0000

    Updated region in config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,8 @@ Rails.application.configure do
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
       s3_region: ENV.fetch('AWS_REGION'),
+      # s3_host_name: 's3-us-west-2.amazonaws.com',
+      # url: ":s3_domain_url"
     }
   }
 
