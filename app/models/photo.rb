77a9461d6cd3f1be9d52de51fc8501438652f56c
Dan Pelensky <dan@pelensky.com>
2017-01-15 15:50:18 +0000

    dependencies set in model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,5 @@
 class Photo < ApplicationRecord
+  belongs_to :user
   has_attached_file :image, :styles => { :medium => "1080x1080>", :thumb => "300x300>" }, :default_url => "/images/:style/missing.png"
  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
  has_many :comments, dependent: :destroy
