5aff0d759ec21f0170ee68dacf0299a0f0f0a13d
Dan Pelensky <dan@pelensky.com>
2017-01-15 18:14:04 +0000

    likes controller generated

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "photos#index"
-  resources :photos do
-    resources :comments
+  resources :photos, shallow: true do
+    resources :comments, :likes
   end
 end
