91d35855d770685980c6c7c0f0f4742abe39471b
DarkChocLatte <darkchoclatte@gmail.com>
2017-01-15 22:46:09 +0000

    added image association with photo

--- instagram_clone/app/models/photo.rb
+++ instagram_clone/app/models/photo.rb
@@ -1,3 +1,6 @@
 class Photo < ApplicationRecord
   belongs_to :user
+
+  has_attached_file :image, styles: { medium: "300x300>" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
