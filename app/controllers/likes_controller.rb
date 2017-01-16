050a0f07d59e51c5710899c470ef56d82670e284
Chris Cooper <cjcooper252@gmail.com>
2017-01-16 12:03:42 +0000

    gets rid of instance vars where unneccesary

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,10 +2,10 @@ class LikesController < ApplicationController
 
   def create
     @picture = Picture.find(params[:picture_id])
-    @like = @picture.likes.build
-    @like.user = current_user
+    like = @picture.likes.build
+    like.user = current_user
 
-    if @like.save
+    if like.save
       render json: { new_like_count: @picture.likes.count }
     end
   end
