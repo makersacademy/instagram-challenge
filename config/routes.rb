bfe78626d21e140a2c7292a44fa8000e6e33a543
Oscar Barlow <oscar.barlow@gmail.com>
2017-01-16 11:55:04 +0000

    only create necessary routes for comments and likes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root to: "photos#index"
   resources :photos do
-    resources :comments
-    resources :likes
+    resources :comments, :only => [:new, :create, :destroy]
+    resources :likes, :only => [:new, :create, :destroy]
   end
 end
