fdb1e66ebab029794879cec477919968f46cb5ad
Oscar Barlow <oscar.barlow@gmail.com>
2017-01-16 11:16:49 +0000

    remove p debugging

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,7 +18,6 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    p params
     @photo = Photo.find(params[:photo_id])
     @comment = current_user.comments.find(params[:id])
     @comment.destroy
