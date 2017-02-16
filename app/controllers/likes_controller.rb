689372e54415ee904fddef72fee68b77def727f5
Amanda Bater <amandabater@Amandas-MacBook-Pro.local>
2017-02-16 14:17:51 +0000

    changes version of ruby in gemfile, deletes unused image files

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,10 @@
 class LikesController < ApplicationController
 
-  # def index
-  #   picture = Picture.find(params[:picture_id])
-  #   redirect_to picture_path(picture)
-  # end
-
   def create
     @picture = Picture.find(params[:picture_id])
     @picture.likes.create
+    # @like = @picture.likes.new
+    # @like.save
     render json: {new_like_count: @picture.likes.count}
   end
 end
