56bfd99e7e92a4e19f6aa698311e433203e0eca4
Amanda Bater <amandabater@Amandas-MacBook-Pro.local>
2017-02-16 14:32:59 +0000

    removes commented code

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,8 +3,6 @@ class LikesController < ApplicationController
   def create
     @picture = Picture.find(params[:picture_id])
     @picture.likes.create
-    # @like = @picture.likes.new
-    # @like.save
     render json: {new_like_count: @picture.likes.count}
   end
 end
