7de8f0313f564a22d1c20038bf3d36475aa58ee5
Kat Hicks <kmhicks92@gmail.com>
2017-02-25 19:06:05 +0000

    Added comments model and associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,5 @@ class Post < ApplicationRecord
 
   belongs_to :user
   has_many :likes
+  has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170225133811) do
+ActiveRecord::Schema.define(version: 20170225190508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "post_id"
+    t.integer  "user_id"
+    t.index ["post_id"], name: "index_comments_on_post_id", using: :btree
+    t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.index ["post_id"], name: "index_likes_on_post_id", using: :btree
+    t.index ["user_id"], name: "index_likes_on_user_id", using: :btree
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "caption"
@@ -24,6 +43,8 @@ ActiveRecord::Schema.define(version: 20170225133811) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.integer  "like_id"
+    t.index ["like_id"], name: "index_posts_on_like_id", using: :btree
     t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
 
@@ -44,5 +65,10 @@ ActiveRecord::Schema.define(version: 20170225133811) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
+  add_foreign_key "posts", "likes"
   add_foreign_key "posts", "users"
 end
