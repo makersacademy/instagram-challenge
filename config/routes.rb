4b53ed71276502f75ed8b36e70e2ddbb3dab2716
Kat Hicks <kmhicks92@gmail.com>
2017-02-25 13:16:19 +0000

    Added one-to-many relationship between user and posts

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :users do
+    resources :posts
+  end
+
   root to: "welcome#index"
 
 end
