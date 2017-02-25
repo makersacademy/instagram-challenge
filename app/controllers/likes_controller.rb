2864eb3e2981833101b2aff4d8d2d8ebb5a9f4fc
Kat Hicks <kmhicks92@gmail.com>
2017-02-25 23:16:51 +0000

    Added validation so that you cannot like your own post

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,11 @@ class LikesController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    Like.create(post_id: @post.id, user_id: current_user.id)
+    if @post.user_id != current_user.id
+      Like.create(post_id: @post.id, user_id: current_user.id)
+    else
+      flash[:alert] = "You cannot like your own post"
+    end
     redirect_to posts_path
   end
 
