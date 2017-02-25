8542f67af46bda9d03fd8ea1d5a263897765251d
Samir Gossain <samir.gossain@gmail.com>
2017-02-25 21:09:13 +0000

    Added Models and Views

---
+++ pineapplegram/app/controllers/pictures_controller.rb
@@ -0,0 +1,26 @@
+class PicturesController < ApplicationController
+
+  def index
+    @pictures = Picture.all
+  end
+
+  def show
+    @pictures = Picture.all
+  end
+
+  def new
+    @pictures = Picture.new
+  end
+
+  def create
+    Picture.create(picture_params)
+    redirect_to '/pictures'
+  end
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:name)
+  end
+
+end
--- pineapplegram/db/schema.rb
+++ pineapplegram/db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170225181706) do
+ActiveRecord::Schema.define(version: 20170225183715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pictures", force: :cascade do |t|
+    t.string   "description"
+    t.integer  "pineapples"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
