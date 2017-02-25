66d9875d9f4f5c76e89a56ff756d501726a50d05
Sam Jones <sam.jones494@googlemail.com>
2017-02-25 16:57:24 +0000

    set homepage to login when no user signed in

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    authenticated :user do
+      root 'posts#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
   resources :posts do
     resources :comments
   end
 
-  root to: "posts#index"
+
 
 end
