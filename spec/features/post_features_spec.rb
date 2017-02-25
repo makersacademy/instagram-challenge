f3893f35c8767a285778e68d11b5dcca51555c40
Tamar Lehmann <tlehmann@hotmail.co.uk>
2017-02-25 15:18:13 +0000

    Generated specs and added first two failing tests for when no posts exist, added posts as resource to routes

---
+++ spec/features/post_features_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+feature 'posts' do
+  context 'no posts have been added' do
+    scenario 'should display a link to add a post' do
+      visit '/posts'
+      expect(page).to have_link 'Create a post'
+    end
+
+    scenario 'display a message that there are no posts' do
+      visit '/posts'
+      expect(page).to have_content 'There are no posts yet!'
+    end
+  end
+end
