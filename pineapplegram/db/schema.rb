9d3f06f7c2d49059b5f2a12bfe569cc04af50e85
Samir Gossain <samir.gossain@gmail.com>
2017-02-25 22:18:24 +0000

    Added Pictures

--- pineapplegram/db/schema.rb
+++ pineapplegram/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170225183715) do
+ActiveRecord::Schema.define(version: 20170225221653) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,8 +18,12 @@ ActiveRecord::Schema.define(version: 20170225183715) do
   create_table "pictures", force: :cascade do |t|
     t.string   "description"
     t.integer  "pineapples"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
