3cff6bed17a97393581e6b209930bbba5e7f1b8c
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2017-02-25 14:55:27 +0000

    Starting to do CSS

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,12 @@ require 'rspec/rails'
 require 'capybara/rails'
 require 'capybara/poltergeist'
 Capybara.javascript_driver = :poltergeist
+
+options = {js_errors: false}
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, options)
+end
+
 require 'support/database_cleaner'
 
 # Add additional requires below this line. Rails is not loaded until this point!
