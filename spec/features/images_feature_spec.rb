f5b89407b932e2908833a3f5e60c6f163438d927
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2017-02-25 08:40:36 +0000

    Added images model

--- spec/features/images_feature_spec.rb
+++ spec/features/images_feature_spec.rb
@@ -8,4 +8,16 @@ feature 'images' do
       expect(page).to have_link 'Add am image'
     end
   end
+
+  context 'restaurants have been added' do
+    before do
+      Images.create()
+    end
+
+    scenario 'display images' do
+      visit '/images'
+      expect(page).not_to have_content('No images yet')
+    end
+  end
+
 end
