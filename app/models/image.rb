57014ff59232ecef87363316311cfa13ae7984c0
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2017-02-25 13:00:04 +0000

    Added missing.png file

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,6 +3,6 @@ class Image < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/img/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
