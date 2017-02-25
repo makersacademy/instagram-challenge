6a6d9226edce8571420708d00eb795f90f07f53c
Tamar Lehmann <tlehmann@hotmail.co.uk>
2017-02-25 16:27:00 +0000

    Added form and create/new routes. Tests passing for adding a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,22 @@
 class PostsController < ApplicationController
 
   def index
+    @posts = Post.all
   end
-  
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    redirect_to '/posts'
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:description)
+  end
+
 end
