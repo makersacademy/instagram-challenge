1d88e3c9c30ed313006d7dbc264a1f7e0fb6e85c
Rob Brentnall <rob.brentnall@gmail.com>
2017-02-25 13:08:11 +0000

    devise setup complete

--- instasham/app/assets/config/manifest.js
+++ instasham/app/assets/config/manifest.js
@@ -1,3 +1,4 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
+// require devise_bootstrap_views;
--- instasham/spec/features/user_features_spec.rb
+++ instasham/spec/features/user_features_spec.rb
@@ -1,5 +1,36 @@
-# require 'rails_helper'
-#
-# feature 'User' do
-#
-# end
+feature "User can Login and out" do
+  context "user not signed in and on the homepage" do
+    it "should see a 'Login' link and a 'sign up' link" do
+      visit('/')
+      expect(page).to have_link('Login')
+      expect(page).to have_link('Sign up')
+    end
+
+    it "should not see 'Logout' link" do
+      visit('/')
+      expect(page).not_to have_link('Logout')
+    end
+  end
+
+  context "user signed in on the homepage" do
+    before do
+      visit('/')
+      click_link('Sign up')
+      fill_in('Email', with: 'test@example.com')
+      fill_in('Password', with: 'apassword')
+      fill_in('Password confirmation', with: 'apassword')
+      click_button('Sign up')
+    end
+
+    it "should see 'Logout' link" do
+      visit('/')
+      expect(page).to have_link('Logout')
+    end
+
+    it "should not see a 'Login' link and a 'sign up' link" do
+      visit('/')
+      expect(page).not_to have_link('Login')
+      expect(page).not_to have_link('Sign up')
+    end
+  end
+end
