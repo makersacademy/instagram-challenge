faf677cf2060c21ffedcf8ef4808c051029a847b
Gurminder Bhachu <gurminderbhachu@hotmail.com>
2017-02-26 12:05:50 +0000

    Photo loads in post

--- insta_clone/app/controllers/posts_controller.rb
+++ insta_clone/app/controllers/posts_controller.rb
@@ -1,26 +1,28 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.all
+    def index
+        @posts = Post.all
+    end
+
+    def new
+        @post = Post.new
+   end
+
+    def create
+        @post = Post.new(post_params)
+        if @post.save
+            redirect_to posts_path
+        else
+            render 'new'
+        end
+    end
+
+    def show
+        @post = Post.find(params[:id])
+   end
+
+    private
+
+    def post_params
+        params[:post].permit(:name, :image)
+   end
   end
-
-  def new
-    @post = Post.new
-  end
-
-  def create
-    @post = Post.create(post_params)
-    redirect_to '/posts'
-  end
-
-  def show
-    @post = Post.find(params[:id])
-  end
-
-
-private
-
-def post_params
-      params[:post].permit(:name, :image)
- end
-
-end
