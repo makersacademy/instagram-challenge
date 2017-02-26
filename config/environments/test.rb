4b33e73b0ae27b6bc764c7e649cc31500adf9de1
Matthew Perry <thesedatedprince@gmail.com>
2017-02-26 21:18:32 +0000

    another travis encryption attempt 3 - tried adding keys in CLI

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -9,6 +9,7 @@ Rails.application.configure do
     :region => ENV['AWS_REGION']
     }
   }
+  
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
