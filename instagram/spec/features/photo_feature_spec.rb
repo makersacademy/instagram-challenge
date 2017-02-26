5420c97a889f3118b71d13c1ffde9cbe4f72a8da
konradschlatte <konrad.schlatte@btinternet.com>
2017-02-26 12:36:46 +0000

    display added

--- instagram/spec/features/photo_feature_spec.rb
+++ instagram/spec/features/photo_feature_spec.rb
@@ -8,4 +8,14 @@ feature 'photos' do
       expect(page).to have_link 'Add a photo'
     end
   end
+  context 'photos have been added' do
+    before do
+    Photo.create(comment: 'cool stuff')
+  end
+    scenario 'display photos' do
+    visit '/photos'
+    expect(page).to have_content('cool stuff')
+    expect(page).not_to have_content('No photos yet')
+  end
+end
 end
