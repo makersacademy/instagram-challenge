a3266e3294cea5b9006622852a9c2814dbd63b41
Stefan Liute <stefanliute@gmail.com>
2017-02-26 16:46:56 +0000

    Individual post pages

--- spec/factories/post.rb
+++ spec/factories/post.rb
@@ -4,3 +4,9 @@ FactoryGirl.define do
     image Rack::Test::UploadedFile.new(Rails.root + 'spec/files/images/sample_02.jpg', 'image/jpg')
   end
 end
+
+# FactoryGirl.define do
+#   factory :user do
+#     avatar { File.new("#{Rails.root}/spec/support/fixtures/image.jpg") }
+#   end
+# end
--- spec/features/display_posts_spec.rb
+++ spec/features/display_posts_spec.rb
@@ -1,12 +1,20 @@
 require 'rails_helper'
 
 feature 'Displaying posts works' do
-  scenario 'via an index on the homepage' do
+  scenario 'via index on the homepage' do
     post_one = create(:post, caption: "My first post")
     post_two = create(:post, caption: "And my second")
     visit '/'
     expect(page).to have_content("My first post")
     expect(page).to have_content("And my second")
     expect(page).to have_css("img[src*='sample_02']")
+  end
+
+  scenario 'or via individual post pages linked from index' do
+    post = create(:post)
+    visit '/'
+    # find(:xpath, "//a[contains(@href,'posts/1')]").click
+    find(:xpath, "//a/img[@alt='Sample 02']/..").click
+    expect(page.current_path).to eq(post_path(post))
   end
 end
