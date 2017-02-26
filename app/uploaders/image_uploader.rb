530120392ec51a03fd7daebe3830d67fd1c71f44
Rob Brentnall <rob.brentnall@gmail.com>
2017-02-26 21:00:11 +0000

    removed image helper file

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,9 +1,9 @@
 class ImageUploader < CarrierWave::Uploader::Base
-
+  require_relative 'image_helpers'
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-  include ApplicationHelper
+  include ImageHelper
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -13,7 +13,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     # "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{processors}"
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}"
   end
 
   def cache_dir
@@ -40,26 +40,32 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   version :None do
     process :quality => 70
+    process resize_to_fit: [640, 640]
   end
 
   version :Toaster do
     process :toaster_filter
+    process resize_to_fit: [640, 640]
   end
 
   version :Lomo do
     process :lomo_filter
+    process resize_to_fit: [640, 640]
   end
 
   version :Kelvin do
     process :kelvin_filter
+    process resize_to_fit: [640, 640]
   end
 
   version :Colortone do
     process :colortone_filter
+    process resize_to_fit: [640, 640]
   end
 
   version :Gotham do
     process :gotham_filter
+    process resize_to_fit: [640, 640]
   end
 
   # version :strip do
@@ -85,5 +91,148 @@ class ImageUploader < CarrierWave::Uploader::Base
   def filename
     "image.jpg" if original_filename
   end
+
+  def toaster_filter
+    manipulate! do |img|
+      img.modulate '150,80,100'
+      img.gamma 1.1
+      img.contrast
+      img.contrast
+      img.contrast
+      img.contrast
+
+      img = yield(img) if block_given?
+      img
+    end
+  end
+
+  def lomo_filter
+    manipulate! do |img|
+      img.channel 'R'
+      img.level '22%'
+      img.channel 'G'
+      img.level '22%'
+
+      img = yield(img) if block_given?
+      img
+    end
+  end
+
+  def kelvin_filter
+    manipulate! do |img|
+      cols, rows = img[:dimensions]
+
+      img.combine_options do |cmd|
+        cmd.auto_gamma
+        cmd.modulate '120,50,100'
+      end
+
+      new_img = img.clone
+      new_img.combine_options do |cmd|
+        cmd.fill 'rgba(255,153,0,0.5)'
+        cmd.draw "rectangle 0,0 #{cols},#{rows}"
+      end
+
+      img = img.composite new_img do |cmd|
+        cmd.compose 'multiply'
+      end
+
+      img = yield(img) if block_given?
+      img
+    end
+  end
+
+  def colortone_filter(color = '#222b6d', level = 100, type = 0)
+    manipulate! do |img|
+      color_img = img.clone
+      color_img.combine_options do |cmd|
+        cmd.fill color
+        cmd.colorize '63%'
+      end
+
+      img = img.composite color_img do |cmd|
+        cmd.compose 'blend'
+        cmd.define "compose:args=#{level},#{100-level}"
+      end
+
+      img = yield(img) if block_given?
+      img
+    end
+  end
+
+  def gotham_filter
+    manipulate! do |img|
+      img.modulate '120,10,100'
+      img.fill '#222b6d'
+      img.colorize 20
+      img.gamma 0.5
+      img.contrast
+
+      img = yield(img) if block_given?
+      img
+    end
+  end
+
+  # def strip
+  #   manipulate! do |img|
+  #     img.strip
+  #
+  #     img = yield(img) if block_given?
+  #     img
+  #   end
+  # end
+
+  def quality(percentage)
+    manipulate! do |img|
+      img.quality(percentage)
+
+      img = yield(img) if block_given?
+      img
+    end
+  end
+
+  # def vignette(path_to_file)
+  #   manipulate! do |img|
+  #     cols, rows = img[:dimensions]
+  #
+  #     vignette_img = ::MiniMagick::Image.open(path_to_file)
+  #     vignette_img.size "#{cols}x#{rows}"
+  #
+  #     img = img.composite(vignette_img) do |cmd|
+  #       cmd.gravity 'center'
+  #       cmd.compose 'multiply'
+  #     end
+  #
+  #     img = yield(img) if block_given?
+  #     img
+  #   end
+  # end
+  #
+  # def pad_it_up(width, height, background=:transparent, gravity='Center')
+  #   manipulate! do |img|
+  #     cols, rows = img[:dimensions]
+  #
+  #     if width > cols || height > rows
+  #       img.combine_options do |cmd|
+  #         if background == :transparent
+  #           cmd.background "rgba(255, 255, 255, 0.0)"
+  #         else
+  #           cmd.background background
+  #         end
+  #         cmd.gravity gravity
+  #         if width > cols && height > rows
+  #           cmd.extent "#{width}x#{height}"
+  #         elsif width > cols
+  #           cmd.extent "#{width}x#{rows}"
+  #         else
+  #           cmd.extent "#{cols}x#{height}"
+  #         end
+  #       end
+  #     end
+  #
+  #     img = yield(img) if block_given?
+  #     img
+  #   end
+  # end
 
 end
