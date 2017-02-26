ff04f820eb47d58cbabbd3d1c62289ba6ba92cc4
Jack <92jackhenderson@gmail.com>
2017-02-26 11:28:32 +0000

    view individual posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,6 +13,20 @@ class PostsController < ApplicationController
     redirect_to posts_path
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+  
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to(post_path(@post))
+  end
+
   private
 
   def post_params
