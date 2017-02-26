4c984e823d2dac6856f0d2296be625c5efd6af70
Jack <92jackhenderson@gmail.com>
2017-02-26 11:39:27 +0000

    create, read, update, destroy implemented

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,7 +16,7 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
-  
+
   def edit
     @post = Post.find(params[:id])
   end
@@ -25,6 +25,12 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @post.update(post_params)
     redirect_to(post_path(@post))
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
 
   private
