687212f0043b236283871ef7a409e3e68eeb501b
Jack <92jackhenderson@gmail.com>
2017-02-26 10:09:31 +0000

    image upload in progress

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
-  has_attached_file :image
+  validates :image, presence: true
+  has_attached_file :image, styles: { :medium => "640x"}
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
