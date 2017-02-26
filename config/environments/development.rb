e39e5f4045f40ef8e8338b6fa358bd670ee886ce
Sam Jones <sam.jones494@googlemail.com>
2017-02-26 12:12:02 +0000

    AWS stored files now display in app

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,6 +58,8 @@ Rails.application.configure do
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+    },
+    :url =>':s3_domain_url',
+    :path => '/:class/:attachment/:id_partition/:style/:filename',
     }
-  }
 end
