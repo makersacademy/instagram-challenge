5fb9cd149c97e19981487f64fff671aa471e35b1
Jack <92jackhenderson@gmail.com>
2017-02-26 12:36:36 +0000

    user signup with username

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ git_source(:github) do |repo_name|
 end
 
 gem 'haml'
+gem 'devise'
 gem 'bootstrap-sass'
 gem 'paperclip'
 gem 'simple_form'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, :controllers => { registrations: 'registrations' }
   resources :posts
   root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
