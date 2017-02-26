f012048abd06c820e67880106967cab23045b43a
Tamar Lehmann <tlehmann@hotmail.co.uk>
2017-02-26 09:28:02 +0000

    Updated AWS Set Up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,9 +85,10 @@ Rails.application.configure do
   config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
-    :bucket => ENV['S3_BUCKET_NAME'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :bucket => ENV.fetch('S3_BUCKET_NAME'),
+    :access_key_id => ENV.fetch('AWS_ACCESS_KEY_ID'),
+    :secret_access_key => ENV('AWS_SECRET_ACCESS_KEY'),
+    :s3_region => ENV.fetch('AWS_REGION'),
   }
 }
   # Do not dump schema after migrations.
