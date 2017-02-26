e65731dab0a5fb755f0e9b9c30e47c2c0da17a36
Jack <92jackhenderson@gmail.com>
2017-02-26 13:37:14 +0000

    posts belong to user .. in progress

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  validates :user_id, presence: true
+  belongs_to :user
   validates :image, presence: true
   has_attached_file :image, styles: { :medium => "640x"}
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
