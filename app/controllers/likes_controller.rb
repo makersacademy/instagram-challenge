0400c9f5f5c45ec23f135fba8f4bd12c77badeb4
Kat Hicks <kmhicks92@gmail.com>
2017-02-26 00:16:08 +0000

    Further styling

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,7 +3,8 @@ class LikesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     if @post.user_id != current_user.id
-      Like.create(post_id: @post.id, user_id: current_user.id)
+      @like = Like.create(post_id: @post.id, user_id: current_user.id)
+      flash[:alert] = "You cannot like a post more than once" if !@like.save
     else
       flash[:alert] = "You cannot like your own post"
     end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,6 @@
 class Like < ApplicationRecord
   belongs_to :post
   belongs_to :user
+
+  validates :user, uniqueness: { scope: :post, message: "You can only like a post once" }
 end
