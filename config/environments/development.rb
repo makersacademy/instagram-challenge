b068e88c6a9547c8a0540633a566df372f63d580
Edyta Wrobel <edyta.wrobel.willdoit@gmail.com>
2017-02-27 00:14:39 +0000

    add css, fix heroku images display

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   },
-    :s3_host_name => "s3-eu-west-2.amazonaws.com", # Added entry
+    :s3_host_name => "s3.eu-west-2.amazonaws.com", # Added entry
   }
 
   # Enable/disable caching. By default caching is disabled.
