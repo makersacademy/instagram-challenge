7f4f19f5cdb41288f61429971a4ff41542ad058d
Rob Brentnall <rob.brentnall@gmail.com>
2017-02-27 07:39:31 +0000

    removed some migrations

--- db/migrate/20170225171821_add_user_id_to_likes.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToLikes < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :likes, :user, foreign_key: true
-  end
-end
--- db/migrate/20170226105952_drop_likes_table.rb
+++
@@ -1,9 +0,0 @@
-class DropLikesTable < ActiveRecord::Migration
-  def up
-    drop_table :likes
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
