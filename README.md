31a1a5f239af870b42c16abe3450457877db4a46
PeteGH <peter@grant-hay.eu>
2017-02-27 09:46:06 +0100

    adds show view, one error, view readme for rest

--- README.md
+++ README.md
@@ -1,11 +1,39 @@
 # README
 
+This is still a work in progress. In its current state it is possible to,
+
+* Add a new photo including a caption
+* Photos are displayed via the index view
+* Photos can be clicked on to see in show view
+
+The following error is still being returned,
+
+```
+1) photos photos have been added viewing photos lets a user view a photo
+     Failure/Error: expect(current_path).to eq 'photos/#{@photo.id}'
+
+       expected: "photos/\#{@photo.id}"
+            got: "/photos"
+
+       (compared using ==)
+```
+And I have not yet identified the reason why the images (thumbs on index/and original in show) are not being displayed correctly.
+
+The following functionality is yet to be completed.
+
+* User login via OmniAuth
+* Association of User and Photo/caption
+* The possibility of adding comments to photos
+* Editing of posts
+* Liking of posts
+* Upload to Heroku
+
 This README would normally document whatever steps are necessary to get the
 application up and running.
 
 Things you may want to cover:
 
-* Ruby version ruby version 2.3.3 
+* Ruby version ruby version 2.3.3
 
 * System dependencies
 
--- spec/features/photos_feature_spec.rb
+++ spec/features/photos_feature_spec.rb
@@ -6,7 +6,6 @@ feature 'photos' do
     scenario 'should display a prompt to add a photo' do
       visit '/photos'
       expect(page).to have_content('No photos have been posted yet!')
-      save_and_open_page
       expect(page).to have_link('Post a new photo')
     end
   end
@@ -17,11 +16,24 @@ feature 'photos' do
       @photo_id = Photo.first.id
     end
 
-    scenario 'display photos' do
+    scenario 'display photos and its caption' do
       visit '/photos'
       expect(page).to have_css("img[src*='test']")
       expect(page).to have_content('Cooking!')
       expect(page).not_to have_content('No photos have been posted yet!')
+    end
+
+    context 'viewing photos' do
+      # let!(:cooking){Photo.create(image:'Cooking!')}
+        scenario 'lets a user view a photo' do
+          # photo_id = Photo.where(caption: 'Cooking!').ids[0]
+          img = find(:css, "img[src*='test']")
+          img.click
+          expect(page).to have_css("img[src*='test']")
+          expect(page).to have_content('Cooking!')
+          save_and_open_page
+          expect(current_path).to eq 'photos/#{@photo.id}'
+        end
     end
   end
 end
