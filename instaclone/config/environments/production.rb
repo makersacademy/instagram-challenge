567b66e2b3338be996856927955664e734e59302
Eleanor Kavanagh-Brown <ekavanaghbrown@gmail.com>
2017-02-27 23:41:18 +0000

    Connect to AWS S3 storage

--- instaclone/config/environments/production.rb
+++ instaclone/config/environments/production.rb
@@ -4,6 +4,15 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
