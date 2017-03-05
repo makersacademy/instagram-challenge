4bdd6ce8107e73f1386143814e3ec63c85fea856
Varvara Shinkarenko <shinkarenko.varvara@gmail.com>
2017-03-05 15:28:45 +0000

    Added more styling

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,8 +9,16 @@ class CommentsController < ApplicationController
 
   def create
     @picture = Picture.find(params[:picture_id])
-    @picture.comments.create_with_user(comments_params, current_user)
-    redirect_to pictures_path
+    @comment = @picture.comments.create_with_user(comments_params, current_user)
+    if @comment.save
+     respond_to do |format|
+       format.html { redirect_to root_path }
+       format.js
+     end
+   else
+     flash[:alert] = "Check the comment form, something went wrong."
+     render root_path
+   end
   end
 
   # def destroy
