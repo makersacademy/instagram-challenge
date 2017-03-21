7711707f9e8767e737872ff696ef5cc7bb3971be
Leke Abolade <leke_abolade92@hotmail.co.uk>
2017-03-20 19:24:18 -0700

    photos save to aws

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,6 +44,18 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :url => ':s3_domain_url',
+  :path => '/:class/:attachment/:id_partition/:style/:filename',
+  :s3_credentials => {
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+    :s3_region => ENV['AWS_S3_REGION']
+  }
+}
+
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
