2553e74b2df11ba6f804473987cf42fa7271a479
Leke Abolade <leke_abolade92@hotmail.co.uk>
2017-03-21 10:50:54 -0700

    adding styling to registration and session pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = "auth.info.email"
+      user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name
       user.avatar = auth.info.image
