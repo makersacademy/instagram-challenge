4ef77c3b858b482f62af273b9f8ca5aad686c9a8
Rick Clark <rkingsleyclark@gmail.com>
2017-03-25 14:12:47 +0000

    Update dev config to remove AWS requirement

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,14 +54,4 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-  config.paperclip_defaults = {
-    storage: :s3,
-    s3_region: ENV.fetch('AWS_REGION'),
-    s3_host_name: ENV.fetch('AWS_HOST_NAME'),
-    s3_credentials: {
-      bucket: ENV.fetch('S3_BUCKET_NAME'),
-      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY')
-    }
-  }
 end
