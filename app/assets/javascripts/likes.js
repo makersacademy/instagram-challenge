b238a49e4ff9479ca91ef7bbed430206738ff490
Varvara Shinkarenko <shinkarenko.varvara@gmail.com>
2017-04-06 16:26:12 +0100

    Pictures can be liked  only by current users

--- app/assets/javascripts/likes.js
+++ app/assets/javascripts/likes.js
@@ -2,12 +2,10 @@ $(document).ready(function() {
 
   $('.add-new-like').on('click', function(event){
       event.preventDefault();
-
-      var likesCount = $(this).siblings('.likes_count');
+      var likeCount = $("#picture_"+response.picture_id+" .likes_count");
 
       $.post(this.href, function(response){
         likesCount.text(response.new_likes_count);
-
     });
   });
 });
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,9 +2,9 @@ class LikesController < ApplicationController
 
   def create
     @picture = Picture.find(params[:picture_id])
-    @like = @picture.likes.create.create_with_user(current_user)
+    @like = @picture.likes.create_with_user(current_user)
     likes_count = @picture.likes.count
-    render json: {new_likes_count: likes_count}
+    render json: {new_likes_count: likes_count, picture_id: @picture.id}
   end
 
 end
