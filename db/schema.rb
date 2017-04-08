984174c521c2480b869fd5e1a5969f80e3c45c08
Ashwini <ashie.oh@gmail.com>
2017-04-08 17:35:36 +0100

    Generate like model and db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170408152009) do
+ActiveRecord::Schema.define(version: 20170408161222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "likes", ["post_id"], name: "index_likes_on_post_id", using: :btree
 
   create_table "posts", force: :cascade do |t|
     t.string   "description"
@@ -26,4 +34,5 @@ ActiveRecord::Schema.define(version: 20170408152009) do
     t.datetime "image_updated_at"
   end
 
+  add_foreign_key "likes", "posts"
 end
