9c8d0da4439871d4d85b7119d92ff3945861082e
Ashwini <ashie.oh@gmail.com>
2017-04-08 15:20:46 +0100

    Add feature test & views to make it pass

---
+++ spec/features/posts_feature_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+feature 'posts' do
+
+  context 'no posts have been added' do
+
+    scenario 'should display a prompt to add post' do
+      visit '/posts'
+      expect(page).to have_content 'No posts yet'
+      expect(page).to have_link 'Add a post'
+    end
+
+  end
+
+end
