4180f8f82062dd295c9f8c2362350013b13582a9
Ashwini <ashie.oh@gmail.com>
2017-04-08 18:40:47 +0100

    Add #edit & #update to controller, small amendments to assets

--- app/assets/javascripts/likes.js
+++ app/assets/javascripts/likes.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
       var likeCount = $(this).siblings('.likes_count');
 
       $.post(this.href, function(response){
-        var likes_text = "♥ " + response.new_like_count
+        var likes_text = "♥ " + response.new_like_count;
         likeCount.text(likes_text );
     });
   });
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,17 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+
+    redirect_to posts_path
+  end
+
   private
 
   def post_params
