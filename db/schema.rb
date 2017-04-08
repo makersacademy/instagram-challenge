8a699e26affc67666b72babd076fb03bdfe42e1b
Ashwini <ashie.oh@gmail.com>
2017-04-08 19:29:47 +0100

    Generate a model and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170408161222) do
+ActiveRecord::Schema.define(version: 20170408181143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "opinions"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
 
   create_table "likes", force: :cascade do |t|
     t.integer  "post_id"
@@ -34,5 +43,6 @@ ActiveRecord::Schema.define(version: 20170408161222) do
     t.datetime "image_updated_at"
   end
 
+  add_foreign_key "comments", "posts"
   add_foreign_key "likes", "posts"
 end
