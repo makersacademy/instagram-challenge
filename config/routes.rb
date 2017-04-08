727c3c3059cffd3551b10cfed2d69ab8ea2b9719
Bernard Malhame <bernard.malhame@gmail.com>
2017-04-08 22:00:39 +0100

    test no photos yet passes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root "posts#index"
+  resources :photos
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
