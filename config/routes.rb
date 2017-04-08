1dd7e34df52c37ad93ec083cd564e899b9bc5f9a
Ashwini <ashie.oh@gmail.com>
2017-04-08 20:59:12 +0100

    Follow devise initialise instruction

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
     resources :comments
   end
 
+  root 'posts#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
