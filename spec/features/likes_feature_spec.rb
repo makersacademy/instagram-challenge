30c8d3a5cb78a43d676f266cfcc5b686c771888c
Ashwini <ashie.oh@gmail.com>
2017-04-08 17:33:35 +0100

    Add a feature spec to like a post and count number of likes

---
+++ spec/features/likes_feature_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+feature 'liking a post' do
+  before do
+    add_new_post
+  end
+
+  scenario 'a user can like a post which updates the likes count' do
+    visit '/posts'
+    click_link "♡"
+    expect(page).to have_content '♥ 1 like'
+  end
+
+end
