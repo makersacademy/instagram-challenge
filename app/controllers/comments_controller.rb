cf3cc29786452c81bc52dd3aa2502df18ad0a9af
Clem Capel-Bird <clemcapelbird@gmail.com>
2017-04-08 13:45:09 +0100

    Only users may leave comments. All tests passing

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!, :except => [:index]
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
 
   # GET /comments
--- spec/features/comments_feature_spec.rb
+++ spec/features/comments_feature_spec.rb
@@ -29,4 +29,22 @@ feature 'Comments' do
       expect(page).to have_content 'test1'
     end
   end
+
+  context 'Only Users can fully use Instagram' do
+
+    before(:each) do
+      visit '/'
+      user_sign_up
+      create_new_post
+      visit '/'
+      click_link 'Sign Out'
+    end
+
+    scenario 'Only Users can post comments' do
+      visit '/'
+      expect(page).to have_content 'Sign In'
+      click_link 'Comment'
+      expect(page).to have_content 'You need to sign in or sign up before continuing.'
+    end
+  end
 end
