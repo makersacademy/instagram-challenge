fbcd3d7960075957e169d8485559714ca9fafcd8
Ashwini <ashie.oh@gmail.com>
2017-04-09 00:18:00 +0100

    Add logic to model and use model logic & js  not to render like link in view

--- app/assets/javascripts/likes.js
+++ app/assets/javascripts/likes.js
@@ -4,10 +4,11 @@ $(document).ready(function() {
       event.preventDefault();
 
       var likeCount = $(this).siblings('.likes_count');
-
+      likeLink = $(this);
       $.post(this.href, function(response){
         var likes_text = "♥ " + response.new_like_count;
         likeCount.text(likes_text );
+        likeLink.text('');
     });
   });
 });
