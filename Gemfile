d2dfd6820bcfb6f67a04038918bcf93389acdf3b
Vicky Roberts <vicki_uk@hotmail.com>
2017-04-08 10:40:22 +0100

    Add post creation

--- Gemfile
+++ Gemfile
@@ -20,6 +20,10 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
+# Use Paperclip for image uploads and resizing
+gem "paperclip", "~> 5.0.0"
+
+
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
---
+++ spec/features/posts_feature_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+feature 'posts' do
+
+  context 'no posts have been added' do
+    scenario 'should display a prompt to add a post' do
+      visit '/posts'
+      expect(page).to have_content 'No posts yet'
+      expect(page).to have_link 'Add a post'
+    end
+  end
+
+  context 'posts have been added' do
+    before do
+      Post.create(caption: 'Driving along the Amalfi coast')
+    end
+
+    scenario 'display posts' do
+      visit '/posts'
+      expect(page).to have_content('Driving along the Amalfi coast')
+      expect(page).not_to have_content('No posts yet')
+    end
+end
+
+
+
+end
