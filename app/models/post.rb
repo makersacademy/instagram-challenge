2e286381ea5883df9bc815d40a1904020f8732fc
Ryan <Ryanchu.ws@gmail.com>
2017-04-08 19:13:18 +0100

    paperclip working

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,13 @@
 class Post < ActiveRecord::Base
 
-  has_attached_file :image
-  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+  has_attached_file :image,
+
+  :styles => {
+    :thumb => "100x100#",
+    :medium => "400x400>"
+  }
+
+  validates_attachment_content_type :image,
+   :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
 end
