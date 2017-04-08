df865404a8f5d274e4c315ad264fffc151773b4e
Noora Al Qasimi <n.k.alqasimi@gmail.com>
2017-04-08 20:37:25 +0100

    Added paperclip and cloudinary (WIP)

--- instagram_clone/app/controllers/posts_controller.rb
+++ instagram_clone/app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, :except => [:index, :show]
-  # before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   # GET /posts
   # GET /posts.json
@@ -71,6 +71,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:caption)
+      params.require(:post).permit(:caption, :image)
     end
 end
