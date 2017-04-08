e941be1648a50d1099d1529467c6c23c73b8103a
Ashwini <ashie.oh@gmail.com>
2017-04-08 18:07:45 +0100

    Use AJAX to update likes for a post

---
+++ app/assets/javascripts/likes.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+  $('.likes-link').on('click', function(event){
+      event.preventDefault();
+
+      var likeCount = $(this).siblings('.likes_count');
+
+      $.post(this.href, function(response){
+        var likes_text = "♥ " + response.new_like_count
+        likeCount.text(likes_text );
+    });
+  });
+});
