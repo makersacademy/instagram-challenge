5197537ebf9faf14c53638682392d4d75c8d4863
Noora Al Qasimi <n.k.alqasimi@gmail.com>
2017-04-08 18:31:34 +0100

    Added a bit of styling

--- instagram_clone/app/controllers/posts_controller.rb
+++ instagram_clone/app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, :except => [:index, :show]
+  # before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   # GET /posts
   # GET /posts.json
