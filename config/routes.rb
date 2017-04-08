ae85e1283421f626835452c755278f3e0b327c5f
Ashwini <ashie.oh@gmail.com>
2017-04-08 18:16:31 +0100

    Create routes for comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :posts, shallow: true do
+  resources :posts do
     resources :likes
+    resources :comments
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
