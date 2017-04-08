dfbf11215247bc2b55101e7979f7bdb1a47b2cc8
Ashwini <ashie.oh@gmail.com>
2017-04-08 19:45:44 +0100

    Add spec to check that opinion is min of 3 letters

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
+  validates :opinions, length: { minimum: 3 }
 end
--- spec/features/comments_feature_spec.rb
+++ spec/features/comments_feature_spec.rb
@@ -11,4 +11,9 @@ feature 'commenting' do
     expect(page).to have_content 'mouth watering'
   end
 
+  scenario 'throw an error if opinion is less than 3 letters long' do
+    comment = add_new_comment('hi')
+    expect(page).to have_content 'Sorry! your comment must be more than 3 letters long'
+  end
+
 end
