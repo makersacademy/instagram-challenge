0be8a1b06c14586c3d2350ad4372508d94997b35
Varvara <shinkarenko.varvara@gmail.com>
2017-04-08 13:01:35 +0100

    Corrected mistakes

--- app/assets/javascripts/likes.js
+++ app/assets/javascripts/likes.js
@@ -1,11 +1,13 @@
-$(document).ready(function() {
-
+var likesHandler = function() {
   $('.add-new-like').on('click', function(event){
       event.preventDefault();
-      var likeCount = $("#picture_"+response.picture_id+" .likes_count");
+
+      var likesCount = $("#picture_"+response.picture_id+" .likes_count");
 
       $.post(this.href, function(response){
         likesCount.text(response.new_likes_count);
     });
   });
-});
+};
+
+$(document).on("turbolinks:load", likesHandler);
