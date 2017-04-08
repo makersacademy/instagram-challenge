8885cf4b2b54cb1296902ae947e1729721c349e8
Ashwini <ashie.oh@gmail.com>
2017-04-08 17:37:37 +0100

    Add has_many likes association

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ActiveRecord::Base
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/assets/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
+  has_many :likes
 end
