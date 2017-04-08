027fdc2f8bdc9508e237e5bdb26a1bca27fadb65
Ashwini <ashie.oh@gmail.com>
2017-04-08 17:36:52 +0100

    Add shallow nested routes for like

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :posts
+  resources :posts, shallow: true do
+    resources :likes
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
