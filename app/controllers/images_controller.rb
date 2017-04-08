d9f48e3585bb1e3d2228ad36c2dc29376b60c07e
Alice Armstrong <tioma001@hotmail.com>
2017-04-09 00:45:56 +0100

    Hidden some functionality from guest users

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,6 +1,6 @@
 class ImagesController < ApplicationController
   before_action :set_image, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /images
   # GET /images.json
