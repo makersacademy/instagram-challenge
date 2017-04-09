d2c797a927503fde5d9643d73d974dc4fd931024
Clem Capel-Bird <clemcapelbird@gmail.com>
2017-04-09 12:59:18 +0100

    Capbybara tests passing for blank to red heart for likes.

--- spec/features/likes_feature_spec.rb
+++ spec/features/likes_feature_spec.rb
@@ -4,17 +4,17 @@ feature 'Likes' do
       user_sign_up
       create_new_post
       visit '/'
-      click_link 'Like'
-      expect(page).to have_content '1 Like'
+      find(:xpath, "//a/img[@alt='new']/..").click
+      expect(page).to have_css 'img.red_like'
     end
 
     scenario 'Users can only like posts once' do
       user_sign_up
       create_new_post
       visit '/'
-      click_link 'Like'
-      expect(page).not_to have_link 'Like'
-      expect(page).to have_content '1 Like'
+      find(:xpath, "//a/img[@alt='new']/..").click
+      expect(page).to have_css 'img.red_like'
+      expect(page).not_to have_css 'img.blank_like'
     end
 
     # scenario 'Users can unlike photos' do
