dcde86e1856e1c015989bde38bdfbe8443fcc416
Eva Balog <evebalog@netscape.net>
2017-04-09 23:56:24 +0100

    Created nested route for Comments and controller/model, first feature test for comments failing

---
+++ instagram_clone/spec/features/comments_feature_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+feature 'commenting' do
+  before { Post.create description: 'Great Day' }
+
+  scenario 'allows users to leave a comment using a form' do
+     visit '/posts'
+     click_link 'Comment'
+     fill_in "Comment", with: "so so"
+     click_button 'Leave Comment'
+
+     expect(current_path).to eq '/posts'
+     expect(page).to have_content('so so')
+  end
+
+end
