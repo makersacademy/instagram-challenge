c88ae203f1fd20feffdd11bbecc307fd5658673b
Bernard Malhame <bernard.malhame@gmail.com>
2017-04-09 18:37:21 +0100

    added sign out

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,20 @@
 class CommentsController < ApplicationController
-  class CommentsController < ApplicationController
 
-    def create
-      @photo = Photo.find(params[:photo_id])
-      @photo.comments.create(comments_params)
-      redirect_to photos_path
-    end
+  def create
+    @photo = Photo.find(params[:photo_id])
+    @photo.comments.create(comments_params)
+    redirect_to photos_path
+  end
 
-    private
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to photos_path
+  end
 
-    def comments_params
-      params.require(:comment).permit(:thoughts, :likes)
-    end
+  private
+
+  def comments_params
+    params.require(:comment).permit(:thoughts, :likes)
   end
 end
