4f3649c1353e645760cf880f979a44fe39c64287
Ashwini <ashie.oh@gmail.com>
2017-04-09 12:48:54 +0100

    Render a json object for ajax request

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,10 +10,12 @@ class CommentsController < ApplicationController
     @comment = @post.comments.create(comment_params)
     current_user.comments << @comment
     if @comment.valid?
-      redirect_to posts_path
+      # redirect_to posts_path # non ajax
+      render json: {opinion: @comment.opinions}
     else
       flash[:notice] = 'Sorry! your comment must be more than 3 letters long'
-      render 'new'
+      # render 'new' # non ajax
+      render json: {error: 'Sorry! your comment must be more than 3 letters long'}
     end
   end
 
