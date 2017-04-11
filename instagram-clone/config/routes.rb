944dfe98c15e6dc7d2a99a0def0f465a2d8e17f2
Katie Koschland <katiekoschland@hotmail.com>
2017-04-11 10:16:40 +0100

    Created posts controller

--- instagram-clone/config/routes.rb
+++ instagram-clone/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :pictures
+  resources :posts
 
   root "pictures#index"
 
---
+++ instagram-clone/spec/features/post_features_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+feature 'posts' do
+  context 'no posts have been added' do
+    scenario 'should display a link to add a post' do
+      visit '/posts'
+      expect(page).to have_content 'There are no posts yet!'
+      expect(page).to have_link 'Create a post'
+    end
+  end
+end
