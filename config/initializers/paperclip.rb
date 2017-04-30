3cb30631dc6952e9cacfb15aacd910db602653d6
Barbara Shinkarenko <shinkarenko.varvara@gmail.com>
2017-04-30 14:31:38 +0100

    Enable the proper display of image

---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,3 @@
+Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
