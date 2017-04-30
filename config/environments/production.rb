b56c2239748e6b15e6119231cccaa6ab493587e4
Barbara Shinkarenko <shinkarenko.varvara@gmail.com>
2017-04-30 15:46:04 +0100

    Added new configurations to aws paperclip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,9 @@ Rails.application.configure do
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-      :s3_region => 'eu-west-2'
+      :s3_region => 'eu-west-2',
+      :url => ":s3_domain_url",
+      :path => ":class/:id.:style.:extension"
     }
   }
 end
