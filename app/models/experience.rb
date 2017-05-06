c17483b86fd908fdec137e5071ff4a4e24bd2293
emmpak <eiliev05@yahoo.com>
2017-05-06 20:35:07 +0100

    Add image presence validation

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,6 +1,8 @@
 class Experience < ApplicationRecord
   belongs_to :user
 
+  validates :image, presence: true
+
   has_attached_file :image, :styles => { :medium => "300x300>", :large => "500x500>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
--- spec/features/experiences_feature_spec.rb
+++ spec/features/experiences_feature_spec.rb
@@ -18,13 +18,39 @@ feature 'experiences' do
 
     scenario 'can see shared experiences' do
       expect(page).to have_css 'img'
-      expect(page).to have_content 'axeman'
       expect(page).not_to have_content 'No experiences yet'
-      expect(current_path).to eq '/'
     end
 
     scenario "guest cannot see the 'post an experience' button" do
       expect(page).not_to have_link "Share an experience"
+    end
+
+    scenario 'is unable to share an experience' do
+      visit new_experience_path
+      expect(current_path).to eq new_user_session_path
+    end
+  end
+
+  context 'signed in user' do
+    before do
+      sign_up
+      add_photo
+    end
+
+    scenario 'can share an experience' do
+      expect(page).to have_css 'img'
+      expect(page).to have_content 'axeman'
+      expect(page).to have_link 'Share an experience'
+    end
+
+    context 'experience validations' do
+      scenario 'user cannot submit an experience without uploading a photo' do
+        click_link 'Share an experience'
+        fill_in 'Description', with: 'No image'
+        click_button 'Post experience'
+        expect(page).not_to have_content 'No image'
+        expect(page).to have_content 'error'
+      end
     end
   end
 end
