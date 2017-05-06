7a70ad58ad6bf956e2a55acd97f4dc517566240b
connie-reinholdsson <connie.reinholdsson@gmail.com>
2017-05-06 16:00:32 +0100

    Added functionality to display statuses

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170506144851) do
+
+  create_table "pictures", force: :cascade do |t|
+    t.string "status"
+    t.integer "likes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
--- spec/features/pictures_feature_spec.rb
+++ spec/features/pictures_feature_spec.rb
@@ -8,4 +8,15 @@ feature 'pictures' do
     expect(page).to have_link 'Add a picture'
     end
   end
+
+  context 'show pictures' do
+    before do
+      Picture.create(status: 'Broccoli is my favourite!')
+    end
+    scenario 'show pictures' do
+    visit '/pictures'
+    expect(page).to have_content 'Broccoli is my favourite!'
+    expect(page).not_to have_content 'No pictures added yet'
+    end
+  end
 end
