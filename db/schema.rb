b27f0dd4eab28d48a2f5876d57c480ffb30668ae
Ruan Odendaal <ruanodendaal@gmail.com>
2017-05-06 10:23:10 +0100

    add ability to add pictures

---
+++ db/schema.rb
@@ -0,0 +1,14 @@
+
+ActiveRecord::Schema.define(version: 20170506091925) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "pictures", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "description"
+  end
+
+end
--- spec/features/picture_feature_spec.rb
+++ spec/features/picture_feature_spec.rb
@@ -8,4 +8,28 @@ feature 'pictures' do
       expect(page).to have_link 'Add a picture'
     end
   end
+
+  context 'pictures have been added' do
+    before do
+      Picture.create(name: 'Me')
+    end
+
+    scenario 'display pictures' do
+      visit '/pictures'
+      expect(page).to have_content('Me')
+      expect(page).not_to have_content('No pictures yet')
+    end
+  end
+
+  context 'creating pictures' do
+    scenario 'prompts user to fill out a form, then displays the new picture' do
+      visit '/pictures'
+      click_link 'Add a picture'
+      fill_in 'Name', with: 'Me'
+      fill_in 'Description', with: 'A picture of me'
+      click_button 'Create Picture'
+      expect(page).to have_content 'Me'
+      expect(current_path).to eq '/pictures'
+    end
+  end
 end
