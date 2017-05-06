3f6d5250e2e43cb5658b40585c2359a343a43b15
Pete Wilkins <pete.p.wilkins@gmail.com>
2017-05-06 11:12:31 +0100

    creating a new picture form and flow completed

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,4 +2,19 @@ class PicturesController < ApplicationController
   def index
     @pictures = Picture.all
   end
+
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    Picture.create(picture_params)
+    redirect_to '/pictures'
+  end
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:title, :url)
+  end
 end
--- spec/features/pictures_feature_spec.rb
+++ spec/features/pictures_feature_spec.rb
@@ -20,4 +20,16 @@ feature 'pictures' do
       expect(page).not_to have_content 'No pictures yet'
     end
   end
+
+  context 'creating pictures' do
+    scenario 'prompts user to fill out a form, then displays the new picture' do
+      visit '/pictures'
+      click_link 'Add a picture'
+      fill_in 'Title', with: 'Just a fan enjoying the game'
+      fill_in 'Url', with: 'http://68.media.tumblr.com/9150621cddccbc705a6805d15b835767/tumblr_mrhvg5sMPz1r1thfzo1_1280.jpg'
+      click_button 'Create Picture'
+      expect(page).to have_content 'Just a fan enjoying the game'
+      expect(current_path).to eq '/pictures'
+    end
+  end
 end
