9ce5460ca0a2dac53db622dcff2f05e4ab2c014c
kittysquee <kittysquee@gmail.com>
2017-05-06 21:05:40 +0100

    Passes likes test -time to relax!

--- spec/features/likes_feature_spec.rb
+++ spec/features/likes_feature_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 feature 'liking posts' do
-  before { Post.create description: "Nice", image:  File.open("#{Rails.root}/spec/fixtures/cat.png")}
+  before { Post.create description: "Nice", image:  File.open("#{Rails.root}/spec/fixtures/cat.png"), user: User.create(       username: 'cat',
+                                          email: 'cat@meow.com',
+                                          password: 'meowmeow',
+                                          password_confirmation: 'meowmeow')}
 
   scenario 'a user can like a post, which updates the like count', js: true do
     visit posts_path
