aac416fa80a924366e4c1b487148b96b92b86007
emmpak <eiliev05@yahoo.com>
2017-05-06 10:28:29 +0100

    Add promp test

---
+++ spec/features/photos_feature_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+feature 'photos' do
+  context 'no photos have been added' do
+    scenario 'should display a prompt to add a photo' do
+      visit '/photos'
+      expect(page).to have_content 'No photos yet'
+      expect(page).to link 'Share an experience'
+    end
+  end
+end
