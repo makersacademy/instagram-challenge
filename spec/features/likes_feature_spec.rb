4dd82d75b5ad83e7a5bdfb0891816ae1b716070c
kittysquee <kittysquee@gmail.com>
2017-05-06 20:27:28 +0100

    Begins to add likes JS

--- spec/features/likes_feature_spec.rb
+++ spec/features/likes_feature_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 feature 'liking posts' do
   before { Post.create description: "Nice", image:  File.open("#{Rails.root}/spec/fixtures/cat.png")}
 
-  scenario 'a user can like a post, which updates the like count' do
+  scenario 'a user can like a post, which updates the like count', js: true do
     visit posts_path
     click_link('Like')
     expect(page).to have_content('1 like')
