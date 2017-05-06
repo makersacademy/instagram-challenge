506b735fd62aab777a7cf77473bdf5ee1014678b
Pete Wilkins <pete.p.wilkins@gmail.com>
2017-05-06 15:53:27 +0100

    user must log in to add a picture

--- spec/features/pictures_feature_spec.rb
+++ spec/features/pictures_feature_spec.rb
@@ -3,33 +3,33 @@ require 'rails_helper'
 feature 'pictures' do
   context 'no pictures have been added' do
     scenario 'should display a prompt to add a picture' do
-      visit '/pictures'
+      visit '/'
       expect(page).to have_content 'No pictures yet'
       expect(page).to have_link 'Add a picture'
     end
   end
 
   context 'pictures have been added' do
-    before do
-      Picture.create(title: 'Just a fan enjoying the game', url: 'http://68.media.tumblr.com/9150621cddccbc705a6805d15b835767/tumblr_mrhvg5sMPz1r1thfzo1_1280.jpg')
-    end
-
     scenario 'display pictures' do
-      visit '/pictures'
+      sign_up
+      add_picture
       expect(page).to have_content 'Just a fan enjoying the game'
       expect(page).not_to have_content 'No pictures yet'
     end
   end
 
-  context 'creating pictures' do
+  context 'adding pictures' do
     scenario 'prompts user to fill out a form, then displays the new picture' do
-      visit '/pictures'
-      click_link 'Add a picture'
-      fill_in 'Title', with: 'Just a fan enjoying the game'
-      fill_in 'Url', with: 'http://68.media.tumblr.com/9150621cddccbc705a6805d15b835767/tumblr_mrhvg5sMPz1r1thfzo1_1280.jpg'
-      click_button 'Add Picture'
+      sign_up
+      add_picture
       expect(page).to have_content 'Just a fan enjoying the game'
       expect(current_path).to eq '/pictures'
+    end
+
+    scenario 'restricted if user not logged in' do
+      visit '/pictures'
+      click_link 'Add a picture'
+      expect(current_path).to eq '/users/sign_in'
     end
   end
 
@@ -45,10 +45,12 @@ feature 'pictures' do
   end
 
   context 'deleting pictures' do
-    before { Picture.create(title:'Just a fan enjoying the game', url: 'http://68.media.tumblr.com/9150621cddccbc705a6805d15b835767/tumblr_mrhvg5sMPz1r1thfzo1_1280.jpg') }
+    before do
+      sign_up
+      add_picture
+    end
 
     scenario 'remove a picture when a user clicks the delete link' do
-      visit '/pictures'
       click_link 'Delete'
       expect(page).not_to have_content 'Just a fan enjoying the game'
       expect(page).to have_content 'Picture deleted successfully'
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -1,8 +1,19 @@
-def sign_up
+def sign_up(email: 'pete@pete.com',
+            password: 'password',
+            password_confirmation: 'password')
   visit '/'
   click_link 'Sign up'
-  fill_in('Email', with: 'pete@pete.com')
-  fill_in('Password', with: 'password')
-  fill_in('Password confirmation', with: 'password')
+  fill_in('Email', with: email)
+  fill_in('Password', with: password)
+  fill_in('Password confirmation', with: password_confirmation)
   click_button 'Sign up'
+end
+
+def add_picture(title: 'Just a fan enjoying the game',
+                url: 'http://68.media.tumblr.com/9150621cddccbc705a6805d15b835767/tumblr_mrhvg5sMPz1r1thfzo1_1280.jpg')
+  visit '/'
+  click_link 'Add a picture'
+  fill_in('Title', with: title)
+  fill_in('Url', with: url)
+  click_button 'Add Picture'
 end
