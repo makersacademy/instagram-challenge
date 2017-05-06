3e6ff09105649657319132daa5f098671ff9b1d0
kittysquee <kittysquee@gmail.com>
2017-05-06 11:37:07 +0100

    Adds create and show to post controller and associated views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,25 @@
 class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    Post.create(post_params)
+    redirect_to posts_path
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:description, :image)
+  end
 end
--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -16,9 +16,33 @@ feature 'posts' do
 
     scenario 'display posts' do
       visit posts_path
-      expect(page).to have_content("#{Rails.root}/spec/fixtures/cat.png")
+      expect(page).to have_css('img', text: "cat.png")
       expect(page).to have_content('Lovely')
       expect(page).not_to have_content('No posts yet')
+    end
+  end
+
+  context 'creating posts' do
+    scenario 'promts user to fill out form, then displays post' do
+      visit posts_path
+      click_link('Add a post')
+      fill_in 'Description', with: 'Lovely'
+      click_button('Choose file')
+      click_button('Create Post')
+      expect(page).to have_content('Lovely')
+      expect(current_path).to eq posts_path
+    end
+  end
+
+  context 'viewing posts' do
+
+    let!(:description){Post.create(description:'Nice')}
+
+    scenario 'lets user view post' do
+      visit posts_path
+      click_link('Nice')
+      expect(page).to have_content('Nice')
+      expect(current_path).to eq post_path(description)
     end
   end
 end
