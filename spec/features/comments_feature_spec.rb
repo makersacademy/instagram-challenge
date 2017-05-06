2cecf94ff201ef83588a184a041ebf1e841e24dd
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-05-06 20:28:31 +0100

    test for comments having authors

--- spec/features/comments_feature_spec.rb
+++ spec/features/comments_feature_spec.rb
@@ -4,15 +4,26 @@ feature 'comments' do
   before do
     User.create(email: 'kate@kate.com', password: '123456', password_confirmation: '123456')
     Picture.create(caption: 'sally', user: User.first)
+    visit '/users/sign_up'
+    fill_in 'email', with: 'sylvia@sylvia.com'
+    fill_in 'password', with: '123456'
+    fill_in 'password confirmation', with: '123456'
+    click_button 'sign up'
   end
 
   scenario 'users can comment on a picture' do
      visit '/pictures'
      fill_in 'any thoughts?', with: 'lovexxx'
      click_button 'comment'
-
      expect(current_path).to eq '/pictures'
      expect(page).to have_content('lovexxx')
+  end
+
+  scenario 'comments have authors email' do
+     visit '/pictures'
+     fill_in 'any thoughts?', with: 'lovexxx'
+     click_button 'comment'
+     expect(page).to have_content('sylvia@sylvia.com')
   end
 
 end
