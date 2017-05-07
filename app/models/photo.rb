1464f1f3e046eaafe436ffd75aada2e89fafc888
Will Schwier <w.schwier@hotmail.co.uk>
2017-05-08 00:02:20 +0100

    Photos working properly

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,10 @@
 class Photo < ApplicationRecord
-  has_attached_file :image
+  has_attached_file :image,
+                    styles: { thumb: ["300x300#", :jpg],
+                              original: ['500x500>', :jpg] },
+                    convert_options: { thumb: "-quality 75 -strip",
+                                       original: "-quality 85 -strip" }
+
   validates_attachment :image,
                      content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
 end
---
+++ spec/features/comment_feature_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+feature 'commenting' do
+  before { Restaurant.create name: 'KFC' }
+
+  scenario 'allows users to leave a comment using a form' do
+     visit '/photos'
+     click_link 'Leave a Comment'
+     fill_in "Thoughts", with: "so so"
+     click_button 'Comment'
+
+     expect(current_path).to eq '/photos'
+     expect(page).to have_content('so so')
+  end
+
+end
