b3fdf9b8717ebcc1445a0bb7d93234988ee0fa4c
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-05-07 12:03:39 +0100

    styling

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,7 @@
 class LikesController < ApplicationController
   before_action :authenticate_user!
 
-  def new
+  def create
     picture = Picture.find(params[:picture_id])
     picture.likes.create
     redirect_to pictures_path
