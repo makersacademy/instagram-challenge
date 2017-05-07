fa9e35eba296008323480f5c715cf98dc21a852c
kittysquee <kittysquee@gmail.com>
2017-05-07 16:08:05 +0100

    Adds ability to add and take away like for single user

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,12 @@ class LikesController < ApplicationController
 
   def perform
     @post = Post.find(params[:post_id])
-    @post.likes.create
+    post_check = @post.likes.where(user: current_user)
+    if post_check.count > 0
+      post_check.destroy_all
+    else
+      @post.likes.create(user: current_user)
+    end
     render json: { new_like_count: @post.likes.count }
   end
 end
