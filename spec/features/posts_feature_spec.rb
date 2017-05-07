941168967c0bd24c6efc87b4659bf76b56ad273a
Freddy Fallon <freddyfallon@gmail.com>
2017-05-07 11:50:26 +0100

    Add upload test

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -1,17 +1,34 @@
 require 'rails_helper'
 
-feature 'posts' do
-  context 'no posts have been created' do
-    scenario 'should display a prompt to create a post' do
+feature 'Posts' do
+  context 'No posts have been created' do
+    scenario 'Should display a prompt to create a post' do
       visit root_path
       expect(page).to have_content 'No posts yet'
       expect(page).to have_link 'Create post'
     end
   end
 
-  context 'post has been added' do
+  context 'Post has been added' do
     before do
-      
+      Post.create(image: File.new(Rails.root + 'spec/fixtures/images/freedom-beach.jpg'), caption: 'Cool')
+    end
+    scenario 'Should show post' do
+      visit root_path
+      expect(page).to have_content 'Cool'
+      expect(page).not_to have_content 'No posts yet'
+    end
+  end
+
+  context 'Creating posts' do
+    scenario 'Prompt user to create post, then display new post' do
+      visit root_path
+      click_link 'Create post'
+      fill_in 'Caption', with: 'Cool'
+      attach_file("Image", Rails.root + 'spec/fixtures/images/freedom-beach.jpg')
+      click_button 'Create Post'
+      expect(page).to have_selector("img")
+      expect(page).to have_content("Cool")
     end
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,4 +2,29 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
 
+  describe 'Images' do
+    it 'should be valid with image file' do
+      post = Post.new image: File.new(Rails.root + 'spec/fixtures/images/freedom-beach.jpg'), caption: 'Cool'
+      expect(post).to be_valid
+    end
+
+    it 'should not be valid without image file' do
+      post = Post.new caption: 'Cool'
+      expect(post).not_to be_valid
+    end
+  end
+
+  describe 'Captions' do
+    it 'should be valid with caption' do
+      post = Post.create image: File.new(Rails.root + 'spec/fixtures/images/freedom-beach.jpg'), caption: 'Cool'
+      expect(post).to be_valid
+    end
+
+    it 'should not be valid without caption' do
+      post = Post.create image: File.new(Rails.root + 'spec/fixtures/images/freedom-beach.jpg')
+      expect(post).not_to be_valid
+    end
+  end
+
+
 end
