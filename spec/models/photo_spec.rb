56a90e5f1d7e0581f1b0d4384e1b668ee0ac1583
Simba14 <seanblundell@gmail.com>
2017-05-07 12:37:19 +0100

    Test associations between user and photo

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 describe Photo, type: :model do
+
+  it { should belong_to(:user) }
+
   it 'is not valid without an image file' do
     photo = Photo.new(caption: 'test', location: 'test')
     expect(photo).to have(1).error_on(:image)
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+describe User, type: :model do
+
+  it { should have_many(:photos) }
+end
