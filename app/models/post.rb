1ac7276380ba6231cf2378a454d96bffc2c42638
Eugenia Guerrero <eugenia.guerrero@me.com>
2017-05-13 15:14:31 +0100

    passing tests for uploading and viewing specific posts with images and captions

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { medium: "600x600>", thumb: "100x100>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  validates :image, attachment_presence: true
+
 
 end
--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-feature 'posts' do
+feature 'no posts' do
   context 'no posts have been added' do
     scenario 'should display a prompt to add a post' do
       visit '/posts'
@@ -9,39 +9,40 @@ feature 'posts' do
     end
   end
 
+feature 'posts' do
+  before do
+    visit '/posts'
+    click_link 'Add a post'
+    page.attach_file('Image', Rails.root + "app/assets/images/andromeda.jpg")
+    fill_in 'Name', with: 'Andromeda'
+    click_button 'Post image'
+  end
+
   context 'posts have been added' do
-    before do
-      Post.create(name: 'Holiday')
-    end
     scenario 'display posts' do
       visit '/posts'
-      expect(page).to have_content 'Holiday'
+      expect(page).to have_content 'Andromeda'
       expect(page).not_to have_content 'No posts yet'
     end
   end
 
   context 'creating posts' do
     scenario 'prompts user to fill out a form, then displays the new post' do
-      visit '/posts'
-      click_link 'Add a post'
-      fill_in 'Name', with: 'Holiday'
-      click_button 'Create Post'
-      expect(page).to have_content 'Holiday'
+      expect(page).to have_content 'Andromeda'
+      expect(page).to have_css 'img'
       expect(current_path).to eq '/posts'
     end
   end
 
   context 'viewing posts' do
-
-    let!(:city) { Post.create(name:'City') }
-
     scenario 'lets a user view a post' do
       visit '/posts'
-      click_link 'City'
-      expect(page).to have_content 'City'
-      expect(current_path).to eq "/posts/#{city.id}"
+      click_link 'Andromeda'
+      expect(page).to have_content 'Andromeda'
+      expect(current_path).to eq "/posts/#{Post.last.id}"
     end
   end
+end
 
 
 end
