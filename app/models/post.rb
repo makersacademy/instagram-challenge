c2fc4c4c2a6be0a77d0ac68939904667a668bda4
Magnus Holm <mail@magnusholm.com>
2017-05-14 15:41:00 +0100

    styling, center photos

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   # belongs_to :user
 
-  has_attached_file :image, styles: { medium: "600x600>", thumb: "100x100>" }
+  has_attached_file :image, styles: { medium: "1200x1200>", thumb: "100x100>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
 end
