e01b4417798320d046a503a234abb40de595e2f4
Magnus Holm <mail@magnusholm.com>
2017-05-14 16:31:26 +0100

    Styling, add flash message alert bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def flash_alerts(level)
+    case level
+      when :notice then "alert alert-info"
+      when :notice then "alert alert-success"
+      when :notice then "alert alert-error"
+      when :notice then "alert alert-error"
+    end 
+  end
+
 end
