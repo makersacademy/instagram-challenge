e51433b1c6f5af509c0d2c919d7d2d0d5a02ae97
Sophie Mann <sklm@live.co.uk>
2017-06-02 20:11:03 +0100

    adds user specs

---
+++ spec/features/user_can_sign_up_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+require 'helpers/user_helper'
+
+RSpec.feature "Sessions",  type: :feature do
+
+  scenario "signs up the user", :type => :feature do
+    sign_up
+    page.should have_content("Welcome! You have signed up successfully.")
+  end
+
+  scenario "signs out the user", :type => :feature do
+    sign_up
+    click_on("Log Out")
+    page.should have_content("Signed out successfully.")
+  end
+
+  scenario "signs in the user", :type => :feature do
+    sign_up
+    click_on("Log Out")
+    log_in
+    page.should have_content("Signed in successfully.")
+  end
+end
---
+++ spec/helpers/user_helper.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+def sign_up(email = "example@example.com", password = "password")
+  visit "/users/sign_up"
+  fill_in 'user_email', :with => email
+  fill_in 'user_password', :with => password
+  fill_in 'user_password_confirmation', :with => password
+  click_button 'Sign up'
+end
+
+def log_in(email = "example@example.com", password = "password")
+  visit "/users/sign_in"
+  fill_in 'user_email', :with => email
+  fill_in 'user_password', :with => password
+  click_button 'Log in'
+end
