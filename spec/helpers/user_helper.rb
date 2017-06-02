3ae143541dc2ae666ebd6e96d29d20df8b318c8a
Sophie Mann <sklm@live.co.uk>
2017-06-02 21:32:29 +0100

    updates spec to accept username

--- spec/helpers/user_helper.rb
+++ spec/helpers/user_helper.rb
@@ -1,14 +1,15 @@
 require 'spec_helper'
 
-def sign_up(email = "example@example.com", password = "password")
+def sign_up(username = "user", email = "example@example.com", password = "password")
   visit "/users/sign_up"
+  fill_in 'user_username', :with => username
   fill_in 'user_email', :with => email
   fill_in 'user_password', :with => password
   fill_in 'user_password_confirmation', :with => password
   click_button 'Sign up'
 end
 
-def log_in(email = "example@example.com", password = "password")
+def log_in(username = "user", email = "example@example.com", password = "password")
   visit "/users/sign_in"
   fill_in 'user_email', :with => email
   fill_in 'user_password', :with => password
