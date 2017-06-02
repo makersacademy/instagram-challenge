4041c858ddfcf6e1cb02eba40dc1faaa4f583d80
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-06-02 21:13:04 +0100

    Add User controller and render User authentication at user#index
    - Update application.html.erb to render view

---
+++ instagram/app/controllers/user_controller.rb
@@ -0,0 +1,4 @@
+class UserController < ApplicationController
+  def index
+  end
+end
---
+++ instagram/test/controllers/user_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class UserControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get user_index_url
+    assert_response :success
+  end
+
+end
