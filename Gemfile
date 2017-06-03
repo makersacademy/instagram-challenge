9d8389f2060b3f2cc8b1fee9d275091050c859db
Paula Muldoon <pmuldoon@umich.edu>
2017-06-03 12:51:42 +0100

    added devise

--- Gemfile
+++ Gemfile
@@ -10,6 +10,8 @@ end
 gem 'rails', '~> 5.1.1'
 # Use postgresql as the database for Active Record
 gem 'pg', '~> 0.18'
+
+gem 'devise'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'welcome/index'
+
+  # get 'welcome#route'
+  
+  root 'home#index'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
