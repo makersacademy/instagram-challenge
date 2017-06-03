559f17e7a6afed61e01c43ecb003cce1732df1a4
BenNoonan1991 <ben.noonan2@gmail.com>
2017-06-03 18:51:58 +0100

    Added devise users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
+  config.action_mailer.default_url_options = { host: 'https://instagram-makers.herokuapp.com/' }
   config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
