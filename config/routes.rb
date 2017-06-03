c706ae1bb3b364a24696fb1a38d0c09f2e6b4543
Timothy Robinson <timmyrobby@gmail.com>
2017-06-03 21:02:02 +0100

    Adds post controller logic

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users, :posts
+  
   constraints Clearance::Constraints::SignedOut.new do
     root to: "clearance/users#new"
   end
