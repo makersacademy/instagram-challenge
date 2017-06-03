1c4fd6cc2d7979277612ceccd32a4f7c1abf7ae1
Paula Muldoon <pmuldoon@umich.edu>
2017-06-03 20:14:25 +0100

    login test passing

--- spec/features/user_auth_spec.rb
+++ spec/features/user_auth_spec.rb
@@ -8,5 +8,11 @@ RSpec.feature "Sessions", type: :feature do
     page.should have_content("Welcome to Instagram!")
   end
 
+  scenario "user can sign in", :type => :feature do
+    sign_up
+    click_link('Log Out')
+    log_in
+    page.should have_content("Logged in as example@starfleet.com.")
+  end
 
 end
--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -7,3 +7,10 @@ def sign_up(email = "example@starfleet.com")
   fill_in 'user_password_confirmation', :with => 'password'
   click_button 'Sign Up'
 end
+
+def log_in(email = "example@starfleet.com")
+  visit '/users/sign_in'
+  fill_in 'user_email', :with => 'example@starfleet.com'
+  fill_in 'user_password', :with => 'password'
+  click_button 'Log In'
+end
