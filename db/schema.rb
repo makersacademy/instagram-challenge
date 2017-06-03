9eadc7d751166437c78e1601ad3a0bd0e0b21c27
Timothy Robinson <timmyrobby@gmail.com>
2017-06-03 20:51:38 +0100

    Changes database to use postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170603185211) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
