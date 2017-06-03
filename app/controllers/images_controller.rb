3127a3e35e588f9a10adbb0975a0b61b64e85ea1
Cristina Iannone <iannonecristina@gmail.com>
2017-06-03 21:40:26 +0100

    Update stylesheet and index view

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -20,6 +20,6 @@ class ImagesController < ApplicationController
   private
 
   def image_params
-    params.require(:image).permit(:image)
+    params.require(:image).permit(:image, :description, :title)
   end
 end
