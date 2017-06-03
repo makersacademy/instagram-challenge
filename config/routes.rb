177913bbbbb70ee3d465849e245fd4048a4024be
Timothy Robinson <timmyrobby@gmail.com>
2017-06-03 15:46:14 +0100

    Adds clearance constraints to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  constraints Clearance::Constraints::SignedOut.new do
+    root to: "clearance/users#new"
+  end
+
+  constraints Clearance::Constraints::SignedIn.new do
+    root to: "posts#index", as: :signed_in_root
+  end
 end
