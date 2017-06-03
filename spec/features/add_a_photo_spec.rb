539517391d70f4119f0ad6248ab9df227988d26c
Alex Chalk <alexchalk17@gmail.com>
2017-06-03 16:21:46 -0400

    Add test image to project; update feature spec

--- spec/features/add_a_photo_spec.rb
+++ spec/features/add_a_photo_spec.rb
@@ -4,8 +4,8 @@ RSpec.feature 'Photos', type: :feature do
   scenario 'user adds a photo' do
     visit '/photos'
     click_on 'Post Photo'
-    fill_in 'photo_title', with: 'Medieval Lion'
-    page.attach_file('photo_image', Rails.root + 'app/assets/images/lion-jpg')
+    fill_in 'article_image_title', with: 'Medieval Lion'
+    page.attach_file('photo_image', Rails.root + 'app/assets/images/lion.jpg')
     click_on 'Upload'
     expect(page).to have_content 'Medieval Lion'
   end
