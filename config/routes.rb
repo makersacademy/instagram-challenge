d5ca900b57b5911c52589a1087f36b41b4b42564
Adam Snow <adamjohnsnow@icloud.com>
2017-06-03 08:48:00 +0100

    finally got users working on devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  devise_for :user
+
+  get 'welcome/index'
+
+  root 'welcome#index'
 end
