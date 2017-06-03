f3b4aa398f1ea48bf603478df2a8adbfdff2e5b6
Alex Chalk <alexchalk17@gmail.com>
2017-06-03 16:12:50 -0400

    Generate photos_controller, add paperclip gem, begin UI
    UI details:
    -Post Photo button at /photos
    -Photo upload form at /photos/new

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,13 @@
+class PhotosController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'photos/index'
+
+  get 'photos/new'
+
+  get 'photos/create'
+
+  get 'photos/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :photos
 end
