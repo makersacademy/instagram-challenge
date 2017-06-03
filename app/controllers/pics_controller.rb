546f0be56d427a99d7218bf3d996fb39baf4938a
danielemanuel <daniel.costea@vkernel.ro>
2017-06-03 18:16:22 +0100

    Multiple pictures are displayed on the index page having the title and description

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -2,6 +2,7 @@ class PicsController < ApplicationController
   before_action :find_pic, only: [:show, :edit, :update, :destroy]
 
   def index
+    @pics = Pic.all.order("created_at DESC")
   end
 
   def show
