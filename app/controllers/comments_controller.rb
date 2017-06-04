1e8ef0e311dacfe027702309edd947a60c5b32be
anaalta <suciuanamaria@yahoo.com>
2017-06-04 20:40:10 +0200

    fixed the comment form appearing multiple times

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,12 @@
 class CommentsController < ApplicationController
   def create
     @picture = Picture.find(params[:picture_id])
-    @comment = @picture.comments.create(params[:comment].permit(:commenter, :body))
+    @comment = @picture.comments.create(comment_params)
     redirect_to picture_path(@picture)
   end
+
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
+
 end
