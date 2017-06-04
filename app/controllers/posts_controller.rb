1ff752fd5656f8f9a31cd55c33603a63c62bd705
Paul Kassar <paulkassar@gmail.com>
2017-06-04 22:57:43 +0100

    test still not passing verry very slow to run rspec

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,8 +2,26 @@ class PostsController < ApplicationController
 
   def index
   end
-  
+
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.create(post_params)
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:caption, :image)
+    redirect_to @post
+  end
+
+
 end
