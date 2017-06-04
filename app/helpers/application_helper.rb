8e7997ef65993b9cc04f1eb23123b144cae80e3d
Timothy Robinson <timmyrobby@gmail.com>
2017-06-04 22:35:53 +0100

    links user names to profile pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
   def date_joined(post)
     post.created_at.strftime("%d/%m/%Y")
   end
+
+  def link_user_name(user)
+    link_to "#{user.username}", "/users/#{user.id}", class: 'user-link'
+  end
 end
