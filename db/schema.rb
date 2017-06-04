93a551ecf571140457729dcc35919bcd191582fb
Unai Motriko <unaimotriko@gmail.com>
2017-06-04 20:50:46 +0100

    it introduces unit tests

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,16 @@
 
 ActiveRecord::Schema.define(version: 20170603145733) do
 
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "post_id"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Post do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'cannot be created without a user' do
+    post = Post.create(description: 'post message')
+    expect(post).to_not be_valid
+  end
+
 end
