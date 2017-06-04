748b079f768201ca2001631fcfb0f5ddcd4700d5
Adam Snow <adamjohnsnow@icloud.com>
2017-06-04 16:10:16 +0100

    change paperclip defaults for S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,9 +93,11 @@ Rails.application.configure do
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
       s3_region: ENV.fetch('AWS_REGION'),
+      Paperclip::Attachment.default_options[:url] = ':s3_domain_url',
+      Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
     }
   }
-  
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
