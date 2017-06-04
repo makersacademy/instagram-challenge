3880e43bc165fdde152f8be325ac2f60386c4570
Adam Snow <adamjohnsnow@icloud.com>
2017-06-04 16:12:20 +0100

    change paperclip defaults for S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,8 +93,8 @@ Rails.application.configure do
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
       s3_region: ENV.fetch('AWS_REGION'),
-      Paperclip::Attachment.default_options[:url] = ':s3_domain_url',
-      Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+      url: ':s3_domain_url',
+      path: '/:class/:attachment/:id_partition/:style/:filename'
     }
   }
 
