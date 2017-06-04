1ef9b83c2ba5e92c7988a81fd79ffd3ef5249ae8
Timothy Robinson <timmyrobby@gmail.com>
2017-06-04 12:12:55 +0100

    Creates and styles navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   include Clearance::User
-  PROFILE_PIC_SIZES = { thumb: 15, small: 50, medium: 150, large: 300 }
+  PROFILE_PIC_SIZES = { nav: 40, small: 50, medium: 150, large: 300 }
   validates_presence_of :name, :username
   has_many :posts
   has_many :comments
