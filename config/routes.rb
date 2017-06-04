4ccaa4ecb0e1e49b4cdca1d595cabf675773db1d
danielemanuel <daniel.costea@vkernel.ro>
2017-06-04 16:21:45 +0100

    User has the ability to like a picture

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
   devise_for :users
-    resources :pics
+    resources :pics do
+      member do
+        put "like", to: "pics#upvote"
+      end
+    end
+
     root "pics#index"
 end
