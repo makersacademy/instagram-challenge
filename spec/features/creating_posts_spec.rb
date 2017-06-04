68a4858648ae433da590d200119012e0587fbda4
Paul Kassar <paulkassar@gmail.com>
2017-06-04 16:49:44 +0100

    controller

---
+++ spec/features/creating_posts_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper.rb'
+
+feature 'Creating posts' do
+  scenario 'can create a post' do
+    visit '/'
+    click_link 'New post'
+    attach_file('Image', "spec/files/images/guitar1.jpg")
+    fill_in 'Caption', with: 'FIRST POST'
+    click_button 'Create'
+    expect(page).to have_content('#firstguitar')
+    expect(page).to have_css("img[src*='guitar1.jpg']")
+  end
+end
