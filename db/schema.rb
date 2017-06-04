4e0a438cffdc27bece180e11dc7a7114db0a1a88
danielemanuel <daniel.costea@vkernel.ro>
2017-06-04 12:20:00 +0100

    User can have multiple pictures , pictures belongs to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170604104750) do
+ActiveRecord::Schema.define(version: 20170604110851) do
 
   create_table "pics", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_pics_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
