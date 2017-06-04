617f13510ecb97f156456fff59edcb7f03817b85
Chris Mitchell <chrisjmit@gmail.com>
2017-06-04 22:53:04 +0100

    display images solved, basic foramtting display added to main

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   config.paperclip_defaults = {
   :storage => :s3,
   :s3_region => 'eu-west-2',
-  :url => 's3_domain_url',
+  :url => ':s3_domain_url',
   :path => "/:class/:attachment/:id_partition/:style/:filename",
   :s3_credentials => {
     :bucket => ENV['S3_BUCKET_NAME'],
