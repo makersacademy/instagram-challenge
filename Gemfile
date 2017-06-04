fa6e9a73675a4875a8f4a94376a3b103b5c75053
Chris Mitchell <chrisjmit@gmail.com>
2017-06-04 23:53:11 +0100

    content added

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
+gem 'acts_as_votable', '~> 0.10.0'
 gem 'aws-sdk'
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -61,6 +61,18 @@ class PostsController < ApplicationController
     end
   end
 
+  def upvote
+    @post = Post.find(params[:id])
+    @post.upvote_by current_user
+    redirect_to :posts
+  end
+
+  def downvote
+    @post = Post.find(params[:id])
+    @post.downvote_by current_user
+    redirect_to :posts
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
