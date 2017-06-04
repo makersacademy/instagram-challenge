fe5b37697079e6c9cef57b9e39329c479cbec3f7
Paula Muldoon <pmuldoon@umich.edu>
2017-06-04 10:38:39 +0100

    photos can be added and rendered

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -69,6 +69,6 @@ class PhotosController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def photo_params
-      params.require(:photo).permit(:title)
+      params.require(:photo).permit(:title, :image)
     end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,5 @@
 class Photo < ApplicationRecord
-  has_attached_file :image, styles =>  { large: '600x600>', medium: "300x300>", thumb: "150x150#" }
+  has_attached_file :image, styles: { large: '600x600>', medium: "300x300>", thumb: "150x150#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
 end
