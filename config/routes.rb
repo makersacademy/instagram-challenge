c0f8413295626ba96bcfa99719578aa5ca6b808f
SimonTanner <simonthetanner@hotmail.com>
2017-06-04 17:40:11 +0100

    Adds welcome page with links for sign up, sign in and sign out. Welcome page displays username if logged in

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+get '/welcome', to: 'welcome#index'
+
+get '/', to: 'welcome#index'
+
+resource :users
+
 end
