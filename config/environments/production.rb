ed74ee2361e87b9c9e0ec80f2248bfc522447431
Adam Snow <adamjohnsnow@icloud.com>
2017-06-04 16:46:28 +0100

    change paperclip defaults for S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,6 +93,8 @@ Rails.application.configure do
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
       s3_region: ENV.fetch('AWS_REGION'),
+      :s3_host_name => "s3-eu-west-2.amazonaws.com",
+      :url => ":s3_host_name",
     }
   }
 
