4f01ef95d9d2f39d292a902a5f13542c962b4ea8
JayWebDevCom <jaiye.s@gmail.com>
2017-06-05 21:39:24 +0100

    add passing sign-in and sign-up tests

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -3,15 +3,17 @@ require './spec/helpers/feature_helper'
 
 feature 'Making posts' do
 
+  scenario 'is not possible by unregistered users' do
+    visit '/'
+    expect(page).not_to have_link 'New Post'
+  end
+
   scenario 'is possible by registered users' do
     sign_up
     click_link 'New Post'
     expect(current_path).to eq new_post_path
-  end
-
-  scenario 'is not possible by unregisteredusers' do
-    visit '/'
-    expect(page).not_to have_link 'New Post'
+    fill_in: 'text', with: 'My favourite actor Muldoon'
+    attach_file('ok', File.absolute_path('/Users/Jaiye/Desktop/Muldoon.png')
   end
 
 end
