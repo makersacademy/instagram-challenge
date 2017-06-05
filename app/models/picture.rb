74d2abcfd9267bf0878208437aee7b0b4c7dea61
anaalta <suciuanamaria@yahoo.com>
2017-06-05 10:04:22 +0200

    some css-ing

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,6 +3,6 @@ class Picture < ApplicationRecord
 
   has_many :comments
 
-  has_attached_file :image, styles: {unique: "300x300" }
+  has_attached_file :image, styles: {unique: "400x400" }
   validates_attachment_content_type :image, :content_type => ['image/jpg', 'image/jpeg', 'image/png']
 end
