41af74a15f0ef5e84963338772d34679c646b3e1
anaalta <suciuanamaria@yahoo.com>
2017-06-05 10:24:35 +0200

    center the pictures and leave it ugly for now

--- db/migrate/20170603145906_create_pictures.rb
+++ db/migrate/20170603145906_create_pictures.rb
@@ -2,7 +2,7 @@ class CreatePictures < ActiveRecord::Migration[5.1]
   def change
     create_table :pictures do |t|
       t.string :title
-      t.attachment :image
+      t.has_attached_file :image
 
       t.timestamps
     end
