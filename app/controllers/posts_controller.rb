3c89788139961f629ffbc20db279061bd6a35fc6
Mário Ribeiro <mail@marioribeiro.com>
2017-07-01 19:19:04 +0100

    Update the controller to enable post creation
    - Update form
    - Create post_params method and permit description

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,22 @@
 class PostsController < ApplicationController
+
   def index
     @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    Post.create(post_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:description)
+  end
+
 end
