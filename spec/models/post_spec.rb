052aceeb2fdf4235fe71295dff6d51ed7e15f0a3
Mário Ribeiro <mail@marioribeiro.com>
2017-07-01 21:37:48 +0100

    Unit test for image presence validation

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  #pending "add some examples to (or delete) #{__FILE__}"
+  it 'is not valid to submit a post without image' do
+    post = Post.new(description: 'Something very cool')
+    expect(post).to have(1).error_on(:image)
+    expect(post).not_to be_valid
+  end
 end
