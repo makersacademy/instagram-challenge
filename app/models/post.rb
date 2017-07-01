9e7a88a98c0b17f84779bb884499d1abb62f03de
Mário Ribeiro <mail@marioribeiro.com>
2017-07-01 20:08:03 +0100

    Refactor for better reading

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
-  has_attached_file :image, styles: { large: "750x750>", thumb: "508x508>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, 
+    styles: { large: "750x750>", thumb: "508x508>" }, 
+    default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
