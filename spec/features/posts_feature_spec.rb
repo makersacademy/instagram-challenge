80ec8fda7968571c4c832d3749d5f6aadb6f3d1e
Mário Ribeiro <mail@marioribeiro.com>
2017-07-01 18:55:46 +0100

    Add initial test for posts (containing only a description)

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -1,11 +1,29 @@
 require 'rails_helper'
 
 feature 'posts' do
+
   context 'no posts available' do
+
     scenario 'should display a link to add a post' do
       visit ('/')
       expect(page).to have_content('No posts available')
       expect(page).to have_content('Add Post')
     end
+
   end
+
+  context 'posts have been added' do
+
+    before do
+      Post.create(description: 'My fantastic dinner')
+    end
+
+    scenario 'display posts' do
+      visit '/'
+      expect(page).to have_content('My fantastic dinner')
+      expect(page).not_to have_content('No posts available')
+    end
+
+  end
+
 end
