e23a19497855ff292d2ae67e5f41135456de66c3
Mário Ribeiro <mail@marioribeiro.com>
2017-07-01 23:32:38 +0100

    Add test for sign in, sign out and sign up link when the user is not signed in

---
+++ spec/features/users_feature_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+feature 'user can sign in and sign out' do
+  context 'user not signed in' do
+    it 'should see the sign in link' do
+      visit '/'
+      expect(page).to have_link 'Sign in'
+      expect(page).to have_link 'Sign up'
+    end
+
+    it 'should not see the link sign out' do
+      visit '/'
+      expect(page).not_to have_link 'Sign out'
+    end
+  end
+end
