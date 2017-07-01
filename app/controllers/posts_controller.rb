437cc1bde1be1fac1813ff6be956926ed5a6edb7
Mário Ribeiro <mail@marioribeiro.com>
2017-07-01 22:18:03 +0100

    Update create to handle errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,13 +8,17 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(post_params)
-    redirect_to root_path
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   private
 
   def post_params
-    params.require(:post).permit(:description,:image)
+    params.require(:post).permit(:description, :image)
   end
 end
