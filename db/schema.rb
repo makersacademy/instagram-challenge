3cbf052f8157bdb485e0996d34c857844d8053f5
“rogrenke” <roland@rolandgrenke.com>
2017-07-02 22:27:13 +0100

    Sets up Clearance

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170702102744) do
+ActiveRecord::Schema.define(version: 20170702211646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "posts", force: :cascade do |t|
-    t.text "description"
-    t.bigint "user_id", null: false
-    t.string "image_file_name", null: false
-    t.string "image_content_type", null: false
-    t.integer "image_file_size", null: false
-    t.datetime "image_updated_at", null: false
-    t.index ["user_id"], name: "index_posts_on_user_id"
-  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -36,5 +26,4 @@ ActiveRecord::Schema.define(version: 20170702102744) do
     t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
-  add_foreign_key "posts", "users"
 end
