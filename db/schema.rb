9a2bdfeb19c62c418991c04bcb35bd3bc8305a5f
Simon Ashbery <si.ashbery@gmail.com>
2017-07-02 15:33:16 +0100

    Adds user model without clearance

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170701171853) do
+ActiveRecord::Schema.define(version: 20170702143204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 20170701171853) do
   create_table "photos", force: :cascade do |t|
     t.string "title"
     t.string "image_file"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "display_name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :user do
+    display_name "MyString"
+    email "MyString"
+    password_digest "MyString"
+  end
+end
