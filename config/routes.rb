4b6f955586d818052be59f94bec30630b234c995
Panteha <panteha.ahmadi@gmail.com>
2017-07-02 10:12:53 +0100

    Added route for comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :posts
-  root 'posts#index'  
+  resources :posts do
+    resources :comments
+end
+  root 'posts#index'
 end
