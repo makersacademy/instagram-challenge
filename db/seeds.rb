a67a63ef9a9d20ea59efaf08fa4eaa53f972fbbc
Christopher Harrop <bannastre@gmail.com>
2017-07-02 12:05:38 +0100

    Add navbar and styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email: 'bann_user@instabann.co', password: 'weakpassword1234')
+Photo.create(name: 'Sunset Forest', photo_file: 'https://c.tadst.com/gfx/750w/sunrise-sunset-sun-calculator.jpg?1', user_id: 1)
+Photo.create(name: 'Sunset Forest', photo_file: 'http://t.wallpaperweb.org/wallpaper/nature/1600x1200/Half_Dome_From_Olmstead_Point_Yosemite_National_Park_California.jpg', user_id: 1)
