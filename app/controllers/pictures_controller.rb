7ca6849734ec73a9ebe96ea3cf83d37b4b2bce4a
Charlotte Fereday <cbrfereday@gmail.com>
2017-07-02 14:27:30 +0100

    Description from the form can now be saved to the database

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -10,6 +10,18 @@ class PicturesController < ApplicationController
     @picture = Picture.new
     render :new
   end
-  
+
+  def create
+    saved_pic = Picture.create(picture_params).save
+    puts "THIS IS THE SAVED PIC"
+    puts saved_pic
+    redirect_to action: "show"
+  end
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:description)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: "pictures#show"
   get '/new', to: "pictures#new"
+  post '/new', to: "pictures#create"
 end
