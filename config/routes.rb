2aff9f537900df3cb2e19993008a391e6789d9e5
Christopher Harrop <bannastre@gmail.com>
2017-07-02 13:25:47 +0100

    Style awesomely - closes #5

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  
+
   resources :photos do
     resources :comments
   end
 
-  root 'welcome#index'
+  root 'photos#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
