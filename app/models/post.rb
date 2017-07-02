e4be946905d9d54b42a2407589031af5306127a6
Panteha <panteha.ahmadi@gmail.com>
2017-07-02 15:20:40 +0100

    Added like model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ApplicationRecord
   has_attached_file :image, styles: { :medium => "640x" }
   validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
   has_many :comments
+  has_many :likes
+  has_many :users, through: :likes
 end
