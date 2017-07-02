fe6cbaac85f375830860f2b0fefb67ce6a76524e
elenamorton <elena.uleia@gmail.com>
2017-07-02 17:34:21 +0100

    Adding posts to a user

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, controller: 'users', only: 'create'
   
+#  as :user do
+#    get 'login', to: 'clearance/sessions#new'
+#    get 'register', to: 'clearance/users#new'
+#    delete 'logout', to: 'clearance/sessions#destroy'
+#  end
+  
+#  clearance_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
+#  
   # resources :users, controller: "clearance/users", only: [:create] do
   #   resource :password,
   #     controller: "clearance/passwords",
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170701182303) do
+ActiveRecord::Schema.define(version: 20170702163127) do
 
   create_table "posts", force: :cascade do |t|
     t.text "description"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20170701182303) do
     t.string "image_content_type"
     t.integer "image_file_size"
     t.datetime "image_updated_at"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
