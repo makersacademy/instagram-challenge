7fc0d9495354c0ae40e1f478de7bfe181547a506
Panteha <panteha.ahmadi@gmail.com>
2017-07-02 18:00:13 +0100

    Added flash

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,14 @@
 class LikesController < ApplicationController
 
   def create
-    @like = Like.create(post_id: params[:post_id],
-                        user_id: current_user.id)
+    @like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
+    if !@like
+      @like = Like.create(post_id: params[:post_id],
+                          user_id: current_user.id)
+    else
+      flash[:post_id] = params[:post_id]
+      flash[:notice] = "You already liked this photo!"
+    end
     redirect_to posts_path
   end
 
