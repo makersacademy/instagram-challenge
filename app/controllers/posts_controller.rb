ea9d709996eb8351ed059e0c8894a24ea94062cf
“rogrenke” <roland@rolandgrenke.com>
2017-07-03 06:27:35 +0100

    creates /index and /view controller for posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,12 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
-
   root to: "welcome#show"
+
+  resources :posts
 end
