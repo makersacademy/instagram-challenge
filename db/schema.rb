d0eca9c901f2b5801bdd82dbddeced386c282f62
wemmm <jenny.wem@gmail.com>
2017-07-03 22:50:40 +0100

    Adds show view to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170702100710) do
+ActiveRecord::Schema.define(version: 20170703211727) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "text"
