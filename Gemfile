e65f832f31ee01f9ee7884b024a10c5997e3db25
Kavita <kkavita92@gmail.com>
2017-07-03 01:02:27 +0100

    - Implement MiniMagick   - Ensure photos are resized to 640x640 when being uploaded   - Change styling to be responsive to image size

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ end
 gem 'bootstrap-sass'
 gem 'carrierwave'
 gem "clearance"
+gem 'mini_magick'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.2'
 # Use sqlite3 as the database for Active Record
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,7 +2,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -12,6 +12,10 @@ class ImageUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  version :thumb do
+    process resize_to_fill: [640, 640]
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
