205098eabd91f446cf5708527da1cbc295e70a91
“rogrenke” <roland@rolandgrenke.com>
2017-07-03 07:13:12 +0100

    Creates /create route for posts enabling creation and saving of posts to db, setting posts/index as root

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
 
+  before_action :require_login
+
   def index
     @posts = Post.all
   end
@@ -9,8 +11,26 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to post_path(@post), notice: "Post created!"
+    else
+      p params
+      @errors = @post.errors.full_messages
+      render :new
+    end
+
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :description)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: "welcome#show"
+  root to: "posts#index"
 
   resources :posts
 end
