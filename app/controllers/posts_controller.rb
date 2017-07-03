369207757cb0690173b79462339ec3884c7cc002
“rogrenke” <roland@rolandgrenke.com>
2017-07-03 07:46:05 +0100

    Implements Paperclip upload, shows picture on /show view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -30,7 +30,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :description)
+    params.require(:post).permit(:title, :description, :image)
   end
 
 end
