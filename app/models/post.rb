eae3095ab6ea8e6719f9aaf66ff473ca96cf531d
“rogrenke” <roland@rolandgrenke.com>
2017-07-08 20:46:34 +0100

    Redesign of post index, 3xn square design, upload and cropping of larger pictures now possible

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
 
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: {large: "600x600#", medium: "300x300#", thumb: "100x100#" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
 end
