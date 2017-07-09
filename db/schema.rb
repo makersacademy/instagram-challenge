70006852971072f1f7ae411e7a68eecbfafc1b39
y0m0 <lixi.daniele@gmail.com>
2017-07-09 20:43:13 +0100

    Adds comment model - belongs_to user and post

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170708161051) do
+ActiveRecord::Schema.define(version: 20170709193806) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "message"
+    t.bigint "post_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title", null: false
@@ -37,5 +47,7 @@ ActiveRecord::Schema.define(version: 20170708161051) do
     t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
---
+++ spec/factories/comments.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :comment do
+    message "MyText"
+    post nil
+    user nil
+  end
+end
