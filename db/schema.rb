53116628223cd5a2d8a0b2b9938b29ec7d6a7c0f
“rogrenke” <roland@rolandgrenke.com>
2017-07-13 12:24:58 +0100

    changes db migrations to work with carrierwave

--- db/schema.rb
+++ db/schema.rb
@@ -35,11 +35,8 @@ ActiveRecord::Schema.define(version: 20170709231441) do
 
   create_table "posts", force: :cascade do |t|
     t.text "description", null: false
+    t.string "image", null: false
     t.bigint "user_id", null: false
-    t.string "image_file_name"
-    t.string "image_content_type"
-    t.integer "image_file_size"
-    t.datetime "image_updated_at"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
