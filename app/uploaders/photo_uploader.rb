c454af9056333457f0a08c5fca5e21aa0d7fbed8
“rogrenke” <roland@rolandgrenke.com>
2017-07-13 17:17:33 +0100

    Changes Carrierwave uploader settings so that pictures are squares

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -39,15 +39,15 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process resize_to_fit: [50, 50]
+    process resize_to_fill: [50, 50]
   end
 
   version :medium do
-    process resize_to_fit: [300, 300]
+    process resize_to_fill: [300, 300]
   end
 
   version :large do
-    process resize_to_fit: [600, 600]
+    process resize_to_fill: [600, 600]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
