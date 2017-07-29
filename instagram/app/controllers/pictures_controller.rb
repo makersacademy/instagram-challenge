cc437915a923684d274b6a6cf42f102ee8ac42f3
Jasper King-Harman <j.kingharman@googlemail.com>
2017-07-29 19:47:48 +0100

    Gettings gems and doing config for image-hosting

--- instagram/app/controllers/pictures_controller.rb
+++ instagram/app/controllers/pictures_controller.rb
@@ -1,6 +1,28 @@
 class PicturesController < ApplicationController
 
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
     @pictures = Picture.all
+  end
+
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    @pic = User.find(current_user.id).pictures.new(pic_params)
+    if @pic.save
+      redirect_to pictures_url
+    else
+      flash[:error].keep = pic.errors.full_messages
+      redirect_to new_picture_url
+    end
+  end
+
+  private
+
+  def pic_params
+    params.require(:picture).permit(:caption)
   end
 end
---
+++ instagram/spec/features/pictures_are_present_spec.rb
@@ -0,0 +1,68 @@
+
+
+require "rails_helper"
+
+
+RSpec.feature "Pictures", type: :feature do
+
+  context "snaps are in db"  do
+
+    before do
+      visit '/'
+      sign_up
+      User.first.pictures.create(caption: "hashtag").save
+      User.first.pictures.create(caption: "secondhashtag").save
+    end
+
+    context "index" do
+
+    scenario "should display them" do
+      visit '/'
+      expect(page).to have_content("hashtag")
+      expect(page).to have_content("secondhashtag")
+    end
+
+    scenario "in reverse chrono order" do
+        visit '/'
+        expect('secondhashtag').to appear_before('hashtag')
+    end
+
+  end
+
+  end
+
+  context "no snaps in db" do
+
+    context "when an account holder" do
+
+    scenario "can add pics" do
+      visit '/'
+      sign_up
+      click_on "Post Picture"
+      fill_in "picture_caption", with: "hashtagthree"
+      click_button "Post"
+      expect(page).to have_content("hashtagthree")
+    end
+
+
+  # return to provide more comp error checks on creation
+
+  end
+
+  context "when not an account holder" do
+
+  scenario "cannot add pics" do
+    visit '/'
+    click_on "Post Picture"
+    expect(page).to have_content("You need to sign in or sign up before continuing.")
+  end
+
+end
+
+  end
+
+
+
+
+
+end
