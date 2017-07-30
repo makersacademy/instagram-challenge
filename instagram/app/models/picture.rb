44b287cb2349d3970e1971255a49c55fffdc8e69
Jasper King-Harman <j.kingharman@googlemail.com>
2017-07-30 19:29:24 +0100

    Beutifying html

--- instagram/app/models/picture.rb
+++ instagram/app/models/picture.rb
@@ -3,7 +3,7 @@ class Picture < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes
 
-  has_attached_file :image, styles: { medium: "700x", thumb: "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, styles: { medium: "700x", thumb: "250x250>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   validates :user_id, presence: true
