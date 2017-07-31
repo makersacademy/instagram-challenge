bb4a26de62f31dc911ca5c51e26a541196f87545
tbscanlon <thomas.scanlon@outlook.com>
2017-07-31 01:25:23 +0100

    Add uploading images

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,60 @@
+class PicturesController < ApplicationController
+  before_action :set_picture, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @pictures = Picture.all
+  end
+
+  def show
+  end
+
+  def new
+    @picture = Picture.new
+  end
+
+  def edit
+  end
+
+  def create
+    @picture = Picture.new(picture_params)
+
+    respond_to do |format|
+      if @picture.save
+        format.html { redirect_to @picture, notice: 'Picture was successfully created.' }
+        format.json { render :show, status: :created, location: @picture }
+      else
+        format.html { render :new }
+        format.json { render json: @picture.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @picture.update(picture_params)
+        format.html { redirect_to @picture, notice: 'Picture was successfully updated.' }
+        format.json { render :show, status: :ok, location: @picture }
+      else
+        format.html { render :edit }
+        format.json { render json: @picture.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @picture.destroy
+    respond_to do |format|
+      format.html { redirect_to pictures_url, notice: 'Picture was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_picture
+      @picture = Picture.find(params[:id])
+    end
+
+    def picture_params
+      params.require(:picture).permit(:description, :image)
+    end
+end
---
+++ app/uploaders/image_uploader.rb
@@ -0,0 +1,25 @@
+class ImageUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  storage :file
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  # Provide a default URL as a default if there hasn't been a file uploaded:
+  # def default_url(*args)
+  #   # For Rails 3.1+ asset pipeline compatibility:
+  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  #
+  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  # end
+
+  version :thumb do
+    process resize_to_fit: [50, 50]
+  end
+
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
+end
