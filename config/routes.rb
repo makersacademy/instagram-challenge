13fddd261e1740ef261ec30f8ced7c06cbe4e953
BDCraven <bencraven@hotmail.com>
2017-08-26 11:29:12 +0100

    Route set to pictures#index and deployed to Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-get 'pictures' => 'pictures#index'
-resources :pictures
+  get 'pictures' => 'pictures#index'
+  resources :pictures
+
+  root 'pictures#index'
 end
