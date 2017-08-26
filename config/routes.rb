e584eb942ad3929b223de9fad9b41079299db5e9
Sapphire Mason-Brown <saphmb@gmail.com>
2017-08-26 16:27:46 +0100

    Rails set-up

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+	# resources :users
+	#
+	# resources :posts do
+	# 	resources :comments
+	# 	resources :likes
+	# end
+end
