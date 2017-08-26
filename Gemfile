7280cf97b8f3940f80bc7305edc864414500fe14
Toby Hamand <tobyhamand@gmail.com>
2017-08-26 22:32:07 +0100

    added time_ago method to post model

--- Gemfile
+++ Gemfile
@@ -35,6 +35,8 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+gem 'octicons_helper'
+
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'capybara', '~> 2.13'
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,36 @@ class Post < ApplicationRecord
   has_attached_file :image, styles: { :medium => "640x640#" }
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+  def long_ago
+    second_difference = (Time.now - created_at).floor
+    minute_difference = second_difference / 60
+    hour_difference = minute_difference / 60
+    day_difference = hour_difference / 24
+    if day_difference > 0
+      if day_difference == 1
+        return "#{day_difference} day ago"
+      else
+        return "#{day_difference} days ago"
+      end
+    elsif hour_difference > 0
+      if hour_difference == 1
+        return "#{hour_difference} hour ago"
+      else
+        return "#{hour_difference} hours ago"
+      end
+    elsif minute_difference > 0
+      if minute_difference == 1
+        return "#{minute_difference} minute ago"
+      else
+        return "#{minute_difference} minutes ago"
+      end
+    elsif second_difference > 0
+      if second_difference < 5
+        return "just now"
+      else
+        return "#{second_difference} seconds ago"
+      end
+    end
+  end
 end
