7d49c45df91d0c13fcda5b8405910d89a972efa6
Toby Hamand <tobyhamand@gmail.com>
2017-08-27 23:12:54 +0100

    reworked root page

--- spec/feature/web_helper.rb
+++ spec/feature/web_helper.rb
@@ -1,6 +1,6 @@
 def login(user)
   visit("/users/sign_in")
-  within(:css, 'form.new_user') do
+  within(:css, 'form.login') do
     fill_in 'user[email]', with: user.email
     fill_in 'user[password]', with: user.password
     click_button 'Log in'
