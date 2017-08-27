20f976119e5226bc2b775cfcf21878e9049fa998
BDCraven <bencraven@hotmail.com>
2017-08-27 13:53:45 +0100

    Update paperclip config default to eu-west-2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,7 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     storage: :s3,
+    s3_host_name: 's3-eu-west-2.amazonaws.com',
     s3_credentials: {
       bucket: ENV.fetch('S3_BUCKET_NAME'),
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
