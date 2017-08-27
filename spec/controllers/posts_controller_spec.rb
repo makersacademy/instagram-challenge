bf44ed8f56ac315e37d15a8b3d23d3986d5e11d3
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-08-27 11:20:25 +0100

    Deleted controller tests. TDDing using feature tests alone for now.

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,13 +1,5 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get posts_index_url
-    assert_response :success
-  end
 
-  test "should get new" do
-    get new_post_url
-    assert_response :success
-  end
 end
