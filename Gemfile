05dceaceaf2fab67c2e46c89187bdada96559893
BDCraven <bencraven@hotmail.com>
2017-08-27 10:20:10 +0100

    User sign up functionality addded with Devise.

--- Gemfile
+++ Gemfile
@@ -31,9 +31,9 @@ gem 'jbuilder', '~> 2.5'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
-
 gem 'paperclip'
 gem 'aws-sdk'
+gem 'devise'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ spec/features/user_management_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+def sign_up(email: 'fred@example.com',
+             password: '123456',
+             password_confirmation: '123456')
+  visit '/users/sign_up'
+  fill_in 'user_email' , with: email
+  fill_in 'user_password', with: password
+  fill_in 'user_password_confirmation', with: password_confirmation
+  click_button 'Sign up'
+end
+
+def sign_in(email: 'fred@example.com',
+             password: '123456')
+  visit '/users/sign_in'
+  fill_in "user_email", with: email
+  fill_in "user_password", with: password
+  click_button 'Log in'
+ end
+
+RSpec.feature "Signup", type: :feature do
+   scenario "New user can sign up" do
+     expect { sign_up }.to change(User, :count).by(1)
+     expect(page).to have_content("Welcome! You have signed up successfully.")
+     expect(User.first.email).to eq 'fred@example.com'
+   end
+
+   scenario 'matching password confirmation is required' do
+     expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+   end
+
+   scenario 'email matches conventions for email' do
+     expect { sign_up(email: 'wrong') }.not_to change(User, :count)
+   end
+ end
+
+RSpec.feature "Signin", type: :feature do
+  scenario "User can sign in" do
+    sign_up
+    click_link "Logout"
+    sign_in
+    expect(page).to have_content("Signed in successfully.")
+  end
+end
+
+RSpec.feature "Signout", type: :feature do
+  scenario "User can sign out" do
+    sign_up
+    click_link 'Logout'
+    expect(page).to have_content("Signed out successfully.")
+  end
+end
