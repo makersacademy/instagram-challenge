1912489ab83a18d3a726e486fc7759277fa40f80
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-08-27 08:55:49 +0100

    Posts displayed in reverse order

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all.reverse
   end
 
   def new
--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -8,6 +8,16 @@ RSpec.feature "Posts", type: :feature do
     expect(page).to have_css 'img.posts-index-picture'
     expect(page).to have_content('Humming birds taking a bath')
   end
+
+  scenario "Posts are displayed in reverse chronological order" do
+    make_a_post
+    make_a_post(picture: Rails.root.join('spec/images/lena_river_siberia.png'),
+                description: 'Endlessly reflecting the sky in its stillness')
+    make_a_post(picture: Rails.root.join('spec/images/greenland.png'),
+                description: 'Painted skies under an Arctic sunset')
+    expect(page.all('.posts-index-description')[0])
+      .to have_content('Painted skies under an Arctic sunset')
+  end
 end
 
 def make_a_post(picture: Rails.root.join('spec/images/humming_birds.png'),
