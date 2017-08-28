eb40f5aaabb88cee329ee2210ea5866e7e0dcacf
James Ransome <jamesransome@msn.com>
2017-08-28 22:11:29 +0100

    users sign up with username and view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_many :posts
+
+  validates :user_name, presence: true, length: { minimum: 6, maximum: 16 }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'posts#index'
-
+  devise_for :users, :controllers => { registrations: 'registrations' }
   resources :posts
-
+  root 'posts#index'
 end
