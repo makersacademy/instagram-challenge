7575283d6adf71a27b4ecc1ccadbdcb1083bc798
Imogen Hardy <imogen.hardy@gmail.com>
2017-08-28 19:59:07 +0100

    force image size

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,11 +3,16 @@ class Photo < ApplicationRecord
   has_many :comments, dependent: :delete_all
   has_many :likes, dependent: :delete_all
   has_and_belongs_to_many :tags
-  has_attached_file :image, styles: { lomo: {
+  has_attached_file :image, styles: { none: {
+                                        geometry: "500x500#"},
+                                      lomo: {
+                                        geometry: "500x500#",
                                         convert_options: "-channel R -level 33% -channel G -level 33%"},
                                       gotham: {
+                                        geometry: "500x500#",
                                         convert_options: "-modulate 120,10,100 -fill '#222b6d' -colorize 20 -gamma 0.5 -contrast -contrast"},
                                       greyscale: {
+                                        geometry: "500x500#",
                                         convert_options: "-grayscale Rec709Luminance"}
                                       }
 
@@ -15,6 +20,6 @@ class Photo < ApplicationRecord
   content_type: { content_type: /\Aimage\/.*\Z/ }
 
   def return_filter
-    filter.to_sym unless filter.nil? || filter == "none"
+    filter.to_sym unless filter.nil?
   end
 end
