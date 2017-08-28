01b9d2b409880ab83f1be52c20e1401dbbfe8fb4
Petra Chocholova <petra.chocholova@gmail.com>
2017-08-28 22:54:15 +0200

    Rails project setup

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'posts/index'
+
+  resources :posts
+  root 'posts#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
