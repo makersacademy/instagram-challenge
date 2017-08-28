4a9a87f7441f4baeee9064801b68382eeaed60c4
James Ransome <jamesransome@msn.com>
2017-08-28 11:26:22 +0100

    posts save with images to db

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,20 @@
 class PostsController < ApplicationController
+  def index
+
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    redirect_to posts_url
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:image, :caption)
+  end
 end
