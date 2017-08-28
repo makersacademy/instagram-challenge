ea415945bf5717564ba0a9809b75713fd5e76ef8
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-08-28 11:16:47 +0100

    Added time ago to posts, refacotored tests in post_spec

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -1,6 +1,22 @@
 require 'rails_helper'
 
 RSpec.feature 'Posts', type: :feature do
+  include ActionView::Helpers::DateHelper
+  let! (:post) do
+    { picture: Rails.root.join('spec/images/humming_birds.png'),
+      description: 'Humming birds taking a bath' }
+  end
+
+  let! (:post2) do
+    { picture: Rails.root.join('spec/images/lena_river_siberia.png'),
+      description: 'Endlessly reflecting the sky in its stillness' }
+  end
+
+  let! (:post3) do
+    { picture: Rails.root.join('spec/images/greenland.png'),
+      description: 'Painted skies under an Arctic sunset' }
+  end
+
   before do
     sign_up
   end
@@ -13,12 +29,17 @@ RSpec.feature 'Posts', type: :feature do
   end
 
   scenario 'Posts are displayed in reverse chronological order' do
-    make_a_post
-    make_a_post(picture: Rails.root.join('spec/images/lena_river_siberia.png'),
-                description: 'Endlessly reflecting the sky in its stillness')
-    make_a_post(picture: Rails.root.join('spec/images/greenland.png'),
-                description: 'Painted skies under an Arctic sunset')
-    expect(page.all('.posts-index-description')[0])
-      .to have_content('Painted skies under an Arctic sunset')
+    make_a_post(post)
+    make_a_post(post2)
+    make_a_post(post3)
+    expect(page.all('.posts-index-description')[0]).to have_content(post3[:description])
+    expect(page.all('.posts-index-description')[1]).to have_content(post2[:description])
+    expect(page.all('.posts-index-description')[2]).to have_content(post[:description])
+  end
+
+  scenario 'Posts display the time when they were made' do
+    time = Time.now
+    make_a_post(post)
+    expect(page.find('.posts-index-time')).to have_content(time_ago_in_words(time).capitalize)
   end
 end
