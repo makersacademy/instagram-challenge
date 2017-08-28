21167bc352d2734b333a1fbaa0965114229cecac
Toby Hamand <tobyhamand@gmail.com>
2017-08-28 23:06:47 +0100

    rewrote javascript logic and html to hide/show form submit button on press

--- app/assets/javascripts/logic.js
+++ app/assets/javascripts/logic.js
@@ -9,16 +9,18 @@
     eventListener: function() {
       var self = this;
       window.addEventListener("submit", function(event){
-        var likehtml = event.target.lastElementChild.outerHTML;
-        var likeid = event.target.lastElementChild.id;
-        var idstring = likeid.substring(4);
-        var likesid = "likes" + idstring;
-        var likeshtmlnumber = Number((document.getElementById(likesid).innerHTML).slice(0, -5));
-        if (likehtml.includes('full-heart')) {
-          event.target.lastElementChild.outerHTML = '<input type="image" src="/assets/empty-heart.svg" id="' + likeid + '">';
+        var idnumber = (event.target.lastElementChild.id).replace(/\D/g,'');
+        var likeid = "like" + idnumber;
+        var unlikeid = "unlike" + idnumber;
+        var likesid = "likes" + idnumber;
+        var likeshtmlnumber = Number((document.getElementById(likesid).innerHTML).replace(/\D/g,''));
+        if (event.target.lastElementChild.id === likeid) {
+          document.getElementById(unlikeid).className = "";
+          document.getElementById(likeid).className = "hide";
           document.getElementById(likesid).innerHTML = String(likeshtmlnumber - 1) + " likes";
-        } else if (likehtml.includes('empty-heart')) {
-          event.target.lastElementChild.outerHTML = '<input type="image" src="/assets/full-heart.svg" id="' + likeid + '">';
+        } else if (event.target.lastElementChild.id === unlikeid) {
+          document.getElementById(unlikeid).className = "hide";
+          document.getElementById(likeid).className = "";
           document.getElementById(likesid).innerHTML = String(likeshtmlnumber + 1) + " likes";
         }
       });
