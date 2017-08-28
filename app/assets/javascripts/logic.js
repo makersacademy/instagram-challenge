48c3db1cbbb733103cc7e7f522374a9f641b4646
Toby Hamand <tobyhamand@gmail.com>
2017-08-28 21:02:12 +0100

    final fix for local

--- app/assets/javascripts/logic.js
+++ app/assets/javascripts/logic.js
@@ -15,10 +15,10 @@
         var likesid = "likes" + idstring;
         var likeshtmlnumber = Number((document.getElementById(likesid).innerHTML).slice(0, -5));
         if (likehtml.includes('full-heart')) {
-          event.target.lastElementChild.outerHTML = '<input type="image" src="/assets/empty-heart-ea0acd42a9e94dd3ce33ebeb85d4d7215712eda964d1aefa71ba27ee63a544f6.svg" id="' + likeid + '">';
+          event.target.lastElementChild.outerHTML = '<input type="image" src="/assets/empty-heart.svg" id="' + likeid + '">';
           document.getElementById(likesid).innerHTML = String(likeshtmlnumber - 1) + " likes";
         } else if (likehtml.includes('empty-heart')) {
-          event.target.lastElementChild.outerHTML = '<input type="image" src="/assets/full-heart-215f068e0159c0e41cfc1873d9f6c6327c929371d0cd403c541fb032c7e87e4e.svg" id="' + likeid + '">';
+          event.target.lastElementChild.outerHTML = '<input type="image" src="/assets/full-heart.svg" id="' + likeid + '">';
           document.getElementById(likesid).innerHTML = String(likeshtmlnumber + 1) + " likes";
         }
       });
