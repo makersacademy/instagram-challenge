e15eb0c46302c2eed35e92b96af86a698fbf347a
Imogen Hardy <imogen.hardy@gmail.com>
2017-08-28 12:32:27 +0100

    photos controller refactoring

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  rescue_from ActionController::InvalidAuthenticityToken do
-    redirect_to root_url
-  end
-
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -22,7 +22,7 @@ class PhotosController < ApplicationController
     @user = current_user
     @photo = @user.photos.new(photo_params)
     if @photo.save
-      tag_creator(tag_params_to_array) if tag_params
+      tag_creator(tag_params.split) if tag_params
       redirect_to profile_path(@user), notice: 'Photo was successfully uploaded.'
     else
       render action: 'new'
@@ -53,20 +53,8 @@ class PhotosController < ApplicationController
     params.require(:photo).permit(:tags)[:tags]
   end
 
-  def tag_params_to_array
-    tag_params.split
-  end
-
   def edit_security_check
     redirect_to root_path unless current_user == Photo.find(params[:id]).user
-  end
-
-  def tag_updater
-    add_tags? ? tag_adder : tag_deleter
-  end
-
-  def add_tags?
-    @photo.tags.length < tag_params_to_array.length
   end
 
   def tag_creator(array)
@@ -74,16 +62,8 @@ class PhotosController < ApplicationController
       .each { |t| @photo.tags << t }
   end
 
-  def tag_adder
-    tag_creator(tag_params_to_array.reject { |t| existing_tags_to_text.include? t })
-  end
-
-  def tag_deleter
+  def tag_updater
     @photo.tags.delete_all
-    tag_creator(tag_params_to_array)
-  end
-
-  def existing_tags_to_text
-    @photo.tags.map { |t| t.text }
+    tag_creator(tag_params.split)
   end
 end
