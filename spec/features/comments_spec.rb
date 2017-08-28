770f264fac512bf2dedca042ec58496fc0585a25
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-08-28 12:55:29 +0100

    Improved comments feature tests to comment on multiple posts, and with multiple comments on a given post

--- spec/features/comments_spec.rb
+++ spec/features/comments_spec.rb
@@ -1,19 +1,50 @@
 require 'rails_helper'
 
 RSpec.feature 'Comments', type: :feature do
+  let! (:post) do
+    { picture: Rails.root.join('spec/images/humming_birds.png'),
+      description: 'Humming birds taking a bath' }
+  end
+
+  let! (:post2) do
+    { picture: Rails.root.join('spec/images/lena_river_siberia.png'),
+      description: 'Endlessly reflecting the sky in its stillness' }
+  end
+
+  # First post (displayed second, hence index 1) has two comments
+  let! (:post_comment) do
+    { comment: 'Adorable!', post_index: 1 }
+  end
+
+  let! (:post_comment2) do
+    { comment: 'Wow...', post_index: 1 }
+  end
+
+  # Second post (displayed first, hence index 0) has one comment
+  let! (:post2_comment) do
+    { comment: 'What a fantastic picture', post_index: 0 }
+  end
+
   before do
     sign_up
-    make_a_post
+    make_a_post(post)
+    make_a_post(post2)
   end
 
   scenario 'Comments can be added to a post' do
-    make_a_comment
-    make_a_comment(comment: 'Wow...')
+    make_a_comment(post_comment)
+    make_a_comment(post_comment2)
+    make_a_comment(post2_comment)
+
     expect(current_path).to eq('/posts')
     expect(page.status_code).to eq(200)
-    within('.comments-comments') do
-      expect(page.all('.comments-comment')[0]).to have_content('Adorable!')
-      expect(page.all('.comments-comment')[1]).to have_content('Wow...')
+
+    within(page.all('.comments-comments')[1]) do
+      expect(page.all('.comments-comment')[0]).to have_content(post_comment[:comment])
+      expect(page.all('.comments-comment')[1]).to have_content(post_comment2[:comment])
+    end
+    within(page.all('.comments-comments')[0]) do
+      expect(page.all('.comments-comment')[0]).to have_content(post2_comment[:comment])
     end
   end
 end
--- spec/helpers/comments.rb
+++ spec/helpers/comments.rb
@@ -1,6 +1,8 @@
 module CommentHelpers
-  def make_a_comment(comment: 'Adorable!')
-    fill_in 'comment_body', with: comment
-    click_button '⏎'
+  def make_a_comment(comment: 'Adorable!', post_index: 0)
+    within(page.all('.comments-new')[post_index]) do
+      fill_in 'comment_body', with: comment
+      click_button '⏎'
+    end
   end
 end
