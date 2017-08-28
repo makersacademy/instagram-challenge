3a2a1076ee8d5eb68d1634d2fb4a73ff3726bc42
James Ransome <jamesransome@msn.com>
2017-08-28 15:46:37 +0100

    frontend improvement

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ git_source(:github) do |repo_name|
 end
 
 gem 'devise'
+gem 'bootstrap-sass', '~> 3.3.6'
 gem 'simple_form'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.3'
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   validates :image, presence: true
 
-  has_attached_file :image, styles: { :medium => "640x" }
+  has_attached_file :image, styles: { medium: "40x" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
 end
