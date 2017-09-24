35ae06dc8939aa44dbfb07121edd867e471ce864
rolandosorbelli <rolando.sorbelli@gmail.com>
2017-09-24 09:59:05 +0100

    fixed bugs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,10 +22,10 @@ Rails.application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
-      bucket: ENV('S3_BUCKET_NAME'),
-      access_key_id: ENV('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV('AWS_SECRET_ACCESS_KEY'),
-      s3_region: ENV('AWS_REGION'),
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
     }
   }
 
