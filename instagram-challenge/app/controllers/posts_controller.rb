422e34078bd34d50cf93603026b333fe98755a77
robincollins <racollins1@me.com>
2017-10-22 00:19:40 +0100

    saving new post but not rendering image. also need to start building tests now and following TDD

--- instagram-challenge/app/controllers/posts_controller.rb
+++ instagram-challenge/app/controllers/posts_controller.rb
@@ -1,12 +1,23 @@
 class PostsController < ApplicationController
 
-  def new
 
+  def show
+    @post = Post.find(params[:id])
   end
 
+  def new
+  end
 
   def create
-    render plain: params[:post].inspect
+    @post = Post.new(post_params)
+
+    @post.save
+    redirect_to @post
   end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :image)
+    end
 
 end
--- instagram-challenge/db/migrate/20171021224224_create_posts.rb
+++ instagram-challenge/db/migrate/20171021224224_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[5.1]
   def change
     create_table :posts do |t|
       t.string :title
-      t.image :image
+      t.text :image
 
       t.timestamps
     end
