76229f282be4fef32f7a1d31dddfea0aa9967eb6
Pablo <paul.ladiv123@gmail.com>
2017-11-18 23:43:43 +0000

    add ability to destroy comments associated to a single post/picture

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,13 @@ class CommentsController < ApplicationController
     redirect_to post_path(@post)
   end
 
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:commenter, :body)
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@
 
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
   mount_uploader :image, ImageUploader
   validates :title, presence: true, length: { minimum: 2 }
   validates :body, presence: true
