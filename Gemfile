992e34365ed38d8625e4e30ac96033b64dae4fd4
Olivia Beresford <oliviaberesford@hotmail.co.uk>
2017-11-18 16:20:21 +0000

    displaying posts on index page

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'simple_form'
 gem 'paperclip', '~> 4.2'
 gem 'pg'
+# gem "factory_girl_rails"
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ spec/factories/post.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  # factory :post do
+  #   caption "MyString"
+  # end
+  factory :post do
+    caption "nofilter"
+    image Rack::Test::UploadedFile.new(Rails.root + 'spec/files/images/foodart.jpg', 'image/jpg')
+  end
+end
