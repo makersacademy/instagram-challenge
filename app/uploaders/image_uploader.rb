5f702756f358d23503dd52346d19f940f2823718
Tim Jones <timjones10@hotmail.com>
2017-11-18 21:44:29 +0000

    Show HTML File for Picture updated to show thumb. Currently does not but shows file path instead.

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -32,9 +32,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
+  version :thumb do
+    process :resize_to_fit => [50, 50]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
