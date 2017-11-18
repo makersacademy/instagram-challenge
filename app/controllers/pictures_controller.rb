1101843042cc674861b40289e8bdbedd23318ca6
Aram Simonian <aram.contacts@gmail.com>
2017-11-18 20:19:08 +0000

    Altered create action

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,7 +2,18 @@ class PicturesController < ApplicationController
   def new
   end
 
+  def show
+    @picture = Picture.find(params[:id])
+  end
+
   def create
-    render plain: params[:picture].inspect
+    @picture = Picture.new(picture_params)
+    @picture.save
+    redirect_to @picture
+  end
+
+private
+  def picture_params
+    params.require(:picture).permit(:title, :text)
   end
 end
