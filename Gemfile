dac6129e2f6355d4523cb227b32d63bdde990201
Pablo <paul.ladiv123@gmail.com>
2017-11-18 17:33:47 +0000

    add mini magic gem, add CSS style for the home page

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+gem 'mini_magick'
 gem 'carrierwave', '~> 1.0'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.4'
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,5 +1,11 @@
-class ImageUploader < CarrierWave::Uploader::Base
 
+
+class ImageUploader < CarrierWave::Uploader::Base
+include CarrierWave::MiniMagick
+
+version :thumb do
+    process resize_to_fill: [350, 350]
+end
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
