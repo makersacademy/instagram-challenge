859ed16e638c59e5877388e3845d279d59c85f5b
Pablo <paul.ladiv123@gmail.com>
2017-11-18 21:45:05 +0000

    add devise gem for user authentication

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@
 
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update]
-
+  before_action :authenticate_user! , except: [ :index, :show ]
   def index
     @posts = Post.order('created_at DESC')
   end
