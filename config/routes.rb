0cee935be7052ac5661d76da6749ecd97ad23eac
Olivia Beresford <oliviaberesford@hotmail.co.uk>
2017-11-19 13:45:27 +0000

    adding a welcome/landing page need to change user log in to allow to see landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
   devise_for :users
-  get 'posts/index'
+  # get 'posts/index'
   resources :posts
-  root  'posts#index'
+  # root  'posts#index'
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
