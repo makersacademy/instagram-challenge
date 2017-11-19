168fec541685e84e7334cf9059697d95b87141e1
Pablo <paul.ladiv123@gmail.com>
2017-11-19 13:39:58 +0000

    add new rating column to comment, display stars on each comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171118224123) do
+ActiveRecord::Schema.define(version: 20171119124730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20171118224123) do
     t.bigint "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "rating"
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
