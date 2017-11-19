0d13dab2dbfc928c67c0312ab3d72d66f48d87fd
mormolis <george.lamprakis@gmail.com>
2017-11-19 12:32:53 +0000

    adds crud operations to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,6 +26,25 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy!
+    redirect_to '/'
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(location: params[:post][:location], description: params[:post][:description])
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
   private
   def post_params
     params.require(:post).permit(:location, :description, :photo)
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,13 @@
 class Post < ApplicationRecord
+  before_destroy :destroy_likes_and_comments
+  
   belongs_to :user
   has_many :likes
   has_many :comments
   mount_uploader :photo, PhotoUploader
+
+  def destroy_likes_and_comments
+    self.likes.destroy_all  
+    self.comments.destroy_all 
+  end
 end
