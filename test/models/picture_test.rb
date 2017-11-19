2d04b3e9571f45d9561154ed8ab8ba180182d7d0
Aram Simonian <aram.contacts@gmail.com>
2017-11-19 15:07:11 +0000

    Added some picture model tests

--- test/models/picture_test.rb
+++ test/models/picture_test.rb
@@ -4,4 +4,19 @@ class PictureTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test 'picture must have a title' do
+    picture = Picture.new
+    assert_not picture.save
+  end
+  test 'picture must have descriptive text' do
+    picture = Picture.new
+    picture.title = 'fake title'
+    assert_not picture.save
+  end
+  test 'picture with title and descriptive text will save' do
+    picture = Picture.new
+    picture.title = 'fake title'
+    picture.text = 'fake description'
+    assert picture.save
+  end
 end
