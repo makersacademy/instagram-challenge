27bcb4ffea588377799d9ead0728275f8c2690f8
Tim Jones <timjones10@hotmail.com>
2017-11-19 17:11:23 +0000

    A User can onlu edit or delete pictures that they have uploaded

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,6 @@
 class PicturesController < ApplicationController
   before_action :set_picture, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /pictures
   # GET /pictures.json
