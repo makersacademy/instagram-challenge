e52097ecf8c07133700840abaf3a01b6ad7bb38d
David Halewood <davidnhalewood@hotmail.com>
2017-11-19 18:04:32 +0000

    Adding to caption length

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,6 +2,6 @@ class Pin < ApplicationRecord
   acts_as_votable
   belongs_to :user
   validates :image, presence: true
-  validates :caption, presence: true, length: { maximum: 16}
+  validates :caption, presence: true, length: { maximum: 20}
   has_many :comments, dependent: :destroy
 end
