cd3dc33acf85c6b8fd290dd81c39ee7d76157f12
David Halewood <davidnhalewood@hotmail.com>
2017-11-19 13:31:26 +0000

    Syntax corrections

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,5 @@
 class Pin < ApplicationRecord
-  belongs_to :user 
+  belongs_to :user
   validates :image, presence: true
   validates :caption, presence: true, length: { maximum: 16}
   has_many :comments, dependent: :destroy
