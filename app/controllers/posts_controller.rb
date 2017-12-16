9a82a9838cb411dd3d7bec705d968bd4d3f528a8
Lewis <lewisyoul@gmail.com>
2017-12-16 14:29:37 +0000

    add destroy functionality to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -32,6 +32,13 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
+  end
+
   def show
     @post = Post.find(params[:id])
   end
