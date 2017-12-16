8e783e673a43fa98d4b1f8fcc5fd57c174e73540
antonio <antoniobelmardacosta@gmail.com>
2017-12-16 21:19:24 +0000

    Add photo controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,4 @@
 Rails.application.routes.draw do
+  get 'photos/index'
+  root 'photos#index'
 end
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 feature 'testing infrastructure' do
   scenario 'testing infrastructure is working' do
     visit '/'
-    expect(page).to have_content("Yay! You’re on Rails!")
+    expect(page).to have_content("Hello")
   end
 end
