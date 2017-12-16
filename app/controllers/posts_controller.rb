727dd03777cb1b5cb3be07af9eb9588cbb67c6f3
Lewis <lewisyoul@gmail.com>
2017-12-16 13:10:08 +0000

    add ability to edit posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,7 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
@@ -14,6 +15,20 @@ class PostsController < ApplicationController
       redirect_to @post
     else
       render 'new'
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
     end
   end
 
