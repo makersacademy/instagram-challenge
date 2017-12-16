749bfd01de6cc47513a4a69a2d90309fc18169ed
Lewis <lewisyoul@gmail.com>
2017-12-16 11:58:23 +0000

    generate post model with caption property

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,21 @@
 class PostsController < ApplicationController
   def new
   end
+
+  def create
+    @post = Post.new(post_params)
+
+    @post.save
+    redirect_to @post
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:caption)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20171216115120) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.text "caption"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
