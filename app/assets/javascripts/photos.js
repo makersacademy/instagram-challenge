d5db8e4dcf78af96d2bb4ca6deecc75f0d629987
Allan Wazacz <cazwazacz@gmail.com>
2017-12-16 18:48:01 +0000

    Change backticks to single quotes

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
   var element = $('#photos');
   $.get('/api/photos', function(photos) {
     photos.photos.forEach(function(photo) {
-      element.append(`<img class="each-photo" src="${photo.photo.url}">`)
-      element.append(`<h3>${photo.description}</h3>`)
+      element.append('<img class="each-photo" src="' + photo.photo.url + '">')
+      element.append('<h3>' + photo.description + '</h3>')
     })
   })
 })
