bfec3ba604fde1441e670fb639362181487dd286
antonio <antoniobelmardacosta@gmail.com>
2017-12-16 23:52:43 +0000

    Add test for photo creation in the database

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,7 +2,19 @@ class PhotosController < ApplicationController
   def index
   end
 
+  def create
+    Photo.create(photo_params)
+    render :index
+  end
+
   def new
     @photo = Photo.new
   end
+
+private
+
+  def photo_params
+    params.require(:photo).permit(:photo)
+  end
+
 end
--- spec/features/adding_photos_spec.rb
+++ spec/features/adding_photos_spec.rb
@@ -1,9 +1,18 @@
 feature 'Add photos to Instagram' do
-  scenario 'form page has fields to add photos' do
+  scenario 'Form page has fields to add photos' do
     visit '/photos/new'
     expect(page).to have_field("photo_title")
     expect(page).to have_field("photo_description")
     expect(page).to have_field("photo_photo")
     expect(page).to have_button("Add Photo")
+  end
+
+  scenario 'Add photo takes to index page' do
+    visit '/photos/new'
+    fill_in('photo_title', with: 'Beach')
+    fill_in('photo_description', with: 'It\'s a very nice beach photo')
+    attach_file("photo_photo", "spec/asset_specs/photos/photo.jpg")
+    click_button("Add Photo")
+    expect(page).to have_current_path('/photos')
   end
 end
