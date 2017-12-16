578fe8a4f0f7012431b81c984a2a71f501fd76f6
Allan Wazacz <cazwazacz@gmail.com>
2017-12-16 18:05:34 +0000

    Retreive photos in reverse order

--- app/controllers/api/photos_controller.rb
+++ app/controllers/api/photos_controller.rb
@@ -1,7 +1,7 @@
 module Api
   class PhotosController < ApplicationController
     def index
-      render :json => { photos: Photo.all }
+      render :json => { photos: Photo.all.reverse }
     end
   end
 end
