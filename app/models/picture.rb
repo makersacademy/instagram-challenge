15f362494d2af4d6eb0ef81fa6da922910f1a09a
gabrielabud <gabriela.budeanu@gmail.com>
2017-12-17 21:36:59 +0000

    Add image validation presence and maximum caption length to picture model

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,4 +2,7 @@ class Picture < ApplicationRecord
   mount_uploader :image, ImageUploader
 
   belongs_to :user
+
+  validates :image, presence: true
+  validates :caption, length: { minimum: 3, maximum: 500 }
 end
