7ce85c1bce8ecff45bb9f4630d28f99f0048edcf
Robert Clayton <robbie.c.clayton@gmail.com>
2017-12-17 23:42:22 +0000

    added user to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,6 +15,6 @@ class CommentsController < ApplicationController
 
   private
     def comment_params
-      params.require(:comment).permit(:body)
+      params.require(:comment).permit(:body, :user_id)
     end
 end
