78491be5bb3465014dfa2446ae54b9d4ae61dd99
Robert Clayton <robbie.c.clayton@gmail.com>
2017-12-17 17:56:37 +0000

    Delete button created

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,6 +34,13 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+  @post = Post.find(params[:id])
+  @post.destroy
+
+  redirect_to posts_path
+end
+
   private
 
   def post_params
