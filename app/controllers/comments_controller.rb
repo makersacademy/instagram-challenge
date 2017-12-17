5661c030d317f57e7890e280d4e448eff21bc09d
Robert Clayton <robbie.c.clayton@gmail.com>
2017-12-17 21:49:28 +0000

    users added through the devise app

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
