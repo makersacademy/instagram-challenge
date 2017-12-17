75cbcc2e4051000fc9c1cc95de4e6df578cc795f
Allan Wazacz <cazwazacz@gmail.com>
2017-12-17 00:07:14 +0000

    Add form for creating comments and show new comment without refreshing

--- public/javascripts/photos_show.js
+++ public/javascripts/photos_show.js
@@ -2,8 +2,27 @@ $(document).ready(function() {
   var singlePhotoDiv = $('#single-photo');
   var id = window.location.pathname.split('/')[2]
   $.get('/api/photos/' + id, function(photo) {
-    console.log(photo);
     singlePhotoDiv.append('<img class="each-photo" src="' + photo.photo.photo.url + '">')
     singlePhotoDiv.append('<h3>' + photo.photo.description + '</h3>')
   })
+
+  $.get('/api/photos/' + id + '/comments', function(comments) {
+    comments.comments.forEach(function(comment) {
+      $('#comments').append('<p>' + comment.body + '</p>')
+    })
+  })
+
+  createCommentForm();
+
+  $('#comment-form').on("submit", function() {
+    var value = $('#comment-body').val();
+    $('#comments').append('<p>' + value + '</p>');
+  })
+
+  function createCommentForm() {
+    $('#form').append('<form id="comment-form" action="/photos/' + id + '/comments" method="POST"></form>');
+    $('#comment-form').append('<input id="comment-body" type="text" name="comment[body]">');
+    $('#comment-form').append('<input id="submit-button" type="submit" value="Submit">');
+  }
+
 })
