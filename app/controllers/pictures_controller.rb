06f29eea9253c1fc73fd73006b44801bdbf9d639
gabrielabud <gabriela.budeanu@gmail.com>
2017-12-17 17:41:17 +0000

    Add user restrictions for edit, update, delete, destroy

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,10 +1,12 @@
 class PicturesController < ApplicationController
   before_action :authenticate_user!
+  before_action :restrict_to_user, only: [:edit, :update, :destroy]
   def index
     @pictures = Picture.all.order('created_at DESC')
   end
 
   def show
+     @picture = Picture.find(params[:id])
   end
 
   def new
@@ -23,13 +25,33 @@ class PicturesController < ApplicationController
   end
 
   def edit
+    @picture = Picture.find(params[:id])
+  end
+
+  def update
+    @picture = Picture.find(params[:id])
   end
 
   def delete
+    @picture = Picture.find(params[:id])
+  end
+
+  def destroy
+    @picture = Picture.find(params[:id])
+    @picture.destroy
+    flash[:notice] = "Subject '#{@picture.caption}' destroyed successfully."
+    redirect_to root_path
   end
 
   private
   def picture_params
     params.require(:picture).permit(:caption, :image)
+  end
+
+  def restrict_to_user
+    unless current_user == @picture.user
+      flash[:alert] = "You have not posted this picture"
+      redirect_to root_path
+    end
   end
 end
