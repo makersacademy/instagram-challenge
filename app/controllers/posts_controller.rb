1e619a8300a91b460fc6cae5ab4e49717e7313c5
Lewis <lewisyoul@gmail.com>
2017-12-17 14:30:37 +0000

    add voting functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -45,6 +45,18 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def upvote
+    @post = Post.find(params[:id])
+    @post.upvote_by current_user
+    redirect_to posts_path
+  end
+
+def downvote
+  @post = Post.find(params[:id])
+  @post.downvote_by current_user
+  redirect_to posts_path
+end
+
   private
 
   def post_params
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
 
   resources :posts do
     resources :comments
+    member do
+      put "like", to: "posts#upvote"
+      put "dislike", to: "posts#downvote"
+    end
   end
 
   root 'welcome#index'
