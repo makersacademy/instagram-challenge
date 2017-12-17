d19a6230973d30c6b25b24de3ed629c48abd14ef
gabrielabud <gabriela.budeanu@gmail.com>
2017-12-17 11:22:12 +0000

    Add jquery to gemfile and configure application.js

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'jbuilder', '~> 2.5'
 
 gem 'carrierwave', '~> 1.0'
 gem 'normalize-rails'
+gem 'jquery-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,4 @@
+
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
@@ -13,3 +14,10 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+
+//= require jquery
+//= require jquery_ujs
+
+$(document).ready(function() {
+    alert("Jquery working");
+});
