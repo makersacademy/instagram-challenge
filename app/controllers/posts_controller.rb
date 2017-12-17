baaabfeebc818db29a5250391339b6858dba9a4f
Robert Clayton <robbie.c.clayton@gmail.com>
2017-12-17 17:32:00 +0000

    validations added
    text must be entered on a post and cannot be more than 2200 characters long

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,8 +14,11 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-    @post.save
-    redirect_to @post
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
   private
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+  validates :text, presence: true,
+            length: { maximum: 2200 }
 end
