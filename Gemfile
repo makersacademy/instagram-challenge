48aaece22afe20f027b0781f00d9a578d0cdd5b5
majdeddine <majda-jambouri@hotmail.fr>
2017-12-17 21:50:54 +0000

    add user login and logout

--- Gemfile
+++ Gemfile
@@ -42,6 +42,7 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails'
   gem 'capybara'
+  gem 'devise'
 end
 
 group :development do
--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -5,11 +5,15 @@ skip_before_action :verify_authenticity_token
   end
 
   def new
-    @picture = Picture.new
+    if current_user
+    @picture = current_user.pictures.build
+    else
+      redirect_to new_user_registration_path
+    end
   end
 
   def create
-    @picture = Picture.new(picture_params)
+    @picture = current_user.pictures.build(picture_params)
     if @picture.save
       redirect_to pictures_path
     else
