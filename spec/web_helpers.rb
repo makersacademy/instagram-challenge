3e7fbca19ebd59e4d9659c77e2d36bc1510e0ff6
cristhiandas <cristhiandf94@gmail.com>
2017-12-17 17:42:20 +0000

    forms

--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -2,7 +2,7 @@ def create_post
   visit('/insta_posts/new')
   fill_in('insta_post[title]', with: 'generic title')
   fill_in('insta_post[description]', with: 'generic description')
-  click_button('Save Insta post')
+  click_button(value: 'Save Insta post')
 end
 
 def create_second_post
