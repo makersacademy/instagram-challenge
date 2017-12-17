d21324029da60f3facefb4dd8e467d57e81729f0
majdeddine <majda-jambouri@hotmail.fr>
2017-12-17 22:36:47 +0000

    refactor comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,8 +2,10 @@ class CommentsController < ApplicationController
 skip_before_action :verify_authenticity_token
 
     def create
+      @user = current_user
       @picture = Picture.find(params[:picture_id])
       @comment = @picture.comments.create(params[:comment].permit(:content))
+      @user.comments << @comment
       redirect_to picture_path(@picture)
     end
 
