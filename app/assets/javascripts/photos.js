87b202c3bb495be81f8cc7876d521194f2f31194
antonio <antoniobelmardacosta@gmail.com>
2017-12-17 19:06:14 +0000

    Add a button for likes and a box for comments

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
-  $('#photos-index-content').append('<p>YAY!</p>')
 
-  $.get("api/photos", function(data) {
+  $.get("/api/photos", function(data) {
     data.photos.forEach(function(photo){
       $('#photos-index-content').append(`<div id=photos-index-photo-${photo.id}></div>`)
       $(`#photos-index-photo-${photo.id}`).append(`<p> ${photo.title}</p>`)
       $(`#photos-index-photo-${photo.id}`).append(`<img src="${photo.image_url}" />`)
+      $(`#photos-index-photo-${photo.id}`).append('<button type="button">Like</button>')
+      $(`#photos-index-photo-${photo.id}`).append('<form> <input type="text" name="comment" placeholder="Comment"><input type="submit" value="Add Comment"></form>')
     })
   })
 });
