145bc3f4cb2040732938306a72c5a7e9ec8a9e94
Allan Wazacz <cazwazacz@gmail.com>
2017-12-17 13:27:26 +0000

    Remove workaround This seems to have been fixed by deleting the bin folder and running rake app:update:bin

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,44 +1,14 @@
-creating_model = false
-
 CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
-  # config.fog_credentials = {
-  #   provider:              'AWS',                        # required
-  #   aws_access_key_id:     ENV["AWS_KEY"],                        # required
-  #   aws_secret_access_key: ENV["AWS_SECRET"],                        # required
-  #   region:                ENV["AWS_REGION"]                  # optional, defaults to 'us-east-1'
-  # }
 
+  config.fog_credentials = {
+    provider:              'AWS',                        # required
+    aws_access_key_id:     ENV["AWS_KEY"],                        # required
+    aws_secret_access_key: ENV["AWS_SECRET"],                        # required
+    region:                ENV["AWS_REGION"]                  # optional, defaults to 'us-east-1'
+  }
 
-  if creating_model
-    config.fog_credentials = {
-      provider:              'AWS',                        # required
-      aws_access_key_id:     'key',                        # required
-      aws_secret_access_key: 'secret',                        # required
-      region:                'region'                  # optional, defaults to 'us-east-1'
-    }
-  else
-    config.fog_credentials = {
-      provider:              'AWS',                        # required
-      aws_access_key_id:     ENV["AWS_KEY"],                        # required
-      aws_secret_access_key: ENV["AWS_SECRET"],                        # required
-      region:                ENV["AWS_REGION"]                  # optional, defaults to 'us-east-1'
-    }
-  end
-
-  # config.fog_credentials = {
-  #   provider:              'AWS',                        # required
-  #   aws_access_key_id:     'key',                        # required
-  #   aws_secret_access_key: 'secret',                        # required
-  #   region:                'region'                  # optional, defaults to 'us-east-1'
-  # }
-
-  # case Rails.env
-  #   when 'production'
-  #     config.fog_directory  = 'binstagram-production'
-  #   when 'development'
-      config.fog_directory  = 'binstagram.development'
-  # end
+    config.fog_directory  = 'binstagram.development'
 
   config.fog_public     = false                                        # optional, defaults to true
   config.fog_attributes = { cache_control: "public, max-age=#{365.day.to_i}" } # optional, defaults to {}
