8549910a9fba53d64e45b7ffc083dbc47ccb5fac
Robert Clayton <robbie.c.clayton@gmail.com>
2017-12-17 17:45:49 +0000

    edit button now built

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,12 +12,25 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
   def create
     @post = Post.new(post_params)
     if @post.save
       redirect_to @post
     else
       render 'new'
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
     end
   end
 
