db2ecba910d92daac493d33cd5644633a6ca14c4
Rhys Kyte <rhyskyte@Rhyss-MacBook-Pro.local>
2018-01-21 22:42:01 +0000

    rough post-feed page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require bootstrap-sprockets
+//= require modernizr-custom
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
