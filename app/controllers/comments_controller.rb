61e4ffc9b4207982f6c044f9cb1a45490807b7a3
umairb1 <umairbashir95@hotmail.com>
2018-01-21 23:02:35 +0000

    Users can now delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,10 +15,16 @@ class CommentsController < ApplicationController
   def create
     @photo = Photo.find(params[:photo_id])
     @comment = @photo.comments.create(comment_params)
-    @photo.save
+    @comment.save
     redirect_to photo_path(@photo)
   end
 
+  def destroy
+    @photo = Photo.find(params[:photo_id])
+    @comment = @photo.comments.find(params[:id])
+    @comment.destroy
+    redirect_to photo_path(@photo)
+  end
 
   private
   def comment_params
