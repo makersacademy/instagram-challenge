d5aa5ab55250392cc92d68c438c4dccc27dea475
umairb1 <umairbashir95@hotmail.com>
2018-01-21 00:40:40 +0000

    Created basic app structure and added rspec testing

---
+++ spec/controllers/posts_controller_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe PostsController, type: :controller do
+  describe "GET index" do
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+
+  end
+end
