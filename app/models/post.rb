aac47550d0267e38fea58faa06030b04e0c511cc
Nikolaos <lunaticnick@hotmail.com>
2018-01-21 21:07:39 +0000

    Can add an image with comment and it shwos in index

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ validates :image, presence: true
 
 has_attached_file :image, styles: { :medium => "640x" }
 
+validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
 end
