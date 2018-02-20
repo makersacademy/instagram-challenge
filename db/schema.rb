6e83b0d51a3c8851d2cbefe39727a86bdf199e32
domvernon <3686359+domvernon@users.noreply.github.com>
2018-02-20 22:43:19 +0000

    db scheme and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180220221857) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
+  end
 
 end
