bc556d6819f2efe1ab627f463b5a53f3b2c47105
domvernon <3686359+domvernon@users.noreply.github.com>
2018-02-20 22:43:58 +0000

    fixes create posts test typos and PASSES

--- spec/features/creating_posts_spec.rb
+++ spec/features/creating_posts_spec.rb
@@ -4,10 +4,10 @@ feature 'Creating posts' do
   scenario 'can create a post' do
     visit '/'
     click_link 'New Post'
-    attach_file('Image', 'spec/images/1.jpg')
-    fill_in 'Caption', with: 'Not Egypt #expensivegreenhouse'
+    attach_file('Image', 'spec/images/1.jpeg')
+    fill_in 'Description', with: 'Not Egypt #expensivegreenhouse'
     click_button 'Create Post'
     expect(page).to have_content('#expensivegreenhouse')
-    expect(page).to have_css("img[src*='1.jpg']")
+    expect(page).to have_css("img[src*='1.jpeg']")
   end
 end
