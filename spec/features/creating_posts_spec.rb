8a1ec63f1d65bd14796312d85c0ed4f50dcdedcc
domvernon <3686359+domvernon@users.noreply.github.com>
2018-02-21 21:09:05 +0000

    failing test for making sure a user posts a picture

--- spec/features/creating_posts_spec.rb
+++ spec/features/creating_posts_spec.rb
@@ -1,13 +1,24 @@
 require 'rails_helper.rb'
 
 feature 'Creating posts' do
-  scenario 'can create a post' do
+  before do
     visit '/'
     click_link 'New Post'
+  end
+
+  scenario 'can create a post' do
     attach_file('Image', 'spec/images/1.jpeg')
     fill_in 'Description', with: 'Not Egypt #expensivegreenhouse'
     click_button 'Create Post'
     expect(page).to have_content('#expensivegreenhouse')
     expect(page).to have_css("img[src*='1.jpeg']")
+  end
+
+  scenario 'a post must have a picture attched to it' do
+    fill_in 'Description', with: 'I hate having to use pictures'
+    click_button 'Create Post'
+    expect(page).to have_content('Why would you not attach a picture?')
+    expect(page).not_to have_css("img[src*='1.jpeg']")
+    expect(current_path).to eq '/posts/new'
   end
 end
