0092cbd9a04f03c2e9cb99d10c2aa68775c71fa5
domvernon <3686359+domvernon@users.noreply.github.com>
2018-02-22 22:48:50 +0000

    failing test for deleting a post

---
+++ spec/features/deleting_posts_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+feature 'Deleting posts' do
+  background do
+    create(:post, description: 'Not my photo #yolo')
+
+    visit '/'
+
+    find(:xpath, "//a[contains(@href,'posts/1')]").click
+    click_link 'Edit Post'
+  end
+
+  scenario 'Can delete a single post' do
+    click_link 'Delete Post'
+
+    expect(page).to have_content('Post deleted.')
+    expect(page).to_not have_content('Not my photo #yolo')
+  end
+end
