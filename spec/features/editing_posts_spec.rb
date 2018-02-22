92a61f0eb2c7c7090c8f562d3777b6728d27a16b
domvernon <3686359+domvernon@users.noreply.github.com>
2018-02-22 22:32:08 +0000

    failing test for showing a message if updating post without an image

--- spec/features/editing_posts_spec.rb
+++ spec/features/editing_posts_spec.rb
@@ -16,4 +16,11 @@ feature 'Editing posts' do
     expect(page).to have_content('Post updated')
     expect(page).to have_content('This is probably a mistake')
   end
+
+  it 'user tries to upload something that is not an image' do
+    attach_file('Image', 'spec/images/wrong.zip')
+    click_button 'Update Post'
+
+    expect(page).to have_content('Something is wrong with your form!')
+  end
 end
