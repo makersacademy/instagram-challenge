740db2bd5f920c9c6aca1d14f4327b23aa9799b6
domvernon <3686359+domvernon@users.noreply.github.com>
2018-02-22 21:26:53 +0000

    drops size of images slightly

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   validates :image, presence: true
-  
-  has_attached_file :image, styles: { medium: "1080x" }
+
+  has_attached_file :image, styles: { medium: "720x" }
 
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
