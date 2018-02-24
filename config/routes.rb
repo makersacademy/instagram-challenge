d2b8d0da6c5d980cf2ffb07596b7f5b21492881f
Ed <ea.litten@gmail.com>
2018-02-24 13:31:38 +0000

    Add devise + welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+
+  get 'welcome/index'
+
+  root to: "welcome#index"
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
