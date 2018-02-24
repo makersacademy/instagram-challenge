abc67849eba1f68ea04f62f44a106c336bb73b72
Alexy <alexandra.fina@yahoo.com>
2018-02-24 17:16:13 +0000

    Errors on page if post blank

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,28 +2,31 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
-  def new
- end
 
- def show
+  def new
+  end
+
+  def show
     @post = Post.find(params[:id])
   end
 
   def new
-		@post = Post.new
-	end
-
- def create
-   @post = Post.new(post_params)
-
-  @post.save
-  redirect_to posts_path
-
+    @post = Post.new
   end
 
- private
+  def create
+    @post = Post.new(post_params)
+
+  if @post.save
+    redirect_to posts_path
+  else
+    render 'new'
+  end
+  end
+
+  private
   def post_params
-    params.require(:post).permit(:description)
+  params.require(:post).permit(:description)
   end
 
 end
