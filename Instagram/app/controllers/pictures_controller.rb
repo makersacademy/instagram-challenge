c7e239c08b72731923f7f5284b9e946dee497492
Alexander Sergides <alexse5121@gmail.com>
2018-02-24 12:48:55 +0000

    added create method for picture controller

--- Instagram/app/controllers/pictures_controller.rb
+++ Instagram/app/controllers/pictures_controller.rb
@@ -2,7 +2,7 @@ class PicturesController < ApplicationController
   def new
   end
   def create
-  @picture = Picture.new(params[:pictures])
+  @picture = Picture.new(params[:picture])
 
   @picture.save
   redirect_to @picture
