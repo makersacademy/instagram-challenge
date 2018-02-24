dea9857ff28523ef904621999a50e4c9da3ccb69
domvernon <3686359+domvernon@users.noreply.github.com>
2018-02-24 11:18:54 +0000

    failing test for mandatory username for signup

--- spec/features/user_signup_spec.rb
+++ spec/features/user_signup_spec.rb
@@ -4,15 +4,20 @@ feature 'User signup' do
   background do
     visit '/'
     click_link 'Register'
+    fill_in 'Email', with: 'test@test.com'
+    fill_in 'Password', with: 'password', match: :first
+    fill_in 'Password confirmation', with: 'password'
   end
 
   scenario 'can create a new user via the index page' do
     fill_in 'Username', with: 'user123'
-    fill_in 'Email', with: 'test@test.com'
-    fill_in 'Password', with: 'password', match: :first
-    fill_in 'Password confirmation', with: 'password'
 
     click_button 'Sign up'
     expect(page).to have_content('Welcome! You have signed up successfully.')
+  end
+
+  scenario 'requires a user name to successfully create an account' do
+    click_button 'Sign up'
+    expect(page).to have_content("can't be blank")
   end
 end
