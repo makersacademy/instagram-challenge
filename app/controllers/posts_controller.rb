679e8179fe2c2ec0d2bad0dee976fb46ff9cbdee
Alexy <alexandra.fina@yahoo.com>
2018-02-24 17:02:19 +0000

    Show all posts and message; init gitignore

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,29 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
   def new
  end
 
+ def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+		@post = Post.new
+	end
+
  def create
-   render plain: params[:post].inspect
- end
+   @post = Post.new(post_params)
+
+  @post.save
+  redirect_to posts_path
+
+  end
+
+ private
+  def post_params
+    params.require(:post).permit(:description)
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,3 @@
 class Post < ApplicationRecord
+  validates :description, presence: true
 end
