952268383cdb898fb1fa5ff006fb7db0e214a40a
domvernon <3686359+domvernon@users.noreply.github.com>
2018-02-24 23:56:14 +0000

    passing test for deleting comments

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :comment do
-    user nil
-    post nil
+    id 1
+    user_id 1
+    post_id 1
     content "MyText"
   end
 end
--- spec/factories/post.rb
+++ spec/factories/post.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :post do
     description 'a description'
     image Rack::Test::UploadedFile.new(Rails.root + 'spec/images/8.jpeg', 'image/jpeg')
+    user_id 1
   end
 end
