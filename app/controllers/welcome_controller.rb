2a84d14cf4ccd2f8aff38a50a98b6832385caa1f
Ed <ea.litten@gmail.com>
2018-02-24 14:51:22 +0000

    Set up devise

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
+
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
   def index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   get 'welcome/index'
 
   root to: "welcome#index"
