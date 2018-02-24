06c5adfc9b39311b880c3c06432743dc4b7ffe4f
Alexander Sergides <alexse5121@gmail.com>
2018-02-24 13:31:38 +0000

    picture show

--- Instagram/app/controllers/pictures_controller.rb
+++ Instagram/app/controllers/pictures_controller.rb
@@ -1,10 +1,21 @@
 class PicturesController < ApplicationController
+
+  def show
+    @picture = Picture.find(params[:id])
+  end
+
   def new
   end
   def create
-  @picture = Picture.new(params[:picture])
+  @picture = Picture.new(picture_params)
 
   @picture.save
   redirect_to @picture
   end
+
+  private
+    def picture_params
+      params.require(:picture).permit(:name_of_picture, :description_of_picture)
+    end
+
 end
