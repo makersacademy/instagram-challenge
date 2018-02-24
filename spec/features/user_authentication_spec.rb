fb0959582f6e732e6b623001045fe00b6d0e87a0
domvernon <3686359+domvernon@users.noreply.github.com>
2018-02-24 12:08:12 +0000

    user must now login to acces any of the app features

--- spec/features/user_authentication_spec.rb
+++ spec/features/user_authentication_spec.rb
@@ -27,7 +27,7 @@ feature 'User authentication' do
     click_button 'Log in'
 
     click_link 'Logout'
-    expect(page).to have_content('Signed out successfully.')
+    expect(page).to have_content('You need to sign in or sign up before continuing.')
   end
 
   scenario 'cannot view index posts without logging in' do
