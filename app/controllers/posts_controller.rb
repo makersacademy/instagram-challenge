d3ea8df55c7466421ee88c6ba6342b3131018c0c
Ed <ea.litten@gmail.com>
2018-02-24 15:27:09 +0000

    Add Posts routes, controller, model

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,51 @@
+class PostsController < ApplicationController
+
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:description)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
 
-  root to: "welcome#index"
+  resources :posts
+
+  root to: "posts#index"
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
