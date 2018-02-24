5aad79987d4c2b6bcd5ef860d6b8899bff5d8ce8
domvernon <3686359+domvernon@users.noreply.github.com>
2018-02-24 11:55:52 +0000

    adds some tests to require the user to login

--- spec/features/user_authentication_spec.rb
+++ spec/features/user_authentication_spec.rb
@@ -29,4 +29,14 @@ feature 'User authentication' do
     click_link 'Logout'
     expect(page).to have_content('Signed out successfully.')
   end
+
+  scenario 'cannot view index posts without logging in' do
+    visit '/'
+    expect(page).to have_content('You need to sign in or sign up before continuing.')
+  end
+
+  scenario 'cannot create a new post without logging in' do
+    visit new_post_path
+    expect(page).to have_content('You need to sign in or sign up before continuing.')
+  end
 end
