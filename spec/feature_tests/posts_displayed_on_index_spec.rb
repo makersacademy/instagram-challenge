e15a07549aeb2cb1b4acbd720a52ccbcb2240218
Leigh-ann <leighann.gant@gmail.com>
2018-02-24 11:18:43 +0000

    Posts can be clicked to display individual post

--- spec/feature_tests/posts_displayed_on_index_spec.rb
+++ spec/feature_tests/posts_displayed_on_index_spec.rb
@@ -1,8 +1,9 @@
 RSpec.describe "Displaying posts", :type => :feature do
+  before(:each) do
+    @post = create(:post)
+  end
+
   feature 'index displays list of posts' do
-    before do
-      create(:post)
-    end
     scenario 'the index displays posts added' do
       visit '/'
       expect(page).to have_css("img[src*='cat.jpg']")
@@ -17,6 +18,14 @@ RSpec.describe "Displaying posts", :type => :feature do
       expect(page).to have_content('My Fave Cat')
       expect(page).to have_content('Post two')
       expect(page).to have_content('Post three')
+    end
+  end
+  
+  feature 'can view individual posts' do
+    scenario 'click on post' do
+      visit '/'
+      find(:xpath, "//a[contains(@href,'posts/5')]").click
+      expect(page.current_path).to eq(post_path(@post))
     end
   end
 end
