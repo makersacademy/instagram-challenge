fb3e01c20ac0db1829ef4c299b74a11b7ad2b6e4
Alexy <alexandra.fina@yahoo.com>
2018-02-24 19:43:07 +0000

    Set up devise, log in and log out, register

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
+  belongs_to :user
   validates :description, presence: true
+  validates :user_id, presence: true
 end
--- db/migrate/20180224154907_create_posts.rb
+++ db/migrate/20180224154907_create_posts.rb
@@ -3,6 +3,8 @@ class CreatePosts < ActiveRecord::Migration[5.1]
     create_table :posts do |t|
       t.string :description
 
+      t.references :user, foreign_key: true
+      
       t.timestamps
     end
   end
