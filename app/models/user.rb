1136197bb9164a4d496633a45bb703cd6a9600dd
domvernon <3686359+domvernon@users.noreply.github.com>
2018-02-24 11:30:39 +0000

    passes tes for minimum username length

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :username, presence: true
+  validates :username, presence: true, length: { minimum: 5 }
 end
