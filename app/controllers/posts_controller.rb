cee88ff8c96edbe2ea2618cf395b8fcd9ef1fe6f
domvernon <3686359+domvernon@users.noreply.github.com>
2018-02-24 15:59:32 +0000

    further checks to see if the correct user is interacting with their post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_action :owned_post, only: [:edit]
+  before_action :owned_post, only: [:edit, :update, :destroy]
 
   def index
     @posts = Post.all
