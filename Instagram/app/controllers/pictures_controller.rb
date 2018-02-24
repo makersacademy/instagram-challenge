82c9911eaf65bd18c5a96f13d38aca46b5eb5cd6
Alexander Sergides <alexse5121@gmail.com>
2018-02-24 14:25:06 +0000

    validation checks

--- Instagram/app/controllers/pictures_controller.rb
+++ Instagram/app/controllers/pictures_controller.rb
@@ -8,12 +8,18 @@ class PicturesController < ApplicationController
   end
 
   def new
+    @picture = Picture.new
   end
-  def create
-  @picture = Picture.new(picture_params)
 
-  @picture.save
-  redirect_to @picture
+
+  def create
+    @picture = Picture.new(picture_params)
+
+    if @picture.save
+      redirect_to @picture
+    else
+      render 'new'
+    end
   end
 
   private
--- Instagram/app/models/picture.rb
+++ Instagram/app/models/picture.rb
@@ -1,2 +1,4 @@
 class Picture < ApplicationRecord
+  validates :name_of_picture, presence: true,
+                  length: { minimum: 5 }
 end
