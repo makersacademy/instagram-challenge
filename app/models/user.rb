2f8607b56a4afb74f918e7ef066a2a2867176dc2
domvernon <3686359+domvernon@users.noreply.github.com>
2018-02-24 11:34:25 +0000

    passes test for maximum username length

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :username, presence: true, length: { minimum: 5 }
+  validates :username, presence: true, length: { minimum: 5, maximum: 16 }
 end
