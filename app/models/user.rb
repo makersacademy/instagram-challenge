fa0e90c30b73ed387a7905be36f10a4e264404f9
Tom Grand <thomas.a.grand@gmail.com>
2018-02-25 21:09:49 +0000

    check uniquenes of email and username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  validates :user_name, presence: true
+  validates :user_name, presence: true, uniqueness: true
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
--- spec/features/login_signup_spec.rb
+++ spec/features/login_signup_spec.rb
@@ -37,5 +37,18 @@ feature 'login' do
       expect(page).to have_content 'The Marvelous Picts Feed!'
    end
 
+   scenario 'I can\'t login with the same username' do
+      click_link 'Logout'
+      click_link 'Sign up'
+      fill_in "user[user_name]", with: 'RomanSlayer'
+      fill_in 'Email', with: 'roman_slayer@gmail.com'
+      fill_in "user[password]", with: 'killtheromans'
+      fill_in "user[password_confirmation]", with: 'killtheromans'
+      click_button 'Sign up'
+      expect(page).to have_content 'Account with email already exists, why not try logging in instead?'
+      expect(page).to have_content 'Username already taken'
+      expect(page).not_to have_content 'Successfully signed up to the best website ever! Good choice :)'
+      expect(page).not_to have_content 'The Marvelous Picts Feed!'
+   end
 
 end
