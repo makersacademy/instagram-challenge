b24d25e28c4473b4efb550a356ffd8e88e1d5682
Tom Grand <thomas.a.grand@gmail.com>
2018-02-25 16:34:28 +0000

    add error message if title has no question mark

--- app/models/pict.rb
+++ app/models/pict.rb
@@ -2,8 +2,14 @@ class Pict < ApplicationRecord
   has_attached_file :image
   validates :image, presence: true
   validates :title, presence: true
+  validate  :has_question_mark
 
   validates_attachment_content_type :image, :content_type =>
     /\Aimage\/.*\Z/
+
+  private
+    def has_question_mark
+       errors.add(:title, "Title needs a question mark at the end") unless title[-1] == '?'
+    end
 
 end
