ce9c1cd9533af061547063e1e8e8207f0b35e0d4
domvernon <3686359+domvernon@users.noreply.github.com>
2018-02-25 10:27:52 +0000

    removes image conversion so that tests do not take a long time to run

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,8 @@ class Post < ApplicationRecord
   validates :user_id, presence: true
   validates :image, presence: true
 
-  has_attached_file :image, styles: { medium: "720x" }
+  has_attached_file :image
+  # has_attached_file :image, styles: { medium: "720x" }
 
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
