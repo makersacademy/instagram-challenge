ae1516c9fea40f2387a5b16a6dc2cb655e52ace2
Tom Grand <thomas.a.grand@gmail.com>
2018-02-25 22:13:16 +0000

    move tests into multiple picts spec

---
+++ spec/features/add_multiple_picts_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+feature 'add many picts' do
+  background do
+    sign_up('roman_slayer@gmail.com', 'RomanSlayer')
+    add_pict('What could this be?', 'We do it all the time', 'TDD' )
+    add_pict('What will this be?', 'Its very important...', 'TDD' )
+    click_link 'To the Pict feed!'
+  end
+
+  scenario 'add multiple picts, and see on main feed' do
+    expect(page).to have_content 'What could this be?'
+    expect(page).to have_content 'What will this be?'
+    expect(page).not_to have_content 'We do it all the time'
+    expect(page).to have_css('img[src*="code_clue.jpg"]')
+  end
+
+  scenario 'checkout one pict from feed' do
+    page.all(:link, 'Have a go')[1].click
+    expect(page).to have_content 'What will this be?'
+    expect(page).to have_content 'Its very important...'
+    expect(page).not_to have_content 'TDD'
+    expect(page).not_to have_content 'Who could this be?'
+  end
+end
