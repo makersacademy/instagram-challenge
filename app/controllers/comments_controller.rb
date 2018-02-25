1d8b9f14fb134765d233e8a872bb23f9ec90fe33
Ed <ea.litten@gmail.com>
2018-02-25 19:51:00 +0000

    Add comments on posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,43 @@
 class CommentsController < ApplicationController
+
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
+    @comment.user_id = current_user.id
+    @comment.save
+    redirect_to @post
+  end
+
+  def update
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    if @comment.update(comment_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    if User.find(@comment.user_id) == current_user
+      @comment.destroy
+    else
+      flash[:notice] = "You may not delete this comment"
+    end
+    redirect_to post_path(@post)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+
 end
