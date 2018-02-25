d2a89f6b5dea61cd951334b0060ee0dd6fbc5433
Ed <ea.litten@gmail.com>
2018-02-25 22:41:37 +0000

    fix typo in carrierwave setup

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -8,7 +8,7 @@ CarrierWave.configure do |config|
     # $ heroku config:add S3_KEY=your_s3_access_key S3_SECRET=your_s3_secret S3_REGION=eu-west-1 S3_ASSET_URL=http://assets.example.com/ S3_BUCKET_NAME=s3_bucket/folder
  
     # Configuration for Amazon S3
-    provider: 'AWS'
+    provider: 'AWS',
     aws_access_key_id: ENV['S3_KEY'] || Figaro.env.aws_access_key_id,
     aws_secret_access_key: ENV['S3_SECRET'] || Figaro.env.aws_secret_access_key,
     region: 'eu-west-2'
