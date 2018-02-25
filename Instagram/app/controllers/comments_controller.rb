2bae2c5773dd7080e8516eb29b0a7da4f7f13a19
Alexander Sergides <alexse5121@gmail.com>
2018-02-25 13:08:45 +0000

    removed Rails authentication method

--- Instagram/app/controllers/comments_controller.rb
+++ Instagram/app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 
 class CommentsController < ApplicationController
 
-  http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+  # http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
 
 
   def create
--- Instagram/app/controllers/pictures_controller.rb
+++ Instagram/app/controllers/pictures_controller.rb
@@ -1,6 +1,6 @@
 class PicturesController < ApplicationController
 
-  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+  # http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
 
 
   def index
