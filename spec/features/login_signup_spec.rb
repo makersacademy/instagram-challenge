c8c8b53f2de152475b2ada28024047a4f23893ec
Tom Grand <thomas.a.grand@gmail.com>
2018-02-25 20:44:32 +0000

    add sign out option

--- spec/features/login_signup_spec.rb
+++ spec/features/login_signup_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
 feature 'login' do
-
-   scenario 'Lets me signup' do
+    background do
       visit '/'
       click_link 'Sign up'
       fill_in "user[user_name]", with: 'RomanSlayer'
@@ -10,8 +9,16 @@ feature 'login' do
       fill_in "user[password]", with: 'killtheromans'
       fill_in "user[password_confirmation]", with: 'killtheromans'
       click_button 'Sign up'
+    end
+
+   scenario 'Lets me signup' do
       expect(page).to have_content 'Successfully signed up to the best website ever! Good choice :)'
       expect(page).to have_content 'The Marvelous Picts Feed!'
+   end
+
+   scenario 'lets me logout' do
+     click_link 'Logout'
+     expect(page).to have_content 'You\'ve logged out, see you again for more Pictogram soon!'
    end
 
 end
