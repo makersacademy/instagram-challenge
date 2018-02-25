b70f5452914a83c5d6b39fecaa7178897269f071
Lewis Moore <lewis.moore2707@gmail.com>
2018-02-25 18:09:33 +0000

    Update devise defaults to log out via get. This enabled the log out function

--- spec/features/creating_users_spec.rb
+++ spec/features/creating_users_spec.rb
@@ -8,4 +8,14 @@ feature 'Creating users' do
     click_button 'Sign up'
     expect(page).to have_content "Welcome!"
   end
+
+  scenario 'cant register without an email address' do
+    visit('/')
+    click_link 'Register'
+    fill_in 'Email', with: ''
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    click_button 'Sign up'
+    expect(page).to have_content "Email can't be blank"
+  end
 end
