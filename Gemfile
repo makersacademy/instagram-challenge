3f93cb137a16ee468650a12864df7d5ff596b227
domvernon <3686359+domvernon@users.noreply.github.com>
2018-02-25 22:08:09 +0000

    fixes broken tests
    co-authored-by: ealitten <ea.litten@gmail.com>

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ group :development, :test do
   gem 'capybara', '~> 2.13'
   gem 'selenium-webdriver'
   gem 'rspec-rails', '~> 3.7'
+  gem 'database_cleaner'
 end
 
 group :development do
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,14 +8,27 @@ require 'rspec/rails'
 require 'capybara/rails'
 require 'support/factory_bot'
 require 'support/auth_helper'
+require 'database_cleaner'
 
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
   config.infer_spec_type_from_file_location!
 
   config.filter_rails_from_backtrace!
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
