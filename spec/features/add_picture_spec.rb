c9c77ff036fb50cfe60101f684ff52c65cb81ce9
Tom Grand <thomas.a.grand@gmail.com>
2018-02-25 09:48:27 +0000

    add test that you can't uplaod without an image

--- spec/features/add_picture_spec.rb
+++ spec/features/add_picture_spec.rb
@@ -1,17 +1,26 @@
 require 'rails_helper'
 
 feature 'add a pict' do
-  scenario 'add a cool pict' do
+  background do
     visit '/'
     click_link 'New Pict'
     fill_in :Hints, with: 'We do it all the time'
     fill_in :Answer, with: 'TDD'
     fill_in :Title, with: 'What could this be?'
+  end
+
+  scenario 'add a cool pict' do
     attach_file('pict[image]', 'spec/docs/images/code_clue.jpg')
     click_button 'Add Pict'
     expect(page).to have_content 'We do it all the time'
     expect(page).to have_content 'Successfully added Pict'
     expect(page).to have_content 'What could this be?'
     expect(page).to have_css('img[src*="code_clue.jpg"]')
+  end
+
+  scenario 'try and no pict...fails' do
+    click_button 'Add Pict'
+    expect(page).to have_content 'Error, you need to add an image'
+    expect(page).not_to have_content 'Successfully added Pict'
   end
 end
