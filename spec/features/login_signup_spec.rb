b8e418bd03331ad8dd8393bd81a27a2da50427d8
Tom Grand <thomas.a.grand@gmail.com>
2018-02-25 20:53:01 +0000

    add checks for nav bar after signup

--- spec/features/login_signup_spec.rb
+++ spec/features/login_signup_spec.rb
@@ -11,19 +11,26 @@ feature 'login' do
       click_button 'Sign up'
     end
 
-   scenario 'Lets me signup' do
+   scenario 'Lets me signup and changes nav bar accordingly' do
       expect(page).to have_content 'Successfully signed up to the best website ever! Good choice :)'
       expect(page).to have_content 'The Marvelous Picts Feed!'
+      expect(page).not_to have_content 'Sign up'
+      expect(page).not_to have_content 'Login'
+      expect(page).to have_content 'Logout'
    end
 
    scenario 'lets me logout' do
      click_link 'Logout'
      expect(page).to have_content 'You\'ve logged out, see you again for more Pictogram soon!'
      expect(page).not_to have_content 'Logout'
+
    end
 
    scenario 'I can login to user account' do
       click_link 'Logout'
+      click_link 'Log in'
+      expect(page).to have_content 'Welcome back to the best website ever! Pictogram to your hearts content!'
+      expect(page).to have_content 'The Marvelous Picts Feed!'
    end
 
 
