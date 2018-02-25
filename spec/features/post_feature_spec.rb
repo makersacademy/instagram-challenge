da3b39350be9faad37a952f5dcc26d92f0f504e3
Lewis Moore <lewis.moore2707@gmail.com>
2018-02-25 10:12:04 +0000

    Add presnece validation to a new post

--- spec/features/post_feature_spec.rb
+++ spec/features/post_feature_spec.rb
@@ -8,4 +8,12 @@ feature 'Creating a post' do
     expect(page).to have_content('#MyImage')
     expect(page).to have_css ("img[src*='picture.jpg']")
   end
+
+  it 'cant create post without an image' do
+    visit('/')
+    click_link 'New Post'
+    fill_in 'Description', with: 'An Image'
+    click_button 'Create Post'
+    expect(page).not_to have_content('An Image')
+  end
 end
