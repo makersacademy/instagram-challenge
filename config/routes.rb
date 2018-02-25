82e2fccf0b9bbb2e613a13a6a07f5e99fab697ce
Alexy <alexandra.fina@yahoo.com>
2018-02-25 14:36:18 +0000

    Change root to posts/index

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  get 'welcome/index'
+  get 'posts/index'
 
   resources :posts do
     resources :comments, :likes, :image
   end
 
-  root 'welcome#index'
+  root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
