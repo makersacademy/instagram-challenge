6a174751aae86b3bfb9e8a5a7d36e9c0519cd9cc
Alexy <alexandra.fina@yahoo.com>
2018-02-25 13:21:31 +0000

    Styling css and changes html display

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -33,7 +33,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fit => [50, 50]
+    process :resize_to_fit => [200, 200]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
