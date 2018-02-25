4ad2020cb32d5765fe7b6b5df837843fbf967431
Tom Grand <thomas.a.grand@gmail.com>
2018-02-25 20:25:01 +0000

    add login with username

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   resources :picts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'picts#index'
--- spec/features/login_signup_spec.rb
+++ spec/features/login_signup_spec.rb
@@ -5,12 +5,12 @@ feature 'login' do
    scenario 'Lets me signup' do
       visit '/'
       click_link 'Sign up'
-      fill_in 'user name', with: 'RomanSlayer'
-      fill_in 'email', with: 'roman_slayer@gmail.com'
-      fill_in 'password', with: 'killtheromans'
-      fill_in 'password[confirmation]', with: 'killtheromans'
+      fill_in "user[user_name]", with: 'RomanSlayer'
+      fill_in 'Email', with: 'roman_slayer@gmail.com'
+      fill_in "user[password]", with: 'killtheromans'
+      fill_in "user[password_confirmation]", with: 'killtheromans'
       click_button 'Sign up'
-      expect(page).to have_content 'Successfully signed up'
+      expect(page).to have_content 'Successfully signed up to the best website ever! Good choice :)'
       expect(page).to have_content 'The Marvelous Picts Feed!'
    end
 
