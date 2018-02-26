61cc31873ba981c22c25cc19303f06b2f539526f
jenniferemshepherd <jennifer.elaine.mairead@gmail.com>
2018-02-26 19:12:23 +0000

    implement post ownership for users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :owned_post, only: [:edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -50,5 +51,12 @@ private
 
   def post_params
     params.require(:post).permit(:image,:caption)
+  end
+
+  def owned_post
+    unless current_user == @post.user
+      flash[:alert] = "That post doesn't belong to you!"
+      redirect_to root_path
+    end
   end
 end
