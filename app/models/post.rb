e10e91df2a317caa1f9a701b769ba364ed55fb14
jenniferemshepherd <jennifer.elaine.mairead@gmail.com>
2018-02-26 10:40:17 +0000

    style via bootstrap

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-  has_attached_file :image
   validates :image, presence: true
   has_attached_file :image, styles: { :medium => "640x" }
+  validates :caption, length: { minimum: 3, maximum: 300 }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
