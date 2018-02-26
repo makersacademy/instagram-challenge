05310e6189c86e1d09fd520de469aab91b7a8e9a
jenniferemshepherd <jennifer.elaine.mairead@gmail.com>
2018-02-26 09:15:48 +0000

    add paperclip for images

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,6 +11,7 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
+  Paperclip.options[:command_path] = "/usr/local/bin/"
 
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
