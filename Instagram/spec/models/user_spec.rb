97459b3f59326d366449b6425f4ac372f55a13b1
Alex Sergides <alexsergides@Alexs-Air.home>
2018-02-27 05:28:52 +0000

    wrote test for user model association

--- Instagram/spec/models/user_spec.rb
+++ Instagram/spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Associations between models and pictures/comments" do
+    it { User.reflect_on_association(:pictures).macro.should   eq(:has_many) }
+    it { User.reflect_on_association(:comments).macro.should   eq(:has_many) }
+  end
 end
