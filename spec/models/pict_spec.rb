3b882a9a116c4b435276b3a387f71aa319704211
Tom Grand <thomas.a.grand@gmail.com>
2018-02-28 08:10:15 +0000

    test factory bots are valid

--- spec/models/pict_spec.rb
+++ spec/models/pict_spec.rb
@@ -2,8 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Pict, type: :model do
 
+  it 'is valid with picture and title' do
+    picture =  fixture_file_upload 'spec/docs/images/code_clue.jpg'
+    pict = Pict.new(title: 'hello?', image: picture)
+    expect(pict).to_not be_valid
+  end
+
   it 'is not valid without a title' do
-    pict = Pict.new(title: nil)
+    picture =  fixture_file_upload 'spec/docs/images/code_clue.jpg'
+    pict = Pict.new(title: nil, image: picture)
     expect(pict).to_not be_valid
   end
 
@@ -11,4 +18,6 @@ RSpec.describe Pict, type: :model do
     pict = Pict.new(title: 'helloo')
     expect(pict).to_not be_valid
   end
+
+  it ''
 end
