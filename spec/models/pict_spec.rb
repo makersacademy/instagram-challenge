e7a74dcb78d2c3691c87a8a65c244ce2d47db40b
Tom Grand <thomas.a.grand@gmail.com>
2018-03-04 20:04:26 +0000

    Refactor pict tests

--- spec/models/pict_spec.rb
+++ spec/models/pict_spec.rb
@@ -1,36 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Pict, type: :model do
+  let(:user) { build_stubbed(:user) }
+  let(:picture) { fixture_file_upload 'spec/docs/images/code_clue.jpg' }
+  let(:title) { 'hello?' }
+  subject(:pict) {described_class.new(title: title, image: picture, user: user)}
 
   it 'is valid with picture, title and user' do
-    user = build_stubbed(:user)
-    picture =  fixture_file_upload 'spec/docs/images/code_clue.jpg'
-    pict = Pict.new(title: 'hello?', image: picture, user: user)
     expect(pict).to be_valid
   end
 
   it 'is not valid without a title' do
-    user = build_stubbed(:user)
-    picture = fixture_file_upload 'spec/docs/images/code_clue.jpg'
-    pict = Pict.new(title: nil, image: picture, user: user)
+    pict.title = nil
     expect(pict).to_not be_valid
   end
 
   it 'is not valid without a user' do
-    picture = fixture_file_upload 'spec/docs/images/code_clue.jpg'
-    pict = Pict.new(title: 'hello?', image: picture, user: nil)
+    pict.user = nil
     expect(pict).to_not be_valid
   end
 
   it 'is not valid without a picture' do
-    user = build_stubbed(:user)
-    pict = Pict.new(title: nil, user: user)
+    pict.image = nil
     expect(pict).to_not be_valid
   end
 
   it 'is not valid if title does not have a question mark' do
-    picture = fixture_file_upload 'spec/docs/images/code_clue.jpg'
-    pict = Pict.new(title: 'helloo', image: picture)
+    pict.title = 'helloo'
     expect(pict).to_not be_valid
   end
 
