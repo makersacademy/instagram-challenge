8a40b4bc4d917c1652d8f5cef88dcc3384a18a38
Tom Grand <thomas.a.grand@gmail.com>
2018-03-04 20:12:58 +0000

    User should have username

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'Should have a username attribute' do
+    user = User.new(email: 'aaaa@aa.com', password: 'password', user_name: 'PictPict')
+    expect(user).to have_attributes(:user_name => 'PictPict')
+  end
+
 end
