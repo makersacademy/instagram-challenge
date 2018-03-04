252c366c034af05284bc268f8283b60f0ba9688e
Tom Grand <thomas.a.grand@gmail.com>
2018-03-04 20:16:06 +0000

    user is invalid without password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,18 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   it 'Should have a username attribute' do
-    user = User.new(email: 'aaaa@aa.com', password: 'password', user_name: 'PictPict')
+    user = User.new(user_name: 'PictPict')
     expect(user).to have_attributes(:user_name => 'PictPict')
+  end
+
+  it 'Should be valid with a username, email and password' do
+    user = User.new(email: 'aaaa@aa.com', password: 'password', user_name: 'PictPict')
+    expect(user).to be_valid
+  end
+
+  it 'Should be invalid valid without a username, email and password' do
+    user = User.new(email: 'aaaa@aa.com', password: 'password')
+    expect(user).to_not be_valid
   end
 
 end
