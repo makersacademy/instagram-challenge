60a8a22405d327a1e16353dbbd220649957f6558
jose <jennymarin1989@hotmail.com>
2018-03-17 19:13:04 +0000

    Using simple_form gem to create the form to post the picture

--- Fotogram/app/controllers/pictures_controller.rb
+++ Fotogram/app/controllers/pictures_controller.rb
@@ -3,7 +3,19 @@ class PicturesController < ApplicationController
   def index
   end
 
+  def new
+    @picture = Picture.new
+  end
 
+  def create
+    @picture = Picture.new(pic_params)
+  end
+
+  private
+
+  def pic_params
+    params.require(:pictures).permit(:title, :description)
+  end
 
 
 end
