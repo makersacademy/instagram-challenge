41aceb608dd9a3fb2523ce502b18180bf2395dd6
Daniel Keen <keendaniel01@gmail.com>
2018-03-17 15:57:05 +0000

    Set up pictures control and can ad new pictures. Show and index views rendered

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,7 +1,27 @@
 class PicturesController < ApplicationController
+  def index
+    @pictures = Picture.all
+  end
+
   def new
   end
 
-  def index
+  def show
+    @picture = Picture.find(params[:id])
+    p @picture.image
   end
+
+  def create
+    p params
+    @picture = Picture.new(picture_params)
+    @picture.save
+    redirect_to @picture
+  end
+
+  private
+    def picture_params
+      params.require(:picture).permit(:image, :description)
+    end
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180317152307) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pictures", force: :cascade do |t|
+    t.text "image"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
