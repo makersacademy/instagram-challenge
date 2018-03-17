6d53d75625c07fe4ed1a8f00f08ca80622f4c2c8
kotpsot <zygmunt.justyna@gmail.com>
2018-03-17 15:42:22 +0000

    added a new photo form

--- Fridge/app/controllers/photos_controller.rb
+++ Fridge/app/controllers/photos_controller.rb
@@ -3,4 +3,26 @@ class PhotosController < ApplicationController
   def index
     @photos = Photo.all
   end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+    if @photo.save
+      flash[:notice] = "Photo has been saved"
+      redirect_to '/photos'
+    else
+      flash[:notice] = "Please add the description"
+      render :new
+    end
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:description)
+  end
+
 end
--- Fridge/app/models/photo.rb
+++ Fridge/app/models/photo.rb
@@ -1,2 +1,3 @@
 class Photo < ApplicationRecord
+  validates :description, presence: true
 end
