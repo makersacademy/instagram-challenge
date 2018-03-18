2a962780bab40bdb41f1c56fed3e66f98fa031c9
Noel Vock <noel1uk@gmail.com>
2018-03-18 17:38:56 +0000

    Add picture validation spec.

---
+++ cloneInsta/spec/features/post_picture_validations_spec.rb
@@ -0,0 +1,9 @@
+feature 'Input is validated' do
+  scenario 'user supplies an invalid url' do
+    visit '/pictures/new'
+    fill_in :"picture[url]", with: ''
+    fill_in :"picture[comment]", with: "It won't work"
+    click_button 'Save Picture'
+    expect(page).to have_content "errors prohibited"
+  end
+end
