96d5f1ba1b276880da8f455cd64ed8bff13c651e
Matt Burstein <j.matthew.burstein@gmail.com>
2018-03-18 10:59:19 +0000

    generate Likes model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180317210436) do
+ActiveRecord::Schema.define(version: 20180318105718) do
 
   create_table "comments", force: :cascade do |t|
     t.string "body"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 20180317210436) do
     t.datetime "updated_at", null: false
     t.index ["picture_id"], name: "index_comments_on_picture_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "picture_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["picture_id"], name: "index_likes_on_picture_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "pictures", force: :cascade do |t|
