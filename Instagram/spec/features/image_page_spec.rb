cb71e9aa132934bc820fad336cf9143bbcacfecc
Hannah Lillis <hannahamanda16@gmail.com>
2018-03-18 11:15:09 +0000

    Pass feature test to see image page

--- Instagram/spec/features/image_page_spec.rb
+++ Instagram/spec/features/image_page_spec.rb
@@ -1,7 +1,12 @@
-feature 'image has its own page' do
+feature 'image has its own page', js: true do
   scenario 'user clicks on image on homepage' do
-    visit '/'
-    find('.thumbnail', visible: false).first.click
-    expect(page).to have_content("Raccoon with Pizza")
+    visit '/images'
+    click_button 'Add an image'
+    attach_file('image[image]', Rails.root + "app/assets/images/Capybara.jpg")
+    fill_in 'image[user]', with: 'Hannah', visible: false
+    fill_in 'image[caption]', with: 'Grumpy Capybara'
+    click_button 'Create Image'
+    find("img[src*='Capybara.jpg']").click
+    expect(page).to have_content("Grumpy Capybara")
   end
 end
