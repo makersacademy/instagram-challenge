798715b7d0f820318c3e67ec9ff9a40e1a0635d5
jose <jennymarin1989@hotmail.com>
2018-03-18 10:41:29 +0000

    Adding show method  and private methods in picture controller

--- Fotogram/app/controllers/pictures_controller.rb
+++ Fotogram/app/controllers/pictures_controller.rb
@@ -1,5 +1,10 @@
 class PicturesController < ApplicationController
+  before_action :find_pic, only: [:show, :edit, :update, :destroy]
+
   def index
+  end
+
+  def show
   end
 
   def new
@@ -8,11 +13,21 @@ class PicturesController < ApplicationController
 
   def create
     @picture = Picture.new(pic_params)
+
+    if @picture.save
+      redirect_to @picture, notice:"Your picture was succesfully posted"
+    else
+      render "new"
+    end
   end
 
   private
 
   def pic_params
-    params.require(:pictures).permit(:title, :description)
+    params.require(:picture).permit(:title, :description)
+  end
+
+  def find_pic
+    @picture = Picture.find(params[:id]) 
   end
 end
