7c871987e4e1a0fb61c4ca8df1dfc9778eb9d4d9
Hannah Lillis <hannahamanda16@gmail.com>
2018-03-18 12:22:08 +0000

    Write failing test for adding a comment

---
+++ Instagram/spec/features/add_comment_spec.rb
@@ -0,0 +1,10 @@
+feature 'add comment', js: true do
+  scenario 'user adds a comment to a picture' do
+    add_an_image_of_lemur
+    find("img[src*='Lemur.jpg']").click
+    fill_in 'image[comment]', with: 'Love this picture!'
+    fill_in 'image[commenter]', with: 'Bob'
+    click_button('Add comment')
+    expect(page).to have_content('Love this picture!')
+  end
+end
