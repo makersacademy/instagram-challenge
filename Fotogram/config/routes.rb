2c9f056525035104c3b7ef002e49055177c8110b
jose <jennymarin1989@hotmail.com>
2018-03-18 20:31:09 +0000

    Showing pictures with title in index.html - pending to solve sign up issues

--- Fotogram/config/routes.rb
+++ Fotogram/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :pictures
   root "pictures#index"
+  resources :pictures
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
 end
