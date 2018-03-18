7ac32098c8d1febc30b5e6603e31a5545166b07f
kotpsot <zygmunt.justyna@gmail.com>
2018-03-18 11:39:15 +0000

    likes are working - trying to move the logic to the mode

--- Fridge/app/controllers/likes_controller.rb
+++ Fridge/app/controllers/likes_controller.rb
@@ -1,13 +1,10 @@
 class LikesController < ApplicationController
 
-  def create
-    p "*******"
-    p "User ID"
-    p params[:user_id]
-    p "photo ID"
-    p params[:photo_id]
-    p "*******"
+  def self.all
+    return Likes.all
+  end
 
+  def create
     @like = Like.new(like_params)
     if @like.save
       flash[:notice] = "Thank you for your like"
@@ -16,11 +13,6 @@ class LikesController < ApplicationController
       flash[:notice] = "Something went wrong"
       redirect_to '/photos'
     end
-    # here I'll create a new like in the Likes database
-    # having the data above.
-
-    # Later I'll show likes on the website
-    # and checkeced if a particular user haven't liked twice.
   end
 
   def like_params
--- Fridge/app/models/photo.rb
+++ Fridge/app/models/photo.rb
@@ -8,4 +8,14 @@ class Photo < ApplicationRecord
   has_attached_file :image, styles: { medium: "239x239>" }, default_url: "/images/default.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
+  def count_likes
+    counter = 0
+    for i in 0...Like.all.count do
+      if Like.all[i].photo_id == this.id then
+        counter += 1
+      end
+    end
+    return counter
+  end
+
 end
