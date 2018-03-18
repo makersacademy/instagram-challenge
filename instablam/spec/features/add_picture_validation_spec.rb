1fff5e0f883483f9309ae6ab679581da08c022c3
Stephen Tilley <stephentilley@gmail.com>
2018-03-18 00:04:11 +0000

    Added and setup devise

---
+++ instablam/spec/features/add_picture_validation_spec.rb
@@ -0,0 +1,25 @@
+feature "Validating adding pictures" do
+  scenario "Error if both fields left blank" do
+    visit '/pictures/new'
+    fill_in :"picture[title]", with: ""
+    fill_in :"picture[picture_url]", with: ""
+    click_button "Create Picture"
+    expect(page).to have_content("prohibited")
+  end
+
+  scenario "Error if title field left blank" do
+    visit '/pictures/new'
+    fill_in :"picture[title]", with: ""
+    fill_in :"picture[picture_url]", with: ""
+    click_button "Create Picture"
+    expect(page).to have_content("prohibited")
+  end
+
+  scenario "Error if picture_url field left blank" do
+    visit '/pictures/new'
+    fill_in :"picture[title]", with: ""
+    fill_in :"picture[picture_url]", with: ""
+    click_button "Create Picture"
+    expect(page).to have_content("prohibited")
+  end
+end
---
+++ instablam/spec/features/edit_picture_validation_spec.rb
@@ -0,0 +1,28 @@
+feature "Validating editing pictures" do
+  scenario "Error if both fields left blank" do
+    create_new_picture
+    click_link "Edit"
+    fill_in "picture[title]", with: ""
+    fill_in "picture[picture_url]", with: ""
+    click_on "Update Picture"
+    expect(page).to have_content("prohibited")
+  end
+
+  scenario "Error if title field left blank" do
+    create_new_picture
+    click_link "Edit"
+    fill_in "picture[title]", with: "New Title"
+    fill_in "picture[picture_url]", with: ""
+    click_on "Update Picture"
+    expect(page).to have_content("prohibited")
+  end
+
+  scenario "Error if url field left blank" do
+    create_new_picture
+    click_link "Edit"
+    fill_in "picture[title]", with: ""
+    fill_in "picture[picture_url]", with: "New link"
+    click_on "Update Picture"
+    expect(page).to have_content("prohibited")
+  end
+end
