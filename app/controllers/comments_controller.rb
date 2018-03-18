a9231440e4b1c4ea69795186841020668bb2d716
Matt Burstein <j.matthew.burstein@gmail.com>
2018-03-18 12:55:34 +0000

    Add user validation to likes and comments controllers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+
   def create
     @picture = Picture.find(params[:picture_id])
     full_params = comment_params
