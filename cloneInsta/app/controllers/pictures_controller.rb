7b537399e5bf0b1b7294fdaf5d891f5c862c2195
Noel Vock <noel1uk@gmail.com>
2018-03-18 16:07:14 +0000

    Create picture model, pictures show. Modify pictures controller. Pass post pictures feature test.

--- cloneInsta/app/controllers/pictures_controller.rb
+++ cloneInsta/app/controllers/pictures_controller.rb
@@ -1,4 +1,20 @@
 class PicturesController < ApplicationController
+  def show
+    @picture = Picture.find(params[:id])
+  end
+
   def new
   end
+
+  def create
+    @picture = Picture.new(picture_params)
+
+    @picture.save
+    redirect_to @picture
+  end
+
+  private
+    def picture_params
+      params.require(:picture).permit(:url, :comment)
+    end
 end
