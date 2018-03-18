7e4618c517440f545a0314d0b1f5f5f76f7a4e39
kotpsot <zygmunt.justyna@gmail.com>
2018-03-18 15:25:00 +0000

    Preventing users from liking twice working

--- Fridge/app/controllers/likes_controller.rb
+++ Fridge/app/controllers/likes_controller.rb
@@ -2,7 +2,8 @@ class LikesController < ApplicationController
 
   def create
     @like = Like.new(like_params)
-    if @like.save
+    if Like.where(["photo_id = ? and user_id = ?", "#{like_params[:photo_id]}", "#{like_params[:user_id]}"]).count == 0 then
+      @like.save
       flash[:notice] = "Thank you for your like"
       redirect_to '/photos'
     else
