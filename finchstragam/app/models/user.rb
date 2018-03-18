e5cb70de73bfaa3083e531ca7a824d2fac395c60
Josué Estévez Fernández <jestfer6@gmail.com>
2018-03-18 02:33:12 +0000

    fixed issue with devise connection, create relations comes next

--- finchstragam/app/models/user.rb
+++ finchstragam/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  extend Devise::Models
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- finchstragam/config/routes.rb
+++ finchstragam/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  
   resources :pictures do
     resources :comments
   end
