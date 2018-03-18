f4811f40bc1630ccab39180a2c428b86d1d5f4d0
Matt Burstein <j.matthew.burstein@gmail.com>
2018-03-18 12:18:46 +0000

    Add user seeds to db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user1, user2 = User.new, User.new
+user1.email = "lint@lint.com"
+user2.email = "linter@linter.com"
+user1.password = "iLoveLint"
+user2.password = "iLoveLintToo"
+[user1, user2].each(&:save!)
