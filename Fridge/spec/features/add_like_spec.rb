8e1cf5f68e1fd9ec754d0a704c31f76c5f77a533
kotpsot <zygmunt.justyna@gmail.com>
2018-03-18 15:05:04 +0000

    minor changes in style, wrote new feature test for double liking

--- Fridge/spec/features/add_like_spec.rb
+++ Fridge/spec/features/add_like_spec.rb
@@ -5,9 +5,21 @@ feature 'Adding likes' do
   scenario 'Number of likes is increasing' do
     sign_up
     upload_photo
-    click_on('Like')
+    click_on('❤️')
     expect(current_path).to eq('/photos')
     expect(page.status_code).to eq(200)
     expect(page.all('.like_count')[0]).to have_content('1')
-    end
+  end
+
+  scenario 'One user cannot like the same photo twice' do
+    sign_up
+    upload_photo
+    click_on('❤️')
+    click_on('❤️')
+    expect(current_path).to eq('/photos')
+    expect(page.status_code).to eq(200)
+    expect(page.all('.like_count')[0]).to have_content('1')
+  end
+
+
   end
