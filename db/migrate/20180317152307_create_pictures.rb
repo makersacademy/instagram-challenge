d875f42f321a68b321dfbdf5b9e281ce56b7651c
Daniel Keen <keendaniel01@gmail.com>
2018-03-18 16:13:26 +0000

    databases linked together

--- db/migrate/20180317152307_create_pictures.rb
+++ db/migrate/20180317152307_create_pictures.rb
@@ -3,7 +3,7 @@ class CreatePictures < ActiveRecord::Migration[5.1]
     create_table :pictures do |t|
       t.text :image
       t.text :description
-
+      t.belongs_to :user, index: true
       t.timestamps
     end
   end
