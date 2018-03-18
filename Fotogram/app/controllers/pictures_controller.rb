ea30fc375ba242c11f6940c9546cee7700fc44db
jose <jennymarin1989@hotmail.com>
2018-03-18 17:46:57 +0000

    adding styling

--- Fotogram/app/controllers/pictures_controller.rb
+++ Fotogram/app/controllers/pictures_controller.rb
@@ -9,11 +9,11 @@ class PicturesController < ApplicationController
   end
 
   def new
-    @picture = Picture.new
+    @picture = current_user.pictures.build #Create new pictures only for the user
   end
 
   def create
-    @picture = Picture.new(pic_params)
+    @picture = current_user.pictures.build (pic_params)
 
     if @picture.save
       redirect_to @picture, notice:"Your picture was succesfully posted"
