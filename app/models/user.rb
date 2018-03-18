a5c26f4e8c6ca8f7ea770244ec6afb36ec9eb00a
Daniel Keen <keendaniel01@gmail.com>
2018-03-18 11:48:24 +0000

    Basic devise user authentication set up

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :pictures
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :views
+  devise_for :users
   resources :pictures
 
   root 'pictures#index'
