bd1c6fa8f7db85822b55cfb4f0e1f89670344bfe
Reena <reena.sanjay.sharma@gmail.com>
2018-03-21 21:54:26 +0000

    added  uploder storage = fog

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,7 +5,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   #storage :file
-  #storage :fog
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
