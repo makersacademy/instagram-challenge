8814707d3bd3858cedfe8f545b6838ac7d2f191c
danielcampos <ddregalo@gmail.com>
2018-04-14 17:38:37 +0100

    Update post controller and model to accept moment image with valid content type jpg,jpeg,gif,png - rubocop offences run and correct

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,14 @@
 class PostsController < ApplicationController
   def index
   end
+
+  def create
+    @post = Post.create(post_params)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:moment)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  # Validate moment content_type for medium and thumb sizes and file types
+  has_attached_file :moment, styles: { medium: "300x300>", thumb: "80x80>" }, default_url: "/images/:style/moment-default.jpg"
+  validates_attachment_content_type :moment, content_type: ['image/jpeg', 'image/jpg', 'image/gif', 'image/png']
 end
