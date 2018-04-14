ad9b8de0f0f3eef3ac0c6005474e0b6598a67f25
Vivian Allen <mrvivianallen@gmail.com>
2018-04-14 11:00:05 +0100

    basic image posting feature test added, red

---
+++ spec/features/posting_spec.rb
@@ -0,0 +1,13 @@
+
+RSpec.feature "User posting", type: :feature do
+
+  scenario "can post an image and see it" do
+    testImageURL = "https://upload.wikimedia.org/wikipedia/commons/f/ff/Domestic_goat_kid_in_capeweed.jpg"
+    click_button "Submit"
+    visit "/"
+    click_link "New post"
+    fill_in "imageURL", with: testImageURL
+    click_button "Submit"
+    expect(page).to have_content(testImageURL)
+  end
+end
