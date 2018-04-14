1349216a9c43e3c644ee2364e73eb959743e5512
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-04-14 18:44:03 +0100

    Add username to user table

--- db/migrate/20180414171541_devise_create_users.rb
+++ db/migrate/20180414171541_devise_create_users.rb
@@ -4,6 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :username
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 20180414171541) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string "username"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
