2863cda9eee960525b8d6e94038bfa69a0573b85
Chiaki Mizuta <chiakimizuta61@gmail.com>
2018-04-14 16:06:14 +0100

    added rmagick and migrated with db

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ ruby '2.5.0'
 
 gem "rubocop", "0.48.1"
 gem "rubocop-rails"
+gem "rmagick", "2.16.0"
+gem "paperclip", "~> 6.0.0"
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.0'
 # Use postgresql as the database for Active Record
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
-
+  Paperclip.options[:command_path] = "/Users/chiakimizuta/.tokaido/bin/"
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
