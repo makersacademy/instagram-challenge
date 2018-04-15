222584dac756737a0d8af7e05405a6994e55074c
Somayeh <mail.somayeh@gmail.com>
2018-04-15 22:06:40 +0100

    generate home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'home/front'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'home#front'
 end
