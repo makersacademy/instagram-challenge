c9ef5f4c4a6f46fbb2d4cae5839a89345084ef5f
George Drayson <drayson.g@mac.com>
2018-04-15 16:20:15 +0100

    Heroku auto deployment test

--- spec/features/user_can_add_a_photo_spec.rb
+++ spec/features/user_can_add_a_photo_spec.rb
@@ -5,6 +5,7 @@ RSpec.feature "Adding photo", type: :feature do
     sign_up
     add_post
     expect(page).to have_content('Beautiful sunset')
+    expect(page).to have_css("img[src*='https://cdn.acidcow.com/pics/20131010/instagram_16.jpg']")
     expect(page).to have_current_path("/posts")
   end
 end
