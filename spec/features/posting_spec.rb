518d9c498c61b1cf10d93d75114b518c6193928e
Vivian Allen <mrvivianallen@gmail.com>
2018-04-15 15:49:28 +0100

    ruby version bug in travis yml fixed

--- spec/features/posting_spec.rb
+++ spec/features/posting_spec.rb
@@ -5,7 +5,7 @@ RSpec.feature "User posting", type: :feature do
 
   scenario "can post an image and see it" do
     testImageURL = "https://upload.wikimedia.org/wikipedia/commons/f/ff/Domestic_goat_kid_in_capeweed.jpg"
-    visit "/"
+    sign_up_test_user
     click_link "Post a Photo"
     fill_in "post[image_url]", with: testImageURL
     click_button "Post"
