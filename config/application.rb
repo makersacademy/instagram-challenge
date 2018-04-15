4d62907a16773d0513a7318d6f4c086b8fd75834
Hemesh-Unka <hemeshunka@gmail.com>
2018-04-15 11:55:09 +0100

    added rspec gem

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Chur
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.generators.test_framework :rspec
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
