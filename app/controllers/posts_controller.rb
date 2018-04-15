e45f3b51ec9b712838bdbaef9b492a4285e941fd
Chiaki Mizuta <chiakimizuta61@gmail.com>
2018-04-15 14:10:37 +0100

    user can delete their posts, actually, all posts at this point

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,11 @@ class PostsController < ApplicationController
     @post.update(post_params)
     redirect_to(post_path(@post))
   end
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end  
 end
 
 private
