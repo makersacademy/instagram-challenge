2e1847dc04ef7ef15e36586c03fe6a463dc7721c
Cindy <jl7e12@gmail.com>
2018-04-15 12:21:15 +0100

    Revert "Add signup, login and logout feature"
    This reverts commit bf7b0a1af7021f8109fa97be9084656fd0ebad58.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,4 @@ class Post < ApplicationRecord
 
   has_attached_file :image, styles: { medium: "500x500>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
-  belongs_to :user
 end
