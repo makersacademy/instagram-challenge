78ec151859d0776959f4bc7ebf289d74d230e124
Cindy <jl7e12@gmail.com>
2018-04-15 20:31:45 +0100

    Added update post feature and time

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,16 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to(post_path(@post))
+  end
+
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
