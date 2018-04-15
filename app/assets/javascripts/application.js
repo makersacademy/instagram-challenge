feb2828f47ea2bab12b2d82a76c0b20485de8f25
Somayeh <mail.somayeh@gmail.com>
2018-04-15 22:02:22 +0100

    Add Navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,16 @@
 //
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
+
+
+@import "bootstrap-sprockets";
+@import "bootstrap";
+@import "font-awesome";
+@import "jquery.datetimepicker";
+@import "*";
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap
+//= require bindWithDelay
+//= require jquery.datetimepicker
 //= require_tree .
