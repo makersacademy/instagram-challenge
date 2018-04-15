1f89e6b4190a7350749598a6614fdf4eccf76b7a
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-04-15 23:39:28 +0100

    Add Comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "home#index"
   devise_for :users
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180415135551) do
+ActiveRecord::Schema.define(version: 20180415222633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "body"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -40,4 +49,5 @@ ActiveRecord::Schema.define(version: 20180415135551) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
 end
