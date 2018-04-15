b9664bed05ed6bffd4110870e1638c0eced221d1
Somayeh <mail.somayeh@gmail.com>
2018-04-15 23:46:43 +0100

    create post to add a new post and picture

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_15_211833) do
+ActiveRecord::Schema.define(version: 2018_04_15_222330) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "attachment"
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
