dd98e8e4dd81ca0d37b18cfd7550446e148fbf7d
agnesdonat <agi.donat@gmail.com>
2018-04-15 20:54:56 +0100

    Gem devise added, sign up/log in buttons on Welcome page tested

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,4 @@
 class WelcomeController < ApplicationController
-  def index
-  end
+  # def index
+  # end
 end
--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -7,4 +7,20 @@ RSpec.describe "Welcome", :type => :feature do
       expect(page).to have_content('Welcome to Cheestagram')
     end
   end
+
+ feature 'user wants to sign up' do
+   scenario 'user clicks Sign up button' do
+     visit '/'
+     click_button('Sign up')
+     expect(page).to have_content('Sign up')
+   end
+ end
+
+ feature 'user wants to log in' do
+   scenario 'user clicks Log in button' do
+    visit '/'
+    click_button('Log in')
+    expect(page).to have_content('Email')
+   end
+  end
 end
