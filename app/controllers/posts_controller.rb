ea65b4f552157adb191d4dfa1a56d2721dc95cf5
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-04-15 21:45:10 +0100

    Add functionality delete post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -33,6 +33,12 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to root_path
+  end
+
   private
 
   def post_params
