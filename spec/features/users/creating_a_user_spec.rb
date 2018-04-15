8f3c4c1267f29a6b81f9c801d86e5ec89623d1a8
antcin <ciniglioantonio@gmail.com>
2018-04-15 16:00:16 +0100

    test and implement functionality that requires a user to have a username

--- spec/features/users/creating_a_user_spec.rb
+++ spec/features/users/creating_a_user_spec.rb
@@ -5,6 +5,7 @@ feature 'Creating a new user' do
     visit '/'
     click_link 'Register'
   end
+
   scenario 'can create a new user via the index page' do
     fill_in 'Username', with: 'jonbonjovi'
     fill_in 'Email', with: 'jonbonjovi@gmail.com'
@@ -13,5 +14,15 @@ feature 'Creating a new user' do
 
     click_button 'Sign up'
     expect(page).to have_content('Welcome to Trollstagram! You have signed up successfully.')
+  end
+
+  scenario 'requires a username to create an account' do
+    fill_in 'Email', with: 'jonbonjovi@gmail.com'
+    fill_in 'Password', with: 'itsMyLife99', match: :first
+    fill_in 'Password confirmation', with: 'itsMyLife99'
+
+    click_button 'Sign up'
+    click_button 'Sign up'
+    expect(page).to have_content("You need a username to start trolling.")
   end
 end
