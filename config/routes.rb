82f93e69791c1d4c526f84e0af7101cc95b76602
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-04-15 14:47:16 +0100

    Add Posts controller, routes & views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'home/index'
-
-  get 'home/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "home#index"
+  devise_for :users
+  resources :posts
 end
