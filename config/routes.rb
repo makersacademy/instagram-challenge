89e79656ecfed1ec1b24fad3242bcd987d931471
paosch <paola_schiaffino@yahoo.co.uk>
2018-04-15 22:59:23 +0100

    User can log out

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     sessions: 'users/sessions'
   }
   get 'welcome/index' # places index view in localhost:3000/welcome/index
+  resources :posts
   root to: 'welcome#index' # places index view in localhost:3000 (home page)
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
