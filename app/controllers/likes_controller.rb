f804a1db050236ed284a4d97d2afde765cef57d6
Vivian Allen <mrvivianallen@gmail.com>
2018-04-15 21:26:26 +0100

    likes working

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -7,6 +7,11 @@ class LikesController < ApplicationController
     redirect_to posts_url
   end
 
+  def destroy
+    @post.likes.where(user_id: current_user.id, post_id: @post.id).destroy_all
+    redirect_to posts_url
+  end
+
   private
     def find_post
       @post = Post.find(params[:post_id])
