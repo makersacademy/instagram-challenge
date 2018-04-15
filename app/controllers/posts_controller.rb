84be0a768f958fdb0ce19db4a4672ececbea2c23
Chiaki Mizuta <chiakimizuta61@gmail.com>
2018-04-15 14:33:38 +0100

    only the user can delete their own posts by setting a user id according to the user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,34 +1,47 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
     @posts = Post.all
   end
+
   def new
     @post = Post.new
   end
+
   def create
-    @post = Post.create(post_params)
-    redirect_to posts_path
+    if @post = Post.create(post_params)
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
+
   def show
-    @post = Post.find(params[:id])
   end
+
   def edit
-    @post = Post.find(params[:id])
   end
+
   def update
-    @post = Post.find(params[:id])
-    @post.update(post_params)
-    redirect_to(post_path(@post))
+    if @post.update(post_params)
+      redirect_to posts_path
+    else
+      render :edit
+    end
   end
+
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
-    redirect_to posts_path
-  end  
+    redirect_to root_path
+  end
 end
 
 private
 
 def post_params
   params.require(:post).permit(:image, :caption)
+end
+
+def set_post
+  @post = Post.find(params[:id])
 end
