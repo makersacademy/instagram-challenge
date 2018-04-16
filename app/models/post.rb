8b44ac55eb6a9f076f98f942e29b1ae3ea5e982a
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-04-16 02:40:51 +0100

    Fix image errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   has_many :comments, dependent: :destroy
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 5 }
