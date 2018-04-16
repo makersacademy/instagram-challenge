2e1d19507da444c5129d80497160b90e068f1d61
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-04-16 08:43:07 +0100

    Config for Heroku deployment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   def index
-    @posts = Post.all.order('created_at DESC')
+    @post = Post.all.order('created_at DESC')
   end
 
   def new
