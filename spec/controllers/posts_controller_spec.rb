7ac937f8ea501bcb144a1f8232e525be98c2b708
Hemesh-Unka <hemeshunka@gmail.com>
2018-04-16 07:42:08 +0100

    removed welcome controller

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -19,13 +19,12 @@ RSpec.describe PostsController, type: :controller do
 
   describe "POST /" do
     it "responds with 200" do
-      post :create, params: { post: { message: "Hello, world!" } }
-      expect(response).to have_http_status(200)
+      post :create, params: { post: { caption: "Hello, world!" } }
+      expect(response).to redirect_to(posts_url)
     end
 
     it "creates a post" do
-      post :create, params: { post: { message: "Hello, world!" } }
-      redirect_to(posts_url)
+      post :create, params: { post: { caption: "Hello, world!" } }
       expect(Post.find_by(message: "Hello, world!")).to be
     end
   end
