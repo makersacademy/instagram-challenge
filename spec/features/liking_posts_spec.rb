df6002de06de7308dc7a54ccf93b86ce81059691
Vivian Allen <mrvivianallen@gmail.com>
2018-04-16 08:05:47 +0100

    unliking tested

--- spec/features/liking_posts_spec.rb
+++ spec/features/liking_posts_spec.rb
@@ -28,4 +28,20 @@ RSpec.feature "Liking posts", type: :feature do
     end
     expect(find("div#post_0")).not_to have_link("Like")
   end
+
+  scenario "can unlike a users photo" do
+    sign_up_test_user_1
+    post_goat
+    expect(find("div#post_0")).to have_text("0 \u2665")
+    click_link "Log Out"
+    create_test_user_2
+    sign_in_test_user_2
+    within("div#post_0") do
+      click_link "Like"
+    end
+    within("div#post_0") do
+      click_link "Unlike"
+    end
+    expect(find("div#post_0")).to have_text("0 \u2665")
+  end
 end
