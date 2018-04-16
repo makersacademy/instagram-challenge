8a13deb19899fee34fd38dc44c4f903b3506ad8c
Thomas Duffy <tomjamesduffy@gmail.com>
2018-04-16 12:35:13 +0100

    Fixed like button

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -41,7 +41,7 @@ class PicsController < ApplicationController
 
   def upvote
     @pic.upvote_by current_user
-    redirect_to :back
+    redirect_back(fallback_location: root_path)
   end
 
   private
