f32cdb2aafca4baac5f83cde40386750297c3558
antcin <ciniglioantonio@gmail.com>
2018-04-21 15:55:37 +0100

    add caption limit; implement time ago helper

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,7 @@ class Post < ApplicationRecord
 
   validates :user_id, presence: true
   validates :image, presence: true
+  validates :caption, length: { minimum: 3, maximum: 300 }
 
   has_attached_file :image, styles: { :medium => '640x'}
 
