46d73b0414ccbf7def3d7cc7430a84219448c09b
danielcampos <ddregalo@gmail.com>
2018-04-27 23:15:45 +0100

    Insert relative time module into app posts helper - TDD feature implementation

--- spec/features/can_post_image_spec.rb
+++ spec/features/can_post_image_spec.rb
@@ -1,9 +1,9 @@
-# Tests that user can post image (using paperclip)
-# RSpec.feature 'Experience', type: :feature do
-#   scenario 'Post image and caption' do
-#     sign_up
-#     create_img_post
-#     expect(page).to have_css("img[src*='test.jpg']")
-#     click_link 'DELETE MOMENT'
-#   end
-# end
+Tests that user can post image (using paperclip)
+RSpec.feature 'Experience', type: :feature do
+  scenario 'Post image and caption' do
+    sign_up
+    create_img_post
+    expect(page).to have_css("img[src*='test.jpg']")
+    click_link 'DELETE MOMENT'
+  end
+end
