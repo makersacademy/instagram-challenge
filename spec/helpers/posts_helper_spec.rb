adc2eba4d737a19d8d92f9fe46b992e35f90d4a9
danielcampos <ddregalo@gmail.com>
2018-04-28 02:44:00 +0100

    Complete post stub testing for time in words plural

--- spec/helpers/posts_helper_spec.rb
+++ spec/helpers/posts_helper_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe PostsHelper, type: :helper do
         expect(helper.get_time(1)).to eq("Posted 1 hour ago")
       end
 
-      it "returns relative post time in hours - singular" do
+      it "returns relative post time in days - singular" do
         allow(helper).to receive(:calculator).and_return(86401)
         expect(helper.get_time(1)).to eq("Posted 1 day ago")
       end
@@ -29,9 +29,41 @@ RSpec.describe PostsHelper, type: :helper do
         expect(helper.get_time(1)).to eq("Posted 1 week ago")
       end
 
-      it "returns relative post time in weeks - singular" do
+      it "returns relative post time in years - singular" do
         allow(helper).to receive(:calculator).and_return(31536001)
         expect(helper.get_time(1)).to eq("Posted 1 year ago")
+      end
+    end
+
+    context "Stub time - plural" do
+      it "returns relative post time in seconds - plural" do
+        allow(helper).to receive(:calculator).and_return(2)
+        expect(helper.get_time(1)).to eq("Posted 2 seconds ago")
+      end
+
+      it "returns relative post time in minutes - plural" do
+        allow(helper).to receive(:calculator).and_return(121)
+        expect(helper.get_time(1)).to eq("Posted 2 minutes ago")
+      end
+
+      it "returns relative post time in hours - plural" do
+        allow(helper).to receive(:calculator).and_return(7201)
+        expect(helper.get_time(1)).to eq("Posted 2 hours ago")
+      end
+
+      it "returns relative post time in days - plural" do
+        allow(helper).to receive(:calculator).and_return(172801)
+        expect(helper.get_time(1)).to eq("Posted 2 days ago")
+      end
+
+      it "returns relative post time in weeks - plural" do
+        allow(helper).to receive(:calculator).and_return(1209601)
+        expect(helper.get_time(1)).to eq("Posted 2 weeks ago")
+      end
+
+      it "returns relative post time in years - plural" do
+        allow(helper).to receive(:calculator).and_return(63072002)
+        expect(helper.get_time(1)).to eq("Posted 2 years ago")
       end
     end
   end
