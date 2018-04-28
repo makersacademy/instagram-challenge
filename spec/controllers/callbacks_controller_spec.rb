e2cd402592518f8a14b60e20d7cdde57af1efd92
danielcampos <ddregalo@gmail.com>
2018-04-28 22:18:02 +0100

    Feature test - sign in with google failure

--- spec/controllers/callbacks_controller_spec.rb
+++ spec/controllers/callbacks_controller_spec.rb
@@ -1,12 +1,10 @@
 # RSpec.describe CallbacksController, type: :controller do
 #
 #   before(:each) do
-#     Rails.application.env_config["devise.mapping"] = Devise.mappings[:user]
-#     Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
 #   end
 #
 #   it "gets new certification form" do
-#     get new_certification_path
-#     expect(response).to be_success
+# OmniAuth.config.mock_auth[:twitter] = :invalid_credentials
+
 #   end
 # end
--- spec/features/can_signin_with_google_spec.rb
+++ spec/features/can_signin_with_google_spec.rb
@@ -1,7 +1,26 @@
 # Tests that a user can sign in with google (using omniauth + oauth2)
 RSpec.describe 'Experience', type: :feature do
-  scenario 'Can sign in with goodle' do
+  scenario 'Can sign in with google' do
+    # Turn on 'test mode' for Omniauth
+    OmniAuth.config.test_mode = true
+    # Mock google authentification hash for integration testing
+    OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new(provider: "google_oauth2",
+      uid: "123456789",
+      password: "testpassword",
+      info: {
+        username: "Miss Test",
+        email: "test@email.com"
+      })
     sign_in_with_google_oauth
     expect(page).to have_link("NEW MOMENT")
+  end
+
+  scenario "Sign in with google failure" do
+    # Turn on 'test mode' for Omniauth
+    OmniAuth.config.test_mode = true
+    # Mock google authentification hash with invalid credentials
+    OmniAuth.config.mock_auth[:google_oauth2] = :invalid_credentials
+    sign_in_with_google_oauth
+    expect(page).to have_content("Could not authenticate you from GoogleOauth2")
   end
 end
