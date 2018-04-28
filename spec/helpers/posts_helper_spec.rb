fbb15fcf9a71ab39876efc2f9d181c53eb39ee63
danielcampos <ddregalo@gmail.com>
2018-04-28 02:15:27 +0100

    Unit test posts helper module: Posted minute-singular

--- spec/helpers/posts_helper_spec.rb
+++ spec/helpers/posts_helper_spec.rb
@@ -3,9 +3,17 @@ require 'posts_helper'
 RSpec.describe PostsHelper, type: :helper do
 
   describe "#get_time" do
-    it "set time posted attribute" do
-      allow(helper).to receive(:calculator).and_return(1)
-      expect(helper.get_time(1)).to eq("Posted 1 second ago")
+    context "Stub time - singular" do
+      it "returns relative post time in seconds - singular" do
+        allow(helper).to receive(:calculator).and_return(1)
+        expect(helper.get_time(1)).to eq("Posted 1 second ago")
+      end
+
+      it "returns relative post time in minutes - singular" do
+        allow(helper).to receive(:calculator).and_return(61)
+        expect(helper.get_time(1)).to eq("Posted 1 minute ago")
+      end
     end
+
   end
 end
