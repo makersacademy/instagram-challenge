cc803a455ef2b2888813b2dfdee839c1e6024afe
danielcampos <ddregalo@gmail.com>
2018-04-28 02:24:34 +0100

    Unit test posts helper module: Posted week-singular

--- spec/helpers/posts_helper_spec.rb
+++ spec/helpers/posts_helper_spec.rb
@@ -23,6 +23,11 @@ RSpec.describe PostsHelper, type: :helper do
         allow(helper).to receive(:calculator).and_return(86401)
         expect(helper.get_time(1)).to eq("Posted 1 day ago")
       end
+
+      it "returns relative post time in weeks - singular" do
+        allow(helper).to receive(:calculator).and_return(604801)
+        expect(helper.get_time(1)).to eq("Posted 1 week ago")
+      end
     end
   end
 end
