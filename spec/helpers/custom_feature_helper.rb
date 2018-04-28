031c437a0c31a27071985aeb4f1567c3624c84ae
danielcampos <ddregalo@gmail.com>
2018-04-29 00:45:51 +0100

    TDD user can only delete their own posts

--- spec/helpers/custom_feature_helper.rb
+++ spec/helpers/custom_feature_helper.rb
@@ -25,6 +25,15 @@ def sign_up
   click_button "Sign up"
 end
 
+def sign_up_user_2
+  visit "/users/sign_up"
+  fill_in "user[username]", with: "testUser2"
+  fill_in "user[email]", with: "example2@email.com"
+  fill_in "user[password]", with: "password2"
+  fill_in "user[password_confirmation]", with: "password2"
+  click_button "Sign up"
+end
+
 def sign_out
   click_link 'SIGN OUT'
 end
