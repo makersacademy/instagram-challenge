9af0b07bdbac87b9ba9b5b38a97e9736876ad952
Shih Han <swang05@mail.bbk.ac.uk>
2018-05-12 17:11:46 +0100

    image can't be blank

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,13 @@
 class Photo < ApplicationRecord
   mount_uploader :image, ImageUploader
+  validates :image, presence: true
+
+  validates_processing_of :image
+  validate :image_size_validation
+
+private
+  def image_size_validation
+    errors[:image] << "should be less than 500KB" if image.size > 0.5.megabytes
+  end
+
 end
