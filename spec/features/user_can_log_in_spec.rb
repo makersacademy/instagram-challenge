71365b93eacc17e238e3108d36619baefa136e9f
Jordan Brooks <jorbrooks92@gmail.com>
2018-05-12 14:46:22 +0100

    add log-in feature test

---
+++ spec/features/user_can_log_in_spec.rb
@@ -0,0 +1,15 @@
+RSpec.feature "Login", type: :feature do 
+  scenario 'user can sign in' do
+    visit "/users/sign_up"
+    fill_in "user_email", with: "example@example.com"
+    fill_in "user_password", with: "ajdpwomakwmnd29q0jediaw"
+    fill_in "user_password_confirmation", with: "ajdpwomakwmnd29q0jediaw"
+    click_button "Sign up"
+    page.find_by_id('log_out').click
+    find("a[href='/users/sign_in']").click
+    fill_in "user_email", with: "example@example.com"
+    fill_in "user_password", with: "ajdpwomakwmnd29q0jediaw"
+    click_button "Log in"
+    expect(page).to have_content("Signed in successfully.")
+  end 
+end 
