eb5ff0005e0c5de73b496dd95666554d332f9ff8
Shih Han <swang05@mail.bbk.ac.uk>
2018-05-12 17:12:25 +0100

    different resize version created

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -33,7 +33,11 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fit => [100, 100]
+    process :resize_to_fit => [500, 500]
+  end
+
+  version :home do
+    process :resize_to_fit => [200, 200]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
