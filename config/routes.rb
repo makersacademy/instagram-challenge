ebd95bbdd9340d774902659c0fe4fabf291dbddc
Jihin Ip <jim_ip20@hotmail.com>
2018-05-12 16:36:00 +0100

    Deleting some unwanted text, adding resources to post

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get 'welcome/index'
   devise_for :users
   root to: 'welcome#index'
+
+  resources :post
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
