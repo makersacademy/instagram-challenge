945d2979773816a5946bbbceefb82b6abd02ee79
Gabriel Hochstatter <gabrielhochstatter@gmail.com>
2018-05-12 21:32:52 +0100

    basic setup

---
+++ fakegram/app/controllers/photos_controller.rb
@@ -0,0 +1,4 @@
+class PhotosController < ApplicationController
+  def new
+  end
+end
--- fakegram/config/routes.rb
+++ fakegram/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :photos
+  
+  root 'welcome#index'
 end
