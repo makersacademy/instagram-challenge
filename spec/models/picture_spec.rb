78532208135d7356d5035b860e6f75ceef279371
Jordan Brooks <jorbrooks92@gmail.com>
2018-05-13 13:39:26 +0100

    picture model specs

--- spec/models/picture_spec.rb
+++ spec/models/picture_spec.rb
@@ -2,4 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Picture, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  
+  it "has no pictures by default" do
+    expect(Picture.count).to eq 0
+  end
+
+  it "does not save without a user" do
+    picture = Picture.create({link: 'http://www.example.com'})
+    picture.save
+    expect(Picture.count).to eq 0
+  end
+
+  it "saves a valid post" do
+    user = User.create(email: 'example@example', password: 123456)
+    picture = Picture.create({link: 'http://www.example.com', user: user})
+    picture.save
+    expect(Picture.count).to eq 1
+  end
 end
