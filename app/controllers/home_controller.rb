414532d68d6d6daa257a773c4882318260958416
Jordan Brooks <jorbrooks92@gmail.com>
2018-05-13 17:41:00 +0100

    implement comments

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   def index
     @post = current_user.posts.build if user_signed_in?
     @picture_feed = Post.last(25)
+    @comment = @picture_feed[0].comments.build if @picture_feed[0]
   end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
   resources :posts, only: [:create, :destroy]
+  resources :comments, only: [:create]
 end
