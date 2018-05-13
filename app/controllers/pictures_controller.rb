93c084e94e1e8665c73c6837345bf5852b494879
Jordan Brooks <jorbrooks92@gmail.com>
2018-05-13 14:39:28 +0100

    succesful pictures controller test for #create action

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,20 +2,18 @@ class PicturesController < ApplicationController
   def create
     @picture = current_user.pictures.build(picture_params)
     @picture.avatar = params[:picture][:avatar]
+
     if @picture.save
       flash[:success] = "Picture created!"
-      redirect_to root_url
+      redirect_to root_url, :status => 201
     else
       render 'home/index'
     end
   end
 
-  def destroy
-  end
-
   private
 
-    def picture_params
-      params.require(:picture).permit(:link, :image)
-    end
+  def picture_params
+    params.require(:picture).permit(:link, :image)
+  end
 end
