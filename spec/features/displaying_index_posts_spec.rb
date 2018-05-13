987f0c5df53a00281003547e15bee2a7578158fb
Andrew Kemp <akemp850@gmail.com>
2018-05-13 19:14:17 +0100

    Added test for links to individual posts from Index page

--- spec/features/displaying_index_posts_spec.rb
+++ spec/features/displaying_index_posts_spec.rb
@@ -9,17 +9,24 @@ describe 'Displaying posts' do
     click_link 'Index'
   end
 
-feature 'Index displays a list of posts' do
-  scenario 'the index page creates the correct post information' do
-    visit '/'
-    click_link 'New Post'
-    attach_file('Image', "spec/files/images/dog.jpg")
-    fill_in 'Caption', with: 'This is post two'
-    click_button 'Create Post'
-    click_link 'Index'
-    expect(page).to have_content("This is post one")
-    expect(page).to have_content("This is post two")
-    expect(page).to have_css("img[src*='dog']")
+  feature 'Index displays a list of posts' do
+    scenario 'the index page creates the correct post information' do
+      visit '/'
+      click_link 'New Post'
+      attach_file('Image', "spec/files/images/dog.jpg")
+      fill_in 'Caption', with: 'This is post two'
+      click_button 'Create Post'
+      click_link 'Index'
+      expect(page).to have_content("This is post one")
+      expect(page).to have_content("This is post two")
+      expect(page).to have_css("img[src*='dog']")
+    end
+
+    scenario 'each post has a link to take you to that post' do
+      visit '/'
+      click_link 'Show'
+      expect(page).to have_content("This is post one")
+      expect(page).to have_css("img[src*='dog']")
+    end
   end
-end
 end
