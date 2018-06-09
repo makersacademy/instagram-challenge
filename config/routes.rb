502a1ec93bd4164a04ba4613f2c0ee7ebffd047b
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-06-09 14:38:19 +0100

    Initial db setup and post index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'posts/index'
+  root 'posts#index'
+  resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20180609130835_create_posts.rb
+++ db/migrate/20180609130835_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-
+      t.string :message
       t.timestamps
     end
   end
