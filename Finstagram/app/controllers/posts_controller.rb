ff12b0ac43edf1ca73d5d5d69eb591681b2b550f
Dione Estabillo <dione.estabillo@gmail.com>
2018-06-09 22:32:46 +0100

    Added edit and delet post functionalities

--- Finstagram/app/controllers/posts_controller.rb
+++ Finstagram/app/controllers/posts_controller.rb
@@ -24,6 +24,22 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to(post_path(@post))
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
   private
 
   def post_params
