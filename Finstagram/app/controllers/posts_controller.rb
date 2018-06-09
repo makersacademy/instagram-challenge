a1ca4c153acbcbbce8e66287e7571d2fc07e6192
Dione Estabillo <dione.estabillo@gmail.com>
2018-06-09 23:10:03 +0100

    Refactored posts controller

--- Finstagram/app/controllers/posts_controller.rb
+++ Finstagram/app/controllers/posts_controller.rb
@@ -1,7 +1,11 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.all
+  end
+
+  def show
   end
 
   def new
@@ -9,40 +13,43 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.create(post_params)
-
-    if @post.save
-      flash[:success] = "Your post has been created."
-      redirect_to @post
+    if @post = Post.create(post_params)
+      flash[:success] = "Your post has been created!"
+      redirect_to posts_path
     else
-      flash[:alert] = "Halt! You need an image to post here!"
+      flash.now[:alert] = "Halt! You need an image to post here!"
       render :new
     end
   end
 
-  def show
-    @post = Post.find(params[:id])
-  end
-
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
-    @post.update(post_params)
-    redirect_to(post_path(@post))
+    if @post.update(post_params)
+      flash[:success] = "Post updated."
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "Sorry, update failed."
+      render :edit
+    end
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
-    redirect_to posts_path
+    redirect_to root_path
   end
+
+
 
   private
 
   def post_params
     params.require(:post).permit(:caption, :image)
   end
+
+  def set_post
+   @post = Post.find(params[:id])
+ end
+
 end
