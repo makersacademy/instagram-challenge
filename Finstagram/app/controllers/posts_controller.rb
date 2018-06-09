4c4b971ee312c9cbc8a58699465eb76e46857f28
Dione Estabillo <dione.estabillo@gmail.com>
2018-06-09 19:54:59 +0100

    Added alert message for  image needed to create post

--- Finstagram/app/controllers/posts_controller.rb
+++ Finstagram/app/controllers/posts_controller.rb
@@ -9,7 +9,14 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.create(post_params)
-    redirect_to @post
+
+    if @post.save
+      flash[:success] = "Your post has been created."
+      redirect_to @post
+    else
+      flash[:alert] = "Halt! You need an image to post here!"
+      render :new
+    end
   end
 
   def show
--- Finstagram/spec/features/creating_posts_spec.rb
+++ Finstagram/spec/features/creating_posts_spec.rb
@@ -10,4 +10,12 @@ feature 'Creating posts' do
     expect(page).to have_content('#coffeetime')
     expect(page).to have_css("img[src*='coffee.jpg']")
   end
+
+    it 'needs an image to create a post' do
+    visit '/'
+    click_link 'New Post'
+    fill_in 'Caption', with: "No picture"
+    click_button 'Create Post'
+    expect(page).to have_content('Halt! You need an image to post here!')
+  end
 end
