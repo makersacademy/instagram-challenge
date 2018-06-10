98332e2b291ae0c9e2231738bd0a4d8a69c7c073
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-06-10 15:41:04 +0100

    Feature test for log out

--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -1,4 +1,4 @@
-def sign_up_sign_in_make_post
+def sign_up_and_sign_in
   visit('/')
   click_link "Sign up"
   fill_in "Username", with: 'Example'
@@ -6,6 +6,9 @@ def sign_up_sign_in_make_post
   fill_in "Password", with: '123456'
   fill_in "Password confirmation", with: '123456'
   click_button "Sign up"
+end
+
+def make_post
   page.attach_file("post_photo", File.absolute_path('./app/assets/images/Iceland_Northern_Lights_Special_4_Nights_1.jpg'))
   fill_in "Caption", with: 'example'
   click_button "Submit"
