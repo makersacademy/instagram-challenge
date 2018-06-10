a6e648f4895c3a824facc98d68e5fec6fec34cc5
Dione Estabillo <dione.estabillo@gmail.com>
2018-06-10 20:42:51 +0100

    Added feature to protect personal posts

--- Finstagram/app/controllers/posts_controller.rb
+++ Finstagram/app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :owned_post, only: [:edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -10,15 +11,17 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   def create
-    if @post = Post.create(post_params)
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
       flash[:success] = "Your post has been created!"
       redirect_to posts_path
     else
-      flash.now[:alert] = "Halt! You need an image to post here!"
+      flash[:alert] = "Sorry, your new post couldn't be created!"
       render :new
     end
   end
@@ -51,6 +54,12 @@ class PostsController < ApplicationController
 
   def set_post
    @post = Post.find(params[:id])
- end
+  end
 
+  def owned_post
+    unless current_user == @post.user
+      flash[:alert] = "This post does not belong to you"
+      redirect_to root_path
+    end
+  end
 end
