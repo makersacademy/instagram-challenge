98d7cf097d8ad4c322ebf44833689c2f23663099
Dione Estabillo <dione.estabillo@gmail.com>
2018-06-10 19:01:37 +0100

    Created Posts and User relationship in db

--- Finstagram/app/models/post.rb
+++ Finstagram/app/models/post.rb
@@ -1,9 +1,12 @@
 class Post < ApplicationRecord
+  validates :user_id, presence: true
 
   validates :image, presence: true
 
   has_attached_file :image, styles: { :medium => "640x" }
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+  belongs_to :user
 
 end
--- Finstagram/db/schema.rb
+++ Finstagram/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_10_153435) do
+ActiveRecord::Schema.define(version: 2018_06_10_175620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 2018_06_10_153435) do
     t.string "image_content_type"
     t.integer "image_file_size"
     t.datetime "image_updated_at"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -65,4 +67,5 @@ ActiveRecord::Schema.define(version: 2018_06_10_153435) do
     t.index ["user_name"], name: "index_users_on_user_name", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
