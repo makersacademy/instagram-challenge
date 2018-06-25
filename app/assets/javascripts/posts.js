96993bcbae20afe926db75ffdef6628ec1160381
Andrew <akemp850@gmail.com>
2018-06-25 10:36:32 +0100

    updated new and edit post forms

---
+++ app/assets/javascripts/posts.js
@@ -0,0 +1,4 @@
+var loadFile = function(event) {
+  var output = document.getElementById('image-preview');
+  output.src = URL.createObjectURL(event.target.files[0]);
+};
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,14 @@ module ApplicationHelper
       success: 'alert-success',
       error: 'alert-danger',
       alert: 'alert-warning',
-      notice: 'aler-info'
+      notice: 'alert-info'
     }[flash_type.to_sym] || flash_type.to_s
+  end
+
+  def form_image_select(post)
+    return image_tag post.image.url(:medium),
+        id: 'image-preview',
+        class: 'img-responsive' if post.image.exists?
+    image_tag 'placeholder.jpg', id: 'image-preview', class: 'img-responsive'
   end
 end
