5bdb8801d1d94179789bf8edd031b92e8020861c
Robert Faldo <robertfaldo@gmail.com>
2018-07-08 15:47:40 +0100

    Add image resize

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,9 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
+
+  process resize_to_fit: [300, 300]
 
   # Choose what kind of storage to use for this uploader:
   storage :file
