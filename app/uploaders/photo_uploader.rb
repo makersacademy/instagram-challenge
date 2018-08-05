c663fca4ca6d30a3cdb54f719e6b3f54050707ed
John Forster <j_forster@hotmail.co.uk>
2018-08-05 09:41:28 +0100

    Photos are renamed with a UUID

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -38,11 +38,22 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   def extension_whitelist
     %w(jpg jpeg gif png)
-   end
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   # def filename
   #   "something.jpg" if original_filename
   # end
+
+  def filename
+    p original_filename.present?
+    "#{secure_token}.#{file.extension}" if original_filename.present?
+  end
+
+  protected
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
+  end
 end
