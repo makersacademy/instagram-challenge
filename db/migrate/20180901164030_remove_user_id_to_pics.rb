387dffb46ce80dd2e94df2b9e83b4a602578f4d9
Farah Jabri <farahjabri@hotmail.com>
2018-09-01 21:32:50 +0100

    changes to db, added user Id

--- db/migrate/20180901164030_remove_user_id_to_pics.rb
+++
@@ -1,4 +0,0 @@
-class RemoveUserIdToPics < ActiveRecord::Migration[5.2]
-  def change
-  end
-end
--- db/migrate/20180901164423_add_user_id_to_pics.rb
+++
@@ -1,6 +0,0 @@
-class AddUserIdToPics < ActiveRecord::Migration[5.2]
-  def change
-    add_column :pics, :user_id, :integer
-    add_index :pics, :user_id
-  end
-end
