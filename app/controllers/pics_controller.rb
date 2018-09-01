1336c5005b7c1661bed06bd8fe1c3822de68d901
Farah Jabri <farahjabri@hotmail.com>
2018-09-01 14:50:25 +0100

    Added form for pic title and description, with new and create actions

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,4 +1,19 @@
 class PicsController < ApplicationController
   def index
-  end 
+  end
+
+  def new
+    @pic = Pic.new
+
+  end
+
+  def create
+    @pic = Pic.new(pic_params)
+  end
+
+  private
+
+  def pic_params
+    params.require(:pic).permit(:title, :description)
+  end
 end
