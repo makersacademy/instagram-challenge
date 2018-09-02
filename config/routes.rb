d9c05a310117868bc73717daa82ce01753751acd
dani <daniboo83@gmail.com>
2018-09-02 19:39:11 +0100

    add pages controller and redirection

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: redirect('instaposts')
+  get 'pages/about'
   resources :instaposts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
