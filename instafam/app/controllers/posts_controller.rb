c3aefa7e27669c12b9e90d003e2d4a9a770ccc9b
Marek <MarekSchmidt@outlook.com>
2018-09-02 21:07:23 +0100

    Users can post. Users can only edit or delete their own post. You can view a post's show page by clicking the image

--- instafam/app/controllers/posts_controller.rb
+++ instafam/app/controllers/posts_controller.rb
@@ -1,11 +1,56 @@
 class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
   def new
     @post = Post.new
   end
 
-  def index
+  def show
+    @post = Post.find(params[:id])
   end
 
-  def show
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:success] = "Successfully created post!"
+      redirect_to post_path(@post)
+    else
+      flash[:error] = @post.error.messages.full
+      redirect_to new_post_path
+    end
   end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(post_params)
+      flash[:success] = 'Post was successfully updated'
+      redirect_to post_path(@post)
+    else
+      flash[:error] = @post.error.messages.full
+      redirect_to edit_post_path
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+     if @post.destroy
+       flash[:success] = 'Post was successfully deleted'
+       redirect_to root_path
+     else
+       flash[:error] = @post.error.messages.full
+       redirect_to post_path(@post)
+     end
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:image, :description).merge(username: current_user.username)
+    end
 end
--- instafam/db/migrate/20180902175337_create_posts.rb
+++ instafam/db/migrate/20180902175337_create_posts.rb
@@ -1,6 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
+      t.string :username
       t.text :description
 
       t.timestamps
