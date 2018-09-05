910215e6191300db816b71940baedb0e9fe99d2c
Angela Wolff <warriorwomenblog@gmail.com>
2018-09-05 11:34:06 +0100

    Updating the carrier_wave initializer to connect with AWS

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -9,8 +9,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   # process resize_to_fit: [300, 300]
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  # storage :file
+   storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,4 +1,14 @@
 
 # frozen_string_literal: true
+# require 'carrierwave/orm/activerecord'
 
-require 'carrierwave/orm/activerecord'
+CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'                        # required
+  config.fog_credentials = {
+    provider:              'AWS',                        # required
+    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
+    aws_secret_access_key: ENV["AWS_SECRET_KEY"]       # required
+
+  }
+  config.fog_directory  = ENV["AWS_BUCKET"]              # required
+end
