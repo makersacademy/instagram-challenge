3b718ce50ef835b1798371fd812b0464a93c08e5
Angela Wolff <warriorwomenblog@gmail.com>
2018-09-05 11:36:34 +0100

    updating ENV variable names to match .env

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -6,8 +6,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
-    aws_secret_access_key: ENV["AWS_SECRET_KEY"]       # required
+    aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],        # required
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]       # required
 
   }
   config.fog_directory  = ENV["AWS_BUCKET"]              # required
