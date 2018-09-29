a6c84684d2672d625f382e31f846b078f9d68cae
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-09-29 13:09:49 +0100

    feat: user can submit new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,16 @@ class PostsController < ApplicationController
   def new
     @posts = Post.new
   end
+
+  def create
+    @post = Post.create(post_params)
+    redirect_to posts_url
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:image, :description)
+  end
+
 end
--- spec/features/user_can_post_an_image_spec.rb
+++ spec/features/user_can_post_an_image_spec.rb
@@ -4,7 +4,8 @@ RSpec.feature "Post image", type: :feature do
   scenario "User can submit an image" do
     visit "/posts/new"
 
-    fill_in "image", with: "https://images.unsplash.com/photo-1538111167946-8e0ed8259271?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f065d19a95cb84ff8445fd56f79d53dd&auto=format&fit=crop&w=1001&q=80"
+    fill_in "Image", with: "https://images.unsplash.com/photo-1538111167946-8e0ed8259271?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f065d19a95cb84ff8445fd56f79d53dd&auto=format&fit=crop&w=1001&q=80"
+    fill_in "Description", with: "A test description"
     click_button "Submit"
 
     expect(page).to have_xpath("//img[contains(@src, 'https://images.unsplash.com/photo-1538111167946-8e0ed8259271?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f065d19a95cb84ff8445fd56f79d53dd&auto=format&fit=crop&w=1001&q=80')]")
