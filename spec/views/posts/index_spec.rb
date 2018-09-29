1376e197d9a615277289e5a6937b9f673f81f935
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-09-29 17:39:46 +0100

    feat: posts show handle of the creator

--- spec/views/posts/index_spec.rb
+++ spec/views/posts/index_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe "posts/index.html.erb", type: :view do
 
   before(:each) do
-    assign(:posts, [Post.new(:image => "http://google.com", :description => "Test description")])
+    test_user = User.new(email: "fake123@cd.com", password: "123456")
+    assign(:posts, [Post.new(:image => "http://google.com", :description => "Test description", :user => test_user)])
   end
 
   scenario "renders new view" do
