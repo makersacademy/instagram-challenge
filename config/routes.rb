9ce0f0f581fe08fb052bbfc698efe862affc2f90
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-09-29 11:12:37 +0100

    feat: create posts/index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'posts/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :posts
 end
