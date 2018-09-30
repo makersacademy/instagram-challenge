9b1e1b86fde81723cb173b94149dc5460c2fca84
Malachy Gilchrist <malachygilchrist@gmail.com>
2018-09-30 22:53:35 +0100

    Write feature test for phot uplaod

---
+++ spec/features/user_can_upload_picture_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+RSpec.feature 'Upload Picture', type: :feature do
+  before(:each) do
+    user_params = {first_name: 'Tess', 
+                   last_name: 'Tester', 
+                   email: 'tess@tester.com', 
+                   password: 'secure'}
+    User.create(user_params)
+    visit '/welcome/home'
+    click_link 'Sign in'
+    fill_in 'session_email', with: 'tess@tester.com'
+    fill_in 'session_password', with: 'secure'
+    click_button 'Log In'
+  end
+
+  scenario 'user uploads picture' do
+    visit '/welcome/images'
+    attach_file('Upload Photo', Rails.root + 'spec/fixtures/test_upload.png')
+    click_button 'Upload'
+    expect(page).to have_css("img[src*='environ-peeling-kuur.jpg']")
+  end
+end
