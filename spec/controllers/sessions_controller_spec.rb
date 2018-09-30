a3c96fbdf7656805f95a164b4e81a57b56fa8ab1
Malachy Gilchrist <malachygilchrist@gmail.com>
2018-09-30 19:26:22 +0100

    Write first sessions controller test

---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.decribe SessionsController, type: :controller do
+  describe 'GET /new' do
+    it 'responds with 200' do
+      get :new
+      expect(response).to have_http_status(200)
+    end
+  end
+end
