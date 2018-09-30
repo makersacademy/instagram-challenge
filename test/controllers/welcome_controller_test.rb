8a67b53f8376d8dd00ab2b862a7d30be2664fa39
Malachy Gilchrist <malachygilchrist@gmail.com>
2018-09-30 19:20:41 +0100

    Remove redundant test directory

--- test/controllers/welcome_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class WelcomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get welcome_home_url
-    assert_response :success
-  end
-
-end
