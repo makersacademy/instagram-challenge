9be483b75c26027b7f789e894da7b2578ab99a12
jdm79 <james.malvern@gmail.com>
2018-09-30 17:29:17 +0100

    feature: run db:migrate to create and up and down method for the post model, to upload images

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_30_132902) do
+ActiveRecord::Schema.define(version: 2018_09_30_162518) do
 
   create_table "posts", force: :cascade do |t|
     t.string "caption"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.bigint "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
