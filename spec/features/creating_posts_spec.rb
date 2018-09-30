702ab068a946f1b970c8d656e5c81bee7996a87d
jdm79 <james.malvern@gmail.com>
2018-09-30 17:49:27 +0100

    test: adds test to check if an image is attached to the post or not

--- spec/features/creating_posts_spec.rb
+++ spec/features/creating_posts_spec.rb
@@ -10,4 +10,13 @@ feature 'Creating posts' do
     expect(page).to have_content('#coffeetime')
     expect(page).to have_css("img[src*='coffee.jpg']")
   end
+
+  scenario 'needs an image to create a post' do
+    visit '/'
+    click_link 'New Post'
+    fill_in 'Caption', with: 'oh no. oh no. no, no, no, no.' 
+    click_button 'Create Post'
+    expect(page).to have_content('oh no you didn\'t. needing an image as well as a caption, please' )
+  end
+
 end
