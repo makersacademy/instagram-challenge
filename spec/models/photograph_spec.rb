7331abbab8b2ecbeac25ac37bd3a42f0c0bd7744
Malachy Gilchrist <malachygilchrist@gmail.com>
2018-09-30 23:18:56 +0100

    Add photograph model tests

--- spec/models/photograph_spec.rb
+++ spec/models/photograph_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Photograph, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = User.create({ first_name: 'Tess', 
+                          last_name: 'Tester', 
+                          email: 'tess@tester.com', 
+                          password: 'secure' })
+    @photo = Photograph.create(user_id: @user.id, 
+                               aws_key: 'test_upload')
+  end
+
+  it 'has an id' do
+    expect(@photo.id).to be
+  end
+  it 'has a user_id' do
+    expect(@photo.user_id).to be
+  end
+  it 'has an aws_key' do
+    expect(@photo.aws_key).to be
+  end
 end
