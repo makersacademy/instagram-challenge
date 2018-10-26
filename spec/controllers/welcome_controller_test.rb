79b0639a0246bac660d537dd2d485099b74d0b72
Marianne Rachid <marianne.rachid@gmail.com>
2018-10-26 20:33:04 +0100

    Initialise rails
    - create rails app
    - install rspec

---
+++ spec/controllers/welcome_controller_test.rb
@@ -0,0 +1,10 @@
+
+require 'test_helper'
+
+class WelcomeControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get welcome_index_url
+    assert_response :success
+  end
+
+end
