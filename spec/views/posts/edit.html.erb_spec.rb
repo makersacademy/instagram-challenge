e825b71fb29b445f9146c1fb7246ecd42a8eddd0
Alex Kharouk <akharoukpp@gmail.com>
2018-10-27 12:41:45 +0100

    lint files

--- spec/views/posts/edit.html.erb_spec.rb
+++ spec/views/posts/edit.html.erb_spec.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe "posts/edit", type: :view do
   before(:each) do
     @post = assign(:post, Post.create!(
-      :caption => "MyString",
-      :picture => "MyString"
+                            caption: "MyString",
+                            picture: "MyString"
     ))
   end
 
@@ -12,7 +14,6 @@ RSpec.describe "posts/edit", type: :view do
     render
 
     assert_select "form[action=?][method=?]", post_path(@post), "post" do
-
       assert_select "input[name=?]", "post[caption]"
 
       assert_select "input[name=?]", "post[picture]"
--- spec/views/posts/new.html.erb_spec.rb
+++ spec/views/posts/new.html.erb_spec.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe "posts/new", type: :view do
   before(:each) do
     assign(:post, Post.new(
-      :caption => "MyString",
-      :picture => "MyString"
+                    caption: "MyString",
+                    picture: "MyString"
     ))
   end
 
@@ -12,7 +14,6 @@ RSpec.describe "posts/new", type: :view do
     render
 
     assert_select "form[action=?][method=?]", posts_path, "post" do
-
       assert_select "input[name=?]", "post[caption]"
 
       assert_select "input[name=?]", "post[picture]"
