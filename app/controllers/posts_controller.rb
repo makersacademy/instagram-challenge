b90829766b02ac0b01b00b14e8aa39d6b1a0d161
Florence Hapgood <flohapgood@icloud.com>
2018-10-27 23:42:46 +0100

    User can add(enormous) images

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,23 @@
 class PostsController < ApplicationController
-  
+
   def index
-  @posts = Post.all.reverse
+    @posts = Post.all.reverse
   end
 
-  def post_params
-    params.require(:post).permit(:title, :content, :image)
-  end
 
   def new
     @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:image, :caption)
   end
 
 end
