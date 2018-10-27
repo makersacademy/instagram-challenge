3b52ca4a5f0e0e50f70c273038ccdee075ff95e1
Alex Kharouk <akharoukpp@gmail.com>
2018-10-27 10:59:05 +0100

    set up rspec

--- test/controllers/landing_page_controller_test.rb
+++ test/controllers/landing_page_controller_test.rb
@@ -3,7 +3,7 @@
 require 'test_helper'
 
 class LandingPageControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
 end
