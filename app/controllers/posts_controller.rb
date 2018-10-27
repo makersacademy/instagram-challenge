d455ebe669d0c5e8a1bfab3ddc8a47fb1b59799b
Florence Hapgood <flohapgood@icloud.com>
2018-10-27 23:20:26 +0100

    Action View added, routes added and form for image created

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,15 @@
 class PostsController < ApplicationController
+  
   def index
+  @posts = Post.all.reverse
   end
+
+  def post_params
+    params.require(:post).permit(:title, :content, :image)
+  end
+
+  def new
+    @post = Post.new
+  end
+
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -57,5 +59,6 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
+  
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
