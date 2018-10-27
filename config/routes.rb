0de6ecb0cc76586e3e1806a3339e74bd61adf53b
djstephenson7 <dan_0502@hotmail.co.uk>
2018-10-27 13:31:36 +0100

    Recreated Users with scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :users
   get 'welcome/index'
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
