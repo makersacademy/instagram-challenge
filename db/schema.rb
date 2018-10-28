e4d954a9614801ed843fda7655cc4ddf973d54d9
Florence Hapgood <flohapgood@icloud.com>
2018-10-28 16:54:31 +0000

    Added forgein key, although not implimented features

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_28_123530) do
+ActiveRecord::Schema.define(version: 2018_10_28_165041) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2018_10_28_123530) do
     t.string "caption"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
