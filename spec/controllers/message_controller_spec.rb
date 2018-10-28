c9f9d8adb9f08213592d7926dff05018ec8deb9f
Andres Faraone <andresfaraone@outlook.com>
2018-10-28 18:19:21 +0000

    modified message controller spec

--- spec/controllers/message_controller_spec.rb
+++ spec/controllers/message_controller_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe MessagesController, type: :controller do
@@ -10,14 +12,14 @@ RSpec.describe MessagesController, type: :controller do
 
   describe 'GET /new' do
     it 'responds with 200' do
-      get :new, params: {user_id: 1}
+      get :new, params: { user_id: 1 }
       expect(response).to have_http_status(200)
     end
   end
 
   describe 'GET /profile' do
     it 'responds with 200' do
-      get :profile, params: {user_id: 1}
+      get :profile, params: { user_id: 1 }
       expect(response).to have_http_status(200)
     end
   end
@@ -30,20 +32,7 @@ RSpec.describe MessagesController, type: :controller do
 
     it "creates a post" do
       post :create, params: { user_id: 1, message: { text: "Hello, world!" } }
-      expect(Message.find_by( text: "Hello, world!")).to be
+      expect(Message.find_by(text: "Hello, world!")).to be
     end
   end
 end
-
-
-#
-#
-#   # describe "DELETE /posts/id" do
-#   #   it "responds with 200" do
-#   #     delete :destroy, params: { delete: { message: "Please delete me!", id: 1 } }
-#   #     # delete :destroy, params: { delete: { id: 1 } }
-#   #     # delete :destroy, id: post.id
-#   #     expect(response).to redirect_to(posts_url)
-#   #   end
-#   # end
-# end
