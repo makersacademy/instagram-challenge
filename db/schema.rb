6cd9e61069178fefc12cde2e8d5ad30a3b36e526
Darcie <darciewalsh@hotmail.com>
2018-10-28 11:16:16 +0000

    Add caption to Images table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181028103635) do
+ActiveRecord::Schema.define(version: 20181028111418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20181028103635) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "caption"
   end
 
   create_table "users", force: :cascade do |t|
