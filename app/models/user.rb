987ce111ec710cdc355d929084206de7869d2881
Darcie <darciewalsh@hotmail.com>
2018-10-28 10:57:07 +0000

    User can now have many posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :images
+  
   devise :database_authenticatable, :registerable,
          :rememberable, :validatable
 end
