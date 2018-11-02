39364daf92515d98dce069d0caac40e0530077ab
Samir Soormally <samirsoormally@gmail.com>
2018-11-02 11:00:25 +0000

    tests: picture controller spec includes when not logged in

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'pictures/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+  resource :pictures
   root to: "pictures#index"
 end
--- spec/controllers/pictures_controller_spec.rb
+++ spec/controllers/pictures_controller_spec.rb
@@ -2,23 +2,26 @@ require 'rails_helper'
 
 RSpec.describe PicturesController, type: :controller do
 
-  before(:each) do
-    user = User.create(username: "fakeUsername", email: "fake@person.com", password: "fakepassword")
-    sign_in user
-  end
-
-  describe "GET /" do
-    it "responds with 200" do
-      get :index
-      expect(response).to have_http_status(200)
+  context "when not signed in" do
+    describe "GET /index" do
+      it "is redirected" do
+        get :index
+        expect(response).to have_http_status(302)
+      end
     end
   end
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+  context "when signed in" do
+    before(:each) do
+      user = User.create(username: "fakeUsername", email: "fake@person.com", password: "fakepassword")
+      sign_in user
+    end
+
+    describe "GET /index" do
+      it "is successful" do
+        get :index
+        expect(response).to have_http_status(200)
+      end
     end
   end
-
 end
