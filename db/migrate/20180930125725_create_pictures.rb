15a9736075eaacd18abfe75bcf2837b0a6f9cb55
Samir Soormally <samirsoormally@gmail.com>
2018-11-04 08:12:58 +0000

    chore: updated pictures db schema to include images

--- db/migrate/20180930125725_create_pictures.rb
+++ db/migrate/20180930125725_create_pictures.rb
@@ -2,7 +2,7 @@ class CreatePictures < ActiveRecord::Migration[5.1]
   def change
     create_table :pictures do |t|
       t.references :user, foreign_key: true
-      t.string :url
+      t.string :caption
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180930131221) do
+ActiveRecord::Schema.define(version: 20181104075802) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "pictures", force: :cascade do |t|
     t.bigint "user_id"
-    t.string "url"
+    t.string "caption"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["user_id"], name: "index_pictures_on_user_id"
   end
 
