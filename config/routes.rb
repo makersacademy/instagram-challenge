9f20e7d3355445e90821fae8684c11f37f51c3dc
Samir Soormally <samirsoormally@gmail.com>
2018-11-07 14:15:58 +0000

    feat: added Like model and db migration

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   get 'pictures/index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resource :pictures
+  resource :pictures do
+    resources :likes
+  end
+
   root to: "pictures#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181104075802) do
+ActiveRecord::Schema.define(version: 20181107134405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "picture_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["picture_id"], name: "index_likes_on_picture_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
 
   create_table "pictures", force: :cascade do |t|
     t.bigint "user_id"
@@ -40,5 +49,7 @@ ActiveRecord::Schema.define(version: 20181104075802) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "likes", "pictures"
+  add_foreign_key "likes", "users"
   add_foreign_key "pictures", "users"
 end
