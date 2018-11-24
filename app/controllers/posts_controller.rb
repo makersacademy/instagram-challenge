91e23dc3c1ab9ae4160754fff6c25988ece00894
RyanWolfen7 <wolfen7lada@gmail.com>
2018-11-24 13:30:21 +0000

    added test for posts index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,5 @@
 class PostsController < ApplicationController
+  def index
+
+  end
 end
--- spec/features/creating_post_spec.rb
+++ spec/features/creating_post_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper.rb'
 
 feature 'Creating posts' do
-  scenario 'User can create a post' do
+  scenario 'can create a post' do
     visit '/'
     click_link 'New Post'
-    attach_file('Image', "spec/files/images/cofee.jpg")
+    attach_file('Image', "spec/files/images/ArmyRyan.jpg")
+    fill_in 'Caption', with: 'Did my makeup today #NoFilter'
+    click_button 'Create Post'
+    expect(page).to have_content('#NoFilter')
+    expect(page).to have_css("img[src*='ArmyRyan.jpg']")
   end
 end
