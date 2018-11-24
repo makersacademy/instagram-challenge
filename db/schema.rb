c99db256b75eedb27d0efb680e859614c65b48ce
Reka <rekapap28@gmail.com>
2018-11-24 21:24:59 +0000

    reset pictures

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_181_124_182_537) do
-  create_table "pictures", force: :cascade do |t|
-    t.string "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 2018_11_24_182537) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +25,5 @@ ActiveRecord::Schema.define(version: 20_181_124_182_537) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
+
 end
