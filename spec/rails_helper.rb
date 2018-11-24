c0033a884f4d2e5d98395e81a3ce58183ec2b240
Chloe Verity <chloeverity@hotmail.co.uk>
2018-11-24 16:07:16 +0000

    Factory Bot now functional, first test passing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -57,6 +57,7 @@ RSpec.configure do |config|
 
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
+  config.include FactoryBot::Syntax::Methods
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
