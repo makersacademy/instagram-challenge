e0e75988c05f12abb3f884968810abe979099db6
rbbri <robin.s.johnson@gmail.com>
2018-11-25 12:44:30 +0000

    rails_apps_composer: testing framework

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,18 @@ Bundler.require(*Rails.groups)
 
 module InstagramChallenge
   class Application < Rails::Application
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: false,
+        request_specs: false
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
