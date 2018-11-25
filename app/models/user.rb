d6d9909f6a85b8c7a141ed2e6e8054f690d99047
Caitlin Cooling <ccooling97@gmail.com>
2018-11-25 11:28:14 +0000

    Setting up devise users table - Deleted old users table - Created new devise users table - Now using devise views - User sees confirmation message when signing up - They are not redirected to a new page yet (meaning some tests have broken

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,6 @@
-# frozen_string_literal: true
-
 class User < ApplicationRecord
-  before_save { email.downcase! }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
-  validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
-  validates :password, length: { minimum: 6 }
-  has_secure_password
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
