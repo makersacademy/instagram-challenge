5e1ae474be055367ceb23d527989f5cb08377718
rbbri <robin.s.johnson@gmail.com>
2018-11-25 14:36:45 +0000

    Fix bug with saving name
    - Add devise paramter sanitizer to ApplicationController

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.5.1'
 gem 'bootsnap', '>= 1.1.0', require: false
+gem 'cloudinary'
 gem 'coffee-rails', '~> 4.2'
 gem 'jbuilder', '~> 2.5'
 gem 'puma', '~> 3.11'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :email, :password, :password_confirmation)
+    end
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name,
+               :email,
+               :password,
+               :password_confirmation,
+               :current_password)
+    end
+  end
 end
