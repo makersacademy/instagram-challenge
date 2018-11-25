4f50bba85d8a5fd067c349f0d1aa88ebc622fc35
Todd Langford-Archer <toddpla@gmail.com>
2018-11-25 09:18:49 +0000

    refactored the likeable functionality into the ApplicationHelper
    this is now accessible to all controllers by inserting
    -----------------------------------------------
    > include ApplicationHelper::Likeable.new <Class>
    -----------------------------------------------
    
    ensure the Model references likes
    ----------------------------------------------
    > has_many :likes, as: :likeable
    ---------------------------------------------

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,4 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,8 @@ require 'pry'
 class PostsController < ApplicationController
   before_action :authenticate_user!
 
+  include ApplicationHelper::Likeable.new Post
+
   def create
     @post = Post.create(post_params)
     redirect_to posts_url
@@ -12,26 +14,6 @@ class PostsController < ApplicationController
   def index
     @post = Post.new
     @posts = Post.order(created_at: :desc)
-  end
-
-  def like
-    @likeable = Post.find(params[:id])
-
-    like = Like.find_by({likeable: @likeable, user: current_user})
-    if like
-      like.destroy
-      flash = "Like Removed!"
-    else
-      Like.create(likeable: Post.find(params[:id]), user: current_user)
-      flash = "Like Counted!"
-    end
-    respond_to do |format|
-      format.html do
-        flash[:notice] = flash
-        redirect_to posts_url
-      end
-      format.js { render :file => "/app/views/likes/like.js.erb" }
-    end
   end
 
   private
