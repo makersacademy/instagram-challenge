c2dba87c052d3d0a9adc8ed76d3f24805dacc86e
Chloe Verity <chloeverity@hotmail.co.uk>
2018-11-25 13:21:36 +0000

    creating Users table with devise

--- Gemfile
+++ Gemfile
@@ -26,6 +26,8 @@ gem 'haml'
 gem 'paperclip', '~> 4.2'
 gem 'simple_form'
 gem 'bootstrap-sass', '~> 3.3.5'
+gem 'devise'
+gem 'html2haml'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_24_132738) do
+ActiveRecord::Schema.define(version: 2018_11_25_131302) do
 
   create_table "posts", force: :cascade do |t|
     t.string "caption"
@@ -20,6 +20,20 @@ ActiveRecord::Schema.define(version: 2018_11_24_132738) do
     t.string "image_content_type"
     t.integer "image_file_size"
     t.datetime "image_updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "user_name"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["user_name"], name: "index_users_on_user_name", unique: true
   end
 
 end
