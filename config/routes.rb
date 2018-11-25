c197c2063d34a55d2b30db084f00b296087c2c46
muge <muge.hasilci@hotmail.com>
2018-11-25 18:50:44 +0000

    user profile page created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/show'
   root 'pages#home'
+  
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show]
 end
