9460cb623e1a1a74b1fdb271de03f15c06449265
Chloe Verity <chloeverity@hotmail.co.uk>
2018-11-25 13:49:32 +0000

    username associated with post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
+  validates :user_id, presence: true
   validates :image, presence: true
+  belongs_to :user
 
   has_attached_file :image, styles: { :medium => "640x" }
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :user_name, presence: true, length: { minimum: 4, maximum: 16 }
+  has_many :posts, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
