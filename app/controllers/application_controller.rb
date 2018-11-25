bede517e421996976d8ef84e7fc6ee3580a22abc
muge <muge.hasilci@hotmail.com>
2018-11-25 00:53:59 +0000

    authentication view, signin, signup updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,4 @@ class ApplicationController < ActionController::Base
    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
  end
- end
 end
