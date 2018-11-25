7d360b6dd12d26c3d3eb4ae48d307d971a7613d9
Caitlin Cooling <ccooling97@gmail.com>
2018-11-25 14:33:58 +0000

    Feature test for log in

---
+++ spec/test_helper.rb
@@ -0,0 +1,9 @@
+def sign_up
+  visit '/'
+  click_link 'Sign Up'
+  fill_in 'Name', with: 'Caitlin Cooling'
+  fill_in 'Email', with: 'caitlin@test.com'
+  fill_in 'Password', with: 'caitlin123'
+  fill_in 'user_password_confirmation', with: 'caitlin123'
+  click_button 'Sign up'
+end
