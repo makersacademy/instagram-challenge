fe0ad6ee259e3b7c124cfba4ef0c43883ad8c871
muge <muge.hasilci@hotmail.com>
2018-11-26 21:50:26 +0000

    sign up page fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_24_205950) do
+ActiveRecord::Schema.define(version: 2018_11_26_211949) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2018_11_24_205950) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "website"
+    t.text "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
