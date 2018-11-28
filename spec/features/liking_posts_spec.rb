5e2050cf39dc0d1b6b68852b0a9ce498e6bc5452
Chloe Verity <chloeverity@hotmail.co.uk>
2018-11-28 13:33:39 +0000

    skeleton for like feature tests

---
+++ spec/features/liking_posts_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+feature 'liking posts' do
+  background do
+
+  end
+  scenario 'user can like another users post' do
+
+  end
+  scenario 'user can unlike a post' do
+    
+  end
+end
