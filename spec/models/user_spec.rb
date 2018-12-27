0f70c63abf1722319aed5bc23a1444dc4f351dbe
Chris Gilbert <chrisgilbert4@icloud.com>
2018-12-27 17:04:53 +0000

    user model test coverage improved

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   subject { described_class.new }
 
-  xit "is valid with valid attributes" do
+  it "is valid with all required attributes" do
     subject.id = 1
     subject.first_name = "Chris"
     subject.last_name = "Gilbert"
@@ -12,10 +12,61 @@ RSpec.describe User, type: :model do
     subject.password = "password"
     subject.created_at = Time.now
     subject.updated_at = Time.now
-    expeect(subject).to be_valid
+    expect(subject).to be_valid
   end
 
-  # it "is not valid without a name, email and password" do
-  #   expect(subject).not_to be_valid
-  # end
+  it "is not valid without a first name" do
+    subject.id = 1
+    subject.last_name = "Gilbert"
+    subject.username = "instachris"
+    subject.email = "email@email.com"
+    subject.password = "password"
+    subject.created_at = Time.now
+    subject.updated_at = Time.now
+    expect(subject).not_to be_valid
+  end
+
+  it "is not valid without a last name" do
+    subject.id = 1
+    subject.first_name = "Chris"
+    subject.username = "instachris"
+    subject.email = "email@email.com"
+    subject.password = "password"
+    subject.created_at = Time.now
+    subject.updated_at = Time.now
+    expect(subject).not_to be_valid
+  end
+
+  it "is not valid without a username" do
+    subject.id = 1
+    subject.first_name = "Chris"
+    subject.last_name = "Gilbert"
+    subject.email = "email@email.com"
+    subject.password = "password"
+    subject.created_at = Time.now
+    subject.updated_at = Time.now
+    expect(subject).not_to be_valid
+  end
+
+  it "is not valid without email" do
+    subject.id = 1
+    subject.first_name = "Chris"
+    subject.last_name = "Gilbert"
+    subject.username = "instachris"
+    subject.password = "password"
+    subject.created_at = Time.now
+    subject.updated_at = Time.now
+    expect(subject).not_to be_valid
+  end
+
+  it "is not valid without password" do
+    subject.id = 1
+    subject.first_name = "Chris"
+    subject.last_name = "Gilbert"
+    subject.username = "instachris"
+    subject.email = "email@email.com"
+    subject.created_at = Time.now
+    subject.updated_at = Time.now
+    expect(subject).not_to be_valid
+  end
 end
