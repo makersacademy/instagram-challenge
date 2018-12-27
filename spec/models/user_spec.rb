f5078417b8f490eac5ecca41a5d4b95cf7db5738
Chris Gilbert <chrisgilbert4@icloud.com>
2018-12-27 14:15:22 +0000

    valid user model test written

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  subject { described_class.new }
+
+  it "is valid with valid attributes" do
+    subject.id = 1
+    subject.first_name = "Chris"
+    subject.last_name = "Gilbert"
+    subject.email = "email@email.com"
+    subject.username = "instachris"
+    subject.created_at = Time.now
+    subject.updated_at = Time.now
+    expeect(subject).to be_valid
+  end
+
+  it "is not valid without a name, email and password" do
+    expect(subject).not_to be_valid
+  end
+end
