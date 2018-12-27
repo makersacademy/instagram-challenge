0bee58f8b48cff4e27370989508132f89ce24a56
Chris Gilbert <chrisgilbert4@icloud.com>
2018-12-27 19:18:05 +0000

    final evening commit

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 2018_12_27_141117) do
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
-    t.string "last_name" 
+    t.string "last_name"
     t.string "username"
     t.string "email"
     t.string "password_hash"
