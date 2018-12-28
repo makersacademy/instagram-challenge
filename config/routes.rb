a0e80619366d856da755f5c07fd1a3dfed3c4272
Chris Gilbert <chrisgilbert4@icloud.com>
2018-12-28 10:16:31 +0000

    sessions controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/destroy'
   root 'posts#index'
   get 'posts' => "posts#index", :as => "posts"
   get "sign_up" => "users#new", :as => "sign_up"
---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe SessionsController, type: :controller do
+
+  describe "GET #new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #destroy" do
+    it "returns http success" do
+      get :destroy
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
