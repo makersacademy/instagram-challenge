666af07d14cdd3af95976436b912bb081cdb518e
Chris Gilbert <chrisgilbert4@icloud.com>
2018-12-29 10:50:19 +0000

    post photo implemented

--- spec/features/post_photo_spec.rb
+++ spec/features/post_photo_spec.rb
@@ -5,15 +5,16 @@ RSpec.feature "Post photo", type: :feature do
     user = FactoryBot.create(:user)
     sign_in(user)
     expect(current_path).to eq "/posts"
-    click_button "New Post"
+    click_link "New Post"
     expect(current_path).to eq "/posts/new"
     fill_in "Caption", with: "This is my photo caption"
-    attach_file("Upload Photo", Rails.root + "spec/fixtures/photo1.png")
+    attach_file("Image", Rails.root + "spec/fixtures/photo1.png")
     click_button "Post!"
     expect(current_path).to eq "/posts"
+    expect(page).to have_content "This is my photo caption"
   end
 
-  xscenario "User unable to post photo unless signed in" do
+  scenario "User unable to post photo unless signed in" do
     visit "/posts"
     expect(page).not_to have_content "New Post"
   end
