40d781497f8b13f4b5052ad3a325daa30b824535
Chris Gilbert <chrisgilbert4@icloud.com>
2018-12-29 13:03:50 +0000

    removed auto generated tests for views

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,69 +5,51 @@ RSpec.describe User, type: :model do
   let(:user) { FactoryBot.build(:user) }
 
   it "is valid with all required attributes" do
-    subject.id = 1
     subject.first_name = user.first_name
     subject.last_name = user.last_name
     subject.username = user.username
     subject.email = user.email
     subject.password = user.password
-    subject.created_at = Time.now
-    subject.updated_at = Time.now
     expect(subject).to be_valid
   end
 
   it "is not valid without a first name" do
-    subject.id = 1
     subject.last_name = user.last_name
     subject.username = user.username
     subject.email = user.email
     subject.password = user.password
-    subject.created_at = Time.now
-    subject.updated_at = Time.now
     expect(subject).not_to be_valid
   end
 
   it "is not valid without a last name" do
-    subject.id = 1
     subject.first_name = user.first_name
     subject.username = user.username
     subject.email = user.email
     subject.password = user.password
-    subject.created_at = Time.now
-    subject.updated_at = Time.now
     expect(subject).not_to be_valid
   end
 
   it "is not valid without a username" do
-    subject.id = 1
     subject.first_name = user.first_name
     subject.last_name = user.last_name
     subject.email = user.email
     subject.password = user.password
-    subject.created_at = Time.now
-    subject.updated_at = Time.now
     expect(subject).not_to be_valid
   end
 
   it "is not valid without email" do
-    subject.id = 1
     subject.first_name = user.first_name
     subject.last_name = user.last_name
     subject.username = user.username
     subject.password = user.password
-    subject.created_at = Time.now
-    subject.updated_at = Time.now
     expect(subject).not_to be_valid
   end
 
   it "is not valid without password" do
-    subject.id = 1
     subject.first_name = user.first_name
     subject.last_name = user.last_name
     subject.username = user.username
     subject.email = user.email
-    subject.created_at = Time.now
-    subject.updated_at = Time.now
     expect(subject).not_to be_valid
   end
 end
