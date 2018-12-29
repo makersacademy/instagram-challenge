5f247f7eb62fe0db194e24c5ee9dceddfa81d259
Chris Gilbert <chrisgilbert4@icloud.com>
2018-12-29 13:10:23 +0000

    post model tested barring image validation: this needs research

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
+require_relative '../support/files_test_helper.rb'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class.new }
+  let(:post) { FactoryBot.build(:post) }
+  let(:image) { FilesTestHelper.png }
+
+  it "is valid with all required attributes" do
+    subject.caption = post.caption
+    subject.image = image
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without a caption" do
+    subject.image = post.image
+    expect(subject).not_to be_valid
+  end
+
+  xit "is not valid without image" do
+    subject.caption = post.caption
+    expect(subject).not_to be_valid
+  end
 end
