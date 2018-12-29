4bc5224c03c492b105c7a44cef61190512033795
Chris Gilbert <chrisgilbert4@icloud.com>
2018-12-29 13:22:06 +0000

    jumobotron added to hompage

---
+++ spec/features/timeline_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.feature "Timeline", type: :feature do
+  scenario "Asks user to add photo if timeline is empty" do
+    visit "/"
+    expect(page).to have_content "There are currenly no photos! Sign In or Sign Up to add one!"
+  end
+end
