b075e989075bcb83cfcd4b4857a1fa5742f35770
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-02 12:13:23 +0000

    likes/users/posts belongs_to/has_many tests implemented

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -7,6 +7,16 @@ RSpec.describe Post, type: :model do
   let(:image) { FilesTestHelper.png }
   let(:user)  { FactoryBot.build(:user) }
 
+  it "has belongs to user" do
+    post = described_class.reflect_on_association(:user)
+    expect(post.macro).to eq :belongs_to
+  end
+
+  it "has many likes" do
+    post = described_class.reflect_on_association(:likes)
+    expect(post.macro).to eq :has_many
+  end
+
   it "is valid with all required attributes" do
     subject.caption = post.caption
     subject.image = image
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,16 @@ RSpec.describe User, type: :model do
   subject { described_class.new }
   let(:user) { FactoryBot.build(:user) }
 
+  it "has many posts" do
+    user = described_class.reflect_on_association(:posts)
+    expect(user.macro).to eq :has_many
+  end
+
+  it "has many likes" do
+    user = described_class.reflect_on_association(:likes)
+    expect(user.macro).to eq :has_many
+  end
+
   it "is valid with all required attributes" do
     subject.first_name = user.first_name
     subject.last_name = user.last_name
