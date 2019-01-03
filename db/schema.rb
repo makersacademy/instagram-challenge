1d1159851ef7455d9bbcb5434964314ab1ff4d77
Darryl Banks <darryl.banks@zombie9.com>
2019-01-03 17:06:27 +0000

    add model for Post and controller for Posts with views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_03_153112) do
+ActiveRecord::Schema.define(version: 2019_01_03_165850) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
---
+++ test/controllers/posts_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class PostsControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get posts_new_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get posts_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get posts_show_url
+    assert_response :success
+  end
+
+end
