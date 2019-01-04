1fb1372c3b4313124cec9a1affc6e501d072749c
Darryl Banks <darryl.banks@zombie9.com>
2019-01-04 10:37:31 +0000

    add association between posts and users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,10 +4,29 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def create
+    @post = Post.new(merge_params)
+    if(@post.save) then redirect_to @post else render 'new' end
+  end
+
   def index
   end
 
   def show
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:description, :user_id)
+  end
+
+  def merge_params
+    post_params.merge(user_id: current_user.id)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_03_165850) do
+ActiveRecord::Schema.define(version: 2019_01_04_101014) do
 
   create_table "posts", force: :cascade do |t|
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
