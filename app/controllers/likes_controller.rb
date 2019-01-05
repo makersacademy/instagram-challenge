d54851f20376e8d856e285123a42a8faf716db1b
Darryl Banks <darryl.banks@zombie9.com>
2019-01-05 16:26:37 +0000

    add exception when trying to like a post more than once

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,7 +3,11 @@ class LikesController < ApplicationController
   before_action :find_post
 
   def create
-    @post.likes.create(user_id: current_user.id)
+    if already_liked?
+      flash[:notice] = "You can't Like a post more than once"
+    else
+      @post.likes.create(user_id: current_user.id)
+    end
     redirect_to post_path(@post)
   end
 
@@ -11,6 +15,10 @@ class LikesController < ApplicationController
 
   def find_post
     @post = Post.find(params[:post_id])
+  end
+
+  def already_liked?
+    Like.where(user_id: current_user.id, post_id:params[:post_id]).exists?
   end
 
 end
