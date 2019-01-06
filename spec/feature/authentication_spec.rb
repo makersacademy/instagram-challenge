7044350ff93a8fbc72586cc8ce3d80db9e1465f3
leoncross <leon@lscross.co.uk>
2019-01-06 13:45:46 +0000

    refactored authentication test

--- spec/feature/authentication_spec.rb
+++ spec/feature/authentication_spec.rb
@@ -9,8 +9,4 @@ feature "Authentication" do
     click_button "Sign up"
     expect(page).to have_content("Welcome! You have signed up successfully.")
   end
-  scenario "Sign up and be greeted" do
-    sign_up_as_user_a
-    expect(page).to have_content("Welcome user_a")
-  end
 end
