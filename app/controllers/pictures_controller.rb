11362c1346952c8ce74f9b29f1a153331b616bf0
Carol Gardiner <carolan.gardiner@gmail.com>
2019-02-02 17:38:10 +0000

    added functionality for edit/update/destroy

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,7 +1,13 @@
 class PicturesController < ApplicationController
+  before_action :find_pic, only: [:show, :edit, :update, :destroy]
+  
   def index
+    @pictures = Picture.all.order("created_at DESC")
   end 
   
+  def show 
+  end
+
   #associated with the view file new.html.haml
   def new
     @picture = Picture.new
@@ -9,11 +15,40 @@ class PicturesController < ApplicationController
 
   #no view file - responsible for creating the component of a picture in the db
   def create
-    @picture = Picture.create(pic_params)
+    @picture = Picture.new(pic_params)
+
+    if @picture.save
+        redirect_to @picture, notice: "Yes pic posted!"
+    else
+      render 'new'
+    end
   end 
+
+  def edit 
+  end 
+
+#deals with changes to the db
+ def update 
+  if @picture.update(pic_params)
+    redirect_to @picture, notice: "Pic has been updated"
+  else
+    render 'edit'
+  end 
+ end 
+
+ def destroy 
+  @picture.destroy
+  redirect_to root_path
+ end
 
   private 
   def pic_params
     params.require(:picture).permit(:title, :description)
   end
+
+  #this allows us to not define show/edit
+  def find_pic
+    @picture = Picture.find(params[:id])
+  end
+
 end
