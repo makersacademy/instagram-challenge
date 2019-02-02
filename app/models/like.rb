0adaacf5c80136003c750f81259b9f4111c6a5b9
beatricecourage <bea_courage@hotmail.co.uk>
2019-02-02 20:51:04 +0000

    added likes

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,5 @@
 class Like < ApplicationRecord
+
+  belongs_to :post
+  
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
 
+  has_many :likes
 
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
