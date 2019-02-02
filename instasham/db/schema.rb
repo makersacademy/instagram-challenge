0cf0421313458dc016453101466ea48a71b82d51
James Hotblack <james.c.hotblack@gmail.com>
2019-02-02 19:26:41 +0000

    Added paperclip gem foruploading images

--- instasham/db/schema.rb
+++ instasham/db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_02_02_190630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "caption"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
