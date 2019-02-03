c54abcdbfcd64399ff95abd5961fbfb543754835
Jethro Whaley <jethro.whaley@gmail.com>
2019-02-03 17:56:59 +0000

    A user can only post if signed in

--- spec/features/create_posts_spec.rb
+++ spec/features/create_posts_spec.rb
@@ -1,14 +1,19 @@
 require 'rails_helper.rb'
 
 feature 'Creating posts' do
-  scenario 'can create a post' do
-    visit '/'
+  scenario 'a logged in user can create a post' do
+    sign_up_steps
     click_link 'New Post'
     attach_file('Image', "files/images/goldblumdog.jpg")
     fill_in 'Caption', with: 'Goldblum dog'
     click_button 'Create Post'
     expect(page).to have_content('Goldblum dog')
     expect(page).to have_css("img[src*='goldblumdog.jpg']")
+  end
+
+  scenario 'A user can only post if logged in' do
+    visit '/'
+    expect(page).not_to have_content('New Post')
   end
 
   scenario 'Adding image is required to create new post' do
