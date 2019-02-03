735d8938c7ae4e41b401d7e9996ad75e90072a15
James Hotblack <james.c.hotblack@gmail.com>
2019-02-03 19:11:06 +0000

    User can now edit and delete posts.

--- instasham/app/controllers/posts_controller.rb
+++ instasham/app/controllers/posts_controller.rb
@@ -9,7 +9,37 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.create(post_params)
+    if @post = Post.create(post_params)
+      flash[:success] = "Your post has been created!"
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "Your new post couldn't be created!  Please check the form."
+      render :new
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:success] = "Post updated."
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "Update failed.  Please check the form."
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
     redirect_to posts_path
   end
 
