2ac5aa63730bacd1bb32f546d64ceaf6e71dc481
Emanuele <emanuelegorga@gmail.com>
2019-02-03 22:44:29 +0000

    Passing test: User can post multiple image and see them on the index

--- spec/support/webhelper.rb
+++
@@ -1,9 +0,0 @@
-module WebHelper
-  def posting
-    visit '/'
-    click_link 'New Post'
-    attach_file('Image', "spec/images/racebike.jpg")
-    fill_in 'Caption', with: 'This is my race bike! #sport'
-    click_button 'Create Post'
-  end
-end
