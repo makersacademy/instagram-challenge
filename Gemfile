c1ff798345b9abc13ff61f680f90cfe8faee5952
Jethro Whaley <jethro.whaley@gmail.com>
2019-02-03 13:44:21 +0000

    first feature passing - post with image and caption can be added

--- Gemfile
+++ Gemfile
@@ -5,7 +5,9 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+gem 'simple_form'
 gem 'devise'
+gem "paperclip", "~> 6.0.0"
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.6', '>= 5.1.6.1'
 # Use postgresql as the database for Active Record
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root 'posts#index'
+
   get '/home', to: 'posts#home'
 
-  # resources :posts
+  resources :posts
   # root 'posts#home'
 end
-
-# root to: "user#index"
