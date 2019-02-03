016db4a84d4ca55bcb876903303913b64d46b5ee
celine kaslin <celine.kaslin@gmail.com>
2019-02-03 18:18:32 +0000

    added from for pic in new.htlm.erb + controller code changed

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -9,6 +9,12 @@ class PicsController < ApplicationController
 
   def create
     @pic = Pic.new(pic_params)
+
+    if @pic.save
+      redirect_to @pic, notice: "Yessss, it was posted!"
+    else
+      render "new"
+    end
   end
 
   private
--- spec/features/Pics_submit_spec.rb
+++ spec/features/Pics_submit_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 feature "Timeline", type: :feature do
   scenario "Can submit pic and view them" do
     visit "/pics"
-    click_link "New post"
-    fill_in "title", with: "Hello, world!"
-    fill_in "description", with: "Hello to the whole world!"
+    click_link "Post New Pic"
+    fill_in "Title", with: "Hello, world!"
+    fill_in "Description", with: "Hello to the whole world!"
     click_button "Submit"
     expect(page).to have_content("Hello, world!")
   end
