ea127dd8bf4a4bd5a08f04dc30b47dc6218c9fb9
Alina Goaga <goaga.alina@gmail.com>
2019-02-15 16:04:40 +0000

    Updated spec helper to help with the running of tests in Travis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -36,6 +36,7 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
+  ActiveRecord::Migration.maintain_test_schema!
   # This option will default to `:apply_to_host_groups` in RSpec 4 (and will
   # have no way to turn it off -- the option exists only for backwards
   # compatibility in RSpec 3). It causes shared context metadata to be
