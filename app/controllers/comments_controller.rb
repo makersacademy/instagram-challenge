45c93a43c0e58549fd411cdeac4b545b872e7c39
Chris Gilbert <chrisgilbert4@icloud.com>
2019-02-18 11:12:02 +0000

    refactred comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,14 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
+  before_action :find_post, only: [:create, :destroy]
   def create
-    @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params.merge(user_id: current_user.id))
     flash[:success] = "Comment added!"
     redirect_to post_path(@post)
   end
 
   def destroy
-    @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
     @comment.destroy
     flash[:success] = "Comment deleted!"
@@ -20,5 +19,9 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:body)
+  end
+
+  def find_post
+    @post = Post.find(params[:post_id])
   end
 end
