e919d6d560c3636768f2498db110d62c96b70fc5
Alina Goaga <goaga.alina@gmail.com>
2019-02-28 14:37:33 +0000

    Included Bootstrap, added navbar and updated index page styling

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,7 +29,6 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
-
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
@@ -50,7 +49,7 @@ Rails.application.configure do
   config.assets.debug = true
 
   # Suppress logger output for asset requests.
-  config.assets.quiet = true
+  config.assets.quiet = false
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
