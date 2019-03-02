3357dc4e6cb75189c9b76527b143957f33d317ff
Simon Green <simon.green@thielsen.co.uk>
2019-03-02 14:16:40 +0000

    :sparkles: adding posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   authenticated :user do
     root 'posts#index', as: :authenticated_root
