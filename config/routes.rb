1e5b4cbe1fcd05f46ea2bacbf446a68c2f2002a5
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-03-02 17:48:42 +0000

    :construction: feat: posts to be displayed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'posts', to: 'posts#index'
+
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
+
 end
