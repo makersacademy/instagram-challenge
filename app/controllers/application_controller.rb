744bc1439562e23b163e85b64f76eee11c790900
Colin Hulme <17675453+ch359@users.noreply.github.com>
2019-03-02 19:41:46 +0000

    Closes #20 Closes #8
    Signed-off-by: Colin Hulme <17675453+ch359@users.noreply.github.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  include Devise::Test::ControllerHelpers
   before_action :authenticate_user!
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create([
+                {
+                    first_name: 'Bob',
+                    last_name: 'Tables',
+                    username: 'bob',
+                    password: 'bbbbbb',
+                    email: 'bob@bob.com',
+                    profile_pic: nil
+                }])
