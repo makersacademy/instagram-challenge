821e87bb769eda0b4f83e6d8fa955b03432bc9af
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-03-02 19:52:29 +0000

    :sparkles: feat: user can now see individual posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,29 @@
 class PostsController < ApplicationController
   def index
-
+    @posts = Post.all
   end
 
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path, notice: "You created a post!"
+    else
+      render :new, alert: "Your post wasn't created"
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'posts', to: 'posts#index'
   get 'posts/new', to: 'posts#new'
-
+  post 'posts', to: 'posts#create'
+  get 'posts/:id', to: 'posts#show', as: :post
+  
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
