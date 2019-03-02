0c7536efd15d3af3d0df0dfde6cfd381c69da7a9
debora <debora.piu92@gmail.com>
2019-03-02 20:12:17 +0000

    user can choose a username (unique) at sign up

--- db/migrate/20190302155034_devise_create_users.rb
+++ db/migrate/20190302155034_devise_create_users.rb
@@ -5,6 +5,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
+      t.string :user_name,          null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
@@ -37,6 +38,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
     end
 
     add_index :users, :email,                unique: true
+    add_index :users, :user_name,            unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2019_03_02_165209) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
+    t.string "user_name", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -32,6 +33,7 @@ ActiveRecord::Schema.define(version: 2019_03_02_165209) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["user_name"], name: "index_users_on_user_name", unique: true
   end
 
   add_foreign_key "posts", "users"
