88a761613a5d61270b61fc2d5c2b11417a97a9fb
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-03-02 22:12:02 +0000

    :white_check_mark: test: user should be able to like the post

--- spec/features/user_features_spec.rb
+++ spec/features/user_features_spec.rb
@@ -20,13 +20,26 @@ RSpec.feature "Show individual posts", type: :feature do
     expect(page).to have_content("Test!")
   end
 end
+
 # New failing feature test:
 # RSpec.feature "Comment", type: :feature do
 #   scenario "Can comment posts" do
 #     visit "/posts"
 #     click_link "Comment"
-#     fill_in "post[commit]", with: "Test!"
-#     clck_button "comment"
+#     fill_in "post[comment]", with: "Test!"
+#     clck_button "Update Post"
 #     expect(page).to have_content("Test!")
 #   end
 # end
+
+# New failing feature test:
+RSpec.feature "Like the post", type: :feature do
+  scenario "Can see individual posts" do
+    visit "/posts"
+    click_link "New Post"
+    fill_in "post[body]", with: "Test!"
+    click_button "Create Post"
+    click_link "Like"
+    expect(page).to have_content("Like 1")
+  end
+end
