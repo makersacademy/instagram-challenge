55413b425b91a55bb3f50df49dbf3bab98922e2e
RubyKobayashi <ruby@rubykobayashi.com>
2019-03-03 11:49:52 +0000

    add devise user model

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+    get 'users/sign_in'
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "posts#index"
+  root to: "users#sign_in"
 end
