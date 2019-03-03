f16197a912318df3ac995269cd1cee359ebdb042
Nikhil Vijayan <nkhilv@gmail.com>
2019-03-03 22:00:52 +0000

    :sparkles: add image upload feature

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -50,6 +50,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :description)
+    params.require(:post).permit(:title, :description, :image)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   validates :user_id, presence: true
   belongs_to :user
   validates :title, presence: true, length: { minimum: 5 }
+  has_one_attached :image
 end
