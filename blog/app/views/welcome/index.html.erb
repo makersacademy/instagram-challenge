69e91f324dcc618c9ef121a7d377512d7bc0a56f
RubyKobayashi <ruby@rubykobayashi.com>
2019-03-03 12:08:04 +0000

    functioning landing page

---
+++ blog/app/views/welcome/index.html.erb
@@ -0,0 +1,2 @@
+
+<h1>Hello, Rails!</h1>
--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-    get 'users/sign_in'
+  get 'welcome/index'
+  root 'welcome#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "users#sign_in"
