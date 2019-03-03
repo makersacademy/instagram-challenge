cca2c45ebf364517344232937ad056de505e5eee
david-mears <kieukmail@gmail.com>
2019-03-03 16:52:01 +0000

    validates usernames unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, uniqueness: true
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,12 @@ RSpec.describe User, type: :model do
   it { should have_db_column(:first_name) }
   it { should have_db_column(:last_name) }
   it { should have_db_column(:profile_pic) }
+
+  subject { User.create(
+    email: 'f@f.com',
+    password: 'fffffff',
+    username: 'f',
+    first_name: 'F',
+    last_name: 'Fson') }
+  it { should validate_uniqueness_of(:username) }
 end
