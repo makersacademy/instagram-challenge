9b976bf50a9365ed2eb28b20cb9719dbc2e283a7
Nikhil Vijayan <nkhilv@gmail.com>
2019-03-03 20:29:19 +0000

    :sparkles: add update post ability

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -33,6 +33,16 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to(post_path(@post))
+  end
+
   private
 
   def post_params
