013c8193fa0f70a0f0fa9db9dbc546b588950054
Amy Jordan <amy_jordan@live.co.uk>
2019-04-27 15:25:42 +0100

    User sees success message on sign out

--- spec/features/sign_out_spec.rb
+++ spec/features/sign_out_spec.rb
@@ -6,4 +6,10 @@ RSpec.feature "Sign in", type: :feature do
     create_user_and_sign_up
     expect(page).to have_button('Sign out')
   end
+
+  scenario "User sees success message when signed out" do
+    create_user_and_sign_up
+    click_button('Sign out')
+    expect(page).to have_content('See you soon!')
+  end
 end
--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -8,7 +8,7 @@ RSpec.feature 'Sign up', type: :feature do
     fill_in("user_password", with: "password")
     fill_in("user_password_confirmation", with: "password")
     click_button("Sign up")
-    expect(page).to have_content('Welcome! You have signed up successfully.')
+    expect(page).to have_content('Welcome to InstaCat! You have signed up successfully.')
   end
 
   scenario 'User redirected to index if not signed up' do
