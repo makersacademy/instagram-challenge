97af47f120d6552aa96da5830e4c1dab85f377fd
Amy Jordan <amy_jordan@live.co.uk>
2019-04-27 15:35:15 +0100

    User prompted to enter their email and password to login

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -6,4 +6,11 @@ RSpec.feature "Sign in", type: :feature do
     visit "/"
     expect(page).to have_link("Sign in")
   end
+
+  scenario "User prompted to enter their email address and password to login" do
+    visit "/"
+    click_link("Sign in")
+    expect(page).to have_field("user_email")
+    expect(page).to have_field("user_password")
+  end
 end
