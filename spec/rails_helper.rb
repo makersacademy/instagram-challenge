e08b3140fe20bb3826ccb9498ba7fd9cb8ddf466
Andrew McLelland <andrew.mclelland23@gmail.com>
2019-04-27 18:31:25 +0100

    Trying to fix coverage display on readme in git repo

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,9 +8,9 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-  SimpleCov::Formatter::Console,
-  ])
+# SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+#   SimpleCov::Formatter::Console,
+#   ])
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
