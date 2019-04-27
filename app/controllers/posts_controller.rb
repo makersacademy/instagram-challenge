555d54092850f11582cc1513c916242dcbe9094e
Amy Jordan <amy_jordan@live.co.uk>
2019-04-27 16:19:47 +0100

    DB migration to associate users with posts Updates ApplicationController to push username through to db

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.create(post_params)
+    @post = Post.create(post_params.merge(user_id: current_user.id))
 
     redirect_to posts_url
   end
