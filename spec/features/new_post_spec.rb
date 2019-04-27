166e0d5f04c0b81e10407676054d6a9e6964ec6b
Amy Jordan <amy_jordan@live.co.uk>
2019-04-27 16:24:05 +0100

    Displays username associated with post

--- spec/features/new_post_spec.rb
+++ spec/features/new_post_spec.rb
@@ -18,4 +18,13 @@ RSpec.feature "Post image", type: :feature do
     expect(page).to have_content('Cute cat')
     expect(page).to have_css("img[src*='stanley.jpg']")
   end
+
+  scenario 'Username displays with posted image' do
+    create_user_and_sign_up
+    click_link 'New post'
+    fill_in 'post_caption', with: 'Cute cat'
+    page.attach_file('post_image', Rails.root + "spec/fixtures/stanley.jpg")
+    click_button 'Create post'
+    expect(page).to have_content("@testuser")
+  end
 end
