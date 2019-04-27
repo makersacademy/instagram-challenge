0928ed17a09b5c513447737915431177480bbec6
Amy Jordan <amy_jordan@live.co.uk>
2019-04-27 21:19:16 +0100

    Username of commenter is displayed Feels a quite dirty in the implementation (calling the model in the view - would appreciate advice!)

--- spec/features/comment_posts_spec.rb
+++ spec/features/comment_posts_spec.rb
@@ -12,4 +12,17 @@ RSpec.feature "Comment", type: :feature do
     click_button 'Add'
     expect(page).to have_content('Your cat is super cute')
   end
+
+  scenario "Username of commenter is displayed" do
+    create_user_and_sign_up
+    click_link 'New post'
+    fill_in 'post_caption', with: 'Cute cat'
+    page.attach_file('post_image', Rails.root + "spec/fixtures/stanley.jpg")
+    click_button 'Create post'
+    click_button('Sign out')
+    create_second_user_and_sign_up
+    fill_in 'comment_body', with: 'Your cat is super cute'
+    click_button 'Add'
+    expect(page).to have_content('secondtestuser')
+  end
 end
