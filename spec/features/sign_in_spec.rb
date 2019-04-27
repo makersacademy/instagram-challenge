bd9712e0ff640d6f090b2ea4b4dd4d5412edd04c
Amy Jordan <amy_jordan@live.co.uk>
2019-04-27 15:37:54 +0100

    User sees success message on sign in

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -7,10 +7,21 @@ RSpec.feature "Sign in", type: :feature do
     expect(page).to have_link("Sign in")
   end
 
-  scenario "User prompted to enter their email address and password to login" do
+  scenario "User prompted to enter their email address and password to sign in" do
     visit "/"
     click_link("Sign in")
     expect(page).to have_field("user_email")
     expect(page).to have_field("user_password")
+  end
+
+  scenario "User sees success message on sign in" do
+    create_user_and_sign_up
+    click_button("Sign out")
+    visit "/"
+    click_link("Sign in")
+    fill_in("user_email", with: "test@com")
+    fill_in("user_password", with: "password")
+    click_button("Sign in")
+    expect(page).to have_content("Welcome back to InstaCat!")
   end
 end
