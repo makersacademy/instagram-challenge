c0db72a2d3a27a3dbaa331bd793060caa3fc4046
Amy Jordan <amy_jordan@live.co.uk>
2019-04-27 15:12:35 +0100

    User redirected to posts page on sign up

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -29,4 +29,14 @@ RSpec.feature 'Sign up', type: :feature do
     click_button("Sign up")
     expect(page).to have_content('Email is invalid')
   end
+
+  scenario 'User redirected to posts page on sign up' do
+    visit '/'
+    fill_in("user_username", with: "testuser")
+    fill_in("user_email", with: "test@com")
+    fill_in("user_password", with: "password")
+    fill_in("user_password_confirmation", with: "password")
+    click_button("Sign up")
+    expect(page).to have_link('New post')
+  end
 end
