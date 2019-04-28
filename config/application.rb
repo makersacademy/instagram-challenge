ce7f0477eb4ed1cf7c3aaf356305756dc2061361
Cosmin <mirceacosmin@gmail.com>
2019-04-28 12:18:27 +0100

    Harmony mode enabled with uglifier

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module Instagram
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.serve_static_assets = true
+    config.assets.initialize_on_precompile = false
     # config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
