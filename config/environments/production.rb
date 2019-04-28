2f65b6ad1c7e0c00aab9da7009615eb44e9893cf
saypop <tome.m.dj@gmail.com>
2019-04-28 02:36:50 +0100

    Devise installation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,9 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "instagram-challenge_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  # Set default url for devise action_mailer !!!Might need to change later, see heroku instructions!!!
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
