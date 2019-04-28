9e6b7b3de0fd5a359850d96592c8926acd342c52
Cosmin <mirceacosmin@gmail.com>
2019-04-28 21:23:52 +0100

    Improved picture adding menu

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -28,7 +28,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.html { redirect_to posts_path, notice: 'Comment was successfully created.' }
         format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new }
@@ -56,7 +56,7 @@ class CommentsController < ApplicationController
   def destroy
     @comment.destroy
     respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
+      format.html { redirect_to posts_path, notice: 'Comment was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
