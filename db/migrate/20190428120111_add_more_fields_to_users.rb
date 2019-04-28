7499972039ad1a91e8aa9eb1bb2066bd24f6add2
saypop <tome.m.dj@gmail.com>
2019-04-28 13:07:03 +0100

    Update user table with columns for: - username - name - bio

---
+++ db/migrate/20190428120111_add_more_fields_to_users.rb
@@ -0,0 +1,7 @@
+class AddMoreFieldsToUsers < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :username, :string
+    add_column :users, :name, :string
+    add_column :users, :bio, :text
+  end
+end
