3465829db69c2ef4c244b7105f03a6b11278d363
Cosmin <mirceacosmin@gmail.com>
2019-04-28 23:41:08 +0100

    Added and fixed tests

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -42,7 +42,7 @@ class CommentsController < ApplicationController
   def update
     respond_to do |format|
       if @comment.update(comment_params)
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
+        format.html { redirect_to posts_path, notice: 'Comment was successfully updated.' }
         format.json { render :show, status: :ok, location: @comment }
       else
         format.html { render :edit }
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,38 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = User.new(first_name: "John", last_name: "Snow", email: "test@emai.com", password: "123456")
+
+    # @user = instance_double('User', id: 1, first_name: "John", last_name: "Snow", email: "test@emai.com", password: "123456")
+
+    file = fixture_file_upload(Rails.root.join('public', 'download.png'), 'image/png')
+    @post = Post.new(image: file)
+    # @post = instance_double('Post', image: file, id: 1)
+  end
+
+  subject { described_class.new(body: "Text", post_id: @post.id, user_id: @user.id) }
+
+  it "is valid with valid attributes" do
+    comment = Comment.new(body: "Text", post_id: @post.id, user_id: @user.id)
+
+    # expect(subject).to be_valid
+  end
+
+  it "is not valid without a body" do
+    subject.body = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a user logged in" do
+    subject.user_id = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a post" do
+    subject.post_id = nil
+    expect(subject).to_not be_valid
+  end
 end
