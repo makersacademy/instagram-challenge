fd6b29c445471500a0784c9d8ddf49e5f13898c4
sarpong <abasimisarpong@rocketmail.com>
2019-05-25 20:40:17 +0100

    Added validation errors to sign up page

--- instagram/app/models/user.rb
+++ instagram/app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :email, presence: true
+  validates :name, presence: true
+  validates :username, presence: true
+  validates :password, presence: true, length: { minimum: 4 , maximum: 10 }
 end
