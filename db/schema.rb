5bbb88a29fd79ec2097a645af6c3c3753651d542
Jordan Roberts <jordanroberts@talk21.com>
2019-05-26 12:36:48 +0100

    Added paperclip gem for images

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_05_26_113333) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "caption"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
+  end
 
 end
