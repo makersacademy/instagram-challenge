7bbb22b274bffc565eaa3d49eff93bd304b21f1d
Jordan Roberts <jordanroberts@talk21.com>
2019-05-26 22:57:47 +0100

    Removed broken likes - needs to be fixed

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,4 @@
 class LikesController < ApplicationController
 
-before_action :find_post
 
- def create
-   @post.likes.create(user_id: current_user.id)
-   redirect_to post_path
- end
-
- private
- def find_post
-   @post = Post.find(params[:post_id])
- end
 end
