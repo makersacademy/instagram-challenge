3ef19dddb8f6b6c77334853bc0a9cbd527307b33
Jordan Roberts <jordanroberts@talk21.com>
2019-05-26 16:38:06 +0100

    Changed from sql to postgres' '

--- spec/features/user_feature_spec.rb
+++ spec/features/user_feature_spec.rb
@@ -1,8 +1,8 @@
 
 require 'rails_helper'
 
-feature 'shows user' do
-  scenario 'user signs up and can be accessed by id' do
+feature 'Users can sign up' do
+  scenario 'user signs up for new account' do
     visit '/users/sign_up'
     fill_in 'user_user_name', with: 'Example Name'
     fill_in 'user_email', with: 'example@example.com'
@@ -12,4 +12,14 @@ feature 'shows user' do
 
     expect(page).to have_content("Foodstagram")
   end
+
+  scenario 'User redirected to posts page on sign up' do
+    visit '/users/sign_up'
+    fill_in 'user_user_name', with: 'Example Name'
+    fill_in 'user_email', with: 'example@example.com'
+    fill_in 'user_password', with: 'examplepassword'
+    fill_in 'user_password_confirmation', with: 'examplepassword'
+    click_button 'Sign up'
+    expect(page).to have_content("New Post")
+end
 end
