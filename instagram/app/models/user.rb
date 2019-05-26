8554ed43727953e8769a1cb6fc4be91af3449a90
sarpong <abasimisarpong@rocketmail.com>
2019-05-26 12:06:25 +0100

    signup page styling done

--- instagram/app/models/user.rb
+++ instagram/app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true
   validates :username, presence: true
-  validates :password, presence: true, length: { minimum: 4 , maximum: 10 }
+  validates :password, presence: true, allow_blank: true, length: { minimum: 4 , maximum: 10 }
 end
