ed2ac45886ab2a43dec19bba6de41899e21fed8b
sarpong <abasimisarpong@rocketmail.com>
2019-05-27 18:29:02 +0100

    user show page almost done

--- instagram/app/controllers/posts_controller.rb
+++ instagram/app/controllers/posts_controller.rb
@@ -24,6 +24,6 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:title, :content)
+    params.require(:post).permit(:title, :content, :avatar)
   end
 end
