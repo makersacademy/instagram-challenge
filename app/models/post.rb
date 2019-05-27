277c5833bc4b8b2b3aaf1eeff85bca335e4d4e4a
Jordan Roberts <jordanroberts@talk21.com>
2019-05-27 11:36:51 +0100

    Tidying up code

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,12 @@
 class Post < ApplicationRecord
-
-
-  validates :user_id, presence: true
-
-  validates :image, presence: true
-
-  has_attached_file :image, styles: { :medium => "640x" }
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   belongs_to :user
   has_many :comments, dependent: :destroy
-  has_many :likes 
+  has_many :likes
 
+  validates :user_id, presence: true
+  validates :image, presence: true
+  has_attached_file :image, styles: { :medium => "640x" }
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
 end
 
