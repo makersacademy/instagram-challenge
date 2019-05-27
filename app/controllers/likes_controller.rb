723bb5513216d7b337783db2043147d1bdcc3bc2
Jordan Roberts <jordanroberts@talk21.com>
2019-05-27 10:32:46 +0100

    Users can only like posts once

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,7 +3,11 @@ class LikesController < ApplicationController
   before_action :find_post
 
   def create
-    @post.likes.create(user_id: current_user.id)
+    if already_liked?
+      flash[:notice] = "You've already liked this picture!"
+    else
+      @post.likes.create(user_id: current_user.id)
+    end
     redirect_to posts_path
   end
 
@@ -11,6 +15,10 @@ class LikesController < ApplicationController
 
   def find_post
     @post = Post.find(params[:post_id])
+  end
+
+  def already_liked?
+    Like.where(user_id: current_user.id, post_id: params[:post_id]).exists?
   end
 
 end
