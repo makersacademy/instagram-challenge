4625838655315fbd0d1b39a02d35bf731799429d
Jordan Roberts <jordanroberts@talk21.com>
2019-05-29 21:49:40 +0100

    Users can edit comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,12 +6,24 @@ class CommentsController < ApplicationController
     redirect_to posts_path
   end
 
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+  end
+
   def destroy
     @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
     @comment.destroy
     flash[:success] = "Comment deleted"
     redirect_to posts_path
+  end
+
+  def update
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.update(comment_params)
+    redirect_to post_path(@post)
   end
 
   private
