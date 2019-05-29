535e1ac0dfa5c9b0a52bb926f9aa5d671d24719e
Jordan Roberts <jordanroberts@talk21.com>
2019-05-29 09:11:51 +0100

    Tests passing for unlike feature

--- spec/features/likes_feature_spec.rb
+++ spec/features/likes_feature_spec.rb
@@ -22,8 +22,33 @@ feature 'Users can like a post' do
     fill_in 'Caption', with: 'Caption'
     click_button('Create Post')
     click_button('Like')
-    click_button('Like')
 
-    expect(page).to have_content("1 Like")
+    expect(page).not_to have_button("Like")
   end
+
+  scenario 'user can unlike a post' do
+    create_new_user
+    visit '/posts'
+    visit new_post_path
+    page.attach_file("post_image", Rails.root + 'spec/fixtures/Cookies.jpg')
+    fill_in 'Caption', with: 'Caption'
+    click_button('Create Post')
+    click_button('Like')
+    click_button('Unlike')
+
+    expect(page).to have_content("Like")
+  end
+
+
+    scenario 'user can only unlike a post that they have liked' do
+      create_new_user
+      visit '/posts'
+      visit new_post_path
+      page.attach_file("post_image", Rails.root + 'spec/fixtures/Cookies.jpg')
+      fill_in 'Caption', with: 'Caption'
+      click_button('Create Post')
+      click_button('Like')
+
+      expect(page).to have_button("Unlike")
+    end
 end
