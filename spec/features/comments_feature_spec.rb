706272fb5706fa90975152a225f0d2f2e38fa3ae
Jordan Roberts <jordanroberts@talk21.com>
2019-05-29 21:04:31 +0100

    Successful delete comment test passes

--- spec/features/comments_feature_spec.rb
+++ spec/features/comments_feature_spec.rb
@@ -14,7 +14,7 @@ feature 'Users can comment on a post' do
     expect(page).to have_content("example comment")
   end
 
-  scenario 'user deletes comment on a post' do
+  scenario 'user can only delete their own comments' do
     create_new_user
     visit '/posts'
     visit new_post_path
@@ -25,4 +25,17 @@ feature 'Users can comment on a post' do
     click_button "Add"
     expect(page).to have_button("Delete comment")
   end
+
+    scenario 'comments are successfully deleted' do
+      create_new_user
+      visit '/posts'
+      visit new_post_path
+      page.attach_file("post_image", Rails.root + 'spec/fixtures/Cookies.jpg')
+      fill_in 'Caption', with: 'Caption'
+      click_button('Create Post')
+      fill_in 'comment_content', with: "new comment"
+      click_button "Add"
+      click_button "Delete comment"
+      expect(page).not_to have_content("new comment")
+    end
 end
