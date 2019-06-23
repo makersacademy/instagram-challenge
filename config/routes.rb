e21cf9d0115208b085f43c71c184c88cc092805a
Laurence <ltaylorapp@gmail.com>
2019-06-23 16:13:31 +0100

    Added signing up functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :widgets
-  resources :users
+  root 'users#index'
+  resources :users, :widgets
 end
--- spec/features/signing_up_spec.rb
+++ spec/features/signing_up_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.feature 'Signing up', type: :feature do
   scenario 'Signing up with suitable credentials' do
     visit '/users/new'
-    fill_in 'name', with: 'test'
-    fill_in 'email', with: 'test@test.com'
-    fill_in 'password', with: '123'
-    fill_in 'password_confirmation', with: '123'
+    fill_in 'user_name', with: 'test'
+    fill_in 'user_email', with: 'test@test.com'
+    fill_in 'user_password', with: '123'
+    fill_in 'user_password_confirmation', with: '123'
     click_button 'Sign Up'
     expect(current_path).not_to eq('/users/new')
   end
