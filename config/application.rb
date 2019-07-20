307a94506ab9d6f2745532486bd07c0073354bc5
petraartep <petra.pogacsas@gmail.com>
2019-07-20 22:17:43 +0100

    setup still

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Instagram
   class Application < Rails::Application
+    config.time_zone = 'Europe/London'
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
