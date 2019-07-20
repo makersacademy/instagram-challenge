b048f2e3e5f030be986a93fe6f57e47523f09024
Leon Linton <leon.linton@live.co.uk>
2019-07-20 18:36:16 +0100

    Columns added to User table

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,12 +1,5 @@
 require 'rails_helper'
 
-# RSpec.describe RoutesController, type: :controller do
+RSpec.describe RoutesController, type: :controller do
 
-# end
-
-RSpec.describe "/" do
-    it "responds with 200" do
-        get :index
-        expect(response).to have_http_status(200)
-    end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,8 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'rspec/rails'
+# note: require 'devise' after require 'rspec/rails'
+require 'devise'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
@@ -31,6 +34,9 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+
+  # For Devise > 4.1.1
+  config.include Devise::Test::ControllerHelpers, :type => :controller
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
