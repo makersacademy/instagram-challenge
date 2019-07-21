f2e6527e6673f40caf42901076437ba684ff6b48
petraartep <petra.pogacsas@gmail.com>
2019-07-21 22:41:48 +0100

    add photos

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,5 @@
 class HomeController < ApplicationController
+  def index
+    @posts = Post.order(created_at: :desc)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'home/index'
-
   root to: 'home#index'
 
+  devise_for :users
   resources :users, only: [:show, :edit, :update]
-  
+  resources :posts, only: [:new, :create]
 end
