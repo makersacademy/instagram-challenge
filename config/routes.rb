ef6194fe2e80cf89462fb40b60549ec909ab4a5e
Anna Sobolewska <sobolewska.a.m@gmail.com>
2019-07-21 14:59:59 +0100

    Added Posts and Photos stubs

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'photo/index'
+  get 'photo/store'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
