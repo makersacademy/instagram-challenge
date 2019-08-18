621ec7c8e01ca5b7b17478aefc94f4a1ba2e51b6
Will Howes <williameldenhowes@gmail.com>
2019-08-18 20:48:01 +0100

    Added times to posts and comments

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,5 @@
 class PhotosController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!
 
   def index
     @photos = Photo.all
