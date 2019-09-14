d3b340952e8551625eade5c06dd3c8400c5524c5
kierepkae <kierepkaedyta@gmail.com>
2019-09-14 18:37:06 +0100

    user can delete a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,12 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+  def destroy
+    @post = current_user.posts.find(params[:id])
+    @post.destroy
+    redirect_to user_path(current_user)
+  end
+
   private
   def post_params
     params.require(:post).permit(:description, :image, :user_id)
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'home/index'
   root to: 'home#index'
   resources :users, only: [:show, :edit, :update]
-  resources :posts, only: [:new, :create, :show]
+  resources :posts, only: [:new, :create, :show, :destroy]
+
 end
