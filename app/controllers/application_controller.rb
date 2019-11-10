821a8f5ced0f8defa4da724329c26ec96a18a8e3
Sid Mason-Malik <49129863+smasonmalik@users.noreply.github.com>
2019-11-10 10:27:30 +0000

    fix: additional user sign up params stored

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
 
 
   protected
@@ -11,4 +11,10 @@ class ApplicationController < ActionController::Base
  def after_sign_out_path_for(_resource)
    "/users/sign_in"
  end
+
+ def configure_permitted_parameters
+   devise_parameter_sanitizer.permit(:sign_up, keys: [:forename, :surname])
+   #in keys list all the additional user input params you want to accept. 
+ end
+
 end
