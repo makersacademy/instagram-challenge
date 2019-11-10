1fba273a68b4b15939657ae7592665cfdcc48d84
kevindhanna <kevindhanna@gmail.com>
2019-11-10 20:51:00 +0000

    add test for authenticated posts#show

--- spec/controllers/api/v1/posts_controller_spec.rb
+++ spec/controllers/api/v1/posts_controller_spec.rb
@@ -1,14 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::PostsController, type: :controller do
-  # before :each do
-  #   key = AuthenticateUserCommand.call(*params.slice(:email, :password).values)
-  # end
+  before :each do
+    User.create(email: 'test@rspec.com', password: 'password')
+    @key = AuthenticateUserCommand.call('test@rspec.com', 'password').result
+  end
 
   describe 'GET /' do
     it 'returns 401 if no token is provided' do
       get :show
       expect(response).to have_http_status(401) 
+    end
+    it 'returns 200 when authenticated' do
+      get :show, params: {token: @key}
     end
   end
 end
