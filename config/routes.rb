88e0c616cb2b90eedec554ffc34ffa1ba8761855
Amy Balmforth <amy.balmforth@gmail.com>
2019-12-07 18:15:15 +0000

    add user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: [:show]
   get 'home/index'
 
   root to: 'home#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
