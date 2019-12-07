9f2cb1d55fc4c34cfadc01485a85c1887ca37f66
Amy Balmforth <amy.balmforth@gmail.com>
2019-12-07 20:13:29 +0000

    can edit user info

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit, :update]
   get 'home/index'
 
   root to: 'home#index'
