efd1be93f1bbd3154a24e155438775c59d1185c4
Robert Woolley <rob@robertwoolley.co.uk>
2019-12-08 20:54:17 +0000

    Update to include work from Catagram repo

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get 'home/index'
+  root 'posts#index'
+  resources :posts
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
