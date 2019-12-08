aaed7e74a82a19d1138465a695bdd74feda8a090
Amy Balmforth <amy.balmforth@gmail.com>
2019-12-08 15:15:17 +0000

    can delete posts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  protected
-
-  def authenticate_user!
-    if user_signed_in?
-      super
-    else
-      redirect_to home_index_url
-    end
-  end
-
-  
-
-
-
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
-    @posts = Post.order(created_at: :desc).page(params[:page]).per(2)
+    if current_user
+      @posts = Post.order(created_at: :desc).page(params[:page]).per(2)
+    else
+      redirect_to new_user_session_path
+    end
   end
 end
