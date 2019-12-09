815f784fa7373d47276f580a1e243cb774c00bb1
Natalie <49326857+natyeo@users.noreply.github.com>
2019-12-09 01:03:17 +0000

    photo resized on feed and shows username

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,9 +8,9 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-require 'jquery3'
-require 'popper'
-require 'bootstrap-sprockets'
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
