87bc2f7c4e0344973bcfc3cc795afeecd729719f
Student <student@Makerss-MacBook-Pro-8.local>
2019-12-09 09:24:14 +0000

    refactored and found issue with image/signout - needs styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable
 
   has_many :posts, :foreign_key => 'user_id', :dependent => :destroy
 end
