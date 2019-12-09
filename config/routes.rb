bf014b710ee3c7937366e8d37e8f6486380be51b
Natalie <49326857+natyeo@users.noreply.github.com>
2019-12-09 14:26:15 +0000

    added comments model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
