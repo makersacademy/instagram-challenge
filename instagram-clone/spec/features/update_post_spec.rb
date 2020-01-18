9a3a407112ff3cc8c359af61431e6fd3ca7d5ba5
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-01-18 22:50:34 +0000

    chore: remove edit button for all foreign posts

--- instagram-clone/spec/features/update_post_spec.rb
+++ instagram-clone/spec/features/update_post_spec.rb
@@ -1,6 +1,9 @@
 RSpec.describe 'updating the description of a post', type: :feature do
-  scenario 'a user wants to update their avocado post' do
+  before do
     sign_up_sam
+  end
+
+  scenario 'a user wants to update their avocado post' do
     post_picture_of_breakfast
     click_on 'edit description'
 
@@ -14,7 +17,6 @@ RSpec.describe 'updating the description of a post', type: :feature do
   end
 
   scenario 'a user wants to update their dog post' do
-    sign_up_sam
     post_picture_of_dog
     click_on 'edit description'
 
@@ -25,5 +27,14 @@ RSpec.describe 'updating the description of a post', type: :feature do
 
     expect(page).to have_css "img[src*='some-dog.jpg']"
     expect(page).to have_content '#dogs #bestboi #likeforlike #woof'
+  end
+
+  it 'is not possible if the user is not the owner of the post' do
+    post_picture_of_breakfast
+    click_on 'Log out'
+
+    sign_up_elodie
+    click_on 'Home'
+    expect(page).not_to have_content 'edit description'
   end
 end
