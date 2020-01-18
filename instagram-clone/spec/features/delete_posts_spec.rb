7f855404b68fb6a3d11ca59e1071fb875886da1f
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-01-18 22:55:47 +0000

    chore: remove delete button from all foreign posts

--- instagram-clone/spec/features/delete_posts_spec.rb
+++ instagram-clone/spec/features/delete_posts_spec.rb
@@ -1,7 +1,10 @@
 RSpec.describe 'deleting a post', type: :feature do
-  scenario 'a user wants to delete a post' do
+  before do
     sign_up_sam
     post_picture_of_breakfast
+  end
+
+  scenario 'a user wants to delete a post' do
     post_picture_of_dog
     all('a', text: 'delete')[0].click
 
@@ -10,5 +13,13 @@ RSpec.describe 'deleting a post', type: :feature do
 
     expect(page).not_to have_css "img[src*='avocado-on-toast.jpg']"
     expect(page).not_to have_content '#blessed'
+  end
+
+  it 'is not possible if the user is not the owner of the post' do
+    click_on 'Log out'
+
+    sign_up_elodie
+    click_on 'Home'
+    expect(page).not_to have_content 'delete'
   end
 end
