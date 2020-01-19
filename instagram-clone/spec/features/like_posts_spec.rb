a2d6a9c1d1da3b924060954fe19b3472410c7b17
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-01-19 13:40:42 +0000

    feat: allow users to like posts on both their profile page and the main feed

--- instagram-clone/spec/features/like_posts_spec.rb
+++ instagram-clone/spec/features/like_posts_spec.rb
@@ -10,6 +10,11 @@ RSpec.describe 'liking a post', type: :feature do
   end
 
   scenario 'a user likes a post' do
+    click_on 'Log out'
+
+    log_in_sam
+    click_on 'Home'
+
     expect(page).to have_content 'Elodie liked this post'
   end
 
@@ -20,7 +25,7 @@ RSpec.describe 'liking a post', type: :feature do
     expect(page).not_to have_content 'Elodie liked this post'
   end
   
-  describe 'multiple likes' do
+  describe 'edge cases' do
     before do
       click_on 'Log out'
 
@@ -29,18 +34,52 @@ RSpec.describe 'liking a post', type: :feature do
       click_on 'Like'
     end
 
-    scenario 'two users like a post' do
-      expect(page).to have_content 'Elodie and Leina liked this post'
+    context 'multiple likes' do
+      scenario 'two users like a post' do
+        click_on 'Log out'
+
+        log_in_sam
+        click_on 'Home'
+
+        expect(page).to have_content 'Elodie and Leina liked this post'
+      end
+
+      scenario 'more than two users like a post' do
+        click_on 'Log out'
+
+        sign_up_emmanuel
+        click_on 'Home'
+        click_on 'Like'
+        click_on 'Log out'
+
+        log_in_sam
+        click_on 'Home'
+
+        expect(page).to have_content 'Elodie and 2 others liked this post'
+      end
     end
 
-    scenario 'more than two users like a post' do
-      click_on 'Log out'
+    context 'liking your own post' do
+      it 'should prioritise the signed-in user in the like description' do
+        click_on 'Log out'
 
-      sign_up_emmanuel
-      click_on 'Home'
-      click_on 'Like'
+        sign_up_emmanuel
+        click_on 'Home'
+        click_on 'Like'
+        click_on 'Log out'
 
-      expect(page).to have_content 'Elodie and 2 others liked this post'
+        log_in_sam
+        click_on 'Home'
+        click_on 'Like'
+
+        expect(page).to have_content 'You and 3 others liked this post'
+
+        click_on 'Profile'
+        post_picture_of_dog
+        click_on 'Like'
+
+        expect(page).to have_content 'You liked this post'
+      end
     end
   end
 end
