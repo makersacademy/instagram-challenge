48a35f95953f188d264f1f833fd0df384015fa5d
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-01-19 00:18:39 +0000

    feat: users can delete comments

--- instagram-clone/spec/features/comments_spec.rb
+++ instagram-clone/spec/features/comments_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe 'comments', type: :feature do
-  scenario 'a user wants to comment on a post' do
+  before do
     sign_up_sam
     post_picture_of_breakfast
     click_on 'Log out'
@@ -8,8 +8,17 @@ RSpec.describe 'comments', type: :feature do
     click_on 'Home'
     fill_in 'body', with: 'I, too, love avocados.'
     click_on 'Post'
+  end
 
+  scenario 'a user wants to comment on a post' do
     expect(page).to have_css "img[src*='avocado-on-toast.jpg']"
     expect(page).to have_content 'Elodie ~ I, too, love avocados.'
+  end
+
+  scenario 'a user wants to delete their comment' do
+    click_on 'delete comment'
+    
+    expect(page).to have_css "img[src*='avocado-on-toast.jpg']"
+    expect(page).not_to have_content 'Elodie ~ I, too, love avocados.'
   end
 end
