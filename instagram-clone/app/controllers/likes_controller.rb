802875d3df11d5c5cb1a70c2eac10a8bbd60f525
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-01-19 12:21:15 +0000

    feat: allow users to unlike posts

--- instagram-clone/app/controllers/likes_controller.rb
+++ instagram-clone/app/controllers/likes_controller.rb
@@ -3,4 +3,9 @@ class LikesController < ApplicationController
     Like.create({ user_id: session[:id], post_id: params[:post_id] })
     redirect_back fallback_location: root_path
   end
+
+  def destroy
+    Like.destroy(params[:id])
+    redirect_back fallback_location: root_path
+  end
 end
--- instagram-clone/spec/features/like_posts_spec.rb
+++ instagram-clone/spec/features/like_posts_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe 'liking a post', type: :feature do
-  scenario ' a user likes a post' do
+  before do
     sign_up_sam
     post_picture_of_breakfast
     click_on 'Log out'
@@ -7,7 +7,16 @@ RSpec.describe 'liking a post', type: :feature do
     sign_up_elodie
     click_on 'Home'
     click_on 'Like'
+  end
 
+  scenario 'a user likes a post' do
     expect(page).to have_content 'Elodie liked your post'
+  end
+
+  scenario 'a user unlikes a post' do
+    click_on 'Unlike'
+
+    expect(page).to have_content 'Like'
+    expect(page).not_to have_content 'Elodie liked your post'
   end
 end
