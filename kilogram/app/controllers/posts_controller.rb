87ad576320261e9b77d6163003c105b45e2a2c19
Marcellofabbri <marcellogfabbri@gmail.com>
2020-02-15 17:55:07 +0000

    a form in posts/new can create a post with a string (image URL) and a text (description)

--- kilogram/app/controllers/posts_controller.rb
+++ kilogram/app/controllers/posts_controller.rb
@@ -1,4 +1,21 @@
 class PostsController < ApplicationController
+  def show
+    @post = Post.find(params[:id])
+  end
+
   def new
+  end
+
+  def create
+    @post = Post.new(post_params)
+  
+    @post.save
+    redirect_to @post
+  end
+
+  private
+  
+  def post_params
+    params.require(:post).permit(:pictureurl, :text)
   end
 end
--- kilogram/config/routes.rb
+++ kilogram/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'welcome/index'
   
   resources :posts
-  
+  #resources :users
+
   root 'welcome#index'
 end
