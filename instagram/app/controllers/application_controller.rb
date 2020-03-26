6d4531529de7f71249bc53b02bdbfa3ae1a51ea7
Daniel Martinez-Gatell <demartinezgatell@gmail.com>
2020-03-26 20:00:26 +0000

    displaying params for new post rout

--- instagram/app/controllers/application_controller.rb
+++ instagram/app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+
+    def new
+    end
+    def create
+    render plain: params[:post].inspect
+    end
 end
---
+++ instagram/app/controllers/posts_controller.rb
@@ -0,0 +1,8 @@
+class PostsController < ApplicationController
+    def new
+    end
+
+
+   
+
+end
