f0f38cd4fad3de99326c658049785e76feb148ad
Daniel Martinez-Gatell <demartinezgatell@gmail.com>
2020-03-26 18:47:56 +0000

    first commit: rails set up

--- finstagram2/app/models/post.rb
+++
@@ -1,4 +0,0 @@
-class Post < ApplicationRecord
-  validates :title, presence: true,
-                  length: { minimum: 5 }
-end
--- finstagram2/spec/features/user_can_submit_posts_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.feature "Timeline", type: :feature do
-  scenario "Can submit posts and view them" do
-    visit "/posts/new"
-    fill_in "Title", with: "Pretty Amazing Post"
-    fill_in "Text", with: "Bla bla bla look at me"
-    click_button "Submit"
-    expect(page).to have_content("Bla bla bla look at me")
-  end
-end
