45ed0da0d0fdefac4096348d5d830b3f5196d4b2
Daniel Martinez-Gatell <demartinezgatell@gmail.com>
2020-03-27 16:56:25 +0000

    showing basic post params

--- instagram/app/controllers/application_controller.rb
+++ instagram/app/controllers/application_controller.rb
@@ -2,7 +2,22 @@ class ApplicationController < ActionController::Base
 
     def new
     end
-    def create
-    render plain: params[:post].inspect
+
+    def show
+        @post = Post.find(params[:id])
     end
+
+    def create
+        @post = Post.new(post_params)
+
+        @post.save
+        redirect_to @post
+    end
+
+    private 
+    def post_params
+        params.require(:post).permit(:title, :text)
+    end
+
+
 end
