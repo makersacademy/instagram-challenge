c49d0e826061d97f53393331ad7430654d39d63c
Daniel Martinez-Gatell <demartinezgatell@gmail.com>
2020-03-30 21:50:37 +0100

    comments model added

--- instagram/config/routes.rb
+++ instagram/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
-  resources :posts 
+  resources :posts do
+    resources :comments
+  end
 
   root 'welcome#index'
 end
--- instagram/db/schema.rb
+++ instagram/db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_27_160446) do
+ActiveRecord::Schema.define(version: 2020_03_30_151509) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "article_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -19,4 +28,5 @@ ActiveRecord::Schema.define(version: 2020_03_27_160446) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "articles"
 end
