6a15bb73628e7ded82b700bb3b79a540b4eb3fb6
Karla <karla.gardiner@gmail.com>
2020-04-10 18:57:48 +0100

    added validations on picture title

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -8,13 +8,32 @@ class PicturesController < ApplicationController
   end
 
   def new
+    @picture = Picture.new
+  end
+
+  def edit
+    @picture = Picture.find(params[:id])
   end
 
   def create
     @picture = Picture.new(picture_params)
     # @picture.image.attach(params[:image])
-    @picture.save
-    redirect_to @picture
+    if @picture.save
+      redirect_to @picture
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @picture = Picture.find(params[:id])
+
+    if @picture.update(picture_params)
+      redirect_to @picture
+    else
+      render 'edit'
+    end
+
   end
 
   private
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,3 +1,4 @@
 class Picture < ApplicationRecord
   has_one_attached :image
+  validates :title, presence: true, length: { minimum: 5 }
 end
