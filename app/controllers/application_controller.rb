662f8d8ad820f976b6beed682df4f28dd4ea3dc0
lilawalker <lilawlker@gmail.com>
2020-04-12 00:25:26 +0100

    user is redirected to sign in page after sign out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   protect_from_forgery with: :exception
+
+  private
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(_resource_or_scope)
+    new_user_session_path
+  end
 end
--- spec/features/user_can_sign_out_spec.rb
+++ spec/features/user_can_sign_out_spec.rb
@@ -7,4 +7,12 @@ RSpec.feature 'User Sign Out', type: :feature do
     expect(page).to have_link 'Log Out'
   end
 
+  scenario 'user is redirected to the sign in page after signing out' do
+    user = FactoryBot.create(:user)
+    login_as(user)
+    click_link 'Log Out'
+    expect(current_path).to eq '/users/sign_in'
+    expect(page).to have_content 'Signed out successfully.'
+  end
+
 end
