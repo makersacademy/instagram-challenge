28d0601722c31548ed14537badbb3a44d54974a5
lilawalker <lilawlker@gmail.com>
2020-04-11 16:58:38 +0100

    usuer can only enter valid email on sign up

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -14,7 +14,12 @@ RSpec.feature 'User Sign Up', type: :feature do
   end
 
   scenario 'the user can only enter valid email' do
-
+    visit '/users/sign_up'
+    fill_in 'user_email', with: 'ruby'
+    fill_in 'user_password', with: 'Password'
+    fill_in 'user_password_confirmation', with: 'Password'
+    click_button 'Sign up'
+    expect(page).to have_content '1 error prohibited this user from being saved:'
   end
 
   scenario 'the user can only enter a password between 6-10 characters' do
