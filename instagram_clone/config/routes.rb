4399040d9f38f25493a32b779fe049bf8889655b
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-04-11 14:44:40 +0100

    Initial setup with devise, added posts functionality with Active Storage and Comments funtionality

--- instagram_clone/config/routes.rb
+++ instagram_clone/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :comments
+  resources :posts
+  resources :insta
+  root to: 'pages#home'
+
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
