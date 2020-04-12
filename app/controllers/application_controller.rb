871a8ab64f4f73da7272341a837f5f4ec5dd4ab6
lilawalker <lilawlker@gmail.com>
2020-04-12 13:57:13 +0100

    tidy up unused files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,6 @@ class ApplicationController < ActionController::Base
 
   private
 
-  # Overwriting the sign_out redirect path method
   def after_sign_out_path_for(_resource_or_scope)
     new_user_session_path
   end
