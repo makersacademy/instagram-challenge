5cb318a4031ff2d04b71e340814b848dcf59c267
lilawalker <lilawlker@gmail.com>
2020-04-12 12:52:45 +0100

    user sees name on homepage after sign up or sign in

--- spec/helpers/user_helper.rb
+++ spec/helpers/user_helper.rb
@@ -2,6 +2,7 @@ module UserHelper
 
   def sign_up
     visit '/users/sign_up'
+    fill_in 'user_name', with: 'Ruby'
     fill_in 'user_email', with: 'ruby@rails.com'
     fill_in 'user_password', with: '123456'
     fill_in 'user_password_confirmation', with: '123456'
