bd462c42d883920bb70ba020faf81861ebd40203
lilawalker <lilawlker@gmail.com>
2020-04-15 18:43:44 +0100

    add comments via scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :comments
   resources :pictures, except: [:update, :edit]
   devise_for :users, controllers: {
         registrations: 'users/registrations'
