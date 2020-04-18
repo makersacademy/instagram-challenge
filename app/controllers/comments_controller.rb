8b3de6e6ef3e1aebc99068d6d456488ebd5a9cd3
lilawalker <lilawlker@gmail.com>
2020-04-18 15:33:59 +0100

    complete tests for creating comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,5 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: [:show, :edit, :update, :destroy]
-
-  def index
-    @comments = Comment.all
-  end
-
-  def show
-  end
+  before_action :set_comment, only: [:edit, :update, :destroy]
 
   def new
     @comment = Comment.new
@@ -21,7 +14,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to picture_path(@comment.picture), notice: 'Comment was successfully created.' }
+        format.html { redirect_to pictures_path, notice: 'Comment was successfully created.' }
       else
         format.html { render :new }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
@@ -32,8 +25,7 @@ class CommentsController < ApplicationController
   def update
     respond_to do |format|
       if @comment.update(comment_params)
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @comment }
+        format.html { redirect_to pictures_path, notice: 'Comment was successfully updated.' }
       else
         format.html { render :edit }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
@@ -44,7 +36,7 @@ class CommentsController < ApplicationController
   def destroy
     @comment.destroy
     respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
+      format.html { redirect_to pictures_path, notice: 'Comment was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -58,5 +50,5 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:text, :picture_id)
   end
-  
+
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,13 +1,20 @@
 FactoryBot.define do
-  factory :picture do
-    image { 'http://image.com' }
-    user { create(:user) }
-  end
 
   factory :user do
-    id { 1 }
     name { 'Ruby Rails' }
     email { 'test@example.com' }
     password { '123456789' }
   end
+
+  factory :picture do
+    image { 'cat.jpeg' }
+    user { create(:user) }
+  end
+
+  factory :comment do
+    text { 'this is a comment' }
+    picture { create(:picture) }
+    user { create(:user) }
+  end
+
 end
