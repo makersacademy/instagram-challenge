96690111f4dc42556986c6c249a7bf7a5ea60243
Graham <grahamalconer@icloud.com>
2020-05-08 20:53:35 +0100

    TEST PASSED - posts index responds with http status 200

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,7 @@
+class PostsController < ApplicationController
+    def new
+    end
+
+    def create
+    end
+end
---
+++ spec/controllers/posts_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe PostsController, type: :controller do
+  describe "GET /" do
+    it "responds with 200" do
+      get :index
+      expect(response).to have_http_status(200)
+    end
+  end
+end
