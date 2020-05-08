ab7fe7930f909c83bc9a5a3a53cf1f856105c4dc
Harry Turnbull <hturnbull@live.co.uk>
2020-05-08 14:04:12 +0100

    added index and new to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,7 @@
 class SessionsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/index'
+  resources :sessions
   resources :users, only: [:new, :create]
 end
