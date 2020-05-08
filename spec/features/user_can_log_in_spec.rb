a410cc54eb873e56bb3b83ed9a562af183f61f59
Harry Turnbull <hturnbull@live.co.uk>
2020-05-08 15:23:58 +0100

    R: user logs in through sessions/new form, WRONG password, and sees error

--- spec/features/user_can_log_in_spec.rb
+++ spec/features/user_can_log_in_spec.rb
@@ -9,4 +9,13 @@ RSpec.feature 'Log In', type: :feature do
     click_on 'Submit'
     expect(page).to have_content 'davedude'
   end
+
+  scenario 'user logs in through sessions/new form, WRONG password, and sees error' do
+    User.create(username: 'davedude', email: 'davedude@example.com', password: 'password123', name: 'Dave Dude')
+    visit 'sessions/new'
+    fill_in 'user[username]', with: 'davedude'
+    fill_in 'user[password]', with: 'wrongpassword'
+    click_on 'Submit'
+    expect(page).to have_content 'Incorrect username or password'
+  end
 end
