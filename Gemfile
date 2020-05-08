cf129df6b7c3811fb5a4cf14aebaab120514380e
Harry Turnbull <hturnbull@live.co.uk>
2020-05-08 11:09:30 +0100

    added simplecov-console

--- Gemfile
+++ Gemfile
@@ -52,6 +52,7 @@ group :test do
   gem 'webdrivers'
   # Coverage with simplecov
   gem 'simplecov', require: false
+  gem 'simplecov-console', require: false
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,10 @@
 require 'simplecov'
+require 'simplecov-console'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
+])
+
 SimpleCov.start 'rails' do
   add_filter '/bin/'
   add_filter '/db/'
