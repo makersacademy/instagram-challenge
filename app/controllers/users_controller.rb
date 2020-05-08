dbcb3735cd51fdbbcec7f6c5e12b9a8fe322b401
Harry Turnbull <hturnbull@live.co.uk>
2020-05-08 13:49:55 +0100

    create a user in user create route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,14 @@ class UsersController < ApplicationController
   end
 
   def create
-    render plain: params[:user].inspect
+    @user = User.create(user_params)
+    session[:user_id] = @user.id
+    redirect_to '/'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :username, :password)
   end
 end
