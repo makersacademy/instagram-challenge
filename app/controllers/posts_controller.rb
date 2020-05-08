e88f95161011c841bd82379333ddac36397bdace
Harry Turnbull <hturnbull@live.co.uk>
2020-05-08 18:30:41 +0100

    added logic to create post on current_user, redirect to root

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,12 +4,13 @@ class PostsController < ApplicationController
   end
 
   def create
-    
+    current_user.posts.create(post_params)
+    redirect_to root_path
   end
 
   private
 
   def post_params
-    params.require(:post).permit(:image, :filter)
+    params.require(:post).permit(:image)
   end
 end
