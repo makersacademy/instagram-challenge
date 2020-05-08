beddcc3d9c844f40d3a21121cdfe4dbd731b8c1d
Harry Turnbull <hturnbull@live.co.uk>
2020-05-08 13:31:11 +0100

    added users as resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:new, :create]
 end
