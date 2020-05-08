7106a9fedd699e74987d508ea3cf111a20592835
Harry Turnbull <hturnbull@live.co.uk>
2020-05-08 14:58:59 +0100

    added sessions create route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,16 @@ class SessionsController < ApplicationController
 
   def new
   end
+
+  def create
+    @user = User.find_by(username: user_params[:username])
+    session[:user_id] = @user.id
+    redirect_to '/'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
 end
