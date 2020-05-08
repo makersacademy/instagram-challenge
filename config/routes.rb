d2bd87b04ef3239aa07ab0f981543c3edec4ee9d
Graham <grahamalconer@icloud.com>
2020-05-08 21:34:04 +0100

    INDEX PAGE WORKS

--- config/routes.rb
+++ config/routes.rb
@@ -3,3 +3,5 @@ Rails.application.routes.draw do
   root "posts#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+#the "posts#index route sets the base root as posts"
