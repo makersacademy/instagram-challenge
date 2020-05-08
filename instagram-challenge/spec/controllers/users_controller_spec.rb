e353e360170c2bc6c37933baf392a7169ae2ca98
Gina Frankel <ginafrankel@Ginas-MacBook-Pro.local>
2020-05-08 21:40:35 +0100

    changed rspec-rails to version 4.0.0  because of issue with previous  version

---
+++ instagram-challenge/spec/controllers/users_controller_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+
+
+
+RSpec.describe UsersController, type: :controller do
+
+  describe "GET  /new" do 
+    it "responds with a 200 ok status " do 
+      get :new
+      expect(response).to have_http_status(200)
+    end 
+  end 
+
+
+end
+
+
+
+
+# RSpec.describe PostsController, type: :controller do
+#   describe "GET /new " do
+#     it "responds with 200" do
+#       get :new
+#       expect(response).to have_http_status(200)
+#     end
+#   end
+
+#   describe "POST /" do
+#     it "responds with 200" do
+#       post :create, params: { post: { message: "Hello, world!" } }
+#       expect(response).to redirect_to(posts_url)
+#     end
+
+#     it "creates a post" do
+#       post :create, params: { post: { message: "Hello, world!" } }
+#       expect(Post.find_by(message: "Hello, world!")).to be
+#     end
+#   end
+
+#   describe "GET /" do
+#     it "responds with 200" do
+#       get :index
+#       expect(response).to have_http_status(200)
+#     end
+#   end
+
+#   describe "DELETE /" do
+#     it "deletes a post" do
+#       post :create, params: { post: { message: "Hello, world!" } }
+#       found_post = Post.find_by(message: "Hello, world!")
+#       delete :destroy, params: { id: found_post.id  }
+#       expect(Post.find_by(message: "Hello, world!")).not_to be
+#     end
+#   end
+
+#   describe "PATCH /" do
+#     it "updates a post" do
+#       post :create, params: { post: { message: "Big Turtle"} }
+#       found_post = Post.find_by(message: "Big Turtle")
+#       patch :update, params: { id: found_post.id, post: { message: "New Turtle" } }
+#       expect(Post.find_by(message: "New Turtle")).to be
+#     end 
+#   end
+# end
