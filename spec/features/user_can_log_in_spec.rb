3e95140d9e3b1ed85bc83d58d257954d90a27236
Harry Turnbull <hturnbull@live.co.uk>
2020-05-08 14:59:36 +0100

    added to test, prepare a user to log in to

--- spec/features/user_can_log_in_spec.rb
+++ spec/features/user_can_log_in_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.feature 'Log In', type: :feature do
-  scenario 'user logs in through sessions/new form and sees their username' do
+  scenario 'user logs in through sessions/new form, correct password, and sees their username' do
+    User.create(username: 'davedude', email: 'davedude@example.com', password: 'password123', name: 'Dave Dude')
     visit 'sessions/new'
     fill_in 'user[username]', with: 'davedude'
     fill_in 'user[password]', with: 'password123'
