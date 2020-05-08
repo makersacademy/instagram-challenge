dab67d96ff01cbe72140d2c97f31d9904405e803
Harry Turnbull <hturnbull@live.co.uk>
2020-05-08 16:42:39 +0100

    RF: use rucocop alternative to if @user && @user.auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(username: user_params[:username])
     
-    if @user && @user.authenticate(user_params[:password])
+    if @user&.authenticate(user_params[:password])
       session[:user_id] = @user.id
       redirect_to root_path
     else
