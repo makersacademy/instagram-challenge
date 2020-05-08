0dd4b7078a21dfe49aea2e258cd5bef51386a863
Harry Turnbull <hturnbull@live.co.uk>
2020-05-08 13:11:32 +0100

    updated test

--- spec/features/users_can_sign_up_spec.rb
+++ spec/features/users_can_sign_up_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.feature 'Sign Up', type: :feature do
-  scenario 'user signs up through form and see their username' do
+  scenario 'user signs up through users/new form and sees their username' do
     visit 'users/new'
-    fill_in 'user[:name]', with: 'Dave Dude'
-    fill_in 'user[:email]', with: 'davedude@example.com'
-    fill_in 'user[:username]', with: 'davedude'
-    fill_in 'user[:password]', with: 'password123'
+    fill_in 'name', with: 'Dave Dude'
+    fill_in 'email', with: 'davedude@example.com'
+    fill_in 'username', with: 'davedude'
+    fill_in 'password', with: 'password123'
     click_on 'Submit'
     expect(page).to have_content 'davedude'
   end
