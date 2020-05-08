2c09c817fe9353522cf1f49393791ef1e388f000
Harry Turnbull <hturnbull@live.co.uk>
2020-05-08 15:24:16 +0100

    user authentication in sessions create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,14 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(username: user_params[:username])
-    session[:user_id] = @user.id
-    redirect_to '/'
+    
+    if @user.authenticate(user_params[:password])
+      session[:user_id] = @user.id
+      redirect_to '/'
+    else
+      @error = true
+      render 'sessions/new'
+    end
   end
 
   private
