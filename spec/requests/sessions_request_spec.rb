222e745ca09ac6a93359dd426e15d44bd1a2f4de
Harry Turnbull <hturnbull@live.co.uk>
2020-05-08 14:23:30 +0100

    rm session get /index test, not requried

--- spec/requests/sessions_request_spec.rb
+++ spec/requests/sessions_request_spec.rb
@@ -2,13 +2,6 @@ require 'rails_helper'
 
 RSpec.describe "Sessions", type: :request do
 
-  describe "GET /index" do
-    it "returns http success" do
-      get "/sessions/index"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
   describe "GET /new" do
     it "returns http success" do
       get "/sessions/new"
