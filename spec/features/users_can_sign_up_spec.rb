d69d3b92eb320eb021665f1cf29f80b7860c4c63
Harry Turnbull <hturnbull@live.co.uk>
2020-05-08 13:34:37 +0100

    adjusted field names again

--- spec/features/users_can_sign_up_spec.rb
+++ spec/features/users_can_sign_up_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.feature 'Sign Up', type: :feature do
   scenario 'user signs up through form and see their username' do
     visit 'users/new'
-    fill_in 'user[:name]', with: 'Dave Dude'
-    fill_in 'user[:email]', with: 'davedude@example.com'
-    fill_in 'user[:username]', with: 'davedude'
-    fill_in 'user[:password]', with: 'password123'
+    fill_in 'user[name]', with: 'Dave Dude'
+    fill_in 'user[email]', with: 'davedude@example.com'
+    fill_in 'user[username]', with: 'davedude'
+    fill_in 'user[password]', with: 'password123'
     click_on 'Submit'
     expect(page).to have_content 'davedude'
   end
