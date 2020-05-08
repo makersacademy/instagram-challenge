028e77f0932dd7730f0538a662f09397bae5391d
Harry Turnbull <hturnbull@live.co.uk>
2020-05-08 14:43:52 +0100

    gave more details to scenario desc

--- spec/features/users_can_sign_up_spec.rb
+++ spec/features/users_can_sign_up_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature 'Sign Up', type: :feature do
-  scenario 'user signs up through form and see their username' do
+  scenario 'user signs up through users/new form and sees their username' do
     visit 'users/new'
     fill_in 'user[name]', with: 'Dave Dude'
     fill_in 'user[email]', with: 'davedude@example.com'
