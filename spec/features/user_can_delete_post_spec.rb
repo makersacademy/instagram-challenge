11e866f24b0b105c03ded06d55cc6c0adb70f82a
Harry Turnbull <hturnbull@live.co.uk>
2020-05-09 12:17:02 +0100

    tweaked test

---
+++ spec/features/user_can_delete_post_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.feature 'Edit a post', type: :feature do
+
+  before(:each) do
+    visit 'users/new'
+    fill_in 'user[name]', with: 'Dave Dude'
+    fill_in 'user[email]', with: 'davedude@example.com'
+    fill_in 'user[username]', with: 'davedude'
+    fill_in 'user[password]', with: 'password123'
+    click_on 'Submit'
+  end
+  
+  scenario 'User clicks on post, clicks delete, and accepts warning, post is deleted' do
+    click_on 'New post'
+    attach_file 'post[image]', './images/post-image.jpg'
+    click_on 'Post'
+    within(first('.post')) do
+      find('a').click
+    end
+    click_on 'Delete'
+    expect(page).to_not have_css '.post'
+  end
+
+  # scenario 'User clicks on another users post, expects not to see edit option' do
+  #   other_user = User.create(name: 'Other Guy', email: 'otherguy@example.com', username: 'otherguy', password: 'password123')
+  #   other_post = other_user.posts.create(filter: 'Greyscale')
+  #   images_dir = Dir.pwd + '/images/post-image.jpg'
+  #   other_post.image.attach(io: File.open(images_dir), filename: 'post-image.png')
+    
+  #   refresh
+  #   within(first('.post')) do
+  #     find('a').click
+  #   end
+  #   expect(page).to_not have_content 'Edit'
+  # end
+end
