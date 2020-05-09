cfc81a26c3dcb0b1dc42cf81daf6088ddff93be2
Harry Turnbull <hturnbull@live.co.uk>
2020-05-09 14:38:26 +0100

    nested comments within posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resource :sessions, only: [:index, :new, :create, :destroy]
   resources :users, only: [:new, :create] do
-    resources :posts, shallow: true
+    resources :posts, shallow: true do
+      resources :comments
+    end
   end
   root 'sessions#index'
 end
