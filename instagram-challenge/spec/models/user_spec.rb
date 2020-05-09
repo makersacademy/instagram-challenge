820dc1ea645d1cd92e4c2d5aeaa7a5190a16a743
Gina Frankel <ginafrankel@Ginas-MacBook-Pro.local>
2020-05-09 13:54:12 +0100

    Model test - Refractor - edited test descriptions to make more readable

--- instagram-challenge/spec/models/user_spec.rb
+++ instagram-challenge/spec/models/user_spec.rb
@@ -2,22 +2,21 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'validating user name ' do 
-    # better way to describe this it block? 
-    it " user name 'gina' user should be valid" do
+    it " standard user name should be valid" do
       user = User.new(name: 'gina', email: 'gina@example.com')
       expect(user).to be_valid
     end 
 
-    it 'user should only  be valid if name is entered' do 
+    it 'user will not be valid if name is entered' do 
       expect(User.new(email: 'gina@example.com')).to_not be_valid
     end 
 
-    it 'user should only  be valid if name is entered' do 
+    it 'user will not  be valid if name is entered' do 
       user = User.new(name: '', email: 'gina@example.com')
       expect(user).to_not be_valid
     end 
 
-    it 'name should not be too long' do
+    it 'user will not be valid if name is too long' do
       long_name = 'g' * 51
       user = User.new(name: long_name, email: 'gina@example.com')
       expect(user).to_not be_valid
