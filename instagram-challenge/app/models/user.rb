e746e982b6af0c96c7e4bd632e6fa9c6737b7100
‘Gina-Frankel <g.frankel22@gmail.com>
2020-05-09 15:10:46 +0100

    Model test - Green -  Validating user email: user will not be valided if they enter the same email as another user

--- instagram-challenge/app/models/user.rb
+++ instagram-challenge/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\S*@[a-z]*\.[a-z]{2,30}([a-z]|\.[a-z]{2,30})/i
-  validates :email, presence: true, format: {with:  VALID_EMAIL_REGEX }
+  validates :email, presence: true, format: {with:  VALID_EMAIL_REGEX },
+  uniqueness: true
 end
