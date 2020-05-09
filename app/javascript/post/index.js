715e58bf0a35071662a7e99b9e25163195bfbbc4
Harry Turnbull <hturnbull@live.co.uk>
2020-05-09 11:12:54 +0100

    used image container to allow for image itself to be rendered

--- app/javascript/post/index.js
+++ app/javascript/post/index.js
@@ -1,30 +1,35 @@
 document.addEventListener("DOMContentLoaded", function() {
   const inpFile = document.getElementById("post_image");
-  const previewImage = document.getElementById("previewImage");
+  const container = document.getElementById("image-preview-container");
+  const image = document.getElementById("previewImage")
   
-  inpFile.addEventListener('change', function() {
-    const file = this.files[0]
-
-    if (file) {
-      const reader = new FileReader()
-
-      previewImage.style.display = "block";
-
-      reader.addEventListener('load', function() {
-        previewImage.setAttribute('src', this.result)
-      })
-
-      reader.readAsDataURL(file)
-    } else {
-      previewImage.style.display = null;
-    }
-  })
+  try {
+    inpFile.addEventListener('change', function() {
+      const file = this.files[0]
+  
+      if (file) {
+        const reader = new FileReader()
+  
+        container.style.display = "block";
+  
+        reader.addEventListener('load', function() {
+          image.setAttribute('src', this.result)
+        })
+  
+        reader.readAsDataURL(file)
+      } else {
+        container.style.display = null;
+      }
+    })
+  } catch (error) {
+    
+  }
 
   const filterSelect = document.getElementById('post_filter')
 
   filterSelect.addEventListener('change', function(e) {
     let selection = e.target.value;
     let filterClass = `filter_${selection.toLowerCase()}`;
-    previewImage.setAttribute('class', filterClass)
+    image.setAttribute('class', filterClass)
   })
 });
