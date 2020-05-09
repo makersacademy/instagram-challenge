ffec35803049b314d24352bca5d1c4fe0536bbf1
‘Gina-Frankel <g.frankel22@gmail.com>
2020-05-09 15:33:48 +0100

    Model test - refractored -  Validating user email: user will not be valided if they enter the same email as another user even if they use capital letters

--- instagram-challenge/app/models/user.rb
+++ instagram-challenge/app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
+  before_validation { self.email = email.downcase }
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\S*@[a-z]*\.[a-z]{2,30}([a-z]|\.[a-z]{2,30})/i
-  validates :email, presence: true, format: {with:  VALID_EMAIL_REGEX },
-  uniqueness: {case_sensitive: false}
+  validates :email, presence: true, 
+            format: {with:  VALID_EMAIL_REGEX },
+            uniqueness: true
 end
--- instagram-challenge/db/schema.rb
+++ instagram-challenge/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_08_210358) do
+ActiveRecord::Schema.define(version: 2020_05_09_142021) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
