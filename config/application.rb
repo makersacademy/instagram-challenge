02354bc8b99dee82d94e4937632508e845419520
Harry Turnbull <hturnbull@live.co.uk>
2020-05-09 13:04:06 +0100

    specified tz in config

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module InstagramChallenge
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # Use GMT
+    config.time_zone = 'Europe/London'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
