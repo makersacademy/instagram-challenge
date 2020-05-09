e54e1b5ff8561018fd698bb3e4aabf5a6aa1c43e
FayeCarter <fayecarter333@gmail.com>
2020-05-09 16:14:19 +0100

    styling added for homepage

--- spec/features/user_sign_up_spec.rb
+++ spec/features/user_sign_up_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.feature 'Sign Up', type: :feature do
   scenario 'User can sign up from Instagram homepage and see their username' do
     visit '/'
-    click_on 'Sign up'
+    click_link 'Sign up'
     fill_in 'user[first_name]', with: 'first name'
     fill_in 'user[last_name]', with: 'last name'
     fill_in 'user[email]', with: 'user@gmail.com'
     fill_in 'user[username]', with: 'username'
     fill_in 'user[password]', with: 'password'
     fill_in 'user[password_confirmation]', with: 'password'
-    click_on 'Sign up'
+    click_button 'Sign up'
     expect(page).to have_content 'username'
     expect(page).not_to have_content 'Sign up'
   end
