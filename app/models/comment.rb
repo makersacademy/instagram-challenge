185da3ce2749e268c408f03c39d4a69f7f3ba093
Harry Turnbull <hturnbull@live.co.uk>
2020-05-09 21:15:08 +0100

    comment formatted_time method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,8 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   delegate :username, to: :user, allow_nil: true
+
+  def formatted_time
+    created_at.nil? ? nil : created_at.strftime("%-dth %b, %l:%M%P")
+  end
 end
