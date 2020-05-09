09bbb2f46598f989ad213a5c86692f7f71ddcd4e
‘Gina-Frankel <g.frankel22@gmail.com>
2020-05-09 15:19:12 +0100

    Model test - Green -  Validating user email: user will not be valided if they enter the same email as another user even if they use capital letters

--- instagram-challenge/app/models/user.rb
+++ instagram-challenge/app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\S*@[a-z]*\.[a-z]{2,30}([a-z]|\.[a-z]{2,30})/i
   validates :email, presence: true, format: {with:  VALID_EMAIL_REGEX },
-  uniqueness: true
+  uniqueness: {case_sensitive: false}
 end
