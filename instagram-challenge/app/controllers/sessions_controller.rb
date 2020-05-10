6eca4b933450786539e7adc05a99893982159f7d
‘Gina-Frankel <g.frankel22@gmail.com>
2020-05-10 21:47:27 +0100

    feature - passing - sign in: user can sign in

--- instagram-challenge/app/controllers/sessions_controller.rb
+++ instagram-challenge/app/controllers/sessions_controller.rb
@@ -1,4 +1,23 @@
 class SessionsController < ApplicationController
   def new
   end
+  def create 
+    puts " in create method"
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in(user)
+      puts "user matches"
+      p user
+      redirect_to user_url(user.id)
+    else 
+      puts " "
+      puts "user does not match"
+    end 
+  end 
+
+
+  def log_in(user)
+    session[:user_id] = user.id
+  end 
+
 end
--- instagram-challenge/spec/features/log_in_spec.rb
+++ instagram-challenge/spec/features/log_in_spec.rb
@@ -5,11 +5,13 @@ require 'rails_helper'
 RSpec.feature "Sign in", type: :feature do
 
   scenario "user can sign in"  do 
-    user = User.create(name: "User", email: "user@example.com", password: "12345")
+    user = User.create(name: "user", email: "user@example.com", password: "12345")
     visit "sessions/new"
-    fill_in 'session[email]', with: 'user'
+    fill_in 'session[email]', with: 'user@example.com'
     fill_in  'session[password]', with: '12345'
     click_on 'Sign in'
-    expect(page).to have_content('Welcome User')
+    expect(page).to have_content('user')
   end 
 end
+
+# test to see where it  has been redirected 
