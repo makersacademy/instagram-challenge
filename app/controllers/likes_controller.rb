5e8cad04ee593b4fc93b153203f5573bdd33feb7
Harry Turnbull <hturnbull@live.co.uk>
2020-05-10 11:46:32 +0100

    added destroy likes route

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -7,6 +7,13 @@ class LikesController < ApplicationController
     @like.save
     redirect_to @post
   end
+  
+  def destroy
+    @post = Post.find(params[:post_id])
+    @like = Like.find(params[:id])
+    @like.destroy
+    redirect_to @post
+  end
 
   private
 
