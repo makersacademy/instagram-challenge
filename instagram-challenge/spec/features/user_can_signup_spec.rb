c4b93681b9235d0c1c480e90409d5abf2e8ebfe6
‘Gina-Frankel <g.frankel22@gmail.com>
2020-05-10 13:46:03 +0100

    Feature: Green - Once user has signed up they are redirected to a page with thier name on it

--- instagram-challenge/spec/features/user_can_signup_spec.rb
+++ instagram-challenge/spec/features/user_can_signup_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 
+
 RSpec.feature "Sign-up", type: :feature do
   scenario "Users see heading Sign up" do 
     visit "/users/new"
     expect(page).to have_content("Sign up")
 end 
-  xscenario "Once user has signed up they are redirected to the home page" do 
+  scenario "Once user has signed up they are redirected to a page with thier name on it" do 
+    user_1 = User.create(name: "elmo", email: "emmo@example.com", password: "abcdef")
     visit "/users/new"
-    fill_in "user[name]", with: "Gina"
-    click_on "Sign up"
-    expect(current_path).to eql('/')
+    expect(page).to have_content('elmo')
   end 
  
 end 
