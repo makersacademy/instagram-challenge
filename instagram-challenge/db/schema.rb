e05efac39a4a2a1c79b541b7c3451d277ee92fef
‘Gina-Frankel <g.frankel22@gmail.com>
2020-05-10 19:08:52 +0100

    remember digest added

--- instagram-challenge/db/schema.rb
+++ instagram-challenge/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_09_144017) do
+ActiveRecord::Schema.define(version: 2020_05_10_180342) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_05_09_144017) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
