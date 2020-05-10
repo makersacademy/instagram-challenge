f64d78b3b1d7868691ff9e891304b63602a7e170
Gina Frankel <ginafrankel@Ginas-MacBook-Pro.local>
2020-05-10 17:56:34 +0100

    Feature - Passed - Sign-up : Once user has signed up they are redirected to a page with thier name on it

--- instagram-challenge/app/controllers/users_controller.rb
+++ instagram-challenge/app/controllers/users_controller.rb
@@ -4,8 +4,20 @@ class UsersController < ApplicationController
   end 
   
   def new
-  
+   @user = User.new
   end
 
+  def create
+    @user = User.new(user_params) 
+    if @user.save == true 
+      redirect_to user_url(@user.id)
+    end 
+  
+  end 
+
+  private 
+  def  user_params
+    params.require(:user).permit(:name, :email, :password)
+  end 
 
 end
