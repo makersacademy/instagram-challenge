c53c5f257f31a465230cbba2592da764a56ec747
Gina Frankel <ginafrankel@Ginas-MacBook-Pro.local>
2020-05-10 21:07:15 +0100

    feature - failing - sign in: user can sign in

--- instagram-challenge/spec/features/log_in_spec.rb
+++ instagram-challenge/spec/features/log_in_spec.rb
@@ -5,11 +5,11 @@ require 'rails_helper'
 RSpec.feature "Sign in", type: :feature do
 
   scenario "user can sign in"  do 
-    user = User.create(name: "user", email: "user@example.com", password: "12345")
+    user = User.create(name: "User", email: "user@example.com", password: "12345")
     visit "sessions/new"
-    fill_in 'user[name]', with: 'user'
-    fill_in  'user[password]', with: '12345'
-    click_on 'sign in'
-    expect(page).to have_content('12345')
+    fill_in 'session[email]', with: 'user'
+    fill_in  'session[password]', with: '12345'
+    click_on 'Sign in'
+    expect(page).to have_content('Welcome User')
   end 
 end
