c639ce55e5253d057e9efcae70e8d13a45dec598
‘Gina-Frankel <g.frankel22@gmail.com>
2020-05-10 22:45:53 +0100

    session logs user in

--- instagram-challenge/app/controllers/sessions_controller.rb
+++ instagram-challenge/app/controllers/sessions_controller.rb
@@ -2,19 +2,18 @@ class SessionsController < ApplicationController
   def new
   end
   def create 
-    puts " in create method"
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in(user)
-      puts "user matches"
-      p user
       redirect_to user_url(user.id)
+      flash[:success] = 'You are signed in successfully'
     else 
       puts " "
+      flash[:danger] = 'Invalid email/password combination' # Not quite right!
       puts "user does not match"
+      redirect_to sessions_new_path
     end 
   end 
-
 
   def log_in(user)
     session[:user_id] = user.id
