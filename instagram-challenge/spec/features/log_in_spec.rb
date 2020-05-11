571f0e61f9dcc7c81e442b3cb747dcf2076e3d05
Gina Frankel <ginafrankel@Ginas-MacBook-Pro.local>
2020-05-11 08:06:19 +0100

    feature - passing - log in: When users sign in with the wrong email they are not  able to  sign in

--- instagram-challenge/spec/features/log_in_spec.rb
+++ instagram-challenge/spec/features/log_in_spec.rb
@@ -17,11 +17,20 @@ RSpec.feature "Sign in", type: :feature do
     user = User.create(name: "user", email: "user@example.com", password: "12345")
     visit "sessions/new"
     fill_in 'session[email]', with: 'user@example.com'
-    fill_in  'session[password]', with: 'wrong password'
+    fill_in  'session[password]', with: 'wrong_password'
     click_on 'Sign in'
     expect(page).to have_no_content("user")
-
   end 
+
+    scenario "When users sign in with the wrong email they are not  able to  sign in" do
+      user = User.create(name: "user", email: "user@example.com", password: "12345")
+      visit "sessions/new"
+      fill_in "session[email]", with: 'wrong_email@example.com'
+      fill_in 'session[password]', with: '12345'
+      click_on 'Sign in'
+      expect(page).to have_no_content("user")
+    end
+   
 end
 
 # test to see where it  has been redirected 
