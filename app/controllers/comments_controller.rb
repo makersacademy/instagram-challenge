679258ee8487b8247a699016a919f98de633aa0f
Harry Turnbull <hturnbull@live.co.uk>
2020-05-11 12:50:31 +0100

    no phantom comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(comment_params)
+    @comment = @post.comments.new(comment_params)
     @comment.user_id = current_user.id
     @comment.save
     redirect_to post_path(@post)
@@ -11,6 +11,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:content)
+    params.permit(:content)
   end
 end
