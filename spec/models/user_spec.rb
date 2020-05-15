cbc99e6c44aceebc3fbe3f2831b71a6120ea80c5
lilawalker <lilawlker@gmail.com>
2020-05-15 16:26:53 +0100

    Add user model validation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it { is_expected.to be }
+
+  subject(:user) { described_class.new(
+      id: 1,
+      name: 'fakeuser',
+      email: 'fake@fakeemail.com',
+      encrypted_password: 'fakepassword'
+    )
+  }
+
+  it 'is not valid without a name' do
+    user.name = nil
+    expect(user).to_not be_valid
+  end
+
+  it 'is not valid without an email' do
+    user.email = nil
+    expect(user).to_not be_valid
+  end
+
+  it 'is not valid without a password' do
+    user.encrypted_password = nil
+    expect(user).to_not be_valid
+  end
+
 end
