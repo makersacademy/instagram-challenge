16a75f43dca94bc465ebf03ba3058c1023be5804
lilawalker <lilawlker@gmail.com>
2020-05-15 16:31:05 +0100

    Add picture model validation tests

--- spec/models/picture_spec.rb
+++ spec/models/picture_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Picture, type: :model do
-  it { is_expected.to be }
+
+  subject(:picture) { described_class.new(
+      id: 1,
+      image: 'fakepicture.jpg',
+    )
+  }
+
+  it 'is not valid without an image' do
+    picture.image = nil
+    expect(picture).to_not be_valid
+  end
+
 end
