73153f6a7615c0f5168417fbf2a61f31f9e1bcc5
lilawalker <lilawlker@gmail.com>
2020-05-18 12:03:12 +0100

    Add Likes controller and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_15_173857) do
+ActiveRecord::Schema.define(version: 2020_05_18_104615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,15 @@ ActiveRecord::Schema.define(version: 2020_04_15_173857) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["picture_id"], name: "index_comments_on_picture_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "picture_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["picture_id"], name: "index_likes_on_picture_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "pictures", force: :cascade do |t|
@@ -48,5 +57,7 @@ ActiveRecord::Schema.define(version: 2020_04_15_173857) do
 
   add_foreign_key "comments", "pictures"
   add_foreign_key "comments", "users"
+  add_foreign_key "likes", "pictures"
+  add_foreign_key "likes", "users"
   add_foreign_key "pictures", "users"
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-
+  
   factory :user do
     sequence(:name) { |n| "Ruby Rails#{n}" }
     sequence(:email) { |n| "test#{n}@example.com" }
@@ -15,6 +15,11 @@ FactoryBot.define do
     text { 'this is a comment' }
     picture { create(:picture) }
     user { create(:user) }
+  end
+
+  factory :like do
+    picture { nil }
+    user { nil }
   end
 
 end
