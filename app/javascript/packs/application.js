4c601772dcd583923dfcbe7921aa726b1601ad89
lilawalker <lilawlker@gmail.com>
2020-05-20 17:56:00 +0100

    Setup Bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,7 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import 'bootstrap'
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
