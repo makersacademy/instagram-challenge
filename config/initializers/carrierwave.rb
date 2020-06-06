d44fde48f56983dcac206b2cb15dbd7f34973060
patrick <patrick_sawyer@hotmail.co.uk>
2020-06-06 20:33:00 +0100

    another travis test

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,32 +1,32 @@
-#  CarrierWave.configure do |config|
-#   config.fog_credentials = {
-#     provider: 'AWS',
-#     region: ,
-#     aws_access_key_id: ,
-#     aws_secret_access_key: 
-#   }
-
-#   config.storage = :fog
-#   config.permissions = 0666
-#   config.cache_dir = "#{Rails.root}/tmp"
-#   config.fog_directory = ENV['FOG_DIRECTORY']
-# end
-
-#connection.directories.new(key: blah).files
-
-CarrierWave.configure do |config|
-  config.storage = :fog
-
-
-config.fog_credentials = {
-    :provider               => 'AWS',                             # required
-    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],            # required
-    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],     # required
-    :region                 => 'eu-west-2'                        # optional, defaults to 'us-east-1'
+ CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider               => 'AWS',                             
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],            
+    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],     
+    :region                 => 'eu-west-2' 
   }
-  config.fog_directory  = ENV['FOG_DIRECTORY'] 
-  config.cache_dir = "#{Rails.root}/tmp"            # required
-  #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
-  #config.fog_public     = false                                  # optional, defaults to true
-  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+
+  config.storage = :fog
+  config.permissions = 0666
+  config.cache_dir = "#{Rails.root}/tmp"
+  config.fog_directory = ENV['FOG_DIRECTORY']
 end
+
+# #connection.directories.new(key: blah).files
+
+# CarrierWave.configure do |config|
+#   config.storage = :fog
+
+
+# config.fog_credentials = {
+#     :provider               => 'AWS',                             # required
+#     :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],            # required
+#     :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],     # required
+#     :region                 => 'eu-west-2'                        # optional, defaults to 'us-east-1'
+#   }
+#   config.fog_directory  = ENV['FOG_DIRECTORY'] 
+#   config.cache_dir = "#{Rails.root}/tmp"            # required
+#   #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
+#   #config.fog_public     = false                                  # optional, defaults to true
+#   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+# end
