26e137078977b20d12fb367c8f1bf439fbd7b7df
patrick <patrick_sawyer@hotmail.co.uk>
2020-06-06 22:32:48 +0100

    travis/heroku/aws test 3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,33 +2,16 @@
 
    CarrierWave.configure do |config|
 
-    if Rails.env.development?
+    config.fog_credentials = {
+      :provider               => 'AWS',                             
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],            
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],     
+      :region                 => 'eu-west-2' 
+    }
 
-      config.fog_credentials = {
-        :provider               => 'AWS',                             
-        :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],            
-        :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],     
-        :region                 => 'eu-west-2' 
-      }
-
-      config.storage = :fog
-      config.permissions = 0666
-      config.cache_dir = "#{Rails.root}/tmp"
-      config.fog_directory = ENV['FOG_DIRECTORY']
-
-    elsif Rails.env.production?
-
-      config.fog_credentials = {
-        :provider               => 'AWS',                             
-        :aws_access_key_id      => process.env.AWS_ACCESS_KEY_ID,            
-        :aws_secret_access_key  => process.env.AWS_SECRET_ACCESS_KEY,     
-        :region                 => 'eu-west-2' 
-      }
-
-      config.storage = :fog
-      config.permissions = 0666
-      config.cache_dir = "#{Rails.root}/tmp"
-      config.fog_directory = ENV['FOG_DIRECTORY']
-    end
+    config.storage = :fog
+    config.permissions = 0666
+    config.cache_dir = "#{Rails.root}/tmp"
+    config.fog_directory = ENV['FOG_DIRECTORY']
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200606131542) do
+ActiveRecord::Schema.define(version: 20200606212532) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,9 @@ ActiveRecord::Schema.define(version: 20200606131542) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name", limit: 20
+    t.string "last_name", limit: 20
+    t.string "username", limit: 20
     t.index ["email"], name: "index_accounts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
   end
