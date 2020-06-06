06ff0047441bd1dad3f1267ec765913bf0809bb3
mbrad26 <mbrad26@gmail.com>
2020-06-06 18:31:38 +0100

    implemented test for  users controller

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
+  describe 'GET #new' do
+    it 'returns http succes' do
+      get :new
 
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
