79a53a583a7c20f7e497b1de654a42d917acaa5c
patrick <patrick_sawyer@hotmail.co.uk>
2020-06-06 22:19:37 +0100

    heroku, travis and aws test 2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,31 +1,34 @@
- CarrierWave.configure do |config|
+ if !Rails.env.staging?
 
-  if Rails.env.development?
+   CarrierWave.configure do |config|
 
-    config.fog_credentials = {
-      :provider               => 'AWS',                             
-      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],            
-      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],     
-      :region                 => 'eu-west-2' 
-    }
+    if Rails.env.development?
 
-    config.storage = :fog
-    config.permissions = 0666
-    config.cache_dir = "#{Rails.root}/tmp"
-    config.fog_directory = ENV['FOG_DIRECTORY']
+      config.fog_credentials = {
+        :provider               => 'AWS',                             
+        :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],            
+        :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],     
+        :region                 => 'eu-west-2' 
+      }
 
-  elsif Rails.env.production?
-    
-    config.fog_credentials = {
-      :provider               => 'AWS',                             
-      :aws_access_key_id      => process.env.'AWS_ACCESS_KEY_ID',            
-      :aws_secret_access_key  => process.env.'AWS_SECRET_ACCESS_KEY',     
-      :region                 => 'eu-west-2' 
-    }
+      config.storage = :fog
+      config.permissions = 0666
+      config.cache_dir = "#{Rails.root}/tmp"
+      config.fog_directory = ENV['FOG_DIRECTORY']
 
-    config.storage = :fog
-    config.permissions = 0666
-    config.cache_dir = "#{Rails.root}/tmp"
-    config.fog_directory = ENV['FOG_DIRECTORY']
+    elsif Rails.env.production?
+
+      config.fog_credentials = {
+        :provider               => 'AWS',                             
+        :aws_access_key_id      => process.env.AWS_ACCESS_KEY_ID,            
+        :aws_secret_access_key  => process.env.AWS_SECRET_ACCESS_KEY,     
+        :region                 => 'eu-west-2' 
+      }
+
+      config.storage = :fog
+      config.permissions = 0666
+      config.cache_dir = "#{Rails.root}/tmp"
+      config.fog_directory = ENV['FOG_DIRECTORY']
+    end
   end
 end
