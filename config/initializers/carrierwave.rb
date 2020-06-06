f1f9923579cb19fb8b6fd5ed37de997f861feef8
patrick <patrick_sawyer@hotmail.co.uk>
2020-06-06 21:56:00 +0100

    travis passing and pics saving to AWS

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -14,22 +14,3 @@
     config.fog_directory = ENV['FOG_DIRECTORY']
   end
 end
-
-# #connection.directories.new(key: blah).files
-
-# CarrierWave.configure do |config|
-#   config.storage = :fog
-
-
-# config.fog_credentials = {
-#     :provider               => 'AWS',                             # required
-#     :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],            # required
-#     :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],     # required
-#     :region                 => 'eu-west-2'                        # optional, defaults to 'us-east-1'
-#   }
-#   config.fog_directory  = ENV['FOG_DIRECTORY'] 
-#   config.cache_dir = "#{Rails.root}/tmp"            # required
-#   #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
-#   #config.fog_public     = false                                  # optional, defaults to true
-#   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
-# end
