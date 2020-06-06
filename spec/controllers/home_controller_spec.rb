6988be23ea10a2f739a98162dbccd5053134872e
mbrad26 <mbrad26@gmail.com>
2020-06-06 18:37:54 +0100

    implemented test for home/index controller

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
+    describe 'GET #index' do
+      it 'returns http succes' do
+        get :index
 
+        expect(response).to have_http_status(:success)
+      end
+    end
 end
