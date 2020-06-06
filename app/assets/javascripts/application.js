1b5748fa1f216b22b130fc5d23e0d74b386accc9
patrick <patrick_sawyer@hotmail.co.uk>
2020-06-06 13:00:19 +0100

    still working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,5 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require 'jquery.min'
 //= require rails-ujs
 //= require_tree .
+//= require bootstrap.min
+
