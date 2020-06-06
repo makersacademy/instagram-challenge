23fe21b00902fd2ee682c6bf7ab68ce712362949
patrick <patrick_sawyer@hotmail.co.uk>
2020-06-06 22:09:21 +0100

    checking travis/aws/heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,7 @@
  CarrierWave.configure do |config|
 
   if Rails.env.development?
+
     config.fog_credentials = {
       :provider               => 'AWS',                             
       :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],            
@@ -12,7 +13,10 @@
     config.permissions = 0666
     config.cache_dir = "#{Rails.root}/tmp"
     config.fog_directory = ENV['FOG_DIRECTORY']
+
   elsif Rails.env.production?
+    
+    config.fog_credentials = {
       :provider               => 'AWS',                             
       :aws_access_key_id      => process.env.'AWS_ACCESS_KEY_ID',            
       :aws_secret_access_key  => process.env.'AWS_SECRET_ACCESS_KEY',     
