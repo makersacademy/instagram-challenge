54f037bdd9e16b8b5b181aa8f5c0ef4bf9bb9a11
patrick <patrick_sawyer@hotmail.co.uk>
2020-06-06 21:40:37 +0100

    travis test 100000000

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,18 @@
  CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',                             
-    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],            
-    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],     
-    :region                 => 'eu-west-2' 
-  }
 
-  config.storage = :fog
-  config.permissions = 0666
-  config.cache_dir = "#{Rails.root}/tmp"
-  config.fog_directory = ENV['FOG_DIRECTORY']
+  if !Rails.env.test? do
+    config.fog_credentials = {
+      :provider               => 'AWS',                             
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],            
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],     
+      :region                 => 'eu-west-2' 
+    }
+
+    config.storage = :fog
+    config.permissions = 0666
+    config.cache_dir = "#{Rails.root}/tmp"
+    config.fog_directory = ENV['FOG_DIRECTORY']
+  end
 end
 
 # #connection.directories.new(key: blah).files
