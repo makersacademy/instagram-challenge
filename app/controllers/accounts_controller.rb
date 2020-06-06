b8e4f859118b11c83b719094335c20cdb131237d
patrick <patrick_sawyer@hotmail.co.uk>
2020-06-06 20:24:10 +0100

    another travis test

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -3,7 +3,7 @@ class AccountsController < ApplicationController
   before_action :authenticate_account!
 
   def index
-
+    @posts = Post.active
   end
 
   def show
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
    mount_uploader :image, ImageUploader
+
+   scope :active, -> { where active: true }
 end
