831ad61d6f2ecba20db4f34c993446e19ffe771c
Catriona <catrionamairi@blueyonder.co.uk>
2020-06-07 16:49:01 +0100

    Show pictures as images

--- spec/views/posts/index.html.erb_spec.rb
+++ spec/views/posts/index.html.erb_spec.rb
@@ -5,18 +5,18 @@ RSpec.describe "posts/index", type: :view do
     assign(:posts, [
       Post.create!(
         message: "Message",
-        picture: "Picture"
+        picture: "https://live.staticflickr.com/8651/29886969203_d3d69bac5e_w.jpg"
       ),
       Post.create!(
         message: "Message",
-        picture: "Picture"
+        picture: "https://live.staticflickr.com/8651/29886969203_d3d69bac5e_w.jpg"
       )
     ])
   end
 
   it "renders a list of posts" do
     render
-    assert_select "tr>td", text: "Message".to_s, count: 2
-    assert_select "tr>td", text: "Picture".to_s, count: 2
+    assert_select "tr>td", message: "Message".to_s#, count: 2
+    assert_select "tr>td", picture: "https://live.staticflickr.com/8651/29886969203_d3d69bac5e_w.jpg".to_s#, count: 2
   end
 end
--- spec/views/posts/show.html.erb_spec.rb
+++ spec/views/posts/show.html.erb_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe "posts/show", type: :view do
   before(:each) do
     @post = assign(:post, Post.create!(
       message: "Message",
-      picture: "Picture"
+      picture: "https://live.staticflickr.com/8651/29886969203_d3d69bac5e_w.jpg"
     ))
   end
 
   it "renders attributes in <p>" do
     render
     expect(rendered).to match(/Message/)
-    expect(rendered).to match(/Picture/)
+    expect(rendered).to match("https://live.staticflickr.com/8651/29886969203_d3d69bac5e_w.jpg")
   end
 end
