56612c3c98fbc40d99a11fa8690f780a90aa4ee1
patrick <patrick_sawyer@hotmail.co.uk>
2020-06-07 20:01:31 +0100

    another css test

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,14 +2,17 @@ class LikesController < ApplicationController
 
   def save_like
     @like = Like.new(post_id: params[:post_id], account_id: current_account.id)
+    @post_id = params[:post_id]
 
     respond_to do |format|
       format.json {
         if @like.save
-          { success: true }
+          @success = true
         else
-          { success: false }
+          @success = false
         end
+
+        render "posts/like"
       }
     end
   end
