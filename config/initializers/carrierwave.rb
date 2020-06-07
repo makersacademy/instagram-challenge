29811cef3e68cdf5c94023230712ef3800738113
patrick <patrick_sawyer@hotmail.co.uk>
2020-06-07 09:42:13 +0100

    another heroku test

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,18 +1,18 @@
-#  if !Rails.env.test?
+ if !Rails.env.test?
 
-#    CarrierWave.configure do |config|
+   CarrierWave.configure do |config|
 
-#     config.fog_credentials = {
-#       :provider               => 'AWS',                             
-#       :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],            
-#       :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],     
-#       :region                 => 'eu-west-2' 
-#     }
+    config.fog_credentials = {
+      :provider               => 'AWS',                             
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],            
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],     
+      :region                 => 'eu-west-2' 
+    }
 
-#     config.storage = :fog
-#     config.permissions = 0666
-#     config.cache_dir = "#{Rails.root}/tmp"
-#     config.fog_directory = ENV['FOG_DIRECTORY']
-#   end
-# end
+    config.storage = :fog
+    # config.permissions = 0666
+    config.cache_dir = "#{Rails.root}/tmp"
+    config.fog_directory = ENV['FOG_DIRECTORY']
+  end
+end
 
