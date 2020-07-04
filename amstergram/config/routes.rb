8d67339b9f90a2545086b79d9bb9a1f6c7514502
Rachael <rachaelewins@hotmail.com>
2020-07-04 15:45:47 +0100

    making routes explicit

--- amstergram/config/routes.rb
+++ amstergram/config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  resources :passwords, controller: "clearance/passwords", only: [:create, :new]
+  resource :session, controller: "clearance/sessions", only: [:create]
+
+  resources :users, only: [:create] do
+    resource :password,
+      controller: "clearance/passwords",
+      only: [:edit, :update]
+  end
+
+  get "/sign_in" => "clearance/sessions#new", as: "sign_in"
+  delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
+  get "/sign_up" => "clearance/users#new", as: "sign_up"
   get 'welcome/index'
   root to: 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
