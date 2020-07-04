d969e0d000b3f1f0dfa4cb080808e84f62f32b83
nadinedelia <nadine.delia@yahoo.com>
2020-07-04 21:26:52 +0200

    feed display not working

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,9 +4,9 @@ class PostsController < ApplicationController
         Post.create(post_params)
         redirect_to root_path
       end
+      
       private
       def post_params
         params.require(:post).permit(:description, :image, :user_id)
       end
-      
 end
