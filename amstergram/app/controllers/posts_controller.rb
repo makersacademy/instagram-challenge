005fbaf743c05d9e8a7f9d260bf9f748257d689e
Rachael <rachaelewins@hotmail.com>
2020-07-05 09:29:37 +0100

    image file form there but linked to posts table not active storage

--- amstergram/app/controllers/posts_controller.rb
+++ amstergram/app/controllers/posts_controller.rb
@@ -16,7 +16,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, images: [])
+    params.require(:post).permit(:title, :content, image: [])
   end
 
 end
--- amstergram/app/models/post.rb
+++ amstergram/app/models/post.rb
@@ -1,3 +1,3 @@
 class Post < ApplicationRecord
-  has_many_attached :images
+  has_one_attached :image
 end
