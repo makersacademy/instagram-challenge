8a2e4be6d205bd21267a22755c53a6ce7da98b7e
Rachael <rachaelewins@hotmail.com>
2020-07-05 09:31:05 +0100

    data in active storage

--- amstergram/app/controllers/posts_controller.rb
+++ amstergram/app/controllers/posts_controller.rb
@@ -16,7 +16,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, image: [])
+    params.require(:post).permit(:image)
   end
 
 end
