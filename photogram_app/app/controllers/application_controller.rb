681ddc2f86478df6e5613cc408b9134650152336
Dhara <dhara.patel95@hotmail.co.uk>
2020-08-01 15:10:27 +0100

    Login form added for user to enter email and password

--- photogram_app/app/controllers/application_controller.rb
+++ photogram_app/app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+  helper_method :logged_in?
+
+  def current_user    
+    User.find_by(id: session[:user_id])  
+  end
+
+  def logged_in?
+    !current_user.nil?  
+  end
 end
--- photogram_app/app/controllers/users_controller.rb
+++ photogram_app/app/controllers/users_controller.rb
@@ -10,6 +10,6 @@ class UsersController < ApplicationController
 
   private
    def user_params
-    params.require(:user).permit(:email,:password)
+    params.require(:user).permit(:email, :password)
   end
 end
