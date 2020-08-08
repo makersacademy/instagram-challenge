f758cfd3b68e1de544bc2771306e70a7462d1113
India Rex <face940@gmail.com>
2020-08-08 15:53:10 +0100

    add rspec and rubocop

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -63,12 +63,12 @@ class PostsController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
     # Only allow a list of trusted parameters through.
-    def post_params
-      params.require(:post).permit(:image, :username, :description, :location)
-    end
+  def post_params
+    params.require(:post).permit(:image, :username, :description, :location)
+  end
 end
