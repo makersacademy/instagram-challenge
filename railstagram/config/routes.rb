c906762be9540bad3ccb570b609e62977a36d3de
DillonBarker <barker.dill98@gmail.com>
2020-08-31 09:48:20 +0100

    created the devise users model and migrations

--- railstagram/config/routes.rb
+++ railstagram/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  get 'welcome/index'
+
+
+  root 'welcome#index'
 end
