fddc10a856e715c043f47a01c570c8cdc8d1a72d
DillonBarker <barker.dill98@gmail.com>
2020-09-03 19:14:29 +0100

    adding pg

--- railstagram/db/schema.rb
+++ railstagram/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_08_31_141746) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
