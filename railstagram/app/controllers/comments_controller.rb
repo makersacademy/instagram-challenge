845520e472400d1c8729c0e1c130519a69816cd6
DillonBarker <barker.dill98@gmail.com>
2020-09-05 10:01:22 +0100

    added comment section;

--- railstagram/app/controllers/comments_controller.rb
+++ railstagram/app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+
     def index
         @post = Post.find(params[:post_id])
         @comments = @post.comments
@@ -6,12 +7,10 @@ class CommentsController < ApplicationController
 
     def create
         @post = Post.find(params[:post_id])
-        
         @comment = @post.comments.create(comment_params.merge(commenter: current_user.username))
-
-    
         redirect_to post_path(@post)
     end
+    
     private
         def comment_params
             params.require(:comment).permit(:body, :commenter)
