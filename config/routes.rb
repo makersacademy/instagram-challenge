ddda29d917e939c73dc8b097a6483b449b10a828
Iliyan-Y <maximizebg@gmail.com>
2020-09-25 17:53:08 +0100

    Photo can be uploaded and displayed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root "photos#index"
+  resources :photos
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
