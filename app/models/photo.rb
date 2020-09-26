918c01457cae7e13af35ee097cd1e6bcdb6466a6
Iliyan-Y <maximizebg@gmail.com>
2020-09-26 11:17:44 +0100

    Photo validation added

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,8 @@
 class Photo < ApplicationRecord
   has_one_attached :photo
+  validates :photo, presence: true
+  validates :photo, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png|jpeg)\Z}i,
+    message: 'File must be GIF, JPG or PNG image.'
+    }
 end
--- spec/features/photo_spec.rb
+++ spec/features/photo_spec.rb
@@ -15,11 +15,17 @@ RSpec.feature "Photo", type: :feature do
   scenario "Post without image should raise error" do 
     visit("/")
     click_link "Create Post"
-    attach_file("Photo", Rails.root + "spec/fixtures/test.jpg")
     fill_in "Description", with: "Hello, world 2 !"
     click_button "Post"
-    
-    #expect(page).to have_selector("img")
-    expect(page).to have_content("photo is required")
+    expect(page).to have_content("Photo can't be blank")
+  end
+
+  scenario "Post without image should raise error" do 
+    visit("/")
+    click_link "Create Post"
+    fill_in "Description", with: "Hello, world 2 !"
+    attach_file("Photo", Rails.root + "spec/fixtures/test.txt")
+    click_button "Post"
+    expect(page).to have_content("Photo File must be GIF, JPG or PNG image.")
   end
 end
