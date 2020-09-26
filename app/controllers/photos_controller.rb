024e6fe379bfc8d93f482f373f58a9861fdfc6e6
Iliyan-Y <maximizebg@gmail.com>
2020-09-26 11:00:47 +0100

    Test updated and test for photo validator added

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -28,7 +28,7 @@ class PhotosController < ApplicationController
 
     respond_to do |format|
       if @photo.save
-        format.html { redirect_to @photo, notice: 'Photo was successfully created.' }
+        format.html { redirect_to root_path, notice: 'Your post was successfully created.' }
         format.json { render :show, status: :created, location: @photo }
       else
         format.html { render :new }
--- spec/features/photo_spec.rb
+++ spec/features/photo_spec.rb
@@ -1,8 +1,25 @@
 require 'rails_helper'
 
 RSpec.feature "Photo", type: :feature do
-  scenario "Test is working" do 
+  scenario "Create post" do 
     visit("/")
-    expect(page).to have_content("Photos")
+    click_link "Create Post"
+    attach_file("Photo", Rails.root + "spec/fixtures/test.jpg")
+    fill_in "Description", with: "Hello, world 2 !"
+    click_button "Post"
+    expect(page).to have_content("Your post was successfully created.")
+    expect(page).to have_selector("img")
+    expect(page).to have_content("Hello, world 2 !")
+  end
+
+  scenario "Post without image should raise error" do 
+    visit("/")
+    click_link "Create Post"
+    attach_file("Photo", Rails.root + "spec/fixtures/test.jpg")
+    fill_in "Description", with: "Hello, world 2 !"
+    click_button "Post"
+    
+    #expect(page).to have_selector("img")
+    expect(page).to have_content("photo is required")
   end
 end
