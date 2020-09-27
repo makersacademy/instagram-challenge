0e4b21e7921555e5ca2511a4b582b7c52636f9d5
Iliyan-Y <maximizebg@gmail.com>
2020-09-27 11:51:00 +0100

    Restrictions and style added

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,15 +4,12 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the passed in user here. For example:
-    #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    #
+    
+    if user.present?
+      can :edit, Photo, user_id: user.id
+      can :destroy, Photo, user_id: user.id
+    end
+    
     # The first argument to `can` is the action you are giving the user
     # permission to do.
     # If you pass :manage it will apply to every action. Other common actions
