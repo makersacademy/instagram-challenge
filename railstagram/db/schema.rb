fc52177f56827b883ee9aa2bd7a5a9266274ae17
DillonBarker <barker.dill98@gmail.com>
2020-09-27 14:57:53 +0100

    fixing gemfile

--- railstagram/db/schema.rb
+++ railstagram/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_03_181913) do
+ActiveRecord::Schema.define(version: 2020_08_31_141746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,8 +18,8 @@ ActiveRecord::Schema.define(version: 2020_09_03_181913) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 2020_09_03_181913) do
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
     t.text "body"
-    t.integer "post_id", null: false
+    t.bigint "post_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
@@ -47,7 +47,7 @@ ActiveRecord::Schema.define(version: 2020_09_03_181913) do
 
   create_table "posts", force: :cascade do |t|
     t.text "caption"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
--- railstagram/spec/features/home_page_spec.rb
+++ railstagram/spec/features/home_page_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 RSpec.feature('Home page') do
     scenario('User vists /') do
         visit('/')
-        expect(page).to have_content "Railstagram"
+        expect(page).to have_content "Sign in"
     end
 end
