f053f759ddbf348fd3649218f2e0746aea6375a1
Victoria Jusko <vikjusko@gmail.com>
2020-10-23 21:59:15 +0100

    posts are working when not connected to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates_presence_of :email, :name       
+  validates_presence_of :email, :name
+  has_many :posts, dependent: :destroy       
 end
