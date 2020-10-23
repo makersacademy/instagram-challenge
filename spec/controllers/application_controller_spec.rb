28810603be1742e171b0149ba1d5a2ea22127173
Louis Kirkham <louiskirkham@gmail.com>
2020-10-23 23:11:22 +0100

    added signin index page

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe UsersController, type: :controller do
+RSpec.describe ApplicationController, type: :controller do
 
   describe "GET /" do
     it "responds with 200" do
