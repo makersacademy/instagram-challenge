7ec71cd8828cdb0b9f0095c081ccaea36ee3024e
Victoria Jusko <vikjusko@gmail.com>
2020-10-23 22:33:54 +0100

    posts are connected to user, pictures are being in active database

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,48 +1,68 @@
 class PostsController < ApplicationController
 before_action :authenticate_user!
+  before_action :set_post, only: [:show, :edit, :update, :destroy, :like]
+  before_action :owned_post, only: [:edit, :update, :destroy]
 
-
-	def index
-  @posts = Post.all.order("created_at DESC")
-end
-
-	def new
-    @post = Post.new
-  end
-
-  def create
-    @post = Post.create(post_params)
-    redirect_to posts_url
+  def index
+    @posts = Post.all.order('created_at DESC')
   end
 
   def show
-    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      flash[:success] = "Your post has been created!"
+      redirect_to root_path
+    else
+      flash[:alert] = "Your new post couldn't be created!  Please check the form."
+      render :new
+    end
   end
 
   def edit
-    @post = Post.find(params[:id])
-  end
-
-  def update
-    @post = Post.find(params[:id])
-    if @post.update(post_params)
-      redirect_to @post
-    else 
-      render 'edit'
-    end
   end
   
-  def destroy 
-    @post = Post.find(params[:id])
+  def update
+    if @post.update(post_params)
+      flash[:success] = "Post updated."
+      redirect_to @post
+    else
+      flash[:alert] = "Update failed.  Please check the form."
+      render "edit"
+    end
+  end
+
+ 
+
+  def destroy
     @post.destroy
-    redirect_to posts_url
-  end 
+    flash[:success] = "Your post has been deleted."
+    redirect_to root_path
+  end
+
 
   private
 
- def post_params
-  	params.require(:post).permit(:image, :caption)
-	end
+  def post_params
+    params.require(:post).permit(:image, :caption)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def owned_post
+    unless current_user == @post.user
+      flash[:alert] = "You cannot edit this post!"
+      redirect_to root_path
+    end
+  end
+
 end
-
-
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
 	has_one_attached :image
-	
+	belongs_to :user
 end
 
