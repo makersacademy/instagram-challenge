3643da3a03c1cdeb384f1f8f77a5a8d6e2748766
Victoria Jusko <vikjusko@gmail.com>
2020-10-23 12:55:44 +0100

    active storage added

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-	has_attached_file :image, styles: { :medium => "640x" }
-	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+	has_one_attached :image
+	
 end
 
