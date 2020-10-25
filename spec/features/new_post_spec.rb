cc5721706a1bbd60f10269223c9c016619ffbc3c
Victoria Jusko <vikjusko@gmail.com>
2020-10-25 09:55:04 +0000

    README updated

--- spec/features/new_post_spec.rb
+++ spec/features/new_post_spec.rb
@@ -1,11 +1,11 @@
 describe "#new" do 
-	it "does not let a user post caption without an image" do
-		sign_up
-		click_link "Add a new post"
-		fill_in "post[caption]", with: "By the beach"
-		click_on "Create Post"
-		expect(page).to have_content("Imagecan't be blank")
-	end 
+	 it "does not let a user post caption without an image" do
+ 		 sign_up
+ 		 click_link "Add a new post"
+ 		 fill_in "post[caption]", with: "By the beach"
+ 		 click_on "Create Post"
+ 		 expect(page).to have_content("Imagecan't be blank")
+ 	end 
 
 	# it "let's you post an image" do 
 	# 	sign_up
@@ -16,4 +16,4 @@ describe "#new" do
 	# 	click_on "Create Post"
 	# 	expect(page).to have_content('Posted by Test')
 	# end 
-end 
+end
--- spec/features/view_all_spec.rb
+++ spec/features/view_all_spec.rb
@@ -1,8 +1,8 @@
 describe "#posts/index" do
-	it "Shows all of the posts that are available in timeline after user has signed in" do
-		sign_up
-		expect(page).to have_content("Edit profile")
-		expect(page).to have_content("Logout")
-		expect(page).to have_content("Logged in as Test")
-	end 
-end 
+	 it "Shows all of the posts that are available in timeline after user has signed in" do
+ 		 sign_up
+ 		 expect(page).to have_content("Edit profile")
+ 		 expect(page).to have_content("Logout")
+ 		 expect(page).to have_content("Logged in as Test")
+ 	end 
+end
