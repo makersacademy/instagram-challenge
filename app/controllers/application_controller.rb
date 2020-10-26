e00e63134ad19404cb996ce54332c3cb8f92d392
Linus Mjornstedt <carl.mjornstedt@gmail.com>
2020-10-26 09:01:47 +0000

    Some things still to do, but layouyt done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+    def edit
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit, :update]
 end
