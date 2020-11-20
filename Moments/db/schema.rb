ef72196e72da3a66d08cdbcbbaf85227b2ead6ae
Emily Sesto <emilyalicesesto@gmail.com>
2020-11-20 22:39:27 +0000

    database changed to postgresql

--- Moments/db/schema.rb
+++ Moments/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20201120192845) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
