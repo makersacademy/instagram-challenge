52d272df049fe8f03a488ac7f5290c27add0f252
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-11-21 15:20:20 +0000

    Passed: user can click link sign up from homepage and is redirected to sign up page

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -1,12 +1,19 @@
 require 'rails_helper'
 
 RSpec.feature "Signing up", type: :feature do
+  scenario "User can click on link to sign up form" do 
+    visit '/'
+    click_link "Sign up"
+    expect(page).to have_content("Sign Up")
+  end
   scenario "User can sign up with unique and valid email and password" do
     visit '/'
     click_link "Sign Up"
-    fill_in "email", with: "arakno@makers.com"
-    fill_in "password", with: "makers4L"
-    fill_in "password_confirmation", with: "makers4L"
+    fill_in "Email", with: "arakno@makers.com"
+    fill_in "Password", with: "makers4L"
+    fill_in "Password confirmation", with: "makers4L"
+    click_button "Sign up"
+    expect(page).to have_content("Welcome to Finstagram")
   end
   # scenario "User cannot sign up with unique and invalid email and password" do
   # end
