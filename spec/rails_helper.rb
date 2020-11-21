35b83a0423c1a82716e2e852a049bcf3df50ec17
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-11-21 11:57:51 +0000

    Added simple cov minimum coverage to rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,12 +1,18 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'spec_helper'
+require 'simplecov'
+SimpleCov.start 'rails' do
+  add_filter "/app/channels/"
+  add_filter "/app/jobs/"
+  add_filter "/app/mailers/"
+end
+SimpleCov.minimum_coverage 90
+
 ENV['RAILS_ENV'] ||= 'test'
-
-require File.expand_path('../config/environment', __dir__)
-
+require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'spec_helper'
 require 'rspec/rails'
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
