7d9e86e63533280dc0656e4e8ae38aefafe8d325
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-11-21 16:02:09 +0000

    set up carrierwave file - incorrectly

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,7 +7,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
+  storage :fog
+
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
