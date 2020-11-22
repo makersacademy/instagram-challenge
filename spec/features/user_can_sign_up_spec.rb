fcdf30291198e3ba94da18e931b49939f3cbcf48
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-11-22 12:31:06 +0000

    Added username and full name parameters to user sign up spec

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -8,29 +8,29 @@ RSpec.feature "Signing up", type: :feature do
   end
   scenario "User can sign up with unique and valid email and password" do
     visit '/'
-    sign_up("arakno@makers.com", "makers4L", "makers4L")
+    sign_up("arakno", "Arabella Knowles", "arakno@makers.com", "makers4L", "makers4L")
     expect(page).to have_content("Successfully signed in")
   end
   scenario "User cannot sign up with unique, invalid email and valid password" do
     visit '/'
-    sign_up("araknomakers.com", "makers4L", "makers4L")
+    sign_up("arakno", "Arabella Knowles", "araknomakers.com", "makers4L", "makers4L")
     expect(page).to have_content("Email is invalid")
   end
   scenario "User cannot sign up with non-unique, valid email and valid password" do
     visit '/'
-    sign_up("ara@knomakers.com", "makers4L", "makers4L")
+    sign_up("arakno", "Arabella Knowles", "arakno@makers.com", "makers4L", "makers4L")
     click_link "Sign out"
-    sign_up("ara@knomakers.com", "bakers4L", "bakers4L")
+    sign_up("araknows", "Arabella Nowles", "arakno@makers.com", "bakers4L", "bakers4L")
     expect(page).to have_content("Email has already been taken")
   end
   scenario "User cannot sign up with unique, valid email and invalid password" do
     visit '/'
-    sign_up("arakno@makers.com", "mak", "mak")
+    sign_up("araknows", "Arabella Nowles", "arakno@makers.com", "mak", "mak")
     expect(page).to have_content("Password is too short")
   end
   scenario "User cannot sign up with unique, valid email and password confirmation mismatch" do
     visit '/'
-    sign_up("arakno@makers.com", "makers", "makers2")
+    sign_up("araknows", "Arabella Nowles", "arakno@makers.com", "makers", "makers2")
     expect(page).to have_content("Password confirmation doesn't match")
   end
 end
