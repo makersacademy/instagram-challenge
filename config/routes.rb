54a4b5003bc44984f511edb9447284d5a6d46a56
Maiara Rodrigues <maiarinhah.eu@gmail.com>
2020-11-22 16:42:23 +0000

    I just remebered to commit, set up, model, view and controllers added

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'home/index'
+
+  root to: 'home#index'
+  resources :users, only: [:show]
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
