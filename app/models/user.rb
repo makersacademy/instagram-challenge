be5e5e52aa26bb2d31f0fdbe6ff7ef685b977843
Maiara Rodrigues <maiarinhah.eu@gmail.com>
2020-11-22 21:45:23 +0000

    Adding images to posts, created new models, introduced active storage

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
+  has_many :posts, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'home/index'
 
   root to: 'home#index'
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit, :update]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
