9510f7ff6aa00a4a56cbcb66c73c667e4a6b872b
xavierloos <xavier.loos04@gmail.com>
2021-01-30 14:53:01 +0000

    Importing Bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,7 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
+import 'bootstrap'
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
