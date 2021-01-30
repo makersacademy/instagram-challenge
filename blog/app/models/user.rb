a4fc2109a6d4e2567f35def61a2ecf36aceb9b74
Chris Abdo <christophermabdo@icloud.com>
2021-01-30 13:27:43 +0000

    added user model

---
+++ blog/app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+    has_secure_password
+    
+    attr_accessor :username, :email, :password, :password_confirmation
+
+    validates :email, presence: true, uniqueness: true
+    validates :username, presence: true, uniqueness: true
+end
