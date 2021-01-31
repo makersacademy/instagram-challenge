31125f0b895264a5825528bc973ed4a2fba03477
Chris Whitehouse <chris_whitehouse@hotmail.com>
2021-01-31 18:19:20 +0000

    user show view implemented

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root "pictures#index"
 
   resources :pictures
+
   get 'signup' => 'users#new'
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
