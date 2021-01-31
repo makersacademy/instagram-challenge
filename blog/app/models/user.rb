34273590728b0c7520536e31a3e642d341d65bc3
Chris Abdo <christophermabdo@icloud.com>
2021-01-31 12:35:56 +0000

    User sign up complete

--- blog/app/models/user.rb
+++ blog/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
-    
-    attr_accessor :username, :email, :password, :password_confirmation
+
+    # attr_accessor :username, :email, :password, :password_confirmation
 
     validates :email, presence: true, uniqueness: true
     validates :username, presence: true, uniqueness: true
