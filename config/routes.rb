598da7060a06ff9f1d76098d3c72001317b0dd36
Aman Tank <amantank187@gmail.com>
2021-01-31 18:30:54 +0000

    refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
+
+  resources :users, only: [:show]
 end
---
+++ db/migrate/20210131180836_add_more_fields_to_users.rb
@@ -0,0 +1,5 @@
+class AddMoreFieldsToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :username, :string
+  end
+end
