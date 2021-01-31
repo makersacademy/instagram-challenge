7b9f26cece29afd91c00fc5e455a1f8ec3206e4d
Lisa <bardelli.lisa@gmail.com>
2021-01-31 18:03:58 +0000

    Remove p statements

--- src/app/controllers/comments_controller.rb
+++ src/app/controllers/comments_controller.rb
@@ -7,18 +7,10 @@ class CommentsController < ApplicationController
   end
 
   def create
-    p 'COMMENT'
-    p @comment
     if session[:user_id] != nil
       Comment.create({user_id: session[:user_id], picture_id: @comment.picture_id, message: params[:comment]['message']})
       redirect_to "/pictures/new"
     end
   end
 
-
-  # private
-
-  # def user_params
-  #   params.require(:user).permit(:username, :email, :password)
-  # end
 end
