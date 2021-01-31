c131ece65e1310a602af1f8ddbf2ef94019ff5ee
WillDixon93 <dixon.will1@gmail.com>
2021-01-31 10:35:23 +0000

    redo

--- Fauxgram/config/application.rb
+++ Fauxgram/config/application.rb
@@ -19,7 +19,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module FauxgramApi
+module Fauxgram
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
