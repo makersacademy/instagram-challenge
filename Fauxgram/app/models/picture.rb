cc35ade210098b54da79922433606c4f5e61e37a
WillDixon93 <dixon.will1@gmail.com>
2021-01-31 11:44:36 +0000

    clean redo

--- Fauxgram/app/models/picture.rb
+++
@@ -1,3 +0,0 @@
-class Picture < ApplicationRecord
-  validates_presence_of :img_link, :created_by
-end
--- Fauxgram/spec/factories/pictures.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :picture do
-    img_link { "MyString" }
-    likes { 1 }
-    liked { false }
-    created_by { "MyString" }
-  end
-end
