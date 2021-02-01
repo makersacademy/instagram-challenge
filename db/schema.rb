be1fce90e216c8923ddd4d4fdc26961780491567
Kiki Dawson <kikiadawson@gmail.com>
2021-02-01 07:45:00 +0000

    picture removed from posts db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_31_194050) do
+ActiveRecord::Schema.define(version: 2021_02_01_074349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,6 @@ ActiveRecord::Schema.define(version: 2021_01_31_194050) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "picture"
     t.string "caption"
     t.integer "likes"
     t.bigint "user_id", null: false
