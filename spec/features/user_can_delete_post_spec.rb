0428da76eeb9e14779814c2aa7cabcaefb3d14bb
Natasha McIntyre <natashamc.cs@gmail.com>
2021-02-01 10:24:31 +0000

    amendments following review

--- spec/features/user_can_delete_post_spec.rb
+++ spec/features/user_can_delete_post_spec.rb
@@ -10,9 +10,12 @@ RSpec.feature "Delete post", type: :feature do
   end
 
   scenario "user cannot delete someone else's picture" do
-    user = User.create(username: "TestUser2", email: "Test2@example.com", password: "password")
-    post = user.posts.create(caption: "Testing")
+    # Create a post owned by a different user
+    second_user = User.create(username: "TestUser2", email: "Test2@example.com", password: "password")
+    second_users_post = user.posts.create(caption: "Testing")
+    # Log in as the first user
     log_in
+    # First user should not see second user's post
     expect(first('.each-post')).not_to have_link "Delete"
   end
 end
