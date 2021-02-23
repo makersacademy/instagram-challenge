ec59977f770dd98618b5a967e1ee3eaddf5ac0af
Miranda Wilson <mirandascwilson@gmail.com>
2021-02-23 14:50:19 +0000

    Add Devise config info

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,6 +38,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # suggested default_url_options from Devise
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
