a071c49e9657a820604f2f9723b36d2eae35295b
Miranda Wilson <mirandascwilson@gmail.com>
2021-02-24 21:58:00 +0000

    Add some routes for Post, currently not allowing new posts to be created

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   has_one_attached :image
 
   # def new
@@ -11,8 +11,16 @@ class Post < ApplicationRecord
   end
 
   def create
-    post_params = params["post"]
-    @post = Post.create(post_params.merge(users_id: current_user.id))
-
+    # post_params = params["post"]
+    # @post = Post.create(post_params.merge(users_id: current_user.id))
+    # params.permit!
+    @post = @user.posts.create(post_params)
   end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :created_at, :updated_at, :users_id)
+  end
+
 end
