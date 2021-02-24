78179c759996a2cfc3eca28d01e9a45f2b56b147
Miranda Wilson <mirandascwilson@gmail.com>
2021-02-24 19:58:50 +0000

    Add sign in form to index page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-
   devise_for :users
-  # Suggested root_url by Devise set up
   root to: "home#index"
 end
 
