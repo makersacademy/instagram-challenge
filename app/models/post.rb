436725bde7599d264667ca2371d5e0241e4efc84
Miranda Wilson <mirandascwilson@gmail.com>
2021-02-24 21:30:50 +0000

    Add post title to index post view, to see all existing posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,17 @@ class Post < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  def create
+  # def new
+  #   @post = current_user.posts.build
+  # end
 
+  def index
+    @post = Post.all
+  end
+
+  def create
+    post_params = params["post"]
+    @post = Post.create(post_params.merge(users_id: current_user.id))
 
   end
 end
