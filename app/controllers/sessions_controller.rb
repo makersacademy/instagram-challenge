52067c9b2a264266efd2825f2614d5d85c35a7d4
Mornevanzyl <catbeatsadrum@gmail.com>
2021-02-25 07:46:56 +0000

    Added login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
+    def new
+    end
+
+    def create
+        user = User.find_by(email: params[:email])
+        if user.present? && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect_to root_path, notice: 'Logged in successfully'
+        else
+            flash[:alert] = 'Invalid email or password'
+            render :new
+        end
+    end
+
     def destroy
         session[:user_id] = nil
         redirect_to root_path, notice: 'User have been logged out'
