3ad6da6f833bb42e14f8f5e3a2a5b70c8fcfafba
Mornevanzyl <catbeatsadrum@gmail.com>
2021-02-25 07:20:12 +0000

    Added user logout capability

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,6 +6,7 @@ class RegistrationsController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             redirect_to root_path, notice: 'User account successfully created'
         else
             render :new
