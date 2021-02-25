9d4e94dce955e3087537895dd68e66bef83e5f54
Mornevanzyl <catbeatsadrum@gmail.com>
2021-02-25 06:37:09 +0000

    User registration view/controller added

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,20 @@
+class RegistrationsController < ApplicationController
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to root_path, notice: 'User account successfully created'
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation)
+    end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+# email: string
+# password_digest: string
+
+# password: string virtual
+# password_confirmation: string virtual
+
+class User < ApplicationRecord
+    has_secure_password
+
+    validates :email, presence: true, uniqueness: {
+        # object = person object being validated
+        # data = { model: "Person", attribute: "Username", value: <username> }
+        message: ->(object, data) do
+          "#{data[:value]} already exists. Please enter a new email address"
+        end
+      }, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: 'Must be a valid email address' }
+end
