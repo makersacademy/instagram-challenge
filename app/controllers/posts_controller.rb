473db67d3e1140bd9484128360fb455d8eff55b4
Miranda Wilson <mirandascwilson@gmail.com>
2021-02-26 18:02:29 +0000

    Allow uploading of image but not display it

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -64,6 +64,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :created_at, :updated_at, :user_id)
+      params.require(:post).permit(:title, :created_at, :updated_at, :user_id, :image)
     end
 end
--- spec/system/post_a_pictures_spec.rb
+++ spec/system/post_a_pictures_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "PostAPictures", type: :system do
     driven_by(:rack_test)
   end
 
-  it "user can post a picture and see it displayed" do
+  it "user can post some text and see it displayed" do
     visit "/"
     click_on "Sign up"
     fill_in "user_email", with: DEFAULT_EMAIL
@@ -14,9 +14,23 @@ RSpec.describe "PostAPictures", type: :system do
     click_button "Sign up"
 
     find("#new_post").click
-    fill_in "title", with: "Writing a post"
+    fill_in "post_title", with: "Writing a post"
     click_button "Create Post"
     expect(page).to have_content "Writing a post"
+  end
+
+  it "user can post an image and see it displayed" do
+    visit "/"
+    click_on "Sign up"
+    fill_in "user_email", with: DEFAULT_EMAIL
+    fill_in "user_password", with: DEFAULT_PASSWORD
+    fill_in "user_password_confirmation", with: DEFAULT_PASSWORD
+    click_button "Sign up"
+
+    find("#new_post").click
+    attach_file("post_image", Rails.root + "spec/support/test_image.jpg")
+    click_button "Create Post"
+    expect(page).to have_content "test_image"
   end
 
 
