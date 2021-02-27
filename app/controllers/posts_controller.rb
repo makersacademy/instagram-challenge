9e4ae40e770353d38ea71051eb630a294f3b0d4b
Mornevanzyl <catbeatsadrum@gmail.com>
2021-02-27 09:13:26 +0000

    Added new post action and list posts on index page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,24 @@ class PostsController < ApplicationController
     def index
         @posts = Current.user.posts
     end
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = Current.user.posts.new(post_params)
+        if @post.save
+            redirect_to posts_path, notice: 'Post successfully created'
+            return
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:title, :body)
+    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  validates :title, length: { minimum: 1, maximum: 50 }
+  validates :body, length: { maximum: 280 }
+
+  # after_initialize do
+  #   self.title ||= 'Empty Title'
+  # end
+
 end
