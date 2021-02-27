e391e373b6f65ea3411be194dc14876eff22945e
Sean Edwards <seantrevoredwards@gmail.com>
2021-02-27 07:20:21 +0000

    added edit html and routes as well ad edit to user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
   def show
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
   root to: "home#index"
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
