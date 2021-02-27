2299c71a0d4b5e15cdf6edb3eece748e822186a8
Reeshul <reeshul@hotmail.com>
2021-02-27 16:03:43 +0000

    verification on post submission, image must be attached

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Post < ApplicationRecord
+  validates :image, presence: true
+
   has_attached_file :image, styles: { medium: '640x' }
 
   validates_attachment_content_type :image, content_type: %r{\Aimage/.*\Z}
--- spec/features/creating_posts_spec.rb
+++ spec/features/creating_posts_spec.rb
@@ -12,6 +12,14 @@ feature 'Creating posts' do
     expect(page).to have_content('#coffeeislife')
     expect(page).to have_css("img[src*='coffee.jpg']")
   end
+
+  it 'needs an image to create a post' do
+    visit '/'
+    click_link 'New Post'
+    fill_in 'Caption', with: '#noimagepost'
+    click_button 'Create Post'
+    expect(page).to have_content('Image required to post')
+  end
 end
 
 
