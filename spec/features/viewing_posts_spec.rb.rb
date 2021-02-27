7eca8ccadcbf18b46a65587eaa28d81987f93d68
Reeshul <reeshul@hotmail.com>
2021-02-27 16:39:41 +0000

    added coverage to gitignore

--- spec/features/viewing_posts_spec.rb.rb
+++ spec/features/viewing_posts_spec.rb.rb
@@ -4,7 +4,6 @@ feature 'Index displays a list of posts' do
   scenario 'the index displays correct created post information' do
     post_one = create(:post, caption: "This is post one")
     post_two = create(:post, caption: "This is the second post")
-
     visit '/'
     expect(page).to have_content("This is post one")
     expect(page).to have_content("This is the second post")
