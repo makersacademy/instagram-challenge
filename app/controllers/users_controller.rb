f45d1b02648a7afad420ff43e0b286cf03ccbac6
Sean Edwards <seantrevoredwards@gmail.com>
2021-02-27 07:26:18 +0000

    added update method to user controller and added strong params permit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,14 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  def update
+    current_user.update(user_params)
+    redirect_to current_user
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :name, :bio, :email, :phone)
+  end 
 end
