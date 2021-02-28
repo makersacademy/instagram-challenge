aa266cd6aff2849c8c7c94c5f220f432be9f6a08
RTurney <r.turney93@gmail.com>
2021-02-28 13:49:49 +0000

    Add welcome page. Root welcome page. All tests green

--- binstagram/spec/features/home_page_spec.rb
+++ binstagram/spec/features/home_page_spec.rb
@@ -1,9 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe 'Home page', type: :system do
+RSpec.describe 'Welcome page', type: :system do
 
   describe 'index page' do
-    visit home_index_path
-    expect(page).to have_content('Welcome to Binstagram!')
+    it 'will welcome users to binstagram' do
+      visit ('/welcome')
+      expect(page).to have_content('Welcome to Binstagram!')
+    end
+
+    it 'will route you to welcome page from "/"' do
+      visit('/')
+      expect(page).to have_content("Welcome to Binstagram!")
+    end
   end
 end
