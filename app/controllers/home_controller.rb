b34bfab1758e2abd00f42810620022c5d32ff32e
Sean Edwards <seantrevoredwards@gmail.com>
2021-02-28 08:52:43 +0000

    debugged kaminari now working page pagnates

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     if current_user
-      @posts = Post.order(created_at: :desc)#.page(params[:page]).per(5)
+      @posts = Post.order(created_at: :desc).page(params[:page]).per(5)
     else
       redirect_to new_user_session_path
     end
