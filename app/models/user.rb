3f38a2972177a67ea8e3f6214964f8857f7ba80e
Miranda Wilson <mirandascwilson@gmail.com>
2021-02-28 16:18:12 +0000

    Add usernames for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :email, uniqueness: true
+  validates :username, uniqueness: true
+
 end
