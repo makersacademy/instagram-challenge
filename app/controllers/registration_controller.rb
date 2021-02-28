e097c2809cee6f5da2d6ebb20b615486cb457097
Charlie Fischer <charlie.a.fischer@gmail.com>
2021-02-28 17:53:08 +0000

    username now permitted

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -3,11 +3,11 @@ class RegistrationsController < Devise::RegistrationsController
   private
   
   def sign_up_params
-    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    params.require(:username).permit(:username, :email, :password, :password_confirmation)
   end
   
   def account_update_params
-    params.require(:user).permit(:username, :email, :password, :password_confirmation, :current_password)
+    params.require(:username).permit(:username, :email, :password, :password_confirmation, :current_password)
   end
   
   
