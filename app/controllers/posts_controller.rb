bff015edb2b833226cd43ff8759d1a90618a0744
Mornevanzyl <catbeatsadrum@gmail.com>
2021-02-28 14:01:42 +0000

    Added actions to edit and delete posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
     before_action :require_user_logged_in!
+    before_action :set_post, only: [:show, :edit, :update, :destroy]
 
     def index
         @posts = Current.user.posts
@@ -7,6 +8,20 @@ class PostsController < ApplicationController
 
     def new
         @post = Post.new
+    end
+
+    def update
+        if @post.update(post_params)
+            redirect_to posts_path, notice: 'Post successfully updated'
+            return
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @post.destroy
+        redirect_to posts_path, notice: 'Post has been deleted'
     end
 
     def create
@@ -19,9 +34,17 @@ class PostsController < ApplicationController
         end
     end
 
+    def edit
+
+    end
+
     private
 
     def post_params
         params.require(:post).permit(:title, :body)
+    end
+
+    def set_post
+        @post = Current.user.posts.find(params[:id])
     end
 end
