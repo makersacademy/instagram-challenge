08906092d6ac7b9460912aeacdcaeea5daab62d3
RTurney <r.turney93@gmail.com>
2021-02-28 14:38:51 +0000

    Add validation for users. All tests green

--- binstagram/app/models/user.rb
+++ binstagram/app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :name, :email
+
+  validates :name, presence: true
 end
--- binstagram/spec/models/user_spec.rb
+++ binstagram/spec/models/user_spec.rb
@@ -2,12 +2,22 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  it "will take in a name and email" do
-    user = User.new(name: 'example', email: 'test@email.com')
+  describe 'Valid set up' do
 
-    expect(user).to respond_to(:name)
-    expect(user.name).to eq 'example'
-    expect(user).to respond_to(:email)
-    expect(user.email).to eq 'test@email.com'
+    it "will take in a name and email" do
+      user = User.new(name: 'example', email: 'test@email.com')
+
+      expect(user).to respond_to(:name)
+      expect(user.name).to eq 'example'
+      expect(user).to respond_to(:email)
+      expect(user.email).to eq 'test@email.com'
+    end
   end
+
+  describe 'user is invalid'
+    before :each do
+      user = User.new(name: '', email: 'test@email.com')
+    end
+
+    it { should_not be_valid }
 end
