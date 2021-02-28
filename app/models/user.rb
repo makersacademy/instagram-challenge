b344bb899e56f793d60d4aa990452c0858d3dca2
Reeshul <reeshul@hotmail.com>
2021-02-28 12:36:42 +0000

    styled sign in and sign up views

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,9 @@ class User < ApplicationRecord
 
   validates :username, presence: true, uniqueness: { case_sensitive: false }
   validates :first_name, presence: true 
-  validates :last_name, presence: true 
+  validates :last_name, presence: true
+
+  def full_name
+    [first_name, last_name].join(' ')
+  end
 end
