007eddd82ccebfc7a05fb655883019ea2e9c8d46
Sean Edwards <seantrevoredwards@gmail.com>
2021-02-28 07:43:48 +0000

    added acgive storage and ability to add images

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'home/index'
   root to: "home#index"
   resources :users, only: [:show, :edit, :update]
+  resources :posts, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
