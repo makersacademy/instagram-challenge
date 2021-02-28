4619eb0eba551473a0a3e0797e26640c217dbda9
jshields123 <jackshields@live.co.uk>
2021-02-28 17:14:51 +0000

    added new post function and links to it

--- insta/app/controllers/posts_controller.rb
+++ insta/app/controllers/posts_controller.rb
@@ -6,4 +6,22 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
+  end
+
+  private
+  def article_params
+    params.require(:post).permit(:title, :body)
+  end
 end
--- insta/app/models/post.rb
+++ insta/app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :body, presence: true, length: {
+    minimum: 10  }
 end
