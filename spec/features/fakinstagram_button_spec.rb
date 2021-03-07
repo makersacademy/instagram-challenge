5b75f4aba542e0202c1703d084780716667b9cf8
Glykeria Stravodimou <strglykeria@gmail.com>
2021-03-07 14:05:14 +0000

    change on the fakinstagram button

--- spec/features/fakinstagram_button_spec.rb
+++ spec/features/fakinstagram_button_spec.rb
@@ -5,7 +5,7 @@ RSpec.feature 'user can update a post' do
   scenario 'and you can see it on the feed' do
     sign_up
     click_link('FakInstagram')
-    expect(current_path).to eq('/home/index')
-    expect(page).to have_content('Welcome, username!')
+    expect(current_path).to eq('/images')
+    expect(page).to have_content('Here is your feed, username!')
   end
 end
