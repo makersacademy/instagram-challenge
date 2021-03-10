a711e6c22372556ba81af358ac82ab6d0e04ada0
Lisa <bardelli.lisa@gmail.com>
2021-03-10 09:53:21 +0000

    Fix comment button

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
         @user = User.find_by_email(params[:email])
         if @user && @user.authenticate(params[:password])
           session[:user_id] = @user.id
-          redirect_to posts_url, notice: "Logged in!"
+          redirect_to posts_url
         else
           flash.now[:alert] = "Email or password is invalid"
           render :new
@@ -17,6 +17,6 @@ class SessionsController < ApplicationController
     
       def destroy
         session[:user_id] = nil
-        redirect_to root_url, notice: "Logged out!"
+        redirect_to root_url
       end
 end
