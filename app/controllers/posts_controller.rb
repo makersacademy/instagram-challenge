d0003b1c7ac469d71a59c33b281fcd0f1a8f5f34
Adam Jonas <adamjonas25@gmail.com>
2021-03-27 17:34:37 +0000

    Implements posts appearing newest first

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.order(created_at: :desc)
   end
 
   def new
---
+++ spec/features/viewing_posts_spec.rb
@@ -0,0 +1,18 @@
+feature 'viewing posts' do
+  scenario 'posts appear in reverse chronological order' do
+    visit('/')
+
+    click_button('New Post')
+    attach_file('image', 'spec/files/images/llama.jpeg')
+    fill_in('caption', with: 'First post')
+    click_button('Create Post')
+
+    click_button('New Post')
+    attach_file('image', 'spec/files/images/llama.jpeg')
+    fill_in('caption', with: 'Second post')
+    click_button('Create Post')
+
+    expect(first('.post')).to have_content('Second post')
+    expect(page).to have_content('First post')
+  end
+end
