7fb1aa0d509fb54cbc80609afb52f560b1fdd3fd
Adam Jonas <adamjonas25@gmail.com>
2021-03-27 16:57:43 +0000

    Implements image attachments for posts and displaying images

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,6 +14,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.permit(:caption)
+    params.permit(:caption, :image)
   end
 end
--- spec/features/creating_posts_spec.rb
+++ spec/features/creating_posts_spec.rb
@@ -2,16 +2,17 @@ feature 'creating posts' do
   scenario 'can create a post with a caption' do
     visit('/')
     click_button('New Post')
+    attach_file('image', 'spec/files/images/llama.jpeg')
     fill_in('caption', with: 'Llama')
     click_button('Create Post')
     expect(page).to have_content('Llama')
     expect(current_path).to eq('/posts')
   end
 
-  xscenario 'can create a post with an image' do
+  scenario 'can create a post with an image' do
     visit('/')
     click_button('New Post')
-    attach_file('Image', 'spec/files/images/llama.jpeg')
+    attach_file('image', 'spec/files/images/llama.jpeg')
     fill_in('caption', with: 'Llama')
     click_button('Create Post')
     expect(page).to have_css('img[src*="llama.jpeg"]')
