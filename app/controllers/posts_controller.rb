c66ba3c8cc278570e7e376bdfd05ba2f0de5984b
sandy <pounoussamy.sandy@gmail.com>
2021-03-28 13:41:34 +0100

    added username to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, except: [ :index, :show ]
 
   # GET /posts or /posts.json
   def index
