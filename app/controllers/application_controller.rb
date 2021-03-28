e882c2061b222c144c9f7ba9665af92d8f2a2194
sandy <pounoussamy.sandy@gmail.com>
2021-03-28 12:52:08 +0100

    added extra parameters to devise user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :username, :first_name, :last_name])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:username, :password])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :password_confirmation, :username, :first_name, :last_name])
+  end
+  
+  protect_from_forgery with: :exception
 end
---
+++ db/migrate/20210328112205_add_parameters_to_users.rb
@@ -0,0 +1,8 @@
+class AddParametersToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :username, :string
+    add_index :users, :username, unique: true
+  end
+end
