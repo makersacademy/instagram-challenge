f856d34c991aa84bc15f02de0d5a9a799ad62046
Adam Jonas <adamjonas25@gmail.com>
2021-03-28 22:14:44 +0100

    Adds user validation for editing and deleting posts

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :user do
     first_name { 'Testy' }
     last_name { 'Testerson' }
-    username { 'testy1' }
-    email { 'testy@test.com' }
+    username { 'testy2' }
+    email { 'testy2@test.com' }
     password { 'Password' }
   end
 end
--- spec/features/deleting_posts_spec.rb
+++ spec/features/deleting_posts_spec.rb
@@ -1,4 +1,4 @@
-feature 'editing posts' do
+feature 'deleting posts' do
   scenario 'posts can be deleted' do
     sign_up
     create_post
@@ -10,5 +10,20 @@ feature 'editing posts' do
     click_link('Delete')
     expect(page).not_to have_content('Llama')
     expect(current_path).to eq('/posts')
+  end
+
+  scenario 'only post owner can delete post' do
+    create :post, :with_image
+    sign_up
+
+    expect(page).to have_css('img[src*="llama.jpeg"]')
+    expect(page).to have_content('Llama')
+    expect(current_path).to eq('/')
+
+    expect(page).to have_css('img[src*="llama.jpeg"]')
+    expect(page).to have_content('Llama')
+    expect(page).not_to have_link('Delete')
+    expect(page).not_to have_link('Edit')
+    expect(current_path).to eq('/')
   end
 end
