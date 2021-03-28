b2fd6c6baada3c7680b662509bc8fba816d96f0e
Anna Nawrocka <anna.nawrocka1988@gmail.com>
2021-03-28 18:55:57 +0100

    rename post controller to pages controller

--- app/controllers/posts_controller.rb
+++
@@ -1,6 +0,0 @@
-class PostsController < ApplicationController
-
-  def index
-  end
-  
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {:registrations => "registrations"}
-  root to: 'posts#index'
+  root to: 'pages#index'
 
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
