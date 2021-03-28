eaceb26f6ae8df15976d6968b6ba7f00332b370e
Raf Swiderski <rafswid@icloud.com>
2021-03-28 17:58:31 +0100

    devise: generate views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/', to: "posts#index"
 end
