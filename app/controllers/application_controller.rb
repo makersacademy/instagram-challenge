40ff3a4eb49de3415b98e3826b41c4d48e41f7eb
Raf Swiderski <rafswid@icloud.com>
2021-03-29 12:32:17 +0100

    first view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+    before_action :authenticate_user!
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+      user_params.permit(:username, :email, :first_name, :last_name, :password)
+    end
+
+    devise_parameter_sanitizer.permit(:update_account) do |user_params|
+      user_params.permit(:username, :email, :first_name, :last_name, :password)
+    end
+  end
 end
