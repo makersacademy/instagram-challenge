de118d394e4e315c3a2f91491cf119499e70fa89
Louise Ballantyne <lousballantyne@icloud.com>
2021-04-23 12:17:41 +0100

    User can sign up and it will navigate to a page with their username

---
+++ birdstagram/app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
+end
