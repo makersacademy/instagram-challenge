137408f5a5c4fdf68c5943e606118c83d5fe92f6
Yates101 <alecyates94@gmail.com>
2021-04-24 13:00:10 +0100

    Header component completed and rendered in main app file

---
+++ src/components/Post/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Post extends React.Component {
+  render() {
+    return <article className="Post" ref="Post">
+      <header>
+
+      </header>
+      <div className="Post-image">
+
+      </div>
+      <div className="Post-caption">
+
+      </div>
+    </article>
+  }
+}
+export default Post;
