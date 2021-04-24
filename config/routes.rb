24a30a7a47888e1f8b1b77f424be78875d12c117
Lou Reade <39278690+loushark@users.noreply.github.com>
2021-04-24 13:52:51 +0100

    configured failing rspec gem with different version and rexml gem, refactored routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/login'
-  get 'sessions/welcome'
-  get 'users/new'
-  get 'users/create'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:new, :create]
+     get 'login', to: 'sessions#new'
+     post 'login', to: 'sessions#create'
+     get 'welcome', to: 'sessions#welcome'
 end
