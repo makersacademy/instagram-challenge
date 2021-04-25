e481c8081fc35cc7f8705555cd31923c38e3b723
Jack Hooper <77017281+crotchetycrow@users.noreply.github.com>
2021-04-25 21:01:45 +0100

    Added a create post route & form/button

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+    #dashboard
+    get "/dashboard" => "users#index"
+
+    resources :posts, only: [:new, :create, :show]
+
   root to: "public#index"
 end
