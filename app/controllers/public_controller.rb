6a8b504779737d9b17c59dceac76c215fa658516
Dominic Meddick <dominicmeddick@gmail.com>
2021-05-23 13:20:29 +0100

    Setup complete

---
+++ app/controllers/public_controller.rb
@@ -0,0 +1,8 @@
+class PublicController < ApplicationController
+
+  def homepage
+
+  end
+
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  devise_for :accounts
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: "public#homepage"
+
+end
