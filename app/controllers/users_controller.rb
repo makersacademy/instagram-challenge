b16bf5e52361db362eaebfcceeffd4883df55d11
Abdur Rehman <wabdulrehman@yahoo.com>
2021-05-23 21:27:06 +0100

    image upload as a post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,6 @@ class UsersController < ApplicationController
   end
 
   def update
-    p params
     current_user.update(user_params)
     redirect_to current_user
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :posts
   devise_for :users
-  root "home#index"
+  root "posts#index"
   resources :users
   post "/users/:id", to: "users#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
