cd2988cb20143fb7b1feb2021ce52caa41492725
Mike Jeuga <mikejeuga@gmail.com>
2021-05-24 13:08:06 +0100

    Comments sorted

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,13 +2,11 @@ class CommentsController < ApplicationController
   def new
     @comment = Comment.new
     @user = User.find_by(params[:user_id])
-    @post = Post.find_by(params[:post_id])
+    @post = Post.find_by(id: params[:format])
   end
 
   def create
-    @user = User.find_by(params[:user_id])
-    @post = Post.find_by(params[:post_id])
-    @comment = @post.comments.create(comments_params)
+    @comment = Comment.create(comments_params)
     redirect_to posts_path
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "home#index"
   put "/post/:id/like", to: "posts#like", as: "like"
-
+  # put "/post/:id/comment", to: "posts#comment", as: "comment"
   resources :posts
   resources :users
   resources :comments
