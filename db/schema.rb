e2fecf777d9de28577b92de32de5428acbbfd2ba
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-05-24 01:04:58 +0100

    Code refactor

--- db/schema.rb
+++ db/schema.rb
@@ -22,8 +22,8 @@ ActiveRecord::Schema.define(version: 2021_05_23_233618) do
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index %w[record_type record_id name blob_id], 
-      name: "index_active_storage_attachments_uniqueness", 
+    t.index %w[record_type record_id name blob_id],
+      name: "index_active_storage_attachments_uniqueness",
       unique: true
   end
 
