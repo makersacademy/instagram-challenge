6d5892d0853b284527e86b4cdf164dc2b6d90695
Charlie Slater <charliemslater1701@gmail.com>
2021-06-19 11:13:22 +0100

    validates user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, length: { within: 6..30 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class.new(
+    first_name: "John", last_name: "Doe", email: "john@example.com", password: "example", 
+    created_at: DateTime.now, updated_at: DateTime.now) }
+
+  describe("Valdations") do
+    it("is valid with correctly given attributes") do
+      expect(subject).to be_valid
+    end
+    
+    it("is not valid without an first name") do
+      subject.first_name = ""
+      expect(subject).not_to be_valid  
+    end
+
+    it("is not valid without an last name") do
+      subject.last_name = ""
+      expect(subject).not_to be_valid  
+    end
+
+    it("is not valid without a valid email") do
+      subject.email = "email+notanemail@h."
+      expect(subject).not_to be_valid  
+    end
+
+    it("is not valid with a password less than 6 character") do
+      subject.password = "small"
+      expect(subject).not_to be_valid  
+    end
+
+    it("is not valid with a password more than 30 character") do
+      subject.password = "smallsmallsmallsmallsmallsmallsmall"
+      expect(subject).not_to be_valid  
+    end
+  end
 end
