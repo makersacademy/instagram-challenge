cc3674d19c21ad8bd348cf94e1c1ce548b8fe83b
Charlie Slater <charliemslater1701@gmail.com>
2021-06-19 10:07:26 +0100

    init user model

---
+++ db/migrate/20210619090512_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :password
+
+      t.timestamps
+    end
+  end
+end
