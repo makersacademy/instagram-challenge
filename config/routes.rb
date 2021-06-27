bd2304445dc4d674efc1e18ffc2ba984f3ec8aee
BecaLParker <becaparker@gmail.com>
2021-06-27 17:55:08 +0100

    Adds about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
+  get 'static_pages/about'
   resources :bamposts
   resources :users
-  root 'users#index'
+  root 'static_pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
