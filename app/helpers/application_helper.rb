2f552e92c79e0b7fe4244c5e5cbfcca6f12b9d19
BecaLParker <becaparker@gmail.com>
2021-06-27 19:16:53 +0100

    refactors page titling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  # Returns the full title on a per-page basis.
+def full_title(page_title = '')
+  base_title = "Bamgram" 
+  if page_title.empty?
+    base_title
+  else
+    page_title + " | " + base_title end
+  end
 end
