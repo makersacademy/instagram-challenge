7f50af56009975236ac5e15d0d3df69864de7902
Kerri McMahon <kerrimcm25@gmail.com>
2021-06-28 17:05:11 +0100

    Cannot like more than once

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,11 @@ class LikesController < ApplicationController
   before_action :find_post
 
   def create
+    if already_liked?
+      flash[:notice] = 'Cannot like more than once'
+    else
     @post.likes.create(user_id: current_user.id)
+    end 
     redirect_to post_path(@post)
   end
 
@@ -10,5 +14,10 @@ class LikesController < ApplicationController
 
   def find_post
     @post = Post.find(params[:post_id])
+  end
+
+  def already_liked?
+    Like.where(user_id: current_user.id, post_id:
+    params[:post_id]).exists?
   end
 end
