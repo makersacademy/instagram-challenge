0324353060c2725dc7e1014ca72274f99936ce55
Jenny Moon <jenny0moon@gmail.com>
2021-07-24 15:55:33 +0100

    Update route for Posts_controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_24_132418) do
+ActiveRecord::Schema.define(version: 2021_07_24_143744) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
