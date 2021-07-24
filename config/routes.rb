c7d2cb393b4787d1ead9ef9ae6f73f7518d96b85
Cynthia Fu <yinnycfu@gmail.com>
2021-07-24 23:34:50 +0100

    layout: finish static pages, added root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/about'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
