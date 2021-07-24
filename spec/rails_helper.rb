17b709868beb092e71aabc2c655d8794676c6858
Cynthia Fu <yinnycfu@gmail.com>
2021-07-24 02:00:00 +0100

    Use database_cleaner to trunc database

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,4 +61,8 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
 end
