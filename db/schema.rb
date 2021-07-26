4a2b15bb706164c978475801bf729abb2e749282
Craig Eaton <craigea92@gmail.com>
2021-07-26 09:22:00 +0100

    Added User Page with Template

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_25_164149) do
+ActiveRecord::Schema.define(version: 2021_07_25_171032) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,6 +25,12 @@ ActiveRecord::Schema.define(version: 2021_07_25_164149) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
+    t.string "name"
+    t.string "website"
+    t.text "bio"
+    t.integer "phone"
+    t.string "gender"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
