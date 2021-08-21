cebe51ffa83ae7a7c23be1db1fc07f550941fabd
Else Meijerink <p.e.meijerink@gmail.com>
2021-08-21 23:13:29 +0100

    Configured app to use bootstrap styling

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -7,6 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import '../js/bootstrap_js_files.js'  
 
 Rails.start()
 Turbolinks.start()
