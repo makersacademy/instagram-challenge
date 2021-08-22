61fd44c9f014b81cb9333e3f2b0ba6775423d584
Else Meijerink <p.e.meijerink@gmail.com>
2021-08-22 20:38:20 +0100

    Implemented devise for user flow handling

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 Rails.application.routes.draw do
+  devise_for :users
   get "posts", to: "posts#index"
   root to: "main#index"
   resources :posts
