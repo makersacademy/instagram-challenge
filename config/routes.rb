42e8e4d44c6b6af99f8782cba6bbf3b7eabb7187
Else Meijerink <p.e.meijerink@gmail.com>
2021-08-22 16:28:14 +0100

    Added home route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "posts", to: "posts#index"
+  root to: "main#index"
 end
