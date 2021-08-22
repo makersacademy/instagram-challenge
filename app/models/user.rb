5f9ed2420976e1195a0bba6c4815c364c5af2bcc
Else Meijerink <p.e.meijerink@gmail.com>
2021-08-22 21:45:56 +0100

    Implemented username registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
+  validates_presence_of :username
+  validates :username, uniqueness: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
