cabd231c96d00541996aedb878a8e8fbcd07982c
sandy <pounoussamy.sandy@gmail.com>
2021-09-15 13:32:48 +0100

    added dependency on destroy action for posts, comments and images

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :posts
-  has_many :comments
+  has_many :posts, :dependent => :destroy
+  has_many :comments, :dependent => :destroy
 end
