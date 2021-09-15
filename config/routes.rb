dfcf93735a41050aeb5ee4b306da04e29d694d8d
sandy <pounoussamy.sandy@gmail.com>
2021-09-15 15:14:44 +0100

    Amended routing to handle unauthenticated users

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   resources :comments
   resources :posts
   devise_for :users
-
-  root 'posts#index'
   
+
+  authenticated :user do
+    root 'posts#index', as: :authenticated_root
+  end
+
+  devise_scope :user do
+    root 'devise/sessions#new' 
+  end
 end
