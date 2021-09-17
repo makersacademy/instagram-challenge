1b265725524c374536bd644753e2d916e8677172
eballantine <lizzy.kayballantine@gmail.com>
2021-09-17 20:26:51 +0100

    user can enter details and sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
   def index
   end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_path, notice: "Account created"
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:firstname, :surname, :email, :username, :password)
+    end
 end
--- spec/features/user_sign_up_spec.rb
+++ spec/features/user_sign_up_spec.rb
@@ -15,4 +15,16 @@ feature 'user sign up' do
     expect(page).to have_content('Email')
     expect(page).to have_content('Password')
   end
+
+  scenario 'user can enter details to sign up' do
+    visit '/'
+    click_on 'Sign Up'
+    fill_in 'First Name', with: 'Lizzy'
+    fill_in 'Surname', with: 'Ballantine'
+    fill_in 'Username', with: 'flyingBurrito'
+    fill_in 'Email', with: 'cecil3@gmail.com'
+    fill_in 'Password', with: 'P@ssword!'
+    click_on 'Create Account'
+    expect(page).to have_content('Account created')
+  end
 end
