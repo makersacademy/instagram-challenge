4e43d6101be2fede4a7dc1499234f6f28c6e491c
edpackard <edpackard81@gmail.com>
2021-09-17 22:52:48 +0100

    checking jest-puppeteer working

--- tests/testing.test.js
+++ tests/testing.test.js
@@ -1,9 +1,14 @@
-describe("Google", () => {
+describe("Express Index", () => {
   beforeAll(async () => {
-    await page.goto("https://google.com");
+    // The port is 4444 here because it is your test server, not your real one
+    await page.goto("http://localhost:4444/");
   });
 
-  it('should be titled "Google"', async () => {
-    await expect(page.title()).resolves.toMatch("Google");
+  it("has title Express", async () => {
+    await expect(page).toMatchElement("title", { text: "Express" });
+  });
+
+  it("should be titled Express(alternative test)", async () => {
+    await expect(page.title()).resolves.toMatch("Express");
   });
 });
