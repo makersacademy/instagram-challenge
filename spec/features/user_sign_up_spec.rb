1fd38fbd5b8dcbb51d6f95fb5be95021dd77f605
eballantine <lizzy.kayballantine@gmail.com>
2021-09-17 18:38:05 +0100

    sign up link takes user to sign up form

--- spec/features/user_sign_up_spec.rb
+++ spec/features/user_sign_up_spec.rb
@@ -5,4 +5,14 @@ feature 'user sign up' do
     visit '/'
     expect(page).to have_link('Sign Up')
   end
+
+  scenario 'sign up link from homepage takes user to sign up form' do
+    visit '/'
+    click_on 'Sign Up'
+    expect(page).to have_content('First Name')
+    expect(page).to have_content('Surname')
+    expect(page).to have_content('Username')
+    expect(page).to have_content('Email')
+    expect(page).to have_content('Password')
+  end
 end
