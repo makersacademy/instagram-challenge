325aba32d1aa7720bc0742f50e720c7bc81c666b
edpackard <edpackard81@gmail.com>
2021-09-17 22:26:37 +0100

    setting up puppeteer and jest-puppeteer

---
+++ jest-puppeteer.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  server: {
+    command: "npm run start-test-server",
+    port: 4444,
+    launchTimeout: 30000,
+    // debug: true // Uncomment this to get visibility over web requests in your tests
+  },
+};
