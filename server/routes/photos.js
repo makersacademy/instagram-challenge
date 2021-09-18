1278639130b58e80d8bfbaf46c4f691b845260dd
edpackard <edpackard81@gmail.com>
2021-09-18 21:28:52 +0100

    removing console logs

--- server/routes/photos.js
+++ server/routes/photos.js
@@ -2,8 +2,6 @@ var express = require("express");
 var router = express.Router();
 const photosController = require("../controllers").photos;
 
-console.log("in the routes");
-
 /* GET photos listing. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "photos route" });
