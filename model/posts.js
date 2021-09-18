3b4a7d5f8ee40a5ecefc9fc43d235b1ce4bd0ef4
danroche10 <danielroche10@gmail.com>
2021-09-18 15:56:07 +0100

    added unit tests for post class

--- model/posts.js
+++ model/posts.js
@@ -1,6 +1,13 @@
 const connection = require('../database/connection.js');
 
 class Post {
+
+  static async addPost(text, user_id) {
+    let newPost = await connection.pool.query(
+      "INSERT INTO posts(text, user_id) VALUES($1, $2) RETURNING id, text, user_id;", [text, user_id]);
+    return { id: newPost.rows[0].id, message: newPost.rows[0].text, userID: newPost.rows[0].user_id };
+  }
+
   static async getPosts() {
     let allPosts = await connection.pool.query(
       'SELECT * FROM posts ORDER BY id ASC'
@@ -8,6 +15,11 @@ class Post {
     return allPosts.rows.map((element) => {
       return { id: element.id, message: element.text, userID: element.user_id };
     });
+  }
+
+  static async getPostById(id) {
+    let post = await connection.pool.query("SELECT * FROM posts WHERE id = $1;", [id]);
+    return { id: post.rows[0].id, message: post.rows[0].text, userID: post.rows[0].user_id };
   }
 }
 
