81ad13f8e7cf2d9095e715d0624502e7806c1a51
moby-codes <mubashar.jaffri@gmail.com>
2021-09-19 18:43:19 +0100

    created user register and user login form succesfully and they are flashing errors if the credentials are incorrect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,18 @@
 class SessionsController < ApplicationController
+
+  def create
+    user = User.find_by(email:login_params[:email])
+    if user  && user.authenticate(login_params[:password])
+      session[:user_id] = user.id
+      redirect_to '/posts'
+    else
+      flash[:login_errors] = ['invalid credentials']
+      redirect_to '/'
+    end
+  end
+
+private
+  def login_params
+    params.require(:login).permit(:email, :password)
+  end
 end
