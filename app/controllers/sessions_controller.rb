13cf11510f8832cc197629b8efeffc02299093b4
sa-mcquanzie <sa.mcquanzie@gmail.com>
2021-09-19 17:03:58 +0100

    Get sessions to work

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user.present? && user.authenticate(params[:password])
       session[:user_id] = user.id
-      session[:user_name] = user.name
+      session[:user_handle] = user.handle
       redirect_to posts_path, notice: 'Logged in successfully'
     else
       flash.now[:alert] = 'Invalid email or password'
