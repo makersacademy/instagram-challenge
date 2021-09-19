72c9b9a9637eb597d161181a68e14542e7b1f2f5
sa-mcquanzie <sa.mcquanzie@gmail.com>
2021-09-19 16:57:42 +0100

    Add sessions controller and update config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :likes
+  root 'posts#index'
+
   resources :posts
-  resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users do resources :posts end
+
+  get 'sign_up', to: 'users#new'
+  post 'sign_up', to: 'users#create'
+  get 'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create', as: 'log_in'
+  delete 'logout', to: 'sessions#destroy'
 end
