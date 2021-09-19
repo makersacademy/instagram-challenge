94dc5394ef2910a92d721439449dc58d7226d4a7
sa-mcquanzie <sa.mcquanzie@gmail.com>
2021-09-19 16:47:27 +0100

    Setting up bcrypt for User password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -64,6 +64,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:handle, :email, :password_digest)
+      params.require(:user).permit(:handle, :email, :password)
     end
 end
