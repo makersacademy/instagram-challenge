57833a841a31866615a64f8d7c3e8904024357aa
moby-codes <mubashar.jaffri@gmail.com>
2021-09-20 09:40:00 +0100

    final commit, pictures not saving

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem 'sass-rails', '~> 6.0'
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
+gem "image_processing", ">= 1.2"
 
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.5'
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,26 @@
 class PostsController < ApplicationController
+
+  def new
+    @post = Post.new
+  end
+
+  def index
+    @posts = Post.all.reverse
+    @like = Like.new
+  end
+
+  def show
+   @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to home_path
+  end
+  
+  private def post_params
+    params.require(:post).permit(:message, :created_at)
+  end
+
 end
