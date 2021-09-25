8d97ef116904e5c8e39e2d3e124564e415939a54
danroche10 <danielroche10@gmail.com>
2021-09-25 11:02:11 +0100

    added a procfile

--- database/connection.js
+++ database/connection.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg');
 
-let dbInfo;
+let pool;
 
 const testDb = {
   user: process.env.DB_USER,
@@ -10,7 +10,7 @@ const testDb = {
   port: process.env.DB_PORT,
 };
 
-const db = {
+const devDb = {
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
@@ -18,12 +18,18 @@ const db = {
   port: process.env.DB_PORT,
 };
 
-if (process.env.NODE_ENV === 'test') {
-  dbInfo = testDb;
+if (process.env.NODE_ENV === 'PRODUCTION') {
+  pool = new Pool({
+    // DATABASE_URL from Heroku
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+} else if (process.env.NODE_ENV === 'test') {
+  pool = new Pool(testDb);
 } else {
-  dbInfo = db;
+  pool = new Pool(devDb);
 }
-
-const pool = new Pool(dbInfo);
 
 module.exports.pool = pool;
