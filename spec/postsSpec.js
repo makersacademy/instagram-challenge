2ca332c6276e8a3904fcd981e92e64d004b3e37c
danroche10 <danielroche10@gmail.com>
2021-10-05 17:36:14 +0100

    each method in Post class calling correct method from postsDb. Tests passing

--- spec/postsSpec.js
+++ spec/postsSpec.js
@@ -18,11 +18,15 @@ fdescribe('Post', () => {
     postsDatabaseMock.newPost.and.callFake(() => mockPostsData);
     postsDatabaseMock.all.and.callFake(() => mockPostsData);
     postsDatabaseMock.findById.and.callFake(() => mockPostsData);
-
     postInstance = new Post(postsDatabaseMock);
   });
 
   describe('#addPost', () => {
+    it('should call correct method in postsDb with correct arguments', async () => {
+      await postInstance.addPost('bbc', 2);
+      expect(postsDatabaseMock.newPost).toHaveBeenCalledWith('bbc', 2);
+    });
+
     it('should return array with length 1 based on calling newPost methd in postsDatabase', async () => {
       const newPost = await postInstance.addPost('bbc', 2);
       expect(newPost).toEqual({
@@ -34,6 +38,10 @@ fdescribe('Post', () => {
   });
 
   describe('#getPosts', () => {
+    it('should call correct method in postsDb', async () => {
+      await postInstance.getPosts();
+      expect(postsDatabaseMock.all).toHaveBeenCalled;
+    });
     it('should return array based on calling .all methd in postsDatabase', async () => {
       const allPosts = await postInstance.getPosts();
       expect(allPosts).toEqual([
@@ -47,6 +55,10 @@ fdescribe('Post', () => {
   });
 
   describe('#getPostById', () => {
+    it('should call correct method in postsDb with correct argument', async () => {
+      await postInstance.getPostById(2);
+      expect(postsDatabaseMock.findById).toHaveBeenCalledWith(2);
+    });
     it('should return array with length 1 based on calling findById methd in postsDatabase', async () => {
       const postWithCorrectID = await postInstance.getPostById(2);
       expect(postWithCorrectID).toEqual({
