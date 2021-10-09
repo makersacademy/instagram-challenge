1d4980b1a71cfd8596cca1febd55cec0e8ed9101
danroche10 <danielroche10@gmail.com>
2021-10-09 15:27:56 +0100

    added a comment to usersDatabaseSpec and deleted helpers file

--- spec/databaseLogic/usersDatabaseSpec.js
+++ spec/databaseLogic/usersDatabaseSpec.js
@@ -1,67 +1,68 @@
-describe("usersDatabase", () => {
-  const usersDatabase = require("../../model/databaseLogic/usersDatabase");
-  require("./databasehelpers");
+// need solution for addUser function altering the database size
+describe('usersDatabase', () => {
+  const usersDatabase = require('../../model/databaseLogic/usersDatabase');
+  require('./databasehelpers');
   let userData;
 
-  describe("#findByUsername", () => {
+  describe('#findByUsername', () => {
     beforeAll(async () => {
-      userData = await usersDatabase.findByUsername("test");
+      userData = await usersDatabase.findByUsername('test');
     });
 
-    it("returns array of correct length", () => {
+    it('returns array of correct length', () => {
       expect(userData.length).toEqual(1);
     });
-    it("returns object with correct keys", () => {
-      expect(Object.keys(userData[0])).toContain("id");
-      expect(Object.keys(userData[0])).toContain("username");
-      expect(Object.keys(userData[0])).toContain("password");
-      expect(Object.keys(userData[0])).toContain("email");
+    it('returns object with correct keys', () => {
+      expect(Object.keys(userData[0])).toContain('id');
+      expect(Object.keys(userData[0])).toContain('username');
+      expect(Object.keys(userData[0])).toContain('password');
+      expect(Object.keys(userData[0])).toContain('email');
     });
 
-    it("returns correct values in object", () => {
-      expect(userData[0].username).toEqual("test");
+    it('returns correct values in object', () => {
+      expect(userData[0].username).toEqual('test');
       expect(userData[0].id).toEqual(1);
-      expect(userData[0].password).toEqual("test");
-      expect(userData[0].email).toEqual("test@test");
+      expect(userData[0].password).toEqual('test');
+      expect(userData[0].email).toEqual('test@test');
     });
   });
 
-  describe("#addUser", () => {
-    it("returns array of length 1", async () => {
+  describe('#addUser', () => {
+    it('returns array of length 1', async () => {
       setTimeout(() => {
         userData = usersDatabase.addUser(
-          "new test user",
-          "new-test-password",
-          "newtestemail@test"
+          'new test user',
+          'new-test-password',
+          'newtestemail@test'
         );
         expect(userData.length).toEqual(1);
       }, 500);
     });
-    it("returns object with correct keys", async () => {
+    it('returns object with correct keys', async () => {
       setTimeout(() => {
         userData = usersDatabase.addUser(
-          "new test user",
-          "new-test-password",
-          "newtestemail@test"
+          'new test user',
+          'new-test-password',
+          'newtestemail@test'
         );
-        expect(Object.keys(userData[0])).toContain("id");
-        expect(Object.keys(userData[0])).toContain("username");
-        expect(Object.keys(userData[0])).toContain("password");
-        expect(Object.keys(userData[0])).toContain("email");
+        expect(Object.keys(userData[0])).toContain('id');
+        expect(Object.keys(userData[0])).toContain('username');
+        expect(Object.keys(userData[0])).toContain('password');
+        expect(Object.keys(userData[0])).toContain('email');
       }, 500);
     });
 
-    it("returns correct values in object", async () => {
+    it('returns correct values in object', async () => {
       setTimeout(() => {
         userData = usersDatabase.addUser(
-          "new test user",
-          "new-test-password",
-          "newtestemail@test"
+          'new test user',
+          'new-test-password',
+          'newtestemail@test'
         );
         expect(userData[0].id).toEqual(1);
-        expect(userData[0].username).toEqual("test");
-        expect(userData[0].password).toEqual("test");
-        expect(userData[0].email).toEqual("test@test");
+        expect(userData[0].username).toEqual('test');
+        expect(userData[0].password).toEqual('test');
+        expect(userData[0].email).toEqual('test@test');
       }, 500);
     });
   });
