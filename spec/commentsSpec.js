0e80197f1a5821f5a3e83f611d9392ff7278fff7
danroche10 <danielroche10@gmail.com>
2021-10-19 09:38:22 +0100

    added unit tests for #addLike in Like class with both passing

--- spec/commentsSpec.js
+++ spec/commentsSpec.js
@@ -44,4 +44,6 @@ describe('Comment', () => {
       ]);
     });
   });
+
+  // Add tests for 'getCommentsByPostId'
 });
--- spec/likesSpec.js
+++ spec/likesSpec.js
@@ -1,1 +1,38 @@
-// add same tests as Post class
+describe('Like', () => {
+  const Like = require('../model/likes');
+  let likesDatabaseMock;
+  beforeEach(async () => {
+    likesDatabaseMock = jasmine.createSpyObj('likesDatabase', [
+      'addLike',
+      'getLikesByPostId',
+    ]);
+    mockLikesData = [
+      {
+        id: 1,
+        post_id: 1,
+        user_id: 1,
+      },
+    ];
+    likesDatabaseMock.addLike.and.callFake(() => mockLikesData);
+    likeInstance = new Like(likesDatabaseMock);
+  });
+
+  describe('#addLike', () => {
+    it('should call correct method in likesDb with correct arguments', async () => {
+      await likeInstance.addLike(1, 1);
+      expect(likesDatabaseMock.addLike).toHaveBeenCalledWith(1, 1);
+    });
+
+    it('should return array with length 1 based on calling addLike method in likesDatabase', async () => {
+      const newLike = await likeInstance.addLike(1, 1);
+      expect(newLike).toEqual([
+        {
+          id: 1,
+          post_id: 1,
+          user_id: 1,
+        },
+      ]);
+    });
+  });
+  // Add tests for 'getLikesByPostId'
+});
