f1577960215acdd9d5c2b16fdd482b318ef0e26b
danroche10 <danielroche10@gmail.com>
2021-10-20 10:35:18 +0100

    when user isn't logged in and tries to like or comment, they get redirectd to login page

--- controllers/comments.js
+++ controllers/comments.js
@@ -3,12 +3,16 @@ const Comment = require('../model/comments');
 const CommentsController = {
   async NewComment(req, res) {
     try {
-      const { userId } = req.session.user;
-      const postId = req.params.id;
-      const { newCommentText } = req.body;
-      const comment = new Comment();
-      await comment.addComment(newCommentText, userId, postId);
-      res.redirect(`/posts/${postId}`);
+      if (req.session.authenticated) {
+        const { userId } = req.session.user;
+        const postId = req.params.id;
+        const { newCommentText } = req.body;
+        const comment = new Comment();
+        await comment.addComment(newCommentText, userId, postId);
+        res.redirect(`/posts/${postId}`);
+      } else {
+        res.redirect('/login');
+      }
     } catch (error) {
       console.log(error.message);
     }
--- controllers/likes.js
+++ controllers/likes.js
@@ -2,11 +2,19 @@ const Like = require('../model/likes');
 
 const LikesController = {
   async NewLike(req, res) {
-    const { userId } = req.session.user;
-    const postId = req.params.id;
-    const like = new Like();
-    await like.addLike(userId, postId);
-    res.redirect(`/posts/${postId}`);
+    try {
+      if (req.session.authenticated) {
+        const { userId } = req.session.user;
+        const postId = req.params.id;
+        const like = new Like();
+        await like.addLike(userId, postId);
+        res.redirect(`/posts/${postId}`);
+      } else {
+        res.redirect('/login');
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
   },
 };
 
