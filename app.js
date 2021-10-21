51291aaf389aaa71d34f319969457f4911f43173
danroche10 <danielroche10@gmail.com>
2021-10-21 09:50:47 +0100

    connected views to stylesheet

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ const homeRouter = require('./routes/home');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
