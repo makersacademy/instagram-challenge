c0cadad6d3156cd7b8d95fef7fd1cc6a50cb2511
Jacob Cho <jacobcho@live.com>
2021-11-12 03:41:52 +0000

    DB table added username, name, website, bio, phone, gender

--- insta/db/schema.rb
+++ insta/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_12_011648) do
+ActiveRecord::Schema.define(version: 2021_11_12_033520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 2021_11_12_011648) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
+    t.string "name"
+    t.string "website"
+    t.text "bio"
+    t.integer "phone"
+    t.string "gender"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
