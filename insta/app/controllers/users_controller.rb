4f8213eeb8f18e6210581025a819aed4a8abc8b5
Jacob Cho <jacobcho@live.com>
2021-11-12 09:23:56 +0000

    edit profile complete

--- insta/app/controllers/users_controller.rb
+++ insta/app/controllers/users_controller.rb
@@ -4,6 +4,19 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id]) #finds the current user to edit that profile
   end
+
+  def update
+    current_user.update(user_params) #update the current user based on params in form edit
+    redirect_to current_user         #goes to private method 
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :name, :website, :bio, :email, :phone, :gender)
+  end
+  #Rails Strong Parameters: we permit these params.  If they aren't permitted, it will be blocked for security
 
 end
--- insta/config/routes.rb
+++ insta/config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   
   root to: 'home#index'
 
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
 
 end
