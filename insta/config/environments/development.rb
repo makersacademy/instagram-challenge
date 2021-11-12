05b10c89d1db2fb048c722d19aadd8329b11b3b4
Jacob Cho <jacobcho@live.com>
2021-11-12 01:25:28 +0000

    devise gem set up

--- insta/config/environments/development.rb
+++ insta/config/environments/development.rb
@@ -1,6 +1,10 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+
+  #<---- Set up the default URL for the DEVISE mailer --->
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
