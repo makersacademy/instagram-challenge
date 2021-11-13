a95068cab230c8cd706b69e7fec5d5d907e501f2
Alex Boyling <alexboyling@gmail.com>
2021-11-13 16:35:28 +0000

    added comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  resources :comments
   resources :posts
   resources :users
   resources :user_sessions
-  root :to => "home#index"
-  get '/', to: 'home#index', as: 'main_page'
+  root :to => "posts#index"
+  get '/', to: 'posts#index', as: 'main_page'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_13_160401) do
+ActiveRecord::Schema.define(version: 2021_11_13_162429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -52,5 +62,7 @@ ActiveRecord::Schema.define(version: 2021_11_13_160401) do
     t.index ["single_access_token"], name: "index_users_on_single_access_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
