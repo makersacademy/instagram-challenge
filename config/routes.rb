fa6270241d35d3e399c5be43f2689ef645ff71ef
Sergei Palmer <zigpalmer@gmail.com>
2021-11-13 16:52:23 +0000

    Fixed user sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'home/index'
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   resources :posts
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/migrate/20211113155410_add_username_to_user.rb
+++
@@ -1,6 +0,0 @@
-class AddUsernameToUser < ActiveRecord::Migration[6.1]
-  def change
-    add_column :users, :username, :string
-    add_index :users, :username, unique: true
-  end
-end
