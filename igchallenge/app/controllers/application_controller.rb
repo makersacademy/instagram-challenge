72737ff5a5096d18354ad5c1d6febbf6f4c201bf
AJOsmaston <dajoxx@gmail.com>
2021-11-14 15:51:32 +0000

    rubocop edits

--- igchallenge/app/controllers/application_controller.rb
+++ igchallenge/app/controllers/application_controller.rb
@@ -5,8 +5,10 @@ class ApplicationController < ActionController::Base
     Current.user = User.find_by(id: session[:user_id]) if session[:user_id]
   end
 
-  def require_user_logged_in
-    redirect_to sign_in_path, alert: 'You must be signed in' if Current.user.nil?
-  end
+  # Put this back in when we have posts
+  
+  # def require_user_logged_in
+  #   redirect_to sign_in_path, alert: 'You must be signed in' if Current.user.nil?
+  # end
 
 end
--- igchallenge/app/controllers/registrations_controller.rb
+++ igchallenge/app/controllers/registrations_controller.rb
@@ -1,21 +1,22 @@
 class RegistrationsController < ApplicationController
 
-    def new
-      @user = User.new
-    end
+  def new
+    @user = User.new
+  end
 
-    def create
-      @user = User.new(user_params)
-      if @user.save
-        session[:user_id] = @user.id
-        redirect_to root_path, notice: 'Successfully created account'
-      else
-        redirect_to sign_up_path, notice: "Something went wrong. Please try again"
-      end
-    end
-
-    private
-    def user_params
-      params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: 'Successfully created account'
+    else
+      redirect_to sign_up_path, notice: "Something went wrong. Please try again"
     end
   end
+
+    private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+end
