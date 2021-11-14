5eb5c50c48615c2cb86713e60ca73fb0bf2bba16
Alex Boyling <alexboyling@gmail.com>
2021-11-14 17:08:47 +0000

    like button

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,26 @@
 class HomeController < ApplicationController
+  before_action :set_like, only: %i[ like ]
+
   def index
+    @posts = Post.all
+    @users = User.all
   end
+
+  def like
+    number = @like.likes + 1
+    respond_to do |format|
+      if @like.update(likes: number)
+        format.html { redirect_to root_url, notice: "Like was successfully updated." }
+      else
+        format.html { redirect_to root_url, notice: "Like was unsuccessful." }
+      end
+    end
+  end
+
+  private
+
+  def set_like
+    @like = Post.find(params[:post_id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :user_sessions
   root :to => "posts#index"
   get '/', to: 'posts#index', as: 'main_page'
+  post 'like', to: 'home#like'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
