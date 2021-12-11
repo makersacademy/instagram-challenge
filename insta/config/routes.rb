fc5c8315bf82f90149af86aa05d73028124ccd3a
bmargerison <bmargerison@outlook.com>
2021-12-11 11:58:24 +0000

    Show individual post

---
+++ insta/config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root "posts#index"
+
+  get "/posts", to: "posts#index"
+  get "/posts/:id", to: "posts#show"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
