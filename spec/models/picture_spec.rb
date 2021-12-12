33addd414a9832d9e131e843677cd6df5497446f
Kate Young <89363807+katemyoung@users.noreply.github.com>
2021-12-12 07:07:40 +0000

    Update picture_spec.rb

--- spec/models/picture_spec.rb
+++ spec/models/picture_spec.rb
@@ -3,4 +3,15 @@ require 'rails_helper'
 RSpec.describe Picture, type: :model do
   pending
   it {  is_expected.to be }
+  
+  it 'should not save picture without image attached' do
+    pending
+  end
+  
+  it 'should save a ' do
+    pending
+    Picture.create()
+    expect(Picture.all.length).to increase_by(1)
+  end
+  
 end
