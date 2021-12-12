4a9cadadcb59ee900739fba429704ec6dfbbfbb1
Kate Young <89363807+katemyoung@users.noreply.github.com>
2021-12-12 07:10:44 +0000

    Update index.html.erb_spec.rb

--- spec/views/pictures/index.html.erb_spec.rb
+++ spec/views/pictures/index.html.erb_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "pictures/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  pending 
+  context 'when there is one saved picture' do
+    it 'displays the picture' do
+      Picture.create(...).save
+      render
+      expect(rendered).to have_css(...img src)
+    end
+  end
 end
