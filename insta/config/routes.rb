f45685cdd2fb5a4481c40435dbc837229cb7cb4c
bmargerison <bmargerison@outlook.com>
2021-12-13 10:42:49 +0000

    Only the user who created a post can delete it

--- insta/config/routes.rb
+++ insta/config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   root "posts#index"
 
   resources :posts
-  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
