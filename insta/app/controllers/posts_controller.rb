fad91d3ad85d0a763453475bbb68441d050ac641
bmargerison <bmargerison@outlook.com>
2021-12-13 10:33:17 +0000

    User can delete posts

--- insta/app/controllers/posts_controller.rb
+++ insta/app/controllers/posts_controller.rb
@@ -24,6 +24,12 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to root_path
+  end
+
   private
   def post_params
     params.require(:post).permit(:title, :body, :user, :image)
