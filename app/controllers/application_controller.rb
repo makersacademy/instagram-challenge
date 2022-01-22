7fc80e8a7a253a29e693aa4373affbfeed37cb61
Francesco <francescocollotto@gmail.com>
2022-01-22 18:18:31 +0000

    signed in user are redirected to posts index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(user) 
+    posts_path
+  end
+
   protected
 
   def configure_permitted_parameters
