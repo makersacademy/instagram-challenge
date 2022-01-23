644bafd1e1fa30d87e5acf2a8306c67af5e5a002
Francesco <francescocollotto@gmail.com>
2022-01-23 15:56:37 +0000

    fix bug comments form create an empty instance of comment creating problems when showing the comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_one_attached :pic
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates :text, presence: true
+  has_one_attached :pic
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :posts
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates_length_of :username, :maximum => 25
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
