ff71028ec75cc117c3aaa60f5185699f74078adf
Kate Young <89363807+katemyoung@users.noreply.github.com>
2022-02-09 15:39:16 +0200

    Add feature test for user signup

--- spec/features/user_sign_up_spec.rb
+++ spec/features/user_sign_up_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
-RSpec.feature "UserSignUps", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "UserSignUps", type: :feature do 
+  scenario "a valid signup" do
+    visit new_user_path
+    fill_in("username", with: "Kate")
+    fill_in("password", with: "123")
+    click_button "Sign up"
+    expect(page).to have_content "Thanks for signing up"
+  end
 end
