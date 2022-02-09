08dc5b36dc986833a9430cbb79a6e02f9405cd22
Kate Young <89363807+katemyoung@users.noreply.github.com>
2022-02-09 16:31:01 +0200

    Add route and controller for creating a new user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'pictures#index'
   resources :pictures
+
+  get '/users/new', to: 'users#new'
   
   # For details one DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,5 +60,5 @@ RSpec.configure do |config|
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
-  # config.filter_gems_from_backtrace("gem name")
+  config.filter_gems_from_backtrace("capybara", "rack-test", "rack", "railties")
 end
