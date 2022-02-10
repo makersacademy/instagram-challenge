6d0b376839b5529c8c854361a2f161efb8600a04
Kate Young <89363807+katemyoung@users.noreply.github.com>
2022-02-10 11:04:48 +0200

    Configure devise gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # Devise gem configuration to config default url options
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
