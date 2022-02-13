577eebcff40a0343c20c42ce62ac16a88b55dde3
Vanessa Florio <vanessaflorio1997@gmail.com>
2022-02-13 13:24:03 +0000

    Added tests for post.rb files

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
-end
+    has_one_attached :image
+    belongs_to :user
+    has_many :likes, dependent: :destroy
+  
+    def like_button
+      if likes.count.zero?
+        '🤍'
+      else
+        '❤️'
+      end
+    end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   root 'post#index'
-  
+  resources :posts
 end
