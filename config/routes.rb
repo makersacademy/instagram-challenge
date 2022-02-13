9e2a50f05ff79e298d6361685537d6f4f87dcec9
Tyrone Williams <Tyronemwilliams@outlook.com>
2022-02-13 00:03:18 +0000

    setup part b

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+    # root "application#index" #controller name 'home' and a method name 'index'
+    root "public#homepage"
 end
