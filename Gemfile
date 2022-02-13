541851d4ae9283e5df39baa6c9b900937b14e04c
KiranKukar <kiranrkukar@gmail.com>
2022-02-13 23:25:06 +0000

    set up flash messages

--- Gemfile
+++ Gemfile
@@ -42,7 +42,7 @@ gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 # Reduces boot times through caching; required in config/boot.rb
 gem "bootsnap", require: false
 
-gem 'flash', '~> 0.1.0'
+gem 'flash'
 
 # Use Sass to process CSS
 # gem "sassc-rails"
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_path, notice: 'Logged in successfully'
     else
-      flash.now[:alert] = 'Invalid email or password'
+      flash[:alert] = 'Invalid email or password'
       render :new
     end
   end
