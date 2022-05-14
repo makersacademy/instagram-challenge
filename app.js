0f453a6d9ba97afe887b57d7daa84e41000cf0bf
Michael Su <mailmsuu@gmail.com>
2022-05-14 12:11:38 +0100

    wrote and passed first test to show title on homepage, added some other files and routes

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require("express-session");
+const methodOverride = require("method-override");
+var hbs = require('hbs');
+const moment = require('moment');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const homeRouter = require("./routes/home");
 
 var app = express();
 
@@ -18,9 +21,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride("_method"));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', homeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
