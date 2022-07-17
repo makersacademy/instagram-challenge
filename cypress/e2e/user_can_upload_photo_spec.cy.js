40a3f9ed143b14b89a94d19cef2a74e4f5d166ad
Kay <kard@hotmail.co.uk>
2022-07-17 14:42:34 +0100

    form to upload and then see a photo works in the browser but not passing the test

--- cypress/e2e/user_can_upload_photo_spec.cy.js
+++ cypress/e2e/user_can_upload_photo_spec.cy.js
@@ -1,8 +1,22 @@
 describe('user can add post',() => {
-  it('allows a user to upload a photo',() => {
+  it('shows the user a form to upload an image',() => {
     cy.visit('/')
     cy.get('#add-post-button').click()
-    cy.get('')
+    cy.get('#name')
+    cy.get('#desc')
+    cy.get('#image')
+    cy.get('button')
   })
- 
+
+  it('allows the user to upload an image that shows on homepage - testing desc and title only',() => {
+    cy.visit('/')
+    cy.get('#add-post-button').click()
+    cy.get('#name').type('Fake photo');
+    cy.get('#desc').type('A lovely photo');
+    cy.get('button').click();
+    cy.get('#title').should('contain', 'Instagram');
+    cy.get('div.image').first().should('contain', 'Fake Photo');
+    // fails because you have to upload a photo for it to work
+  })
+
 })
