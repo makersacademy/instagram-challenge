da26d683b1247c0f127a1b2765633e72ed5d5222
Kay <kard@hotmail.co.uk>
2022-07-17 15:02:26 +0100

    removed excess code in the app.js

--- app.js
+++ app.js
@@ -68,39 +68,4 @@ const upload = multer({ storage: storage });
 
 // image handler
 
-// app.get('/', (req, res) => {
-//   imgModel.find({}, (err, items) => {
-//       if (err) {
-//           console.log(err);
-//           res.status(500).send('An error occurred', err);
-//       }
-//       else {
-//           res.render('imagesPage', { items: items });
-//       }
-//   });
-// });
-
-// image uploader
-
-// app.post('/', upload.single('image'), (req, res, next) => {
-  
-//   var obj = {
-//       name: req.body.name,
-//       desc: req.body.desc,
-//       img: {
-//           data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
-//           contentType: 'image/png'
-//       }
-//   }
-//   imgModel.create(obj, (err, item) => {
-//       if (err) {
-//           console.log(err);
-//       }
-//       else {
-//           // item.save();
-//           res.redirect('/');
-//       }
-//   });
-// });
-
 module.exports = app;
