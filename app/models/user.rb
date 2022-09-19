3d9352ca2d85dd08b6c32fb422c05e640cd5e832
sophie <soph_lew@hotmail.co.uk>
2022-09-19 22:32:47 +0100

    added sign in, sign up, and log out feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+        #  :confirmable, :lockable, :timeoutable, :omniauthable
 end
