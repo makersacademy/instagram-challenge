2c1d47cbca4ee21da245d9015dd77bf360e11729
sophie <soph_lew@hotmail.co.uk>
2022-09-19 22:52:07 +0100

    sign out feature fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :validatable
+         #:trackable,
         #  :confirmable, :lockable, :timeoutable, :omniauthable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
   root to: "home#index"
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
